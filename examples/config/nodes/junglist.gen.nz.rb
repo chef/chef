04d774ac51418692ba7709a2edda8ea88d1a4775
AJ Christensen <aj@junglist.gen.nz>
2008-09-24 03:04:51 +1200

    Node file for junglist

---
+++ examples/config/nodes/junglist.gen.nz.rb
@@ -0,0 +1,14 @@
+##
+# Nodes should have a unique name
+##
+name "junglist.gen.nz"
+
+##
+# Nodes can set arbitrary arguments
+##
+owner "AJ Christensen"
+
+##
+# Nodes should have recipes
+##
+recipes "tempfile", "fakefile"
