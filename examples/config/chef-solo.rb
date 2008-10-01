915cc3f82c8044c5c5528b357898404494085d5c
Adam Jacob <adam@hjksolutions.com>
2008-09-30 21:49:25 -0700

    Updating the example config to sue file_cache_path

--- examples/config/chef-solo.rb
+++ examples/config/chef-solo.rb
@@ -6,5 +6,6 @@ node_path         File.join(File.dirname(__FILE__), "nodes")
 search_index_path File.join(File.dirname(__FILE__), "..", "search_index")
 log_level         :info
 file_store_path   "/tmp/chef"
+file_cache_path   "/tmp/chef/cache"
 
 Chef::Log::Formatter.show_time = false
