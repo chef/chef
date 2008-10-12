6b7e37ea4c1f0af99e51711a807b3b9873d6653a
Adam Jacob <adam@hjksolutions.com>
2008-10-11 22:11:49 -0700

    Updating to no longer use UV - pure ruby syntax gem instead

--- chef-server/lib/init.rb
+++ chef-server/lib/init.rb
@@ -117,6 +117,7 @@ Merb.push_path(:helper, File.join(File.dirname(__FILE__), "helpers"))
 Merb.push_path(:public, File.join(File.dirname(__FILE__), "public"))
 
 require 'merb-haml'
+require 'syntax/convertors/html'
 
 
 #
