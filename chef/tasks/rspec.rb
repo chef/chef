5c83cd36db72b93f532e9d6f8d6619d65c814484
Bryan McLellan <btm@loftninjas.org>
2008-10-13 19:24:57 -0700

    lib/spec/translator was removed in rspec 1.1.5 (rspec git 0ecb1014)   remove require, commented example code, duplicate require for spectask

--- chef/tasks/rspec.rb
+++ chef/tasks/rspec.rb
@@ -2,14 +2,6 @@ require 'rubygems'
 require 'rake'
 require 'spec/rake/spectask'
 
-#desc "Run all examples"
-#Spec::Rake::SpecTask.new('spec') do |t|
-#  t.spec_files = FileList[File.join(File.dirname(__FILE__), "..", "spec", "**", "*.rb")]
-#end
-
-require 'spec/rake/spectask'
-require 'spec/translator'
-
 CHEF_ROOT = File.join(File.dirname(__FILE__), "..")
 
 task :default => :spec
