70432073c98601ac056f220df496070e533a43b7
Adam Jacob <adam@hjksolutions.com>
2009-02-03 15:31:26 -0800

    Updating search to support attributes and not running a block.

--- chef/lib/chef/recipe.rb
+++ chef/lib/chef/recipe.rb
@@ -89,13 +89,17 @@ class Chef
       @collection.resources(*args)
     end
     
-    def search(type, query, &block)
+    def search(type, query, attributes=[], &block)
       Chef::Log.debug("Searching #{type} index with #{query}")
       r = Chef::REST.new(Chef::Config[:search_url])
-      results = r.get_rest("search/#{type}?q=#{query}")
+      results = r.get_rest("search/#{type}?q=#{query}&a=#{attributes.join(',')}")
       Chef::Log.debug("Searching #{type} index with #{query} returned #{results.length} entries")
-      results.each do |sr|
-        block.call(sr)
+      if block
+        results.each do |sr|
+          block.call(sr)
+        end
+      else
+        results
       end
     end
     
