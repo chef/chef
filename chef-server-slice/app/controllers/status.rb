1511c704e553ebb44ebd02f95f3d2049a01a5fc9
zeusfaber <joe@joetify.com>
2009-03-13 08:21:45 -0700

    changed author info

--- chef-server-slice/app/controllers/status.rb
+++ chef-server-slice/app/controllers/status.rb
@@ -1,7 +1,6 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Author:: Christopher Brown (<cb@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Joe Williams (joe@joetify.com)
+# Copyright:: Copyright (c) 2009 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- chef-server-slice/app/helpers/status_helper.rb
+++ chef-server-slice/app/helpers/status_helper.rb
@@ -1,6 +1,6 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Joe Williams (joe@joetify.com)
+# Copyright:: Copyright (c) 2009 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
