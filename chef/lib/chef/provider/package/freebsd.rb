e9ec201f0727b3ae65e864219da00fd4fabf0f19
Matthew Landauer <matthew@openaustralia.org>
2009-03-13 13:17:50 +1100

    Updated authors

--- chef/lib/chef/provider/package/freebsd.rb
+++ chef/lib/chef/provider/package/freebsd.rb
@@ -1,6 +1,7 @@
 #
-# Author:: Bryan McLellan (btm@loftninjas.org)
-# Copyright:: Copyright (c) 2009 Bryan McLellan
+# Authors:: Bryan McLellan (btm@loftninjas.org)
+#           Matthew Landauer (matthew@openaustralia.org)
+# Copyright:: Copyright (c) 2009 Bryan McLellan, Matthew Landauer
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
