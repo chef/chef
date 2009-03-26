5fe1641804850a93059b41b8644b184e372b8b4b
AJ Christensen <aj@junglist.gen.nz>
2009-03-27 03:22:59 +1300

    CHEF-169: support 0.8 and 0.9 couchdb versions with different view uri's

--- chef/lib/chef/config.rb
+++ chef/lib/chef/config.rb
@@ -63,6 +63,7 @@ class Chef
       :remotefile_url => "http://localhost:4000",
       :search_url => "http://localhost:4000",
       :couchdb_database => "chef",
+      :couchdb_version => nil,
       :openid_store_path => "/var/chef/openid/db",
       :openid_cstore_path => "/var/chef/openid/cstore",
       :file_cache_path => "/var/chef/cache",
