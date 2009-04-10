74750012d4ea84f0e5fe6118868c15dd1dd8ebb0
Adam Jacob <adam@hjksolutions.com>
2009-04-09 18:51:03 -0700

    Updating Link provider delete action specs, for chef-208

--- chef/lib/chef/provider/link.rb
+++ chef/lib/chef/provider/link.rb
@@ -69,13 +69,13 @@ class Chef
       
       def action_delete
         if ::File.exists?(@new_resource.target_file)
-	      if @new_resource.link_type == :symbolic 
-	        if ::File.symlink?(@new_resource.target_file)
+  	      if @new_resource.link_type == :symbolic 
+  	        if ::File.symlink?(@new_resource.target_file)
               Chef::Log.info("Deleting #{@new_resource} at #{@new_resource.target_file}")
               ::File.delete(@new_resource.target_file)
               @new_resource.updated = true
             else
-              raise "Cannot delete #{@new_resource} at #{@new_resource.target_file}! Not a symbolic link."
+              raise Chef::Exceptions::Link, "Cannot delete #{@new_resource} at #{@new_resource.target_file}! Not a symbolic link."
             end
           elsif @new_resource.link_type == :hard 
             if ::File.exists?(@new_resource.to) && ::File.stat(@current_resource.target_file).ino == ::File.stat(@new_resource.to).ino
@@ -83,7 +83,7 @@ class Chef
               ::File.delete(@new_resource.target_file)
               @new_resource.updated = true
             else
-              raise "Cannot delete #{@new_resource} at #{@new_resource.target_file}! Not a hard link."
+              raise Chef::Exceptions::Link, "Cannot delete #{@new_resource} at #{@new_resource.target_file}! Not a hard link."
             end
           end
         end
