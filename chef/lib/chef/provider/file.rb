2d23fa5c6f40bafb328bc6d64c2c950dc64ca134
Nuo Yan <nuoyan@nuo-yans-macbook-pro.local>
2009-04-22 10:39:06 -0700

    fixing bug CHEF-195

--- chef/lib/chef/provider/file.rb
+++ chef/lib/chef/provider/file.rb
@@ -159,7 +159,7 @@ class Chef
       
       def backup(file=nil)
         file ||= @new_resource.path
-        if @new_resource.backup && ::File.exist?(file)
+        if @new_resource.backup >0 && ::File.exist?(file)
           time = Time.now
           savetime = time.strftime("%Y%m%d%H%M%S")
           backup_filename = "#{@new_resource.path}.chef-#{savetime}"
