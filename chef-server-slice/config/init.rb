380846b3fecff141c46cfc69af880006a1a9f6c2
Mark Imbriaco <mark@imbriaco.com>
2009-04-27 01:29:15 -0400

    Make logging configuration consistently use Chef::Config to get log level and log file path.

--- chef-server-slice/config/init.rb
+++ chef-server-slice/config/init.rb
@@ -39,6 +39,6 @@ Merb::Config.use do |c|
   c[:exception_details] = true
   c[:reload_classes] = false
   c[:log_level] = Chef::Config[:log_level]
-  c[:log_stream] = Chef::Config[:log_location]
+  c[:log_file] = Chef::Config[:log_location]
 end
 
