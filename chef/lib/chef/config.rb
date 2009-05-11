e4dd398e235679a461c76985cbd5421615c83d36
AJ Christensen <aj@junglist.gen.nz>
2009-05-12 03:46:28 +1200

    CHEF-151: Config class cleanup
      Sort the config options alphabetically.
      Add manage_secret_key method back.

--- chef/lib/chef/config.rb
+++ chef/lib/chef/config.rb
@@ -22,53 +22,70 @@ require 'mixlib/config'
 class Chef::Config
   extend(Mixlib::Config)
 
-  daemonize nil
-  user nil
-  group nil
-  pid_file nil
-  delay 0
-  interval 1800
-  splay nil
-  solo  false
-  json_attribs nil
+  authorized_openid_identifiers nil
   cookbook_path [ "/var/chef/site-cookbooks", "/var/chef/cookbooks" ]
-  validation_token nil
-  node_path "/var/chef/node"
+  couchdb_database "chef"
+  couchdb_url "http://localhost:5984"
+  couchdb_version nil
+  daemonize nil
+  delay 0
+  executable_path ENV['PATH'] ? ENV['PATH'].split(File::PATH_SEPARATOR) : []
+  file_cache_path "/var/chef/cache"
   file_store_path "/var/chef/store"
-  search_index_path "/var/chef/search_index"
+  group nil
+  http_retry_count 5
+  http_retry_delay 5
+  interval 1800
+  json_attribs nil
   log_level :info
   log_location STDOUT
+  node_name nil
+  node_path "/var/chef/node"
+  openid_cstore_couchdb false
+  openid_cstore_path "/var/chef/openid/cstore"
   openid_providers nil
-  ssl_verify_mode :verify_none
+  openid_store_couchdb false
+  openid_store_path "/var/chef/openid/db"
+  openid_url "http://localhost:4001"
+  pid_file nil
+  queue_host "localhost"
+  queue_password ""
+  queue_port 61613
+  queue_retry_count 5
+  queue_retry_count 5
+  queue_retry_delay 5
+  queue_retry_delay 5
+  queue_user ""
+  registration_url "http://localhost:4000"
+  remotefile_url "http://localhost:4000"
+  rest_timeout 60
+  run_command_stderr_timeout 120
+  run_command_stdout_timeout 120
+  search_index_path "/var/chef/search_index"
+  search_url "http://localhost:4000"
+  solo  false
+  splay nil
   ssl_client_cert ""
   ssl_client_key ""
-  rest_timeout 60
-  couchdb_url "http://localhost:5984"
-  registration_url "http://localhost:4000"
-  openid_url "http://localhost:4001"
+  ssl_verify_mode :verify_none
   template_url "http://localhost:4000"
-  remotefile_url "http://localhost:4000"
-  search_url "http://localhost:4000"
-  couchdb_version nil
-  couchdb_database "chef"
-  openid_store_couchdb false
-  openid_cstore_couchdb false
-  openid_store_path "/var/chef/openid/db"
-  openid_cstore_path "/var/chef/openid/cstore"
-  file_cache_path "/var/chef/cache"
-  node_name nil
-  executable_path ENV['PATH'] ? ENV['PATH'].split(File::PATH_SEPARATOR) : []
-  http_retry_delay 5
-  http_retry_count 5
-  queue_retry_delay 5
-  queue_retry_count 5
-  queue_retry_delay 5
-  queue_retry_count 5
-  queue_user ""
-  queue_password ""
-  queue_host "localhost"
-  queue_port 61613
-  run_command_stdout_timeout 120
-  run_command_stderr_timeout 120
-  authorized_openid_identifiers nil
+  user nil
+  validation_token nil
+  
+  # Manages the chef secret session key
+  # === Returns
+  # <newkey>:: A new or retrieved session key
+  #
+  def manage_secret_key
+    newkey = nil
+    if Chef::FileCache.has_key?("chef_server_cookie_id")
+      newkey = Chef::FileCache.load("chef_server_cookie_id")
+    else
+      chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+      newkey = ""
+      40.times { |i| newkey << chars[rand(chars.size-1)] }
+      Chef::FileCache.store("chef_server_cookie_id", newkey)
+    end
+    newkey
+  end
 end
