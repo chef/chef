014a69af1cdce619de82afaf6cdb4e6ac658fede
Adam Jacob <adam@opscode.com>
2009-06-09 18:28:57 -0700

    I missed a test

--- chef/spec/unit/provider/remote_file_spec.rb
+++ chef/spec/unit/provider/remote_file_spec.rb
@@ -41,6 +41,7 @@ describe Chef::Provider::RemoteFile, "do_remote_file" do
     @rest = mock(Chef::REST, { })
     @tempfile = mock(Tempfile, { :path => "/tmp/foo", })
     @tempfile.stub!(:open).and_return(@tempfile)
+    @tempfile.stub!(:closed?).and_return(false)
     @tempfile.stub!(:close)
     @rest.stub!(:get_rest).and_return(@tempfile)
     @resource = Chef::Resource::RemoteFile.new("seattle")
