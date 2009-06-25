75834e5ebf100f1b33399f4e9a7dced3a6d527d1
AJ Christensen <aj@opscode.com>
2009-06-25 20:52:18 +1200

    CHEF-401: Fix regression for passenger.
    I adjusted the chef-server to use log_stream, missed this part.. :(

--- chef-server/config/environments/production.rb
+++ chef-server/config/environments/production.rb
@@ -3,7 +3,7 @@ Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
   c[:log_level] = Chef::Config[:log_level]
-  c[:log_file] = Chef::Config[:log_location]
+  c[:log_stream] = Chef::Config[:log_location]
   # or redirect logger using IO handle
   # c[:log_stream] = STDOUT
 }
