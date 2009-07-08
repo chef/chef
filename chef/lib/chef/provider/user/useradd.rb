d692c967b5ae36336b4dbc55e3eb20049c8b6466
Grant Zanetti <grort@mani.ac.nz>
2009-07-08 14:40:16 +1200

    CHEF-437: Fix bug where manage_home and no home set resulted in a home of -m   * If home isn't set then don;t use -d option   * Put home in quotes in case there's anything else funky

--- chef/lib/chef/provider/user/useradd.rb
+++ chef/lib/chef/provider/user/useradd.rb
@@ -88,13 +88,13 @@ class Chef
               end
             end
           end
-          if @current_resource.home != @new_resource.home
+          if @current_resource.home != @new_resource.home && @new_resource.home
             if @new_resource.supports[:manage_home]
               Chef::Log.debug("Managing the home directory for #{@new_resource}")
-              opts << " -d #{@new_resource.home} -m"
+              opts << " -d '#{@new_resource.home}' -m"
             else
               Chef::Log.debug("Setting #{@new_resource} home to #{@new_resource.home}")
-              opts << " -d #{@new_resource.home}"
+              opts << " -d '#{@new_resource.home}'"
             end
           end
           opts << " #{@new_resource.username}"
