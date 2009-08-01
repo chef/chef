328a7bf568328419031bd86b07fcfe4fdd4e8e55
Adam Jacob <adam@opscode.com>
2009-07-31 18:43:39 -0700

    Adding Chef::Node::Attribute, which puts all the smarts in node attributes

---
+++ chef/spec/unit/node/attribute_spec.rb
@@ -0,0 +1,323 @@
+#
+# Author:: Adam Jacob (<adam@opscode.com>)
+# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+require 'chef/node/attribute'
+
+describe Chef::Node::Attribute do 
+  before(:each) do
+    @attribute_hash = 
+      {"dmi"=>{},
+        "command"=>{"ps"=>"ps -ef"},
+        "platform_version"=>"10.5.7",
+        "platform"=>"mac_os_x",
+        "ipaddress"=>"192.168.0.117",
+        "network"=>
+    {"default_interface"=>"en1",
+      "interfaces"=>
+    {"vmnet1"=>
+      {"flags"=>
+        ["UP", "BROADCAST", "SMART", "RUNNING", "SIMPLEX", "MULTICAST"],
+          "number"=>"1",
+          "addresses"=>
+        {"00:50:56:c0:00:01"=>{"family"=>"lladdr"},
+          "192.168.110.1"=>
+        {"broadcast"=>"192.168.110.255",
+          "netmask"=>"255.255.255.0",
+          "family"=>"inet"}},
+          "mtu"=>"1500",
+          "type"=>"vmnet",
+          "arp"=>{"192.168.110.255"=>"ff:ff:ff:ff:ff:ff"},
+          "encapsulation"=>"Ethernet"},
+          "stf0"=>
+        {"flags"=>[],
+          "number"=>"0",
+          "addresses"=>{},
+          "mtu"=>"1280",
+          "type"=>"stf",
+          "encapsulation"=>"6to4"},
+          "lo0"=>
+        {"flags"=>["UP", "LOOPBACK", "RUNNING", "MULTICAST"],
+          "number"=>"0",
+          "addresses"=>
+        {"::1"=>{"scope"=>"Node", "prefixlen"=>"128", "family"=>"inet6"},
+          "127.0.0.1"=>{"netmask"=>"255.0.0.0", "family"=>"inet"},
+          "fe80::1"=>{"scope"=>"Link", "prefixlen"=>"64", "family"=>"inet6"}},
+          "mtu"=>"16384",
+          "type"=>"lo",
+          "encapsulation"=>"Loopback"},
+          "gif0"=>
+        {"flags"=>["POINTOPOINT", "MULTICAST"],
+          "number"=>"0",
+          "addresses"=>{},
+          "mtu"=>"1280",
+          "type"=>"gif",
+          "encapsulation"=>"IPIP"},
+          "vmnet8"=>
+        {"flags"=>
+          ["UP", "BROADCAST", "SMART", "RUNNING", "SIMPLEX", "MULTICAST"],
+            "number"=>"8",
+            "addresses"=>
+          {"192.168.4.1"=>
+            {"broadcast"=>"192.168.4.255",
+              "netmask"=>"255.255.255.0",
+              "family"=>"inet"},
+              "00:50:56:c0:00:08"=>{"family"=>"lladdr"}},
+              "mtu"=>"1500",
+              "type"=>"vmnet",
+              "arp"=>{"192.168.4.255"=>"ff:ff:ff:ff:ff:ff"},
+              "encapsulation"=>"Ethernet"},
+              "en0"=>
+            {"status"=>"inactive",
+              "flags"=>
+            ["UP", "BROADCAST", "SMART", "RUNNING", "SIMPLEX", "MULTICAST"],
+              "number"=>"0",
+              "addresses"=>{"00:23:32:b0:32:f2"=>{"family"=>"lladdr"}},
+              "mtu"=>"1500",
+              "media"=>
+            {"supported"=>
+              {"autoselect"=>{"options"=>[]},
+                "none"=>{"options"=>[]},
+                "1000baseT"=>
+              {"options"=>["full-duplex", "flow-control", "hw-loopback"]},
+                "10baseT/UTP"=>
+              {"options"=>
+                ["half-duplex", "full-duplex", "flow-control", "hw-loopback"]},
+                  "100baseTX"=>
+                {"options"=>
+                  ["half-duplex", "full-duplex", "flow-control", "hw-loopback"]}},
+                    "selected"=>{"autoselect"=>{"options"=>[]}}},
+                    "type"=>"en",
+                    "encapsulation"=>"Ethernet"},
+                    "en1"=>
+                  {"status"=>"active",
+                    "flags"=>
+                  ["UP", "BROADCAST", "SMART", "RUNNING", "SIMPLEX", "MULTICAST"],
+                    "number"=>"1",
+                    "addresses"=>
+                  {"fe80::223:6cff:fe7f:676c"=>
+                    {"scope"=>"Link", "prefixlen"=>"64", "family"=>"inet6"},
+                      "00:23:6c:7f:67:6c"=>{"family"=>"lladdr"},
+                      "192.168.0.117"=>
+                    {"broadcast"=>"192.168.0.255",
+                      "netmask"=>"255.255.255.0",
+                      "family"=>"inet"}},
+                      "mtu"=>"1500",
+                      "media"=>
+                    {"supported"=>{"autoselect"=>{"options"=>[]}},
+                      "selected"=>{"autoselect"=>{"options"=>[]}}},
+                      "type"=>"en",
+                      "arp"=>
+                    {"192.168.0.72"=>"0:f:ea:39:fa:d5",
+                      "192.168.0.1"=>"0:1c:fb:fc:6f:20",
+                      "192.168.0.255"=>"ff:ff:ff:ff:ff:ff",
+                      "192.168.0.3"=>"0:1f:33:ea:26:9b",
+                      "192.168.0.77"=>"0:23:12:70:f8:cf",
+                      "192.168.0.152"=>"0:26:8:7d:2:4c"},
+                      "encapsulation"=>"Ethernet"},
+                      "en2"=>
+                    {"status"=>"active",
+                      "flags"=>
+                    ["UP", "BROADCAST", "SMART", "RUNNING", "SIMPLEX", "MULTICAST"],
+                      "number"=>"2",
+                      "addresses"=>
+                    {"169.254.206.152"=>
+                      {"broadcast"=>"169.254.255.255",
+                        "netmask"=>"255.255.0.0",
+                        "family"=>"inet"},
+                        "00:1c:42:00:00:01"=>{"family"=>"lladdr"},
+                        "fe80::21c:42ff:fe00:1"=>
+                      {"scope"=>"Link", "prefixlen"=>"64", "family"=>"inet6"}},
+                        "mtu"=>"1500",
+                        "media"=>
+                      {"supported"=>{"autoselect"=>{"options"=>[]}},
+                        "selected"=>{"autoselect"=>{"options"=>[]}}},
+                        "type"=>"en",
+                        "encapsulation"=>"Ethernet"},
+                        "fw0"=>
+                      {"status"=>"inactive",
+                        "flags"=>["BROADCAST", "SIMPLEX", "MULTICAST"],
+                        "number"=>"0",
+                        "addresses"=>{"00:23:32:ff:fe:b0:32:f2"=>{"family"=>"lladdr"}},
+                        "mtu"=>"4078",
+                        "media"=>
+                      {"supported"=>{"autoselect"=>{"options"=>["full-duplex"]}},
+                        "selected"=>{"autoselect"=>{"options"=>["full-duplex"]}}},
+                        "type"=>"fw",
+                        "encapsulation"=>"1394"},
+                        "en3"=>
+                      {"status"=>"active",
+                        "flags"=>
+                      ["UP", "BROADCAST", "SMART", "RUNNING", "SIMPLEX", "MULTICAST"],
+                        "number"=>"3",
+                        "addresses"=>
+                      {"169.254.206.152"=>
+                        {"broadcast"=>"169.254.255.255",
+                          "netmask"=>"255.255.0.0",
+                          "family"=>"inet"},
+                          "00:1c:42:00:00:00"=>{"family"=>"lladdr"},
+                          "fe80::21c:42ff:fe00:0"=>
+                        {"scope"=>"Link", "prefixlen"=>"64", "family"=>"inet6"}},
+                          "mtu"=>"1500",
+                          "media"=>
+                        {"supported"=>{"autoselect"=>{"options"=>[]}},
+                          "selected"=>{"autoselect"=>{"options"=>[]}}},
+                          "type"=>"en",
+                          "encapsulation"=>"Ethernet"}}},
+                          "fqdn"=>"latte.local",
+                          "ohai_time"=>1249065590.90391,
+                          "domain"=>"local",
+                          "os"=>"darwin",
+                          "platform_build"=>"9J61",
+                          "os_version"=>"9.7.0",
+                          "hostname"=>"latte",
+                          "macaddress"=>"00:23:6c:7f:67:6c",
+                          "music" => { "jimmy_eat_world" => "nice" }
+    }
+  
+    @default_hash = {
+      "domain" => "opscode.com",
+      "music" => { 
+        "jimmy_eat_world" => "is fun!",
+        "mastodon" => "rocks",
+        "mars_volta" => "is loud and nutty"
+      }
+    }
+    @override_hash = {
+      "macaddress" => "00:00:00:00:00:00",
+      "music" => {
+        "mars_volta" => "cicatriz"
+      }
+    }
+    @attributes = Chef::Node::Attribute.new(@attribute_hash, @default_hash, @override_hash)
+  end
+
+  describe "initialize" do
+    it "should return a Chef::Node::Attribute" do
+      @attributes.should be_a_kind_of(Chef::Node::Attribute)
+    end
+
+    it "should take an Attribute, Default and Override hash" do
+      lambda { Chef::Node::Attribute.new({}, {}, {}) }.should_not raise_error
+    end
+
+    [ :attribute, :default, :override ].each do |accessor|
+      it "should set #{accessor}" do
+        na = Chef::Node::Attribute.new({ :attribute => true }, { :default => true }, { :override => true })
+        na.send(accessor).should == { accessor => true } 
+      end
+    end
+
+    it "should set the state to an empty array" do
+      @attributes.state.should == []
+    end
+
+    it "should allow you to set the initial state" do
+      na = Chef::Node::Attribute.new({}, {}, {}, [ "first", "second", "third" ])
+      na.state.should == [ "first", "second", "third" ]
+    end
+  end
+
+  describe "[]" do
+    it "should return override data if it exists" do
+      @attributes["macaddress"].should == "00:00:00:00:00:00"
+    end
+
+    it "should return attribute data if it is not overridden" do
+      @attributes["platform"].should == "mac_os_x"
+    end
+
+    it "should return default data if it is not overriden or in attribute data" do
+      @attributes["music"]["mastodon"].should == "rocks"
+    end
+
+    it "should prefer the override data over an available default" do
+      @attributes["music"]["mars_volta"].should == "cicatriz"
+    end
+
+    it "should prefer the attribute data over an available default" do
+      @attributes["music"]["jimmy_eat_world"].should == "nice"
+    end
+
+    it "should return the merged hash if all three have values" do
+      result = @attributes["music"]
+      result["mars_volta"] = "cicatriz"
+      result["jimmy_eat_world"] = "nice"
+      result["mastodon"] = "rocks"
+    end
+  end
+
+  describe "auto_vivifiy" do
+    it "should set the hash key to a mash if it does not exist" do
+      @attributes.auto_vivifiy({}, "one")["one"].should be_a_kind_of(Mash)
+    end
+
+    it "should raise an exception if the key does exist and does not respond to has_key?" do
+      lambda { @attributes.auto_vivifiy({ "one" => "value" }) }.should raise_error(ArgumentError)
+    end
+
+    it "should not alter the value if the key exists and responds to has_key?" do
+      @attributes.auto_vivifiy({ "one" => { "will" => true } }, "one")["one"].should have_key("will")
+    end
+  end
+
+  describe "set_value" do
+    it "should set the value for a top level key" do
+      to_check = {}
+      @attributes.set_value(to_check, "one", "some value")
+      to_check["one"].should == "some value"
+    end
+
+    it "should set the value for a second level key" do
+      to_check = {}
+      @attributes.state = [ "one" ]
+      @attributes.set_value(to_check, "two", "some value")
+      to_check["one"]["two"].should == "some value"
+    end
+
+    it "should set the value for a very deep key" do
+      to_check = {}
+      @attributes.state = [ "one", "two", "three", "four", "five" ]
+      @attributes.set_value(to_check, "six", "some value")
+      to_check["one"]["two"]["three"]["four"]["five"]["six"].should == "some value"
+    end
+  end
+
+  describe "[]=" do
+    it "should set the attribute value" do
+      @attributes["longboard"] = "surfing"
+      @attributes["longboard"].should == "surfing"
+      @attributes.attribute["longboard"].should == "surfing"
+      @attributes.override["longboard"].should == "surfing"
+    end
+
+    it "should set deeply nested attribute value when auto_vivifiy_on_read is true" do
+      puts "\n\n\n\n"
+      @attributes.auto_vivifiy_on_read = true
+      @attributes["longboard"]["hunters"]["comics"] = "surfing"
+      @attributes["longboard"]["hunters"]["comics"].should == "surfing"
+      @attributes.attribute["longboard"]["hunters"]["comics"].should == "surfing"
+      @attributes.override["longboard"]["hunters"]["comics"].should == "surfing"
+    end
+
+    it "should die if you try and do nested attributes that do not exist without read vivification" do
+      lambda { @attributes["foo"]["bar"] = :baz }.should raise_error
+    end
+  end
+
+end
