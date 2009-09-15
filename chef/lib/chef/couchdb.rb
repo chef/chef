a145cbb729579c666c1436ac2db21087e711e2c7
Adam Jacob <adam@opscode.com>
2009-09-14 17:13:30 -0700

    Should be setting the headers to {}, not false

--- chef/lib/chef/couchdb.rb
+++ chef/lib/chef/couchdb.rb
@@ -252,7 +252,7 @@ class Chef
     def view_uri(design, view)
       Chef::Config[:couchdb_version] ||= @rest.run_request(:GET,
                                                            URI.parse(@rest.url + "/"),
-                                                           false,
+                                                           {},
                                                            10,
                                                            false)["version"].gsub(/-.+/,"").to_f
       case Chef::Config[:couchdb_version]
