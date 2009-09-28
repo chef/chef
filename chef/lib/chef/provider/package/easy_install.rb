1d3cfc281c3667f9853e6d935165b2634e5e4f4e
zeusfaber <joe@joetify.com>
2009-09-28 10:18:45 -0700

    initial add of easy_install stuff

---
+++ chef/lib/chef/provider/package/easy_install.rb
@@ -0,0 +1,34 @@
+#
+# Author:: Joe Williams (<joe@joetify.com>)
+# Copyright:: Copyright (c) 2009 Joe Williams
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/provider/package'
+require 'chef/mixin/command'
+require 'chef/resource/package'
+
+class Chef
+  class Provider
+    class Package
+      class EasyInstall < Chef::Provider::Package
+
+
+
+
+      end
+    end
+  end
+end
