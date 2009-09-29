ef1d4d37c7b02a1a270245d06978f7acae3c9d4a
zeusfaber <joe@joetify.com>
2009-09-28 21:36:59 -0700

    its kinda dirty but it works

--- chef/lib/chef/provider/package/easy_install.rb
+++ chef/lib/chef/provider/package/easy_install.rb
@@ -19,23 +19,17 @@
 require 'chef/provider/package'
 require 'chef/mixin/command'
 require 'chef/resource/package'
-
 class Chef
   class Provider
     class Package
       class EasyInstall < Chef::Provider::Package
 
-        def install_check(name, version)
+        def install_check(name)
           command = "python -c \"import sys; print sys.path\""
           check = false
-          if version
-            package = "#{name}-#{version}"
-          else
-            package = "#{name}"
-          end
           status = popen4(command) do |pid, stdin, stdout, stderr|
             stdout.each do |line|
-              if line.include? package
+              if line.include? "#{name}"
                 check = true
               end
             end
@@ -53,25 +47,45 @@ class Chef
           @current_resource.package_name(@new_resource.package_name)
           @current_resource.version(nil)
 
-          if install_check(@new_resource.package_name, @new_resource.version)
+          # get the currently installed version if installed
+          if install_check(@new_resource.package_name)
+            command = "python -c \"import #{@new_resource.package_name}; print #{@new_resource.package_name}.__path__\""
+            pid, stdin, stdout, stderr = popen4(command)
+            install_location = stdout.readline
+            install_location[/\S\S(.*)\/(.*)-(.*)-py(.*).egg\S/]
+            package_version = $3
+          else
+            package_version = nil
+          end
+
+          if package_version == @new_resource.version
             Chef::Log.debug("#{@new_resource.package_name} at version #{@new_resource.version}")
             @current_resource.version(@new_resource.version)
+          else
+            Chef::Log.debug("#{@new_resource.package_name} at version #{package_version}")
+            @current_resource.version(package_version)
           end
 
           @current_resource
         end
 
         def candidate_version
-          no_version = ""
-          no_version
+           return @candidate_version if @candidate_version
+
+           # do a dry run to get the latest version
+           command = "#{easy_install_binary_path} -n #{@new_resource.package_name}"
+           pid, stdin, stdout, stderr = popen4(command)
+           dry_run_output = ""
+           stdout.each do |line|
+             dry_run_output << line
+           end
+           dry_run_output[/(.*)Best match: (.*) (.*)\n/]
+           @candidate_version = $3
+           @candidate_version
         end
 
         def install_package(name, version)
-          if version == ""
-            run_command(:command => "#{easy_install_binary_path} #{name}")
-          else
-            run_command(:command => "#{easy_install_binary_path} \"#{name}==#{version}\"")
-          end
+          run_command(:command => "#{easy_install_binary_path} \"#{name}==#{version}\"")
         end
 
         def upgrade_package(name, version)
@@ -85,7 +99,6 @@ class Chef
         def purge_package(name, version)
           remove_package(name, version)
         end
-
 
       end
     end
