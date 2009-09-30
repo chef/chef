a76f0d5ca115d4ea7a51b21b8dc0ead4ed91f4eb
Dan DeLeo <danielsdeleo@mac.com>
2009-09-30 00:27:04 -0600

    wicked speed, but borken right now. don't use!

--- chef/lib/chef/cookbook_loader.rb
+++ chef/lib/chef/cookbook_loader.rb
@@ -16,6 +16,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# IDEAS:
+# HAVE A BASENAME-CENTRIC FILE CLASS,
+# KEEP a hash
+#{"basename1" => "path/to/basename1"}
+# then loop over least important to most important, storing results in the hash
+# at the end we can do hash#values
+
+# cascading files work similarly except by keeping the end and removing the "base path"
+#{"subdir/deeper/template.conf.erb" => "/full/path/to/subdir/template.conf.erb"}
+
 require 'chef/config'
 require 'chef/cookbook'
 require 'chef/cookbook/metadata'
@@ -35,22 +45,22 @@ class Chef
     
     def load_cookbooks
       cookbook_settings = Hash.new
-      Chef::Config.cookbook_path.each do |cb_path|
+      Chef::Config.cookbook_path.reverse.each do |cb_path|
         Dir[File.join(cb_path, "*")].each do |cookbook|
           next unless File.directory?(cookbook)          
           cookbook_name = File.basename(cookbook).to_sym
           unless cookbook_settings.has_key?(cookbook_name)
             cookbook_settings[cookbook_name] = { 
-              :ignore_regexes => Array.new,
-              :attribute_files => Array.new,
-              :definition_files => Array.new,
-              :recipe_files => Array.new,
-              :template_files => Array.new,
-              :remote_files => Array.new,
-              :lib_files => Array.new,
-              :resource_files => Array.new,
-              :provider_files => Array.new,
-              :metadata_files => Array.new
+              :ignore_regexes   => Array.new,
+              :attribute_files  => Hash.new,
+              :definition_files => Hash.new,
+              :recipe_files     => Hash.new,
+              :template_files   => Hash.new,
+              :remote_files     => Hash.new,
+              :lib_files        => Hash.new,
+              :resource_files   => Hash.new,
+              :provider_files   => Hash.new,
+              :metadata_files   => Array.new
             }
           end
           ignore_regexes = load_ignore_file(File.join(cookbook, "ignore"))
@@ -105,16 +115,20 @@ class Chef
           end
         end
       end
+      
+      require "pp"
+      pp cookbook_settings
+      
       cookbook_settings.each_key do |cookbook|
         @cookbook[cookbook] = Chef::Cookbook.new(cookbook)
-        @cookbook[cookbook].attribute_files = cookbook_settings[cookbook][:attribute_files]
-        @cookbook[cookbook].definition_files = cookbook_settings[cookbook][:definition_files]
-        @cookbook[cookbook].recipe_files = cookbook_settings[cookbook][:recipe_files]
-        @cookbook[cookbook].template_files = cookbook_settings[cookbook][:template_files]
-        @cookbook[cookbook].remote_files = cookbook_settings[cookbook][:remote_files]
-        @cookbook[cookbook].lib_files = cookbook_settings[cookbook][:lib_files]
-        @cookbook[cookbook].resource_files = cookbook_settings[cookbook][:resource_files]
-        @cookbook[cookbook].provider_files = cookbook_settings[cookbook][:provider_files]
+        @cookbook[cookbook].attribute_files = cookbook_settings[cookbook][:attribute_files].values
+        @cookbook[cookbook].definition_files = cookbook_settings[cookbook][:definition_files].values
+        @cookbook[cookbook].recipe_files = cookbook_settings[cookbook][:recipe_files].values
+        @cookbook[cookbook].template_files = cookbook_settings[cookbook][:template_files].values
+        @cookbook[cookbook].remote_files = cookbook_settings[cookbook][:remote_files].values
+        @cookbook[cookbook].lib_files = cookbook_settings[cookbook][:lib_files].values
+        @cookbook[cookbook].resource_files = cookbook_settings[cookbook][:resource_files].values
+        @cookbook[cookbook].provider_files = cookbook_settings[cookbook][:provider_files].values
         @metadata[cookbook] = Chef::Cookbook::Metadata.new(@cookbook[cookbook])
         cookbook_settings[cookbook][:metadata_files].each do |meta_json|
           @metadata[cookbook].from_json(IO.read(meta_json))
@@ -139,38 +153,43 @@ class Chef
     private
     
       def load_ignore_file(ignore_file)
+        p :loading_ignore_file, ignore_file
+        
         results = Array.new
         if File.exists?(ignore_file) && File.readable?(ignore_file)
           IO.foreach(ignore_file) do |line|
             next if line =~ /^#/
             next if line =~ /^\w*$/
             line.chomp!
+            p :adding_ingnore_line, line
             results << Regexp.new(line)
           end
         end
         results
       end
       
-      def load_cascading_files(file_glob, base_path, result_array, ignore_regexes)
+      def load_cascading_files(file_glob, base_path, result_hash, ignore_regexes)
         # To handle dotfiles like .ssh
         Dir.glob(File.join(base_path, "**/#{file_glob}"), File::FNM_DOTMATCH).each do |file|
           next if skip_file(file, ignore_regexes)
           file =~ /^#{base_path}\/(.+)$/
           singlecopy = $1
-          unless result_array.detect { |f| f =~ /#{singlecopy}$/ }
-            result_array << file
-          end
+          result_hash[singlecopy] = file
+          #unless result_array.detect { |f| f =~ /#{singlecopy}$/ }
+          #  result_array << file
+          #end
         end
       end
       
-      def load_files_unless_basename(file_glob, result_array, ignore_regexes)
+      def load_files_unless_basename(file_glob, result_hash, ignore_regexes)
         Dir[file_glob].each do |file|
           next if skip_file(file, ignore_regexes)
           file_basename = File.basename(file)
+          result_hash[file_basename] = file
           # If we've seen a file with this basename before, skip it.
-          unless result_array.detect { |f| File.basename(f) == file_basename }  
-            result_array << file
-          end
+          #unless result_array.detect { |f| File.basename(f) == file_basename }  
+          #  result_array << file
+          #end
         end
       end
       
