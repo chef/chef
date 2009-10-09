a6ab75f1beaffa429e276f38226bb2eac8c41e40
Dan DeLeo <danielsdeleo@mac.com>
2009-10-08 21:21:44 -0600

    have to use ivar instead of getter ATM

--- chef/lib/chef/provider.rb
+++ chef/lib/chef/provider.rb
@@ -53,7 +53,7 @@ class Chef
       provider_collection, @collection = @collection, Chef::ResourceCollection.new
       
       instance_eval(*args, &block)
-      Chef::Runner.new(@node, collection).converge
+      Chef::Runner.new(@node, @collection).converge
       
       @collection = provider_collection
     end
