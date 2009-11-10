03936a1468af4aa12acbc59e6be354e30ed0cc81
Adam Jacob <adam@opscode.com>
2009-11-09 18:22:30 -0800

    Updating specs for CHEF-695

--- chef/spec/unit/provider/file_spec.rb
+++ chef/spec/unit/provider/file_spec.rb
@@ -224,13 +224,24 @@ describe Chef::Provider::File do
     File.stub!(:chmod).and_return(1)
     @provider.action_touch
   end
+
+  it "should keep 1 backup copy if specified" do
+    @provider.load_current_resource
+    @provider.new_resource.stub!(:path).and_return("/tmp/s-20080705111233")
+    @provider.new_resource.stub!(:backup).and_return(1)
+    Dir.stub!(:[]).and_return([ "/tmp/s-20080705111233", "/tmp/s-20080705111232", "/tmp/s-20080705111223"])
+    FileUtils.should_receive(:rm).with("/tmp/s-20080705111223").once.and_return(true)
+    FileUtils.should_receive(:rm).with("/tmp/s-20080705111232").once.and_return(true)
+    FileUtils.stub!(:cp).and_return(true)
+    File.stub!(:exist?).and_return(true)
+    @provider.backup
+  end
   
   it "should backup a file no more than :backup times" do
     @provider.load_current_resource
     @provider.new_resource.stub!(:path).and_return("/tmp/s-20080705111233")
     @provider.new_resource.stub!(:backup).and_return(2)
     Dir.stub!(:[]).and_return([ "/tmp/s-20080705111233", "/tmp/s-20080705111232", "/tmp/s-20080705111223"])
-    FileUtils.should_receive(:rm).with("/tmp/s-20080705111232").once.and_return(true)
     FileUtils.should_receive(:rm).with("/tmp/s-20080705111223").once.and_return(true)
     FileUtils.stub!(:cp).and_return(true)
     File.stub!(:exist?).and_return(true)
