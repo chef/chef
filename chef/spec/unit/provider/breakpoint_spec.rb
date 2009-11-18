d7677d6e0ea94dc11523a707a5c2e74e56b5d25b
Dan DeLeo <danielsdeleo@mac.com>
2009-11-18 11:13:13 -0700

    breakpoint R&P pause convergence via stepable iterator

---
+++ chef/spec/unit/provider/breakpoint_spec.rb
@@ -0,0 +1,51 @@
+#
+# Author:: Daniel DeLeo (<dan@kallistec.com>)
+# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+describe Chef::Provider::Breakpoint do
+  
+  before do
+    @resource = Chef::Resource::Breakpoint.new
+    @node = Chef::Node.new
+    @collection = mock("resource collection")
+    @provider = Chef::Provider::Breakpoint.new(@node,@resource, @collection)
+  end
+  
+  it "responds to load_current_resource" do
+    @provider.should respond_to(:load_current_resource)
+  end
+  
+  it "gets the iterator from @collection and pauses it" do
+    Shef.stub!(:running?).and_return(true)
+    @iterator = mock("stepable_iterator")
+    @collection.stub!(:iterator).and_return(@iterator)
+    @iterator.should_receive(:pause)
+    @provider.action_break
+    @resource.updated.should == true
+  end
+
+  it "doesn't pause the iterator if Shef isn't running" do
+    Shef.stub!(:running?).and_return(false)
+    @iterator = mock("stepable_iterator")
+    @collection.stub!(:iterator).and_return(@iterator)
+    @iterator.should_not_receive(:pause)
+    @provider.action_break
+  end
+
+end
---
+++ chef/spec/unit/resource/breakpoint_spec.rb
@@ -0,0 +1,43 @@
+#
+# Author:: Daniel DeLeo (<dan@kallistec.com>)
+# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+
+describe Chef::Resource::Breakpoint do
+  
+  before do
+    @breakpoint = Chef::Resource::Breakpoint.new
+  end
+  
+  it "allows the action :break" do
+    @breakpoint.allowed_actions.should include(:break)
+  end
+  
+  it "defaults to the break action" do
+    @breakpoint.action.should == "break"
+  end
+  
+  it "names itself after the line number of the file where it's created" do
+    @breakpoint.name.should match(/breakpoint_spec\.rb\:[\d]{2}\:in \`new\'$/)
+  end
+  
+  it "uses the breakpoint provider" do
+    @breakpoint.provider.should == Chef::Provider::Breakpoint
+  end
+  
+end
