7daefbc62639ae473d6b78971c2228c11e21e0e7
Daniel DeLeo <dan@opscode.com>
2010-02-08 23:25:05 -0800

    Add XML Escape mixin
    copied/adapted from http://intertwingly.net/stories/2005/09/28/xchar.rb

---
+++ chef/lib/chef/mixin/xml_escape.rb
@@ -0,0 +1,78 @@
+#
+# Author:: Daniel DeLeo (<dan@opscode.com>)
+# Copyright:: Copyright (c) 2009 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class Chef
+  module Mixin
+    module XMLEscape
+      CP1252 = {
+        128 => 8364, # euro sign
+        130 => 8218, # single low-9 quotation mark
+        131 =>  402, # latin small letter f with hook
+        132 => 8222, # double low-9 quotation mark
+        133 => 8230, # horizontal ellipsis
+        134 => 8224, # dagger
+        135 => 8225, # double dagger
+        136 =>  710, # modifier letter circumflex accent
+        137 => 8240, # per mille sign
+        138 =>  352, # latin capital letter s with caron
+        139 => 8249, # single left-pointing angle quotation mark
+        140 =>  338, # latin capital ligature oe
+        142 =>  381, # latin capital letter z with caron
+        145 => 8216, # left single quotation mark
+        146 => 8217, # right single quotation mark
+        147 => 8220, # left double quotation mark
+        148 => 8221, # right double quotation mark
+        149 => 8226, # bullet
+        150 => 8211, # en dash
+        151 => 8212, # em dash
+        152 =>  732, # small tilde
+        153 => 8482, # trade mark sign
+        154 =>  353, # latin small letter s with caron
+        155 => 8250, # single right-pointing angle quotation mark
+        156 =>  339, # latin small ligature oe
+        158 =>  382, # latin small letter z with caron
+        159 =>  376} # latin capital letter y with diaeresis
+
+      # http://www.w3.org/TR/REC-xml/#dt-chardata
+      PREDEFINED = {
+        38 => '&amp;', # ampersand
+        60 => '&lt;',  # left angle bracket
+        62 => '&gt;'}  # right angle bracket
+
+      # http://www.w3.org/TR/REC-xml/#charsets
+      VALID = [[0x9, 0xA, 0xD], (0x20..0xD7FF), 
+        (0xE000..0xFFFD), (0x10000..0x10FFFF)]
+
+      def xml_escape(unescaped_str)
+        begin
+          unescaped_str.unpack("U*").map {|char| xml_escape_char!(char)}.join
+        rescue
+          unescaped_str.unpack("C*").map {|char| xml_escape_char!(char)}.join
+        end
+      end
+
+      private
+
+      def xml_escape_char!(char)
+        char = CP1252[char] || char
+        char = 42 unless VALID.detect {|range| range.include? char}
+        char = PREDEFINED[char] || (char<128 ? char.chr : "&##{char};")
+      end
+
+    end
+  end
+end
---
+++ chef/spec/unit/mixin/xml_escape_spec.rb
@@ -0,0 +1,54 @@
+#
+# Author:: Adam Jacob (<adam@opscode.com>)
+# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+
+class XMLEscapingTestHarness
+  include Chef::Mixin::XMLEscape
+end
+
+describe Chef::Mixin::XMLEscape do
+  before do
+    @escaper = XMLEscapingTestHarness.new
+  end
+
+  it "escapes ampersands to '&amp;'" do
+    @escaper.xml_escape("&").should == "&amp;"
+  end
+
+  it "escapes angle brackets to &lt; or &gt;" do
+    @escaper.xml_escape("<").should == "&lt;"
+    @escaper.xml_escape(">").should == "&gt;"
+  end
+
+  it "does not modify ASCII strings" do
+    @escaper.xml_escape('foobarbaz!@#$%^*()').should == 'foobarbaz!@#$%^*()'
+  end
+
+  it "converts invalid bytes to asterisks" do
+    @escaper.xml_escape("\x00").should == "*"
+  end
+
+  it "converts UTF-8 correctly" do
+    @escaper.xml_escape("\xC2\xA9").should == '&#169;'
+  end
+
+  it "converts win 1252 characters correctly" do
+    @escaper.xml_escape("\x80").should == '&#8364;'
+  end
+end
