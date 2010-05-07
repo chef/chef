c5bda1388324513f13fe8336b373236dcc364f9d
Chris Walters <cw@opscode.com>
2010-05-07 16:19:45 -0700

    CookbookFileVendor stub

---
+++ chef/lib/chef/cookbook_file_vendor.rb
@@ -0,0 +1,25 @@
+#
+# Author:: Christopher Walters (<cw@opscode.com>)
+# Author:: Tim Hinderliter (<tim@opscode.com>)
+# Copyright:: Copyright (c) 2010 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class Chef
+  class CookbookFileVendor
+    def get_file(node, cookbook, segment, filename)
+    end
+  end
+end
