dbe9f647c90ef0aae18da1f1350428cbe4c099ee
Tim Hinderliter <tim@opscode.com>
2010-05-07 18:15:02 -0700

    CookbookFileVendor implemented

--- chef/lib/chef/cookbook_file_vendor.rb
+++ chef/lib/chef/cookbook_file_vendor.rb
@@ -17,9 +17,75 @@
 # limitations under the License.
 #
 
+
+# This class handles fetching of cookbook files based on specificity. A local
+# filesystem cache is maintained based on the checksum of files retrieved.
 class Chef
   class CookbookFileVendor
-    def get_file(node, cookbook, segment, filename)
+    def initialize
+      @cookbook_cache = Hash.new
+      @checksum_cache = Chef::Cache::FileCacheByChecksum.new
     end
+
+    # Gets the full pathname for the given cookbook part, returned in specificity 
+    # preference order.
+    def get_filename(node, cookbook_name, segment, filename)
+      checksum = get_preferred_checksum(node, cookbook_name, segment, filename)
+      raise Chef::Exceptions::FileNotFound, "missing file #{segment}/#{filename} for cookbook #{cookbook_name}" unless checksum
+
+      checksum_filename = @checksum_cache.get_path(checksum)
+      unless checksum_filename
+        # We don't have that checksum in the cache. Fetch it.
+        # TODO: timh: 2010-4-7: Make this download streaming instead of loading it all in memory
+        url = "/cookbooks/#{cookbook_name}/#{cookbook_version}/#{checksum}"
+        
+        # Second argument is true, so we treat the result as raw, not JSON
+        tempfile = rest.get_rest(url, true)
+        checksum_filename = @checksum_cache.put(checksum, tempfile.path)
+        tempfile.close
+      end
+      checksum_filename
+    end
+
+    # Figure out the checksum for a given segment/filename based on finding
+    # the most specific version of the file available.
+    def get_preferred_checksum(node, cookbook_name, segment, filename)
+      platform, version = Chef::Platform.find_platform_and_version(node)
+      fqdn = node[:fqdn]
+
+      # Most specific to least specific places to find the filename
+      preferences = [
+        File.join("host-#{fqdn}", filename),
+        File.join("#{platform}-#{version}", filename),
+        File.join(platform, filename),
+        File.join("default", filename)
+      ]
+      
+      cookbook = get_cookbook(cookbook_name)
+
+      cookbook_checksum_by_filename = Hash.new
+      [ :resources, :providers, :recipes, :definitions, :libraries, :attributes, :files, :templates, :root_files ].each do |segment|
+        cookbook[segment].inject(cookbook_checksum_by_filename) do |memo, segment_file|
+          checksum = segment_file['checksum']
+          memo[segment_file.path] = checksum
+          memo
+        end
+      end
+      
+      # Walk most- to least-specific until we find the filename.
+      preferences.find { |filename| cookbook_checksum_by_filename[filename] }
+    end
+    
+    # Get the cookbook from the cache or fetch if it's not yet in the
+    # cache.
+    def get_cookbook(cookbook_name)
+      unless @cookbook_cache[cookbook_name]
+        url = "/cookbooks/#{cookbook_name}"
+        @cookbook_cache[cookbook_name] = rest.get_rest(url)
+      end
+      
+      @cookbook_cache[cookbook_name]
+    end
+    
   end
 end
