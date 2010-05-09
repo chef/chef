545adc98baf864f86adf27c1b7197f24ebfcd6a7
Daniel DeLeo <dan@opscode.com>
2010-05-09 10:05:05 -0700

    whitespace and trailing newline

--- chef/tasks/rspec.rb
+++ chef/tasks/rspec.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,7 +43,7 @@ begin
         IO.readlines("#{CHEF_ROOT}/spec/rcov.opts").map {|l| l.chomp.split " "}.flatten
       end
     end
-  
+
     desc "Print Specdoc for all specs"
     Spec::Rake::SpecTask.new(:doc) do |t|
       t.spec_opts = ["--format", "specdoc", "--dry-run"]
@@ -57,7 +57,7 @@ begin
         t.spec_files = FileList["spec/#{sub}/**/*_spec.rb"]
       end
     end
-  
+
     desc "Translate/upgrade specs using the built-in translator"
     task :translate do
       translator = ::Spec::Translator.new
