497f18f0f5a72f28b05c7be1d09dadd4bf7c530c
Chris Walters, Tim Hinderliter <cw@opscode.com,tim@opscode.com>
2010-05-17 15:33:09 -0700

    Chef::Cookbook refactor to use CookbookFileVendor, as well as moved some load logic to Chef::Node and Chef::RunContext.
    * Cookbook now depends on the CookbookFileVendor abstraction to manage how and where files get on disk
    * Moved load_attributes to Chef::Node
    * Moved load_definitions, load_recipes, load_providers, and load_libraries to Chef::RunContext
    * Replaced set_with_names paradigm with an explicitly stored map from short name to filename

--- chef/lib/chef/cookbook.rb
+++ chef/lib/chef/cookbook.rb
@@ -2,7 +2,8 @@
 # Author:: Adam Jacob (<adam@opscode.com>)
 # Author:: Nuo Yan (<nuo@opscode.com>)
 # Author:: Christopher Walters (<cw@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Tim Hinderliter (<tim@opscode.com>)
+# Copyright:: Copyright (c) 2008-2010 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,19 +22,22 @@ require 'chef/log'
 require 'chef/node'
 require 'chef/resource_definition_list'
 require 'chef/recipe'
-require 'chef/mixin/convert_to_class_name'
 
 class Chef
   class Cookbook
-    include Chef::Mixin::ConvertToClassName
-    include Chef::Mixin::Checksum
     include Chef::IndexQueue::Indexable
-    
-    attr_accessor :definition_files, :template_files, :remote_files,
-      :lib_files, :resource_files, :provider_files, :root_files, :name,
-      :metadata, :metadata_files, :status, :couchdb_rev, :couchdb
-    attr_reader :recipe_files, :attribute_files, :couchdb_id
 
+    attr_accessor :definition_files, :template_files, :remote_files,
+      :library_files, :resource_files, :provider_files, :root_files, :name,
+      :metadata, :metadata_files, :status, :couchdb_rev, :couchdb
+    attr_reader :couchdb_id
+    attr_reader :file_vendor
+
+    attr_reader :recipe_filename_by_name
+    attr_reader :attribute_filename_by_short_filename
+    
+    COOKBOOK_SEGMENTS = [ :resources, :providers, :recipes, :definitions, :libraries, :attributes, :files, :templates, :root_files ]
+    
     DESIGN_DOCUMENT = {
       "version" => 5,
       "language" => "javascript",
@@ -117,32 +121,33 @@ class Chef
     # object<Chef::Cookbook>:: Duh. :)
     def initialize(name, couchdb=nil)
       @name = name
-      @attribute_files = Array.new
-      @attribute_names = Hash.new
-      @definition_files = Array.new
-      @template_files = Array.new
-      @remote_files = Array.new
-      @recipe_files = Array.new
-      @recipe_names = Hash.new
-      @lib_files = Array.new
-      @resource_files = Array.new
-      @provider_files = Array.new
-      @metadata_files = Array.new
+#       @attribute_files = Array.new
+#       @attribute_names = Hash.new
+#       @definition_files = Array.new
+#       @template_files = Array.new
+#       @remote_files = Array.new
+#       @recipe_files = Array.new
+#       @recipe_names = Hash.new
+#       @lib_files = Array.new
+#       @resource_files = Array.new
+#       @provider_files = Array.new
+#       @metadata_files = Array.new
       @couchdb_id = nil
       @couchdb = couchdb || Chef::CouchDB.new
-      @couchdb_rev = nil 
+      @couchdb_rev = nil
       @status = :ready
-      @manifest = nil 
+      @manifest = nil
+      @file_vendor = nil
       @metadata = {}
     end
-    
+
     def version
-      @metadata.version
+      metadata.version
     end
     
     def version=(new_version)
       manifest["version"] = new_version
-      @metadata.version(new_version)
+      metadata.version(new_version)
     end
 
     def manifest
@@ -153,8 +158,8 @@ class Chef
     end
     
     def manifest=(new_manifest)
-      @manifest = new_manifest
-      @checksums = extract_checksums_from_manifest(new_manifest)
+      self.manifest = new_manifest
+      self.checksums = extract_checksums_from_manifest(new_manifest)
     end
     
     # Returns a hash of checksums to either nil or the on disk path (which is
@@ -169,162 +174,79 @@ class Chef
     def full_name
       "#{name}-#{version}"
     end
-    
-    # Loads all the library files in this cookbook via require.
-    #
-    # === Returns
-    # true:: Always returns true
-    def load_libraries
-      @lib_files.each do |file|
-        Chef::Log.debug("Loading cookbook #{name} library file: #{file}")
-        require file
-      end
-      true
-    end
-    
-    # Loads all the attribute files in this cookbook within a particular <Chef::Node>.
-    #
-    # === Parameters
-    # node<Chef::Node>:: The Chef::Node to apply the attributes to
-    #
-    # === Returns
-    # node<Chef::Node>:: The updated Chef::Node object
-    #
-    # === Raises
-    # <ArgumentError>:: If the argument is not a kind_of? <Chef::Node>
-    def load_attributes(node)
-      @attribute_files.each do |file|
-        load_attribute_file(file, node)
-      end
-      node
-    end
 
-    def load_attribute_file(file, node)
-      Chef::Log.debug("Loading attributes from #{file}")
-      node.from_file(file)
-    end
-
-    def load_attribute(name, node)
-      attr_name = shorten_name(name)
-      file = @attribute_files[@attribute_names[attr_name]]
-      load_attribute_file(file, node)
-      node
+    def attribute_filenames=(*filenames)
+      self.attribute_filenames = filenames.flatten
+      self.attribute_filename_by_short_filename = filenames_by_name(filenames)
+      attribute_filenames
     end
     
-    # Loads all the resource definitions in this cookbook.
-    #
-    # === Returns
-    # definitions<Hash>: A hash of <Chef::ResourceDefinition> objects, keyed by name.
-    def load_definitions
-      results = Hash.new
-      @definition_files.each do |file|
-        Chef::Log.debug("Loading cookbook #{name}'s definitions from #{file}")
-        resourcelist = Chef::ResourceDefinitionList.new
-        resourcelist.from_file(file)
-        results.merge!(resourcelist.defines) do |key, oldval, newval|
-          Chef::Log.info("Overriding duplicate definition #{key}, new found in #{file}")
-          newval
-        end
-      end
-      results
-    end
-
-    # Loads all the resources in this cookbook.
-    #
-    # === Returns
-    # true:: Always returns true
-    def load_resources
-      @resource_files.each do |file|
-        Chef::Log.debug("Loading cookbook #{name}'s resources from #{file}")
-        Chef::Resource.build_from_file(name, file)
-      end
-    end
-    
-    # Loads all the providers in this cookbook.
-    #
-    # === Returns
-    # true:: Always returns true
-    def load_providers
-      @provider_files.each do |file|
-        Chef::Log.debug("Loading cookbook #{name}'s providers from #{file}")
-        Chef::Provider.build_from_file(name, file)
-      end
-    end
-    
-    def recipe_files=(*args)
-      @recipe_files, @recipe_names = set_with_names(args.flatten)
-      @recipe_files
-    end
-
-    def attribute_files=(*args)
-      @attribute_files, @attribute_names = set_with_names(args.flatten)
-      @attribute_files
-    end
-    
-    def recipe?(name)
-      lookup_name = name
-      if name =~ /(.+)::(.+)/
-        cookbook_name = $1
-        lookup_name = $2
-        return false unless cookbook_name == @name
-      end
-      @recipe_names.has_key?(lookup_name)
-    end
-    
-    def recipes
+    # Return recipe names in the form of cookbook_name::recipe_name
+    def fully_qualified_recipe_names
       results = Array.new
-      @recipe_names.each_key do |rname|
-        results << "#{@name}::#{rname}"
+      recipe_filename_by_name.each_key do |rname|
+        results << "#{name}::#{rname}"
       end
       results
     end
     
-    def load_recipe(name, node, collection=nil, definitions=nil, cookbook_loader=nil)
-      cookbook_name = @name
-      recipe_name = shorten_name(name) 
+    def recipe_filenames=(*filenames)
+      self.recipe_filenames = filenames.flatten
+      self.recipe_filename_by_name = filenames_by_name(filenames)
+      recipe_filenames
+    end
+    
+    # called from DSL
+    def load_recipe(recipe_name, run_context)
+      cookbook_name = self.name
       
-      unless @recipe_names.has_key?(recipe_name)
-        raise ArgumentError, "Cannot find a recipe matching #{recipe_name} in cookbook #{@name}"
+      unless recipe_filename_by_name.has_key?(recipe_name)
+        raise ArgumentError, "Cannot find a recipe matching #{recipe_name} in cookbook #{name}"
       end
-      Chef::Log.debug("Found recipe #{recipe_name} in cookbook #{cookbook_name}") if Chef::Log.debug?
-      recipe = Chef::Recipe.new(cookbook_name, recipe_name, node, 
-                                collection, definitions, cookbook_loader)
-      recipe.from_file(@recipe_files[@recipe_names[recipe_name]])
+      Chef::Log.debug("Found recipe #{recipe_name} in cookbook #{cookbook_name}")
+      recipe = Chef::Recipe.new(cookbook_name, recipe_name, run_context)
+      recipe_filename = recipe_filenames_by_name[recipe_name]
+      raise Chef::Exceptions::RecipeNotFound, "could not find recipe #{recipe_name} for cookbook #{self.name}" unless recipe_filename
+      
+      recipe.from_file(recipe_filename)
       recipe
     end
 
-    def segment_files(segment)
-      files_list = nil
-      case segment
-      when :attributes
-        files_list = attribute_files
-      when :recipes
-        files_list = recipe_files
-      when :definitions
-        files_list = definition_files
-      when :libraries
-        files_list = lib_files
-      when :providers
-        files_list = provider_files
-      when :resources
-        files_list = resource_files
-      when :files
-        files_list = remote_files
-      when :templates
-        files_list = template_files
-      when :root_files
-        files_list = root_files
+    def segment_filenames(segment)
+      raise ArgumentError, "invalid segment #{segment}: must be one of #{COOKBOOK_SEGMENTS.join(', ')}"
+      if manifest[segment]
+        manifest[segment].map{|segment_file| file_vendor.get_filename(segment_file["path"]) }
       else
-        raise ArgumentError, "invalid segment #{segment}: must be one of :attributes, :recipes, :definitions, :remote_files, :template_files, :resources, :providers, :libraries, or :root_files"
+        []
       end
-      files_list
+    end
+
+    def preferred_filename(node, segment, filename)
+      platform, version = Chef::Platform.find_platform_and_version(node)
+      fqdn = node[:fqdn]
+
+      # Most specific to least specific places to find the filename
+      preferences = [
+        File.join("host-#{fqdn}", filename),
+        File.join("#{platform}-#{version}", filename),
+        File.join(platform, filename),
+        File.join("default", filename)
+      ]
+      
+      found_pref = preferences.find{ |preferred_file| manifest[segment.to_s][preferred_file] }
+      if found_pref
+        manifest_record = manifest[segment.to_s][found_pref]
+        file_vendor.get_filename(manifest_record)
+      else
+        raise Chef::Exceptions::FileNotFound, "cookbook #{name} does not contain file #{segment}/#{filename}"
+      end
     end
 
     def to_json(*a)
       result = manifest.dup
       result['json_class'] = self.class.name
       result['chef_type'] = 'cookbook'
-      result["_rev"] = @couchdb_rev if @couchdb_rev
+      result["_rev"] = couchdb_rev if couchdb_rev
       result.to_json(*a)
     end
 
@@ -344,10 +266,25 @@ class Chef
       cookbook.metadata = Chef::Cookbook::Metadata.from_hash(o["metadata"])
       cookbook
     end
+    
+    # Parses a potentially fully-qualified recipe name into its
+    # cookbook name and recipe short name.
+    #
+    # For example:
+    #   "aws::elastic_ip" returns ["aws", "elastic_ip"]
+    #   "aws" returns ["aws", "default"]
+    def self.parse_recipe_name(recipe_name)
+      rmatch = recipe_name.match(/(.+?)::(.+)/)
+      if rmatch
+        [ rmatch[1], rmatch[2] ]
+      else
+        [ recipe_name, "default" ]
+      end
+    end
 
     def generate_manifest_with_urls(&url_generator)
       rendered_manifest = manifest.dup
-      [ 'resources', 'providers', 'recipes', 'definitions', 'libraries', 'attributes', 'files', 'templates', 'root_files' ].each do |segment|
+      COOKBOOK_SEGMENTS.map{|s|s.to_s}.each do |segment|
         if rendered_manifest.has_key?(segment)
           rendered_manifest[segment].each do |segment_file|
             url_options = { :cookbook_name => name.to_s, :cookbook_version => version, :checksum => segment_file["checksum"] }
@@ -366,13 +303,13 @@ class Chef
     end
 
     def save
-      chef_server_rest.put_rest("cookbooks/#{@name}/#{version}", self)
+      chef_server_rest.put_rest("cookbooks/#{name}/#{version}", self)
       self
     end
     alias :create :save
 
     def destroy
-      chef_server_rest.delete_rest("cookbooks/#{@name}/#{version}")
+      chef_server_rest.delete_rest("cookbooks/#{name}/#{version}")
       self
     end
 
@@ -413,41 +350,23 @@ class Chef
     end
 
     def cdb_destroy
-      (couchdb || Chef::CouchDB.new).delete("cookbook", full_name, @couchdb_rev)
+      (couchdb || Chef::CouchDB.new).delete("cookbook", full_name, couchdb_rev)
     end
 
     def cdb_save
-      @couchdb_rev = @couchdb.store("cookbook", full_name, self)["rev"]
+      self.couchdb_rev = couchdb.store("cookbook", full_name, self)["rev"]
     end
 
     def couchdb_id=(value)
-      @couchdb_id = value
+      self.couchdb_id = value
       self.index_id = value
     end
 
     private
-
-    def shorten_name(name)
-      short_name = nil
-      nmatch = name.match(/^(.+?)::(.+)$/)
-      short_name = nmatch ? nmatch[2] : name
-    end
-
-    def set_with_names(file_list)
-      files = file_list
-      names = Hash.new
-      files.each_index do |i|
-        file = files[i]
-        case file
-        when /(.+\/)(.+).rb$/
-          names[$2] = i
-        when /(.+).rb$/
-          names[$1] = i
-        else  
-          names[file] = i
-        end
-      end
-      [ files, names ]
+    
+    # for each filename, produce a mapping of base filename (i.e. recipe name or attribute file) to on disk location
+    def filenames_by_name(filenames)
+      filenames.select{|filename| filename =~ /\.rb$/}.inject({}){|memo, filename| memo[File.basename(filename, '.rb')] = filename ; memo }
     end
     
     def generate_manifest
@@ -464,8 +383,8 @@ class Chef
       }
       checksums_to_on_disk_paths = {}
 
-      [ :resources, :providers, :recipes, :definitions, :libraries, :attributes, :files, :templates, :root_files ].each do |segment|
-        segment_files(segment).each do |segment_file|
+      COOKBOOK_SEGMENTS.each do |segment|
+        segment_filenames(segment).each do |segment_file|
           next if File.directory?(segment_file)
 
           file_name = nil
@@ -509,14 +428,16 @@ class Chef
       manifest[:version] = metadata.version
       manifest[:name] = full_name
 
-      @checksums = checksums_to_on_disk_paths
-      @manifest = manifest
+      self.checksums = checksums_to_on_disk_paths
+      self.file_vendor = Chef::CookbookFileVendor.new(manifest)
+      self.manifest = manifest
     end
 
     def extract_checksums_from_manifest(manifest)
       checksums = {}
-      [ :resources, :providers, :recipes, :definitions, :libraries, :attributes, :files, :templates, :root_files ].each do |segment|
-        manifest[segment.to_s].each do |segment_file|
+      COOKBOOK_SEGMENTS.map{|s|s.to_s}.each do |segment|
+        next unless manifest.has_key?(segment)
+        manifest[segment].each do |segment_file|
           checksums[segment_file["checksum"]] = nil
         end
       end
