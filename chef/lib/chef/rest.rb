cbd6c06108aca2b1c33ad0f80429d5c2db3ee80f
Doug MacEachern <dougm@vmware.com>
2010-05-19 16:52:04 -0700

    (CHEF-1234) Tempfile.binmode required for binary files on Windows platforms

--- chef/lib/chef/rest.rb
+++ chef/lib/chef/rest.rb
@@ -345,6 +345,9 @@ class Chef
 
     def stream_to_tempfile(url, response)
       tf = Tempfile.open("chef-rest")
+      if RUBY_PLATFORM =~ /mswin|mingw32|windows/
+        tf.binmode #required for binary files on Windows platforms
+      end
       Chef::Log.debug("Streaming download from #{url.to_s} to tempfile #{tf.path}")
       # Stolen from http://www.ruby-forum.com/topic/166423
       # Kudos to _why!
