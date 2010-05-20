4653d7f98a8923a1bd41dca8f6c6ce9dd04eafb5
Daniel DeLeo <dan@opscode.com>
2010-05-20 12:00:59 -0700

    fix specs for updated contracts && overmocking

--- chef/spec/unit/provider/http_request_spec.rb
+++ chef/spec/unit/provider/http_request_spec.rb
@@ -18,191 +18,117 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
 
-describe Chef::Provider::HttpRequest, "initialize" do
+describe Chef::Provider::HttpRequest do
   before(:each) do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::HttpRequest", 
-      :null_object => true,
-      :name => "adam",
-      :url => "http://www.opscode.com",
-      :message => "is cool"
-    )
-    @provider = Chef::Provider::HttpRequest.new(@node, @new_resource)
-  end
-  
-  it "should return a Chef::Provider::HttpRequest" do
-    @provider.should be_a_kind_of(Chef::Provider::HttpRequest)
-  end
-end
+    @node = Chef::Node.new
+    @run_context = Chef::RunContext.new(@node, {})
+    
+    @new_resource = Chef::Resource::HttpRequest.new('adam')
+    @new_resource.name "adam"
+    @new_resource.url "http://www.opscode.com"
+    @new_resource.message "is cool"
 
-describe Chef::Provider::HttpRequest, "load_current_resource" do  
-  before(:each) do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::HttpRequest", 
-      :null_object => true,
-      :name => "adam",
-      :url => "http://www.opscode.com",
-      :message => "is cool"
-    )
-    @provider = Chef::Provider::HttpRequest.new(@node, @new_resource)
+    @provider = Chef::Provider::HttpRequest.new(@new_resource, {})
   end
   
-  it "should set up a Chef::REST client, with no authentication" do
-    Chef::REST.should_receive(:new).with(@new_resource.url, nil, nil)
-    @provider.load_current_resource
+  describe "load_current_resource" do  
+  
+    it "should set up a Chef::REST client, with no authentication" do
+      Chef::REST.should_receive(:new).with(@new_resource.url, nil, nil)
+      @provider.load_current_resource
+    end
   end
-end
 
-describe Chef::Provider::HttpRequest, "action_get" do  
-  before(:each) do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::HttpRequest", 
-      :null_object => true,
-      :name => "adam",
-      :url => "http://www.opscode.com",
-      :message => "is cool"
-    )
-    @rest = mock("Chef::REST", 
-      :null_object => true, 
-      :create_url => "http://www.opscode.com",
-      :run_request => "you made it!"
-    )
-    @provider = Chef::Provider::HttpRequest.new(@node, @new_resource)
-    @provider.rest = @rest
-  end
-  
-  it "should create the url with a message argument" do
-    @rest.should_receive(:create_url).with("#{@new_resource.url}?message=#{@new_resource.message}")
-    @provider.action_get
-  end
-  
-  it "should inflate a message block at runtime" do
-    @new_resource.stub!(:message).and_return(lambda { "return" })
-    @rest.should_receive(:create_url).with("#{@new_resource.url}?message=return")
-    @provider.action_get
-  end
-  
-  it "should run a GET request" do
-    @rest.should_receive(:run_request).with(:GET, @rest.create_url, {}, false, 10, false)
-    @provider.action_get
-  end
-  
-  it "should update the resource" do
-    @new_resource.should_receive(:updated=).with(true)
-    @provider.action_get
-  end
-end
+  describe "when making REST calls" do
+    before(:each) do
+      @rest = mock("Chef::REST", :create_url => "http://www.opscode.com", :run_request => "you made it!" )
+      @provider.rest = @rest
+    end
 
-describe Chef::Provider::HttpRequest, "action_put" do  
-  before(:each) do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::HttpRequest", 
-      :null_object => true,
-      :name => "adam",
-      :url => "http://www.opscode.com",
-      :message => "is cool"
-    )
-    @rest = mock("Chef::REST", 
-      :null_object => true, 
-      :create_url => "http://www.opscode.com",
-      :run_request => "you made it!"
-    )
-    @provider = Chef::Provider::HttpRequest.new(@node, @new_resource)
-    @provider.rest = @rest
-  end
+    describe "action_get" do  
+      it "should create the url with a message argument" do
+        @rest.should_receive(:create_url).with("#{@new_resource.url}?message=#{@new_resource.message}")
+        @provider.action_get
+      end
   
-  it "should create the url" do
-    @rest.should_receive(:create_url).with("#{@new_resource.url}")
-    @provider.action_put
-  end
+      it "should inflate a message block at runtime" do
+        @new_resource.stub!(:message).and_return(lambda { "return" })
+        @rest.should_receive(:create_url).with("#{@new_resource.url}?message=return")
+        @provider.action_get
+      end
   
-  it "should run a PUT request with the message as the payload" do
-    @rest.should_receive(:run_request).with(:PUT, @rest.create_url, {}, @new_resource.message, 10, false)
-    @provider.action_put
-  end
+      it "should run a GET request" do
+        @rest.should_receive(:run_request).with(:GET, @rest.create_url, {}, false, 10, false)
+        @provider.action_get
+      end
   
-  it "should inflate a message block at runtime" do
-    @new_resource.stub!(:message).and_return(lambda { "return" })
-    @rest.should_receive(:run_request).with(:PUT, @rest.create_url, {}, "return", 10, false)    
-    @provider.action_put
-  end
-  
-  it "should update the resource" do
-    @new_resource.should_receive(:updated=).with(true)
-    @provider.action_put
-  end
-end
+      it "should update the resource" do
+        @new_resource.should_receive(:updated=).with(true)
+        @provider.action_get
+      end
+    end
 
-describe Chef::Provider::HttpRequest, "action_post" do  
-  before(:each) do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::HttpRequest", 
-      :null_object => true,
-      :name => "adam",
-      :url => "http://www.opscode.com",
-      :message => "is cool"
-    )
-    @rest = mock("Chef::REST", 
-      :null_object => true, 
-      :create_url => "http://www.opscode.com",
-      :run_request => "you made it!"
-    )
-    @provider = Chef::Provider::HttpRequest.new(@node, @new_resource)
-    @provider.rest = @rest
-  end
+    describe "action_put" do  
+      it "should create the url" do
+        @rest.should_receive(:create_url).with("#{@new_resource.url}")
+        @provider.action_put
+      end
   
-  it "should create the url" do
-    @rest.should_receive(:create_url).with("#{@new_resource.url}")
-    @provider.action_post
-  end
+      it "should run a PUT request with the message as the payload" do
+        @rest.should_receive(:run_request).with(:PUT, @rest.create_url, {}, @new_resource.message, 10, false)
+        @provider.action_put
+      end
   
-  it "should run a PUT request with the message as the payload" do
-    @rest.should_receive(:run_request).with(:POST, @rest.create_url, {}, @new_resource.message, 10, false)
-    @provider.action_post
-  end
+      it "should inflate a message block at runtime" do
+        @new_resource.stub!(:message).and_return(lambda { "return" })
+        @rest.should_receive(:run_request).with(:PUT, @rest.create_url, {}, "return", 10, false)    
+        @provider.action_put
+      end
   
-  it "should inflate a message block at runtime" do
-    @new_resource.stub!(:message).and_return(lambda { "return" })
-    @rest.should_receive(:run_request).with(:POST, @rest.create_url, {}, "return", 10, false)    
-    @provider.action_post
-  end
-  
-  it "should update the resource" do
-    @new_resource.should_receive(:updated=).with(true)
-    @provider.action_post
-  end
-end
+      it "should update the resource" do
+        @new_resource.should_receive(:updated=).with(true)
+        @provider.action_put
+      end
+    end
 
-describe Chef::Provider::HttpRequest, "action_delete" do  
-  before(:each) do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::HttpRequest", 
-      :null_object => true,
-      :name => "adam",
-      :url => "http://www.opscode.com",
-      :message => "is cool"
-    )
-    @rest = mock("Chef::REST", 
-      :null_object => true, 
-      :create_url => "http://www.opscode.com",
-      :run_request => "you made it!"
-    )
-    @provider = Chef::Provider::HttpRequest.new(@node, @new_resource)
-    @provider.rest = @rest
-  end
+    describe "action_post" do  
+      it "should create the url" do
+        @rest.should_receive(:create_url).with("#{@new_resource.url}")
+        @provider.action_post
+      end
   
-  it "should create the url" do
-    @rest.should_receive(:create_url).with("#{@new_resource.url}")
-    @provider.action_delete
-  end
+      it "should run a PUT request with the message as the payload" do
+        @rest.should_receive(:run_request).with(:POST, @rest.create_url, {}, @new_resource.message, 10, false)
+        @provider.action_post
+      end
   
-  it "should run a DELETE request" do
-    @rest.should_receive(:run_request).with(:DELETE, @rest.create_url, {}, false, 10, false)
-    @provider.action_delete
-  end
+      it "should inflate a message block at runtime" do
+        @new_resource.stub!(:message).and_return(lambda { "return" })
+        @rest.should_receive(:run_request).with(:POST, @rest.create_url, {}, "return", 10, false)    
+        @provider.action_post
+      end
   
-  it "should update the resource" do
-    @new_resource.should_receive(:updated=).with(true)
-    @provider.action_delete
+      it "should update the resource" do
+        @new_resource.should_receive(:updated=).with(true)
+        @provider.action_post
+      end
+    end
+
+    describe "action_delete" do  
+      it "should create the url" do
+        @rest.should_receive(:create_url).with("#{@new_resource.url}")
+        @provider.action_delete
+      end
+  
+      it "should run a DELETE request" do
+        @rest.should_receive(:run_request).with(:DELETE, @rest.create_url, {}, false, 10, false)
+        @provider.action_delete
+      end
+  
+      it "should update the resource" do
+        @new_resource.should_receive(:updated=).with(true)
+        @provider.action_delete
+      end
+    end
   end
 end
