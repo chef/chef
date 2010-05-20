24dc8f89f8c10e61a49ad70582daeafc31551c79
Daniel DeLeo <dan@opscode.com>
2010-05-20 13:17:16 -0700

    update specs for new object contracts

--- chef/spec/unit/provider/group/usermod_spec.rb
+++ chef/spec/unit/provider/group/usermod_spec.rb
@@ -18,92 +18,88 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "spec_helper"))
 
-describe Chef::Provider::Group::Usermod, "modify_group_members" do
+describe Chef::Provider::Group::Usermod do
   before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group",
-      :null_object => true,
-      :group_name => "aj",
-      :members => [ "all", "your", "base" ],
-      :append => false
-    )
-    @new_resource.stub!(:to_s).and_return("group[aj]")
-    @provider = Chef::Provider::Group::Usermod.new(@node, @new_resource)
-    @provider.stub!(:run_command).and_return(true)
+    @node = Chef::Node.new
+    @run_context = Chef::RunContext.new(@node, {})
+    @new_resource = Chef::Resource::Group.new("wheel")
+    @new_resource.members [ "all", "your", "base" ]
+    @provider = Chef::Provider::Group::Usermod.new(@new_resource, @run_context)
+    @provider.stub!(:run_command)
   end
   
-  describe "with an empty members array" do
-    before do
-      @new_resource.stub!(:members).and_return([])
-    end
-    
-    it "should log an appropriate message" do
-      Chef::Log.should_receive(:debug).with("group[aj]: not changing group members, the group has no members")
-      @provider.modify_group_members
-    end
-  end
+  describe "modify_group_members" do
   
-  describe "with supplied members" do
-    platforms = {
-      "openbsd" => "-G",
-      "netbsd" => "-G",
-      "solaris" => "-a -G"
-    }
-
-    before do
-      @new_resource.stub!(:members).and_return(["all", "your", "base"])
-    end
-
-    it "should raise an error when setting the entire group directly" do
-      lambda { @provider.modify_group_members }.should raise_error(Chef::Exceptions::Group, "setting group members directly is not supported by #{@provider.to_s}")
-    end
+    describe "with an empty members array" do
+      before do
+        @new_resource.stub!(:members).and_return([])
+      end
     
-    platforms.each do |platform, flags|
-      it "should usermod each user when the append option is set on #{platform}" do
-        @node.stub!(:[]).with(:platform).and_return(platform)
-        @new_resource.stub!(:append).and_return(true)
-        @provider.should_receive(:run_command).with({:command => "usermod #{flags} aj all"})
-        @provider.should_receive(:run_command).with({:command => "usermod #{flags} aj your"})
-        @provider.should_receive(:run_command).with({:command => "usermod #{flags} aj base"})
+      it "should log an appropriate message" do
+        Chef::Log.should_receive(:debug).with("group[wheel]: not changing group members, the group has no members")
         @provider.modify_group_members
       end
     end
-  end
-end
-
-describe Chef::Provider::Group::Usermod, "load_current_resource" do
-  before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group", :null_object => true, :group_name => "aj")
-    @provider = Chef::Provider::Group::Usermod.new(@node, @new_resource)
-    File.stub!(:exists?).and_return(false)
-  end
-
-  it "should raise an error if the required binary /usr/sbin/groupadd doesn't exist" do
-    File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(false)
-    lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
-  end
-  it "should raise an error if the required binary /usr/sbin/groupmod doesn't exist" do
-    File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(true)
-    File.should_receive(:exists?).with("/usr/sbin/groupmod").and_return(false)
-    lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
-  end
-  it "should raise an error if the required binary /usr/sbin/groupdel doesn't exist" do
-    File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(true)
-    File.should_receive(:exists?).with("/usr/sbin/groupmod").and_return(true)
-    File.should_receive(:exists?).with("/usr/sbin/groupdel").and_return(false)
-    lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
-  end
-  it "should raise an error if the required binary /usr/sbin/usermod doesn't exist" do
-    File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(true)
-    File.should_receive(:exists?).with("/usr/sbin/groupmod").and_return(true)
-    File.should_receive(:exists?).with("/usr/sbin/groupdel").and_return(true)
-    File.should_receive(:exists?).with("/usr/sbin/usermod").and_return(false)
-    lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
-  end
   
-  it "shouldn't raise an error if the required binaries exist" do
-    File.stub!(:exists?).and_return(true)
-    lambda { @provider.load_current_resource }.should_not raise_error(Chef::Exceptions::Group)
+    describe "with supplied members" do
+      platforms = {
+        "openbsd" => "-G",
+        "netbsd" => "-G",
+        "solaris" => "-a -G"
+      }
+
+      before do
+        @new_resource.stub!(:members).and_return(["all", "your", "base"])
+      end
+
+      it "should raise an error when setting the entire group directly" do
+        lambda { @provider.modify_group_members }.should raise_error(Chef::Exceptions::Group, "setting group members directly is not supported by #{@provider.to_s}")
+      end
+    
+      platforms.each do |platform, flags|
+        it "should usermod each user when the append option is set on #{platform}" do
+          @node[:platform] = platform
+          @new_resource.stub!(:append).and_return(true)
+          @provider.should_receive(:run_command).with({:command => "usermod #{flags} wheel all"})
+          @provider.should_receive(:run_command).with({:command => "usermod #{flags} wheel your"})
+          @provider.should_receive(:run_command).with({:command => "usermod #{flags} wheel base"})
+          @provider.modify_group_members
+        end
+      end
+    end
+  end
+
+  describe "when loading the current resource" do
+    before do
+      File.stub!(:exists?).and_return(false)
+    end
+
+    it "should raise an error if the required binary /usr/sbin/groupadd doesn't exist" do
+      File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(false)
+      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
+    end
+    it "should raise an error if the required binary /usr/sbin/groupmod doesn't exist" do
+      File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(true)
+      File.should_receive(:exists?).with("/usr/sbin/groupmod").and_return(false)
+      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
+    end
+    it "should raise an error if the required binary /usr/sbin/groupdel doesn't exist" do
+      File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(true)
+      File.should_receive(:exists?).with("/usr/sbin/groupmod").and_return(true)
+      File.should_receive(:exists?).with("/usr/sbin/groupdel").and_return(false)
+      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
+    end
+    it "should raise an error if the required binary /usr/sbin/usermod doesn't exist" do
+      File.should_receive(:exists?).with("/usr/sbin/groupadd").and_return(true)
+      File.should_receive(:exists?).with("/usr/sbin/groupmod").and_return(true)
+      File.should_receive(:exists?).with("/usr/sbin/groupdel").and_return(true)
+      File.should_receive(:exists?).with("/usr/sbin/usermod").and_return(false)
+      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
+    end
+  
+    it "shouldn't raise an error if the required binaries exist" do
+      File.stub!(:exists?).and_return(true)
+      lambda { @provider.load_current_resource }.should_not raise_error(Chef::Exceptions::Group)
+    end
   end
 end
