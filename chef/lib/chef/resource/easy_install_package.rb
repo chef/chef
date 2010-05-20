ddefcd379569ca7797c60b3f5d8241e211846832
Daniel DeLeo <dan@opscode.com>
2010-05-20 00:04:05 -0700

    fix method signatures

--- chef/lib/chef/resource/easy_install_package.rb
+++ chef/lib/chef/resource/easy_install_package.rb
@@ -22,7 +22,7 @@ class Chef
   class Resource
     class EasyInstallPackage < Chef::Resource::Package
       
-      def initialize(name, run_context)
+      def initialize(name, run_context=nil)
         super
         @resource_name = :easy_install_package
         @provider = Chef::Provider::Package::EasyInstall
