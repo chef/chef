8157dd8cb7e0e0dada3942448df1a518761f3d6e
Daniel DeLeo <dan@opscode.com>
2010-05-20 13:43:56 -0700

    updated method signatures

--- chef/spec/unit/provider/deploy/timestamped_spec.rb
+++ chef/spec/unit/provider/deploy/timestamped_spec.rb
@@ -26,7 +26,8 @@ describe Chef::Provider::Deploy::Timestamped do
     @expected_release_dir = "/my/deploy/dir/releases/20040815162342"
     @resource = Chef::Resource::Deploy.new("/my/deploy/dir")
     @node = Chef::Node.new
-    @timestamped_deploy = Chef::Provider::Deploy::Timestamped.new(@node, @resource)
+    @run_context = Chef::RunContext.new(@node, {})
+    @timestamped_deploy = Chef::Provider::Deploy::Timestamped.new(@resource, @run_context)
     @runner = mock("runnah", :null_object => true)
     Chef::Runner.stub!(:new).and_return(@runner)
   end
