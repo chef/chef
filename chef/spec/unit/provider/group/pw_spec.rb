71c0aeb18fad1aa3d7f66171df6abe91592e2e92
Daniel DeLeo <dan@opscode.com>
2010-05-20 11:20:57 -0700

    fix pw group provider for new contracts+overmocking

--- chef/spec/unit/provider/group/pw_spec.rb
+++ chef/spec/unit/provider/group/pw_spec.rb
@@ -18,183 +18,118 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "spec_helper"))
 
-describe Chef::Provider::Group::Pw, "set_options" do
+describe Chef::Provider::Group::Pw do
   before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group",
-      :null_object => true,
-      :group_name => "aj",
-      :gid => 50,
-      :members => [ "root", "aj"]
-    )
-    @current_resource = mock("Chef::Resource::Group",
-      :null_object => true,
-      :group_name => "aj",
-      :gid => 50,
-      :members => [ "root", "aj"]
-    )
-    @provider = Chef::Provider::Group::Pw.new(@node, @new_resource)
+    @node = Chef::Node.new
+    @run_context = Chef::RunContext.new(@node, {})
+    
+    @new_resource = Chef::Resource::Group.new("wheel")
+    @new_resource.gid 50
+    @new_resource.members [ "root", "aj"]
+
+    @current_resource = Chef::Resource::Group.new("aj")
+    @current_resource.gid 50
+    @current_resource.members [ "root", "aj"]
+    @provider = Chef::Provider::Group::Pw.new(@new_resource, @run_context)
     @provider.current_resource = @current_resource
   end
   
-  field_list = {
-    :gid => "-g"
-  }
-  
-  field_list.each do |attribute, option|
-    it "should check for differences in #{attribute.to_s} between the current and new resources" do
-        @new_resource.should_receive(attribute)
-        @current_resource.should_receive(attribute)
-        @provider.set_options
-    end
-    it "should set the option for #{attribute} if the new resources #{attribute} is not null" do
-      @new_resource.stub!(attribute).and_return("wowaweea")
-      @provider.set_options.should eql(" #{@new_resource.group_name} #{option} '#{@new_resource.send(attribute)}'")
-    end
-  end
-  
-  it "should combine all the possible options" do
-    match_string = " aj"
-    field_list.sort{ |a,b| a[0] <=> b[0] }.each do |attribute, option|
-      @new_resource.stub!(attribute).and_return("hola")
-      match_string << " #{option} 'hola'"
-    end
-    @provider.set_options.should eql(match_string)
-  end
-end
-
-describe Chef::Provider::Group::Pw, "create_group" do
-  before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group", :null_object => true)
-    @provider = Chef::Provider::Group::Pw.new(@node, @new_resource)
-    @provider.stub!(:run_command).and_return(true)
-    @provider.stub!(:set_options).and_return(" monkey")
-    @provider.stub!(:set_members_option).and_return(" -M purple")
-  end
-  
-  it "should run pw groupadd with the return of set_options and set_members_option" do
-    @provider.should_receive(:run_command).with({ :command => "pw groupadd monkey -M purple" }).and_return(true)
-    @provider.create_group
-  end
-end
-
-describe Chef::Provider::Group::Pw, "manage_group" do
-  before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group", :null_object => true)
-    @provider = Chef::Provider::Group::Pw.new(@node, @new_resource)
-    @provider.stub!(:run_command).and_return(true)
-    @provider.stub!(:set_options).and_return(" monkey")
-    @provider.stub!(:set_members_option).and_return(" -M purple")
-  end
-  
-  it "should run pw groupmod with the return of set_options" do
-    @provider.should_receive(:run_command).with({ :command => "pw groupmod monkey -M purple" }).and_return(true)
-    @provider.manage_group
-  end
-
-end
-
-describe Chef::Provider::Group::Pw, "remove_group" do
-  before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group",
-      :null_object => true,
-      :group_name => "aj"
-    )
-    @provider = Chef::Provider::Group::Pw.new(@node, @new_resource)
-    @provider.stub!(:run_command).and_return(true)
-  end
-  
-  it "should run pw groupdel with the new resources group name" do
-    @provider.should_receive(:run_command).with({ :command => "pw groupdel aj" }).and_return(true)
-    @provider.remove_group
-  end
-end
-
-describe Chef::Provider::Group::Pw, "set_members_option" do
-  before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group",
-      :null_object => true,
-      :group_name => "aj",
-      :members => [ "all", "your", "base" ]
-    )
-    @current_resource = mock("Chef::Resource::Group",
-      :null_object => true,
-      :group_name => "aj",
-      :members => [ "all", "your", "base" ]
-    )
-    @new_resource.stub!(:to_s).and_return("group[aj]")
-    @provider = Chef::Provider::Group::Pw.new(@node, @new_resource)
-    @provider.current_resource = @current_resource
-  end
-  
-  describe "with an empty members array in both the new and current resource" do
-    before do
-      @new_resource.stub!(:members).and_return([])
-      @current_resource.stub!(:members).and_return([])
+  describe "when setting options for the pw command" do
+    it "does not set the gid option if gids match or are unmanaged" do
+      @provider.set_options.should ==  " wheel"
     end
     
-    it "should log an appropriate message" do
-      Chef::Log.should_receive(:debug).with("group[aj]: not changing group members, the group has no members")
-      @provider.set_members_option
-    end
-    
-    it "should set no options" do
-      @provider.set_members_option.should eql("")
+    it "sets the option for gid if it is not nil" do
+      @new_resource.gid(42)
+      @provider.set_options.should eql(" wheel -g '42'")
     end
   end
 
-  describe "with an empty members array in the new resource and existing members in the current resource" do
-    before do
-      @new_resource.stub!(:members).and_return([])
-      @current_resource.stub!(:members).and_return(["all", "your", "base"])
-    end
-    
-    it "should log an appropriate message" do
-      Chef::Log.should_receive(:debug).with("group[aj]: removing group members all, your, base")
-      @provider.set_members_option
-    end
-    
-    it "should set the -d option with the members joined by ','" do
-      @provider.set_members_option.should eql(" -d all,your,base")
+  describe "when creating a group" do
+    it "should run pw groupadd with the return of set_options and set_members_option" do
+      @new_resource.gid(23)
+      @provider.should_receive(:run_command).with({ :command => "pw groupadd wheel -g '23' -M root,aj" }).and_return(true)
+      @provider.create_group
     end
   end
+
+  describe "when managing the group" do
   
-  describe "with supplied members array in the new resource and an empty members array in the current resource" do
-    before do
-      @new_resource.stub!(:members).and_return(["all", "your", "base"])
-      @current_resource.stub!(:members).and_return([])
+    it "should run pw groupmod with the return of set_options" do
+      @new_resource.gid(42)
+      @provider.should_receive(:run_command).with({ :command => "pw groupmod wheel -g '42' -M root,aj" }).and_return(true)
+      @provider.manage_group
     end
-    
-    it "should log an appropriate debug message" do
-      Chef::Log.should_receive(:debug).with("group[aj]: setting group members to all, your, base")
-      @provider.set_members_option
-    end
-    
-    it "should set the -M option with the members joined by ','" do
-      @provider.set_members_option.should eql(" -M all,your,base")
-    end
-  end
-end
 
-describe Chef::Provider::Group::Pw, "load_current_resource" do
-  before do
-    @node = mock("Chef::Node", :null_object => true)
-    @new_resource = mock("Chef::Resource::Group", :null_object => true, :group_name => "aj")
-    @provider = Chef::Provider::Group::Pw.new(@node, @new_resource)
-    File.stub!(:exists?).and_return(false)
   end
 
-  it "should raise an error if the required binary /usr/sbin/pw doesn't exist" do
-    File.should_receive(:exists?).with("/usr/sbin/pw").and_return(false)
-    lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
+  describe "when removing the group" do
+    it "should run pw groupdel with the new resources group name" do
+      @provider.should_receive(:run_command).with({ :command => "pw groupdel wheel" }).and_return(true)
+      @provider.remove_group
+    end
   end
+
+  describe "when setting group membership" do
   
-  it "shouldn't raise an error if /usr/sbin/pw exists" do
-    File.stub!(:exists?).and_return(true)
-    lambda { @provider.load_current_resource }.should_not raise_error(Chef::Exceptions::Group)
+    describe "with an empty members array in both the new and current resource" do
+      before do
+        @new_resource.stub!(:members).and_return([])
+        @current_resource.stub!(:members).and_return([])
+      end
+    
+      it "should log an appropriate message" do
+        Chef::Log.should_receive(:debug).with("group[wheel]: not changing group members, the group has no members")
+        @provider.set_members_option
+      end
+    
+      it "should set no options" do
+        @provider.set_members_option.should eql("")
+      end
+    end
+
+    describe "with an empty members array in the new resource and existing members in the current resource" do
+      before do
+        @new_resource.stub!(:members).and_return([])
+        @current_resource.stub!(:members).and_return(["all", "your", "base"])
+      end
+    
+      it "should log an appropriate message" do
+        Chef::Log.should_receive(:debug).with("group[wheel]: removing group members all, your, base")
+        @provider.set_members_option
+      end
+    
+      it "should set the -d option with the members joined by ','" do
+        @provider.set_members_option.should eql(" -d all,your,base")
+      end
+    end
+  
+    describe "with supplied members array in the new resource and an empty members array in the current resource" do
+      before do
+        @new_resource.stub!(:members).and_return(["all", "your", "base"])
+        @current_resource.stub!(:members).and_return([])
+      end
+    
+      it "should log an appropriate debug message" do
+        Chef::Log.should_receive(:debug).with("group[wheel]: setting group members to all, your, base")
+        @provider.set_members_option
+      end
+    
+      it "should set the -M option with the members joined by ','" do
+        @provider.set_members_option.should eql(" -M all,your,base")
+      end
+    end
+  end
+
+  describe"load_current_resource" do
+    it "should raise an error if the required binary /usr/sbin/pw doesn't exist" do
+      File.should_receive(:exists?).with("/usr/sbin/pw").and_return(false)
+      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Group)
+    end
+  
+    it "shouldn't raise an error if /usr/sbin/pw exists" do
+      File.stub!(:exists?).and_return(true)
+      lambda { @provider.load_current_resource }.should_not raise_error(Chef::Exceptions::Group)
+    end
   end
 end
