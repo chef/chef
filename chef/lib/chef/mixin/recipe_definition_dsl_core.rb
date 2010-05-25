e69b283d2996fda42bee2e7c8710499b5d5636a4
Daniel DeLeo <dan@opscode.com>
2010-05-25 16:39:56 -0700

    call super to raise the real error instead of being confusing

--- chef/lib/chef/mixin/recipe_definition_dsl_core.rb
+++ chef/lib/chef/mixin/recipe_definition_dsl_core.rb
@@ -51,10 +51,8 @@ class Chef
           # Otherwise, we're rocking the regular resource call route.
           method_name = method_symbol.to_s
           rname = convert_to_class_name(method_name)
-          
+
           super unless Chef::Resource.const_defined?(rname)
-          
-          raise ArgumentError, "Creating a resource requires a name argument" if args.length < 1
 
           # If we have a resource like this one, we want to steal its state
           args << run_context
