ba3ee82df45cdfaa1bc9b24eaeac7320ccd3c2f5
Daniel DeLeo <dan@opscode.com>
2010-05-26 17:36:13 -0700

    remote directory working again

--- chef/spec/unit/cookbook_manifest_spec.rb
+++ chef/spec/unit/cookbook_manifest_spec.rb
@@ -54,52 +54,52 @@ describe "Chef::Cookbook manifest" do
        # directory adirectory
        {
          :name => "anotherfile1.rb",
-         :path => "files/host-examplehost.example.org/adirectory/anotherfile1.rb",
+         :path => "files/host-examplehost.example.org/adirectory/anotherfile1.rb.host",
          :checksum => "csum-host-1",
          :specificity => "host-examplehost.example.org"
        },
        {
          :name => "anotherfile2.rb",
-         :path => "files/host-examplehost.example.org/adirectory/anotherfile2.rb",
+         :path => "files/host-examplehost.example.org/adirectory/anotherfile2.rb.host",
          :checksum => "csum-host-2",
          :specificity => "host-examplehost.example.org"
        },
        
        {
          :name => "anotherfile1.rb",
-         :path => "files/ubuntu-9.10/adirectory/anotherfile1.rb",
+         :path => "files/ubuntu-9.10/adirectory/anotherfile1.rb.platform-version",
          :checksum => "csum-platver-1",
          :specificity => "ubuntu-9.10"
        },
        {
          :name => "anotherfile2.rb",
-         :path => "files/ubuntu-9.10/adirectory/anotherfile2.rb",
+         :path => "files/ubuntu-9.10/adirectory/anotherfile2.rb.platform-version",
          :checksum => "csum-platver-2",
          :specificity => "ubuntu-9.10"
        },
        
        {
          :name => "anotherfile1.rb",
-         :path => "files/ubuntu/adirectory/anotherfile1.rb",
+         :path => "files/ubuntu/adirectory/anotherfile1.rb.platform",
          :checksum => "csum-plat-1",
          :specificity => "ubuntu"
        },
        {
          :name => "anotherfile2.rb",
-         :path => "files/ubuntu/adirectory/anotherfile2.rb",
+         :path => "files/ubuntu/adirectory/anotherfile2.rb.platform",
          :checksum => "csum-plat-2",
          :specificity => "ubuntu"
        },
        
        {
          :name => "anotherfile1.rb",
-         :path => "files/default/adirectory/anotherfile1.rb",
+         :path => "files/default/adirectory/anotherfile1.rb.default",
          :checksum => "csum-default-1",
          :specificity => "default"
        },
        {
          :name => "anotherfile2.rb",
-         :path => "files/default/adirectory/anotherfile2.rb",
+         :path => "files/default/adirectory/anotherfile2.rb.default",
          :checksum => "csum-default-2",
          :specificity => "default"
        },
@@ -154,64 +154,119 @@ describe "Chef::Cookbook manifest" do
     manifest_record[:checksum].should == "csum-default"
   end
   
-  ###
-  # Directory preference tests
-  ###
-  
-  it "should return a directory of manifest records based on priority preference: host" do
-    node = Chef::Node.new
-    node[:platform] = "ubuntu"
-    node[:platform_version] = "9.10"
-    node[:fqdn] = "examplehost.example.org"
-    
-    manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
-    manifest_records.should_not be_nil
-    manifest_records.size.should == 2
-    
-    checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
-    checksums.sort.should == ["csum-host-1", "csum-host-2"]
+  describe "when fetching the contents of a directory by file specificity" do
+
+    it "should return a directory of manifest records based on priority preference: host" do
+      node = Chef::Node.new
+      node[:platform] = "ubuntu"
+      node[:platform_version] = "9.10"
+      node[:fqdn] = "examplehost.example.org"
+
+      manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
+      manifest_records.should_not be_nil
+      manifest_records.size.should == 2
+
+      checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
+      checksums.sort.should == ["csum-host-1", "csum-host-2"]
+    end
+
+    it "should return a directory of manifest records based on priority preference: platform & version" do
+      node = Chef::Node.new
+      node[:platform] = "ubuntu"
+      node[:platform_version] = "9.10"
+      node[:fqdn] = "differenthost.example.org"
+
+      manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
+      manifest_records.should_not be_nil
+      manifest_records.size.should == 2
+
+      checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
+      checksums.sort.should == ["csum-platver-1", "csum-platver-2"]
+    end
+
+    it "should return a directory of manifest records based on priority preference: platform only" do
+      node = Chef::Node.new
+      node[:platform] = "ubuntu"
+      node[:platform_version] = "1.0"
+      node[:fqdn] = "differenthost.example.org"
+
+      manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
+      manifest_records.should_not be_nil
+      manifest_records.size.should == 2
+
+      checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
+      checksums.sort.should == ["csum-plat-1", "csum-plat-2"]
+    end
+
+    it "should return a directory of manifest records based on priority preference: default" do
+      node = Chef::Node.new
+      node[:platform] = "notubuntu"
+      node[:platform_version] = "1.0"
+      node[:fqdn] = "differenthost.example.org"
+
+      manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
+      manifest_records.should_not be_nil
+      manifest_records.size.should == 2
+
+      checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
+
+      checksums.sort.should == ["csum-default-1", "csum-default-2"]
+    end
   end
-  
-  it "should return a directory of manifest records based on priority preference: platform & version" do
-    node = Chef::Node.new
-    node[:platform] = "ubuntu"
-    node[:platform_version] = "9.10"
-    node[:fqdn] = "differenthost.example.org"
-    
-    manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
-    manifest_records.should_not be_nil
-    manifest_records.size.should == 2
-    
-    checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
-    checksums.sort.should == ["csum-platver-1", "csum-platver-2"]
+
+  ## Globbing the relative paths out of the manifest records ##
+
+  describe "when globbing for relative file paths based on filespecificity" do
+    it "should return a list of relative paths based on priority preference: host" do
+      node = Chef::Node.new
+      node[:platform] = "ubuntu"
+      node[:platform_version] = "9.10"
+      node[:fqdn] = "examplehost.example.org"
+
+      filenames = @cookbook.relative_filenames_in_preferred_directory(node, :files, "adirectory")
+      filenames.should_not be_nil
+      filenames.size.should == 2
+
+      filenames.sort.should == ['anotherfile1.rb.host', 'anotherfile2.rb.host']
+    end
+
+    it "should return a list of relative paths based on priority preference: platform & version" do
+      node = Chef::Node.new
+      node[:platform] = "ubuntu"
+      node[:platform_version] = "9.10"
+      node[:fqdn] = "differenthost.example.org"
+
+      filenames = @cookbook.relative_filenames_in_preferred_directory(node, :files, "adirectory")
+      filenames.should_not be_nil
+      filenames.size.should == 2
+
+      filenames.sort.should == ['anotherfile1.rb.platform-version', 'anotherfile2.rb.platform-version']
+    end
+
+    it "should return a list of relative paths based on priority preference: platform only" do
+      node = Chef::Node.new
+      node[:platform] = "ubuntu"
+      node[:platform_version] = "1.0"
+      node[:fqdn] = "differenthost.example.org"
+
+      filenames = @cookbook.relative_filenames_in_preferred_directory(node, :files, "adirectory")
+      filenames.should_not be_nil
+      filenames.size.should == 2
+
+      filenames.sort.should == ['anotherfile1.rb.platform', 'anotherfile2.rb.platform']
+    end
+
+    it "should return a list of relative paths based on priority preference: default" do
+      node = Chef::Node.new
+      node[:platform] = "notubuntu"
+      node[:platform_version] = "1.0"
+      node[:fqdn] = "differenthost.example.org"
+
+      filenames = @cookbook.relative_filenames_in_preferred_directory(node, :files, "adirectory")
+      filenames.should_not be_nil
+      filenames.size.should == 2
+
+      filenames.sort.should == ['anotherfile1.rb.default', 'anotherfile2.rb.default']
+    end
   end
-  
-  it "should return a directory of manifest records based on priority preference: platform only" do
-    node = Chef::Node.new
-    node[:platform] = "ubuntu"
-    node[:platform_version] = "1.0"
-    node[:fqdn] = "differenthost.example.org"
-    
-    manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
-    manifest_records.should_not be_nil
-    manifest_records.size.should == 2
-    
-    checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
-    checksums.sort.should == ["csum-plat-1", "csum-plat-2"]
-  end
-  
-  it "should return a directory of manifest records based on priority preference: default" do
-    node = Chef::Node.new
-    node[:platform] = "notubuntu"
-    node[:platform_version] = "1.0"
-    node[:fqdn] = "differenthost.example.org"
-    
-    manifest_records = @cookbook.preferred_manifest_records_for_directory(node, :files, "adirectory")
-    manifest_records.should_not be_nil
-    manifest_records.size.should == 2
-    
-    checksums = manifest_records.map{ |manifest_record| manifest_record[:checksum] }
-    checksums.sort.should == ["csum-default-1", "csum-default-2"]
-  end
-  
 end
