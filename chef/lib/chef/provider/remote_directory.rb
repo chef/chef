15327eb5a678ac104fd67a297b6744a3e259636d
Joshua Sierles <joshua@37signals.com>
2010-06-07 13:18:53 +0400

    [CHEF-301] Directories and files with a dot (.ssh for example) should be copied with the remote_directory resource

--- chef/lib/chef/provider/remote_directory.rb
+++ chef/lib/chef/provider/remote_directory.rb
@@ -36,8 +36,11 @@ class Chef
         super
         Chef::Log.debug("Doing a remote recursive directory transfer for #{@new_resource}")
           
-        files_to_purge = Set.new Dir[::File.join(@new_resource.path, '**', '*')]
-
+        files_to_purge = Set.new(
+          Dir.glob(::File.join(@new_resource.path, '**', '*'), ::File::FNM_DOTMATCH).select do |name|
+            name !~ /(?:^|#{Regexp.escape(::File::SEPARATOR)})\.\.?$/
+          end
+        )
         files_to_transfer.each do |cookbook_file_relative_path|
           create_cookbook_file(cookbook_file_relative_path)
           files_to_purge.delete(::File.dirname(::File.join(@new_resource.path, cookbook_file_relative_path)))
