1242697298df56e45937d3afc1c4909680a2f677
Adam Jacob <adam@opscode.com>
2010-06-08 14:21:46 -0700

    Adding a rescue loop to EC2 bootstrapping

--- chef/lib/chef/knife/ec2_server_create.rb
+++ chef/lib/chef/knife/ec2_server_create.rb
@@ -117,15 +117,17 @@ class Chef
         puts "#{h.color("Private DNS Name", :cyan)}: #{server.private_dns_name}"
         puts "#{h.color("Private IP Address", :cyan)}: #{server.private_ip_address}"
 
-        puts "\nWaiting 15 seconds for SSH...\n"
-
-        sleep 15
-
-        bootstrap = Chef::Knife::Bootstrap.new
-        bootstrap.name_args = [ server.ip_address, @name_args ].flatten
-        bootstrap.config[:ssh_user] = config[:ssh_user] 
-        bootstrap.config[:chef_node_name] = server.id
-        bootstrap.run
+        begin
+          bootstrap = Chef::Knife::Bootstrap.new
+          bootstrap.name_args = [ server.ip_address, @name_args ].flatten
+          bootstrap.config[:ssh_user] = config[:ssh_user] 
+          bootstrap.config[:chef_node_name] = server.id
+          bootstrap.run
+        rescue Errno::ECONNREFUSED
+          puts h.color("Connection refused on SSH, retrying - CTRL-C to abort")
+          sleep 1
+          retry
+        end
 
         puts "\n"
         puts "#{h.color("Instance ID", :cyan)}: #{server.id}"
