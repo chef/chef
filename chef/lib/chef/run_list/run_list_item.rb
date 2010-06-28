fe5596d708ff9af80e2e0f4312eed073a4ce8b55
Thom May <thom.may@betfair.com>
2010-06-28 14:04:35 +0100

    Add version handling to recipe items

--- chef/lib/chef/run_list/run_list_item.rb
+++ chef/lib/chef/run_list/run_list_item.rb
@@ -15,26 +15,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'chef/cookbook/metadata/version'
+
 class Chef
   class RunList
     class RunListItem
-      QUALIFIED_RECIPE = %r{^recipe\[([^\]]+)\]$}
+      QUALIFIED_RECIPE = %r{^recipe\[([^\],]+)(,(#{Chef::Cookbook::Metadata::Version::PATTERN}))?\]$}
       QUALIFIED_ROLE   = %r{^role\[([^\]]+)\]$}
 
-      attr_reader :name
+      attr_reader :name, :type, :version
 
-      attr_reader :type
 
       def initialize(item)
+        @version = nil
         case item
         when Hash
           assert_hash_is_valid_run_list_item!(item)
           @type = (item['type'] || item[:type]).to_sym
           @name = item['name'] || item[:name]
+          if (item.has_key?('version') || item.has_key?(:version))
+            @version = item['version'] || item[:version]
+          end
         when String
           if match = QUALIFIED_RECIPE.match(item)
             @type = :recipe
             @name = match[1]
+            @version = match[3] if match[3]
           elsif match = QUALIFIED_ROLE.match(item)
             @type = :role
             @name = match[1]
@@ -48,7 +54,7 @@ class Chef
       end
 
       def to_s
-        "#{@type}[#{@name}]"
+        "#{@type}[#{@name}#{@version ? ",#{@version}" :""}]"
       end
 
       def role?
@@ -63,7 +69,7 @@ class Chef
         if other.kind_of?(String)
           self.to_s == other.to_s
         else
-          other.respond_to?(:type) && other.respond_to?(:name) && other.type == @type && other.name == @name
+          other.respond_to?(:type) && other.respond_to?(:name) && other.respond_to?(:version) && other.type == @type && other.name == @name && other.version == @version
         end
       end
 
