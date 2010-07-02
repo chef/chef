e4fa84dee534a2174352789cb8cd1092cc90147c
Daniel DeLeo <dan@opscode.com>
2010-07-02 12:45:53 -0700

    bump version to 0.9.6 for release

--- chef-solr/lib/chef/solr/version.rb
+++ chef-solr/lib/chef/solr/version.rb
@@ -1,5 +1,5 @@
 class Chef
   class Solr
-    VERSION = '0.9.4'
+    VERSION = '0.9.6'
   end
 end
--- chef/lib/chef/version.rb
+++ chef/lib/chef/version.rb
@@ -16,5 +16,5 @@
 # limitations under the License.
 
 class Chef
-  VERSION = '0.9.4'
+  VERSION = '0.9.6'
 end
