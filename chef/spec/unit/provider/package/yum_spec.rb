25629d4f2644c6da98a4a1e6bbbcfba3a0973d99
Daniel DeLeo <dan@opscode.com>
2010-07-08 16:57:05 -0700

    cleanup specs for less mocking, better organization

--- chef/spec/unit/provider/package/yum_spec.rb
+++ chef/spec/unit/provider/package/yum_spec.rb
@@ -18,12 +18,11 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "spec_helper"))
 
-describe Chef::Provider::Package::Yum, "load_current_resource" do
+describe Chef::Provider::Package::Yum do
   before(:each) do
     @node = Chef::Node.new
     @run_context = Chef::RunContext.new(@node, {})
     @new_resource = Chef::Resource::Package.new('cups')
-    @current_resource = Chef::Resource::Package.new('cups')
     @status = mock("Status", :exitstatus => 0)
     @yum_cache = mock(
       'Chef::Provider::Yum::YumCache',
@@ -34,7 +33,6 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
     )
     Chef::Provider::Package::Yum::YumCache.stub!(:instance).and_return(@yum_cache)
     @provider = Chef::Provider::Package::Yum.new(@new_resource, @run_context)
-    Chef::Resource::Package.stub!(:new).and_return(@current_resource)
     @stderr = mock("STDERR", :null_object => true)
     @pid = mock("PID", :null_object => true)
   end
@@ -42,24 +40,24 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
   describe "when loading the current system state" do
 
     it "should create a current resource with the name of the new_resource" do
-      Chef::Resource::Package.should_receive(:new).and_return(@current_resource)
       @provider.load_current_resource
+      @provider.current_resource.name.should == "cups"
     end
 
     it "should set the current resources package name to the new resources package name" do
-      @current_resource.should_receive(:package_name).with(@new_resource.package_name)
       @provider.load_current_resource
+      @provider.current_resource.package_name.should == "cups"
     end
 
     it "should set the installed version to nil on the current resource if no installed package" do
       @yum_cache.stub!(:installed_version).and_return(nil)
-      @current_resource.should_receive(:version).with(nil).and_return(true)
       @provider.load_current_resource
+      @provider.current_resource.version.should be_nil
     end
 
     it "should set the installed version if yum has one" do
-      @current_resource.should_receive(:version).with("1.2.4-11.18.el5").and_return(true)
       @provider.load_current_resource
+      @provider.current_resource.version.should == "1.2.4-11.18.el5"
     end
 
     it "should set the candidate version if yum info has one" do
@@ -68,11 +66,11 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
     end
 
     it "should return the current resouce" do
-      @provider.load_current_resource.should eql(@current_resource)
+      @provider.load_current_resource.should eql(@provider.current_resource)
     end
   end
 
-  describe Chef::Provider::Package::Yum, "install_package" do
+  describe "when installing a package" do
     it "should run yum install with the package name and version" do
       @provider.should_receive(:run_command_with_systems_locale).with({
         :command => "yum -d0 -e0 -y  install emacs-1.0"
@@ -96,9 +94,17 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
       @provider.install_package("emacs", "21.4-20.el5")
     end
 
+    it "installs the package with the options given in the resource" do
+      @provider.candidate_version = '11'
+      @new_resource.stub!(:options).and_return("--disablerepo epmd")
+      @provider.should_receive(:run_command_with_systems_locale).with({
+        :command => "yum -d0 -e0 -y --disablerepo epmd install cups-11"
+      })
+      @provider.install_package(@new_resource.name, @provider.candidate_version)
+    end
   end
 
-  describe Chef::Provider::Package::Yum, "upgrade_package" do
+  describe "when upgrading a package" do
     it "should run yum update if the package is installed and no version is given" do
       @provider.should_receive(:run_command_with_systems_locale).with({
         :command => "yum -d0 -e0 -y  update cups"
@@ -123,9 +129,8 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
     end
 
     it "should run yum install if the package is not installed" do
+      @current_resource = Chef::Resource::Package.new('cups')
       @provider.candidate_version = '11'
-      @current_resource.stub!(:version).and_return(nil)
-      @current_resource.stub!(:options).and_return(nil)
       @provider.should_receive(:run_command_with_systems_locale).with({
         :command => "yum -d0 -e0 -y  install cups-11"
       })
@@ -133,7 +138,7 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
     end
   end
 
-  describe Chef::Provider::Package::Yum, "remove_package" do
+  describe "when removing a package" do
     it "should run yum remove with the package name" do
       @provider.should_receive(:run_command_with_systems_locale).with({
         :command => "yum -d0 -e0 -y  remove emacs-1.0"
@@ -150,23 +155,12 @@ describe Chef::Provider::Package::Yum, "load_current_resource" do
     end
   end
 
-  describe Chef::Provider::Package::Yum, "purge_package" do
+  describe "when purging a package" do
     it "should run yum remove with the package name" do
       @provider.should_receive(:run_command_with_systems_locale).with({
         :command => "yum -d0 -e0 -y  remove emacs-1.0"
       })
       @provider.purge_package("emacs", "1.0")
-    end
-  end
-
-  describe Chef::Provider::Package::Yum, "use_options" do
-    it "should run yum with options" do
-      @provider.candidate_version = '11'
-      @new_resource.stub!(:options).and_return("--disablerepo epmd")
-      @provider.should_receive(:run_command_with_systems_locale).with({
-        :command => "yum -d0 -e0 -y --disablerepo epmd install cups-11"
-      })
-      @provider.install_package(@new_resource.name, @provider.candidate_version)
     end
   end
 
