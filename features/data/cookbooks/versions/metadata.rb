02ba4636f6b3bac6778bcfc37a453f8469059ab7
Thom May <thom.may@betfair.com>
2010-07-12 16:07:00 +0100

    update metadata for versions cookbooks

--- features/data/cookbooks/versions/metadata.rb
+++ features/data/cookbooks/versions/metadata.rb
@@ -1,4 +1,4 @@
-version "1.0"
+version "0.1.0"
 maintainer "Bobo T. Clown"
 maintainer_email "bobo@example.com"
 long_description "Can haz versions?"
--- features/data/cookbooks_not_uploaded_at_feature_start/versions/metadata.rb
+++ features/data/cookbooks_not_uploaded_at_feature_start/versions/metadata.rb
@@ -1,4 +1,4 @@
-version "2.0"
+version "0.2.0"
 maintainer "Bobo T. Clown"
 maintainer_email "bobo@example.com"
 long_description "Can haz versions?"
