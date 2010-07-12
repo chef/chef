49a5415baaa2aa95c8ae7eb4609e8d8bab85ff16
Thom May <thom.may@betfair.com>
2010-07-12 12:16:21 +0100

    Add two versions of a cookbook called "versions".
    One uploaded at start, one not uploaded

---
+++ features/data/cookbooks/versions/recipes/default.rb
@@ -0,0 +1,5 @@
+file "#{node[:tmpdir]}/thundercats_are_go.txt" do
+  content "1"
+  action :create
+end
+
---
+++ features/data/cookbooks_not_uploaded_at_feature_start/versions/recipes/default.rb
@@ -0,0 +1,5 @@
+file "#{node[:tmpdir]}/thundercats_are_go.txt" do
+  content "2"
+  action :create
+end
+
