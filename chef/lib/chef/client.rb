4e4c8e0c220cf6ff7b4e0696f4569e48839ed2c5
Adam Jacob <adam@opscode.com>
2010-07-19 22:19:27 -0700

    Adding chef version to chef-client/solo output, fixes CHEF-184

--- chef/lib/chef/client.rb
+++ chef/lib/chef/client.rb
@@ -32,6 +32,7 @@ require 'chef/cookbook/cookbook_collection'
 require 'chef/cookbook/file_vendor'
 require 'chef/cookbook/file_system_file_vendor'
 require 'chef/cookbook/remote_file_vendor'
+require 'chef/version'
 require 'ohai'
 
 class Chef
@@ -72,7 +73,7 @@ class Chef
       begin
         run_status = Chef::RunStatus.new(node)
         run_status.start_clock
-        Chef::Log.info("Starting Chef Run (version #{::Chef::VERSION})")
+        Chef::Log.info("Starting Chef Run (Version #{Chef::VERSION})")
         
         if Chef::Config[:solo]
           Chef::Cookbook::FileVendor.on_create { |manifest| Chef::Cookbook::FileSystemFileVendor.new(manifest) }
