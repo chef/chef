eb734ebece8fffce170a5ed01c50e0af32229399
sdelano <stephen@opscode.com>
2010-08-12 11:09:00 -0700

    spec tests for knife environment commands

---
+++ chef/spec/unit/knife/environment_edit_spec.rb
@@ -0,0 +1,69 @@
+#
+# Author:: Stephen Delano (<stephen@ospcode.com>)
+# Copyright:: Copyright (c) 2010 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+
+describe Chef::Knife::EnvironmentEdit do
+  before(:each) do
+    @knife = Chef::Knife::EnvironmentEdit.new
+    @knife.stub!(:msg).and_return true
+    @knife.stub!(:output).and_return true
+    @knife.stub!(:show_usage).and_return true
+    @knife.name_args = [ "production" ]
+
+    @environment = Chef::Environment.new
+    @environment.name("production")
+    @environment.description("Please edit me")
+    @environment.stub!(:save).and_return true
+    Chef::Environment.stub!(:load).and_return @environment
+    @knife.stub(:edit_data).and_return @environment
+  end
+
+  it "should load the environment" do
+    Chef::Environment.should_receive(:load).with("production")
+    @knife.run
+  end
+
+  it "should let you edit the environment" do
+    @knife.should_receive(:edit_data).with(@environment)
+    @knife.run
+  end
+
+  it "should save the environment" do
+    @environment.should_receive(:save)
+    @knife.run
+  end
+
+  it "should not print the environment" do
+    @knife.should_not_receive(:output)
+    @knife.run
+  end
+
+  it "shoud show usage and exit when no environment name is provided" do
+    @knife.name_args = []
+    lambda { @knife.run }.should raise_error(SystemExit)
+  end
+
+  describe "with --print-after" do
+    it "should pretty print the environment, formatted for display" do
+      @knife.config[:print_after] = true
+      @knife.should_receive(:output).with(@environment)
+      @knife.run
+    end
+  end
+end
---
+++ chef/spec/unit/knife/environment_list_spec.rb
@@ -0,0 +1,54 @@
+#
+# Author:: Stephen Delano (<stephen@ospcode.com>)
+# Copyright:: Copyright (c) 2010 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+
+describe Chef::Knife::EnvironmentList do
+  before(:each) do
+    @knife = Chef::Knife::EnvironmentList.new
+    @knife.stub!(:msg).and_return true
+    @knife.stub!(:output).and_return true
+    @knife.stub!(:show_usage).and_return true
+
+    @environments = {
+      "production" => "http://localhost:4000/environments/production",
+      "development" => "http://localhost:4000/environments/development",
+      "testing" => "http://localhost:4000/environments/testing"
+    }
+    Chef::Environment.stub!(:list).and_return @environments
+  end
+
+  it "should make an api call to list the environments" do
+    Chef::Environment.should_receive(:list)
+    @knife.run
+  end
+
+  it "should print the environment names in a sorted list" do
+    names = @environments.keys.sort { |a,b| a <=> b }
+    @knife.should_receive(:output).with(names)
+    @knife.run
+  end
+
+  describe "with --with-uri" do
+    it "should print and unsorted list of the environments and their URIs" do
+      @knife.config[:with_uri] = true
+      @knife.should_receive(:output).with(@environments)
+      @knife.run
+    end
+  end
+end
