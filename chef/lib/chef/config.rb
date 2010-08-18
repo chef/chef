5dab14da24519223aa089f32ea8dc34ee549b47c
sdelano <stephen@opscode.com>
2010-08-17 17:33:28 -0700

    remove "default" environment from Chef::Config

--- chef/lib/chef/config.rb
+++ chef/lib/chef/config.rb
@@ -236,8 +236,5 @@ class Chef
     # valid user and group name
     user_valid_regex [ /^([-a-zA-Z0-9_.]+)$/, /^\d+$/ ]
     group_valid_regex [ /^([-a-zA-Z0-9_.]+)$/, /^\d+$/ ]
-
-    # Environments
-    environment "default"
   end
 end
