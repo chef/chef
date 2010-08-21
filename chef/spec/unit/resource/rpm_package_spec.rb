97d1c9eec4c480db8e18e77b6cbdad87038c92c8
Tommy Bishop <bishop.thomas@gmail.com>
2010-08-20 17:15:10 -0700

    [CHEF-1422] update author/copyright info

--- chef/spec/unit/resource/rpm_package_spec.rb
+++ chef/spec/unit/resource/rpm_package_spec.rb
@@ -1,6 +1,6 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Thomas Bishop (<bishop.thomas@gmail.com>)
+# Copyright:: Copyright (c) 2010 Thomas Bishop
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
