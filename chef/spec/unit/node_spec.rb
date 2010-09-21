6c9c919a0727f9ddb6bb6df3888447cf1f6a139e
Nuo Yan <nuo@opscode.com>
2010-09-21 15:16:21 -0700

    fixing CHEF-1541

--- chef/spec/unit/node_spec.rb
+++ chef/spec/unit/node_spec.rb
@@ -93,7 +93,6 @@ describe Chef::Node do
     it "should not accept name doesn't match /^[\-[:alnum:]_:.]+$/" do
       lambda { @node.name("space in it")}.should raise_error(Chef::Exceptions::ValidationFailed)
     end
-
   end
 
   describe "when modifying Node attributes" do
