638d9e4d83adf47a93a73503dfc6da8cf281f1ba
Daniel DeLeo <dan@opscode.com>
2010-10-08 14:26:40 -0700

    bump version for next 0.9.10 RC version

--- chef-server-webui/lib/chef-server-webui/version.rb
+++ chef-server-webui/lib/chef-server-webui/version.rb
@@ -1,3 +1,3 @@
 module ChefServerWebui
-  VERSION = '0.9.10.rc.1'
+  VERSION = '0.9.10.rc.2'
 end
--- chef-solr/lib/chef/solr/version.rb
+++ chef-solr/lib/chef/solr/version.rb
@@ -1,5 +1,5 @@
 class Chef
   class Solr
-    VERSION = '0.9.10.rc.1'
+    VERSION = '0.9.10.rc.2'
   end
 end
