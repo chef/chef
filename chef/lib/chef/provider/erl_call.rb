eeea77c35085d3f6441cc879675d0c8192a7fcd6
joewilliams <joe@der-dieb.local>
2010-10-20 15:22:04 -0700

    ensure we wait for the pid to die before moving on
    Signed-off-by: Daniel DeLeo <dan@opscode.com>

--- chef/lib/chef/provider/erl_call.rb
+++ chef/lib/chef/provider/erl_call.rb
@@ -54,24 +54,28 @@ class Chef
         end
 
         command = "erl_call -e #{distributed} #{node} #{cookie}"
-        
-        pid, stdin, stdout, stderr = popen4(command, :waitlast => true)
 
-        Chef::Log.debug("Running erl_call[#{@new_resource.name}]")
-        Chef::Log.debug("erl_call[#{@new_resource.name}] command: #{command}")
-        Chef::Log.debug("erl_call[#{@new_resource.name}] code: #{@new_resource.code}")
+        begin
+          pid, stdin, stdout, stderr = popen4(command, :waitlast => true)
 
-        @new_resource.code.each_line { |line| stdin.puts "#{line.chomp!}" }
-        stdin.close
+          Chef::Log.debug("Running erl_call[#{@new_resource.name}]")
+          Chef::Log.debug("erl_call[#{@new_resource.name}] command: #{command}")
+          Chef::Log.debug("erl_call[#{@new_resource.name}] code: #{@new_resource.code}")
 
-        Chef::Log.info("Ran erl_call[#{@new_resource.name}] successfully")
-        Chef::Log.debug("erl_call[#{@new_resource.name}] output: ")
+          @new_resource.code.each_line { |line| stdin.puts "#{line.chomp!}" }
+          stdin.close
 
-        stdout.each_line { |line| Chef::Log.debug("#{line}")}
-        stderr.each_line { |line| Chef::Log.debug("#{line}")}
-        stdout.close
-        stderr.close
-        
+          Chef::Log.info("Ran erl_call[#{@new_resource.name}] successfully")
+          Chef::Log.debug("erl_call[#{@new_resource.name}] output: ")
+
+          stdout.each_line { |line| Chef::Log.debug("#{line}")}
+          stderr.each_line { |line| Chef::Log.debug("#{line}")}
+          stdout.close
+          stderr.close
+        ensure
+          Process.wait(pid) if pid
+        end
+
       end
 
     end
