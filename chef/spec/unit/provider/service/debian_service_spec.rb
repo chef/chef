36ac64f4b965e15523d736b7cff48012d38b53e7
Laurent <laurent+git@u-picardie.fr>
2010-10-27 13:33:03 +0200

    ok, trying not to duplicate !

--- chef/spec/unit/provider/service/debian_service_spec.rb
+++ chef/spec/unit/provider/service/debian_service_spec.rb
@@ -32,6 +32,7 @@ describe Chef::Provider::Service::Debian, "load_current_resource" do
     @provider.current_resource = @current_resource
     
     @pid, @stdin, @stdout, @stderr = nil, nil, nil, nil
+    
   end
   
   it "ensures /usr/sbin/update-rc.d is available" do
@@ -39,133 +40,95 @@ describe Chef::Provider::Service::Debian, "load_current_resource" do
     lambda { @provider.assert_update_rcd_available }.should raise_error(Chef::Exceptions::Service)
   end
 
-  describe "when using squeeze/earlier and update-rc.d shows the init script linked to rc*.d/" do
-    before do
-      @provider.stub!(:run_command)
-      @provider.stub!(:assert_update_rcd_available)
-      @status = mock("Status", :exitstatus => 0)
-
-      @stdout = StringIO.new("update-rc.d: using dependency based boot sequencing")
-      result2=<<-UPDATE_RC_D_SUCCESS
-insserv: remove service /etc/init.d/../rc0.d/K20chef-client
-insserv: remove service /etc/init.d/../rc1.d/K20chef-client
-insserv: remove service /etc/init.d/../rc2.d/S20chef-client
-insserv: remove service /etc/init.d/../rc3.d/S20chef-client
-insserv: remove service /etc/init.d/../rc4.d/S20chef-client
-insserv: remove service /etc/init.d/../rc5.d/S20chef-client
-insserv: remove service /etc/init.d/../rc6.d/K20chef-client
-insserv: dryrun, not creating .depend.boot, .depend.start, and .depend.stop
-UPDATE_RC_D_SUCCESS
-      @stderr = StringIO.new(result2)
-      @provider.stub!(:popen4).and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
-    end
-
-    it "says the service is enabled" do
-      @provider.service_currently_enabled?.should be_true
-    end
-
-    it "stores the 'enabled' state" do
-      Chef::Resource::Service.stub!(:new).and_return(@current_resource)
-      @provider.load_current_resource.should equal(@current_resource)
-      @current_resource.enabled.should be_true
-    end
-
-    it "stores the start/stop priorities of the service" do
-      @provider.load_current_resource
-      expected_priorities = {"6"=>[:stop, "20"],
-                             "0"=>[:stop, "20"],
-                             "1"=>[:stop, "20"],
-                             "2"=>[:start, "20"],
-                             "3"=>[:start, "20"],
-                             "4"=>[:start, "20"],
-                             "5"=>[:start, "20"]}
-      @provider.current_resource.priority.should == expected_priorities
-    end
-  end
-
-  describe "when using squeeze/earlier and update-rc.d shows the init script isn't linked to rc*.d" do
-    before do
-      @provider.stub!(:run_command)
-      @provider.stub!(:assert_update_rcd_available)
-      @status = mock("Status", :exitstatus => 0)
-      @stdout = StringIO.new("update-rc.d: using dependency based boot sequencing")
-      @stderr = StringIO.new("")
-      @provider.stub!(:popen4).and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
-    end
-
-    it "says the service is disabled" do
-      @provider.service_currently_enabled?.should be_false
-    end
-
-    it "stores the 'disabled' state" do
-      Chef::Resource::Service.stub!(:new).and_return(@current_resource)
-      @provider.load_current_resource.should equal(@current_resource)
-      @current_resource.enabled.should be_false
-    end
-  end
-    
-  describe "when using lenny/older and update-rc.d shows the init script linked to rc*.d/" do
-    before do
-      @provider.stub!(:run_command)
-      @provider.stub!(:assert_update_rcd_available)
-      @status = mock("Status", :exitstatus => 0)
-
-      result=<<-UPDATE_RC_D_SUCCESS
- Removing any system startup links for /etc/init.d/chef ...
+  
+  {"Debian/Lenny and older" => {
+      "linked" => {
+        "stdout" => " Removing any system startup links for /etc/init.d/chef ...
    /etc/rc0.d/K20chef
    /etc/rc1.d/K20chef
    /etc/rc2.d/S20chef
    /etc/rc3.d/S20chef
    /etc/rc4.d/S20chef
    /etc/rc5.d/S20chef
-   /etc/rc6.d/K20chef
-UPDATE_RC_D_SUCCESS
-      @stdout = StringIO.new(result)
-      @stderr = StringIO.new("")
-      @provider.stub!(:popen4).and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
+   /etc/rc6.d/K20chef",
+        "stderr" => ""
+      },
+      "not linked" => {
+        "stdout" => " Removing any system startup links for /etc/init.d/chef ...",
+        "stderr" => ""
+      },
+    },
+    "Debian/Squeeze and earlier" => {
+      "linked" => {
+        "stdout" => "update-rc.d: using dependency based boot sequencing",
+        "stderr" => "insserv: remove service /etc/init.d/../rc0.d/K20chef-client
+insserv: remove service /etc/init.d/../rc1.d/K20chef-client
+insserv: remove service /etc/init.d/../rc2.d/S20chef-client
+insserv: remove service /etc/init.d/../rc3.d/S20chef-client
+insserv: remove service /etc/init.d/../rc4.d/S20chef-client
+insserv: remove service /etc/init.d/../rc5.d/S20chef-client
+insserv: remove service /etc/init.d/../rc6.d/K20chef-client
+insserv: dryrun, not creating .depend.boot, .depend.start, and .depend.stop"
+      },
+      "not linked" => {
+        "stdout" => "update-rc.d: using dependency based boot sequencing",
+        "stderr" => ""
+      }
+    }
+  }.each do |model, streams|
+    describe "when update-rc.d shows the init script linked to rc*.d/" do
+      before do
+        @provider.stub!(:run_command)
+        @provider.stub!(:assert_update_rcd_available)
+        @status = mock("Status", :exitstatus => 0)
+
+        @stdout = StringIO.new(streams["linked"]["stdout"])
+        @stderr = StringIO.new(streams["linked"]["stderr"])
+        @provider.stub!(:popen4).and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
+      end
+
+      it "says the service is enabled" do
+        @provider.service_currently_enabled?.should be_true
+      end
+
+      it "stores the 'enabled' state" do
+        Chef::Resource::Service.stub!(:new).and_return(@current_resource)
+        @provider.load_current_resource.should equal(@current_resource)
+        @current_resource.enabled.should be_true
+      end
+
+      it "stores the start/stop priorities of the service" do
+        @provider.load_current_resource
+        expected_priorities = {"6"=>[:stop, "20"],
+          "0"=>[:stop, "20"],
+          "1"=>[:stop, "20"],
+          "2"=>[:start, "20"],
+          "3"=>[:start, "20"],
+          "4"=>[:start, "20"],
+          "5"=>[:start, "20"]}
+        @provider.current_resource.priority.should == expected_priorities
+      end
     end
 
-    it "says the service is enabled" do
-      @provider.service_currently_enabled?.should be_true
-    end
+    describe "when using squeeze/earlier and update-rc.d shows the init script isn't linked to rc*.d" do
+      before do
+        @provider.stub!(:run_command)
+        @provider.stub!(:assert_update_rcd_available)
+        @status = mock("Status", :exitstatus => 0)
+        @stdout = StringIO.new(streams["not linked"]["stdout"])
+        @stderr = StringIO.new(streams["not linked"]["stderr"])
+        @provider.stub!(:popen4).and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
+      end
 
-    it "stores the 'enabled' state" do
-      Chef::Resource::Service.stub!(:new).and_return(@current_resource)
-      @provider.load_current_resource.should equal(@current_resource)
-      @current_resource.enabled.should be_true
-    end
+      it "says the service is disabled" do
+        @provider.service_currently_enabled?.should be_false
+      end
 
-    it "stores the start/stop priorities of the service" do
-      @provider.load_current_resource
-      expected_priorities = {"6"=>[:stop, "20"],
-                             "0"=>[:stop, "20"],
-                             "1"=>[:stop, "20"],
-                             "2"=>[:start, "20"],
-                             "3"=>[:start, "20"],
-                             "4"=>[:start, "20"],
-                             "5"=>[:start, "20"]}
-      @provider.current_resource.priority.should == expected_priorities
-    end
-  end
-
-  describe "when using lenny/older and update-rc.d shows the init script isn't linked to rc*.d" do
-    before do
-      @provider.stub!(:run_command)
-      @provider.stub!(:assert_update_rcd_available)
-      @status = mock("Status", :exitstatus => 0)
-      @stdout = StringIO.new(" Removing any system startup links for /etc/init.d/chef ...")
-      @stderr = StringIO.new("")
-      @provider.stub!(:popen4).and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
-    end
-    
-    it "says the service is disabled" do
-      @provider.service_currently_enabled?.should be_false
-    end
-    
-    it "stores the 'disabled' state" do
-      Chef::Resource::Service.stub!(:new).and_return(@current_resource)
-      @provider.load_current_resource.should equal(@current_resource)
-      @current_resource.enabled.should be_false
+      it "stores the 'disabled' state" do
+        Chef::Resource::Service.stub!(:new).and_return(@current_resource)
+        @provider.load_current_resource.should equal(@current_resource)
+        @current_resource.enabled.should be_false
+      end
     end
   end
 
