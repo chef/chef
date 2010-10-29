9f582d3851a6c76669424e947d38a4bd6df7c0b4
Tim Hinderliter <tim@opscode.com>
2010-10-29 15:11:07 -0700

    Fix CHEF-1835 -- child chef-chef now spawned using fork/exec instead of popen4, so we don't use grep shenanigans to determine its pid.

--- features/data/cookbooks/run_interval/recipes/default.rb
+++ features/data/cookbooks/run_interval/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: run_interval
 # Recipe:: default
 #
-# Copyright 2009, Opscode
+# Copyright 2009, 2010, Opscode
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Force chef-client to exit once this cookbook has been applied twice.
+# The test depends on chef having run twice, so this number is tied to
+# run_interval.feature!
+
+$run_interval_global ||= 2
+
+$run_interval_global -= 1
+exit(2) if $run_interval_global == 0
