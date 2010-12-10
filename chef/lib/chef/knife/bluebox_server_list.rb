42f166d0855cd2422275b6f45f17614727f2f75a
Jesse Proudman <jesse.proudman@blueboxgrp.com>
2010-12-09 17:17:47 -0800

    Revert "Update the fog commands to use Wesley's refactored object space."
    This reverts commit e9b9ca1e2a3f07f65b0e657eb56342d360379ec0.

--- chef/lib/chef/knife/bluebox_server_list.rb
+++ chef/lib/chef/knife/bluebox_server_list.rb
@@ -35,8 +35,8 @@ class Chef
         require 'net/ssh/multi'
         require 'readline'
 
-        bluebox = Fog::Bluebox::Compute.new(
-  	      :bluebox_customer_id => Chef::Config[:knife][:bluebox_customer_id],
+        bluebox = Fog::Bluebox.new(
+  	  :bluebox_customer_id => Chef::Config[:knife][:bluebox_customer_id],
           :bluebox_api_key => Chef::Config[:knife][:bluebox_api_key]
         )
 
