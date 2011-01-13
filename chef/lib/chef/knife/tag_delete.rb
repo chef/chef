12324ec6d1287dc0516722d778cfd4a9396f5b10
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-12 17:03:53 -0800

    Added `knife tag delete`

---
+++ chef/lib/chef/knife/tag_delete.rb
@@ -0,0 +1,28 @@
+require 'chef/knife'
+require 'chef/node'
+
+class Chef
+  class Knife
+    class TagDelete < Knife
+
+      banner "knife tag delete NODE TAG ..."
+
+      def run
+        name = @name_args[0]
+        tags = @name_args[1..-1].join(",").split(/\s*,\s*/)
+
+        unless name or tags.empty?
+          show_usage
+          # TODO: blah blah
+          Chef::Log.fatal("You must specify a name name")
+          exit 1
+        end
+
+        node = Chef::Node.load name
+        tags.each do |tag|
+          node.tags.delete tag
+        end
+      end
+    end
+  end
+end
