3ccff3d229c2d781c0aa4c36529936a1627ef9de
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-12 16:58:58 -0800

    Added `knife tag create` and spec

---
+++ chef/lib/chef/knife/tag_create.rb
@@ -0,0 +1,28 @@
+require 'chef/knife'
+require 'chef/node'
+
+class Chef
+  class Knife
+    class TagCreate < Knife
+
+      banner "knife tag create NODE TAG"
+
+      def run
+        name = @name_args[0]
+        tags = @name_args[1..-1].join(",").split(/\s*,\s*/)
+
+        unless name or tags.empty?
+          show_usage
+          # TODO: blah blah
+          Chef::Log.fatal("You must specify a name name")
+          exit 1
+        end
+
+        node = Chef::Node.load name
+        tags.each do |tag|
+          node.tags << tag
+        end
+      end
+    end
+  end
+end
