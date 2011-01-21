f950072a22da3b1059546a1010f68aefaf916d3a
Daniel DeLeo <dan@opscode.com>
2011-01-21 15:38:11 -0800

    fix block binding/precedence error. whitespace purge

--- chef-server-api/app/controllers/cookbooks.rb
+++ chef-server-api/app/controllers/cookbooks.rb
@@ -9,9 +9,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,14 +23,14 @@ require 'chef/cookbook_loader'
 require 'chef/cookbook/metadata'
 
 class Cookbooks < Application
-  
+
   provides :json
 
   before :authenticate_every
   before :params_helper
-  
+
   attr_accessor :cookbook_name, :cookbook_version
-  
+
   def params_helper
     self.cookbook_name = params[:cookbook_name]
     self.cookbook_version = params[:cookbook_version]
@@ -38,7 +38,7 @@ class Cookbooks < Application
 
   include Chef::Mixin::Checksum
   include Merb::TarballHelper
-  
+
   def index
     cookbook_list = Chef::CookbookVersion.cdb_list_latest.keys.sort
     response = Hash.new
@@ -58,11 +58,12 @@ class Cookbooks < Application
   end
 
   def index_recipes
-    display Chef::CookbookVersion.cdb_list(true).inject({}) do |memo, f| 
+    recipes_with_versions = Chef::CookbookVersion.cdb_list(true).inject({}) do|memo, f|
       memo[f.name] ||= {}
       memo[f.name][f.version] = f.recipe_filenames_by_name.keys
       memo
     end
+    display recipes_with_versions
   end
 
   def show_versions
@@ -78,7 +79,7 @@ class Cookbooks < Application
 
   def show_file
     cookbook = get_cookbook_version(cookbook_name, cookbook_version)
-    
+
     checksum = params[:checksum]
     raise NotFound, "Cookbook #{cookbook_name} version #{cookbook_version} does not contain a file with checksum #{checksum}" unless cookbook.checksums.keys.include?(checksum)
 
@@ -96,9 +97,9 @@ class Cookbooks < Application
     end
 
     unless params["inflated_object"].version == cookbook_version
-      raise(BadRequest, "You said the cookbook was version #{params['inflated_object'].version}, but the URL says it should be #{cookbook_version}.") 
+      raise(BadRequest, "You said the cookbook was version #{params['inflated_object'].version}, but the URL says it should be #{cookbook_version}.")
     end
-    
+
     begin
       cookbook = Chef::CookbookVersion.cdb_load(cookbook_name, cookbook_version)
       cookbook.manifest = params['inflated_object'].manifest
@@ -106,29 +107,29 @@ class Cookbooks < Application
       Chef::Log.debug("Cookbook #{cookbook_name} version #{cookbook_version} does not exist")
       cookbook = params['inflated_object']
     end
-    
+
     # ensure that all checksums referred to by the manifest have been uploaded.
     Chef::CookbookVersion::COOKBOOK_SEGMENTS.each do |segment|
       next unless cookbook.manifest[segment]
       cookbook.manifest[segment].each do |manifest_record|
         checksum = manifest_record[:checksum]
         path = manifest_record[:path]
-        
+
         begin
           checksum_obj = Chef::Checksum.cdb_load(checksum)
         rescue Chef::Exceptions::CouchDBNotFound => cdbx
           checksum_obj = nil
         end
-        
+
         raise BadRequest, "Manifest has checksum #{checksum} (path #{path}) but it hasn't yet been uploaded" unless checksum_obj
       end
     end
-    
+
     raise InternalServerError, "Error saving cookbook" unless cookbook.cdb_save
 
     display cookbook
   end
-  
+
   def destroy
     begin
       cookbook = get_cookbook_version(cookbook_name, cookbook_version)
@@ -156,6 +157,6 @@ class Cookbooks < Application
       raise
     end
   end
-  
+
 end
 
