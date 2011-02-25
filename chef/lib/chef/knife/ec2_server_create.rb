32354129777404372e7eb9a6073687d3502d34da
Daniel DeLeo <dan@opscode.com>
2011-02-25 11:43:45 -0800

    use the correct config setting for availability zone

--- chef/lib/chef/knife/ec2_server_create.rb
+++ chef/lib/chef/knife/ec2_server_create.rb
@@ -155,7 +155,7 @@ class Chef
           :groups => config[:security_groups],
           :flavor_id => config[:flavor],
           :key_name => Chef::Config[:knife][:aws_ssh_key_id],
-          :availability_zone => Chef::Config[:availability_zone]
+          :availability_zone => Chef::Config[:knife][:availability_zone]
         )
 
         puts "#{h.color("Instance ID", :cyan)}: #{server.id}"
