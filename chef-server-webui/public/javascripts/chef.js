2b9b1a7fa2463d3f216a991d245ee28a6ba98f1d
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-03-15 17:17:50 +0300

    [CHEF-1956] Upgrade jQuery from 1.3.2 to 1.5.1. Replace livequery plugin with delegate.

--- chef-server-webui/public/javascripts/chef.js
+++ chef-server-webui/public/javascripts/chef.js
@@ -116,22 +116,21 @@ $(document).ready(function(){
   });
 
   // livequery hidden form for link_to ajax magic
-  $('a[method]').livequery(function(){
-    var message = $(this).attr('confirm');
-    var method  = $(this).attr('method');
+  $(document.body).delegate('a[method]', 'click', function(e){
+    var $this = $(this);
+    var message = $this.attr('confirm'), method  = $this.attr('method');
 
     if (!method && !message) return;
 
-    $(this).click(function(event){
-      if (message && !confirm(message)) {
-        event.preventDefault();
-        return;
-      }
+    if (message && !confirm(message)) {
+      event.preventDefault();
+      return;
+    }
 
-      if (method === 'post' || method === 'put' || method === 'delete') {
-        event.preventDefault();
-        var form = $("<form/>").attr('method', 'post').attr('action', this.href).attr('style', 'display: none');
-        if (method !== "post") {
+    if (method === 'post' || method === 'put' || method === 'delete') {
+      event.preventDefault();
+      var form = $("<form/>").attr('method', 'post').attr('action', this.href).attr('style', 'display: none');
+      if (method !== "post") {
           form.append($('<input type="hidden" name="_method"/>').attr('value', method));
         }
         form.insertAfter(this).submit();
