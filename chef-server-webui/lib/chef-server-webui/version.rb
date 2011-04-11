4586e44fb3f2e02496391e3cb282c46921e75f4e
Daniel DeLeo <dan@opscode.com>
2011-04-10 20:56:50 -0700

    bump version to beta 9 for release

--- chef-server-webui/lib/chef-server-webui/version.rb
+++ chef-server-webui/lib/chef-server-webui/version.rb
@@ -1,3 +1,3 @@
 module ChefServerWebui
-  VERSION = '0.10.0.beta.8'
+  VERSION = '0.10.0.beta.9'
 end
--- chef-server/lib/chef-server/version.rb
+++ chef-server/lib/chef-server/version.rb
@@ -17,5 +17,5 @@
 #
 
 module ChefServer
-  VERSION = '0.10.0.beta.8'
+  VERSION = '0.10.0.beta.9'
 end
