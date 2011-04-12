675bac8b23393b716aef475c0c27470dc0c55c0e
Daniel DeLeo <dan@opscode.com>
2011-04-12 11:19:44 -0700

    [CHEF-2217] don't colorize on windows

--- chef/lib/chef/knife/core/ui.rb
+++ chef/lib/chef/knife/core/ui.rb
@@ -93,7 +93,7 @@ class Chef
       # determined by the value of `config[:color]`. When output is not to a
       # terminal, colored output is never used
       def color?
-        Chef::Config[:color] && stdout.tty?
+        Chef::Config[:color] && stdout.tty? && (RUBY_PLATFORM !~ /mswin|mingw32|windows/)
       end
 
       def ask(*args, &block)
