a5d3f25dc5902dd169e1d4db2e881eaaf65d2ad5
Bryan McLellan <btm@opscode.com>
2011-05-20 16:02:33 -0700

    remove double space in zypper command line for versions before 1.0

--- chef/lib/chef/provider/package/zypper.rb
+++ chef/lib/chef/provider/package/zypper.rb
@@ -93,7 +93,7 @@ class Chef
         def install_package(name, version)
           if zypper_version < 1.0
             run_command(
-              :command => "zypper install -y  #{name}"
+              :command => "zypper install -y #{name}"
             )
           elsif version
             run_command(
@@ -109,7 +109,7 @@ class Chef
         def upgrade_package(name, version)
           if zypper_version < 1.0
             run_command(
-              :command => "zypper install -y  #{name}"
+              :command => "zypper install -y #{name}"
             )
           elsif version
             run_command(
@@ -125,7 +125,7 @@ class Chef
         def remove_package(name, version)
           if zypper_version < 1.0
             run_command(
-              :command => "zypper remove -y  #{name}"
+              :command => "zypper remove -y #{name}"
             )
           elsif version
             run_command(
