fbbbef777a4cdc470b39b0f38a4a8b27eba3ccef
Bryan McLellan <btm@opscode.com>
2011-07-28 16:19:52 -0700

    Bump version for 0.10.4.rc.3

--- chef-server-webui/lib/chef-server-webui/version.rb
+++ chef-server-webui/lib/chef-server-webui/version.rb
@@ -1,3 +1,3 @@
 module ChefServerWebui
-  VERSION = '0.10.4.rc.2'
+  VERSION = '0.10.4.rc.3'
 end
--- chef-server/lib/chef-server/version.rb
+++ chef-server/lib/chef-server/version.rb
@@ -17,5 +17,5 @@
 #
 
 module ChefServer
-  VERSION = '0.10.4.rc.2'
+  VERSION = '0.10.4.rc.3'
 end
