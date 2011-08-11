61092a4daa6a088fb8ea9553752a1bd50bd1bc39
Bryan McLellan <btm@opscode.com>
2011-08-11 16:06:01 -0700

    Bump version for development

--- chef-expander/lib/chef/expander/version.rb
+++ chef-expander/lib/chef/expander/version.rb
@@ -23,7 +23,7 @@ require 'open3'
 module Chef
   module Expander
 
-    VERSION = "0.10.4"
+    VERSION = "0.10.5"
 
     def self.version
       @rev ||= begin
--- chef-server-webui/lib/chef-server-webui/version.rb
+++ chef-server-webui/lib/chef-server-webui/version.rb
@@ -1,3 +1,3 @@
 module ChefServerWebui
-  VERSION = '0.10.4'
+  VERSION = '0.10.5'
 end
