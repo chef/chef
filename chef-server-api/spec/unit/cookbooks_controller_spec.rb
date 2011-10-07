986d7c4f3426c31e4c126020cf1649ad0d9c17bf
Andrea Campi <andrea.campi@zephirworks.com>
2011-10-07 20:13:52 +0200

    CHEF-2674: while here, fix a typo in the name of a "describe" block.

--- chef-server-api/spec/unit/cookbooks_controller_spec.rb
+++ chef-server-api/spec/unit/cookbooks_controller_spec.rb
@@ -21,7 +21,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_model_helper')
 require 'chef/node'
 require 'pp'
 
-describe "Coobkooks Controller" do
+describe "Cookbooks Controller" do
   before do
     Merb.logger.set_log(StringIO.new)
   end
