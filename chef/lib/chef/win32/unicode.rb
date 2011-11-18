601ea4d57955bf5320d12eb5d7d76b30c26cb20f
Seth Chisamore <schisamo@opscode.com>
2011-11-17 22:43:46 -0500

    unicode conversion level-up

--- chef/lib/chef/win32/unicode.rb
+++ chef/lib/chef/win32/unicode.rb
@@ -1,5 +1,6 @@
 #
 # Author:: John Keiser (<jkeiser@opscode.com>)
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
 # Copyright:: Copyright 2011 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
@@ -16,16 +17,106 @@
 # limitations under the License.
 #
 
+require 'chef/win32/api/unicode'
+
+class Chef
+  module Win32
+    class Unicode
+
+      # Inspired by Windows::Unicode from 'windows-pr'
+      # (C) 2006-2010, Daniel J. Berger
+      # All Rights Reserved
+
+      class << self
+        include Chef::Win32::API::Unicode
+
+        # Maps a character string to a UTF-16 (wide) character string using the
+        # specified +encoding+.  If no encoding is specified, then CP_UTF8
+        # is used if $KCODE (or the encoding name in Ruby 1.9.x) is set to UTF8.
+        # Otherwise, CP_ACP is used.
+        #
+        def multi_to_wide(string, code_page=nil)
+          return nil unless string
+          raise TypeError unless string.is_a?(String)
+          return string if IsTextUnicode(string, string.size, nil)
+          code_page ||= default_code_page
+
+          in_string = FFI::MemoryPointer.from_string(string)
+          num_chars = MultiByteToWideChar(code_page, 0, in_string, -1, nil, 0)
+
+          if num_chars > 0
+            out_bytes = FFI::MemoryPointer.new num_chars*2
+            MultiByteToWideChar(code_page, 0, in_string, -1, out_bytes, num_chars)
+            out_bytes.get_bytes(0, num_chars*2)
+          else
+            Chef::Win32::Error.raise!
+          end
+        end
+
+        # Maps a wide character string to a new character string using the
+        # specified +encoding+. If no encoding is specified, then CP_UTF8
+        # is used if $KCODE (or the encoding name in Ruby 1.9.x) is set to UTF8.
+        # Otherwise, CP_ACP is used.
+        #
+        def wide_to_multi(wstring, code_page=nil)
+          return nil unless wstring
+          raise TypeError unless wstring.is_a?(String)
+          # UTF-8 is desired just convert internally from UTF-16LE
+          return utf16_to_utf8(wstring) if !code_page && utf8?
+          code_page ||= default_code_page
+          in_string = FFI::MemoryPointer.from_string(wstring)
+          num_bytes = WideCharToMultiByte(code_page, 0, in_string, -1, nil, 0, nil, nil)
+
+          if num_bytes > 0
+            out_string = FFI::MemoryPointer.new num_bytes
+            WideCharToMultiByte(code_page, 0, in_string, -1, out_string, num_bytes, nil, nil)
+            out_string.read_string
+          else
+            Chef::Win32::Error.raise!
+          end
+        end
+
+        private
+        def default_code_page
+          utf8? ? CP_UTF8 : CP_ACP
+        end
+
+        def default_ruby_encoding
+          if "".respond_to?(:force_encoding) && defined?(Encoding)
+            Encoding.default_external.to_s
+          else
+            $KCODE
+          end
+        end
+
+        def utf8?
+          %w{UTF-8 UTF8}.include?(default_ruby_encoding)
+        end
+
+        def utf16_to_utf8(string)
+          if RUBY_VERSION.to_f >= 1.9
+            string.force_encoding("UTF-16LE").encode("UTF-8")
+          else
+            require 'iconv'
+            Iconv.conv("UTF-8", "UTF-16LE", string)
+          end
+        end
+      end
+
+    end
+  end
+end
+
 module FFI
   class Pointer
     def read_wstring(num_wchars)
-      self.get_bytes(0, num_wchars*2).force_encoding("UTF-16LE").encode("UTF-8")
+      Chef::Win32::Unicode.wide_to_multi(self.get_bytes(0, num_wchars*2))
     end
   end
 end
 
 class String
   def to_wstring
-    self.encode("UTF-16LE")
+    Chef::Win32::Unicode.multi_to_wide(self)
   end
 end
