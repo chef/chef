635e8249fe391f1618a1d90c152f38de5c94593a
Seth Chisamore <schisamo@opscode.com>
2011-11-21 18:37:05 -0500

    initial version of WindowsDirectory resource/provider

---
+++ chef/lib/chef/provider/windows_directory.rb
@@ -0,0 +1,68 @@
+#
+# Author:: Adam Jacob (<adam@opscode.com>)
+# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/provider'
+require 'chef/provider/directory'
+require 'chef/windows_file_access_control'
+
+class Chef
+  class Provider
+    class WindowsDirectory < Chef::Provider::Directory
+      def load_current_resource
+        @current_resource = Chef::Resource::WindowsDirectory.new(@new_resource.name)
+        @current_resource.path(@new_resource.path)
+
+        # TODO ADD NEW Win32 SECURITY HOOKS HERE
+        # if ::File.exist?(@current_resource.path) && ::File.directory?(@current_resource.path)
+        #   cstats = ::File.stat(@current_resource.path)
+        #   @current_resource.owner(cstats.uid)
+        #   @current_resource.group(cstats.gid)
+        #   @current_resource.mode("%o" % (cstats.mode & 007777))
+        # end
+        @current_resource
+      end
+
+      def set_owner
+        # TODO REMOVE THIS STUB
+      end
+
+      def set_group
+        # TODO REMOVE THIS STUB
+      end
+
+      def set_mode
+        # TODO REMOVE THIS STUB
+      end
+
+      def set_all_access_controls(directory)
+        access_controls = Chef::WindowsFileAccessControl.new(@new_resource, directory)
+        access_controls.set_all
+        @new_resource.updated_by_last_action(access_controls.modified?) unless @new_resource.updated_by_last_action?
+      end
+
+      def action_create
+        super
+        set_all_access_controls(@new_resource.path)
+      end
+
+      def action_delete
+        super
+      end
+    end
+  end
+end
---
+++ chef/lib/chef/resource/windows_directory.rb
@@ -0,0 +1,41 @@
+#
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/resource/directory'
+require 'chef/provider/windows_directory'
+require 'chef/mixin/windows_securable'
+
+class Chef
+  class Resource
+    class WindowsDirectory < Chef::Resource::Directory
+      include Chef::Mixin::WindowsSecurable
+
+      provides :directory, :on_platforms => ["windows"]
+
+      def initialize(name, run_context=nil)
+        super
+        @resource_name = :windows_directory
+        @path = name
+        @action = :create
+        @inherits = false
+        @provider = Chef::Provider::WindowsDirectory
+      end
+
+    end
+  end
+end
