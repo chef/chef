2e62baee7cfe0a7a75755abc989bb4004dbae1f8
Seth Chisamore <schisamo@opscode.com>
2011-11-21 16:35:26 -0500

    refactor WindowsFileAccessControl to more closely match FileAccessControl, ensure it works outside the happy path

--- chef/lib/chef/windows_file_access_control.rb
+++ chef/lib/chef/windows_file_access_control.rb
@@ -23,54 +23,65 @@ class Chef
   class WindowsFileAccessControl
     include Chef::Win32::API::Security
 
-    def self.apply_security_policy(resource, securable_object)
-      provider = WindowsFileAccessControl.new(resource, securable_object)
-      provider.apply_security_policy()
-    end
-
-    def apply_security_policy
-      modified = false
-
-      existing = securable_object.security_descriptor
-      # Apply owner and group
-      if existing.owner != target_owner
-        Chef::Log.info("Changing owner from #{existing.owner.account_name} to #{target_owner.account_name}")
-        securable_object.owner = target_owner
-        modified = true
-      end
-      if existing.group != target_group
-        Chef::Log.info("Changing group from #{existing.group.account_name} to #{target_group.account_name}")
-        securable_object.group = target_group
-        modified = true
-      end
-
-      # Apply DACL and inherits
-      target_dacl = build_target_dacl
-      if existing.dacl_inherits? != target_inherits
-        Chef::Log.info("Changing DACL and inherits")
-        securable_object.set_dacl(target_dacl, target_inherits)
-        modified = true
-      elsif !acls_equal(target_dacl, existing.dacl)
-        Chef::Log.info("Changing DACL")
-        securable_object.dacl = target_dacl
-        modified = true
-      end
-
-      modified
-    end
-
-    private
-
     def initialize(resource, securable_object)
       @resource = resource
+      if securable_object.kind_of?(String)
+        securable_object = Chef::Win32::Security::SecurableObject.new(securable_object.dup)
+      end
       @securable_object = securable_object
+      @existing_descriptor = securable_object.security_descriptor
+      Chef::Log.warn("Existing security descriptor for [#{securable_object}] apears to be nil") unless @existing_descriptor
+      @modified = false
     end
-    
+
     attr_reader :resource
     attr_reader :securable_object
+    attr_reader :existing_descriptor
 
     Security = Chef::Win32::Security
     ACE = Security::ACE
+
+    def modified?
+      @modified
+    end
+
+    def set_all
+      set_owner
+      set_group
+      set_rights
+    end
+
+    def set_owner
+      # Apply owner and group
+      if (owner = target_owner) && (owner != existing_descriptor.owner)
+        Chef::Log.info("Changing owner from #{existing_descriptor.owner.account_name} to #{target_owner.account_name}")
+        securable_object.owner = target_owner
+        modified
+      end
+    end
+
+    def set_group
+      if (group = target_group) && (group != existing_descriptor.group)
+        Chef::Log.info("Changing group from #{existing_descriptor.group.account_name} to #{target_group.account_name}")
+        securable_object.group = target_group
+        modified
+      end
+    end
+
+    def set_rights
+      # Apply DACL and inherits
+      if (target_dacl = build_target_dacl)
+        if existing_descriptor.dacl_inherits? != target_inherits
+          Chef::Log.info("Changing DACL and inherits")
+          securable_object.set_dacl(target_dacl, target_inherits)
+          modified
+        elsif !acls_equal(target_dacl, existing_descriptor.dacl)
+          Chef::Log.info("Changing DACL")
+          securable_object.dacl = target_dacl
+          modified
+        end
+      end
+    end
 
     def acls_equal(target_acl, actual_acl)
       actual_acl = actual_acl.select { |ace| !ace.inherited? }
@@ -104,22 +115,25 @@ class Chef
     end
 
     def build_target_dacl
-      acls = []
-      resource.rights.each_pair do |type, users|
-        case type
-        when :deny
-          users.each { |user| acls.push ACE.access_denied(get_sid(user), GENERIC_ALL) }
-        when :read
-          users.each { |user| acls.push(ACE.access_allowed(get_sid(user), GENERIC_READ | GENERIC_EXECUTE)) }
-        when :write
-          users.each { |user| acls.push(ACE.access_allowed(get_sid(user), GENERIC_WRITE | GENERIC_READ | GENERIC_EXECUTE)) }
-        when :full
-          users.each { |user| acls.push(ACE.access_allowed(get_sid(user), GENERIC_ALL)) }
-        else
-          raise "Unknown rights type #{type}"
+      unless resource.rights.nil?
+        acls = []
+        resource.rights.each_pair do |type, users|
+          users = [users] unless users.kind_of? Array
+          case type
+          when :deny
+            users.each { |user| acls.push ACE.access_denied(get_sid(user), GENERIC_ALL) }
+          when :read
+            users.each { |user| acls.push(ACE.access_allowed(get_sid(user), GENERIC_READ | GENERIC_EXECUTE)) }
+          when :write
+            users.each { |user| acls.push(ACE.access_allowed(get_sid(user), GENERIC_WRITE | GENERIC_READ | GENERIC_EXECUTE)) }
+          when :full
+            users.each { |user| acls.push(ACE.access_allowed(get_sid(user), GENERIC_ALL)) }
+          else
+            raise "Unknown rights type #{type}"
+          end
         end
+        Chef::Win32::Security::ACL.create(acls)
       end
-      Chef::Win32::Security::ACL.create(acls)
     end
 
     def get_sid(value)
@@ -130,6 +144,12 @@ class Chef
       else
         raise "Must specify username, group or SID: #{value}"
       end
+    end
+
+    private
+
+    def modified
+      @modified = true
     end
   end
 end
