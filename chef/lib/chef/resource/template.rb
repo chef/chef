81225dac37a91a8c78b8572f4d20faa5cccfc0be
Seth Chisamore <schisamo@opscode.com>
2011-11-22 12:06:36 -0500

    Template resource/provider now supports cross-platform security

--- chef/lib/chef/resource/template.rb
+++ chef/lib/chef/resource/template.rb
@@ -1,14 +1,15 @@
 #
 # Author:: Adam Jacob (<adam@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Copyright:: Copyright (c) 2008, 2011 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,11 +18,16 @@
 #
 
 require 'chef/resource/file'
+require 'chef/provider/template'
+require 'chef/mixin/securable'
 
 class Chef
   class Resource
     class Template < Chef::Resource::File
-      
+      include Chef::Mixin::Securable
+
+      provides :template, :on_platforms => :all
+
       def initialize(name, run_context=nil)
         super
         @resource_name = :template
@@ -30,6 +36,7 @@ class Chef
         @cookbook = nil
         @local = false
         @variables = Hash.new
+        @provider = Chef::Provider::Template
       end
 
       def source(file=nil)
@@ -47,7 +54,7 @@ class Chef
           :kind_of => [ Hash ]
         )
       end
-      
+
       def cookbook(args=nil)
         set_or_return(
           :cookbook,
