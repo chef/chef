a9b7a4e4a68336e3544514eb4c5423e9da227000
Seth Chisamore <schisamo@opscode.com>
2011-11-22 09:32:40 -0500

    fix copyright/author data

--- chef/lib/chef/provider/windows_directory.rb
+++ chef/lib/chef/provider/windows_directory.rb
@@ -1,6 +1,6 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Copyright:: Copyright (c) 2011 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
