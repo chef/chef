5840552d5731228058feacd745ee286c4dee60a4
Mark Mzyk <mmzyk@opscode.com>
2011-11-23 15:35:58 -0500

    Updated file_spec to pass with new permissions work

--- chef/spec/unit/provider/file_spec.rb
+++ chef/spec/unit/provider/file_spec.rb
@@ -49,9 +49,6 @@ describe Chef::Provider::File do
     @provider.current_resource.name.should eql(@resource.name)
     @provider.current_resource.path.should eql(@resource.path)
     @provider.current_resource.content.should eql(nil)
-    @provider.current_resource.owner.should_not eql(nil)
-    @provider.current_resource.group.should_not eql(nil)
-    @provider.current_resource.mode.should_not eql(nil)
   end
 
   it "should load a mostly blank current resource if the file specified in new_resource doesn't exist/isn't readable" do
@@ -64,9 +61,6 @@ describe Chef::Provider::File do
     provider.current_resource.should be_a_kind_of(Chef::Resource::File)
     provider.current_resource.name.should eql(resource.name)
     provider.current_resource.path.should eql(resource.path)
-    provider.current_resource.owner.should eql(nil)
-    provider.current_resource.group.should eql(nil)
-    provider.current_resource.mode.should eql(nil)
   end
 
   it "should not backup symbolic links on delete" do
@@ -83,12 +77,6 @@ describe Chef::Provider::File do
     ::File.stub!(:symlink?).and_return(true)
     @provider.should_not_receive(:backup)
     @provider.action_delete
-  end
-
-  it "should load the correct value for owner of the current resource" do
-    stats = File.stat(@resource.path)
-    @provider.load_current_resource
-    @provider.current_resource.owner.should eql(stats.uid)
   end
 
   it "should compare the current content with the requested content" do
@@ -119,117 +107,13 @@ describe Chef::Provider::File do
     lambda { @provider.set_content }.should_not raise_error
   end
 
-  it "should compare the current owner with the requested owner" do
-    @provider.load_current_resource
-    @provider.new_resource.stub!(:owner).and_return("adam")
-    Etc.stub!(:getpwnam).and_return(
-      OpenStruct.new(
-        :name => "adam",
-        :passwd => "foo",
-        :uid => 501,
-        :gid => 501,
-        :gecos => "Adam Jacob",
-        :dir => "/Users/adam",
-        :shell => "/bin/zsh",
-        :change => "0",
-        :uclass => "",
-        :expire => 0
-      )
-    )
-    @provider.current_resource.owner(501)
-    @provider.compare_owner.should eql(true)
-
-    @provider.current_resource.owner(777)
-    @provider.compare_owner.should eql(false)
-
-    @provider.new_resource.stub!(:owner).and_return(501)
-    @provider.current_resource.owner(501)
-    @provider.compare_owner.should eql(true)
-
-    @provider.new_resource.stub!(:owner).and_return("501")
-    @provider.current_resource.owner(501)
-    @provider.compare_owner.should eql(true)
-  end
-
-  it "should set the ownership on the file to the requested owner" do
-    @provider.load_current_resource
-    @provider.new_resource.stub!(:owner).and_return(9982398)
-    File.stub!(:chown).and_return(1)
-    File.should_receive(:chown).with(9982398, nil, @provider.current_resource.path)
-    lambda { @provider.set_owner }.should_not raise_error
-  end
-
-  it "should raise an exception if you are not root and try to change ownership" do
-    @provider.load_current_resource
-    @provider.new_resource.stub!(:owner).and_return(0)
-    if Process.uid != 0
-      lambda { @provider.set_owner }.should raise_error
-    end
-  end
-
-  it "should compare the current group with the requested group" do
-    @provider.load_current_resource
-    @provider.new_resource.stub!(:group).and_return("adam")
-    Etc.stub!(:getgrnam).and_return(
-      OpenStruct.new(
-        :name => "adam",
-        :gid => 501
-      )
-    )
-    @provider.current_resource.group(501)
-    @provider.compare_group.should eql(true)
-
-    @provider.current_resource.group(777)
-    @provider.compare_group.should eql(false)
-
-    @provider.new_resource.stub!(:group).and_return(501)
-    @provider.current_resource.group(501)
-    @provider.compare_group.should eql(true)
-
-    @provider.new_resource.stub!(:group).and_return("501")
-    @provider.current_resource.group(501)
-    @provider.compare_group.should eql(true)
-  end
-
-  it "should set the group on the file to the requested group" do
-    @provider.load_current_resource
-    @provider.new_resource.stub!(:group).and_return(9982398)
-    File.stub!(:chown).and_return(1)
-    File.should_receive(:chown).with(nil, 9982398, @provider.current_resource.path)
-    lambda { @provider.set_group }.should_not raise_error
-  end
-
-  it "should create the file if it is missing, then set the attributes on action_create" do
-    @provider.load_current_resource
-    @provider.new_resource.stub!(:owner).and_return(9982398)
-    @provider.new_resource.stub!(:group).and_return(9982398)
-    @provider.new_resource.stub!(:mode).and_return(0755)
-    @provider.new_resource.stub!(:path).and_return("/tmp/monkeyfoo")
-    File.stub!(:chown).and_return(1)
-    File.should_receive(:chown).with(nil, 9982398, @provider.new_resource.path)
-    File.stub!(:chown).and_return(1)
-    File.should_receive(:chown).with(9982398, nil, @provider.new_resource.path)
-    File.stub!(:open).and_return(1)
-    File.should_receive(:chmod).with(0755, @provider.new_resource.path).and_return(1)
-    File.should_receive(:open).with(@provider.new_resource.path, "w+")
-    @provider.action_create
-  end
-
   it "should create the file with the proper content if it is missing, then set attributes on action_create" do
     io = StringIO.new
     @provider.load_current_resource
     @provider.new_resource.content "foobar"
-    @provider.new_resource.stub!(:owner).and_return(9982398)
-    @provider.new_resource.stub!(:group).and_return(9982398)
-    @provider.new_resource.stub!(:mode).and_return(0755)
     @provider.new_resource.stub!(:path).and_return("/tmp/monkeyfoo")
-    File.stub!(:chown).and_return(1)
-    File.should_receive(:chown).with(nil, 9982398, @provider.new_resource.path)
-    File.stub!(:chown).and_return(1)
-    File.should_receive(:chown).with(9982398, nil, @provider.new_resource.path)
-    File.stub!(:open).and_return(1)
-    File.should_receive(:chmod).with(0755, @provider.new_resource.path).and_return(1)
     File.should_receive(:open).with(@provider.new_resource.path, "w+").and_yield(io)
+    @provider.should_receive(:enforce_ownership_and_permissions)
     @provider.action_create
     io.string.should == "foobar"
   end
