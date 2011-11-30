c27d945439e00a1592d99948f832df2947680ff9
Igor Afonov <afonov@gmail.com>
2011-11-30 15:10:28 +0200

    Custom variables support in cron provider.

--- chef/lib/chef/provider/cron.rb
+++ chef/lib/chef/provider/cron.rb
@@ -52,17 +52,8 @@ class Chef
               cron_found = true
               @cron_exists = true
               next
-            when /^MAILTO=(\S*)/
-              @current_resource.mailto($1) if cron_found
-              next
-            when /^PATH=(\S*)/
-              @current_resource.path($1) if cron_found
-              next
-            when /^SHELL=(\S*)/
-              @current_resource.shell($1) if cron_found
-              next
-            when /^HOME=(\S*)/
-              @current_resource.home($1) if cron_found
+            when /^(\S*)=(\S*)/
+              set_environment_var($1, $2) if cron_found
               next
             when CRON_PATTERN
               if cron_found
@@ -89,7 +80,7 @@ class Chef
       end
 
       def compare_cron
-        [ :minute, :hour, :day, :month, :weekday, :command, :mailto, :path, :shell, :home ].any? do |cron_var|
+        [ :minute, :hour, :day, :month, :weekday, :command, :mailto, :path, :shell, :home, :environment ].any? do |cron_var|
           !@new_resource.send(cron_var).nil? && @new_resource.send(cron_var) != @current_resource.send(cron_var)
         end
       end
@@ -102,6 +93,9 @@ class Chef
         newcron << "# Chef Name: #{new_resource.name}\n"
         [ :mailto, :path, :shell, :home ].each do |v|
           newcron << "#{v.to_s.upcase}=#{@new_resource.send(v)}\n" if @new_resource.send(v)
+        end
+        @new_resource.environment.each do |name, value|
+          newcron << "#{name}=#{value}\n"
         end
         newcron << "#{@new_resource.minute} #{@new_resource.hour} #{@new_resource.day} #{@new_resource.month} #{@new_resource.weekday} #{@new_resource.command}\n"
 
@@ -181,6 +175,16 @@ class Chef
         end
       end
 
+      private
+
+      def set_environment_var(attr_name, attr_value)
+        method_name = attr_name.downcase.to_sym
+        if @current_resource.respond_to?(method_name)
+          @current_resource.send(method_name, attr_value)
+        else
+          @current_resource.environment(@current_resource.environment.merge(attr_name => attr_value))
+        end
+      end
     end
   end
 end
