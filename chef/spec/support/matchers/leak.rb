1dc72be97f703db450ae2f8f792ab06d473e1ed9
Seth Chisamore <schisamo@opscode.com>
2011-12-01 11:32:45 -0800

    add handle leak matcher

--- chef/spec/support/matchers/leak.rb
+++ chef/spec/support/matchers/leak.rb
@@ -18,10 +18,9 @@
 
 require File.join(File.dirname(__FILE__), '..', 'prof', 'gc')
 require File.join(File.dirname(__FILE__), '..', 'prof', 'win32')
-require 'rspec/matchers/be_within'
 
 module Matchers
-  class Leak
+  module LeakBase
     include RSpec::Matchers
 
     def initialize(opts={}, &block)
@@ -30,8 +29,16 @@ module Matchers
       @variance = opts[:variance] || 1000
     end
 
-    def matches?(given_proc)
+    def failure_message_for_should
+      "expected final measure [#{@final_measure}] to be greater than or within +/- #{@variance} delta of initial measure [#{@initial_measure}]"
+    end
 
+    def failure_message_for_should_not
+      "expected final measure [#{@final_measure}] to be less than or within +/- #{@variance} delta of initial measure [#{@initial_measure}]"
+    end
+
+    private
+    def match(measure, given_proc)
       profiler.start
 
       @initial_measure = 0
@@ -41,25 +48,16 @@ module Matchers
         given_proc.call
       end
 
-      @initial_measure = profiler.working_set_size
+      @initial_measure = profiler.send(measure)
 
       @iterations.times do
         given_proc.call
       end
 
-      @final_measure = profiler.working_set_size
+      @final_measure = profiler.send(measure)
       @final_measure > (@initial_measure + @variance)
     end
 
-    def failure_message_for_should
-      "expected #{@final_measure} to be greater than or within +/- #{@variance} delta of #{@initial_measure}"
-    end
-
-    def failure_message_for_should_not
-      "expected #{@final_measure} to be less than or within +/- #{@variance} delta of #{@initial_measure}"
-    end
-
-    private
     def profiler
       @profiler ||= begin
         case RbConfig::CONFIG['host_os']
@@ -70,10 +68,29 @@ module Matchers
         end
       end
     end
+
   end
 
-  def leak(opts, &block)
-    Matchers::Leak.new(opts, &block)
+  class LeakMemory
+    include LeakBase
+
+    def matches?(given_proc)
+      match(:working_set_size, given_proc)
+    end
   end
-  alias_method :leak_memory, :leak
+
+  class LeakHandles
+    include LeakBase
+
+    def matches?(given_proc)
+      match(:handle_count, given_proc)
+    end
+  end
+
+  def leak_memory(opts, &block)
+    Matchers::LeakMemory.new(opts, &block)
+  end
+  def leak_handles(opts, &block)
+    Matchers::LeakHandles.new(opts, &block)
+  end
 end
