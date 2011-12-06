ae2615a898a2a9763affcd98546ef6a54cdf4936
Daniel DeLeo <dan@opscode.com>
2011-12-05 16:30:48 -0800

    always grab a random pwent instead of using getlogin

--- spec/mixlib/shellout/shellout_spec.rb
+++ spec/mixlib/shellout/shellout_spec.rb
@@ -64,16 +64,12 @@ describe Mixlib::ShellOut do
 
   it "computes the uid of the user when a string/symbolic username is given" do
     unless windows?
-      if username = Etc.getlogin
-        expected_uid = Etc.getpwuid.uid
-      else
-        user_struct = Etc.getpwent
-        username = user_struct.name
-        expected_uid = user_struct.uid
-      end
+      user_struct = Etc.getpwent
+      username = user_struct.name
+      expected_uid = user_struct.uid
 
-      @shell_cmd.user = Etc.getlogin
-      @shell_cmd.uid.should == Etc.getpwuid.uid
+      @shell_cmd.user = username
+      @shell_cmd.uid.should == expected_uid
     end
   end
 
