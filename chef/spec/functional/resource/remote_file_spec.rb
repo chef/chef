768965072ef21b09d46fd1fd4dcfe78d6c2b3711
Seth Chisamore <schisamo@opscode.com>
2011-12-29 17:35:53 -0500

    Chef::Resource::RemoteFile functional tests

---
+++ chef/spec/functional/resource/remote_file_spec.rb
@@ -0,0 +1,36 @@
+#
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+
+describe Chef::Resource::RemoteFile do
+  include_context Chef::Resource::File
+
+  let(:file_base) { "remote_file_spec" }
+  let(:source) { 'http://opscode-chef-spec-data.s3.amazonaws.com/integration/remote_file/nyan_cat.png' }
+  let(:expected_content) { IO.read(File.join(CHEF_SPEC_DATA, 'remote_file', 'nyan_cat.png')) }
+
+  let!(:resource) do
+    resource = Chef::Resource::RemoteFile.new(path)
+    resource.source(source)
+    resource
+  end
+
+  it_behaves_like "a file resource"
+  it_behaves_like "a securable resource"
+end
