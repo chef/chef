c160b82b2dc33902c900227a47d7e919719e656f
Seth Chisamore <schisamo@opscode.com>
2011-12-29 17:36:18 -0500

    Chef::Resource::Template functional tests

---
+++ chef/spec/functional/resource/template_spec.rb
@@ -0,0 +1,64 @@
+#
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+
+describe Chef::Resource::Template do
+
+  include_context Chef::Resource::File
+
+  let(:file_base) { "template_spec" }
+  let(:expected_content) { "slappiness is a warm gun" }
+
+  let(:node) do
+    node = Chef::Node.new
+    node[:slappiness] = "a warm gun"
+    node
+  end
+
+  let!(:resource) do
+    cookbook_repo = File.expand_path(File.join(CHEF_SPEC_DATA, "cookbooks"))
+    Chef::Cookbook::FileVendor.on_create { |manifest| Chef::Cookbook::FileSystemFileVendor.new(manifest, cookbook_repo) }
+    cookbook_collection = Chef::CookbookCollection.new(Chef::CookbookLoader.new(cookbook_repo))
+    run_context = Chef::RunContext.new(node, cookbook_collection)
+    resource = Chef::Resource::Template.new(path, run_context)
+    resource.source('openldap_stuff.conf.erb')
+    resource.cookbook('openldap')
+    resource
+  end
+
+  it_behaves_like "a file resource"
+  it_behaves_like "a securable resource"
+
+  context "when the target file does not exist" do
+    it "creates the template with the rendered content using the variable attribute when the :create action is run" do
+      resource.source('openldap_variable_stuff.conf.erb')
+      resource.variables(:secret => "nutella")
+      resource.run_action(:create)
+      IO.read(path).should == "super secret is nutella"
+    end
+
+    it "creates the template with the rendered content using a local erb file when the :create action is run" do
+      resource.source(File.expand_path(File.join(CHEF_SPEC_DATA,'cookbooks','openldap','templates','default','openldap_stuff.conf.erb')))
+      resource.cookbook(nil)
+      resource.local(true)
+      resource.run_action(:create)
+      IO.read(path).should == expected_content
+    end
+  end
+end
