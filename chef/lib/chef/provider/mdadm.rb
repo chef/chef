13e4cd9977ad05b887fe2343faf15eb5ae4a5d4f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-02-15 17:41:41 -0800

    CHEF-2935: Add metadata option to the mdadm resource

--- chef/lib/chef/provider/mdadm.rb
+++ chef/lib/chef/provider/mdadm.rb
@@ -26,7 +26,7 @@ class Chef
 
       #include Chef::Mixin::Command
       include Chef::Mixin::ShellOut
-      
+
       def popen4
         raise Exception, "deprecated, bitches"
       end
@@ -51,7 +51,7 @@ class Chef
 
       def action_create
         unless @current_resource.exists
-          command = "yes | mdadm --create #{@new_resource.raid_device} --chunk=#{@new_resource.chunk} --level #{@new_resource.level} --raid-devices #{@new_resource.devices.length} #{@new_resource.devices.join(" ")}"
+          command = "yes | mdadm --create #{@new_resource.raid_device} --chunk=#{@new_resource.chunk} --level #{@new_resource.level} --metadata=#{@new_resource.metadata} --raid-devices #{@new_resource.devices.length} #{@new_resource.devices.join(" ")}"
           Chef::Log.debug("#{@new_resource} mdadm command: #{command}")
           #pid, stdin, stdout, stderr = popen4(command)
           shell_out!(command)
