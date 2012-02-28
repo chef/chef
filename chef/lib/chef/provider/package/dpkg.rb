4b516e6bb27c3c3fd905e46f1e2237d506c3fc21
Matthew Kent <mkent@magoazul.com>
2012-02-27 17:45:13 -0800

    CHEF-2488: Handle periods in dpkg names as they are allowed by the spec.

--- chef/lib/chef/provider/package/dpkg.rb
+++ chef/lib/chef/provider/package/dpkg.rb
@@ -25,7 +25,7 @@ class Chef
   class Provider
     class Package
       class Dpkg < Chef::Provider::Package::Apt
-        DPKG_INFO = /([a-z\d\-\+]+)\t([\w\d.~-]+)/
+        DPKG_INFO = /([a-z\d\-\+\.]+)\t([\w\d.~-]+)/
         DPKG_INSTALLED = /^Status: install ok installed/
         DPKG_VERSION = /^Version: (.+)$/
 
