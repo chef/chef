fa620f48f57b060b4b07f56a86946b9b4a57086f
trevoro <trevoro@joyent.com>
2012-03-16 22:54:13 +0000

    TOOLS-98 Adding smartos spec tests

--- chef/lib/chef/resource/smartos_package.rb
+++ chef/lib/chef/resource/smartos_package.rb
@@ -23,8 +23,8 @@ class Chef
   class Resource
     class SmartOSPackage < Chef::Resource::Package
         
-      def initialize(name, collection=nil, node=nil)
-        super(name, collection, node)
+      def initialize(name, run_context=nil)
+        super
         @resource_name = :smartos_package
         @provider = Chef::Provider::Package::SmartOS
       end
