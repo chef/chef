c72d608c61c4d8ac68bbc7c1419a49f31ecff70d
Logan Lowell <fractaloop@thefrontside.net>
2012-03-29 13:03:09 -0500

    Remove old Merb WebUI

--- chef-server-webui/lib/chef-server-webui.rb
+++
@@ -1,4 +0,0 @@
-require 'chef-server-webui/version'
-
-CHEF_SERVER_WEBUI_VERSION = ::ChefServerWebui::VERSION
-CHEF_SERVER_WEBUI_ROOT = File.expand_path(File.dirname(__FILE__) + '/../')
--- chef-server-webui/lib/chef-server-webui/version.rb
+++
@@ -1,3 +0,0 @@
-module ChefServerWebui
-  VERSION = '11.0.0.alpha'
-end
