c2e68863e2732ff86d715804da31c38a3961ef77
Steven Danna <steve@opscode.com>
2012-04-03 14:43:24 -0700

    Refactor cookbook_upload, fixing CHEF-2543.
    This commit extracts the logic to determine which cookbooks to upload
    into its own method, simplifying the main run method.  As a side
    effect, this also solves CHEF-2543.
    
    The commit also includes basic tests for this method; however, the
    tests do not yet provide full coverage.
    
    Conflicts:
    
    	chef/lib/chef/knife/cookbook_upload.rb
    	chef/spec/unit/knife/cookbook_upload_spec.rb

--- chef/spec/unit/knife/cookbook_upload_spec.rb
+++ chef/spec/unit/knife/cookbook_upload_spec.rb
@@ -1,5 +1,6 @@
 #
 # Author:: Matthew Kent (<mkent@magoazul.com>)
+# Author:: Steven Danna (<steve@opscode.com>)
 # Copyright:: Copyright (c) 2012 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
@@ -19,6 +20,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
 
 require 'chef/cookbook_uploader'
+require 'timeout'
 
 describe Chef::Knife::CookbookUpload do
   before(:each) do
@@ -27,13 +29,14 @@ describe Chef::Knife::CookbookUpload do
 
     @cookbook = Chef::CookbookVersion.new('test_cookbook')
 
-    @cookbook_loader = mock('Chef::CookbookLoader')
+    @cookbook_loader = {}
     @cookbook_loader.stub!(:[]).and_return(@cookbook)
     @cookbook_loader.stub!(:merged_cookbooks).and_return([])
     Chef::CookbookLoader.stub!(:new).and_return(@cookbook_loader)
 
-    @stdout = StringIO.new
-    @knife.ui.stub!(:stdout).and_return(@stdout)
+    @output = StringIO.new
+    @knife.ui.stub!(:stdout).and_return(@output)
+    @knife.ui.stub!(:stderr).and_return(@output)
   end
 
   describe 'run' do
@@ -60,55 +63,81 @@ describe Chef::Knife::CookbookUpload do
         @knife.ui.should_receive(:info).with(/Uploaded 1 cookbook/)
         @knife.run
       end
-
-      describe 'with -a or --all' do
-        before(:each) do
-          @knife.config[:all] = true
-          @test_cookbook1 = Chef::CookbookVersion.new('test_cookbook1')
-          @test_cookbook2 = Chef::CookbookVersion.new('test_cookbook2')
-          @cookbook_loader.stub!(:each).and_yield("test_cookbook1", @test_cookbook1).and_yield("test_cookbook2", @test_cookbook2)
-          @cookbook_loader.stub!(:cookbook_names).and_return(["test_cookbook1", "test_cookbook2"])
-        end
-
-        it 'should upload all cookbooks' do
-          @knife.should_receive(:upload).twice
-          @knife.run
-        end
-
-        it 'should report on success' do
-          @knife.should_receive(:upload).twice
-          @knife.ui.should_receive(:info).with(/Uploaded 2 cookbooks/)
-          @knife.run
-        end
-
-        it 'should update the version constraints for an environment' do
-          @knife.stub!(:assert_environment_valid!).and_return(true)
-          @knife.config[:environment] = "production"
-          @knife.should_receive(:update_version_constraints).once
-          @knife.run
-        end
-      end
-
-      describe 'when a frozen cookbook exists on the server' do
-        it 'should fail to replace it' do
-          @knife.stub!(:upload).and_raise(Chef::Exceptions::CookbookFrozen)
-          @knife.ui.should_receive(:error).with(/Failed to upload 1 cookbook/)
-          lambda { @knife.run }.should raise_error(SystemExit)
-        end
-
-        it 'should not update the version constraints for an environment' do
-          @knife.stub!(:assert_environment_valid!).and_return(true)
-          @knife.config[:environment] = "production"
-          @knife.stub!(:upload).and_raise(Chef::Exceptions::CookbookFrozen)
-          @knife.ui.should_receive(:error).with(/Failed to upload 1 cookbook/)
-          @knife.ui.should_receive(:warn).with(/Not updating version constraints/)
-          @knife.should_not_receive(:update_version_constraints)
-          lambda { @knife.run }.should raise_error(SystemExit)
-        end
-      end
-
-      # TODO: many more specs!
     end
 
-  end # run
-end
+    # This is testing too much.  We should break it up.
+    describe 'when specifying a cookbook name with dependencies' do
+      it "should upload all dependencies once" do
+        @knife.name_args = ["test_cookbook2"]
+        @knife.config[:depends] = true
+        @test_cookbook1 = Chef::CookbookVersion.new('test_cookbook1')
+        @test_cookbook2 = Chef::CookbookVersion.new('test_cookbook2')
+        @test_cookbook3 = Chef::CookbookVersion.new('test_cookbook3')
+        @test_cookbook2.metadata.depends("test_cookbook3")
+        @test_cookbook3.metadata.depends("test_cookbook1")
+        @test_cookbook3.metadata.depends("test_cookbook2")
+        @cookbook_loader.stub!(:[])  do |ckbk|
+          { "test_cookbook1" =>  @test_cookbook1,
+            "test_cookbook2" =>  @test_cookbook2,
+            "test_cookbook3" => @test_cookbook3 }[ckbk]
+        end
+        @knife.should_receive(:upload).exactly(3).times
+        Timeout::timeout(5) do
+          @knife.run
+        end.should_not raise_error(Timeout::Error)
+      end
+    end
+
+    it "should freeze the version of the cookbooks if --freeze is specified" do
+      @knife.config[:freeze] = true
+      @cookbook.should_receive(:freeze_version).once
+      @knife.run
+    end
+
+    describe 'with -a or --all' do
+      before(:each) do
+        @knife.config[:all] = true
+        @test_cookbook1 = Chef::CookbookVersion.new('test_cookbook1')
+        @test_cookbook2 = Chef::CookbookVersion.new('test_cookbook2')
+        @cookbook_loader.stub!(:each).and_yield("test_cookbook1", @test_cookbook1).and_yield("test_cookbook2", @test_cookbook2)
+        @cookbook_loader.stub!(:cookbook_names).and_return(["test_cookbook1", "test_cookbook2"])
+      end
+
+      it 'should upload all cookbooks' do
+        @knife.should_receive(:upload).twice
+        @knife.run
+      end
+
+      it 'should report on success' do
+        @knife.should_receive(:upload).twice
+        @knife.ui.should_receive(:info).with(/Uploaded 2 cookbooks/)
+        @knife.run
+      end
+
+      it 'should update the version constraints for an environment' do
+        @knife.stub!(:assert_environment_valid!).and_return(true)
+        @knife.config[:environment] = "production"
+        @knife.should_receive(:update_version_constraints).once
+        @knife.run
+      end
+    end
+
+    describe 'when a frozen cookbook exists on the server' do
+      it 'should fail to replace it' do
+        @knife.stub!(:upload).and_raise(Chef::Exceptions::CookbookFrozen)
+        @knife.ui.should_receive(:error).with(/Failed to upload 1 cookbook/)
+        lambda { @knife.run }.should raise_error(SystemExit)
+      end
+
+      it 'should not update the version constraints for an environment' do
+        @knife.stub!(:assert_environment_valid!).and_return(true)
+        @knife.config[:environment] = "production"
+        @knife.stub!(:upload).and_raise(Chef::Exceptions::CookbookFrozen)
+        @knife.ui.should_receive(:error).with(/Failed to upload 1 cookbook/)
+        @knife.ui.should_receive(:warn).with(/Not updating version constraints/)
+        @knife.should_not_receive(:update_version_constraints)
+        lambda { @knife.run }.should raise_error(SystemExit)
+      end
+    end
+  end
+end # run
