4e695bb1dc20eaa5c9512454222888f861a82e06
Logan Lowell <fractaloop@thefrontside.net>
2012-04-05 12:05:45 -0500

    Allow asset pipeline in production

--- chef-server-webui/config/environments/production.rb
+++ chef-server-webui/config/environments/production.rb
@@ -15,7 +15,7 @@ ChefServerWebui::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
