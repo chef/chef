82e8202d43ed7bc9ba2f1f8d2b34bfe57f045c0b
j3p0uk <j3p0uk@hotmail.com>
2012-04-06 08:55:39 +0100

    Update to most appropriate regex for cookbook version

--- chef-server-api/config/router.rb
+++ chef-server-api/config/router.rb
@@ -70,19 +70,19 @@ Merb::Router.prepare do
   match("/cookbooks/:cookbook_name/:cookbook_version",
         :method => 'put',
         :cookbook_name => /[\w\.]+/,
-        :cookbook_version => /\d+\.\d+\.\d+/
+        :cookbook_version => /^(\d+.\d+(?:.\d+)?)$/
         ).to(:controller => "cookbooks", :action => "update")
 
   match("/cookbooks/:cookbook_name/:cookbook_version",
         :method => 'get',
         :cookbook_name => /[\w\.]+/,
-        :cookbook_version => /\d+\.\d+\.\d+|_latest/
+        :cookbook_version => /^((?:\d+.\d+(?:.\d+)?)|_latest)$/
         ).to(:controller => "cookbooks", :action => "show").name(:cookbook_version)
 
   match("/cookbooks/:cookbook_name/:cookbook_version",
         :method => 'delete',
         :cookbook_name => /[\w\.]+/,
-        :cookbook_version => /\d+\.\d+\.\d+|_latest/
+        :cookbook_version => /^((?:\d+.\d+(?:.\d+)?)|_latest)$/
         ).to(:controller => "cookbooks", :action => "destroy")
 
   match("/cookbooks/:cookbook_name",
@@ -92,7 +92,7 @@ Merb::Router.prepare do
 
   match("/cookbooks/:cookbook_name/:cookbook_version/files/:checksum",
         :cookbook_name => /[\w\.]+/,
-        :cookbook_version => /\d+\.\d+\.\d+|_latest/
+        :cookbook_version => /^((?:\d+.\d+(?:.\d+)?)|_latest)$/
         ).to(
              :controller => "cookbooks",
              :action => "show_file"
