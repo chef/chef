65245627927d9d6f7dc0f4feed622ca77666723e
Ho-Sheng Hsiao <hosheng.hsiao@gmail.com>
2012-04-10 22:30:55 -0400

    [CHEF-2994][RSPEC] Use metadata filtering for platform-specific test

--- chef/spec/unit/mixin/command_spec.rb
+++ chef/spec/unit/mixin/command_spec.rb
@@ -21,81 +21,79 @@ require 'spec_helper'
 describe Chef::Mixin::Command do
 
   if windows?
+    pending '#popen4 is deprecated'
+    pending '#run_command should work on windows'
+  end
 
-    pending("TODO MOVE: this is a platform specific integration test.")
+  describe "popen4", :unix_only do
+    include Chef::Mixin::Command
 
-  else
+    it "should be possible to read the child process's stdout and stderr" do
+      popen4("sh -c 'echo hello && echo world >&2'") do |pid, stdin, stdout, stderr|
+        stdout.read.should == "hello\n"
+        stderr.read.should == "world\n"
+      end
+    end
 
-    describe "popen4" do
-      include Chef::Mixin::Command
+    it "should default all commands to be run in the POSIX standard C locale" do
+      popen4("echo $LC_ALL") do |pid, stdin, stdout, stderr|
+        stdout.read.strip.should == "C"
+      end
+    end
 
-      it "should be possible to read the child process's stdout and stderr" do
-        popen4("sh -c 'echo hello && echo world >&2'") do |pid, stdin, stdout, stderr|
-          stdout.read.should == "hello\n"
-          stderr.read.should == "world\n"
+    it "should respect locale when specified explicitly" do
+      popen4("echo $LC_ALL", :environment => {"LC_ALL" => "es"}) do |pid, stdin, stdout, stderr|
+        stdout.read.strip.should == "es"
+      end
+    end
+
+    it "should end when the child process reads from STDIN and a block is given" do
+      lambda {Timeout.timeout(4) do
+        popen4("ruby -e 'while gets; end'", :waitlast => true) do |pid, stdin, stdout, stderr|
+          (1..5).each { |i| stdin.puts "#{i}" }
         end
       end
+      }.should_not raise_error
+    end
 
-      it "should default all commands to be run in the POSIX standard C locale" do
-        popen4("echo $LC_ALL") do |pid, stdin, stdout, stderr|
-          stdout.read.strip.should == "C"
-        end
-      end
+    describe "when a process detaches but doesn't close STDOUT and STDERR [CHEF-584]" do
 
-      it "should respect locale when specified explicitly" do
-        popen4("echo $LC_ALL", :environment => {"LC_ALL" => "es"}) do |pid, stdin, stdout, stderr|
-          stdout.read.strip.should == "es"
-        end
-      end
-
-      it "should end when the child process reads from STDIN and a block is given" do
+      it "returns immediately after the first child process exits" do
         lambda {Timeout.timeout(4) do
-            popen4("ruby -e 'while gets; end'", :waitlast => true) do |pid, stdin, stdout, stderr|
-              (1..5).each { |i| stdin.puts "#{i}" }
-            end
+          pid, stdin,stdout,stderr = nil,nil,nil,nil
+          evil_forker="exit if fork; 10.times { sleep 1}"
+          popen4("ruby -e '#{evil_forker}'") do |pid,stdin,stdout,stderr|
           end
-        }.should_not raise_error
-      end
-
-      describe "when a process detaches but doesn't close STDOUT and STDERR [CHEF-584]" do
-
-        it "returns immediately after the first child process exits" do
-          lambda {Timeout.timeout(4) do
-            pid, stdin,stdout,stderr = nil,nil,nil,nil
-            evil_forker="exit if fork; 10.times { sleep 1}"
-            popen4("ruby -e '#{evil_forker}'") do |pid,stdin,stdout,stderr|
-            end
-          end}.should_not raise_error
-        end
-
+        end}.should_not raise_error
       end
 
     end
 
-    describe "run_command" do
-      include Chef::Mixin::Command
+  end
 
-      it "logs the command's stderr and stdout output if the command failed" do
-        Chef::Log.stub!(:level).and_return(:debug)
-        begin
-          run_command(:command => "sh -c 'echo hello; echo world >&2; false'")
-          violated "Exception expected, but nothing raised."
-        rescue => e
-          e.message.should =~ /STDOUT: hello/
+  describe "run_command", :unix_only do
+    include Chef::Mixin::Command
+
+    it "logs the command's stderr and stdout output if the command failed" do
+      Chef::Log.stub!(:level).and_return(:debug)
+      begin
+        run_command(:command => "sh -c 'echo hello; echo world >&2; false'")
+        violated "Exception expected, but nothing raised."
+      rescue => e
+        e.message.should =~ /STDOUT: hello/
           e.message.should =~ /STDERR: world/
-        end
+      end
+    end
+
+    describe "when a process detaches but doesn't close STDOUT and STDERR [CHEF-584]" do
+      it "returns successfully" do
+        # CHEF-2916 might have added a slight delay here, or our CI infrastructure is burdened. Bumping timeout from 2 => 4 -- btm
+        lambda {Timeout.timeout(4) do
+          evil_forker="exit if fork; 10.times { sleep 1}"
+          run_command(:command => "ruby -e '#{evil_forker}'")
+        end}.should_not raise_error
       end
 
-      describe "when a process detaches but doesn't close STDOUT and STDERR [CHEF-584]" do
-        it "returns successfully" do
-          # CHEF-2916 might have added a slight delay here, or our CI infrastructure is burdened. Bumping timeout from 2 => 4 -- btm
-          lambda {Timeout.timeout(4) do
-            evil_forker="exit if fork; 10.times { sleep 1}"
-            run_command(:command => "ruby -e '#{evil_forker}'")
-          end}.should_not raise_error
-        end
-
-      end
     end
   end
 end
