04fb97a45ce63e4c14b38d9b308169281cfa109a
Guilhem Lettron <guilhem@lettron.fr>
2012-04-23 02:54:14 +0300

    add parameters to resource/service.rb

--- chef/lib/chef/resource/service.rb
+++ chef/lib/chef/resource/service.rb
@@ -28,7 +28,8 @@ class Chef
         @service_name = name
         @enabled = nil
         @running = nil
-        @pattern = service_name 
+        @parameters = nil
+        @pattern = service_name
         @start_command = nil
         @stop_command = nil
         @status_command = nil
