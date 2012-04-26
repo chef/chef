6311df0ce03191734c10a1fb6b45f07af2ec8e4d
Ho-Sheng Hsiao <hosheng.hsiao@gmail.com>
2012-04-26 02:14:46 -0400

    [CHEF-2994][PROVIDER] Refactored Package::Rpm to smaller methods

--- chef/lib/chef/provider/package/rpm.rb
+++ chef/lib/chef/provider/package/rpm.rb
@@ -30,43 +30,14 @@ class Chef
         def load_current_resource
           @current_resource = Chef::Resource::Package.new(@new_resource.name)
           @current_resource.package_name(@new_resource.package_name)
-          @new_resource.version(nil)
 
-          if @new_resource.source
-            unless ::File.exists?(@new_resource.source)
-              raise Chef::Exceptions::Package, "Package #{@new_resource.name} not found: #{@new_resource.source}"
-            end
+          assert_install_action_requires_source!
 
-            Chef::Log.debug("#{@new_resource} checking rpm status")
-            status = popen4("rpm -qp --queryformat '%{NAME} %{VERSION}-%{RELEASE}\n' #{@new_resource.source}") do |pid, stdin, stdout, stderr|
-              stdout.each do |line|
-                case line
-                when /([\w\d_.-]+)\s([\w\d_.-]+)/
-                  @current_resource.package_name($1)
-                  @new_resource.version($2)
-                end
-              end
-            end
-          else
-            if Array(@new_resource.action).include?(:install)
-              raise Chef::Exceptions::Package, "Source for package #{@new_resource.name} required for action install"
-            end
-          end
+          rpm_name, rpm_version = package_name_and_version
 
-          Chef::Log.debug("#{@new_resource} checking install state")
-          status = popen4("rpm -q --queryformat '%{NAME} %{VERSION}-%{RELEASE}\n' #{@current_resource.package_name}") do |pid, stdin, stdout, stderr|
-            stdout.each do |line|
-              case line
-              when /([\w\d_.-]+)\s([\w\d_.-]+)/
-                Chef::Log.debug("#{@new_resource} current version is #{$2}")
-                @current_resource.version($2)
-              end
-            end
-          end
-
-          unless status.exitstatus == 0 || status.exitstatus == 1
-            raise Chef::Exceptions::Package, "rpm failed - #{status.inspect}!"
-          end
+          @new_resource.version          rpm_version
+          @current_resource.package_name rpm_name
+          @current_resource.version      installed_version
 
           @current_resource
         end
@@ -86,6 +57,51 @@ class Chef
             shell_out_with_systems_locale! "rpm #{@new_resource.options} -e #{name}-#{version}"
           else
             shell_out_with_systems_locale! "rpm #{@new_resource.options} -e #{name}"
+          end
+        end
+
+        def assert_install_action_requires_source!
+          return if @new_resource.source || !Array(@new_resource.action).include?(:install)
+          raise Chef::Exceptions::Package, "Source for package #{@new_resource.name} required for action install"
+        end
+
+        def assert_rpm_exists!
+          return if ::File.exists?(@new_resource.source)
+          raise Chef::Exceptions::Package, "Package #{@new_resource.name} not found: #{@new_resource.source}"
+        end
+
+        def package_name_and_version
+          return unless @new_resource.source
+          assert_rpm_exists!
+
+
+          Chef::Log.debug("#{@new_resource} checking rpm status")
+          status = popen4("rpm -qp --queryformat '%{NAME} %{VERSION}-%{RELEASE}\n' #{@new_resource.source}") do |pid, stdin, stdout, stderr|
+            stdout.each do |line|
+              case line
+              when /([\w\d_.-]+)\s([\w\d_.-]+)/
+                return [$1, $2] # package_name, version
+              end
+            end
+          end
+
+          return [nil, nil]
+        end
+
+        def installed_version
+          Chef::Log.debug("#{@new_resource} checking install state")
+          status = popen4("rpm -q --queryformat '%{NAME} %{VERSION}-%{RELEASE}\n' #{@current_resource.package_name}") do |pid, stdin, stdout, stderr|
+            stdout.each do |line|
+              case line
+              when /([\w\d_.-]+)\s([\w\d_.-]+)/
+                Chef::Log.debug("#{@new_resource} current version is #{$2}")
+                return $2 # installed version
+              end
+            end
+          end
+
+          unless status.exitstatus == 0 || status.exitstatus == 1
+            raise Chef::Exceptions::Package, "rpm failed - #{status.inspect}!"
           end
         end
 
