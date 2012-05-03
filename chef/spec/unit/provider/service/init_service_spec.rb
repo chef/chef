5e34a191e10c3b0d6f3ac4ab60332d0f4d07d139
Marc Paradise <marc@opscode.com>
2012-05-02 18:16:59 -0700

    Fix broken tests in init_service_spec

--- chef/spec/unit/provider/service/init_service_spec.rb
+++ chef/spec/unit/provider/service/init_service_spec.rb
@@ -94,16 +94,19 @@ PS
   end
   
   describe "when the node has not specified a ps command" do
-    before do
-      @node[:command] = {:ps => nil}
-    end
     
-    it "should set running to false if the node has a nil ps attribute" do
-      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Service)
+    it "should raise an error if the node has a nil ps attribute" do
+      @node[:command] = {:ps => nil}
+      @provider.load_current_resource
+      @provider.define_resource_requirements
+      lambda { @provider.process_resource_requirements(:any) }.should raise_error(Chef::Exceptions::Service)
     end
 
-    it "should set running to false if the node has an empty ps attribute" do
-      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Service)
+    it "should raise an error if the node has an empty ps attribute" do
+      @node[:command] = {:ps => ""}
+      @provider.load_current_resource
+      @provider.define_resource_requirements
+      lambda { @provider.process_resource_requirements(:any) }.should raise_error(Chef::Exceptions::Service)
     end
     
   end
@@ -133,7 +136,9 @@ RUNNING_PS
 
     it "should raise an exception if ps fails" do
       @provider.stub!(:shell_out!).and_raise(Mixlib::ShellOut::ShellCommandFailed)
-      lambda { @provider.load_current_resource }.should raise_error(Chef::Exceptions::Service)
+      @provider.load_current_resource
+      @provider.define_resource_requirements
+      lambda { @provider.process_resource_requirements(:any) }.should raise_error(Chef::Exceptions::Service)
     end
   end
 
