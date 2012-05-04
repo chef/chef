fabc8b263dcdcd7f6de3cc391ad85b984c2dbc5f
Adam Jacob <adam@opscode.com>
2012-05-04 14:20:36 -0700

    Adding clean functionality to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,15 +5,28 @@
 
 set -e
 
-# First, we gotta install our own deps - this is using system ruby
-bundle install
+if [ $CLEAN = "true" ]; then
+  rm -rf /opt/chef || true
+  mkdir -p /opt/chef && chown jenkins /opt/chef
+  rm -rf /opt/chef-server || true
+  mkdir -p /opt/chef-server && chown jenkins /opt/chef
+  rm -r /var/cache/omnibus/pkg/* || true
+  rm pkg/* || true 
+  bundle update
+else
+  bundle install
+fi
 
 # Omnibus build server prep tasks, including build ruby 
 sudo env OMNIBUS_GEM_PATH=$(bundle show omnibus) chef-solo -c jenkins-solo.rb -j jenkins-dna.json -l debug
 
 # Aaand.. new ruby
 export PATH=/usr/local/bin:$PATH
-bundle install
+if [ $CLEAN = "true" ]; then
+  bundle update
+else
+  bundle install
+fi 
 
 rake projects:$1
 
