5a9d0d8dcc7efdd5be22b0aaa19ec609d6508ec4
Adam Jacob <adam@opscode.com>
2012-05-04 10:56:06 -0700

    Working build configs

--- jenkins-solo.rb
+++ jenkins-solo.rb
@@ -1,3 +1,3 @@
-pwd = File.dirname(__FILE__)
+pwd = File.expand_path(File.dirname(__FILE__))
 file_cache_path "#{pwd}/chef-solo/cache"
-cookbook_path "#{ENV["omnibus_path"]}/cookbooks"
+cookbook_path "#{ENV["OMNIBUS_GEM_PATH"]}/cookbooks"
--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,15 @@
 # Build you some jenkins
 #
 
+# First, we gotta install our own deps - this is using system ruby
 bundle install
-omnibus_path = $(bundle show omnibus)
-chef-solo -c jenkins-solo.rb -j jenkins-dna.json
+
+# Omnibus build server prep tasks, including build ruby 
+sudo env OMNIBUS_GEM_PATH=$(bundle show omnibus) chef-solo -c jenkins-solo.rb -j jenkins-dna.json -l debug
+
+# Aaand.. new ruby
+export PATH=/usr/local/bin:$PATH
+bundle install
+
+rake projects:$1
+
