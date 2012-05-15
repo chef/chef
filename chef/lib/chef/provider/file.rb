d4d66fd6e807293c3d795094ce3fdf7225d97a24
Marc Paradise <marc@opscode.com>
2012-05-15 12:58:01 -0400

    make directory missing message less redundant

--- chef/lib/chef/provider/file.rb
+++ chef/lib/chef/provider/file.rb
@@ -109,7 +109,7 @@ class Chef
           parent_directory = ::File.dirname(@new_resource.path)
 
           a.assertion { ::File.directory?(parent_directory) }
-          a.failure_message(Chef::Exceptions::EnclosingDirectoryDoesNotExist, "Parent directory #{parent_directory} does not exist, cannot create #{@new_resource.path}")
+          a.failure_message(Chef::Exceptions::EnclosingDirectoryDoesNotExist, "Parent directory #{parent_directory} does not exist.")
           a.whyrun("Assuming directory #{parent_directory} would have been created")
         end
 
