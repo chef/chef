5466daa0af6e6398c923cb01a3fccfd11a619c58
Lamont Granquist <lamont@opscode.com>
2012-05-24 14:34:46 -0700

    fix jenkins-node username

--- jenkins.sh
+++ jenkins.sh
@@ -10,9 +10,9 @@ mkdir -p chef-solo/cache
 
 if [ $CLEAN = "true" ]; then
   sudo rm -rf /opt/chef || true
-  sudo mkdir -p /opt/chef && sudo chown jenkins /opt/chef
+  sudo mkdir -p /opt/chef && sudo chown jenkins-node /opt/chef
   sudo rm -rf /opt/chef-server || true
-  sudo mkdir -p /opt/chef-server && sudo chown jenkins /opt/chef
+  sudo mkdir -p /opt/chef-server && sudo chown jenkins-node /opt/chef
   sudo rm -r /var/cache/omnibus/pkg/* || true
   sudo rm /var/cache/omnibus/build/*/*.manifest || true
   sudo rm pkg/* || true 
