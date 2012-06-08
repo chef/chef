8b03d64877c877a054f2a5de6e0e04f074bbd8fc
aschlaep <aschlaep@opscode.com>
2012-06-08 12:18:13 -0700

    further work on client-release

--- jenkins/client-release.rb
+++ jenkins/client-release.rb
@@ -1,10 +1,10 @@
-
+#!/usr/bin/env ruby
 
 
 require 'bundler/setup'
 
 # Want to upload debian, ubuntu 10, ubuntu 11, centos 5, centos 6
-# Input - S3 credentials, OS platform, OS version, architecture, Chef version, package iteration
+# Input - filename, S3 credentials, OS platform, OS version, architecture, Chef version, package iteration
 
 # debian:
 # http://opscode-full-stack.s3.amazonaws.com/debian-6.0.1-i686/chef-full_0.10.10-1_i386.deb
@@ -26,32 +26,49 @@ require 'bundler/setup'
 # http://opscode-full-stack.s3.amazonaws.com/ubuntu-11.04-i686/chef-full_10.12.0.rc.1-1_i386.deb
 # http://opscode-full-stack.s3.amazonaws.com/ubuntu-11.04-x86_64/chef-full_10.12.0.rc.1-1_amd64.deb
 
-def package_name(os_platform, os_version, chef_version, package_iteration, architecture)
-    arch_dir = if (architecture == 'i386')
-    	       	  'i686/'
-	       else
-		  'x86-64/'
-	       end
-    arch_file_ext = if (os_platform == 'centos')
-    	      	  arch_dir + '.rpm'
-		else
-		  if (architecture == 'i386')
-		     'i386.deb'
-		  else
-		     'amd64.deb'
-		  end
-		end
+def package_name(os_platform, os_version, chef_version, architecture, package_iteration = nil)
+  arch_dir = if (architecture == 'i386')
+               'i686/'
+             else
+               'x86_64/'
+             end
+  arch_file_ext = if (os_platform == 'centos')
+                    arch_dir + '.rpm'
+                  else
+                    if (architecture == 'i386')
+                      'i386.deb'
+                    else
+                      'amd64.deb'
+                    end
+                  end
+  chef_filename = 'chef-full_' + chef_version
+  if (!package_iteration.nil?)
     package_iteration = '-' + package_iteration.to_s
-    case os_platform
-    when 'debian'
-    	 directorybase = 'debian-6.0.1-'
-	 chef_filename = 'chef-full_' + chef_version + '_' + package_iteration + '_'
-    when 'ubuntu'
-    	directorybase = 'ubuntu-' + os_version + '-'
-	chef_filename = 'chef-full_' + chef_version + package_iteration + '_'
-    when 'centos'
-    	directorybase = 'el-' + os_version + '-'
-	chef_filename = 'chef-full-' + chef_version + package_iteration + '.'
+    chef_filename += package_iteration
+  end
+  case os_platform
+  when 'debian'
+    directorybase = 'debian-6.0.1-'
+    chef_filename += '_'
+  when 'ubuntu'
+    directorybase = 'ubuntu-' + os_version + '-'
+    chef_filename += '_'
+  when 'centos'
+    directorybase = 'el-' + os_version + '-'
+    chef_filename += '.'
+  end
+  filename = directorybase + arch_dir + chef_filename + arch_file_ext
+end
+
+os_list = ['debian', 'centos', 'ubuntu']
+arch_list = ['i386', 'x86_64']
+osversion_list = ['10.04', '5'] #made up versions, will differ across operating systems
+
+os_list.each do |os|
+  arch_list.each do |arch|
+    osversion_list.each do |osversion|
+      puts package_name(os, osversion, '10.12.0.rc.1', arch, 1)
+      puts package_name(os, osversion, '10.12.0.rc.1', arch)
     end
-    filename = directorybase + arch_dir + chef_filename + arch_file_ext
+  end
 end
