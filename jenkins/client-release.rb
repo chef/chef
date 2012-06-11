a8fb61c9828c54bc2fe6c2912a5a40a8835c625d
aschlaep <aschlaep@opscode.com>
2012-06-11 16:06:28 -0700

    Modified client-release.rb to new naming scheme

--- jenkins/client-release.rb
+++ jenkins/client-release.rb
@@ -28,59 +28,41 @@ require 'uber-s3'
 # http://opscode-full-stack.s3.amazonaws.com/ubuntu-11.04-x86_64/chef-full_10.12.0.rc.1-1_amd64.deb
 
 # This will take in data about the OS, chef, and architecture and spit out the proper upload directory
-def package_name(os_platform, os_version, chef_version, architecture, package_iteration = nil)
+def package_name(filepath, os_platform, os_version, architecture)
+  filename = filepath.split("/")[-1]
   arch_dir = if (architecture == 'i386')
                'i686/'
              else
                'x86_64/'
              end
-  arch_file_ext = if (os_platform == 'centos')
-                    arch_dir.chomp('/') + '.rpm'
-                  else
-                    if (architecture == 'i386')
-                      'i386.deb'
-                    else
-                      'amd64.deb'
-                    end
-                  end
-  chef_filename = 'chef-full_' + chef_version
-  if (!package_iteration.nil?)
-    package_iteration = '-' + package_iteration.to_s
-    chef_filename += package_iteration
-  end
   case os_platform
   when 'debian'
     directorybase = 'debian-6.0.1-'
-    chef_filename += '_'
   when 'ubuntu'
     directorybase = 'ubuntu-' + os_version + '-'
-    chef_filename += '_'
   when 'centos'
     directorybase = 'el-' + os_version + '-'
-    chef_filename += '.'
   end
-  filename = directorybase + arch_dir + chef_filename + arch_file_ext
+  packagename = directorybase + arch_dir + filename
 end
 
 # os_list = ['debian', 'centos', 'ubuntu']
 # arch_list = ['i386', 'x86_64']
-# osversion_list = ['10.04', '5'] #made up versions, will differ across operating systems
+# os_version = ['10.04', '6.2']
 
 # os_list.each do |os|
 #   arch_list.each do |arch|
-#     osversion_list.each do |osversion|
-#       puts package_name(os, osversion, '10.12.0.rc.1', arch, 1)
-#       puts package_name(os, osversion, '10.12.0.rc.1', arch)
-#     end
+#     os_version.each do |osversion|
+#       puts package_name("/foo/bar/foobar/madeup.file", os, osversion, arch)
+#       end
 #   end
 # end
 
-# ARGV = [filename, os_platform, os_version, chef_version, architecture, credentials, package_iteration]
+# ARGV = [filepath, os_platform, os_version, architecture, credentials]
 
-package_iter = package_name(ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[6]) # format upload directory
-package = package_name(ARGV[1], ARGV[2], ARGV[3], ARGV[4])
+package = package_name(ARGV[0], ARGV[1], ARGV[2], ARGV[3]) # format upload directory
 
-(key, secret, bucket) = IO.read(ARGV[5]).lines.to_a  # Read in s3 credentials
+(key, secret, bucket) = IO.read(ARGV[4]).lines.to_a  # Read in s3 credentials
 
 s3 = UberS3.new({
   :access_key         => key.chomp,
@@ -91,6 +73,4 @@ s3 = UberS3.new({
 
 file = IO.read(ARGV[0])
 
-s3.store(package_iter, file, :access => :public_read)
 s3.store(package, file, :access => :public_read)
-
