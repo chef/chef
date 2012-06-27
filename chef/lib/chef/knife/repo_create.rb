75985e72faa2760593267b94a673ed442bd72f57
Chris Kelly <amateurhuman@gmail.com>
2012-06-26 22:48:34 -0700

    Bare repo creation class and spec

---
+++ chef/lib/chef/knife/repo_create.rb
@@ -0,0 +1,9 @@
+require 'chef/knife'
+
+class Chef
+  class Knife
+    class RepoCreate < Knife
+
+    end
+  end
+end
---
+++ chef/spec/unit/knife/repo_create_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'tmpdir'
+
+describe Chef::Knife::RepoCreate do
+
+end
