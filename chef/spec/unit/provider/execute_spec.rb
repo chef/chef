1fbd9f3d8809d6f003b1ea74c8179c9ceeeecd82
Bryan McLellan <btm@opscode.com>
2012-07-12 12:48:00 -0700

    remove my crufty debugging from execute spec

--- chef/spec/unit/provider/execute_spec.rb
+++ chef/spec/unit/provider/execute_spec.rb
@@ -52,9 +52,6 @@ describe Chef::Provider::Execute do
   end
 
   it "should do nothing if the sentinel file exists" do
-    puts "STDOUT: #{STDOUT.tty?}"
-    puts "daemon: #{Chef::Config[:daemon]}"
-    puts "log: #{Chef::Log.info?}"
     @provider.stub!(:load_current_resource)
     File.should_receive(:exists?).with(@new_resource.creates).and_return(true)
     @provider.should_not_receive(:shell_out!)
