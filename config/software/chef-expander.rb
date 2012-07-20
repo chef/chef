144af507c8b1db073ff6e50eaa1e48f32d631dea
Seth Chisamore <schisamo@opscode.com>
2012-07-19 20:37:42 -0400

    don't hardcode /opt/chef-server prefer install_dir

--- config/software/chef-expander.rb
+++ config/software/chef-expander.rb
@@ -25,7 +25,7 @@ source :git => "git://github.com/opscode/chef"
 project_dir = "#{source_dir}/#{name}/#{name}"
 
 build do
-  bundle "install --path=/opt/chef-server/embedded/service/gem", :cwd => project_dir
+  bundle "install --path=#{install_dir}/embedded/service/gem", :cwd => project_dir
   command "mkdir -p #{install_dir}/embedded/service/chef-expander"
   command "#{install_dir}/embedded/bin/rsync -a #{project_dir}/ --delete --exclude=.git/*** --exclude=.gitignore #{install_dir}/embedded/service/chef-expander/"
 end
