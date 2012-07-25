aa44bfbf8cbdbebe6e1061ddcc8ca913bcf98170
Stephen Delano <stephen@opscode.com>
2012-07-25 14:56:32 -0700

    use the jenkins-packaged source for running unit tests

--- jenkins/client-test.sh
+++ jenkins/client-test.sh
@@ -2,19 +2,20 @@
 #
 # Test you some omnibus client
 #
-
 set -e
 set -x
 
-# TODO:
-# In the future, test-specific boxes will slurp up the artifacts
-# (package and source) and build the test scenario from scratch.
-# For now, we will run tests immediately after the build, so we know
-# where everything is located.
-
-cd /var/cache/omnibus/src/chef/chef
-mkdir bundle
-
+# install the chef package
+# TODO: here
 export PATH=/opt/chef/bin:/opt/chef/embedded/bin:$PATH
+
+# extract the chef source code
+tar xvzf pkg/chef*.tar.gz
+
+# install all of the development gems
+cd chef/chef
+mkdir bundle
 bundle install --without server --path bundle
+
+# run the tests
 sudo env PATH=$PATH bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o $WORKSPACE/test.xml -f documentation spec
