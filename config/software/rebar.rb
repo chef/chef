2975cd900654df00a258b6cb3d8b9b8200f9adee
Seth Chisamore <schisamo@opscode.com>
2012-08-08 18:06:25 -0400

    add rebar as a dependency

---
+++ config/software/rebar.rb
@@ -0,0 +1,19 @@
+name "rebar"
+version "2.0.0"
+
+dependencies ["erlang"]
+
+source :git => "https://github.com/basho/rebar.git"
+
+relative_path "rebar"
+
+env = {
+  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
+  "LD_FLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+build do
+  command "./bootstrap", :env => env
+  command "cp ./rebar #{install_dir}/embedded/bin/"
+end
