897fddf7594cc2cd92abfa258938b6c809acd1bd
Lamont Granquist <lamont@opscode.com>
2012-08-16 13:23:02 -0700

    making omnibus.rb match opscode-omnibus usage, adding caching back

--- omnibus.rb
+++
@@ -1,8 +0,0 @@
-
-Omnibus.configure do |o|
-  # o.s3_access_key   = "something"
-  # o.s3_secret_key   = "something"
-  #o.s3_bucket       = ""
-  o.use_s3_caching  = false 
-  o.solaris_compiler = "gcc"
-end
