7793c31698b4347d6d620b84a7a7f3c6de4cbfa0
Bryan McLellan <btm@opscode.com>
2012-09-13 08:27:09 -0700

    CHEF-3439: Ensure that a command line argument still wins
    When using knife ssh -a on the command line, this value should always win,
    regardless of the value in the config file or the presence of a cloud attribute.

--- chef/lib/chef/knife/ssh.rb
+++ chef/lib/chef/knife/ssh.rb
@@ -128,6 +128,8 @@ class Chef
                    # see #configure_attribute for the source of config[:attribute] and config[:override_attribute]
                    if !config[:override_attribute] && item[:cloud] and item[:cloud][:public_hostname]
                      i = item[:cloud][:public_hostname]
+                   elsif config[:override_attribute]
+                     i = format_for_display(item)[config[:override_attribute]]
                    else
                      i = format_for_display(item)[config[:attribute]]
                    end
@@ -346,7 +348,7 @@ class Chef
         # Thus we can differentiate between a config file value and a command line override at this point by checking config[:attribute]
         # We can tell here if fqdn was passed from the command line, rather than being the default, by checking config[:attribute]
         # However, after here, we cannot tell these things, so we must preserve config[:attribute]
-        config[:override_attribute] = Chef::Config[:knife][:ssh_attribute] || config[:attribute]
+        config[:override_attribute] = config[:attribute] || Chef::Config[:knife][:ssh_attribute] 
         config[:attribute] = (Chef::Config[:knife][:ssh_attribute] ||
                               config[:attribute] ||
                               "fqdn").strip
