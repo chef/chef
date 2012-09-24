b8302633024653eba1f198ebeb8f3e1c725585a1
jkeiser <jkeiser@opscode.com>
2012-09-24 13:24:24 -0700

    CHEF-3477: Don't print brackets when pretty printing nodes

--- chef/spec/unit/knife/core/ui_spec.rb
+++ chef/spec/unit/knife/core/ui_spec.rb
@@ -2,7 +2,8 @@
 # Author:: Adam Jacob (<adam@opscode.com>)
 # Author:: Tim Hinderliter (<tim@opscode.com>)
 # Author:: Daniel DeLeo (<dan@opscode.com>)
-# Copyright:: Copyright (c) 2008, 2011 Opscode, Inc.
+# Author:: John Keiser (<jkeiser@opscode.com>)
+# Copyright:: Copyright (c) 2008, 2011, 2012 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,6 +37,88 @@ describe Chef::Knife::UI do
     it "should print only the keys if --with-uri is false" do
       @ui.config[:with_uri] = false
       @ui.format_list_for_display({ :marcy => :playground }).should == [ :marcy ]
+    end
+  end
+
+  describe "output", :focus do
+    it "formats strings appropriately" do
+      @ui.output("hi")
+      @out.string.should == "hi\n"
+    end
+
+    it "formats hashes appropriately" do
+      @ui.output({'hi' => 'a', 'lo' => 'b' })
+      @out.string.should == "hi: a\nlo: b\n"
+    end
+
+    it "formats empty hashes appropriately" do
+      @ui.output({})
+      @out.string.should == "\n"
+    end
+
+    it "formats arrays appropriately" do
+      @ui.output([ 'a', 'b' ])
+      @out.string.should == "a\nb\n"
+    end
+
+    it "formats empty arrays appropriately" do
+      @ui.output([ ])
+      @out.string.should == "\n"
+    end
+
+    it "formats single-member arrays appropriately" do
+      @ui.output([ 'a' ])
+      @out.string.should == "a\n"
+    end
+
+    it "formats nested single-member arrays appropriately" do
+      @ui.output([ [ 'a' ] ])
+      @out.string.should == "a\n"
+    end
+
+    it "formats nested arrays appropriately" do
+      @ui.output([ [ 'a', 'b' ], [ 'c', 'd' ]])
+      @out.string.should == "a\nb\nc\nd\n"
+    end
+
+    it "formats nested arrays with single- and empty subarrays appropriately" do
+      @ui.output([ [ 'a', 'b' ], [ 'c' ], [], [ 'd', 'e' ]])
+      @out.string.should == "a\nb\nc\nd\ne\n"
+    end
+
+    it "formats hashes with empty array members appropriately" do
+      @ui.output({ 'a' => [], 'b' => 'c' })
+      @out.string.should == "a:\nb: c\n"
+    end
+
+    it "formats hashes with single-member array values appropriately" do
+      @ui.output({ 'a' => [ 'foo' ], 'b' => 'c' })
+      @out.string.should == "a: foo\nb: c\n"
+    end
+
+    it "formats hashes with array members appropriately" do
+      @ui.output({ 'a' => [ 'foo', 'bar' ], 'b' => 'c' })
+      @out.string.should == "a:\n  foo\n  bar\nb: c\n"
+    end
+
+    it "formats hashes with single-member nested array values appropriately" do
+      @ui.output({ 'a' => [ [ 'foo' ] ], 'b' => 'c' })
+      @out.string.should == "a:\n  foo\nb: c\n"
+    end
+
+    it "formats hashes with nested array values appropriately" do
+      @ui.output({ 'a' => [ [ 'foo', 'bar' ], [ 'baz', 'bjork' ] ], 'b' => 'c' })
+      @out.string.should == "a:\n  foo\n  bar\n  baz\n  bjork\nb: c\n"
+    end
+
+    it "formats hashes with hash values appropriately" do
+      @ui.output({ 'a' => { 'aa' => 'bb', 'cc' => 'dd' }, 'b' => 'c' })
+      @out.string.should == "a:\n  aa: bb\n  cc: dd\nb: c\n"
+    end
+
+    it "formats hashes with empty hash values appropriately" do
+      @ui.output({ 'a' => { }, 'b' => 'c' })
+      @out.string.should == "a:\nb: c\n"
     end
   end
 
@@ -89,8 +172,8 @@ describe Chef::Knife::UI do
       it "should return the URIs" do
         response = {
           "cookbook_name"=>{
-            "1.0.0" => "http://url/cookbooks/1.0.0", 
-            "2.0.0" => "http://url/cookbooks/2.0.0", 
+            "1.0.0" => "http://url/cookbooks/1.0.0",
+            "2.0.0" => "http://url/cookbooks/2.0.0",
             "3.0.0" => "http://url/cookbooks/3.0.0"}
         }
         @ui.config[:with_uri] = true
