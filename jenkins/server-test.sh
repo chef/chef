a548896f7e6fb1d60342f7d8d5420ee6488e2bd3
Seth Chisamore <schisamo@opscode.com>
2012-10-08 11:54:06 -0400

    build a smarter test script that can be reused across OSC and OPC

--- jenkins/server-test.sh
+++ jenkins/server-test.sh
@@ -5,7 +5,7 @@ export LANG=en_US.UTF-8
 set -e
 set -x
 
-echo "Attempting to remove chef-server, forcing success in case it's not installed"
+echo "Attempting to remove ${project_name}, forcing success in case it's not installed"
 
 # Check whether a command exists - returns 0 if it does, 1 if it does not
 exists() {
@@ -17,20 +17,45 @@ exists() {
   fi
 }
 
-if exists dpkg;
+usage()
+{
+  echo >&2 \
+  "usage: $0 [-p project_name]"
+}
+
+# Get command line arguments
+while getopts sv: opt
+do
+  case "$opt" in
+    p)  project_name="$OPTARG";;
+    \?) usage; exit 1;;
+  esac
+done
+
+if [ -z $projec_name ]
 then
-  sudo dpkg -P chef-server || true
-  sudo jenkins/server-killer.sh
-  sudo dpkg -i pkg/chef-server*deb
-else
-  sudo rpm -ev chef-server  || true
-  sudo jenkins/server-killer.sh
-  sudo rpm -Uvh pkg/chef-server*rpm
+  usage
+  exit 1
 fi
 
-export PATH=/opt/chef-server/bin:/opt/chef-server/embedded/bin:$PATH
-sudo chef-server-ctl reconfigure
+if exists dpkg;
+then
+  sudo dpkg -P $project_name || true
+  sudo jenkins/server-killer.sh
+  sudo dpkg -i pkg/$project_name*deb
+else
+  sudo rpm -ev $project_name || true
+  sudo jenkins/server-killer.sh
+  sudo rpm -Uvh pkg/$project_name*rpm
+fi
+
+OMNIBUS_BIN = "$(printf "%s:" /opt/*/bin)"
+OMNIBUS_EMBEDDED_BIN = "$(printf "%s:" /opt/*/embedded/bin)"
+export PATH=$OMNIBUS_BIN:OMNIBUS_EMBEDDED_BIN:$PATH
+
+sudo "${project_name}-ctl" reconfigure
 sleep 120
-sudo chef-server-ctl test -J $WORKSPACE/pedant.xml
+sudo "${project_name}-ctl" test -J $WORKSPACE/pedant.xml --all
+
 # when build succeeds, nuke the packages
 find . -type d -maxdepth 1 -mindepth 1 | xargs rm -rf
