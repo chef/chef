7d3b8310cca78acad34a7f3865ea4843d21f6cb3
Seth Falcon <seth@opscode.com>
2012-10-09 11:31:45 -0700

    Add Kevin's patched rebar that retries failed git clones

---
+++ config/software/rebar.rb
@@ -0,0 +1,36 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "rebar"
+version "retry_use_source"
+
+dependencies ["erlang"]
+
+source :git => "https://github.com/kevsmith/rebar.git"
+
+relative_path "rebar"
+
+env = {
+  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
+  "LD_FLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+build do
+  command "./bootstrap", :env => env
+  command "cp ./rebar #{install_dir}/embedded/bin/"
+end
