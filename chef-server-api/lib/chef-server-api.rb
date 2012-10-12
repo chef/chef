46c553aab06158021a51fd44c7b589ce29d54235
Seth Chisamore <schisamo@opscode.com>
2012-10-12 16:08:12 -0400

    [OC-3564] remove `chef-server-api`
    This component has been superseded by `erchef` in Chef 11+:
    https://github.com/opscode/erchef

--- chef-server-api/lib/chef-server-api.rb
+++
@@ -1,4 +0,0 @@
-require 'chef-server-api/version'
-
-CHEF_SERVER_VERSION = ChefServerApi::VERSION
-CHEF_SERVER_API_ROOT = File.expand_path(File.dirname(__FILE__) + '/../')
--- chef-server-api/lib/chef-server-api/version.rb
+++
@@ -1,3 +0,0 @@
-module ChefServerApi
-  VERSION = '11.0.0.alpha'
-end
