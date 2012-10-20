cace8de44f1d4dcf69ff27a680ba78d8e1e57a6b
Michael Saffitz <m@saffitz.com>
2012-10-20 11:17:36 -0700

    [CHEF-3543] Support configuration of ssh_user, identity_file, and host_key_verify from knife.rb for bootstrap

--- chef/lib/chef/knife/bootstrap.rb
+++ chef/lib/chef/knife/bootstrap.rb
@@ -200,13 +200,13 @@ class Chef
         ssh = Chef::Knife::Ssh.new
         ssh.ui = ui
         ssh.name_args = [ server_name, ssh_command ]
-        ssh.config[:ssh_user] = config[:ssh_user]
+        ssh.config[:ssh_user] = Chef::Config[:knife][:ssh_user] || config[:ssh_user]
         ssh.config[:ssh_password] = config[:ssh_password]
         ssh.config[:ssh_port] = Chef::Config[:knife][:ssh_port] || config[:ssh_port]
         ssh.config[:ssh_gateway] = Chef::Config[:knife][:ssh_gateway] || config[:ssh_gateway]
-        ssh.config[:identity_file] = config[:identity_file]
+        ssh.config[:identity_file] = Chef::Config[:knife][:identity_file] || config[:identity_file]
         ssh.config[:manual] = true
-        ssh.config[:host_key_verify] = config[:host_key_verify]
+        ssh.config[:host_key_verify] = Chef::Config[:knife][:host_key_verify] || config[:host_key_verify]
         ssh.config[:on_error] = :raise
         ssh
       end
