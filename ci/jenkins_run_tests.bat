1dbff12baedf3e8fca2ce3adcc4dfdbf2dd8fcdf
danielsdeleo <dan@opscode.com>
2012-11-15 14:47:42 -0800

    try running functional tests first
    ...should help avoid pollution of global state from unit tests

--- ci/jenkins_run_tests.bat
+++ ci/jenkins_run_tests.bat
@@ -2,4 +2,4 @@ set PATH=C:\Ruby192\bin;%PATH%
 
 ruby -v
 call bundle install --binstubs --path vendor/bundle || ( call rm Gemfile.lock && call bundle install --binstubs --path vendor/bundle )
-ruby bin\rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec/unit spec/functional
+ruby bin\rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec/functional spec/unit
