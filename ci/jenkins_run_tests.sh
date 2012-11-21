229606ac439e61aeb6b60649fb0a5b3f81b8f07f
danielsdeleo <dan@opscode.com>
2012-11-21 13:14:50 -0800

    don't use binstubs, overwrites real bin/ files

--- ci/jenkins_run_tests.sh
+++ ci/jenkins_run_tests.sh
@@ -5,5 +5,5 @@ export PATH=/usr/local/bin:$PATH
 ruby -v;
 # remove the Gemfile.lock and try again if bundler fails.
 # This should take care of Gemfile changes that result in "bad" bundles without forcing us to rebundle every time
-bundle install --binstubs --path vendor/bundle || ( rm Gemfile.lock && bundle install --binstubs --path vendor/bundle )
-bin/rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec;
+bundle install --path vendor/bundle || ( rm Gemfile.lock && bundle install --path vendor/bundle )
+bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec;
