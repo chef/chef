19d5a2ffd6d42a0f226555ea2cdeae79c6b42c0c
Bryan McLellan <btm@opscode.com>
2012-11-28 10:15:48 -0800

    move test.xml ci output file back up to workspace directory

--- ci/jenkins_run_tests.bat
+++ ci/jenkins_run_tests.bat
@@ -6,3 +6,4 @@ cd chef
 ruby -v
 call bundle install --binstubs --path vendor/bundle || ( call rm Gemfile.lock && call bundle install --binstubs --path vendor/bundle )
 ruby bin\rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec/functional spec/unit
+move test.xml ..
--- ci/jenkins_run_tests.sh
+++ ci/jenkins_run_tests.sh
@@ -10,3 +10,4 @@ ruby -v;
 # This should take care of Gemfile changes that result in "bad" bundles without forcing us to rebundle every time
 bundle install --binstubs --path vendor/bundle || ( rm Gemfile.lock && bundle install --binstubs --path vendor/bundle )
 bin/rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec;
+mv test.xml ..
