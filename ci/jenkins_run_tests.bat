72aee57b0360773d846c4e8e55d5c6129b70a374
Bryan McLellan <btm@loftninjas.org>
2012-11-29 17:04:35 -0500

    Preserve return code from running rspec

--- ci/jenkins_run_tests.bat
+++ ci/jenkins_run_tests.bat
@@ -6,4 +6,10 @@ cd chef
 ruby -v
 call bundle install --binstubs --path vendor/bundle || ( call rm Gemfile.lock && call bundle install --binstubs --path vendor/bundle )
 ruby bin\rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec/functional spec/unit spec/stress
+set RSPEC_ERRORLVL=%ERRORLEVEL%
+
+REM place rspec output back in jenkins working directory
 move test.xml ..
+
+REM Return the error level from rspec
+exit /B %RSPEC_ERRORLVL%
--- ci/jenkins_run_tests.sh
+++ ci/jenkins_run_tests.sh
@@ -10,4 +10,10 @@ ruby -v;
 # This should take care of Gemfile changes that result in "bad" bundles without forcing us to rebundle every time
 bundle install --binstubs --path vendor/bundle || ( rm Gemfile.lock && bundle install --binstubs --path vendor/bundle )
 bin/rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec;
+RSPEC_RETURNCODE=$?
+
+# move the rspec results back into the jenkins working directory
 mv test.xml ..
+
+# exit with the result of running rspec
+return $RSPEC_RETURNCODE
