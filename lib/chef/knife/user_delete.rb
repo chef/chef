cd7178d0ed7b7eb1df1063ee03d2946ff7d482f1
Steven Danna <steve@opscode.com>
2012-12-16 23:49:37 -0800

    Add user commands to knife.
    This commit adds the following commands to knife:
    
        knife user list
        knife user show USERNAME
        knife user create USERNAME
        knife user delete USERNAME
        knife user reregister USERNAME
    
    Note that since Chef::User objects do not contain a json_class, the
    edit object method will not work as expecated.

---
+++ lib/chef/knife/user_delete.rb
@@ -0,0 +1,46 @@
+#
+# Author:: Steven Danna (<steve@opscode.com>)
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/knife'
+
+class Chef
+  class Knife
+    class UserDelete < Knife
+
+      deps do
+        require 'chef/user'
+        require 'chef/json_compat'
+      end
+
+      banner "knife user delete USER (options)"
+
+      def run
+        @user_name = @name_args[0]
+
+        if @user_name.nil?
+          show_usage
+          ui.fatal("You must specify a user name")
+          exit 1
+        end
+
+        delete_object(Chef::User, @user_name)
+      end
+
+    end
+  end
+end
