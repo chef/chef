63f890ac668520f29332cfdd8517f2fc7eda1d78
jkeiser <jkeiser@opscode.com>
2012-12-18 22:11:50 -0800

    Fix specification to verify correct results (without actually installing gems)

--- spec/unit/knife/core/subcommand_loader_spec.rb
+++ spec/unit/knife/core/subcommand_loader_spec.rb
@@ -38,12 +38,23 @@ describe Chef::Knife::SubcommandLoader do
   end
 
   it "finds files from latest version of installed gems" do
-    gemlist = [
-        '/usr/lib/ruby/gems/knife-ec2-0.5.12/lib/chef/knife/ec2_base.rb',
-        '/usr/lib/ruby/gems/knife-ec2-0.5.10/lib/chef/knife/ec2_base.rb'
+    gems = [ double('knife-ec2-0.5.12') ]
+    gem_files = [
+      '/usr/lib/ruby/gems/knife-ec2-0.5.12/lib/chef/knife/ec2_base.rb',
+      '/usr/lib/ruby/gems/knife-ec2-0.5.12/lib/chef/knife/ec2_otherstuff.rb'
     ]
-    Gem.stub!(:find_files).and_return(gemlist) 
-    @loader.find_subcommands_via_rubygems.each {|rel_path, abs_path| abs_path.should_not match(/knife-ec2-(?!(0.5.12))/)}
+    $LOAD_PATH.should_receive(:map).and_return([])
+    if Gem::Specification.respond_to? :latest_specs
+      Gem::Specification.should_receive(:latest_specs).and_return(gems)
+      gems[0].should_receive(:matches_for_glob).with('chef/knife/*.rb{,.rb,.bundle}').and_return(gem_files)
+    else
+      Gem.source_index.should_receive(:latest_specs).and_return(gems)
+      gems[0].should_receive(:require_paths).twice.and_return(['lib'])
+      gems[0].should_receive(:full_gem_path).and_return('/usr/lib/ruby/gems/knife-ec2-0.5.12')
+      Dir.should_receive(:[]).with('/usr/lib/ruby/gems/knife-ec2-0.5.12/lib/chef/knife/*.rb').and_return(gem_files)
+    end
+    @loader.should_receive(:find_subcommands_via_dirglob).and_return({})
+    @loader.find_subcommands_via_rubygems.values.select { |file| file =~ /knife-ec2/ }.sort.should == gem_files
   end
 
   it "finds files using a dirglob when rubygems is not available" do
