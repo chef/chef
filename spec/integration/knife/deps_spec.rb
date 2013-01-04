a0e4d02c49054a383790e3fd36a88a3ba35cb142
jkeiser <jkeiser@opscode.com>
2013-01-04 09:12:57 -0800

    Make deps report error when file does not exist

--- spec/integration/knife/deps_spec.rb
+++ spec/integration/knife/deps_spec.rb
@@ -9,7 +9,7 @@ describe 'knife deps' do
     when_the_chef_server 'has a role with no run_list' do
       role 'starring', {}
       it 'knife deps reports no dependencies' do
-        knife('deps', '--remote', '/roles/starring.json').stdout.should == "/roles/starring.json\n"
+        knife('deps --remote /roles/starring.json').should_succeed "/roles/starring.json\n"
       end
     end
 
@@ -19,11 +19,12 @@ describe 'knife deps' do
       cookbook 'quiche', '1.0.0', { 'metadata.rb' => '', 'recipes' => { 'default.rb' => '' } }
       cookbook 'soup', '1.0.0', { 'metadata.rb' => '', 'recipes' => { 'chicken.rb' => '' } }
       it 'knife deps reports all dependencies' do
-        knife('deps', '--remote', '/roles/starring.json').stdout.should == "/roles/minor.json
+        knife('deps --remote /roles/starring.json').should_succeed <<EOM
+/roles/minor.json
 /cookbooks/quiche
 /cookbooks/soup
 /roles/starring.json
-"
+EOM
       end
     end
 
@@ -33,25 +34,26 @@ describe 'knife deps' do
       cookbook 'quiche', '1.0.0', { 'metadata.rb' => '', 'recipes' => { 'default.rb' => '' } }
       cookbook 'soup', '1.0.0', { 'metadata.rb' => '', 'recipes' => { 'chicken.rb' => '' } }
       it 'knife deps reports all dependencies' do
-        knife('deps', '--remote', '/roles/starring.json').stdout.should == "/roles/minor.json
+        knife('deps --remote /roles/starring.json').should_succeed <<EOM
+/roles/minor.json
 /cookbooks/quiche
 /cookbooks/soup
 /roles/starring.json
-"
+EOM
       end
     end
 
     when_the_chef_server 'has a node with no environment or run_list' do
       node 'mort', {}
       it 'knife deps reports just the node and _default environment' do
-        knife('deps', '--remote', '--repo-mode=everything', '/nodes/mort.json').stdout.should == "/environments/_default.json\n/nodes/mort.json\n"
+        knife('deps --remote --repo-mode=everything /nodes/mort.json').should_succeed "/environments/_default.json\n/nodes/mort.json\n"
       end
     end
     when_the_chef_server 'has a node with an environment' do
       environment 'desert', {}
       node 'mort', { 'chef_environment' => 'desert' }
       it 'knife deps reports just the node' do
-        knife('deps', '--remote', '--repo-mode=everything', '/nodes/mort.json').stdout.should == "/environments/desert.json\n/nodes/mort.json\n"
+        knife('deps --remote --repo-mode=everything /nodes/mort.json').should_succeed "/environments/desert.json\n/nodes/mort.json\n"
       end
     end
     when_the_chef_server 'has a node with roles and recipes in its run_list' do
@@ -60,37 +62,38 @@ describe 'knife deps' do
       cookbook 'soup', '1.0.0', { 'metadata.rb' => '', 'recipes' => { 'chicken.rb' => '' } }
       node 'mort', { 'run_list' => %w(role[minor] recipe[quiche] recipe[soup::chicken]) }
       it 'knife deps reports just the node' do
-        knife('deps', '--remote', '--repo-mode=everything', '/nodes/mort.json').stdout.should == "/environments/_default.json
+        knife('deps --remote --repo-mode=everything /nodes/mort.json').should_succeed <<EOM
+/environments/_default.json
 /roles/minor.json
 /cookbooks/quiche
 /cookbooks/soup
 /nodes/mort.json
-"
+EOM
       end
     end
     when_the_chef_server 'has a cookbook with no dependencies' do
       cookbook 'quiche', '1.0.0', { 'metadata.rb' => '', 'recipes' => { 'default.rb' => '' } }
       it 'knife deps reports just the cookbook' do
-        knife('deps', '--remote', '/cookbooks/quiche').stdout.should == "/cookbooks/quiche\n"
+        knife('deps --remote /cookbooks/quiche').should_succeed "/cookbooks/quiche\n"
       end
     end
     when_the_chef_server 'has a cookbook with dependencies' do
       cookbook 'kettle', '1.0.0', { 'metadata.rb' => '' }
       cookbook 'quiche', '1.0.0', { 'metadata.rb' => 'depends "kettle"', 'recipes' => { 'default.rb' => '' } }
       it 'knife deps reports just the cookbook' do
-        knife('deps', '--remote', '/cookbooks/quiche').stdout.should == "/cookbooks/kettle\n/cookbooks/quiche\n"
+        knife('deps --remote /cookbooks/quiche').should_succeed "/cookbooks/kettle\n/cookbooks/quiche\n"
       end
     end
     when_the_chef_server 'has a data bag' do
       data_bag 'bag', { 'item' => {} }
       it 'knife deps reports just the data bag' do
-        knife('deps', '--remote', '/data_bags/bag/item.json').stdout.should == "/data_bags/bag/item.json\n"
+        knife('deps --remote /data_bags/bag/item.json').should_succeed "/data_bags/bag/item.json\n"
       end
     end
     when_the_chef_server 'has an environment' do
       environment 'desert', {}
       it 'knife deps reports just the environment' do
-        knife('deps', '--remote', '/environments/desert.json').stdout.should == "/environments/desert.json\n"
+        knife('deps --remote /environments/desert.json').should_succeed "/environments/desert.json\n"
       end
     end
     when_the_chef_server 'has a deep dependency tree' do
@@ -103,16 +106,18 @@ describe 'knife deps' do
       node 'bart', { 'run_list' => [ 'role[minor]' ] }
 
       it 'knife deps reports all dependencies' do
-        knife('deps', '--remote', '--repo-mode=everything', '/nodes/mort.json').stdout.should == "/environments/desert.json
+        knife('deps --remote --repo-mode=everything /nodes/mort.json').should_succeed <<EOM
+/environments/desert.json
 /roles/minor.json
 /cookbooks/quiche
 /cookbooks/soup
 /roles/starring.json
 /nodes/mort.json
-"
+EOM
       end
       it 'knife deps * reports all dependencies of all things' do
-        knife('deps', '--remote', '--repo-mode=everything', '/nodes/*').stdout.should == "/environments/_default.json
+        knife('deps --remote --repo-mode=everything /nodes/*').should_succeed <<EOM
+/environments/_default.json
 /roles/minor.json
 /nodes/bart.json
 /environments/desert.json
@@ -120,10 +125,11 @@ describe 'knife deps' do
 /cookbooks/soup
 /roles/starring.json
 /nodes/mort.json
-"
+EOM
       end
       it 'knife deps a b reports all dependencies of a and b' do
-        knife('deps', '--remote', '--repo-mode=everything', '/nodes/bart.json', '/nodes/mort.json').stdout.should == "/environments/_default.json
+        knife('deps --remote --repo-mode=everything /nodes/bart.json /nodes/mort.json').should_succeed <<EOM
+/environments/_default.json
 /roles/minor.json
 /nodes/bart.json
 /environments/desert.json
@@ -131,10 +137,11 @@ describe 'knife deps' do
 /cookbooks/soup
 /roles/starring.json
 /nodes/mort.json
-"
+EOM
       end
       it 'knife deps --tree /* shows dependencies in a tree' do
-        knife('deps', '--remote', '--tree', '--repo-mode=everything', '/nodes/*').stdout.should == "/nodes/bart.json
+        knife('deps --remote --tree --repo-mode=everything /nodes/*').should_succeed <<EOM
+/nodes/bart.json
   /environments/_default.json
   /roles/minor.json
 /nodes/mort.json
@@ -143,16 +150,17 @@ describe 'knife deps' do
     /roles/minor.json
     /cookbooks/quiche
     /cookbooks/soup
-"
+EOM
       end
       it 'knife deps --tree --no-recurse shows only the first level of dependencies' do
-        knife('deps', '--remote', '--tree', '--no-recurse', '--repo-mode=everything', '/nodes/*').stdout.should == "/nodes/bart.json
+        knife('deps --remote --tree --no-recurse --repo-mode=everything /nodes/*').should_succeed <<EOM
+/nodes/bart.json
   /environments/_default.json
   /roles/minor.json
 /nodes/mort.json
   /environments/desert.json
   /roles/starring.json
-"
+EOM
       end
     end
 
@@ -163,16 +171,22 @@ describe 'knife deps' do
         cookbook 'baz', '1.0.0', { 'metadata.rb' => 'depends "foo"' }
         cookbook 'self', '1.0.0', { 'metadata.rb' => 'depends "self"' }
         it 'knife deps prints each once' do
-          knife('deps', '--remote', '/cookbooks/foo', '/cookbooks/self') do
-            stdout.should == "/cookbooks/baz\n/cookbooks/bar\n/cookbooks/foo\n/cookbooks/self\n"
-            stderr.should == "WARNING: No knife configuration file found\n"
-          end
+          knife('deps --remote /cookbooks/foo /cookbooks/self').should_succeed <<EOM
+/cookbooks/baz
+/cookbooks/bar
+/cookbooks/foo
+/cookbooks/self
+EOM
         end
         it 'knife deps --tree prints each once' do
-          knife('deps', '--remote', '--tree', '/cookbooks/foo', '/cookbooks/self') do
-            stdout.should == "/cookbooks/foo\n  /cookbooks/bar\n    /cookbooks/baz\n      /cookbooks/foo\n/cookbooks/self\n  /cookbooks/self\n"
-            stderr.should == "WARNING: No knife configuration file found\n"
-          end
+          knife('deps --remote --tree /cookbooks/foo /cookbooks/self').should_succeed <<EOM
+/cookbooks/foo
+  /cookbooks/bar
+    /cookbooks/baz
+      /cookbooks/foo
+/cookbooks/self
+  /cookbooks/self
+EOM
         end
       end
       when_the_chef_server 'has roles with circular dependencies' do
@@ -181,13 +195,15 @@ describe 'knife deps' do
         role 'baz', { 'run_list' => [ 'role[foo]' ] }
         role 'self', { 'run_list' => [ 'role[self]' ] }
         it 'knife deps prints each once' do
-          knife('deps', '--remote', '/roles/foo.json', '/roles/self.json') do
-            stdout.should == "/roles/baz.json\n/roles/bar.json\n/roles/foo.json\n/roles/self.json\n"
-            stderr.should == "WARNING: No knife configuration file found\n"
-          end
+          knife('deps --remote /roles/foo.json /roles/self.json').should_succeed <<EOM
+/roles/baz.json
+/roles/bar.json
+/roles/foo.json
+/roles/self.json
+EOM
         end
         it 'knife deps --tree prints each once' do
-          knife('deps', '--remote', '--tree', '/roles/foo.json', '/roles/self.json') do
+          knife('deps --remote --tree /roles/foo.json /roles/self.json') do
             stdout.should == "/roles/foo.json\n  /roles/bar.json\n    /roles/baz.json\n      /roles/foo.json\n/roles/self.json\n  /roles/self.json\n"
             stderr.should == "WARNING: No knife configuration file found\n"
           end
@@ -198,111 +214,106 @@ describe 'knife deps' do
     context 'missing objects' do
       when_the_chef_server 'is empty' do
         it 'knife deps /blah reports an error' do
-          knife('deps', '--remote', '/blah') do
-            stdout.should == "/blah\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /blah: No such file or directory\n"
-          end
+          knife('deps --remote /blah').should_fail(
+            :exit_code => 2,
+            :stdout => "/blah\n",
+            :stderr => "ERROR: /blah: No such file or directory\n"
+          )
         end
         it 'knife deps /roles/x.json reports an error' do
-          knife('deps', '--remote', '/roles/x.json') do
-            stdout.should == "/roles/x.json\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /roles/x.json: No such file or directory\n"
-          end
+          knife('deps --remote /roles/x.json').should_fail(
+            :exit_code => 2,
+            :stdout => "/roles/x.json\n",
+            :stderr => "ERROR: /roles/x.json: No such file or directory\n"
+          )
         end
         it 'knife deps /nodes/x.json reports an error' do
-          knife('deps', '--remote', '--repo-mode=everything', '/nodes/x.json') do
-            stdout.should == "/nodes/x.json\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /nodes/x.json: No such file or directory\n"
-          end
+          knife('deps --remote --repo-mode=everything /nodes/x.json').should_fail(
+            :exit_code => 2,
+            :stdout => "/nodes/x.json\n",
+            :stderr => "ERROR: /nodes/x.json: No such file or directory\n"
+          )
         end
         it 'knife deps /environments/x.json reports an error' do
-          knife('deps', '--remote', '/environments/x.json') do
-            stdout.should == "/environments/x.json\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /environments/x.json: No such file or directory\n"
-          end
+          knife('deps --remote /environments/x.json').should_fail(
+            :exit_code => 2,
+            :stdout => "/environments/x.json\n",
+            :stderr => "ERROR: /environments/x.json: No such file or directory\n"
+          )
         end
         it 'knife deps /cookbooks/x reports an error' do
-          knife('deps', '--remote', '/cookbooks/x') do
-            stdout.should == "/cookbooks/x\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /cookbooks/x: No such file or directory\n"
-          end
+          knife('deps --remote /cookbooks/x').should_fail(
+            :exit_code => 2,
+            :stdout => "/cookbooks/x\n",
+            :stderr => "ERROR: /cookbooks/x: No such file or directory\n"
+          )
         end
         it 'knife deps /data_bags/bag/item reports an error' do
-          knife('deps', '--remote', '/data_bags/bag/item') do
-            stdout.should == "/data_bags/bag/item\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /data_bags/bag/item: No such file or directory\n"
-          end
+          knife('deps --remote /data_bags/bag/item').should_fail(
+            :exit_code => 2,
+            :stdout => "/data_bags/bag/item\n",
+            :stderr => "ERROR: /data_bags/bag/item: No such file or directory\n"
+          )
         end
       end
       when_the_chef_server 'is missing a dependent cookbook' do
         role 'starring', { 'run_list' => [ 'recipe[quiche]'] }
         it 'knife deps reports the cookbook, along with an error' do
-          knife('deps', '--remote', '/roles/starring.json') do
-            stdout.should == "/cookbooks/quiche\n/roles/starring.json\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /cookbooks/quiche: No such file or directory\n"
-          end
+          knife('deps --remote /roles/starring.json').should_fail(
+            :exit_code => 2,
+            :stdout => "/cookbooks/quiche\n/roles/starring.json\n",
+            :stderr => "ERROR: /cookbooks/quiche: No such file or directory\n"
+          )
         end
       end
       when_the_chef_server 'is missing a dependent environment' do
         node 'mort', { 'chef_environment' => 'desert' }
         it 'knife deps reports the environment, along with an error' do
-          knife('deps', '--remote', '--repo-mode=everything', '/nodes/mort.json') do
-            stdout.should == "/environments/desert.json\n/nodes/mort.json\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /environments/desert.json: No such file or directory\n"
-          end
+          knife('deps --remote --repo-mode=everything /nodes/mort.json').should_fail(
+            :exit_code => 2,
+            :stdout => "/environments/desert.json\n/nodes/mort.json\n",
+            :stderr => "ERROR: /environments/desert.json: No such file or directory\n"
+          )
         end
       end
       when_the_chef_server 'is missing a dependent role' do
         role 'starring', { 'run_list' => [ 'role[minor]'] }
         it 'knife deps reports the role, along with an error' do
-          knife('deps', '--remote', '/roles/starring.json') do
-            stdout.should == "/roles/minor.json\n/roles/starring.json\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /roles/minor.json: No such file or directory\n"
-          end
+          knife('deps --remote /roles/starring.json').should_fail(
+            :exit_code => 2,
+            :stdout => "/roles/minor.json\n/roles/starring.json\n",
+            :stderr => "ERROR: /roles/minor.json: No such file or directory\n"
+          )
         end
       end
     end
     context 'invalid objects' do
       when_the_chef_server 'is empty' do
         it 'knife deps / reports an error' do
-          knife('deps', '--remote', '/') do
-            stdout.should == "/\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: / is not a Chef object!\n"
-          end
+          knife('deps --remote /').should_succeed("/\n")
         end
         it 'knife deps /roles reports an error' do
-          knife('deps', '--remote', '/roles') do
-            stdout.should == "/roles\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /roles is not a Chef object!\n"
-          end
+          knife('deps --remote /roles').should_succeed("/roles\n")
         end
       end
       when_the_chef_server 'has a data bag' do
         data_bag 'bag', { 'item' => {} }
-        it 'knife deps /data_bags/bag returns an error' do
-          knife('deps', '--remote', '/data_bags/bag') do
-            stdout.should == "/data_bags/bag\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /data_bags/bag is not a Chef object!\n"
-          end
+        it 'knife deps /data_bags/bag shows no dependencies' do
+          knife('deps --remote /data_bags/bag').should_succeed("/data_bags/bag\n")
         end
       end
       when_the_chef_server 'has a cookbook' do
         cookbook 'blah', '1.0.0', { 'metadata.rb' => '' }
-        it 'knife deps on a cookbook file returns an error' do
-          knife('deps', '--remote', '/cookbooks/blah/metadata.rb') do
-            stdout.should == "/cookbooks/blah/metadata.rb\n"
-            stderr.should == "WARNING: No knife configuration file found\nERROR: /cookbooks/blah/metadata.rb is not a Chef object!\n"
-          end
+        it 'knife deps on a cookbook file shows no dependencies' do
+          knife('deps --remote /cookbooks/blah/metadata.rb').should_succeed(
+            "/cookbooks/blah/metadata.rb\n"
+          )
         end
       end
     end
 
     it 'knife deps --no-recurse reports an error' do
-      knife('deps', '--no-recurse', '/') do
-        stdout.should == ""
-        stderr.should == "WARNING: No knife configuration file found\nERROR: --no-recurse requires --tree\n"
-        exit_code.should == 1
-      end
+      knife('deps --no-recurse /').should_fail("ERROR: --no-recurse requires --tree\n")
     end
   end
 end
--- spec/support/shared/integration/knife_support.rb
+++ spec/support/shared/integration/knife_support.rb
@@ -20,10 +20,19 @@ module KnifeSupport
     end
 
     def exit_code
-      @exit_code
+      @exit_code || 0
     end
 
     def run(*args, &block)
+      # Allow knife('role from file roles/blah.json') rather than requiring the
+      # arguments to be split like knife('role', 'from', 'file', 'roles/blah.json')
+      # If any argument will have actual spaces in it, the long form is required.
+      # (Since knife commands always start with the command name, and command
+      # names with spaces are always multiple args, this is safe.)
+      if args.length == 1
+        args = args[0].split(/\s+/)
+      end
+
       # This is Chef::Knife.run without load_commands and load_deps--we'll
       # load stuff ourselves, thank you very much
       @stdout = StringIO.new
@@ -39,12 +48,53 @@ module KnifeSupport
         # Don't print stuff
         Chef::Config[:verbosity] = 0
         instance.configure_chef
-        @exit_code = instance.run
+        instance.run
+
+      # This is how rspec catches exit()
       rescue SystemExit => e
         @exit_code = e.status
       end
 
       instance_eval(&block) if block
+    end
+
+    def should_fail(*args)
+      expected = {}
+      args.each do |arg|
+        if arg.is_a?(Hash)
+          expected.merge!(arg)
+        elsif arg.is_a?(Integer)
+          expected[:exit_code] = arg
+        else
+          expected[:stderr] = arg
+        end
+      end
+      expected[:exit_code] = 1 if !expected[:exit_code]
+      should_result_in(expected)
+    end
+
+    def should_succeed(*args)
+      expected = {}
+      args.each do |arg|
+        if arg.is_a?(Hash)
+          expected.merge!(arg)
+        else
+          expected[:stdout] = arg
+        end
+      end
+      should_result_in(expected)
+    end
+
+    def should_result_in(expected)
+      expected[:stdout] = '' if !expected[:stdout]
+      expected[:stderr] = '' if !expected[:stderr]
+      expected[:exit_code] = 0 if !expected[:exit_code]
+      # TODO make this go away
+      stderr_actual = stderr.sub(/^WARNING: No knife configuration file found\n/, '')
+
+      stdout.should == expected[:stdout]
+      stderr_actual.should == expected[:stderr]
+      exit_code.should == expected[:exit_code]
     end
   end
 end
