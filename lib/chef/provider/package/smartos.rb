1e990e1aee66d16df7d8add20de69ea15499bbc1
sawanoboly <sawanoboriyu@higanworks.com>
2013-01-08 12:03:58 +0900

    CHEF-3364 Fix up Smartos Package provider. Copied from https://github.com/benr/chef/tree/19c83ab362dab4931b50b7dbe322b0e44b4e64e3

--- lib/chef/provider/package/smartos.rb
+++ lib/chef/provider/package/smartos.rb
@@ -2,6 +2,7 @@
 # Authors:: Trevor O (trevoro@joyent.com)
 #           Bryan McLellan (btm@loftninjas.org)
 #           Matthew Landauer (matthew@openaustralia.org)
+#           Ben Rockwood (benr@joyent.com)
 # Copyright:: Copyright (c) 2009 Bryan McLellan, Matthew Landauer
 # License:: Apache License, Version 2.0
 #
@@ -17,10 +18,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Notes
-#
-#  * Supports installing using a local package name
-#  * Otherwise reverts to installing from the pkgsrc repositories URL
 
 require 'chef/provider/package'
 require 'chef/mixin/shell_out'
@@ -36,47 +33,61 @@ class Chef
 
 
         def load_current_resource
-					Chef::Log.debug("#{@new_resource} loading current resource")
-					@current_resource = Chef::Resource::Package.new(@new_resource.name)
-					@current_resource.package_name(@new_resource.package_name)
-					@current_resource.version(nil)
+          Chef::Log.debug("#{@new_resource} loading current resource")
+          @current_resource = Chef::Resource::Package.new(@new_resource.name)
+          @current_resource.package_name(@new_resource.package_name)
+          @current_resource.version(nil)
           check_package_state(@new_resource.package_name)
-					@current_resource # modified by check_package_state
-				end
-				
-				def check_package_state(name)
-					Chef::Log.debug("#{@new_resource} checking package #{name}")
-					# XXX
-					version = nil
-					info = shell_out!("pkg_info -E \"#{name}*\"", :env => nil, :returns => [0,1])
-					
-					if info.stdout
-						version = info.stdout[/^#{@new_resource.package_name}-(.+)/, 1]
+          @current_resource # modified by check_package_state
+        end
+
+        def check_package_state(name)
+          Chef::Log.debug("#{@new_resource} checking package #{name}")
+          version = nil
+          info = shell_out!("/opt/local/sbin/pkg_info -E \"#{name}*\"", :env => nil, :returns => [0,1])
+
+          if info.stdout
+            version = info.stdout[/^#{@new_resource.package_name}-(.+)/, 1]
           end
 
-					if !version
-						@current_resource.version(nil)
-					else
-						@current_resource.version(version)
-					end
+          if !version
+            @current_resource.version(nil)
+          else
+            @current_resource.version(version)
+          end
         end
 
+        def candidate_version
+          return @candidate_version if @candidate_version
+          name = nil
+          version = nil
+          pkg = shell_out!("/opt/local/bin/pkgin se #{new_resource.package_name}", :env => nil, :returns => [0,1])
+          pkg.stdout.each_line do |line|
+            case line
+            when /^#{name}/
+              name, version = line.split[0].split(/-([^-]+)$/)
+            end
+          end
+          @candidate_version = version
+          version
+        end
+     
         def install_package(name, version)
-					Chef::Log.debug("#{@new_resource} installing package #{name}-#{version}")
-					package = "#{name}-#{version}"
-          out = shell_out!("pkgin -y install #{package}", :env => nil)
+          Chef::Log.debug("#{@new_resource} installing package #{name} version #{version}")
+          package = "#{name}-#{version}"
+          out = shell_out!("/opt/local/bin/pkgin -y install #{package}", :env => nil)
         end
 
-				def upgrade_package(name, version)
-					Chef::Log.debug("#{@new_resource} upgrading package #{name}-#{version}")
-					install_package(name, version)
-				end
+        def upgrade_package(name, version)
+          Chef::Log.debug("#{@new_resource} upgrading package #{name} version #{version}")
+          install_package(name, version)
+        end
 
-				def remove_package(name, version)
-					Chef::Log.debug("#{@new_resource} removing package #{name}-#{version}")
-					package = "#{name}-#{version}"
-          out = shell_out!("pkgin -y remove #{package}", :env => nil)
-				end
+        def remove_package(name, version)
+          Chef::Log.debug("#{@new_resource} removing package #{name} version #{version}") 
+          package = "#{name}"
+          out = shell_out!("/opt/local/bin/pkgin -y remove #{package}", :env => nil)
+        end
 
       end
     end
