7fef814215a782cf5202066099052c9fa7f9c5bb
jkeiser <jkeiser@opscode.com>
2013-01-16 14:19:42 -0800

    Make knife diff error out when there are no matches

--- lib/chef/knife/diff.rb
+++ lib/chef/knife/diff.rb
@@ -34,10 +34,18 @@ class Chef
         patterns = pattern_args_from(name_args.length > 0 ? name_args : [ "" ])
 
         # Get the matches (recursively)
+        error = false
         patterns.each do |pattern|
-          Chef::ChefFS::CommandLine.diff_print(pattern, chef_fs, local_fs, config[:recurse] ? nil : 1, output_mode, proc { |entry| format_path(entry) } ) do |diff|
+          found_match = Chef::ChefFS::CommandLine.diff_print(pattern, chef_fs, local_fs, config[:recurse] ? nil : 1, output_mode, proc { |entry| format_path(entry) } ) do |diff|
             stdout.print diff
           end
+          if !found_match
+            ui.error "#{pattern}: No such file or directory on remote or local"
+            error = true
+          end
+        end
+        if error
+          exit 1
         end
       end
     end
