1ab881fa492a8707e48084562294ef0bcf1eb9c3
jkeiser <jkeiser@opscode.com>
2013-01-16 11:01:41 -0800

    Make knife show exit with error code on error

--- lib/chef/knife/show.rb
+++ lib/chef/knife/show.rb
@@ -1,5 +1,6 @@
 require 'chef/chef_fs/knife'
 require 'chef/chef_fs/file_system'
+require 'chef/chef_fs/file_system/not_found_error'
 
 class Chef
   class Knife
@@ -15,20 +16,29 @@ class Chef
 
       def run
         # Get the matches (recursively)
+        error = false
         pattern_args.each do |pattern|
           Chef::ChefFS::FileSystem.list(config[:local] ? local_fs : chef_fs, pattern) do |result|
             if result.dir?
-              ui.error "#{result.path_for_printing}: is a directory" if pattern.exact_path
+              ui.error "#{format_path(result)}: is a directory" if pattern.exact_path
+              error = true
             else
               begin
                 value = result.read
-                output "#{result.path_for_printing}:"
+                output "#{format_path(result)}:"
                 output(format_for_display(value))
+              rescue Chef::ChefFS::FileSystem::OperationNotAllowedError => e
+                ui.error "#{format_path(e.entry)}: #{e.reason}."
+                error = true
               rescue Chef::ChefFS::FileSystem::NotFoundError => e
-                ui.error "#{e.entry.path_for_printing}: No such file or directory"
+                ui.error "#{format_path(e.entry)}: No such file or directory"
+                error = true
               end
             end
           end
+        end
+        if error
+          exit 1
         end
       end
     end
