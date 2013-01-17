5fc6ccc2d5a48a61c9d8abef1cead59fc0f4557d
jkeiser <jkeiser@opscode.com>
2013-01-17 15:44:34 -0800

    Protect output against difference in hash key order

--- lib/chef/chef_fs/file_system/cookbooks_dir.rb
+++ lib/chef/chef_fs/file_system/cookbooks_dir.rb
@@ -33,7 +33,7 @@ class Chef
         end
 
         def children
-          @children ||= rest.get_rest(api_path).map { |key, value| CookbookDir.new(key, self, value) }
+          @children ||= rest.get_rest(api_path).map { |key, value| CookbookDir.new(key, self, value) }.sort_by { |c| c.name }
         end
 
         def create_child_from(other)
--- lib/chef/chef_fs/file_system/data_bags_dir.rb
+++ lib/chef/chef_fs/file_system/data_bags_dir.rb
@@ -34,7 +34,7 @@ class Chef
 
         def children
           begin
-            @children ||= rest.get_rest(api_path).keys.map do |entry|
+            @children ||= rest.get_rest(api_path).keys.sort.map do |entry|
               DataBagDir.new(entry, self, true)
             end
           rescue Net::HTTPServerException
