a63fe9db2756bc258448a32d07a658ed09046d4e
jkeiser <jkeiser@opscode.com>
2013-01-17 17:32:17 -0800

    Fix issue where pwd cookbook path not detected correctly in Ruby 1.8.7

--- lib/chef/chef_fs/knife.rb
+++ lib/chef/chef_fs/knife.rb
@@ -125,7 +125,7 @@ class Chef
             realest_path = Chef::ChefFS::PathUtils.realest_path(path)
             if absolute_path[0,realest_path.length] == realest_path &&
               (absolute_path.length == realest_path.length ||
-                absolute_path[realest_path.length] =~ /#{PathUtils.regexp_path_separator}/)
+                absolute_path[realest_path.length,1] =~ /#{PathUtils.regexp_path_separator}/)
               relative_path = Chef::ChefFS::PathUtils::relative_to(absolute_path, realest_path)
               return relative_path == '.' ? "/#{name}" : "/#{name}/#{relative_path}"
             end
