696de2d38dc58e70f21325df065ee0da9b5e07b5
danielsdeleo <dan@opscode.com>
2013-01-18 14:13:00 -0800

    missed a version.rb in version change

--- chef/lib/chef/version.rb
+++ chef/lib/chef/version.rb
@@ -17,7 +17,7 @@
 
 class Chef
   CHEF_ROOT = File.dirname(File.expand_path(File.dirname(__FILE__)))
-  VERSION = '10.18.1'
+  VERSION = '10.18.2'
 end
 
 # NOTE: the Chef::Version class is defined in version_class.rb
