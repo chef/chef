ffc93ff39aa340107e93b95d77fa3643b9384da3
danielsdeleo <dan@opscode.com>
2013-01-17 16:33:37 -0800

    rm accidentally merged 10.x ci script changes

--- ci/jenkins_run_tests.sh
+++ ci/jenkins_run_tests.sh
@@ -9,8 +9,5 @@ bundle install --path vendor/bundle || ( rm Gemfile.lock && bundle install --pat
 bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec;
 RSPEC_RETURNCODE=$?
 
-# move the rspec results back into the jenkins working directory
-mv test.xml ..
-
 # exit with the result of running rspec
 exit $RSPEC_RETURNCODE
