54a28ff3336b08e83ae7f2a6903c7a7f97bb4b78
John Keiser <john@johnkeiser.com>
2013-01-18 21:17:16 -0800

    Use cross-platform method for terminal width

--- lib/chef/knife/list.rb
+++ lib/chef/knife/list.rb
@@ -1,5 +1,6 @@
 require 'chef/chef_fs/knife'
 require 'chef/chef_fs/file_system'
+require 'highline'
 
 class Chef
   class Knife
@@ -115,8 +116,7 @@ class Chef
         if config[:one_column] || !stdout.isatty
           columns = 0
         else
-          # TODO: tput cols is not cross platform
-          columns = Integer(`tput cols`)
+          columns = HighLine::SystemExtensions.terminal_size[0]
         end
         current_line = ''
         results.each do |result|
