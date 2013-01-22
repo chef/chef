c2f9ff4d3bdb7ead885ebe238ebf7d611ab99a20
Jesse Campbell <hikeit@gmail.com>
2013-01-22 13:07:41 -0500

    add attribution for ftp

--- chef/lib/chef/provider/remote_file.rb
+++ chef/lib/chef/provider/remote_file.rb
@@ -1,5 +1,6 @@
 #
 # Author:: Adam Jacob (<adam@opscode.com>)
+# Author:: Jesse Campbell (<hikeit@gmail.com>)
 # Copyright:: Copyright (c) 2008 Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
