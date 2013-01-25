96836778d8387264da061b611f8c9fbc3ba73b79
Josh Mahowald <jmahowald@angel.com>
2013-01-24 23:16:11 -0600

    Creating new version so don't conflict

--- chef/lib/chef/version.rb
+++ chef/lib/chef/version.rb
@@ -17,7 +17,7 @@
 
 class Chef
   CHEF_ROOT = File.dirname(File.expand_path(File.dirname(__FILE__)))
-  VERSION = '10.18.2'
+  VERSION = '10.18.3'
 end
 
 # NOTE: the Chef::Version class is defined in version_class.rb
