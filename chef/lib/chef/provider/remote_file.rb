e24fb8d7f0627269a724c8e2281fae8955ebf947
Jesse Campbell <hikeit@gmail.com>
2013-02-04 19:51:25 -0500

    Clean up remote_file

--- chef/lib/chef/provider/remote_file.rb
+++ chef/lib/chef/provider/remote_file.rb
@@ -19,10 +19,9 @@
 
 require 'chef/provider/file'
 require 'rest_client'
+require 'chef/provider/remote_file/ftp'
 require 'uri'
 require 'tempfile'
-require 'net/https'
-require 'net/ftp'
 
 class Chef
   class Provider
@@ -43,9 +42,11 @@ class Chef
           if URI::HTTP === uri
             #HTTP or HTTPS
             raw_file = RestClient::Request.execute(:method => :get, :url => @new_resource.source, :raw_response => true).file
-          else
+          elsif URI::FTP === uri
             #FTP
-            raw_file = ftp_fetch(uri)
+            raw_file = FTP::fetch(uri, @new_resource.ftp_active_mode)
+          else
+            raise "Invalid uri. Only http(s) and ftp are currently supported"
           end
           if matches_current_checksum?(raw_file)
             Chef::Log.debug "#{@new_resource} target and source checksums are the same - not updating"
@@ -93,57 +94,6 @@ class Chef
           Chef::Log.debug "#{@new_resource} checksum changed from #{@current_resource.checksum} to #{@new_resource.checksum}"
           backup @new_resource.path
         end
-      end
-
-      private
-
-      def ftp_fetch(uri)
-        # Shamelessly stolen from open-uri
-        # Fetches the file using Net::FTP, returning a Tempfile
-        path = uri.path
-        path = path.sub(%r{\A/}, '%2F') # re-encode the beginning slash because uri library decodes it.
-        directories = path.split(%r{/}, -1)
-        directories.each {|d|
-          d.gsub!(/%([0-9A-Fa-f][0-9A-Fa-f])/) { [$1].pack("H2") }
-        }
-        unless filename = directories.pop
-          raise ArgumentError, "no filename: #{uri.inspect}"
-        end
-        directories.each {|d|
-          if /[\r\n]/ =~ d
-            raise ArgumentError, "invalid directory: #{d.inspect}"
-          end
-        }
-        if /[\r\n]/ =~ filename
-          raise ArgumentError, "invalid filename: #{filename.inspect}"
-        end 
-        typecode = uri.typecode
-        if typecode && /\A[aid]\z/ !~ typecode
-          raise ArgumentError, "invalid typecode: #{typecode.inspect}"
-        end
-
-        tempfile = Tempfile.new(filename)
-
-        # The access sequence is defined by RFC 1738
-        ftp = Net::FTP.new
-        ftp.connect(uri.hostname, uri.port)
-        ftp.passive = true if !@new_resource.ftp_active_mode
-        # todo: extract user/passwd from .netrc.
-        user = 'anonymous'
-        passwd = nil
-        user, passwd = uri.userinfo.split(/:/) if uri.userinfo
-        ftp.login(user, passwd)
-        directories.each {|cwd|
-          ftp.voidcmd("CWD #{cwd}")
-        }
-        if typecode
-          # xxx: typecode D is not handled.
-          ftp.voidcmd("TYPE #{typecode.upcase}")
-        end
-        ftp.getbinaryfile(filename, tempfile.path)
-        ftp.close
-
-        tempfile
       end
     end
   end
