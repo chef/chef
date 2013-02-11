7863204c9bac7a4ac094d365738dac05ea297e70
Xabier de Zuazo <xabier@onddo.com>
2013-02-11 14:31:46 +0100

    [CHEF-3029] source directive using full path (required to work properly)

--- lib/chef/provider/ifconfig/debian.rb
+++ lib/chef/provider/ifconfig/debian.rb
@@ -60,7 +60,7 @@ iface <%= @new_resource.device %> inet static
           converge_by ('modify configuration file : /etc/network/interfaces') do
             Dir.mkdir('/etc/network/interfaces.d') unless ::File.directory?('/etc/network/interfaces.d')
             conf = Chef::Util::FileEdit.new('/etc/network/interfaces')
-            conf.insert_line_if_no_match('^\s*source\s+interfaces[.]d/[*]\s*$', 'source interfaces.d/*')
+            conf.insert_line_if_no_match('^\s*source\s+/etc/network/interfaces[.]d/[*]\s*$', 'source /etc/network/interfaces.d/*')
             conf.write_file
           end
         end
