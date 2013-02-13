8c3d3f1549ce06f8b37009735e37d7cfbc9687ec
Bryan McLellan <btm@opscode.com>
2013-02-13 07:51:57 -0800

    Don't install server deps when testing client
    dep_selector requires gecode installed for a build environment

--- ci/jenkins_run_tests.bat
+++ ci/jenkins_run_tests.bat
@@ -4,7 +4,7 @@ REM Chef 10 contains the client in the chef/ sub-directory
 cd chef
 
 ruby -v
-call bundle install --binstubs --without docgen --path vendor/bundle || ( call rm Gemfile.lock && call bundle install --binstubs --path vendor/bundle )
+call bundle install --binstubs --without docgen server --path vendor/bundle || ( call rm Gemfile.lock && call bundle install --binstubs --path vendor/bundle )
 ruby bin\rspec -r rspec_junit_formatter -f RspecJunitFormatter -o test.xml -f documentation spec/functional spec/unit spec/stress
 set RSPEC_ERRORLVL=%ERRORLEVEL%
 
