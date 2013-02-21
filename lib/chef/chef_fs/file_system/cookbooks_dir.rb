8f8c02dfa5927c74e2cb31017b76bb2543a7dd53
Ho-Sheng Hsiao <hosh@opscode.com>
2013-02-21 14:02:08 -0800

    [COOKBOOKS] Added symlinked proxy hack
     - Rather than backporting CHEF-3307, we add a hack here to make versioned cookbook work
     - When cookbooks_dir attempts to upload a versioned cookbook, it will:
       (1) Create a temporary directory
       (2) Symlink a canonical cookbook name to the versioned cookbook. ( /tmp/x -> /path/cookbooks/x-1.0.0 )
       (3) Upload the proxy
       (4) Remove the temporary directory
    
    This makes the versioned cookbook compatible with Chef 11-stable and 10-stable.

--- lib/chef/chef_fs/file_system/cookbooks_dir.rb
+++ lib/chef/chef_fs/file_system/cookbooks_dir.rb
@@ -19,6 +19,8 @@
 require 'chef/chef_fs/file_system/rest_list_dir'
 require 'chef/chef_fs/file_system/cookbook_dir'
 
+require 'tmpdir'
+
 class Chef
   module ChefFS
     module FileSystem
@@ -66,15 +68,30 @@ class Chef
           upload_cookbook_from(other)
         end
 
+
         def upload_cookbook_from(other)
           other_cookbook_version = other.chef_object
           # TODO this only works on the file system.  And it can't be broken into
           # pieces.
           begin
-            uploader = Chef::CookbookUploader.new(other_cookbook_version, other.parent.file_path, :rest => rest)
+            if Chef::Config[:versioned_cookbooks]
+              cookbook_name = Chef::ChefFS::FileSystem::ChefRepositoryFileSystemEntry.canonical_cookbook_name(other.name)
+
+              # TODO: Find some better way to express this
+              # We want to symlink a cookbook with a non-versioend cookbook name in a temporary
+              # directory, and then upload it. This allows us to use Chef 10-stable and 11-stable.
+              proxy_cookbook_path = make_proxy_cookbook_dir(cookbook_name, other.file_path )
+              proxy_loader = Chef::Cookbook::CookbookVersionLoader.new("#{proxy_cookbook_path}/#{cookbook_name}", other.parent.chefignore)
+              proxy_loader.load_cookbooks
+              uploader = Chef::CookbookUploader.new(proxy_loader.cookbook_version, proxy_cookbook_path, :rest => rest)
+              actual_cookbook_path = proxy_cookbook_path
+            else
+              uploader = Chef::CookbookUploader.new(other_cookbook_version, other.parent.file_path, :rest => rest)
+              actual_cookbook_path = other.parent.file_path
+            end
             # Work around the fact that CookbookUploader doesn't understand chef_repo_path (yet)
             old_cookbook_path = Chef::Config.cookbook_path
-            Chef::Config.cookbook_path = other.parent.file_path if !Chef::Config.cookbook_path
+            Chef::Config.cookbook_path = actual_cookbook_path if !Chef::Config.cookbook_path
             begin
               if uploader.respond_to?(:upload_cookbook)
                 uploader.upload_cookbook
@@ -82,6 +99,7 @@ class Chef
                 uploader.upload_cookbooks
               end
             ensure
+              FileUtils.remove_entry_secure proxy_cookbook_path if Chef::Config[:versioned_cookbooks]
               Chef::Config.cookbook_path = old_cookbook_path
             end
           rescue Net::HTTPServerException => e
@@ -93,6 +111,19 @@ class Chef
             else
               raise
             end
+          end
+        end
+
+        # Knife currently does not understand versioned cookbooks
+        # Cookbook Version uploader also requires a lot of refactoring
+        # to make this work. So instead, we make a temporary cookbook
+        # symlinking back to real cookbook, and upload the proxy.
+        #
+        # This will create a temporary dir, symlink, and return the temporary
+        # dir
+        def make_proxy_cookbook_dir(cookbook_name, cookbook_path)
+          Dir.mktmpdir.tap do |dir|
+            File.symlink cookbook_path, "#{dir}/#{cookbook_name}"
           end
         end
 
