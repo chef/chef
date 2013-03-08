4e5c6f9d4003011019134007bb0dea77d047082e
Kannan Manickam <arangamani.kannan@gmail.com>
2013-03-08 00:05:20 -0800

    removed duplicate for push in resource_collection

--- lib/chef/resource_collection.rb
+++ lib/chef/resource_collection.rb
@@ -54,6 +54,9 @@ class Chef
       end
     end
 
+    # 'push' is an alias method to <<
+    alias_method :push, :<<
+     
     def insert(resource)
       is_chef_resource(resource)
       if @insert_after_idx
@@ -70,14 +73,6 @@ class Chef
       else
         @resources << resource
         @resources_by_name[resource.to_s] = @resources.length - 1
-      end
-    end
-
-    def push(*args)
-      args.flatten.each do |arg|
-        is_chef_resource(arg)
-        @resources.push(arg)
-        @resources_by_name[arg.to_s] = @resources.length - 1
       end
     end
 
