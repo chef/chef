84b7eb2acffa3c256d9d3e32d9f4e45ef0e0281c
jkeiser <jkeiser@opscode.com>
2013-03-14 10:50:37 -0700

    Adjust to new arguments to ChefServerRootDir.new

--- spec/unit/chef_fs/file_system/chef_server_root_dir_spec.rb
+++ spec/unit/chef_fs/file_system/chef_server_root_dir_spec.rb
@@ -144,8 +144,9 @@ describe Chef::ChefFS::FileSystem::ChefServerRootDir do
     {
       :chef_server_url => 'url',
       :node_name => 'username',
-      :client_key => 'key'
-    }, 'everything')
+      :client_key => 'key',
+      :repo_mode => 'everything'
+    })
   }
 
   before(:each) do
