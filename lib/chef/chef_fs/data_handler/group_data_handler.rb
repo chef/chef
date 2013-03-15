c7726a0453183098d276010483e6c6cf1acdfd4e
jkeiser <jkeiser@opscode.com>
2013-03-14 20:22:49 -0700

    Fix groups PUT, make groups download prettier

--- lib/chef/chef_fs/data_handler/group_data_handler.rb
+++ lib/chef/chef_fs/data_handler/group_data_handler.rb
@@ -10,14 +10,30 @@ class Chef
             'name' => remove_dot_json(entry.name),
             'groupname' => remove_dot_json(entry.name),
             'users' => [],
-            'actors' => [],
             'clients' => [],
             'groups' => [],
           }
           if entry.org
             defaults['orgname'] = entry.org
           end
-          super(group, defaults)
+          result = super(group, defaults)
+          if result['actors'] && result['actors'].sort.uniq == (result['users'] + result['clients']).sort.uniq
+            result.delete('actors')
+          end
+          result
+        end
+
+        def normalize_for_put(group, entry)
+          result = super(group, entry)
+          result['actors'] = {
+            'users' => result['users'],
+            'clients' => result['clients'],
+            'groups' => result['groups']
+          }
+          result.delete('users')
+          result.delete('clients')
+          result.delete('groups')
+          result
         end
 
         def preserve_key(key)
