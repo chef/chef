b8caad872f930ffccf12a079ac5809f11607b6fc
Lamont Granquist <lamont@opscode.com>
2013-03-15 17:35:49 -0700

    windows fix for drive letters

--- lib/chef/provider/remote_file.rb
+++ lib/chef/provider/remote_file.rb
@@ -53,7 +53,7 @@ class Chef
             raw_file.close!
           else
             Chef::Log.info("#{@new_resource} downloaded from #{raw_file_source}")
-            description = [] 
+            description = []
             description << "copy file downloaded from #{raw_file_source} into #{@new_resource.path}"
             description << diff_current(raw_file.path)
             converge_by(description) do
@@ -64,7 +64,7 @@ class Chef
               save_fileinfo(raw_file_source)
             end
             # whyrun mode cleanup - the temp file will never be used,
-            # so close/unlink it here. 
+            # so close/unlink it here.
             if whyrun_mode?
               raw_file.close!
             end
@@ -157,9 +157,13 @@ class Chef
         return raw_file
       end
 
+      def cache_path
+        cache_path = new_resource.name.sub(/^([A-Za-z]:)/, "")  # strip drive letter on Windows
+      end
+
       def load_fileinfo
         begin
-          Chef::JSONCompat.from_json(Chef::FileCache.load("remote_file/#{new_resource.name}"))
+          Chef::JSONCompat.from_json(Chef::FileCache.load("remote_file/#{cache_path}"))
         rescue Chef::Exceptions::FileNotFound
           nil
         end
@@ -171,7 +175,7 @@ class Chef
         cache["last_modified"] = @new_resource.last_modified
         cache["src"] = source
         cache["checksum"] = @new_resource.checksum
-        Chef::FileCache.store("remote_file/#{new_resource.name}", cache.to_json)
+        Chef::FileCache.store("remote_file/#{cache_path}", cache.to_json)
       end
     end
   end
