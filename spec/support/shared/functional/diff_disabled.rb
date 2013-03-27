eca7dbdf852d75c176593b66d5dcaf0fe517a87a
danielsdeleo <dan@opscode.com>
2013-03-27 10:54:55 -0700

    forcibly turn diffing back on after diff tests

--- spec/support/shared/functional/diff_disabled.rb
+++ spec/support/shared/functional/diff_disabled.rb
@@ -1,11 +1,10 @@
 
 shared_context "diff disabled"  do
   before do
-    @original_diff_disable ||= Chef::Config[:diff_disabled]
     Chef::Config[:diff_disabled] = true
   end
 
   after do
-    Chef::Config[:diff_disabled] = @original_diff_disable
+    Chef::Config[:diff_disabled] = false
   end
 end
