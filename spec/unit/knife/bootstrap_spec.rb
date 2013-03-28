9953894b5c789bdd0a4e2938bcc19647daf16e62
Chirag Jog <chirag.jog@gmail.com>
2013-03-28 13:52:28 +1100

    Review comments for better readability

--- spec/unit/knife/bootstrap_spec.rb
+++ spec/unit/knife/bootstrap_spec.rb
@@ -181,16 +181,16 @@ describe Chef::Knife::Bootstrap do
         and @knife.options[:use_sudo_password][:long].to_s.should_not == ''
     end
 
-    it "should enable password to sudo from stdin for knife ssh bootstrap" do
+    it "uses the password from --ssh-password for sudo when --sudo-use-password is set" do
       @knife.config[:use_sudo] = true
       @knife.config[:use_sudo_password] = true
-      @knife.ssh_command.include?("echo #{@knife.config[:ssh_password]} | sudo -S").should == true
+      @knife.ssh_command.should include("echo #{@knife.config[:ssh_password]} | sudo -S")
     end
 
-    it "should not enable password to sudo from stdin for knife ssh bootstrap" do
+    it "should not honor --use-sudo-password when --use-sudo is not set" do
       @knife.config[:use_sudo] = false
       @knife.config[:use_sudo_password] = true
-      @knife.ssh_command.include?("echo #{@knife.config[:ssh_password]} | sudo -S").should == false
+      @knife.ssh_command.should_not include("echo #{@knife.config[:ssh_password]} | sudo -S")
     end
 
     context "from the knife config file" do
