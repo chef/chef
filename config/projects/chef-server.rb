259c672c77151564fbe1b27ea861dc16d0932e67
Christopher Maier <cm@opscode.com>
2013-04-18 13:41:09 -0400

    Update project definitions to latest version of DSL

--- config/projects/chef-server.rb
+++ config/projects/chef-server.rb
@@ -16,42 +16,40 @@
 #
 
 name "chef-server"
+maintainer "Opscode, Inc."
+homepage "http://www.opscode.com"
 
 replaces        "chef-server"
 install_path    "/opt/chef-server"
 build_version   Omnibus::BuildVersion.new.semver
-build_iteration "1"
-
-deps = []
+build_iteration 1
 
 # global
-deps << "chef-gem" # for embedded chef-solo
-deps << "preparation" # creates required build directories
-deps << "chef-server-cookbooks" # used by chef-server-ctl reconfigure
-deps << "chef-server-scripts" # assorted scripts used by installed instance
-deps << "chef-server-ctl" # additional project-specific chef-server-ctl subcommands
-deps << "nginx" # load balacning
-deps << "runit"
-deps << "unicorn" # serves up Rack apps (chef-server-webui)
+dependency "chef-gem" # for embedded chef-solo
+dependency "preparation" # creates required build directories
+dependency "chef-server-cookbooks" # used by chef-server-ctl reconfigure
+dependency "chef-server-scripts" # assorted scripts used by installed instance
+dependency "chef-server-ctl" # additional project-specific chef-server-ctl subcommands
+dependency "nginx" # load balacning
+dependency "runit"
+dependency "unicorn" # serves up Rack apps (chef-server-webui)
 
 # the backend
-deps << "postgresql"
-deps << "rabbitmq"
-deps << "chef-solr"
-deps << "chef-expander"
-deps << "bookshelf" # S3 API compatible object store
+dependency "postgresql"
+dependency "rabbitmq"
+dependency "chef-solr"
+dependency "chef-expander"
+dependency "bookshelf" # S3 API compatible object store
 
 # the front-end services
-deps << "erchef" # the actual Chef Server REST API
-deps << "chef-server-webui"
+dependency "erchef" # the actual Chef Server REST API
+dependency "chef-server-webui"
 
 # integration testing
-deps << "chef-pedant" # test ALL THE THINGS!
+dependency "chef-pedant" # test ALL THE THINGS!
 
 # version manifest file
-deps << "version-manifest"
-
-dependencies deps
+dependency "version-manifest"
 
 exclude "\.git*"
 exclude "bundler\/git"
