f350b01cd0e2e743a970e8e1c9680d4ddf72279b
adamedx <adamed@opscode.com>
2013-05-03 09:10:43 -0700

    Address Win2k8 issues, extra output to test mixlib-shellout

--- spec/functional/win32/versions_spec.rb
+++ spec/functional/win32/versions_spec.rb
@@ -26,14 +26,39 @@ describe "Chef::ReservedNames::Win32::Version", :windows_only do
   before do
     host = WMI::Win32_OperatingSystem.find(:first)
 
+    @version = Chef::ReservedNames::Win32::Version.new
+
+    is_win2k8 = false
+  
+    os_version = host.send('Version')
+
+    # The operating system version is a string in the following form
+    # that can be split into components based on the '.' delimiter:
+    # MajorVersionNumber.MinorVersionNumber.BuildNumber
+    os_version_components = os_version.split('.')
+
+    if os_version_components.length < 2
+      raise 'WMI returned a Windows version from Win32_OperatingSystem.Version ' +
+        'with an unexpected format. The Windows version could not be determined.'
+    end
+
+    # Windows 6.0 is Windows Server 2008, so test the major and
+    # minor version components
+    is_win2k8 = os_version_components[0] == '6' && os_version_components[1] == '0'
+
     # Ensure that the OS name returned by WMI does not have extended
     # characters like the registered trademark that get mapped to
     # the character 'r' -- this is an issue for Win2k8 only.
     # This gets normalized below
-    @current_os_version_canonicalized = host.caption.gsub("Serverr", "Server")
+    if ! is_win2k8
+      @current_os_version_canonicalized = host.caption
+    else
+      @current_os_version_canonicalized = 'Windows Server 2008'
+    end
 
-    @version = Chef::ReservedNames::Win32::Version.new
+    `gem contents 'mixlib-shellout'`
   end
+
   context "Windows Operating System version" do
     it "should match the version from WMI", :not_supported_on_win2k3 do
       puts "WMI: #{@current_os_version_canonicalized} Resource: #{@version.marketing_name}"
