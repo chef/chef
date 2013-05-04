ab5c57b998745661f64d46cb4e46c5d4f5db6641
adamedx <adamed@opscode.com>
2013-05-04 10:59:17 -0700

    Version library fixes for Win2k3 non-existent GetProductInfo API, misplaced GetSystemMetrics API calls, irregular characters in Win2k3, Win2k8 WMI test validation

--- spec/functional/win32/versions_spec.rb
+++ spec/functional/win32/versions_spec.rb
@@ -24,13 +24,49 @@ end
 
 describe "Chef::ReservedNames::Win32::Version", :windows_only do
   before do
+
     host = WMI::Win32_OperatingSystem.find(:first)
-    @current_os_version = host.caption
+
+    # Use WMI to determine current OS version.
+    # On Win2k8R2 and later, we can dynamically obtain marketing
+    # names for comparison from WMI so the test should not
+    # need to be modified when new Windows releases arise.
+    # For Win2k3 and Win2k8, we use static names in this test
+    # based on the version number information from WMI.
+    if Chef::Platform::windows_server_2003?
+      @current_os_version = 'Windows Server 2003 R2'
+    elsif is_windows_server_2008?(host)
+      @current_os_version = 'Windows Server 2008'
+    else
+      @current_os_version = host.caption      
+    end
+
     @version = Chef::ReservedNames::Win32::Version.new
   end
+
   context "Windows Operating System version" do
     it "should match the version from WMI" do
       @current_os_version.include?(@version.marketing_name).should == true
     end
+  end
+ 
+  def is_windows_server_2008?(wmi_host)
+    is_win2k8 = false
+    
+    os_version = wmi_host.send('Version')
+
+    # The operating system version is a string in the following form
+    # that can be split into components based on the '.' delimiter:
+    # MajorVersionNumber.MinorVersionNumber.BuildNumber
+    os_version_components = os_version.split('.')
+
+    if os_version_components.length < 2
+      raise 'WMI returned a Windows version from Win32_OperatingSystem.Version ' +
+        'with an unexpected format. The Windows version could not be determined.'
+    end
+
+    # Windows 6.0 is Windows Server 2008, so test the major and
+    # minor version components
+    is_win2k8 = os_version_components[0] == '6' && os_version_components[1] == '0'
   end
 end
