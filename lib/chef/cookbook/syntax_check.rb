5144cf38f43e82778f9fb55b9ffb1c86e6436baa
Greg Thornton <xdissent@me.com>
2013-05-23 18:02:59 -0500

    Use cookbook_path accessor instead of instance variable in syntax_check

--- lib/chef/cookbook/syntax_check.rb
+++ lib/chef/cookbook/syntax_check.rb
@@ -104,13 +104,13 @@ class Chef
       end
 
       def chefignore
-        @chefignore ||= Chefignore.new(File.dirname(@cookbook_path))
+        @chefignore ||= Chefignore.new(File.dirname(cookbook_path))
       end
 
       def remove_ignored_files(file_list)
         return file_list unless chefignore.ignores.length > 0
         file_list.reject do |full_path|
-          cookbook_pn = Pathname.new @cookbook_path
+          cookbook_pn = Pathname.new cookbook_path
           full_pn = Pathname.new full_path
           relative_pn = full_pn.relative_path_from cookbook_pn
           chefignore.ignored? relative_pn.to_s
