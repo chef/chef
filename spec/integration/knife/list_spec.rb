jkeiser/knife-essentials

Add "knife list" tests

spec/integration/list_spec.rb
@@ -1,0 +1,104 @@
+require 'support/integration_helper'
+require 'chef/knife/list_essentials'
+
+describe 'knife list' do
+  extend IntegrationSupport
+  include KnifeSupport
+
+  when_the_chef_server "is empty" do
+    it "knife list / returns all top level directories" do
+      knife('list', '/').stdout.should == "/cookbooks
+/data_bags
+/environments
+/roles
+"
+    end
+
+    it "knife list -R / returns everything" do
+      knife('list', '-R', '/').stdout.should == "/:
+cookbooks
+data_bags
+environments
+roles
+
+/cookbooks:
+
+/data_bags:
+
+/environments:
+_default.json
+
+/roles:
+"
+    end
+  end
+
+  when_the_chef_server "has plenty of stuff in it" do
+    client 'client1', {}
+    client 'client2', {}
+    cookbook 'cookbook1', '1.0.0', { 'metadata.rb' => '' }
+    cookbook 'cookbook2', '1.0.1', { 'metadata.rb' => '', 'recipes' => { 'default.rb' => '' } }
+    data_bag 'bag1', { 'item1' => {}, 'item2' => {} }
+    data_bag 'bag2', { 'item1' => {}, 'item2' => {} }
+    environment 'environment1', {}
+    environment 'environment2', {}
+    node 'node1', {}
+    node 'node2', {}
+    role 'role1', {}
+    role 'role2', {}
+    user 'user1', {}
+    user 'user2', {}
+
+    it "knife list / returns all top level directories" do
+      knife('list', '/').stdout.should == "/cookbooks
+/data_bags
+/environments
+/roles
+"
+    end
+
+    it "knife list -R / returns everything" do
+      knife('list', '-R', '/').stdout.should == "/:
+cookbooks
+data_bags
+environments
+roles
+
+/cookbooks:
+cookbook1
+cookbook2
+
+/cookbooks/cookbook1:
+metadata.rb
+
+/cookbooks/cookbook2:
+metadata.rb
+recipes
+
+/cookbooks/cookbook2/recipes:
+default.rb
+
+/data_bags:
+bag1
+bag2
+
+/data_bags/bag1:
+item1.json
+item2.json
+
+/data_bags/bag2:
+item1.json
+item2.json
+
+/environments:
+_default.json
+environment1.json
+environment2.json
+
+/roles:
+role1.json
+role2.json
+"
+    end
+  end
+end

spec/support/integration_helper.rb
@@ -1,0 +1,124 @@
+#
+# Author:: John Keiser (<jkeiser@opscode.com>)
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'tmpdir'
+require 'fileutils'
+require 'chef_zero/server'
+require 'support/knife_support'
+require 'support/spec_helper'
+
+require 'chef/knife/delete_essentials'
+require 'chef/knife/deps_essentials'
+require 'chef/knife/diff_essentials'
+require 'chef/knife/download_essentials'
+require 'chef/knife/list_essentials'
+require 'chef/knife/raw_essentials'
+require 'chef/knife/show_essentials'
+require 'chef/knife/upload_essentials'
+
+module IntegrationSupport
+  def when_the_repository(description, contents, &block)
+    context "When the local repository #{description}" do
+      before :each do
+        raise "Can only create one directory per test" if @repository_dir
+        @repository_dir = Dir.mktmpdir('chef_repo')
+        create_child_files(@repository_dir, contents)
+        Chef::Config.chef_repo_path = @repository_dir
+      end
+
+      after :each do
+        if @repository_dir
+          FileUtils.remove_directory_secure(@repository_dir)
+          @repository_dir = nil
+        end
+      end
+
+      instance_eval(&block)
+    end
+  end
+
+  def when_the_chef_server(description, &block)
+    context "When the Chef server #{description}" do
+      before :each do
+        raise "Attempt to create multiple servers in one test" if @server
+        # Set up configuration so that clients will point to the server
+        Thin::Logging.silent = true
+        @server = ChefZero::Server.new(:port => 8889)
+        Chef::Config.chef_server_url = @server.url
+        Chef::Config.node_name = 'admin'
+        Chef::Config.client_key = File.join(File.dirname(__FILE__), 'stickywicket.pem')
+
+        # Start the server
+        @server.start_background
+      end
+
+      def self.client(name, client)
+        before(:each) { @server.load_data({ 'clients' => { name => client }}) }
+      end
+
+      def self.cookbook(name, version, cookbook)
+        before(:each) { @server.load_data({ 'cookbooks' => { "#{name}-#{version}" => cookbook }}) }
+      end
+
+      def self.data_bag(name, data_bag)
+        before(:each) { @server.load_data({ 'data' => { name => data_bag }}) }
+      end
+
+      def self.environment(name, environment)
+        before(:each) { @server.load_data({ 'environments' => { name => environment }}) }
+      end
+
+      def self.node(name, node)
+        before(:each) { @server.load_data({ 'nodes' => { name => node }}) }
+      end
+
+      def self.role(name, role)
+        before(:each) { @server.load_data({ 'roles' => { name => role }}) }
+      end
+
+      def self.user(name, user)
+        before(:each) { @server.load_data({ 'users' => { name => user }}) }
+      end
+
+      after :each do
+        if @server
+          @server.stop
+          @server = nil
+        end
+      end
+
+      instance_eval(&block)
+    end
+  end
+
+  private
+
+  def create_child_files(real_dir, contents)
+    contents.each_pair do |entry_name, entry_contents|
+      entry_path = File.join(real_dir, entry_name)
+      if entry_contents.is_a? Hash
+        Dir.mkdir(entry_path)
+        create_child_files(entry_path, entry_contents)
+      else
+        File.open(entry_path, "w") do |file|
+          file.write(entry_contents)
+        end
+      end
+    end
+  end
+end

chef/chef

spec/integration/knife/list_spec.rb
@@ -1,0 +1,104 @@
+require 'support/shared/integration/integration_helper'
+require 'chef/knife/list'
+
+describe 'knife list' do
+  extend IntegrationSupport
+  include KnifeSupport
+
+  when_the_chef_server "is empty" do
+    it "knife list / returns all top level directories" do
+      knife('list', '/').stdout.should == "/cookbooks
+/data_bags
+/environments
+/roles
+"
+    end
+
+    it "knife list -R / returns everything" do
+      knife('list', '-R', '/').stdout.should == "/:
+cookbooks
+data_bags
+environments
+roles
+
+/cookbooks:
+
+/data_bags:
+
+/environments:
+_default.json
+
+/roles:
+"
+    end
+  end
+
+  when_the_chef_server "has plenty of stuff in it" do
+    client 'client1', {}
+    client 'client2', {}
+    cookbook 'cookbook1', '1.0.0', { 'metadata.rb' => '' }
+    cookbook 'cookbook2', '1.0.1', { 'metadata.rb' => '', 'recipes' => { 'default.rb' => '' } }
+    data_bag 'bag1', { 'item1' => {}, 'item2' => {} }
+    data_bag 'bag2', { 'item1' => {}, 'item2' => {} }
+    environment 'environment1', {}
+    environment 'environment2', {}
+    node 'node1', {}
+    node 'node2', {}
+    role 'role1', {}
+    role 'role2', {}
+    user 'user1', {}
+    user 'user2', {}
+
+    it "knife list / returns all top level directories" do
+      knife('list', '/').stdout.should == "/cookbooks
+/data_bags
+/environments
+/roles
+"
+    end
+
+    it "knife list -R / returns everything" do
+      knife('list', '-R', '/').stdout.should == "/:
+cookbooks
+data_bags
+environments
+roles
+
+/cookbooks:
+cookbook1
+cookbook2
+
+/cookbooks/cookbook1:
+metadata.rb
+
+/cookbooks/cookbook2:
+metadata.rb
+recipes
+
+/cookbooks/cookbook2/recipes:
+default.rb
+
+/data_bags:
+bag1
+bag2
+
+/data_bags/bag1:
+item1.json
+item2.json
+
+/data_bags/bag2:
+item1.json
+item2.json
+
+/environments:
+_default.json
+environment1.json
+environment2.json
+
+/roles:
+role1.json
+role2.json
+"
+    end
+  end
+end

spec/support/shared/integration/integration_helper.rb
@@ -1,0 +1,124 @@
+#
+# Author:: John Keiser (<jkeiser@opscode.com>)
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'tmpdir'
+require 'fileutils'
+require 'chef_zero/server'
+require 'support/shared/integration/knife_support'
+require 'spec_helper'
+
+require 'chef/knife/delete'
+require 'chef/knife/deps'
+require 'chef/knife/diff'
+require 'chef/knife/download'
+require 'chef/knife/list'
+require 'chef/knife/raw'
+require 'chef/knife/show'
+require 'chef/knife/upload'
+
+module IntegrationSupport
+  def when_the_repository(description, contents, &block)
+    context "When the local repository #{description}" do
+      before :each do
+        raise "Can only create one directory per test" if @repository_dir
+        @repository_dir = Dir.mktmpdir('chef_repo')
+        create_child_files(@repository_dir, contents)
+        Chef::Config.chef_repo_path = @repository_dir
+      end
+
+      after :each do
+        if @repository_dir
+          FileUtils.remove_directory_secure(@repository_dir)
+          @repository_dir = nil
+        end
+      end
+
+      instance_eval(&block)
+    end
+  end
+
+  def when_the_chef_server(description, &block)
+    context "When the Chef server #{description}" do
+      before :each do
+        raise "Attempt to create multiple servers in one test" if @server
+        # Set up configuration so that clients will point to the server
+        Thin::Logging.silent = true
+        @server = ChefZero::Server.new(:port => 8889)
+        Chef::Config.chef_server_url = @server.url
+        Chef::Config.node_name = 'admin'
+        Chef::Config.client_key = File.join(File.dirname(__FILE__), 'stickywicket.pem')
+
+        # Start the server
+        @server.start_background
+      end
+
+      def self.client(name, client)
+        before(:each) { @server.load_data({ 'clients' => { name => client }}) }
+      end
+
+      def self.cookbook(name, version, cookbook)
+        before(:each) { @server.load_data({ 'cookbooks' => { "#{name}-#{version}" => cookbook }}) }
+      end
+
+      def self.data_bag(name, data_bag)
+        before(:each) { @server.load_data({ 'data' => { name => data_bag }}) }
+      end
+
+      def self.environment(name, environment)
+        before(:each) { @server.load_data({ 'environments' => { name => environment }}) }
+      end
+
+      def self.node(name, node)
+        before(:each) { @server.load_data({ 'nodes' => { name => node }}) }
+      end
+
+      def self.role(name, role)
+        before(:each) { @server.load_data({ 'roles' => { name => role }}) }
+      end
+
+      def self.user(name, user)
+        before(:each) { @server.load_data({ 'users' => { name => user }}) }
+      end
+
+      after :each do
+        if @server
+          @server.stop
+          @server = nil
+        end
+      end
+
+      instance_eval(&block)
+    end
+  end
+
+  private
+
+  def create_child_files(real_dir, contents)
+    contents.each_pair do |entry_name, entry_contents|
+      entry_path = File.join(real_dir, entry_name)
+      if entry_contents.is_a? Hash
+        Dir.mkdir(entry_path)
+        create_child_files(entry_path, entry_contents)
+      else
+        File.open(entry_path, "w") do |file|
+          file.write(entry_contents)
+        end
+      end
+    end
+  end
+end