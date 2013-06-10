fad0c545f1fc23692651d15a8c379ebb1aa75fcb
danielsdeleo <dan@opscode.com>
2013-06-10 08:25:21 -0700

    remove space between method call and parens
    Ruby emits a warning for code like `some_method (args)`.

--- spec/functional/resource/template_spec.rb
+++ spec/functional/resource/template_spec.rb
@@ -192,7 +192,7 @@ describe Chef::Resource::Template do
 
     include_context "diff disabled"
 
-    let (:expected_content) {
+    let(:expected_content) {
       "Template rendering libraries\r\nshould support\r\ndifferent line endings\r\n\r\n"
     }
 
