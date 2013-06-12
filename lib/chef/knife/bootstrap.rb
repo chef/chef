4b581e08ac9ace36fc673b298de51aa633f2d3b1
Adam DePue <adepue@hearsaycorp.com>
2013-06-12 23:02:42 +0000

    Single ticks are required to escape passwords with special characters in them

--- lib/chef/knife/bootstrap.rb
+++ lib/chef/knife/bootstrap.rb
@@ -244,7 +244,7 @@ class Chef
         command = render_template(read_template)
 
         if config[:use_sudo]
-          command = config[:use_sudo_password] ? "echo #{config[:ssh_password]} | sudo -S #{command}" : "sudo #{command}"
+          command = config[:use_sudo_password] ? "echo '#{config[:ssh_password]}' | sudo -S #{command}" : "sudo #{command}"
         end
 
         command
