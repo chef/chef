0ece12ff7d0a0b6aa4d048bc2c288c200da59016
John Keiser <jkeiser@opscode.com>
2013-06-13 07:20:10 -0700

    Save children after creating

--- lib/chef/chef_fs/file_system/chef_repository_file_system_root_dir.rb
+++ lib/chef/chef_fs/file_system/chef_repository_file_system_root_dir.rb
@@ -53,7 +53,9 @@ class Chef
           child_paths[name].each do |path|
             Dir.mkdir(path)
           end
-          make_child_entry(name)
+          child = make_child_entry(name)
+          @children << child
+          child
         end
 
         def json_class
