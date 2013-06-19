fa7c906d1b4a34720cdde2fc4ef5551727aa9c00
John Keiser <jkeiser@opscode.com>
2013-06-18 19:06:53 -0600

    Fix environment_path default from Chef 11.6

--- lib/chef/chef_fs/config.rb
+++ lib/chef/chef_fs/config.rb
@@ -171,6 +171,9 @@ class Chef
         if @chef_config[:role_path] == @chef_config.platform_specific_path('/var/chef/roles')
           @chef_config[:role_path] = nil
         end
+        if @chef_config[:environment_path] == @chef_config.platform_specific_path('/var/chef/environments')
+          @chef_config[:environment_path] = nil
+        end
 
         # Infer chef_repo_path from cookbook_path if not speciifed
         if !@chef_config[:chef_repo_path]
