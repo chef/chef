0b8b6349ed5dc7f88134693922e95e8294129261
kaustubh-d <kaustubh@clogeny.com>
2013-07-01 02:22:51 -0700

    add cron tests for cron attributes and negative tests.

--- spec/functional/provider/cron_spec.rb
+++ spec/functional/provider/cron_spec.rb
@@ -47,4 +47,79 @@ describe "Chef Provider for Cron" do
       @provider.cron_exists.should be_false
     end
   end
+
+  describe "testcase 3: create action with various attributes" do
+    def create_and_validate
+      @provider.load_current_resource
+      @provider.cron_exists.should be_false
+      @provider.run_action(:create)
+      # Verify if the cron is created successfully
+      @provider.load_current_resource
+      @provider.cron_exists.should be_true
+    end
+
+    def validate_cron_attribute(attribute, expected_value)
+      # Test if the attribute exists as attribute or on command
+      current_resource = @provider.current_resource
+      new_val = current_resource.send(attribute.to_sym)
+      if new_val
+        expect(new_val).to eql(expected_value)
+      else
+        # command should have attribute value set
+        expect(current_resource.command).to include(attribute.upcase)
+        expect(current_resource.command).to include(expected_value)
+      end
+    end
+
+    after do
+      @provider.load_current_resource
+      @provider.run_action(:delete)
+      # Verify if the cron is deleted successfully
+      @provider.load_current_resource
+      @provider.cron_exists.should be_false
+    end
+
+    it "should create a crontab entry for mailto attribute" do
+      @new_resource.mailto "cheftest@example.com"
+      create_and_validate
+      validate_cron_attribute("mailto", "cheftest@example.com")
+    end
+
+    it "should create a crontab entry for path attribute" do
+      @new_resource.path "/usr/local/bin"
+      create_and_validate
+      validate_cron_attribute("path", "/usr/local/bin")
+    end
+
+    it "should create a crontab entry for shell attribute" do
+      @new_resource.shell "/bin/bash"
+      create_and_validate
+      validate_cron_attribute("shell", "/bin/bash")
+    end
+
+    it "should create a crontab entry for home attribute" do
+      @new_resource.home "/home/opscode"
+      create_and_validate
+      validate_cron_attribute("home", "/home/opscode")
+    end
+  end
+
+  describe "testcase 4: negative tests for create action" do
+    def create_and_validate
+      @provider.load_current_resource
+      @provider.cron_exists.should be_false
+      expect { @provider.run_action(:create) }.to raise_error(Chef::Exceptions::Cron, "Error updating state of #{@new_resource.name}, exit: 1")
+    end
+
+    it "should not create cron with invalid minute" do
+      @new_resource.minute "invalid"
+      create_and_validate
+    end
+
+    it "should not create cron with invalid user" do
+      @new_resource.user "1-really-really-invalid-user-name"
+      create_and_validate
+    end
+
+  end
 end
