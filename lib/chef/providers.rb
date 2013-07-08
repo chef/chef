3aedb865c80ab3aeb16bb32f4232d68909e318fc
deepalijagtap <deepali.jagtap@clogeny.com>
2013-07-08 06:24:17 -0500

    require aix mount provider

--- lib/chef/providers.rb
+++ lib/chef/providers.rb
@@ -97,6 +97,7 @@ require 'chef/provider/group/usermod'
 require 'chef/provider/group/windows'
 
 require 'chef/provider/mount/mount'
+require 'chef/provider/mount/aix'
 require 'chef/provider/mount/windows'
 
 require 'chef/provider/deploy/revision'
