f5504ced80a46279f0038183e9a7dd4fc8706e7f
chef-training <kd@ubuntu.(none)>
2013-07-09 11:17:40 -0700

    re add Chef::Provider::Cron::Solaris

--- lib/chef/platform/provider_mapping.rb
+++ lib/chef/platform/provider_mapping.rb
@@ -249,7 +249,7 @@ class Chef
             :default => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::Ips,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod
             }
           },
@@ -257,7 +257,7 @@ class Chef
             :default => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::Ips,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod
             }
           },
@@ -265,7 +265,7 @@ class Chef
             :default => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::Solaris,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod
             }
           },
@@ -273,7 +273,7 @@ class Chef
             :default => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::Ips,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod,
               :user => Chef::Provider::User::Solaris,
             }
@@ -282,14 +282,14 @@ class Chef
             :default => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::Ips,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod,
               :user => Chef::Provider::User::Solaris,
             },
             ">= 5.9" => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::Solaris,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod,
               :user => Chef::Provider::User::Solaris,
             }
@@ -298,7 +298,7 @@ class Chef
             :default => {
               :service => Chef::Provider::Service::Solaris,
               :package => Chef::Provider::Package::SmartOS,
-              :cron => Chef::Provider::Cron::Unix,
+              :cron => Chef::Provider::Cron::Solaris,
               :group => Chef::Provider::Group::Usermod
             }
           },
@@ -321,7 +321,7 @@ class Chef
           :aix => {
             :default => {
               :group => Chef::Provider::Group::Aix,
-              :cron => Chef::Provider::Cron::Unix
+              :cron => Chef::Provider::Cron::Aix
             }
           },
           :default  => {
