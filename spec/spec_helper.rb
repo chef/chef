35a6157353b87d913472d21ed292b70fd2abdf01
Chirag Jog <chirag@clogeny.com>
2013-07-10 21:14:44 +0100

    Introduce a filter to run on linux(as root) or windows

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,6 +99,7 @@ RSpec.configure do |config|
   config.filter_run_excluding :ruby_18_only => true unless ruby_18?
   config.filter_run_excluding :ruby_19_only => true unless ruby_19?
   config.filter_run_excluding :requires_root => true unless ENV['USER'] == 'root'
+  config.filter_run_excluding :requires_root_or_running_windows => true unless (ENV['USER'] == 'root' or windows?)
   config.filter_run_excluding :requires_unprivileged_user => true if ENV['USER'] == 'root'
   config.filter_run_excluding :uses_diff => true unless has_diff?
 
