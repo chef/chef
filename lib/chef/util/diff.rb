9a0974a48e4f86fc58e43829a338cd59b9579720
Claire McQuin <claire@opscode.com>
2013-07-17 11:18:58 -0700

    IO:: -> IO.

--- lib/chef/util/diff.rb
+++ lib/chef/util/diff.rb
@@ -143,8 +143,8 @@ class Chef
         diff_str = ""
         file_length_difference = 0
 
-        old_data = IO::readlines(old_file).map { |e| e.chomp }
-        new_data = IO::readlines(new_file).map { |e| e.chomp }
+        old_data = IO.readlines(old_file).map { |e| e.chomp }
+        new_data = IO.readlines(new_file).map { |e| e.chomp }
         diff_data = ::Diff::LCS.diff(old_data, new_data)
 
         return diff_str if old_data.empty? && new_data.empty?
