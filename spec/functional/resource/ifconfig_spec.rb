49f5f4dd1bf4515e398f04043e997d209d62d861
kaustubh-d <kausubh@clogeny.com>
2013-07-17 08:34:39 -0700

    add aix- ifconfig functional tests.

--- spec/functional/resource/ifconfig_spec.rb
+++ spec/functional/resource/ifconfig_spec.rb
@@ -17,8 +17,10 @@
 #
 
 require 'functional/resource/base'
+require 'chef/mixin/shell_out'
 
 describe Chef::Resource::Ifconfig, :unix_only do
+  include Chef::Mixin::ShellOut
 
   let(:new_resource) do
     new_resource = Chef::Resource::Ifconfig.new('10.10.0.1', run_context)
@@ -34,20 +36,54 @@ describe Chef::Resource::Ifconfig, :unix_only do
     provider.load_current_resource
   end
 
-  def network_interface_en0
+  def network_interface_for_test
+    # use loopback interface for tests
     case ohai[:platform]
     when :aix
-      'en0'
+      'lo0'
     else
-      'eth0'
+      'lo'
     end
   end
 
+  def network_interface_alias
+    case ohai[:platform]
+    when :aix
+      network_interface_for_test
+    else
+      network_interface_for_test + ":10"
+    end
+  end
+
+  # platform specific test setup and validation routines
+
+  def setup_add_interface(resource)
+    resource.device network_interface_alias
+    resource.is_vip = true if ohai[:platform] == :aix
+  end
+
+  def interface_should_exists(interface)
+    expect(shell_out("ifconfig #{@interface} | grep 10.10.0.1").exitstatus).to eq(0)
+  end
+
+  def interface_should_not_exists(interface)
+    expect(shell_out("ifconfig #{@interface} | grep 10.10.0.1").exitstatus).to eq(1)
+  end
+
+  def interface_persistence_should_exists(interface)
+    # TODO: for AIX query ODM
+  end
+
+  def interface_persistence_should_not_exists(interface)
+    # TODO: for AIX query ODM
+  end
+
+  # Actual tests
+
   describe "#load_current_resource" do
-    # test en0 interface
     it 'should load given interface' do
-      new_resource.device network_interface_en0
-      expect(current_resource.device).to eql(network_interface_en0)
+      new_resource.device network_interface_for_test
+      expect(current_resource.device).to eql(network_interface_for_test)
       expect(current_resource.inet_addr).to match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
     end
   end
@@ -56,11 +92,50 @@ describe Chef::Resource::Ifconfig, :unix_only do
     after do
       new_resource.run_action(:delete)
     end
-    it "should add interface eth0:10 (vip)" do
-      new_resource.device "eth0:10"
+    it "should add interface (vip)" do
+      setup_add_interface(new_resource)
       new_resource.run_action(:add)
-      expect(current_resource.device).to eql("eth0:10")
-      expect(current_resource.inet_addr).to eql("10.10.0.1")
+      interface_should_exists(network_interface_alias)
+      interface_persistence_should_exists(network_interface_alias)
+    end
+  end
+
+  describe "#action_enable", ohai[:platform] != :aix do
+    after do
+      new_resource.run_action(:delete)
+    end
+    it "should enable interface (vip)" do
+      setup_add_interface(new_resource)
+      new_resource.run_action(:enable)
+      interface_should_exists(network_interface_alias)
+    end
+  end
+
+  describe "#action_disable", ohai[:platform] != :aix do
+    before do
+      setup_add_interface(new_resource)
+      new_resource.run_action(:add)
+    end
+    after do
+      new_resource.run_action(:delete)
+    end
+    it "should disable interface (vip)" do
+      new_resource.run_action(:disable)
+      new_resource.should be_updated_by_last_action
+      interface_should_not_exists(network_interface_alias)
+    end
+  end
+
+  describe "#action_delete", ohai[:platform] != :aix do
+    before do
+      setup_add_interface(new_resource)
+      new_resource.run_action(:add)
+    end
+    it "should delete interface (vip)" do
+      new_resource.run_action(:delete)
+      new_resource.should be_updated_by_last_action
+      interface_should_not_exists(network_interface_alias)
+      interface_persistence_should_not_exists(network_interface_alias)
     end
   end
 end
