3c0dff6db61c01b041c6ea698e151a90948d20d8
Lamont Granquist <lamont@opscode.com>
2013-07-18 21:38:18 -0700

    use gem specs, stop copying jenkins artifacts around

--- jenkins/client-test.bat
+++ jenkins/client-test.bat
@@ -14,22 +14,12 @@ SET omnibus_package=%WORKSPACE%\%BUILD_NUMBER%\%omnibus_package%
 
 call msiexec INSTALLLOCATION=C:\opscode /qb /i %omnibus_package% || GOTO :error
 
-rem # extract the chef source code
-mkdir src\chef
-FOR %%i IN (src\chef*.tar.gz) DO SET omnibus_source_tarball=%%i
-call C:\opscode\chef\bin\tar.exe xvzf %omnibus_source_tarball% -C src\chef || GOTO :error
-
-rem # COMPAT HACK - Chef 11 finally has the core Chef code in the root of the
-rem # project repo. Since the Chef Client pipeline needs to build/test Chef 10.x
-rem # and 11 releases our test script need to handle both cases gracefully.
-IF EXIST .\src\chef\chef (
-  cd .\src\chef\chef
-) ELSE (
-  cd .\src\chef
-)
-
 rem # use rspec and gems from omnibus
 set PATH=C:\opscode\chef\bin;C:\opscode\chef\embedded\bin;%PATH%
+
+rem # run against the specs that are packaged in the chef gem
+rem # sorry about the chef-20 bug on the line below, but this really needs to be rewritten in powershell before then...
+cd c:\opscode\chef\embedded\lib\ruby\gems\1.9.1\gems\chef-1*
 
 rem # run the tests -- exclude spec/stress on windows
 rem # we do not bundle exec here in order to test against the gems in the omnibus package
