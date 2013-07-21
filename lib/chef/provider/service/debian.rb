9371c776731397606f613f932224ea771ec1bb5c
Jack Foy <jfoy@whitepages.com>
2013-07-20 21:25:25 -0700

    Remove superfluous empty parentheses in Service::Debian

--- lib/chef/provider/service/debian.rb
+++ lib/chef/provider/service/debian.rb
@@ -125,14 +125,14 @@ class Chef
           @new_resource.enabled(true)
         end
 
-        def enable_service()
+        def enable_service
           if @new_resource.priority.is_a? Integer
             run_command(:command => "/usr/sbin/update-rc.d -f #{@new_resource.service_name} remove")
             run_command(:command => "/usr/sbin/update-rc.d #{@new_resource.service_name} defaults #{@new_resource.priority} #{100 - @new_resource.priority}")
           elsif @new_resource.priority.is_a? Hash
             # we call the same command regardless of we're enabling or disabling
             # users passing a Hash are responsible for setting their own start priorities
-            set_priority()
+            set_priority
           else # No priority, go with update-rc.d defaults
             run_command(:command => "/usr/sbin/update-rc.d -f #{@new_resource.service_name} remove")
             run_command(:command => "/usr/sbin/update-rc.d #{@new_resource.service_name} defaults")
@@ -140,7 +140,7 @@ class Chef
 
         end
 
-        def disable_service()
+        def disable_service
           if @new_resource.priority.is_a? Integer
             # Stop processes in reverse order of start using '100 - start_priority'
             run_command(:command => "/usr/sbin/update-rc.d -f #{@new_resource.service_name} remove")
@@ -148,7 +148,7 @@ class Chef
           elsif @new_resource.priority.is_a? Hash
             # we call the same command regardless of we're enabling or disabling
             # users passing a Hash are responsible for setting their own stop priorities
-            set_priority()
+            set_priority
           else
             # no priority, using '100 - 20 (update-rc.d default)' to stop in reverse order of start
             run_command(:command => "/usr/sbin/update-rc.d -f #{@new_resource.service_name} remove")
@@ -156,7 +156,7 @@ class Chef
           end
         end
 
-        def set_priority()
+        def set_priority
           args = ""
           @new_resource.priority.each do |level, o|
             action = o[0]
