dfc2bd737eb50395a8c8b2ecc14b581ed5906140
siddheshwar-more <siddheshwar.more@clogeny.com>
2013-07-22 18:27:38 +0530

    Updated spec helper to set requires_root true for windows administrator user

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,7 +111,7 @@ RSpec.configure do |config|
   config.filter_run_excluding :selinux_only => true unless selinux_enabled?
   config.filter_run_excluding :ruby_18_only => true unless ruby_18?
   config.filter_run_excluding :ruby_19_only => true unless ruby_19?
-  config.filter_run_excluding :requires_root => true unless ENV['USER'] == 'root'
+  config.filter_run_excluding :requires_root => true unless ENV['USER'] == 'root' or ENV['USER'] == 'Administrator'
   config.filter_run_excluding :requires_root_or_running_windows => true unless (ENV['USER'] == 'root' or windows?)
   config.filter_run_excluding :requires_unprivileged_user => true if ENV['USER'] == 'root'
   config.filter_run_excluding :uses_diff => true unless has_diff?
