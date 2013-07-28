711a8baebd6ccbdac54a30aa6fe8d7b0493e84b7
Ryota Arai <ryota.arai@gmail.com>
2013-07-28 21:33:14 +0900

    Improve an error message "No cookbook found".

--- lib/chef/client.rb
+++ lib/chef/client.rb
@@ -561,9 +561,12 @@ class Chef
           if directory_not_empty?(cookbook_path)
             break
           else
-            msg = "No cookbook found in #{Chef::Config[:cookbook_path].inspect}, make sure cookbook_path is set correctly."
+            msg = "No cookbook found in #{cookbook_path}, make sure cookbook_path is set correctly."
             Chef::Log.fatal(msg)
-            raise Chef::Exceptions::CookbookNotFound, msg if is_last_element?(index, Chef::Config[:cookbook_path])
+            if is_last_element?(index, Chef::Config[:cookbook_path])
+              msg = "No cookbook found in #{Chef::Config[:cookbook_path].inspect}, make sure cookbook_path is set correctly."
+              raise Chef::Exceptions::CookbookNotFound, msg
+            end
           end
         end
       else
