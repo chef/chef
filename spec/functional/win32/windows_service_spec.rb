4bf3e295635afe55a4e5936e6277577cddb22377
Mukta A <mukta.aphale@clogeny.com>
2013-08-26 13:04:46 +0530

    Unit tests for windows fork when chef client runs as service

---
+++ spec/functional/win32/windows_service_spec.rb
@@ -0,0 +1,55 @@
+#
+# Author:: Mukta Aphale (<mukta.aphale@clogeny.com>)
+# Copyright:: Copyright (c) 2013 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'spec_helper'
+if Chef::Platform.windows?
+  require 'chef/application/windows_service'
+end
+
+describe "Chef::Application::WindowsService", :windows_only do
+  let (:instance) {Chef::Application::WindowsService.new}
+  let (:shell_out_result) {Object.new}
+  before do
+    instance.stub(:parse_options)
+    shell_out_result.stub(:stdout)
+    shell_out_result.stub(:stderr)
+  end
+  it "runs chef-client in new process" do
+    instance.should_receive(:configure_chef).twice
+    instance.service_init
+    (instance.instance_variable_get(:@service_signal)).stub(:wait)
+    instance.should_receive(:state).and_return(RUNNING)
+    instance.should_receive(:run_chef_client).and_call_original
+    instance.should_receive(:shell_out)
+    instance.service_main
+  end
+  it "passes config params to new process" do
+    Chef::Config[:config_file] = "test_config_file"
+    Chef::Config[:log_location] = "log_file_path"
+    Chef::Config[:log_level] = :debug
+    Chef::Config[:chef_server_url] = "chef_server_url"
+    Chef::Config[:force_logger] = true
+    Chef::Config[:force_formatter] = true
+    instance.should_receive(:configure_chef).twice
+    instance.service_init
+    (instance.instance_variable_get(:@service_signal)).stub(:wait)
+    instance.should_receive(:state).and_return(RUNNING)
+    instance.should_receive(:run_chef_client).and_call_original
+    instance.should_receive(:shell_out).with("chef-client  --no-fork -c test_config_file -L log_file_path -l debug -S chef_server_url --force-formatter --force-logger").and_return(shell_out_result)
+    instance.service_main
+  end
+end
