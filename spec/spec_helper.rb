7c67790bdf302d15a7bddb530f4fa2383a0e0e18
Mukta A <mukta.aphale@clogeny.com>
2013-09-01 13:01:01 +0530

    Renamed unsupported_group_provider_platform

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -107,7 +107,8 @@ RSpec.configure do |config|
   config.filter_run_excluding :windows32_only => true unless windows32?
   config.filter_run_excluding :system_windows_service_gem_only => true unless system_windows_service_gem?
   config.filter_run_excluding :unix_only => true unless unix?
-  config.filter_run_excluding :buggy_platforms => true if (os_x? or solaris? or freebsd? or File.exists?("/etc/SuSE-release"))
+  # Remove this filter once these issues are fixed: OC-9764, OC-9765, OC-9766, OC-9767
+  config.filter_run_excluding :unsupported_group_provider_platform => true if (os_x? or solaris? or freebsd? or File.exists?("/etc/SuSE-release"))
   config.filter_run_excluding :supports_cloexec => true unless supports_cloexec?
   config.filter_run_excluding :selinux_only => true unless selinux_enabled?
   config.filter_run_excluding :ruby_18_only => true unless ruby_18?
