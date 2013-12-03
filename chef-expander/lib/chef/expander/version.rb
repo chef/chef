8a1a44bfebe0482348a20eeb577be24ead39a066
sersut <serdar@opscode.com>
2013-12-03 15:07:45 -0800

    Bump up the version number for release.

--- chef-expander/lib/chef/expander/version.rb
+++ chef-expander/lib/chef/expander/version.rb
@@ -23,7 +23,7 @@ require 'open3'
 module Chef
   module Expander
 
-    VERSION = "10.30.0.rc.2"
+    VERSION = "10.30.0"
 
     def self.version
       @rev ||= begin
--- chef-server/lib/chef-server/version.rb
+++ chef-server/lib/chef-server/version.rb
@@ -17,5 +17,5 @@
 #
 
 module ChefServer
-  VERSION = '10.30.0.rc.2'
+  VERSION = '10.30.0'
 end
