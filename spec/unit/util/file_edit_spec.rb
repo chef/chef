e88889de783c666e42d9e7e7be7d8b22760e2fbe
Chris Bandy <bandy.chris@gmail.com>
2014-01-10 08:02:52 +0000

    CHEF-4173: Test that insert_line_if_no_match works more than once

--- spec/unit/util/file_edit_spec.rb
+++ spec/unit/util/file_edit_spec.rb
@@ -141,5 +141,13 @@ HOSTS
       newfile = File.new(@tempfile.path).readlines
       newfile[1].should_not match(/replacement/)
     end
+
+    it "should work more than once" do
+      @fedit.insert_line_if_no_match(/missing/, "added")
+      @fedit.insert_line_if_no_match(/missing/, "twice")
+      @fedit.write_file
+      newfile = File.new(@tempfile.path).readlines
+      newfile.last.should match(/twice/)
+    end
   end
 end
