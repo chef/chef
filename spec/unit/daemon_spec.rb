652389fbd848861997b9b0c67fae7b2f5ecef064
Lamont Granquist <lamont@scriptkiddie.org>
2014-01-29 14:05:42 -0800

    fix rspec_reset problem
    follows the pattern in the rspec-mocks spec tests of defining a
    helper module:
    
    https://github.com/rspec/rspec-mocks/blob/cee433c89125a3984df33c87eb61985613adce9b/spec/spec_helper.rb

--- spec/unit/daemon_spec.rb
+++ spec/unit/daemon_spec.rb
@@ -79,7 +79,7 @@ describe Chef::Daemon do
     end
 
     it "changes the working directory to root" do
-      Dir.rspec_reset
+      reset Dir
       Dir.should_receive(:chdir).with("/").and_return(0)
       Chef::Daemon.change_privilege
     end
