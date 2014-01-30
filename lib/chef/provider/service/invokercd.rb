9e8496900e28064927a800aeea75401bdb93b8ab
Szymon Szypulski <szymon.szypulski@ragnarson.com>
2014-01-30 14:29:22 +0100

    Rewrite invoke service provider

--- lib/chef/provider/service/invokercd.rb
+++ lib/chef/provider/service/invokercd.rb
@@ -27,8 +27,102 @@ class Chef
 
         def initialize(new_resource, run_context)
           super
-          @init_command = "/usr/sbin/invoke-rc.d #{@new_resource.service_name}"
+          @init_command = "/usr/sbin/invoke-rc.d"
+          @invokercd_init_command = "#{@init_command} #{@new_resource.service_name}"
         end
+
+        def start_service
+          if @new_resource.start_command
+            shell_out!(@new_resource.start_command)
+          else
+            shell_out!("#{@invokercd_init_command} start")
+          end
+        end
+
+        def stop_service
+          if @new_resource.stop_command
+            shell_out!(@new_resource.stop_command)
+          else
+            shell_out!("#{@invokercd_init_command} stop")
+          end
+        end
+
+        def restart_service
+          if @new_resource.restart_command
+            shell_out!(@new_resource.restart_command)
+          elsif not @new_resource.supports[:restart] == true
+            stop_service
+            sleep 1
+            start_service
+          else
+            shell_out!("#{@invokercd_init_command} restart")
+          end
+        end
+
+        def reload_service
+          if @new_resource.reload_command
+            shell_out!(@new_resource.reload_command)
+          else
+            shell_out!("#{@invokercd_init_command} reload")
+          end
+        end
+
+        protected
+          def determine_current_status!
+            if @new_resource.status_command
+              Chef::Log.debug("#{@new_resource} you have specified a status command, running..")
+
+              begin
+                if shell_out(@new_resource.status_command).exitstatus == 0
+                  @current_resource.running true
+                  Chef::Log.debug("#{@new_resource} is running")
+                end
+              rescue Mixlib::ShellOut::ShellCommandFailed, SystemCallError
+              # ShellOut sometimes throws different types of Exceptions than ShellCommandFailed.
+              # Temporarily catching different types of exceptions here until we get Shellout fixed.
+              # TODO: Remove the line before one we get the ShellOut fix.
+                @status_load_success = false
+                @current_resource.running false
+                nil
+              end
+
+            elsif @new_resource.supports[:status]
+              Chef::Log.debug("#{@new_resource} supports status, running")
+              begin
+                if shell_out("#{@invokercd_init_command} status").exitstatus == 0
+                  @current_resource.running true
+                  Chef::Log.debug("#{@new_resource} is running")
+                end
+              # ShellOut sometimes throws different types of Exceptions than ShellCommandFailed.
+              # Temporarily catching different types of exceptions here until we get Shellout fixed.
+              # TODO: Remove the line before one we get the ShellOut fix.
+              rescue Mixlib::ShellOut::ShellCommandFailed, SystemCallError
+                @status_load_success = false
+                @current_resource.running false
+                nil
+              end
+            else
+              Chef::Log.debug "#{@new_resource} falling back to process table inspection"
+              r = Regexp.new(@new_resource.pattern)
+              Chef::Log.debug "#{@new_resource} attempting to match '#{@new_resource.pattern}' (#{r.inspect}) against process list"
+              begin
+                shell_out!(ps_cmd).stdout.each_line do |line|
+                  if r.match(line)
+                    @current_resource.running true
+                    break
+                  end
+                end
+
+                @current_resource.running false unless @current_resource.running
+                Chef::Log.debug "#{@new_resource} running: #{@current_resource.running}"
+              # ShellOut sometimes throws different types of Exceptions than ShellCommandFailed.
+              # Temporarily catching different types of exceptions here until we get Shellout fixed.
+              # TODO: Remove the line before one we get the ShellOut fix.
+              rescue Mixlib::ShellOut::ShellCommandFailed, SystemCallError
+                @ps_command_failed = true
+              end
+            end
+          end
       end
     end
   end
