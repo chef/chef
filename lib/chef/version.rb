17307a586520481ea5481ecbe357c19e20fbe7b2
sersut <serdar@opscode.com>
2014-02-06 09:08:51 -0800

    Bump the version number for release.

--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -17,7 +17,7 @@
 
 class Chef
   CHEF_ROOT = File.dirname(File.expand_path(File.dirname(__FILE__)))
-  VERSION = '11.10.0.rc.1'
+  VERSION = '11.10.0'
 end
 
 # NOTE: the Chef::Version class is defined in version_class.rb
