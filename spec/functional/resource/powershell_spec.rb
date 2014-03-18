f623ac4e8ee291ea46211278b76b3ee13d686761
Adam Edwards <adamed@opscode.com>
2014-03-18 08:37:37 -0700

    Powershell guard functional tests

--- spec/functional/resource/powershell_spec.rb
+++ spec/functional/resource/powershell_spec.rb
@@ -177,6 +177,150 @@ describe Chef::Resource::WindowsScript::PowershellScript, :windows_only do
     end
   end
 
+  describe "when executing guards" do
+
+    before(:each) do
+      resource.not_if.clear
+      resource.only_if.clear
+    end
+
+    it "evaluates a powershell $false for a not_if block as false" do
+      resource.not_if  "$false"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a powershell $true for a not_if block as true" do
+      resource.not_if  "$true"
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a powershell $false for an only_if block as false" do
+      resource.only_if  "$false"
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a powershell $true for a not_if block as true" do
+      resource.only_if  "$true"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a not_if block using powershell.exe" do
+      resource.not_if  "exit([int32](![System.Environment]::CommandLine.Contains('powershell.exe')))"
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates an only_if block using powershell.exe" do
+      resource.only_if  "exit([int32](![System.Environment]::CommandLine.Contains('powershell.exe')))"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a not_if block as false" do
+      resource.not_if { false }
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a not_if block as true" do
+      resource.not_if { true }
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates an only_if block as false" do
+      resource.only_if { false }
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates an only_if block as true" do
+      resource.only_if { true }
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a non-zero powershell exit status for not_if as true" do
+      resource.not_if  "exit 37"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a zero powershell exit status for not_if as false" do
+      resource.not_if  "exit 0"
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a failed executable exit status for not_if as false" do
+      resource.not_if  windows_process_exit_code_not_found_content
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a successful executable exit status for not_if as true" do
+      resource.not_if  windows_process_exit_code_success_content
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a failed executable exit status for only_if as false" do
+      resource.only_if  windows_process_exit_code_not_found_content
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a successful executable exit status for only_if as true" do
+      resource.only_if  windows_process_exit_code_success_content
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a failed cmdlet exit status for not_if as true" do
+      resource.not_if  "throw 'up'"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a successful cmdlet exit status for not_if as true" do
+      resource.not_if  "cd ."
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a failed cmdlet exit status for only_if as false" do
+      resource.only_if  "throw 'up'"
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a successful cmdlet exit status for only_if as true" do
+      resource.only_if  "cd ."
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a not_if block using the cwd guard parameter" do
+      custom_cwd = "#{ENV['SystemRoot']}\\system32\\drivers\\etc"
+      resource.not_if  "exit ! [int32]($pwd.path -eq '#{custom_cwd}')", :cwd => custom_cwd
+      resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates an only_if block using the cwd guard parameter" do
+      custom_cwd = "#{ENV['SystemRoot']}\\system32\\drivers\\etc"
+      resource.only_if  "exit ! [int32]($pwd.path -eq '#{custom_cwd}')", :cwd => custom_cwd
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a 64-bit resource with a 64-bit guard and interprets boolean false as zero status code", :windows64_only do
+      resource.architecture :x86_64
+      resource.only_if  "exit [int32]($env:PROCESSOR_ARCHITECTURE -ne 'AMD64')"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a 64-bit resource with a 64-bit guard and interprets boolean true as nonzero status code", :windows64_only do
+      resource.architecture :x86_64
+      resource.only_if  "exit [int32]($env:PROCESSOR_ARCHITECTURE -eq 'AMD64')"
+    resource.should_skip?(:run).should be_true
+    end
+
+    it "evaluates a 32-bit resource with a 32-bit guard and interprets boolean false as zero status code" do
+      resource.architecture :i386
+      resource.only_if  "exit [int32]($env:PROCESSOR_ARCHITECTURE -ne 'X86')"
+      resource.should_skip?(:run).should be_false
+    end
+
+    it "evaluates a 32-bit resource with a 32-bit guard and interprets boolean true as nonzero status code" do
+      resource.architecture :i386
+      resource.only_if  "exit [int32]($env:PROCESSOR_ARCHITECTURE -eq 'X86')"
+      resource.should_skip?(:run).should be_true
+    end
+  end
+
   def get_script_output
     script_output = File.read(script_output_path)
   end
