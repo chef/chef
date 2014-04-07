a6d6930136cabac3ce9befcef9427a0a1215f653
Lamont Granquist <lamont@scriptkiddie.org>
2014-04-07 11:35:51 -0700

    bump to 11.14.0.alpha.1 to add annotated tag

--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -1,4 +1,4 @@
-#
+
 # Author:: Daniel DeLeo (<dan@opscode.com>)
 # Copyright:: Copyright (c) 2010-2011 Opscode, Inc.
 # License:: Apache License, Version 2.0
@@ -17,7 +17,7 @@
 
 class Chef
   CHEF_ROOT = File.dirname(File.expand_path(File.dirname(__FILE__)))
-  VERSION = '11.14.0.alpha.0'
+  VERSION = '11.14.0.alpha.1'
 end
 
 # NOTE: the Chef::Version class is defined in version_class.rb
