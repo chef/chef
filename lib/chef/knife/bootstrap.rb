34056828954efae8fc7c6deb80e7ba4c8cd27e80
curiositycasualty <BlkDragon96@gmail.com>
2014-04-23 11:45:16 -0700

    .first instead of .include

--- lib/chef/knife/bootstrap.rb
+++ lib/chef/knife/bootstrap.rb
@@ -217,7 +217,7 @@ class Chef
         if Array(@name_args).first.nil?
           ui.error("Must pass an FQDN or ip to bootstrap")
           exit 1
-        elsif Array(@name_args).include?("windows")
+        elsif Array(@name_args).first == "windows"
           ui.warn("Hostname containing 'windows' specified. Please install 'knife-windows' if you are attempting to bootstrap a Windows node via WinRM.")
         end
       end
