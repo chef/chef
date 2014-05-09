9c03546f033a4b9720b84911525ed4ab869c830a
Bryan McLellan <btm@getchef.com>
2014-05-09 14:43:18 -0700

    CHEF-5247: Add a unit test
    Unfortunately the way that Chef::Provider::User::Solaris subclasses
    Chef::Provider::User::Useradd we call both Solaris#manage_password
    and Useradd#manage_user whenever we call Solaris#manage_user. If we
    are updating a password, we only want Solaris#manage_password, so we
    have a kind of convulted way set to tp turn Useradd#manage_user into
    a noop. This test tries to ensure we can noop in the way we want to,
    but we really need a functional or integration test here.

--- spec/unit/provider/user/useradd_spec.rb
+++ spec/unit/provider/user/useradd_spec.rb
@@ -37,4 +37,14 @@ describe Chef::Provider::User::Useradd do
   }
 
   include_examples "a useradd-based user provider", supported_useradd_options
+
+  describe "manage_user" do
+    # CHEF-5247: Chef::Provider::User::Solaris subclasses Chef::Provider::User::Useradd, but does not use usermod to change passwords.
+    # Thus, a call to Solaris#manage_user calls Solaris#manage_password and Useradd#manage_user, but the latter should be a no-op.
+    it "should not run the command if universal_options is an empty array" do
+      provider.stub(:universal_options).and_return([])
+      expect(provider).not_to receive(:shell_out!)
+      provider.manage_user
+    end
+  end
 end
