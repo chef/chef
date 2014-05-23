e60260cf2ad1ba769ed20c68fbf7c96d252481d1
Lamont Granquist <lamont@scriptkiddie.org>
2014-05-23 10:45:25 -0700

    copyright update

--- lib/chef/provider/mount.rb
+++ lib/chef/provider/mount.rb
@@ -1,6 +1,7 @@
 #
-# Author:: Joshua Timberman (<joshua@opscode.com>)
-# Copyright:: Copyright (c) 2009 Opscode, Inc
+# Author:: Joshua Timberman (<joshua@getchef.com>)
+# Author:: Lamont Granquist (<lamont@getchef.com>)
+# Copyright:: Copyright (c) 2009 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
