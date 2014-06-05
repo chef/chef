c17c9c86acc66e818603e5a732eef4d343d78aa6
sersut <serdar@opscode.com>
2014-06-05 11:54:35 -0700

    Another version bump for the security release in order not to rewrite git history.

--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -17,7 +17,7 @@
 
 class Chef
   CHEF_ROOT = File.dirname(File.expand_path(File.dirname(__FILE__)))
-  VERSION = '11.12.6'
+  VERSION = '11.12.8'
 end
 
 # NOTE: the Chef::Version class is defined in version_class.rb
