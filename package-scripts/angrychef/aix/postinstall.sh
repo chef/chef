c70bbff28692e594bf3057e959e789d84a12eb68
sersut <serdar@opscode.com>
2014-06-24 16:38:58 -0700

    Point the package scripts to angrychef directories.

--- package-scripts/angrychef/aix/postinstall.sh
+++ package-scripts/angrychef/aix/postinstall.sh
@@ -1,6 +1,5 @@
-ln -sn /opt/chef/bin/chef-client /usr/bin/chef-client
-ln -sn /opt/chef/bin/chef-solo /usr/bin/chef-solo
-ln -sn /opt/chef/bin/knife /usr/bin/knife
-ln -sn /opt/chef/bin/ohai /usr/bin/ohai
-ln -sn /opt/chef/bin/shef /usr/bin/shef
-
+ln -sn /opt/angrychef/bin/chef-client /usr/bin/chef-client
+ln -sn /opt/angrychef/bin/chef-solo /usr/bin/chef-solo
+ln -sn /opt/angrychef/bin/knife /usr/bin/knife
+ln -sn /opt/angrychef/bin/ohai /usr/bin/ohai
+ln -sn /opt/angrychef/bin/shef /usr/bin/shef
