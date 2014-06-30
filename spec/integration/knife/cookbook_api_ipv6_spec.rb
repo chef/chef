1ee424ed7540414f0b8297bad1058cfd887f6aac
Julian C. Dunn <jdunn@getchef.com>
2014-06-30 13:35:07 -0400

    Make sure to call chef-client and knife that we just built, not just some random ones hanging around in $PATH.

--- spec/integration/knife/cookbook_api_ipv6_spec.rb
+++ spec/integration/knife/cookbook_api_ipv6_spec.rb
@@ -62,6 +62,7 @@ END_VALIDATION_PEM
     end
 
     let(:chef_dir) { File.join(File.dirname(__FILE__), "..", "..", "..", "bin") }
+    let(:knife) { "ruby #{chef_dir}/knife" }
 
     let(:knife_config_flag) { "-c '#{path_to("config/knife.rb")}'" }
 
@@ -88,7 +89,7 @@ END_CLIENT_RB
         end
 
         it "successfully uploads a cookbook" do
-          shell_out!("knife cookbook upload apache2 #{knife_config_flag}", :cwd => chef_dir)
+          shell_out!("#{knife} cookbook upload apache2 #{knife_config_flag}", :cwd => chef_dir)
           versions_list_json = Chef::HTTP::Simple.new("http://[::1]:8900").get("/cookbooks/apache2", "accept" => "application/json")
           versions_list = Chef::JSONCompat.from_json(versions_list_json)
           versions_list["apache2"]["versions"].should_not be_empty
@@ -96,7 +97,7 @@ END_CLIENT_RB
 
         context "and the cookbook has been uploaded to the server" do
           before do
-            shell_out!("knife cookbook upload apache2 #{knife_config_flag}", :cwd => chef_dir)
+            shell_out!("#{knife} cookbook upload apache2 #{knife_config_flag}", :cwd => chef_dir)
           end
 
           it "downloads the cookbook" do
