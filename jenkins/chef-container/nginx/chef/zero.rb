4cdd55da75a1d75814a4c57891b6c4b7fc7b768c
Tom Duffield <tom@getchef.com>
2014-06-30 13:06:38 -0500

    removing Docker-build files

--- jenkins/chef-container/nginx/chef/zero.rb
+++
@@ -1,7 +0,0 @@
-require 'chef-init'
-
-cookbook_path   ["/etc/chef/cookbooks"]
-ssl_verify_mode :verify_peer
-
-#Ohai::Config[:directory] = "/etc/chef/ohai_plugins"
-#Ohai::Config[:hints_path] = "/etc/chef/ohai/hints"
