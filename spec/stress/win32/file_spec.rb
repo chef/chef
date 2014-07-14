b6e249fa02c796d9678f68f1f527d382896bb91d
sersut <serdar@opscode.com>
2014-07-14 15:58:06 -0700

    Make the windows memory leak test more robust.

--- spec/stress/win32/file_spec.rb
+++ spec/stress/win32/file_spec.rb
@@ -24,15 +24,9 @@ describe 'Chef::ReservedNames::Win32::File', :windows_only do
     @path = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "data", "old_home_dir", "my-dot-emacs"))
   end
 
-  it "should not leak memory" do
-    pending "Fix required for CHEF-5004"
-    test = lambda { Chef::ReservedNames::Win32::File.symlink?(@path) }
-    test.should_not leak_memory(:warmup => 50, :iterations => 100)
-  end
-
   it "should not leak significant memory" do
     test = lambda { Chef::ReservedNames::Win32::File.symlink?(@path) }
-    test.should_not leak_memory(:warmup => 50000, :iterations => 100)
+    test.should_not leak_memory(:warmup => 50000, :iterations => 50000)
   end
 
   it "should not leak handles" do
