2b976ec7a19cb22adc9eab5ff76a5f6af017f265
Seth Vargo <sethvargo@gmail.com>
2014-07-22 17:52:50 -0400

    Use windows_safe_path in chefdk-windows Path

--- config/software/chef-windows.rb
+++ config/software/chef-windows.rb
@@ -71,5 +71,13 @@ build do
   # be "PATH" or "Path". If *both* are set, only one is honored.
   path_key = ENV.keys.grep(/\Apath\Z/i).first
 
-  bundle "install", :env => { path_key => "#{install_dir}\\embedded\\bin;#{install_dir}\\embedded\\mingw\\bin;C:\\Windows\\system32;C:\\Windows;C:\\Windows\\System32\\Wbem"}
+  bundle "install", env: {
+    path_key => [
+      windows_safe_path(install_dir, 'embedded', 'bin'),
+      windows_safe_path(install_dir, 'embedded', 'mingw', 'bin'),
+      windows_safe_path('C:/Windows/system32'),
+      windows_safe_path('C:/Windows'),
+      windows_safe_path('C:/Windows/System32/Wbem'),
+    ].join(File::PATH_SEPARATOR)
+  }
 end
