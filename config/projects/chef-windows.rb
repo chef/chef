9890220ff2f489d2387fdef9f92b3a4fe5503b53
Seth Vargo <sethvargo@gmail.com>
2014-07-22 17:53:22 -0400

    Reduce complexity of chef gem relative search in chef-windows

--- config/projects/chef-windows.rb
+++ config/projects/chef-windows.rb
@@ -44,27 +44,28 @@ dependency "chef-windows"
 resources_path File.join(files_path, "chef")
 
 msi_parameters do
-  msi_parameters = { }
-
   # Find path in which chef gem is installed to.
-  # Note that install_dir is something like: c:\\opscode\\chef
-  chef_path_regex = "#{install_path.gsub(File::ALT_SEPARATOR, File::SEPARATOR)}/**/gems/chef-[0-9]*"
-  chef_gem_paths = Dir[chef_path_regex].select{ |path| File.directory?(path) }
-  unless chef_gem_paths.length == 1
-    raise "Expected one but found #{chef_gem_paths.length} installation directories \
-      for chef gem using: #{chef_path_regex}. Found paths: #{chef_gem_paths.inspect}."
+  # Note that install_dir is something like: c:/opscode/chef
+  search_pattern = "#{install_dir}/**/gems/chef-[0-9]*"
+  chef_gem_path  = Dir.glob(search_pattern).find do |path|
+    File.directory?(path)
   end
-  chef_gem_path = chef_gem_paths.first
+
+  if chef_gem_path.nil?
+    raise "Could not find a chef gem in `#{search_pattern}'!"
+  end
+
   # Convert the chef gem path to a relative path based on install_dir
   # We are going to use this path in the startup command of chef
   # service. So we need to change file seperators to make windows
   # happy.
-  chef_gem_path.gsub!(File::SEPARATOR, File::ALT_SEPARATOR)
-  chef_gem_path.slice!(install_path.gsub(File::SEPARATOR, File::ALT_SEPARATOR) + File::ALT_SEPARATOR)
-  msi_parameters[:chef_gem_path] = chef_gem_path
+  relative_path = Pathname.new(chef_gem_path)
+    .relative_path_from(Pathname.new(install_dir))
+    .to_s
 
-  # Upgrade code for Chef MSI
-  msi_parameters[:upgrade_code] = "D607A85C-BDFA-4F08-83ED-2ECB4DCD6BC5"
-
-  msi_parameters
+  # Return the result as a hash
+  {
+    chef_gem_path: relative_path.gsub(File::SEPARATOR, File::ALT_SEPARATOR),
+    upgrade_code:  'D607A85C-BDFA-4F08-83ED-2ECB4DCD6BC5',
+  }
 end
