3497c4bb2887867d93d194bbd8c2e3ef7c945950
Seth Vargo <sethvargo@gmail.com>
2014-07-22 12:00:58 -0400

    Update chef to use new Omnibus APIs

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -1,6 +1,5 @@
 #
-# Copyright:: Copyright (c) 2012-2014 Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,31 +27,15 @@ source :git => "git://github.com/opscode/chef"
 
 relative_path "chef"
 
-env = with_embedded_path()
-env = with_standard_compiler_flags(env)
-
 build do
-  # COMPAT HACK :( - Chef 11 finally has the core Chef code in the root of the
-  # project repo. Since the Chef Client pipeline needs to build/test Chef 10.x
-  # and 11 releases our software definition need to handle both cases
-  # gracefully.
+  env = with_standard_compiler_flags(with_embedded_path)
+
   block do
-    build_commands = self.builder.build_commands
-    chef_root = File.join(self.project_dir, "chef")
-    if File.exists?(chef_root)
-      build_commands.each_index do |i|
-        cmd = build_commands[i].dup
-        if cmd.is_a? Array
-          if cmd.last.is_a? Hash
-            cmd_opts = cmd.pop.dup
-            cmd_opts[:cwd] = chef_root
-            cmd << cmd_opts
-          else
-            cmd << {:cwd => chef_root}
-          end
-          build_commands[i] = cmd
-        end
-      end
+    if File.exist?("#{project_dir}/chef")
+      # We are on Chef 10 and need to adjust the relative path. In Chef 10, the
+      # Chef Client and Chef Server were in the same repo (like Rails), but in
+      # Chef 11, the server has been moved to its own project.
+      software.relative_path('chef/chef')
     end
   end
 
@@ -68,52 +51,47 @@ build do
   # #{install_dir}/bin which gets created by appbundler later on.
   if %w(chef angrychef chef-container).include?(project.name)
     # install chef first so that ohai gets installed into /opt/chef/bin/ohai
-    rake "gem", :env => env
+    rake "gem", env: env
 
     command "rm -f pkg/chef-*-x86-mingw32.gem"
 
-    gem ["install pkg/chef-*.gem",
-      "-n #{install_dir}/bin",
-      "--no-rdoc --no-ri"].join(" "), :env => env
+    gem "install pkg/chef-*.gem" \
+        " --bindir '#{install_dir}/bin'" \
+        " --no-document", env: env
 
-    # install the whole bundle, so that we get dev gems (like rspec) and can later test in CI
-    # against all the exact gems that we ship (we will run rspec unbundled in the test phase).
-    bundle "install --without server docgen", :env => env
+    # install the whole bundle, so that we get dev gems (like rspec) and can
+    # later test in CI against all the exact gems that we ship (we will run
+    # rspec unbundled in the test phase).
+    bundle "install --without server docgen", env: env
   else
     # install the whole bundle first
-    bundle "install --without server docgen", :env => env
+    bundle "install --without server docgen", env: env
 
-    rake "gem", :env => env
+    rake "gem", env: env
 
     command "rm -f pkg/chef-*-x86-mingw32.gem"
 
     # Don't use -n #{install_dir}/bin. Appbundler will take care of them later
-    gem ["install pkg/chef-*.gem",
-      "--no-rdoc --no-ri"].join(" "), :env => env
+    gem "install pkg/chef-*.gem " \
+        " --no-document", env: env
   end
 
-  auxiliary_gems = []
-  auxiliary_gems << "ruby-shadow" unless platform == "aix"
+  auxiliary_gems = {}
+  auxiliary_gems['ruby-shadow'] = '>= 0.0.0' unless Ohai['platform'] == 'aix'
 
-  gem ["install",
-       auxiliary_gems.join(" "),
-       "--no-rdoc --no-ri"].join(" "), :env => env
-
-  #
-  # TODO: the "clean up" section below was cargo-culted from the
-  # clojure version of omnibus that depended on the build order of the
-  # tasks and not dependencies. if we really need to clean stuff up,
-  # we should probably stick the clean up steps somewhere else
-  #
-
-  # clean up
-  ["docs",
-   "share/man",
-   "share/doc",
-   "share/gtk-doc",
-   "ssl/man",
-   "man",
-   "info"].each do |dir|
-    command "rm -rf #{install_dir}/embedded/#{dir}"
+  auxiliary_gems.each do |name, version|
+    gem "install #{name}" \
+        " --version '#{version}'" \
+        " --no-document" \
+        " --verbose", env: env
   end
+
+  # Clean up
+  delete("#{install_dir}/embedded/docs")
+  delete("#{install_dir}/embedded/share/man")
+  delete("#{install_dir}/embedded/share/doc")
+  delete("#{install_dir}/embedded/share/gtk-doc")
+  delete("#{install_dir}/embedded/ssl/man")
+  delete("#{install_dir}/embedded/man")
+  delete("#{install_dir}/embedded/info")
 end
