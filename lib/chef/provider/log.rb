2e60d36fd24bdae88aecedd7f85a11be82e9ecb2
Lamont Granquist <lamont@scriptkiddie.org>
2014-07-31 09:39:59 -0700

    log resource should support why-runt

--- lib/chef/provider/log.rb
+++ lib/chef/provider/log.rb
@@ -25,6 +25,10 @@ class Chef
       # Chef log provider, allows logging to chef's logs from recipes
       class ChefLog < Chef::Provider
 
+        def whyrun_supported?
+          true
+        end
+
         # ordered array of the log levels
         @@levels = [ :debug, :info, :warn, :error, :fatal ]
 
