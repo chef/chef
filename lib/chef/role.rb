chef/chef

commit a93af4060369f23299652b92fdc451468a3e5534

Added duplicate role detection for Role.from_disk
Now Role.from_disk raises an error when it detects two roles
of the same type (.rb or .json) exist anywhere in the roles directory
or subdirectories.

lib/chef/role.rb
@@ -236,8 +236,12 @@ class Chef
       paths = Array(Chef::Config[:role_path])
       paths.each do |path|
         roles_files = Dir.glob(File.join(path, "**", "**"))
-        js_path = roles_files.detect { |file| file.match /#{name}\.json$/ }
-        rb_path = roles_files.detect { |file| file.match /#{name}\.rb$/ }
+        js_files = roles_files.select { |file| file.match /#{name}\.json$/ }
+        rb_files = roles_files.select { |file| file.match /#{name}\.rb$/ }
+        if js_files.count > 1 or rb_files.count > 1
+          raise Chef::Exceptions::DuplicateRole, "Multiple roles of same type found named #{name}"
+        end
+        js_path, rb_path = js_files.first, rb_files.first
 
         if js_path && (File.exists?(js_path) || force == "json")
           # from_json returns object.class => json_class in the JSON.

lib/chef/exceptions.rb
@@ -64,6 +64,7 @@ class Chef
     class PrivateKeyMissing < RuntimeError; end
     class CannotWritePrivateKey < RuntimeError; end
     class RoleNotFound < RuntimeError; end
+    class DuplicateRole < RuntimeError; end
     class ValidationFailed < ArgumentError; end
     class InvalidPrivateKey < ArgumentError; end
     class ConfigurationError < ArgumentError; end

spec/unit/role_spec.rb
@@ -290,6 +290,12 @@ EOR
       File.should_not_receive(:exists?)
       lambda {@role.class.from_disk("lolcat")}.should raise_error(Chef::Exceptions::RoleNotFound)
     end
+
+    it "should raise an exception if two files exist with the same name" do
+      Dir.should_receive(:glob).and_return(["#{Chef::Config[:role_path]}/memes/lolcat.rb", "#{Chef::Config[:role_path]}/lolcat.rb"])
+      File.should_not_receive(:exists?)
+      lambda {@role.class.from_disk("lolcat")}.should raise_error(Chef::Exceptions::DuplicateRole)
+    end
   end
 
   describe "when loading from disk and role_path is an array" do

commit 0e5952187e11d0f0264d2841fa0246c7f5f28c81

Fixes #1739 - regression from a93af4060369f23299652b92fdc451468a3e5534 which matches files too widely. Signed CLA.

lib/chef/role.rb
@@ -236,8 +236,8 @@ class Chef
       paths = Array(Chef::Config[:role_path])
       paths.each do |path|
         roles_files = Dir.glob(File.join(path, "**", "**"))
-        js_files = roles_files.select { |file| file.match /#{name}\.json$/ }
-        rb_files = roles_files.select { |file| file.match /#{name}\.rb$/ }
+        js_files = roles_files.select { |file| file.match /\/#{name}\.json$/ }
+        rb_files = roles_files.select { |file| file.match /\/#{name}\.rb$/ }
         if js_files.count > 1 or rb_files.count > 1
           raise Chef::Exceptions::DuplicateRole, "Multiple roles of same type found named #{name}"
         end