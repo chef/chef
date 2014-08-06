67395bc0dfef776aa6505bb4652a71dd8d1a0329
Tom Duffield <tom@opscode.com>
2014-08-06 08:46:28 -0500

    hardcode chef and rubygems versions in chef-container to 11.12.8

--- config/projects/chef-container.rb
+++ config/projects/chef-container.rb
@@ -34,6 +34,12 @@ install_dir     "/opt/chef"
 resources_path File.join(files_path, "chef")
 mac_pkg_identifier "com.getchef.pkg.chef-container"
 
+# use the same rubygems as the chef project
+override :rubygems, version: "1.8.29"
+
+# hardcode the version of chef
+override :chef, version: "11.12.8"
+
 dependency "preparation"
 dependency "chef"
 dependency "chef-init"
