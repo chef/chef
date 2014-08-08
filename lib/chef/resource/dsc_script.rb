262f174320be0f1cc0e600dc2d2527dbd3b8266a
Adam Edwards <adamed@opscode.com>
2014-08-08 13:55:41 -0700

    Initial dsc_configuration resource implementation

---
+++ lib/chef/resource/dsc_script.rb
@@ -0,0 +1,101 @@
+#
+# Author:: Adam Edwards (<adamed@getchef.com>)
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class Chef
+  class Resource
+    class DscScript < Chef::Resource
+
+      provides :dsc_script, :on_platforms => ["windows"]
+
+      def initialize(name, run_context=nil)
+        super
+        @allowed_actions.push(:run)
+        @action = 'run'
+        provider(Chef::Provider::DscScript)
+      end
+
+      def code(arg=nil)
+        if arg && command
+          raise ArgumentError, "Only one of 'code' and 'command' properties may be specified"
+        end
+        if arg && configuration_name
+          raise ArgumentError, "Attribute `code` may not be set if `configuration_name` is set"
+        end
+        set_or_return(
+          :code,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def configuration_name(arg=nil)
+        if arg && code
+          raise ArgumentError, "Attribute `configuration_name` may not be set if `code` is set"
+        end
+        set_or_return(
+          :configuration_name,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def command(arg=nil)
+        if arg && code
+          raise ArgumentError, "Only one of 'code' and 'command' properties may be specified"
+        end
+        set_or_return(
+          :command,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def flags(arg=nil)
+        set_or_return(
+          :flags,
+          arg,
+          :kind_of => [ Hash ]
+        )
+      end
+
+      def cwd(arg=nil)
+        set_or_return(
+          :cwd,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def environment(arg=nil)
+        set_or_return(
+          :environment,
+          arg,
+          :kind_of => [ Hash ]
+        )
+      end
+
+      def timeout(arg=nil)
+        set_or_return(
+          :timeout,
+          arg,
+          :kind_of => [ Integer ]
+        )
+      end
+    end
+  end
+end
---
+++ spec/unit/resource/dsc_script_spec.rb
@@ -0,0 +1,69 @@
+#
+# Author:: Adam Edwards (<adamed@getchef.com>)
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe Chef::Resource::DscScript do
+  let(:dsc_test_run_context) {
+    node = Chef::Node.new
+    empty_events = Chef::EventDispatch::Dispatcher.new
+    Chef::RunContext.new(node, {}, empty_events)
+  }
+  let(:dsc_test_resource_name) { 'DSCTest' }
+  let(:dsc_test_resource) {
+    Chef::Resource::DscScript.new(dsc_test_resource_name, dsc_test_run_context) 
+  }
+  let(:configuration_code) {'echo "This is supposed to create a configuration document."'}
+  let(:configuration_path) {'c:/myconfigs/formatc.ps1'}
+  let(:configuration_name) { 'formatme' }
+
+  it "allows the configuration attribute to be set" do
+    dsc_test_resource.code(configuration_code)
+    expect(dsc_test_resource.code).to eq(configuration_code)
+  end
+
+  it "allows the path attribute to be set" do
+    dsc_test_resource.command(configuration_path)
+    expect(dsc_test_resource.command).to eq(configuration_path)
+  end
+
+  it "allows the configuration_name attribute to be set" do
+    dsc_test_resource.configuration_name(configuration_name)
+    expect(dsc_test_resource.configuration_name).to eq(configuration_name)
+  end
+
+  it "raises an ArgumentError exception if an attempt is made to set the configuration attribute when the path attribute is already set" do
+    dsc_test_resource.command(configuration_path)
+    expect { dsc_test_resource.code(configuration_code) }.to raise_error(ArgumentError)
+  end
+
+  it "raises an ArgumentError exception if an attempt is made to set the path attribute when the configuration attribute is already set" do
+    dsc_test_resource.code(configuration_code)
+    expect { dsc_test_resource.command(configuration_path) }.to raise_error(ArgumentError)
+  end
+
+  it "raises an ArgumentError exception if an attempt is made to set the configuration_name attribute when the configuration attribute is already set" do
+    dsc_test_resource.code(configuration_code)
+    expect { dsc_test_resource.configuration_name(configuration_name) }.to raise_error(ArgumentError)
+  end
+
+  it "raises an ArgumentError exception if an attempt is made to set the configuration attribute when the configuration_name attribute is already set" do
+    dsc_test_resource.configuration_name(configuration_name)
+    expect { dsc_test_resource.code(configuration_code) }.to raise_error(ArgumentError)
+  end
+end
