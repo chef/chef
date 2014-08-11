7e829c812b65465c8686294c9796c353d1b314a9
Christopher Webber <cwebber@getchef.com>
2014-08-11 12:53:35 -0700

    Switches to https://supermarket.getchef.com
    This modifies knife cookbook site to use supermarket instead of
    cookbooks.opscode.com. Additionally, it switches to using HTTPS instead
    of HTTP.

--- lib/chef/knife/cookbook_site_unshare.rb
+++ lib/chef/knife/cookbook_site_unshare.rb
@@ -41,7 +41,7 @@ class Chef
         confirm "Do you really want to unshare the cookbook #{@cookbook_name}"
 
         begin
-          rest.delete_rest "http://cookbooks.opscode.com/api/v1/cookbooks/#{@name_args[0]}"
+          rest.delete_rest "https://supermarket.getchef.com/api/v1/cookbooks/#{@name_args[0]}"
         rescue Net::HTTPServerException => e
           raise e unless e.message =~ /Forbidden/
           ui.error "Forbidden: You must be the maintainer of #{@cookbook_name} to unshare it."
