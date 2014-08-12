7841cd0a6864600012c2fe43c8483c3baea844e2
James Belchamber <james@belchamber.com>
2014-08-12 09:30:38 +0100

    added tests for mount remounting

--- spec/unit/provider/mount/windows_spec.rb
+++ spec/unit/provider/mount/windows_spec.rb
@@ -111,6 +111,20 @@ describe Chef::Provider::Mount::Windows do
         allow(@current_resource).to receive(:mounted).and_return(true)
         @provider.mount_fs
       end
+
+      it "should remount the filesystem if it is mounted and the options have changed" do
+        @vol.should_receive(:add).with(:remote => @new_resource.device,
+                                       :username => @new_resource.username,
+                                       :domainname => @new_resource.domain,
+                                       :password => @new_resource.password)
+        @provider.mount_fs
+      end
+
+      it "should not mount the filesystem if it is mounted and the options have not changed" do
+        @vol.should_not_receive(:add)
+        @current_resource.stub(:mounted).and_return(true)
+        @provider.mount_fs
+      end
     end
 
     describe "when unmounting a file system" do
