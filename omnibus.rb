009df41515efdc92f6e235260282438e9def40c8
Seth Vargo <sethvargo@gmail.com>
2014-08-28 17:51:29 -0400

    Remove example omnibus.rb and use a real one

--- omnibus.rb
+++ omnibus.rb
@@ -1,9 +1,46 @@
-# s3_access_key "something"
-# s3_secret_key "something"
+#
+# This file is used to configure the Omnibus projects in this repo. It contains
+# some minimal configuration examples for working with Omnibus. For a full list
+# of configurable options, please see the documentation for +omnibus/config.rb+.
+#
 
-s3_bucket "opscode-omnibus-cache"
+# Build internally
+# ------------------------------
+# By default, Omnibus uses system folders (like +/var+ and +/opt+) to build and
+# cache components. If you would to build everything internally, you can
+# uncomment the following options. This will prevent the need for root
+# permissions in most cases.
+#
+# Uncomment this line to change the default base directory to "local"
+# -------------------------------------------------------------------
+# base_dir './local'
+#
+# Alternatively you can tune the individual values
+# ------------------------------------------------
+# cache_dir     './local/omnibus/cache'
+# git_cache_dir './local/omnibus/cache/git_cache'
+# source_dir    './local/omnibus/src'
+# build_dir     './local/omnibus/build'
+# package_dir   './local/omnibus/pkg'
+# package_tmp   './local/omnibus/pkg-tmp'
+
+# Disable git caching
+# ------------------------------
+# use_git_caching false
+
+# Enable S3 asset caching
+# ------------------------------
 use_s3_caching true
-solaris_compiler "gcc"
-build_dmg true
-sign_pkg true
-signing_identity "Developer ID Installer: Opscode Inc. (9NBR9JL2R2)"
+s3_access_key  ENV['AWS_ACCESS_KEY_ID']
+s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
+s3_bucket      'opscode-omnibus-cache'
+
+# Customize compiler bits
+# ------------------------------
+solaris_compiler 'gcc'
+# build_retries 5
+
+# Load additional software
+# ------------------------------
+# software_gems ['omnibus-software', 'my-company-software']
+# local_software_dirs ['/path/to/local/software']
