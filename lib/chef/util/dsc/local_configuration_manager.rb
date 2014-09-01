c90fba5be7d07a53d83e43513bdad0948a1484a9
Adam Edwards <adamed@opscode.com>
2014-09-01 09:15:21 -0700

    No erroraction stop since that filters the excepiton

--- lib/chef/util/dsc/local_configuration_manager.rb
+++ lib/chef/util/dsc/local_configuration_manager.rb
@@ -74,7 +74,7 @@ class Chef::Util::DSC
 
     def lcm_command_code(configuration_path, test_only_parameters)
       <<-EOH
-$ProgressPreference = 'SilentlyContinue';start-dscconfiguration -path #{@configuration_path} -wait -force -erroraction 'stop' #{test_only_parameters}
+$ProgressPreference = 'SilentlyContinue';start-dscconfiguration -path #{@configuration_path} -wait -force #{test_only_parameters}
 EOH
     end
 
@@ -83,7 +83,7 @@ EOH
           # LCM returns an error if any of the resources do not support the opptional What-If
           Chef::Log::warn("Received error while testing configuration due to resource not supporting 'WhatIf'")
         elsif output_has_dsc_module_failure?(what_if_exception_output)
-          Chef::Log::warn('Received error while testing configuration due to a module for an imported resource possibly not being fully installed:\n#{what_if_exception_output.gsub(/\s+/, ' ')}')
+          Chef::Log::warn("Received error while testing configuration due to a module for an imported resource possibly not being fully installed:\n#{what_if_exception_output.gsub(/\s+/, ' ')}")
         else
           raise Chef::Exceptions::PowershellCmdletException, "Powershell Cmdlet failed: #{what_if_exception_output.gsub(/\s+/, ' ')}"
         end
@@ -92,7 +92,7 @@ EOH
     def output_has_dsc_module_failure?(what_if_output)
       !! (what_if_output.match(/\sCimException/) &&
         what_if_output =~ /ProviderOperationExecutionFailure/ &&
-        what_if_output =~ /\smodule is installed/)
+        what_if_output =~ /\smodule\s+is\s+installed/)
     end
 
     def configuration_update_required?(what_if_output)
