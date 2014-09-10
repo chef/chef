d70d2bdd723c57ed6225817ad61e347d21b16096
Seth Chisamore <schisamo@getchef.com>
2014-09-10 14:39:03 -0400

    Bump Ruby/RubyGems/Bundler versions; Fixes ##228

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -38,10 +38,10 @@ else
   install_dir "#{default_root}/#{name}"
 end
 
-override :bundler,        version: "1.7.0"
+override :bundler,        version: "1.7.2"
 override :ruby,           version: "2.1.2"
 override :'ruby-windows', version: "2.0.0-p451"
-override :rubygems,       version: "2.2.1"
+override :rubygems,       version: "2.4.1"
 
 dependency "preparation"
 dependency "chef"
