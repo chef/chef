60d03db307e19b783aceeef1692afc3127bfcdbf
Seth Chisamore <schisamo@getchef.com>
2014-09-12 15:26:52 -0400

    Don’t escape back slashes in the verify scripts

--- jenkins/verify-chef-container.sh
+++ jenkins/verify-chef-container.sh
@@ -4,10 +4,10 @@ export PATH=/opt/chef/bin:$PATH
 
 # Ensure the calling environment (disapproval look Bundler) does not
 # infect our Ruby environment created by the `chef-int` cli.
-for ruby_env_var in RUBYOPT \\
-                    BUNDLE_BIN_PATH \\
-                    BUNDLE_GEMFILE \\
-                    GEM_PATH \\
+for ruby_env_var in RUBYOPT \
+                    BUNDLE_BIN_PATH \
+                    BUNDLE_GEMFILE \
+                    GEM_PATH \
                     GEM_HOME
 do
   unset $ruby_env_var
--- jenkins/verify-chef.sh
+++ jenkins/verify-chef.sh
@@ -27,10 +27,10 @@ fi
 
 # Ensure the calling environment (disapproval look Bundler) does not
 # infect our Ruby environment created by the `chef-client` cli.
-for ruby_env_var in RUBYOPT \\
-                    BUNDLE_BIN_PATH \\
-                    BUNDLE_GEMFILE \\
-                    GEM_PATH \\
+for ruby_env_var in RUBYOPT \
+                    BUNDLE_BIN_PATH \
+                    BUNDLE_GEMFILE \
+                    GEM_PATH \
                     GEM_HOME
 do
   unset $ruby_env_var
