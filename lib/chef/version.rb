5aa790b1ee4aac0771047f650eec207e25d5fcc7
Lamont Granquist <lamont@scriptkiddie.org>
2014-09-16 18:29:34 -0700

    bumping version to 11.16.2

--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -17,7 +17,7 @@
 
 class Chef
   CHEF_ROOT = File.dirname(File.expand_path(File.dirname(__FILE__)))
-  VERSION = '11.16.0'
+  VERSION = '11.16.2'
 end
 
 # NOTE: the Chef::Version class is defined in version_class.rb
