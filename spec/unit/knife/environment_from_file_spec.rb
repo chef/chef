e85e1399270192cf9ee29c5263a9b7aca4ca92fa
Claire McQuin <claire@getchef.com>
2014-09-19 13:03:33 -0700

    Fix broken spec.

--- spec/unit/knife/environment_from_file_spec.rb
+++ spec/unit/knife/environment_from_file_spec.rb
@@ -57,8 +57,8 @@ describe Chef::Knife::EnvironmentFromFile do
       end
 
       it "loads all environments with -a" do
-        File.stub(:expand_path).with("./environments/*.{json,rb}").and_return("/tmp/environments")
-        Dir.stub(:glob).with("/tmp/environments").and_return(["spec.rb", "apple.rb"])
+        File.stub(:expand_path).with("./environments/").and_return("/tmp/environments")
+        Dir.stub(:glob).with("/tmp/environments/*.{json,rb}").and_return(["spec.rb", "apple.rb"])
         @knife.name_args = []
         @knife.stub(:config).and_return({:all => true})
         @environment.should_receive(:save).twice
