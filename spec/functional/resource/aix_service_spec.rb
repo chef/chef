36aca4d584cba3600789e0668aaa7eb67dfa84c5
kaustubh <kaustubh.deo@gmail.com>
2014-09-22 10:02:30 +0530

    fix copyright

--- spec/functional/resource/aix_service_spec.rb
+++ spec/functional/resource/aix_service_spec.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 #
 # Author:: Kaustubh Deorukhkar (<kaustubh@clogeny.com>)
-# Copyright:: Copyright (c) 2014 Opscode, Inc.
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
