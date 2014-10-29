b92c309b0f1aa0837f76ab89d6c81c36076ceca9
Claire McQuin <claire@getchef.com>
2014-10-29 14:55:12 -0700

    Remove treat_symbols_as_metadata_keys_with_true_values = true, now true by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,12 +18,6 @@
 # If you need to add anything in here, don't.
 # Add it to one of the files in spec/support
 
-# Configure this first so it doesn't trigger annoying warning when we use it.
-# Main rspec configuration comes later
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-end
-
 # Abuse ruby's constant lookup to avoid undefined constant errors
 module Shell
   JUST_TESTING_MOVE_ALONG = true unless defined? JUST_TESTING_MOVE_ALONG
@@ -159,7 +153,6 @@ RSpec.configure do |config|
   }
 
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.before(:each) do
     Chef::Config.reset
