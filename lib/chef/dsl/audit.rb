7d6e9f1e603f21241a77c61441f98f05d7d4ace4
Claire McQuin <claire@getchef.com>
2014-11-05 13:35:21 -0800

    add rspec/its

--- lib/chef/dsl/audit.rb
+++ lib/chef/dsl/audit.rb
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#require 'chef/audit'
+
 require 'chef/audit/chef_example_group'
 
 class Chef
