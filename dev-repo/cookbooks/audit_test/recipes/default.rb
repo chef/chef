71ca48ea4c872bee8d887b1db1014c526c4a0050
Serdar Sutay <serdar@opscode.com>
2014-11-13 14:23:44 -0800

    Basic test for audit mode.

---
+++ dev-repo/cookbooks/audit_test/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: audit_test
+# Recipe:: default
+#
+# Copyright (c) 2014 The Authors, All Rights Reserved.
+
+controls "basic control" do
+  it "should pass" do
+    expect(2 - 2).to eq(0)
+  end
+end
