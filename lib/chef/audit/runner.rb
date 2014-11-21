246c9dc60e1bed3b237b2454da445c3ef5c92b7e
Claire McQuin <claire@getchef.com>
2014-11-21 10:05:55 -0800

    Use a central runner

--- lib/chef/audit/runner.rb
+++ lib/chef/audit/runner.rb
@@ -30,8 +30,68 @@ class Chef
       end
 
       def run
-        run_context.controls_groups.each { |ctls_grp| ctls_grp.run }
+        setup
+        register_controls
+        runner = RSpec::Core::Runner.new(nil)
+        runner.run_specs(RSpec.world.ordered_example_groups)
       end
+
+      private
+      def setup
+        require_deps
+        set_streams
+        add_formatters
+        disable_should_syntax
+        configure_specinfra
+        add_example_group_methods
+      end
+
+      def register_controls
+        run_context.controls.each do |name, group|
+          ctl_grp = RSpec::Core::ExampleGroup.__controls__(*group[:args], &group[:block])
+          RSpec.world.register(ctl_grp)
+        end
+      end
+
+      def require_deps
+        require 'rspec'
+        require 'rspec/its'
+        require 'specinfra'
+        require 'serverspec/helper'
+        require 'serverspec/matcher'
+        require 'serverspec/subject'
+        require 'chef/audit/audit_event_proxy'
+        require 'chef/audit/rspec_formatter'
+      end
+
+      def set_streams
+        RSpec.configuration.output_stream = Chef::Config[:log_location]
+        RSpec.configuration.error_stream = Chef::Config[:log_location]
+      end
+
+      def add_formatters
+        RSpec.configuration.add_formatter(Chef::Audit::AuditEventProxy)
+        RSpec.configuration.add_formatter(Chef::Audit::RspecFormatter)
+        Chef::Audit::AuditEventProxy.events = run_context.events
+      end
+
+      def disable_should_syntax
+        RSpec.configure do |config|
+          config.expect_with :rspec do |c|
+            c.syntax = :expect
+          end
+        end
+      end
+
+      def configure_specinfra
+        Specinfra.configuration.backend = :exec
+      end
+
+      def add_example_group_methods
+        RSpec::Core::ExampleGroup.define_example_group_method :__controls__
+        RSpec::Core::ExampleGroup.define_example_group_method :control
+      end
+
     end
   end
 end
