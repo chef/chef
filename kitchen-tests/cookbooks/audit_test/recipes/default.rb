0ff708f8e8097643619f51398de4591caedfb822
Claire McQuin <claire@getchef.com>
2014-11-20 16:32:38 -0800

    Wrap in control block until we get top-level tests working.

--- kitchen-tests/cookbooks/audit_test/recipes/default.rb
+++ kitchen-tests/cookbooks/audit_test/recipes/default.rb
@@ -5,7 +5,9 @@
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
 controls "basic control" do
-  it "should pass" do
-    expect(2 - 2).to eq(0)
+  control "math" do
+    it "should pass" do
+      expect(2 - 2).to eq(0)
+    end
   end
 end
