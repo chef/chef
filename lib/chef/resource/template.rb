d728f5b8d8179bd81b2ff44ef8ddae89e573bebe
Xabier de Zuazo <xabier@onddo.com>
2014-11-27 23:00:56 +0100

    Update some ruby 1.8 related comments (issue #2497)

--- lib/chef/resource/template.rb
+++ lib/chef/resource/template.rb
@@ -102,9 +102,8 @@ class Chef
       #
       # ==== Method Arguments:
       # Helper methods can also take arguments. The syntax available for
-      # argument specification will be dependent on ruby version. Ruby 1.8 only
-      # supports a subset of the argument specification syntax available for
-      # method definition, whereas 1.9 supports the full syntax.
+      # argument specification supports full syntax available for method
+      # definition.
       #
       # Continuing the above example of simplifying attribute access, we can
       # define a helper to look up app-specific attributes like this:
