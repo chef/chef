6c6849e274af1b4ff74d7132b3a1fd4e2f27af5f
tyler-ball <tyleraball@gmail.com>
2014-12-02 16:12:24 -0800

    Updating JSON schema version since new fields were added

--- lib/chef/audit/audit_reporter.rb
+++ lib/chef/audit/audit_reporter.rb
@@ -28,7 +28,7 @@ class Chef
       attr_reader :rest_client, :audit_data, :ordered_control_groups, :run_status
       private :rest_client, :audit_data, :ordered_control_groups, :run_status
 
-      PROTOCOL_VERSION = '0.1.0'
+      PROTOCOL_VERSION = '0.1.1'
 
       def initialize(rest_client)
         @rest_client = rest_client
