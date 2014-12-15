0bc237e655f45187de005db6e750e0c4a640fc51
Jay Mundrawala <jdmundrawala@gmail.com>
2014-12-15 09:25:20 -0800

    Changed spec description

--- spec/unit/provider/package/windows/msi_spec.rb
+++ spec/unit/provider/package/windows/msi_spec.rb
@@ -60,14 +60,14 @@ describe Chef::Provider::Package::Windows::MSI do
   end
 
   describe "install_package" do
-    it "calls shell_out!" do
+    it "calls msiexec /qn /i" do
       expect(provider).to receive(:shell_out!).with(/msiexec \/qn \/i \"calculator.msi\"/, kind_of(Hash))
       provider.install_package("unused", "unused")
     end
   end
 
   describe "remove_package" do
-    it "calls shell_out!" do
+    it "calls msiexec /qn /x" do
       expect(provider).to receive(:shell_out!).with(/msiexec \/qn \/x \"calculator.msi\"/, kind_of(Hash))
       provider.remove_package("unused", "unused")
     end
