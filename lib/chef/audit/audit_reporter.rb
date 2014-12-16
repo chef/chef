3e52984f5868559dc273440ef3815aadf352cb61
tyler-ball <tyleraball@gmail.com>
2014-12-16 09:33:50 -0800

    Adding license information

--- lib/chef/audit/audit_reporter.rb
+++ lib/chef/audit/audit_reporter.rb
@@ -1,7 +1,7 @@
 #
-# Auther:: Tyler Ball (<tball@getchef.com>)
+# Auther:: Tyler Ball (<tball@chef.io>)
 #
-# Copyright:: Copyright (c) 2014 Opscode, Inc.
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
