ec6b5ae47ffd6cab07515a82fbb809dd40663b83
Ryan Chipman <rchipman@mit.edu>
2015-02-01 16:24:59 -0500

    workaround for ohai bug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -84,7 +84,7 @@ Dir["spec/support/**/*.rb"].
 OHAI_SYSTEM = Ohai::System.new
 OHAI_SYSTEM.all_plugins("platform")
 TEST_PLATFORM = OHAI_SYSTEM["platform"].dup.freeze
-TEST_PLATFORM_VERSION = OHAI_SYSTEM["platform_version"].dup.freeze if OHAI_SYSTEM["platform_version"]
+TEST_PLATFORM_VERSION = OHAI_SYSTEM["platform_version"] ? OHAI_SYSTEM["platform_version"].dup.freeze : "no_version"
 
 RSpec.configure do |config|
   config.include(Matchers)
@@ -155,7 +155,7 @@ RSpec.configure do |config|
     type, target_provider = criteria.first
 
     platform = TEST_PLATFORM.dup
-    platform_version = TEST_PLATFORM_VERSION.dup if TEST_PLATFORM_VERSION
+    platform_version = TEST_PLATFORM_VERSION.dup
 
     begin
       provider_for_running_platform = Chef::Platform.find_provider(platform, platform_version, type)
