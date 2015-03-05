dad7323e652fb7026c19ef79e1f818c50288dae8
Seth Chisamore <schisamo@chef.io>
2015-03-05 15:56:02 -0500

    Update Chef’s verify scripts
    This updates the verify scripts to execute Chef’s RSpec test suite. The
    verify scripts are executed by the test stage of our CI pipelines.

--- jenkins/verify-chef.sh
+++ jenkins/verify-chef.sh
@@ -2,7 +2,13 @@
 
 # $PROJECT_NAME is set by Jenkins, this allows us to use the same script to verify
 # Chef and Angry Chef
-export PATH=/opt/$PROJECT_NAME/bin:$PATH
+PATH=/opt/$PROJECT_NAME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH
+
+# We don't want to add the embedded bin dir to the main PATH as this
+# could mask issues in our binstub shebangs.
+EMBEDDED_BIN_DIR=/opt/$PROJECT_NAME/embedded/bin
+export PATH
 
 # sanity check that we're getting symlinks from the pre-install script
 if [ ! -L "/usr/bin/chef-client" ]; then
@@ -37,3 +43,21 @@ do
 done
 
 chef-client --version
+
+# Exercise various packaged tools to validate binstub shebangs
+$EMBEDDED_BIN_DIR/ruby --version
+$EMBEDDED_BIN_DIR/gem --version
+$EMBEDDED_BIN_DIR/bundle --version
+$EMBEDDED_BIN_DIR/rspec --version
+
+# ffi-yajl must run in c-extension mode or we take perf hits, so we force it
+# before running rspec so that we don't wind up testing the ffi mode
+FORCE_FFI_YAJL=ext
+export FORCE_FFI_YAJL
+
+PATH=/opt/$PROJECT_NAME/bin:/opt/$PROJECT_NAME/embedded/bin:$PATH
+export PATH
+
+# Test against the appbundle'd Chef
+cd /opt/$PROJECT_NAME/embedded/apps/chef
+sudo env PATH=$PATH TERM=xterm bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o $WORKSPACE/test.xml -f documentation spec/functional spec/unit
