f281ec618bc1263ff3905368d6c2eb32b8608228
anjutiwari <anjutiwari5@gmail.com>
2015-04-09 21:33:35 -0700

    Modified exception message for invalid version

--- lib/chef/version_class.rb
+++ lib/chef/version_class.rb
@@ -57,9 +57,9 @@ class Chef
       @major, @minor, @patch =
         case str.to_s
         when /^(\d+)\.(\d+)\.(\d+)$/
-          [ $1, $2, $3 ]
+          [ $1.to_i, $2.to_i, $3.to_i ]
         when /^(\d+)\.(\d+)$/
-          [ $1, $2, 0 ]
+          [ $1.to_i, $2.to_i, 0 ]
         else
           msg = "'#{str.to_s}' does not match 'x.y.z' or 'x.y', Where x,y and z are numbers"
           raise Chef::Exceptions::InvalidCookbookVersion.new( msg )
