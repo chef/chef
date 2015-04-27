f0d48e98e3f14a4a41c97021edc731a9b491618d
Lamont Granquist <lamont@scriptkiddie.org>
2015-04-27 10:40:57 -0700

    typo fix in comment

--- lib/chef/provider_resolver.rb
+++ lib/chef/provider_resolver.rb
@@ -47,7 +47,7 @@ class Chef
     def enabled_handlers
       @enabled_handlers ||=
         providers.select do |klass|
-          # NB: this is different from resoruce_resolver which must pass a resource_name
+          # NB: this is different from resource_resolver which must pass a resource_name
           # FIXME: deprecate this and normalize on passing resource_name here
           klass.provides?(node, resource)
         end.sort {|a,b| a.to_s <=> b.to_s }
