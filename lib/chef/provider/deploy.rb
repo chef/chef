3614b228567c280a0a999edf674666c2c5924517
John Keiser <john@johnkeiser.com>
2015-05-15 23:05:55 -0700

    Give run contexts children instead of using external duping

--- lib/chef/provider/deploy.rb
+++ lib/chef/provider/deploy.rb
@@ -375,9 +375,7 @@ class Chef
       def gem_resource_collection_runner
         gems_collection = Chef::ResourceCollection.new
         gem_packages.each { |rbgem| gems_collection.insert(rbgem) }
-        gems_run_context = run_context.dup
-        gems_run_context.resource_collection = gems_collection
-        Chef::Runner.new(gems_run_context)
+        Chef::Runner.new(run_context.create_child)
       end
 
       def gem_packages
