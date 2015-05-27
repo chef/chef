6d1b736f6d581fb6a63f5b51708b0a75c566321e
John Keiser <john@johnkeiser.com>
2015-05-27 10:15:17 -0700

    Don't require a file we are already in

--- lib/chef/client/notification_registry.rb
+++ lib/chef/client/notification_registry.rb
@@ -18,8 +18,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'chef/client/notification_registry'
-
 class Chef
   class Client
     module NotificationRegistry
