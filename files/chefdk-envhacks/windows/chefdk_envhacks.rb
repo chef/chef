e93809c3573bad95dbea7bc6f67b3c523d56c326
Jay Mundrawala <jdmundrawala@gmail.com>
2015-06-09 20:57:59 -0700

    Added chefdk_envhacks
    These hacks to the environment pick a reasonable HOME directory
    on Windows if `ENV['HOME']` does not exist. Should should fix
    issues like https://github.com/chef/chef-dk/issues/415
    
    This file can be run automagically by setting RUBYOPT='-rchefdk_envhacks'

---
+++ files/chefdk-envhacks/windows/chefdk_envhacks.rb
@@ -0,0 +1,49 @@
+## Environment hacks for running Ruby with ChefDK ##
+# ENV['HOME'] is not set by default on Windows. We need to set this to
+# something sensible since a lot of Ruby code depends on it. It is important 
+# for this directory to exist and be available, so we are introducing logic
+# here to pick a working HOME
+#
+# You can find this file the repo at https://github.com/chef/omnibus-chef
+
+if !ENV['HOME'] || !File.exists?(ENV['HOME'])
+  old_home = ENV['HOME']
+  found = false
+  alternate_homes = ["#{ENV['HOMEDRIVE']}#{ENV['HOMEPATH']}", ENV['USERPROFILE']]
+
+  alternate_homes.each do |path|
+    if File.exists?(path)
+      ENV['HOME'] = path
+      found = true
+      break
+    end
+  end
+
+  STDERR.puts <<-EOF
+The HOME environment variable (#{old_home}) was not set to a valid value.
+
+  EOF
+
+  if found
+    STDERR.puts <<-EOF
+Falling back to using #{ENV['HOME']} as the home directory. If you would like
+to use another directory as HOME, please set the HOME environment variable.
+    EOF
+  else
+    STDERR.puts <<-EOF
+Could note find a suitable HOME directory. Tried:
+#{alternate_homes.join("\n")}
+
+Some Ruby binaries may not function correctly. You can set the HOME 
+environment variable to a directory that exists to try to solve this.
+    EOF
+  end
+
+  STDERR.puts <<-EOF
+
+If you would not like ChefDK to try to fix the HOME environment variable,
+check the RUBYOPT environment variable. Removing "-rchefdk_envhacks" will
+prevent this modification to your HOME environment variable.
+
+  EOF
+end
