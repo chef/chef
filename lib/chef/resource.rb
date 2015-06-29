b45894a439bcff3a49af2bf5e817cbe662a0329a
Jay Mundrawala <jdmundrawala@gmail.com>
2015-06-29 10:17:10 -0700

    Get tests passing

--- lib/chef/resource.rb
+++ lib/chef/resource.rb
@@ -1157,7 +1157,7 @@ class Chef
       action = action.to_sym
       new_action_provider_class.action(action, &recipe_block)
       self.allowed_actions += [ action ]
-      default_action action if default_action == :nothing
+      default_action action if default_action == [:nothing]
     end
 
     #
