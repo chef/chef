fdd5d0912a2565301bab784a7c7d3b1ee0bc8d20
John Keiser <john@johnkeiser.com>
2015-07-06 13:17:04 -0600

    Merge branch 'jk/3634'

--- lib/chef/dsl/resources.rb
+++ lib/chef/dsl/resources.rb
@@ -10,14 +10,16 @@ class Chef
       def self.add_resource_dsl(dsl_name)
         begin
           module_eval(<<-EOM, __FILE__, __LINE__+1)
-            def #{dsl_name}(name=nil, created_at=nil, &block)
-              declare_resource(#{dsl_name.inspect}, name, created_at || caller[0], &block)
+            def #{dsl_name}(*args, &block)
+              Chef::Log.deprecation("Cannot create resource #{dsl_name} with more than one argument. All arguments except the name (\#{args[0].inspect}) will be ignored. This will cause an error in Chef 13. Arguments: \#{args}") if args.size > 1
+              declare_resource(#{dsl_name.inspect}, args[0], caller[0], &block)
             end
           EOM
         rescue SyntaxError
           # Handle the case where dsl_name has spaces, etc.
-          define_method(dsl_name.to_sym) do |name=nil, created_at=nil, &block|
-            declare_resource(dsl_name, name, created_at || caller[0], &block)
+          define_method(dsl_name.to_sym) do |*args, &block|
+            Chef::Log.deprecation("Cannot create resource #{dsl_name} with more than one argument. All arguments except the name (#{args[0].inspect}) will be ignored. This will cause an error in Chef 13. Arguments: #{args}") if args.size > 1
+            declare_resource(dsl_name, args[0], caller[0], &block)
           end
         end
       end
