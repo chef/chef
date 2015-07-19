d62844985c5e5493580605355e8376c8acc4009e
Noah Kantrowitz <noah@coderanger.net>
2015-07-19 11:19:11 -0700

    Remove warnings from tests too.

--- spec/unit/recipe_spec.rb
+++ spec/unit/recipe_spec.rb
@@ -154,8 +154,6 @@ describe Chef::Recipe do
           end
 
           it "selects the first one alphabetically" do
-            expect(Chef::Log).to receive(:warn).with("You declared a new resource TottenhamHotspur for resource football, but it comes alphabetically after Sounders and has the same filters ({:platform=>\"nbc_sports\"}), so it will not be used. Use override: true if you want to use it for football.")
-
             Sounders.provides :football, platform: "nbc_sports"
             TottenhamHotspur.provides :football, platform: "nbc_sports"
 
