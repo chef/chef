9ac8de1de500bfa97bb15ec7d663ea419645b93d
Tyler Ball <tyler-ball@users.noreply.github.com>
2015-08-05 16:30:17 -0600

    Updating to use the latest chef-provisioning-aws
    In anticipation of 0.7.0 release

--- config/projects/chefdk.rb
+++ config/projects/chefdk.rb
@@ -76,7 +76,7 @@ override :zlib,           version: "1.2.8"
 override :'chef-provisioning-fog', version: "v0.13.2"
 override :'chef-provisioning-vagrant', version: "v0.9.0"
 override :'chef-provisioning-azure', version: "v0.3.2"
-override :'chef-provisioning-aws', version: "v1.3.0"
+override :'chef-provisioning-aws', version: "v1.3.1"
 
 dependency "preparation"
 dependency "chefdk"
