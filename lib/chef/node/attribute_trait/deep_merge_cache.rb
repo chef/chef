173764fd5bf26a30dee99a47ab58eadf6da82ee3
Lamont Granquist <lamont@scriptkiddie.org>
2015-08-08 20:57:39 -0700

    more perf tweaks

--- lib/chef/node/attribute_trait/deep_merge_cache.rb
+++ lib/chef/node/attribute_trait/deep_merge_cache.rb
@@ -12,16 +12,16 @@ class Chef
         end
 
         def [](key)
+          if __deep_merge_cache.key?(key) && __deep_merge_cache[key][:__deep_merge_cache]
+            return __deep_merge_cache[key].regular_reader(:__deep_merge_cache)
+          end
+
           if is_a?(Hash) && self.class.deep_merge_cache_population
-            if __deep_merge_cache.key?(key) && __deep_merge_cache[key][:__deep_merge_cache]
-              return __deep_merge_cache[key].regular_reader(:__deep_merge_cache)
-            else
-              val = super
-              __deep_merge_cache[key] ||= Chef::Node::Mash.new(wrapped_object: {})
-              __deep_merge_cache[key].regular_writer(:__deep_merge_cache, val)
-              val.__deep_merge_cache = __deep_merge_cache[key] if val.is_a?(DeepMergeCache)
-              return val
-            end
+            val = super
+            __deep_merge_cache[key] ||= Chef::Node::Mash.new(wrapped_object: {})
+            __deep_merge_cache[key].regular_writer(:__deep_merge_cache, val)
+            val.__deep_merge_cache = __deep_merge_cache[key] if val.is_a?(DeepMergeCache)
+            return val
           else
             val = super
             val.__deep_merge_cache = __deep_merge_cache[key] if val.is_a?(DeepMergeCache)
