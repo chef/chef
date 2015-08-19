a117ba358c16b2f22992d04adb5289e3e483f582
John Keiser <john@johnkeiser.com>
2015-08-19 15:15:13 -0700

    Chef-DK -> ChefDK; display error for 10s if there is an error

--- resources/chefdk/msi/assets/start-chefdk.ps1
+++ resources/chefdk/msi/assets/start-chefdk.ps1
@@ -1,22 +1,29 @@
-$conemulocation = "$env:programfiles\ConEmu\Conemu64.exe"
-$chefdkinit = 'chef shell-init powershell | iex; Import-Module chef; $env:CHEFDK_ENV_FIX = 1'
-$chefdkgreeting = "echo 'PowerShell $($PSVersionTable.psversion.tostring()) ($([System.Environment]::OSVersion.VersionString))';write-host -foregroundcolor darkyellow 'Ohai, welcome to Chef-DK!`n'"
-$chefdkcommand = "$chefdkinit;$chefdkgreeting"
-$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = [Security.Principal.WindowsPrincipal] $identity
-$titleprefix = ""
-if($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    $titleprefix = "Administrator: "
+Try {
+    $conemulocation = "$env:programfiles\ConEmu\Conemu64.exe"
+    $chefdkinit = 'chef shell-init powershell | iex; Import-Module chef; $env:CHEFDK_ENV_FIX = 1'
+    $chefdkgreeting = "echo 'PowerShell $($PSVersionTable.psversion.tostring()) ($([System.Environment]::OSVersion.VersionString))';write-host -foregroundcolor darkyellow 'Ohai, welcome to ChefDK!`n'"
+    $chefdkcommand = "$chefdkinit;$chefdkgreeting"
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity
+    $titleprefix = ""
+    if($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {
+        $titleprefix = "Administrator: "
+    }
+    
+    $chefdktitle = "$($titleprefix)ChefDK ($env:username)"
+    
+    if ( test-path $conemulocation )
+    {
+        start-process $conemulocation -argumentlist '/title',"`"$chefdktitle`"",'/cmd','powershell.exe','-noexit','-command',$chefdkcommand
+    }
+    else
+    {
+        start-process powershell.exe -argumentlist '-noexit','-command',"$chefdkcommand; (get-host).ui.rawui.windowtitle = '$chefdktitle'"
+    }
 }
-
-$chefdktitle = "$($titleprefix)Chef-DK ($env:username)"
-
-if ( test-path $conemulocation )
+Catch
 {
-    start-process $conemulocation -argumentlist '/title',"`"$chefdktitle`"",'/cmd','powershell.exe','-noexit','-command',$chefdkcommand
-}
-else
-{
-    start-process powershell.exe -argumentlist '-noexit','-command',"$chefdkcommand; (get-host).ui.rawui.windowtitle = '$chefdktitle'"
+    sleep 10
+    Throw
 }
