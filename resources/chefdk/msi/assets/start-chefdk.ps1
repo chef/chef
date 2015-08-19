a8b535c3e371f3107fb70f7990bd85aa4d52d396
John Keiser <john@johnkeiser.com>
2015-08-19 14:19:46 -0700

    Add ChefDK shortcut for Windows

---
+++ resources/chefdk/msi/assets/start-chefdk.ps1
@@ -0,0 +1,22 @@
+$conemulocation = "$env:programfiles\ConEmu\Conemu64.exe"
+$chefdkinit = 'chef shell-init powershell | iex; Import-Module chef; $env:CHEFDK_ENV_FIX = 1'
+$chefdkgreeting = "echo 'PowerShell $($PSVersionTable.psversion.tostring()) ($([System.Environment]::OSVersion.VersionString))';write-host -foregroundcolor darkyellow 'Ohai, welcome to Chef-DK!`n'"
+$chefdkcommand = "$chefdkinit;$chefdkgreeting"
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = [Security.Principal.WindowsPrincipal] $identity
+$titleprefix = ""
+if($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    $titleprefix = "Administrator: "
+}
+
+$chefdktitle = "$($titleprefix)Chef-DK ($env:username)"
+
+if ( test-path $conemulocation )
+{
+    start-process $conemulocation -argumentlist '/title',"`"$chefdktitle`"",'/cmd','powershell.exe','-noexit','-command',$chefdkcommand
+}
+else
+{
+    start-process powershell.exe -argumentlist '-noexit','-command',"$chefdkcommand; (get-host).ui.rawui.windowtitle = '$chefdktitle'"
+}
