a053dc3761fe9d3137a55fcac9f8874e1da82ed4
Steven Danna <steve@chef.io>
2015-08-25 17:29:48 +0100

    Fix copyright notice in gem_glob_loader

--- lib/chef/knife/core/gem_glob_loader.rb
+++ lib/chef/knife/core/gem_glob_loader.rb
@@ -1,6 +1,6 @@
 # Author:: Christopher Brown (<cb@opscode.com>)
 # Author:: Daniel DeLeo (<dan@opscode.com>)
-# Copyright:: Copyright (c) 2009, 2011 Opscode, Inc.
+# Copyright:: Copyright (c) 2009-2015 Chef Software, Inc
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
