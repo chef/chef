ff00ed1121828d0c7434ee2a6f160121ddf018fb
Chris Doherty <cdoherty@chef.io>
2015-09-09 14:14:59 -0700

    Revert part of commit 58ce64d0a95c7e5ba16ff8f902b5e9ff855e7ef0 (PR #474) by downgrading RubyGems from 2.4.8 to 2.4.4, to stop the "superclass mismatch for class Notification" error.

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -42,11 +42,7 @@ override :bundler,        version: "1.10.6"
 override :ruby,           version: "2.1.6"
 
 override :'ruby-windows', version: "2.0.0-p645"
-if windows?
-  override :rubygems,       version: "jdm/2.4.8-patched"
-else
-  override :rubygems,       version: "2.4.8"
-end
+override :rubygems,       version: "2.4.4"
 
 # Chef Release version pinning
 override :chef, version: ENV['CHEF_VERSION'] || "master"
