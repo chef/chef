55f9fe2812a071c09a6a1ae2a6a0e2d2bc89ff5a
Salim Alam <salam@chef.io>
2015-09-30 14:46:32 -0700

    Use accessor instead of class variable (cleanup)

--- lib/chef/provider/template/content.rb
+++ lib/chef/provider/template/content.rb
@@ -29,30 +29,30 @@ class Chef
 
         def template_location
           @template_file_cache_location ||= begin
-            template_finder.find(@new_resource.source, :local => @new_resource.local, :cookbook => @new_resource.cookbook)
+            template_finder.find(new_resource.source, :local => new_resource.local, :cookbook => new_resource.cookbook)
           end
         end
 
         private
 
         def file_for_provider
-          context = TemplateContext.new(@new_resource.variables)
-          context[:node] = @run_context.node
+          context = TemplateContext.new(new_resource.variables)
+          context[:node] = run_context.node
           context[:template_finder] = template_finder
 
           # helper variables
-          context[:cookbook_name] = @new_resource.cookbook_name unless context.keys.include?(:coookbook_name)
-          context[:recipe_name] = @new_resource.recipe_name unless context.keys.include?(:recipe_name)
-          context[:recipe_line_string] = @new_resource.source_line unless context.keys.include?(:recipe_line_string)
-          context[:recipe_path] = @new_resource.source_line_file unless context.keys.include?(:recipe_path)
-          context[:recipe_line] = @new_resource.source_line_number unless context.keys.include?(:recipe_line)
-          context[:template_name] = @new_resource.source unless context.keys.include?(:template_name)
+          context[:cookbook_name] = new_resource.cookbook_name unless context.keys.include?(:coookbook_name)
+          context[:recipe_name] = new_resource.recipe_name unless context.keys.include?(:recipe_name)
+          context[:recipe_line_string] = new_resource.source_line unless context.keys.include?(:recipe_line_string)
+          context[:recipe_path] = new_resource.source_line_file unless context.keys.include?(:recipe_path)
+          context[:recipe_line] = new_resource.source_line_number unless context.keys.include?(:recipe_line)
+          context[:template_name] = new_resource.source unless context.keys.include?(:template_name)
           context[:template_path] = template_location unless context.keys.include?(:template_path)
 
-          context._extend_modules(@new_resource.helper_modules)
+          context._extend_modules(new_resource.helper_modules)
           output = context.render_template(template_location)
 
-          tempfile = Chef::FileContentManagement::Tempfile.new(@new_resource).tempfile
+          tempfile = Chef::FileContentManagement::Tempfile.new(new_resource).tempfile
           tempfile.binmode
           tempfile.write(output)
           tempfile.close
@@ -61,7 +61,7 @@ class Chef
 
         def template_finder
           @template_finder ||= begin
-            TemplateFinder.new(run_context, @new_resource.cookbook_name, @run_context.node)
+            TemplateFinder.new(run_context, new_resource.cookbook_name, run_context.node)
           end
         end
       end
