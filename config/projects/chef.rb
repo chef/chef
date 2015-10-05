21a3943cb0528aa51600dc1825d8c906ce205d26
John Keiser <john@johnkeiser.com>
2015-10-05 08:57:39 -0700

    Build Chef 12.5.0

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -20,7 +20,7 @@ maintainer "Chef Software, Inc. <maintainers@chef.io>"
 homepage "https://www.chef.io"
 
 build_iteration 1
-build_version '12.4.3'
+build_version '12.5.0'
 
 if windows?
   # NOTE: Ruby DevKit fundamentally CANNOT be installed into "Program Files"
