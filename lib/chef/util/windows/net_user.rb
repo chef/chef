208784a0c8eea3ced159b856a4b506df0a933b33
Matt Wrock <matt@mattwrock.com>
2015-10-06 15:27:47 -0700

    add windows user to new group if gid changes

--- lib/chef/util/windows/net_user.rb
+++ lib/chef/util/windows/net_user.rb
@@ -78,7 +78,10 @@ class Chef::Util::Windows::NetUser < Chef::Util::Windows
 
   def set_info(args)
     begin
-      rc = NetUser::net_user_set_info_l3(nil, @username, transform_usri3(args))
+      transformed_args = transform_usri3(args)
+      group_name = transformed_args.delete(:usri3_primary_group_id)
+      NetUser::net_user_set_info_l3(nil, @username, transformed_args)
+      NetUser::net_local_group_add_member(nil, group_name, args[:name]) if group_name
     rescue Chef::Exceptions::Win32APIError => e
       raise ArgumentError, e
     end
