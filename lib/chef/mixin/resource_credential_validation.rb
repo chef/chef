d91e027040e1b6b67006ad9a2d5f95fe32de360c
Adam Edwards <adamed@opscode.com>
2015-10-11 07:53:02 -0700

    Enable user identity for execute resources

---
+++ lib/chef/mixin/resource_credential_validation.rb
@@ -0,0 +1,42 @@
+#
+# Author:: Adam Edwards (<adamed@chef.io>)
+# Copyright:: Copyright (c) 2015 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class Chef
+  module Mixin
+    module ResourceCredentialValidation
+
+      def validate_credential(user, domain, password)
+        if ! Chef::Platform.windows?
+          if domain || password
+            raise Exceptions::UnsupportedPlatform,
+                  "The domain and password properties are only supported on the Windows platform"
+          else
+            return
+          end
+        end
+
+        if password.nil?
+          raise ArgumentError, "No password property was specified when the user property was specified"
+        end
+      end
+
+      private(:validate_credential)
+
+    end
+  end
+end
