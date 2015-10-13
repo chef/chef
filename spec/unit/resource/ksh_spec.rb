d6f11b2716b16520825b3502de10b3821b24d43c
Nolan Davidson <nolan.davidson@gmail.com>
2015-10-13 08:15:49 -0400

    updating copyright statment in ksh_spec.rb

--- spec/unit/resource/ksh_spec.rb
+++ spec/unit/resource/ksh_spec.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Nolan Davidson (<nolan.davidson@gmail.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Copyright:: Copyright (c) 2015 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
