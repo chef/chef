9469d3a5ab22655fe13a4dd09823bd371fce3c10
Tim Smith <tsmith84@gmail.com>
2015-10-18 20:39:42 -0700

    Update messaging to match LWRP -> Custom Resource rename
    If you showed up in a post-LWRP world these would be confusing

--- lib/chef/resource/lwrp_base.rb
+++ lib/chef/resource/lwrp_base.rb
@@ -1,8 +1,8 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Author:: Christopher Walters (<cw@opscode.com>)
-# Author:: Daniel DeLeo (<dan@opscode.com>)
-# Copyright:: Copyright (c) 2008-2012 Opscode, Inc.
+# Author:: Adam Jacob (<adam@chef.io>)
+# Author:: Christopher Walters (<cw@chef.io>)
+# Author:: Daniel DeLeo (<dan@chef.io>)
+# Copyright:: Copyright (c) 2008-2015 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,7 +45,7 @@ class Chef
 
         def build_from_file(cookbook_name, filename, run_context)
           if LWRPBase.loaded_lwrps[filename]
-            Chef::Log.info("LWRP resource #{filename} from cookbook #{cookbook_name} has already been loaded!  Skipping the reload.")
+            Chef::Log.info("Custom resource #{filename} from cookbook #{cookbook_name} has already been loaded!  Skipping the reload.")
             return loaded_lwrps[filename]
           end
 
@@ -60,7 +60,7 @@ class Chef
           # Make a useful string for the class (rather than <Class:312894723894>)
           resource_class.instance_eval do
             define_singleton_method(:to_s) do
-              "LWRP resource #{resource_name} from cookbook #{cookbook_name}"
+              "Custom resource #{resource_name} from cookbook #{cookbook_name}"
             end
             define_singleton_method(:inspect) { to_s }
           end
