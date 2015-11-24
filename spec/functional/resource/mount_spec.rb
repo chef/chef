67dc553a920eef8beb9365194897aefba4cb86a9
Jay Mundrawala <jdmundrawala@gmail.com>
2015-11-24 11:55:31 -0800

    Skip mount test in travis

--- spec/functional/resource/mount_spec.rb
+++ spec/functional/resource/mount_spec.rb
@@ -23,7 +23,7 @@ require 'tmpdir'
 # run this test only for following platforms.
 include_flag = !(['ubuntu', 'centos', 'aix', 'solaris2'].include?(ohai[:platform]))
 
-describe Chef::Resource::Mount, :requires_root, :external => include_flag do
+describe Chef::Resource::Mount, :requires_root, :skip_travis, :external => include_flag do
 
   include Chef::Mixin::ShellOut
 
