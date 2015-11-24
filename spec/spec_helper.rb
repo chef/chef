dafab397e054ae41133582651050fa1dc433d293
Jay Mundrawala <jdmundrawala@gmail.com>
2015-11-24 11:52:15 -0800

    Add tag to skip travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -121,6 +121,7 @@ RSpec.configure do |config|
 
   config.filter_run_excluding :skip_appveyor => true if ENV["APPVEYOR"]
   config.filter_run_excluding :appveyor_only => true unless ENV["APPVEYOR"]
+  config.filter_run_excluding :skip_travis => true if ENV["TRAVIS"]
 
   config.filter_run_excluding :windows_only => true unless windows?
   config.filter_run_excluding :not_supported_on_mac_osx_106 => true if mac_osx_106?
