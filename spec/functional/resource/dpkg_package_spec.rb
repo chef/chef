afaf15cf82308259e8e9a60e0c2836ce316ccdc0
Lamont Granquist <lamont@scriptkiddie.org>
2015-12-04 17:01:36 -0800

    disable tests on 32-bit

--- spec/functional/resource/dpkg_package_spec.rb
+++ spec/functional/resource/dpkg_package_spec.rb
@@ -18,7 +18,7 @@
 require 'spec_helper'
 require 'chef/mixin/shell_out'
 
-describe Chef::Resource::DpkgPackage, :requires_root, :debian_family_only do
+describe Chef::Resource::DpkgPackage, :requires_root, :debian_family_only, arch: "x86_64" do
   include Chef::Mixin::ShellOut
 
   let(:apt_data) { File.join(CHEF_SPEC_DATA, "apt") }
