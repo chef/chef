d86ae6ce4b65a0555860afcf0e4f1f2443614558
Adam Edwards <adamed@opscode.com>
2015-12-08 13:29:44 -0800

    Functional tests for user_context

--- lib/chef/provider/remote_file/network_file.rb
+++ lib/chef/provider/remote_file/network_file.rb
@@ -47,10 +47,8 @@ class Chef
             tempfile = Chef::FileContentManagement::Tempfile.new(new_resource).tempfile
             Chef::Log.debug("#{new_resource} staging #{@source} to #{tempfile.path}")
 
-            identity = qualify_identity( new_resource.remote_user, new_resource.remote_user_domain )
-
-            with_user_context(identity[:user], identity[:domain], new_resource.remote_user_password) do
-              ::File.open(@source, 'rb') do | remote_file |
+            with_user_context(new_resource.remote_user, new_resource.remote_user_domain, new_resource.remote_user_password) do
+            ::File.open(@source, 'rb') do | remote_file |
                 while data = remote_file.read(TRANSFER_CHUNK_SIZE)
                   tempfile.write(data)
                 end
