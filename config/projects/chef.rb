e36e2d62ffb3831735737c2c42ed225191bd48ce
Salim Alam <salam@chef.io>
2015-12-14 10:42:24 -0800

    Bump version to 12.6.0 for release

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -20,7 +20,7 @@ maintainer "Chef Software, Inc. <maintainers@chef.io>"
 homepage "https://www.chef.io"
 
 build_iteration 1
-build_version '12.5.1'
+build_version '12.6.0'
 
 if windows?
   # NOTE: Ruby DevKit fundamentally CANNOT be installed into "Program Files"
