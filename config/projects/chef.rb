58fbfef503631eeb8a661ba95da6134169d49462
Salim Alam <salam@chef.io>
2015-12-17 15:53:33 -0800

    Repin to master

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -53,8 +53,8 @@ else
 end
 
 # Chef Release version pinning
-override :chef, version: '12.6-release'
-override :ohai, version: '8.8.1'
+override :chef, version: 'master'
+override :ohai, version: 'master'
 
 
 dependency "preparation"
