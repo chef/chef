163342b013f2f57555dd27727362ebf382afdfa3
Bryan McLellan <btm@chef.io>
2015-12-17 05:37:53 -0800

    Fix verify-chef.sh for BSD

--- jenkins/verify-chef.sh
+++ jenkins/verify-chef.sh
@@ -23,23 +23,23 @@ fi
 export USR_BIN_DIR
 
 # sanity check that we're getting the correct symlinks from the pre-install script
-# solaris doesn't have readlink or test -e. proceed with caution.
-if [ ! -L $USR_BIN_DIR/chef-client ] || [ `ls -n $USR_BIN_DIR/chef-client | awk '{print$NF}'` != "$BIN_DIR/chef-client" ]; then
+# solaris doesn't have readlink or test -e. ls -n is different on BSD. proceed with caution.
+if [ ! -L $USR_BIN_DIR/chef-client ] || [ `ls -l $USR_BIN_DIR/chef-client | awk '{print$NF}'` != "$BIN_DIR/chef-client" ]; then
   echo "$USR_BIN_DIR/chef-client symlink to $BIN_DIR/chef-client was not correctly created by the pre-install script!"
   exit 1
 fi
 
-if [ ! -L $USR_BIN_DIR/knife ] || [ `ls -n $USR_BIN_DIR/knife | awk '{print$NF}'` != "$BIN_DIR/knife" ]; then
+if [ ! -L $USR_BIN_DIR/knife ] || [ `ls -l $USR_BIN_DIR/knife | awk '{print$NF}'` != "$BIN_DIR/knife" ]; then
   echo "$USR_BIN_DIR/knife symlink to $BIN_DIR/knife was not correctly created by the pre-install script!"
   exit 1
 fi
 
-if [ ! -L $USR_BIN_DIR/chef-solo ] || [ `ls -n $USR_BIN_DIR/chef-solo | awk '{print$NF}'` != "$BIN_DIR/chef-solo" ]; then
+if [ ! -L $USR_BIN_DIR/chef-solo ] || [ `ls -l $USR_BIN_DIR/chef-solo | awk '{print$NF}'` != "$BIN_DIR/chef-solo" ]; then
   echo "$USR_BIN_DIR/chef-solo symlink to $BIN_DIR/chef-solo was not correctly created by the pre-install script!"
   exit 1
 fi
 
-if [ ! -L $USR_BIN_DIR/ohai ] || [ `ls -n $USR_BIN_DIR/ohai | awk '{print$NF}'` != "$BIN_DIR/ohai" ]; then
+if [ ! -L $USR_BIN_DIR/ohai ] || [ `ls -l $USR_BIN_DIR/ohai | awk '{print$NF}'` != "$BIN_DIR/ohai" ]; then
   echo "$USR_BIN_DIR/ohai symlink to $BIN_DIR/ohai was not correctly created by the pre-install script!"
   exit 1
 fi
