66e625d32db38f9e0aecae39a7d81cecd07cb28d
Anthony Neto <aneto@denirostaff.com>
2016-01-11 18:02:04 -0800

    prepending chef to tempfile name

--- lib/chef/file_content_management/tempfile.rb
+++ lib/chef/file_content_management/tempfile.rb
@@ -65,6 +65,7 @@ class Chef
       def tempfile_basename
         basename = ::File.basename(@new_resource.name)
         basename.insert 0, "." unless Chef::Platform.windows?  # dotfile if we're not on windows
+        basename << '-chef-'
         basename
       end
 
