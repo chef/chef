3fa247386be7523a2b488d65dfe35cf753bd9802
Thom May <thom@chef.io>
2016-01-13 10:09:06 +0000

    Correct uses of fail to raise
    Style/SignalException - 3

--- lib/chef/chef_fs/file_system/chef_repository_file_system_cookbook_dir.rb
+++ lib/chef/chef_fs/file_system/chef_repository_file_system_cookbook_dir.rb
@@ -37,7 +37,7 @@ class Chef
             # want to spend a lot of time adding code to the main Chef libraries
             if root.versioned_cookbooks
               canonical_name = canonical_cookbook_name(File.basename(file_path))
-              fail "When versioned_cookbooks mode is on, cookbook #{file_path} must match format <cookbook_name>-x.y.z"  unless canonical_name
+              raise "When versioned_cookbooks mode is on, cookbook #{file_path} must match format <cookbook_name>-x.y.z"  unless canonical_name
 
               # KLUDGE: We shouldn't have to use instance_variable_set
               loader.instance_variable_set(:@cookbook_name, canonical_name)
