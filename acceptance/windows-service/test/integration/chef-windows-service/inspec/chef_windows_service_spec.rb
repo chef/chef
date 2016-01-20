b2134edcb616117cdad9a5c117b0dd93ffe3c4bb
Salim Alam <salam@chef.io>
2016-01-19 19:35:40 -0800

    Add check for Windows OS

--- acceptance/windows-service/test/integration/chef-windows-service/inspec/chef_windows_service_spec.rb
+++ acceptance/windows-service/test/integration/chef-windows-service/inspec/chef_windows_service_spec.rb
@@ -1,8 +1,11 @@
+only_if do
+  os['family'] == 'windows'
+end
+
 describe command("chef-service-manager") do
   it { should exist }
   its('exit_status') { should eq 0 }
 end
-
 
 describe service('chef-client') do
   it { should_not be_enabled }
