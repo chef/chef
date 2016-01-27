6a2598d655180bc6f8986cf842405b35d648992c
Lamont Granquist <lamont@scriptkiddie.org>
2016-01-27 15:22:26 -0800

    fixed my .vimrc auto-copyright header

--- lib/chef/cookbook/gem_installer.rb
+++ lib/chef/cookbook/gem_installer.rb
@@ -1,7 +1,7 @@
 #--
 # Author:: Christopher Walters (<cw@opscode.com>)
 # Author:: Tim Hinderliter (<tim@opscode.com>)
-# Copyright:: Copyright (c) 2010-2015 Chef Software, Inc.
+# Copyright:: Copyright (c) 2010-2016 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
