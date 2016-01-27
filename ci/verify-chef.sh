chef/chef

commit 2bcb7d06dcb49c953b9e666b24c8bd3cb56da2c5

export CHEF_FIPS=1 in the chef-fips jenkins pipeline

ci/verify-chef.sh
@@ -86,4 +86,9 @@ if [ ! -f "Gemfile.lock" ]; then
   exit 1
 fi
 
-sudo env PATH=$PATH TERM=xterm bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o $WORKSPACE/test.xml -f documentation spec/functional spec/unit
+CHEF_FIPS=0
+if [ $PIPELINE_NAME='chef-fips'];
+then
+    CHEF_FIPS=1
+fi
+sudo env PATH=$PATH TERM=xterm CHEF_FIPS=$CHEF_FIPS bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o $WORKSPACE/test.xml -f documentation spec/functional spec/unit

ci/verify-chef.bat
@@ -52,5 +52,8 @@ IF "%PIPELINE_NAME%" == "chef-13" (
   call bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o %WORKSPACE%\test.xml -f documentation spec/unit spec/functional
 ) ELSE (
   REM ; Running unit tests
+  IF "%PIPELINE_NAME%" == "chef-fips" (
+    set CHEF_FIPS=1
+  )
   call bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o %WORKSPACE%\test.xml -f documentation spec/unit spec/functional
 )

commit 490aee527802ba149b68cb4ead1db2cf73006ec7

Fix bug in sh script that runs test in jenkins
Some testers fail with

```
ci/verify-chef.sh: test: ] missing`
```

This regression was introduced by
https://github.com/chef/chef/commit/2bcb7d06dcb49c953b9e666b24c8bd3cb56da2c5

ci/verify-chef.sh
@@ -86,9 +86,10 @@ if [ ! -f "Gemfile.lock" ]; then
   exit 1
 fi
 
-CHEF_FIPS=0
-if [ $PIPELINE_NAME='chef-fips'];
-then
+unset CHEF_FIPS
+if [ $PIPELINE_NAME="chef-fips" ]; then
+    echo "Setting fips mode"
     CHEF_FIPS=1
+    export CHEF_FIPS
 fi
 sudo env PATH=$PATH TERM=xterm CHEF_FIPS=$CHEF_FIPS bundle exec rspec -r rspec_junit_formatter -f RspecJunitFormatter -o $WORKSPACE/test.xml -f documentation spec/functional spec/unit