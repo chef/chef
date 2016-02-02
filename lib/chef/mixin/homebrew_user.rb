b91f7edaa062c7e59fb01c27b6b7fb1ad7e50571
Noah Kantrowitz <noah@coderanger.net>
2016-02-02 15:23:17 -0800

    Update for chef.io domain in copyright lines too.

--- lib/chef/mixin/homebrew_user.rb
+++ lib/chef/mixin/homebrew_user.rb
@@ -3,7 +3,7 @@
 # Author:: Graeme Mathieson (<mathie@woss.name>)
 #
 # Copyright 2011-2016, Chef Software Inc.
-# Copyright 2014-2016, Chef Software, Inc <legal@getchef.com>
+# Copyright 2014-2016, Chef Software, Inc <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- spec/unit/resource/homebrew_package_spec.rb
+++ spec/unit/resource/homebrew_package_spec.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Joshua Timberman (<joshua@chef.io>)
-# Copyright 2014-2016, Chef Software, Inc. <legal@getchef.com>
+# Copyright 2014-2016, Chef Software, Inc. <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
