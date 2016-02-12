f890cc4480c601b08c657cee2d24dd3ed472c054
Lamont Granquist <lamont@scriptkiddie.org>
2016-02-12 11:48:56 -0800

    fix alignment

--- lib/chef/application/knife.rb
+++ lib/chef/application/knife.rb
@@ -63,7 +63,7 @@ class Chef::Application::Knife < Chef::Application
     :long         => "--disable-editing",
     :description  => "Do not open EDITOR, just accept the data as is",
     :boolean      => true,
-    :default => false
+    :default      => false
 
   option :help,
     :short        => "-h",
