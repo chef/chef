b01b75fd0ea20be903b1bcfe7608e2b307bc1f44
Lamont Granquist <lamont@scriptkiddie.org>
2016-02-16 15:42:46 -0800

    be a bit more forceful about the message
    again for reference, dan deprecated this behavior in 0.10.4
    back in 2011:
    
    https://github.com/chef/chef/commit/5a9fee8edaede311ac03a15d52fbc66dad83b576

--- lib/chef/cookbook_loader.rb
+++ lib/chef/cookbook_loader.rb
@@ -69,7 +69,7 @@ class Chef
     def warn_about_cookbook_shadowing
       unless merged_cookbooks.empty?
         Chef::Log.deprecation "The cookbook(s): #{merged_cookbooks.join(', ')} exist in multiple places in your cookbook_path. " +
-          "A composite version has been compiled.  In a future version of Chef this behavior will be removed."
+          "A composite version has been compiled.  This has been deprecated since 0.10.4, in Chef 13 this behavior will be REMOVED."
       end
     end
 
