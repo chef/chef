3f9301526e6774c7e7e3b4081c9715978ab3ecd2
Jay Mundrawala <jdmundrawala@gmail.com>
2016-02-24 11:15:12 -0800

    Add windows support for fips_spec

--- acceptance/fips/test/integration/fips/serverspec/fips_spec.rb
+++ acceptance/fips/test/integration/fips/serverspec/fips_spec.rb
@@ -2,11 +2,34 @@ require 'mixlib/shellout'
 require 'bundler'
 
 describe "Chef Fips Specs" do
+  def windows?
+    if RUBY_PLATFORM =~ /mswin|mingw|windows/
+      true
+    else
+      false
+    end
+  end
+
+  let(:chef_dir) do
+    if windows?
+      Dir.glob('c:/opscode/chef/embedded/lib/ruby/gems/*/gems/chef-[0-9]*').last
+    else
+      Dir.glob('/opt/chef/embedded/lib/ruby/gems/*/gems/chef-[0-9]*').last
+    end
+  end
+
+  let(:path) do
+    if windows?
+      'C:\opscode\chef\embedded\bin'
+    else
+      "/opt/chef/embedded/bin"
+    end
+  end
+
   it 'passes the unit and functional specs' do
-    chef_dir = Dir.glob('/opt/chef/embedded/lib/ruby/gems/*/gems/chef-[0-9]*').last
     Bundler.with_clean_env do
       ruby_cmd = Mixlib::ShellOut.new(
-        'bundle exec rspec spec/unit spec/functional', :env => {'PATH' => "#{ENV['PATH']}:/opt/chef/embedded/bin",
+        'bundle exec rspec spec/unit spec/functional', :env => {'PATH' => "#{ENV['PATH']}:#{path}",
                                                                 'GEM_PATH' => nil, 'GEM_CACHE'=>nil, 'GEM_HOME'=>nil,
                                                                 'CHEF_FIPS'=>'1'},
         :live_stream => STDOUT, :cwd => chef_dir)
@@ -14,4 +37,3 @@ describe "Chef Fips Specs" do
     end
   end
 end
-
