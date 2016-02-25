8cd125bf14c7637d2d28172c8a078cae66ac5c36
Lamont Granquist <lamont@scriptkiddie.org>
2016-02-24 18:33:28 -0800

    add a pending check for the object equality

--- spec/unit/node/cow_mash_spec.rb
+++ spec/unit/node/cow_mash_spec.rb
@@ -4,12 +4,19 @@ require "chef/node/cow_mash"
 
 describe Chef::Node::COWMash do
   context "#keep_if" do
-    it "works" do
+    it "behaves correctly" do
       hash = { "foo" => true, "bar" => false }
       cow = Chef::Node::COWMash.new(wrapped_object: hash)
       expect(cow.keep_if { |k, v| v }).to eql({ "foo" => true })
       expect(hash).to eql({ "foo" => true, "bar" => false })
       expect(cow).to eql({ "foo" => true })
+    end
+
+    it "returns the object it mutates" do
+      pending "not yet fixed"
+      hash = { "foo" => true, "bar" => false }
+      cow = Chef::Node::COWMash.new(wrapped_object: hash)
+      expect(cow.keep_if { |k, v| v }).to equal(cow)
     end
   end
 end
