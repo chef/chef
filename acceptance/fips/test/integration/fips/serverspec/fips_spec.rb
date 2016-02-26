4ec761fd674aabe7d766a3017c03a328d1164777
Jay Mundrawala <jdmundrawala@gmail.com>
2016-02-25 17:37:14 -0800

    Use the correct path separator

--- acceptance/fips/test/integration/fips/serverspec/fips_spec.rb
+++ acceptance/fips/test/integration/fips/serverspec/fips_spec.rb
@@ -29,9 +29,9 @@ describe "Chef Fips Specs" do
   it "passes the unit and functional specs" do
     Bundler.with_clean_env do
       ruby_cmd = Mixlib::ShellOut.new(
-        "bundle exec rspec -t ~requires_git spec/unit spec/functional", :env => { "PATH" => "#{ENV['PATH']}:#{path}",
+        "bundle exec rspec -t ~requires_git spec/unit spec/functional", :env => { "PATH" => [ENV['PATH'], path].join(File::PATH_SEPARATOR),
                                                                                   "GEM_PATH" => nil, "GEM_CACHE" => nil, "GEM_HOME" => nil,
-                                                                                  "CHEF_FIPS" => "1" },
+                                                                                  "CHEF_FIPS" => "1"},
                                                                                   :live_stream => STDOUT, :cwd => chef_dir, :timeout => 3600)
       expect { ruby_cmd.run_command.error! }.not_to raise_exception
     end
