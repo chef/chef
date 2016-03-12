f59f66cd27c36cc4fe31c6b8197758b165784f20
Nathan Williams <nathan@teamtreehouse.com>
2016-03-11 16:06:07 -0800

    rm unnecessary mutex

--- lib/chef/dsl/declare_resource.rb
+++ lib/chef/dsl/declare_resource.rb
@@ -22,7 +22,6 @@ require "chef/exceptions"
 class Chef
   module DSL
     module DeclareResource
-      BUILD_RESOURCE_SEMAPHORE = Mutex.new
 
       #
       # Instantiates a resource (via #build_resource), then adds it to the
@@ -89,9 +88,7 @@ class Chef
       #
       def build_resource(type, name, created_at = nil, run_context: self.run_context, &resource_attrs_block)
         created_at ||= caller[0]
-        BUILD_RESOURCE_SEMAPHORE.synchronize do
-          require "chef/resource_builder" unless defined?(Chef::ResourceBuilder)
-        end
+        require "chef/resource_builder" unless defined?(Chef::ResourceBuilder)
 
         Chef::ResourceBuilder.new(
           type:                type,
