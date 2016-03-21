18f0ee2354ae2539bf63956ef99a37a188ef67eb
danielsdeleo <dan@chef.io>
2016-03-21 15:33:29 -0700

    Pin all lower-level libs to latest versions

--- omnibus/config/projects/chef.rb
+++ omnibus/config/projects/chef.rb
@@ -64,6 +64,30 @@ if windows?
   dependency "ruby-windows-devkit-bash"
 end
 
+# Lower level library pins
+override :xproto,             version: "7.0.28"
+override :"util-macros",      version: "1.19.0"
+override :makedepend,         version: "1.0.5"
+
+## We are currently on the latest of these:
+#override :"ncurses",          version: "5.9"
+#override :"zlib",             version: "1.2.8"
+#override :"pkg-config-lite",  version: "0.28-1"
+#override :"libffi",           version: "3.2.1"
+#override :"libyaml",          version: "0.1.6"
+#override :"libiconv",         version: "1.14"
+#override :"liblzma",          version: "5.2.2"
+#override :"libxml2",          version: "2.9.3"
+#override :"libxslt",          version: "1.1.28"
+
+## according to comment in omnibus-sw, latest versions don't work on solaris
+# https://github.com/chef/omnibus-software/blob/aefb7e79d29ca746c3f843673ef5e317fa3cba54/config/software/libtool.rb#L23
+#override :"libtool"
+
+## These can float as they are frequently updated in a way that works for us
+#override :"cacerts",                             # probably best to float?
+#override :"openssl"                              # leave this?
+
 dependency "clean-static-libs"
 
 package :rpm do
