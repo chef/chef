8b2889a132f149306876fc257c76267310005ca1
John Keiser <john@johnkeiser.com>
2016-03-23 14:07:43 -0700

    Bump 12.8 -> 12.9

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -21,7 +21,7 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "12.8.9"
+  VERSION = "12.9.0"
 end
 
 #
--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -21,7 +21,7 @@
 
 class Chef
   CHEF_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "12.8.9"
+  VERSION = "12.9.0"
 end
 
 #
