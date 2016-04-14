2d48106e6c6afa454ea7c7974de124e977b56112
Thom May <thom@chef.io>
2016-04-14 15:15:22 +0100

    Move unused code into backcompat

--- lib-backcompat/chef/chef_fs/file_system/repository/file_system_root_dir.rb
+++ lib-backcompat/chef/chef_fs/file_system/repository/file_system_root_dir.rb
@@ -24,6 +24,7 @@ class Chef
       module Repository
         class FileSystemRootDir < FileSystemEntry
           def initialize(file_path)
+            Chef.log_deprecation "Chef::ChefFS::FileSystem::Repository::FileSystemRootDir is deprecated."
             super("", nil, file_path)
           end
         end
