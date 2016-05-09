afe0aa20bb521d175c7e2a9ec2fabe1050db3995
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-09 09:07:57 -0700

    adding more func tests to travis

---
+++ kitchen-tests/cookbooks/base/metadata.rb
@@ -0,0 +1,22 @@
+name             "base"
+maintainer       ""
+maintainer_email ""
+license          ""
+description      "Installs/Configures base"
+long_description "Installs/Configures base"
+version          "0.1.0"
+
+depends          'apt'
+depends          'build-essential'
+depends          'chef-client'
+depends          'epel'
+depends          'fail2ban'
+depends          'logrotate'
+depends          'multipackage'
+depends          'nscd'
+depends          'ntp'
+depends          'openssh'
+depends          'resolver'
+depends          'sudo'
+depends          'ubuntu'
+depends          'users'
---
+++ kitchen-tests/cookbooks/base/recipes/packages.rb
@@ -0,0 +1,9 @@
+
+
+pkgs = %w{lsof tcpdump strace zsh dmidecode ltrace bc curl wget telnet subversion git traceroute htop iptraf tmux s3cmd sysbench }
+
+# this deliberately calls the multipackage API N times in order to do one package installation in order to exercise the
+# multipackage cookbook.
+pkgs.each do |pkg|
+  multipackage pkgs
+end
