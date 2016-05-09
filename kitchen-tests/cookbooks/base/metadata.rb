76db00d4688a1576d0eb4e0ffbdc68ca650f59a9
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-09 09:18:36 -0700

    rubocop

--- kitchen-tests/cookbooks/base/metadata.rb
+++ kitchen-tests/cookbooks/base/metadata.rb
@@ -6,17 +6,17 @@ description      "Installs/Configures base"
 long_description "Installs/Configures base"
 version          "0.1.0"
 
-depends          'apt'
-depends          'build-essential'
-depends          'chef-client'
-depends          'epel'
-depends          'fail2ban'
-depends          'logrotate'
-depends          'multipackage'
-depends          'nscd'
-depends          'ntp'
-depends          'openssh'
-depends          'resolver'
-depends          'sudo'
-depends          'ubuntu'
-depends          'users'
+depends          "apt"
+depends          "build-essential"
+depends          "chef-client"
+depends          "epel"
+depends          "fail2ban"
+depends          "logrotate"
+depends          "multipackage"
+depends          "nscd"
+depends          "ntp"
+depends          "openssh"
+depends          "resolver"
+depends          "sudo"
+depends          "ubuntu"
+depends          "users"
