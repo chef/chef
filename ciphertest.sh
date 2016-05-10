b947d34a8c6772f46213d54deaa1ba3341fd312e
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-10 12:54:56 -0700

    test all the ciphers net-ssh supports

--- ciphertest.sh
+++ ciphertest.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# uses "/root/tmp/dd.txt" as a temporary file!
-for cipher in aes128-cbc aes128-ctr aes128-gcm@openssh.com aes192-cbc aes192-ctr aes256-cbc aes256-ctr aes256-gcm@openssh.com arcfour arcfour128 arcfour256 blowfish-cbc cast128-cbc chacha20-poly1305@openssh.com 3des-cbc ; do
+for cipher in aes128-cbc 3des-cbc blowfish-cbc cast128-cbc aes192-cbc aes256-cbc rijndael-cbc@lysator.liu.se idea-cbc none arcfour128 arcfour256 arcfour aes128-ctr aes192-ctr aes256-ctr camellia128-cbc camellia192-cbc camellia256-cbc camellia128-cbc@openssh.org camellia192-cbc@openssh.org camellia256-cbc@openssh.org camellia128-ctr camellia192-ctr camellia256-ctr camellia128-ctr@openssh.org camellia192-ctr@openssh.org camellia256-ctr@openssh.org cast128-ctr blowfish-ctr 3des-ctr aes256-gcm@openssh.com aes128-gcm@openssh.com; do
     for i in 1 2 3 ; do
-        echo
-        echo "Cipher: $cipher (try $i)"
-
-        dd if=/dev/zero bs=1024k count=1024 2>/tmp/dd.txt | pv --size 1G | time -p ssh -o 'Compression=no' -o 'StrictHostKeyChecking=no' -c "$cipher" localhost 'cat > /dev/null'
-        grep -v records /tmp/dd.txt
+        dd if=/dev/zero bs=1024k count=1024 2>/dev/null | pv -F "%a $cipher $i" --size 1G | ssh -q -x -o 'Compression=no' -o 'StrictHostKeyChecking=no' -c "$cipher" localhost 'cat > /dev/null'
     done
 done
