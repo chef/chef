fabe00c7ed4e0eeb6885c76d29ea7ec6d4b36441
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-10 15:13:55 -0700

    testing hmacs now

--- ciphertest.sh
+++ ciphertest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pv --help
-for cipher in aes128-cbc 3des-cbc blowfish-cbc cast128-cbc aes192-cbc aes256-cbc rijndael-cbc@lysator.liu.se idea-cbc none arcfour128 arcfour256 arcfour aes128-ctr aes192-ctr aes256-ctr cast128-ctr blowfish-ctr 3des-ctr; do
+for hmac in hmac-sha1 hmac-md5 hmac-sha1-96 hmac-md5-96 hmac-ripemd160 hmac-ripemd160@openssh.com hmac-sha2-256 hmac-sha2-512 hmac-sha2-256-96 hmac-sha2-512-96 none; do
     for i in 1 2 3 ; do
-      echo "$cipher $i"
-      dd if=/dev/zero bs=1024k count=1024 2>/dev/null | pv --size 1G | ssh -q -x -o 'Compression=no' -o 'StrictHostKeyChecking=no' -c "$cipher" localhost 'cat > /dev/null'
+      echo "$hmac $i"
+      dd if=/dev/zero bs=1024k count=1024 2>/dev/null | pv --size 1G | ssh -q -x -o "MACs=$hmac" -o 'Compression=no' -o 'StrictHostKeyChecking=no' -c "aes256-ctr" localhost 'cat > /dev/null'
     done
 done
