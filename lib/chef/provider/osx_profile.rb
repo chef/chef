f8db5e7169ebd207f4174717d3eef0f305483115
Ajay Chand <achand@fb.com>
2016-05-10 15:01:26 -0700

    fix profile provider for nil

--- lib/chef/provider/osx_profile.rb
+++ lib/chef/provider/osx_profile.rb
@@ -54,11 +54,10 @@ class Chef
             @new_resource.profile_name
         end
 
-        if all_profiles.empty?
-          current_profile = nil
-        else
-          current_profile = all_profiles["_computerlevel"].find do |item|
-            item["ProfileIdentifier"] == @new_profile_identifier
+        current_profile = nil
+        if all_profiles && !all_profiles.empty?
+          current_profile = all_profiles['_computerlevel'].find do |item|
+            item['ProfileIdentifier'] == @new_profile_identifier
           end
         end
         @current_resource.profile(current_profile)
