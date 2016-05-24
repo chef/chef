040e7473b458d1c917291d9857c15848a647e126
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-24 13:50:05 -0700

    convert kitchen tests to docker + other fixes
    - use kitchen-dokken instead of kitchen-ec2
    - add tests for gem metadata + chef-sugar
    - convert to inspec and add some tests
    
    this disables the webapp stuff completely and its testing for now, that
    needs to be updated to more modern httpd/mysql cookbooks and have its
    testing converted to inspec.
    
    more O/S distros are also in the pipeline.

--- kitchen-tests/cookbooks/base/recipes/packages.rb
+++ kitchen-tests/cookbooks/base/recipes/packages.rb
@@ -1,6 +1,6 @@
 
 
-pkgs = %w{lsof tcpdump strace zsh dmidecode ltrace bc curl wget telnet subversion git traceroute htop iptraf tmux s3cmd sysbench }
+pkgs = %w{lsof tcpdump strace zsh dmidecode ltrace bc curl wget telnet subversion git traceroute htop tmux s3cmd sysbench }
 
 # this deliberately calls the multipackage API N times in order to do one package installation in order to exercise the
 # multipackage cookbook.
