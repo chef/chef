5f63ad8716e03a29a515aed8c138f41327638901
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-25 10:18:40 -0700

    add a few more distros to kitchen testing
    adds debian, centos-7 and fedora-23

--- kitchen-tests/cookbooks/base/recipes/default.rb
+++ kitchen-tests/cookbooks/base/recipes/default.rb
@@ -7,13 +7,16 @@
 
 hostname "chef-travis-ci.chef.io"
 
-if node[:platform_family] == "debian"
+if node["platform_family"] == "debian"
   include_recipe "ubuntu"
   apt_update "packages"
 end
 
-if %w{rhel fedora}.include?(node[:platform_family])
+if %w{rhel fedora}.include?(node["platform_family"])
   include_recipe "selinux::disabled"
+end
+
+if node["platform_family"] == "rhel"
   include_recipe "yum-epel"
 end
 
@@ -32,6 +35,9 @@ include_recipe "sudo"
 include_recipe "chef-client::delete_validation"
 include_recipe "chef-client::config"
 include_recipe "chef-client"
+
+# hack needed for debian-7 on docker
+directory "/var/run/sshd"
 
 include_recipe "openssh"
 
