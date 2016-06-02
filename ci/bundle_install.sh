21e0a9085f707a04a25794b5df9c1cd07638e707
John Keiser <john@johnkeiser.com>
2016-06-01 21:55:39 -0700

    Use --user-install to allow gem install to happen

--- ci/bundle_install.sh
+++ ci/bundle_install.sh
@@ -2,6 +2,7 @@
 
 set -evx
 
+gem environment
 bundler_version=$(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
-gem install bundler -v $bundler_version
+gem install bundler -v $bundler_version --user-install
 bundle _${bundler_version}_ install
