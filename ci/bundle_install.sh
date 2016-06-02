af75a764e1f64032788ed218e28a396d8c1b733b
John Keiser <john@johnkeiser.com>
2016-06-01 21:31:04 -0700

    Add set -evx to shell scripts

--- ci/bundle_install.sh
+++ ci/bundle_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -evx
+
 gem update --system $(grep rubygems omnibus_overrides.rb | cut -d'"' -f2)
 gem install bundler -v $(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
 bundle install
