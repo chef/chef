9e4dbf391f62df449fbe8c99e3d1777cbb7f6ccf
John Keiser <john@johnkeiser.com>
2016-06-06 16:37:21 -0700

    Install fewer binary dependencies and dependencies in general when bumping version and updating dependencies

--- ci/version_bump.sh
+++ ci/version_bump.sh
@@ -5,3 +5,5 @@ set -evx
 . ci/bundle_install.sh
 
 bundle exec rake version:bump
+
+git checkout .bundle/config
--- ci/version_show.sh
+++ ci/version_show.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-set -evx
-
-. ci/bundle_install.sh
-
-bundle exec rake version:show
+cat VERSION
