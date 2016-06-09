8ed24be9cc9836c2158ef23f35fe0cca871555e2
Chef Versioner <chef-versioner@chef.io>
2016-06-09 06:02:44 +0000

    Bump version of chef to 12.11.26 by Chef Versioner.

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -21,7 +21,7 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "12.11.25"
+  VERSION = "12.11.26"
 end
 
 #
--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -21,7 +21,7 @@
 
 class Chef
   CHEF_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "12.11.25"
+  VERSION = "12.11.26"
 end
 
 #
