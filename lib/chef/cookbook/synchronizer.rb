0cd3117c34bfb89e5e39c5c80bb661a3bafb9959
Lamont Granquist <lamont@scriptkiddie.org>
2016-07-29 16:04:59 -0700

    stop constantly using new ServerAPI objects
    keep one-per-thread though since i doubt we're threadsafe

--- lib/chef/cookbook/synchronizer.rb
+++ lib/chef/cookbook/synchronizer.rb
@@ -291,7 +291,7 @@ class Chef
     end
 
     def server_api
-      Chef::ServerAPI.new(Chef::Config[:chef_server_url])
+      Thread.current[:server_api] ||= Chef::ServerAPI.new(Chef::Config[:chef_server_url])
     end
 
   end
