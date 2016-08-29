cc394d527b7718d0b2070daef2a490d2d859a651
Lamont Granquist <lamont@scriptkiddie.org>
2016-08-29 09:58:43 -0700

    bump openssl to 1.0.2g
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- version_policy.rb
+++ version_policy.rb
@@ -44,7 +44,7 @@ OMNIBUS_OVERRIDES = {
 
   ## These can float as they are frequently updated in a way that works for us
   #override "cacerts" =>"???",
-  #override "openssl" =>"???",
+  "openssl" => "1.0.2g",
 }
 
 #
