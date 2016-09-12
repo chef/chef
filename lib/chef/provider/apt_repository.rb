e767cb990a6cfd680b56e633cc89cc4a2520e744
Tim Smith <tsmith@chef.io>
2016-09-12 13:34:40 -0700

    Install apt-transport-https when needed in apt_repository
    Right now a system lacking apt-transport-https will fail if you define a
    https repo. We should run apt-get update and then install
    apt-transport-https before defining anything with https in the URI
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/provider/apt_repository.rb
+++ lib/chef/provider/apt_repository.rb
@@ -51,6 +51,15 @@ class Chef
           end
         end
 
+        # make sure we have apt-transport-https installed before we try to
+        # apt-get update with a https repo defined
+        if new_resource.uri =~ /^https/
+          declare_resource(:package, "apt-transport-https") do
+            action :install
+            notifies :update, "apt_update[#{new_resource.name}]", :before
+          end
+        end
+
         declare_resource(:execute, "apt-cache gencaches") do
           ignore_failure true
           action :nothing
