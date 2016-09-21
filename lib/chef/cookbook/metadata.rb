3d2a2e2e8cf340523a6bb223f9419689557c118e
andy-dufour <adufour@chef.io>
2016-09-21 00:11:14 -0400

    Fixing metadata.json validation, adding spec test to ensure we're actually validating the JSON passed to us

--- lib/chef/cookbook/metadata.rb
+++ lib/chef/cookbook/metadata.rb
@@ -641,8 +641,8 @@ class Chef
         VERSION_CONSTRAINTS.each do |dependency_type, hash_key|
           if dependency_group = o[hash_key]
             dependency_group.each do |cb_name, constraints|
-              if metadata.respond_to?(method_name)
-                metadata.public_send(method_name, cb_name, *Array(constraints))
+              if metadata.respond_to?(dependency_type)
+                metadata.public_send(dependency_type, cb_name, *Array(constraints))
               end
             end
           end
