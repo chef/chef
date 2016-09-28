992487ad24010c6a0c68311234ded1c2cfb951cc
Matt Wrock <matt@mattwrock.com>
2016-09-27 20:12:34 -0700

    bumping chef minor version (#5390)

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -21,7 +21,7 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "12.14.99"
+  VERSION = "12.15.0"
 end
 
 #
--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -21,7 +21,7 @@
 
 class Chef
   CHEF_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "12.14.99"
+  VERSION = "12.15.0"
 end
 
 #
