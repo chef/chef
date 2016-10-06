b0c760d3d70e476b23fb595f20b96db74b09f17f
Lamont Granquist <lamont@scriptkiddie.org>
2016-10-05 17:29:52 -0700

    add learnchef testing to travis matrix
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ kitchen-tests/cookbooks/awesome_customers_ubuntu_wrapper/metadata.rb
@@ -0,0 +1,9 @@
+name "awesome_customers_ubuntu_wrapper"
+maintainer "The Authors"
+maintainer_email "you@example.com"
+license "all_rights"
+description "Installs/Configures awesome_customers_ubuntu"
+long_description "Installs/Configures awesome_customers_ubuntu"
+version "0.1.0"
+
+depends "awesome_customers_ubuntu"
