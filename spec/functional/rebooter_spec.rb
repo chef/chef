c1d7bd6b23438084e0ffcd6848eeef2bd3f388b7
Christoph <cgriesshammer@gmail.com>
2016-11-30 17:06:58 -0500

    Revert "Updated tests for commit c0d58cf"
    This reverts commit 7b5f359122d13faa6cb2e61ffe74ec3556e8cfb7.

--- spec/functional/rebooter_spec.rb
+++ spec/functional/rebooter_spec.rb
@@ -43,7 +43,7 @@ describe Chef::Platform::Rebooter do
 
   let(:expected) do
     {
-      :windows => "#{ENV['SystemDrive']}/Windows/System32/shutdown.exe /r /t 300 /c \"rebooter spec test\"",
+      :windows => 'C:/Windows/System32/shutdown.exe /r /t 300 /c "rebooter spec test"',
       :linux => 'shutdown -r +5 "rebooter spec test"',
     }
   end
