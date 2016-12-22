aa5575fe4c4e14d65760f5bc0cceff0dc20e2f49
Tim Smith <tsmith@chef.io>
2016-12-21 16:10:44 -0800

    Require net-ssh-multi 1.2.1 and remove our monkeypatch
    The fix was merged upstream and released in 1.2.1
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/knife/ssh.rb
+++ lib/chef/knife/ssh.rb
@@ -26,7 +26,6 @@ class Chef
       deps do
         require "net/ssh"
         require "net/ssh/multi"
-        require "chef/monkey_patches/net-ssh-multi"
         require "readline"
         require "chef/exceptions"
         require "chef/search/query"
