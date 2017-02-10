e358aa2d288ad9eff627ad97c8d84c72882e0bd0
Tom Duffield <tom@chef.io>
2017-02-10 11:10:37 -0600

    Bundle with changelog group for version bump
    Signed-off-by: Tom Duffield <tom@chef.io>

--- ci/bundle_install.sh
+++ ci/bundle_install.sh
@@ -5,5 +5,6 @@ set -evx
 gem environment
 bundler_version=$(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
 gem install bundler -v $bundler_version --user-install --conservative
-export BUNDLE_WITHOUT=omnibus_package:test:pry:integration:docgen:maintenance:travis:aix:bsd:linux:mac_os_x:solaris:windows:development:changelog:travis
+# WITH: changelog (for version bumping and changelog creation)
+export BUNDLE_WITHOUT=omnibus_package:test:pry:integration:docgen:maintenance:travis:aix:bsd:linux:mac_os_x:solaris:windows:development:travis
 bundle _${bundler_version}_ install
