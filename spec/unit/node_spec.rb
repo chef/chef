c16cdbb69e4397400eb0237ac2be042a933248c9
Lamont Granquist <lamont@scriptkiddie.org>
2017-03-31 09:34:08 -0700

    remove more deprecated method_missing access
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- spec/unit/node_spec.rb
+++ spec/unit/node_spec.rb
@@ -265,12 +265,6 @@ describe Chef::Node do
         expect(node[:snoopy][:is_a_puppy]).to eq(true)
       end
 
-      it "should allow you to set an attribute with set_unless with method_missing but emit a deprecation warning" do
-        Chef::Config[:treat_deprecation_warnings_as_errors] = false
-        node.normal_unless.snoopy.is_a_puppy = false
-        expect(node[:snoopy][:is_a_puppy]).to eq(false)
-      end
-
       it "should allow you to set an attribute with set_unless" do
         node.normal_unless[:snoopy][:is_a_puppy] = false
         expect(node[:snoopy][:is_a_puppy]).to eq(false)
