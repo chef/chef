f3bcce215fc360164874a18b0b15cf1c10d340fe
Lamont Granquist <lamont@scriptkiddie.org>
2017-05-09 10:23:40 -0700

    add note to hack
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- ci/verify-chef.bat
+++ ci/verify-chef.bat
@@ -83,6 +83,11 @@ REM ; ffi-yajl must run in c-extension mode for perf, so force it so we don't ac
 set FORCE_FFI_YAJL=ext
 
 set
+REM ; we should use bundler, but aren't because of a bug
+REM ; this should be `bundle exec rspec yadda yadda...`
+REM ; the bug is here: https://github.com/bundler/bundler/issues/5644
+REM ; while this opens us up to some testing edge conditions if we're doing our job right to build the omnibus package and its gems correct that shouldn't matter
+REM ; since that assumption has gone wrong before we should really go back to using bundler at some point
 call %EMBEDDED_BIN_DIR%\rspec -r rspec_junit_formatter -f RspecJunitFormatter -o %WORKSPACE%\test.xml -f documentation spec/functional
 
 GOTO :EOF
