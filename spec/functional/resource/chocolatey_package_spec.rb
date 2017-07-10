ee8d204eaa86b45c48570c012542fc2037fbd2f2
MSys <harikesh.kolekar@msystechnologies.com>
2017-07-10 19:35:33 +0530

    update appveyor to create four vm and spec to run on diff vm
    Signed-off-by: MSys <harikesh.kolekar@msystechnologies.com>

--- spec/functional/resource/chocolatey_package_spec.rb
+++ spec/functional/resource/chocolatey_package_spec.rb
@@ -18,101 +18,103 @@
 require "spec_helper"
 require "chef/mixin/powershell_out"
 
-describe Chef::Resource::ChocolateyPackage, :windows_only, :choco_installed do
-  include Chef::Mixin::PowershellOut
+RSpec.describe "Time consuming spec skip when all specs run on appveyor", :if => ENV['spec_to_run'] != 'all' do
+  describe Chef::Resource::ChocolateyPackage, :windows_only, :choco_installed do
+    include Chef::Mixin::PowershellOut
 
-  let(:package_name) { "test-A" }
-  let(:package_list) { proc { powershell_out!("choco list -lo -r #{Array(package_name).join(' ')}").stdout.chomp } }
-  let(:package_source) { File.join(CHEF_SPEC_ASSETS, "chocolatey_feed") }
+    let(:package_name) { "test-A" }
+    let(:package_list) { proc { powershell_out!("choco list -lo -r #{Array(package_name).join(' ')}").stdout.chomp } }
+    let(:package_source) { File.join(CHEF_SPEC_ASSETS, "chocolatey_feed") }
 
-  subject do
-    new_resource = Chef::Resource::ChocolateyPackage.new("test choco package", run_context)
-    new_resource.package_name package_name
-    new_resource.source package_source
-    new_resource
-  end
-
-  context "installing a package" do
-    after { remove_package }
-
-    it "installs the latest version" do
-      subject.run_action(:install)
-      expect(package_list.call).to eq("#{package_name}|2.0")
+    subject do
+      new_resource = Chef::Resource::ChocolateyPackage.new("test choco package", run_context)
+      new_resource.package_name package_name
+      new_resource.source package_source
+      new_resource
     end
 
-    it "does not install if already installed" do
-      subject.run_action(:install)
-      subject.run_action(:install)
-      expect(subject).not_to be_updated_by_last_action
-    end
+    context "installing a package" do
+      after { remove_package }
 
-    it "installs version given" do
-      subject.version "1.0"
-      subject.run_action(:install)
-      expect(package_list.call).to eq("#{package_name}|1.0")
-    end
-
-    it "installs new version if one is already installed" do
-      subject.version "1.0"
-      subject.run_action(:install)
-      expect(package_list.call).to eq("#{package_name}|1.0")
-
-      subject.version "2.0"
-      subject.run_action(:install)
-      expect(package_list.call).to eq("#{package_name}|2.0")
-    end
-
-    context "installing multiple packages" do
-      let(:package_name) { [ "test-A", "test-B" ] }
-
-      it "installs both packages" do
+      it "installs the latest version" do
         subject.run_action(:install)
-        expect(package_list.call).to eq("test-A|2.0\r\ntest-B|1.0")
+        expect(package_list.call).to eq("#{package_name}|2.0")
+      end
+
+      it "does not install if already installed" do
+        subject.run_action(:install)
+        subject.run_action(:install)
+        expect(subject).not_to be_updated_by_last_action
+      end
+
+      it "installs version given" do
+        subject.version "1.0"
+        subject.run_action(:install)
+        expect(package_list.call).to eq("#{package_name}|1.0")
+      end
+
+      it "installs new version if one is already installed" do
+        subject.version "1.0"
+        subject.run_action(:install)
+        expect(package_list.call).to eq("#{package_name}|1.0")
+
+        subject.version "2.0"
+        subject.run_action(:install)
+        expect(package_list.call).to eq("#{package_name}|2.0")
+      end
+
+      context "installing multiple packages" do
+        let(:package_name) { [ "test-A", "test-B" ] }
+
+        it "installs both packages" do
+          subject.run_action(:install)
+          expect(package_list.call).to eq("test-A|2.0\r\ntest-B|1.0")
+        end
+      end
+
+      it "raises if package is not found" do
+        subject.package_name "blah"
+        expect { subject.run_action(:install) }.to raise_error Chef::Exceptions::Package
       end
     end
 
-    it "raises if package is not found" do
-      subject.package_name "blah"
-      expect { subject.run_action(:install) }.to raise_error Chef::Exceptions::Package
+    context "upgrading a package" do
+      after { remove_package }
+
+      it "upgrades to a specific version" do
+        subject.version "1.0"
+        subject.run_action(:install)
+        expect(package_list.call).to eq("#{package_name}|1.0")
+
+        subject.version "1.5"
+        subject.run_action(:upgrade)
+        expect(package_list.call).to eq("#{package_name}|1.5")
+      end
+
+      it "upgrades to the latest version if no version given" do
+        subject.version "1.0"
+        subject.run_action(:install)
+        expect(package_list.call).to eq("#{package_name}|1.0")
+
+        subject2 = Chef::Resource::ChocolateyPackage.new("test-A", run_context)
+        subject2.source package_source
+        subject2.run_action(:upgrade)
+        expect(package_list.call).to eq("#{package_name}|2.0")
+      end
     end
-  end
 
-  context "upgrading a package" do
-    after { remove_package }
-
-    it "upgrades to a specific version" do
-      subject.version "1.0"
-      subject.run_action(:install)
-      expect(package_list.call).to eq("#{package_name}|1.0")
-
-      subject.version "1.5"
-      subject.run_action(:upgrade)
-      expect(package_list.call).to eq("#{package_name}|1.5")
+    context "removing a package" do
+      it "removes an installed package" do
+        subject.run_action(:install)
+        remove_package
+        expect(package_list.call).to eq("")
+      end
     end
 
-    it "upgrades to the latest version if no version given" do
-      subject.version "1.0"
-      subject.run_action(:install)
-      expect(package_list.call).to eq("#{package_name}|1.0")
-
-      subject2 = Chef::Resource::ChocolateyPackage.new("test-A", run_context)
-      subject2.source package_source
-      subject2.run_action(:upgrade)
-      expect(package_list.call).to eq("#{package_name}|2.0")
+    def remove_package
+      pkg_to_remove = Chef::Resource::ChocolateyPackage.new(package_name, run_context)
+      pkg_to_remove.source = package_source
+      pkg_to_remove.run_action(:remove)
     end
-  end
-
-  context "removing a package" do
-    it "removes an installed package" do
-      subject.run_action(:install)
-      remove_package
-      expect(package_list.call).to eq("")
-    end
-  end
-
-  def remove_package
-    pkg_to_remove = Chef::Resource::ChocolateyPackage.new(package_name, run_context)
-    pkg_to_remove.source = package_source
-    pkg_to_remove.run_action(:remove)
   end
 end
