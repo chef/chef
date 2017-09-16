9b0e03786d0fdf954d36eafb4dde9db49acd3840
Tim Smith <tsmith@chef.io>
2017-09-15 22:09:37 -0700

    Use Chef's built in multipackage
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- kitchen-tests/cookbooks/base/recipes/packages.rb
+++ kitchen-tests/cookbooks/base/recipes/packages.rb
@@ -7,12 +7,7 @@
 
 # this is just a list of package that exist on every O/S we test, and often aren't installed by default.  you don't
 # have to get too clever here, you can delete packages if they don't exist everywhere we test.
-pkgs = %w{lsof tcpdump strace zsh dmidecode ltrace bc curl wget telnet subversion git traceroute htop tmux }
-
-# this deliberately calls the multipackage API N times in order to do one package installation in order to exercise multipackage api
-pkgs.each do |pkg|
-  multipackage pkgs
-end
+package %w{lsof tcpdump strace zsh dmidecode ltrace bc curl wget telnet subversion git traceroute htop tmux }
 
 gems = %w{fpm aws-sdk}
 
