25cf147c73e60ac5f7a2b4a8a1967b7637334473
Tim Smith <tsmith@chef.io>
2017-12-13 15:55:00 -0800

    Update openssl to 1.0.2.n
    Also properly update the omnibus_overrides file. I didn't regen this last time so the libs never updated.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- version_policy.rb
+++ version_policy.rb
@@ -33,7 +33,7 @@ OMNIBUS_OVERRIDES = {
   "makedepend" => "1.0.5",
   "ncurses" => "5.9",
   "pkg-config-lite" => "0.28-1",
-  "ruby" => "2.3.1",
+  "ruby" => "2.3.5",
   # Leave dev-kit pinned to 4.5 on 32-bit, because 4.7 is 20MB larger and we don't want
   # to unnecessarily make the client any fatter. (Since it's different between
   # 32 and 64, we have to do it in the project file still.)
@@ -45,7 +45,7 @@ OMNIBUS_OVERRIDES = {
 
   ## These can float as they are frequently updated in a way that works for us
   #override "cacerts" =>"???",
-  "openssl" => "1.0.2l",
+  "openssl" => "1.0.2n",
 }
 
 #
