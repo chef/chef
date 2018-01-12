b46ae663ddf0277c88dea188bffbadb5c4e2fabb
Zane Geiger <zane.geiger@relativity.com>
2018-01-12 14:29:59 -0600

    Change single quotes to double quotes to appease the Style Gods
    Signed-off-by: Zane Geiger <zane.geiger@relativity.com>

--- lib/chef/knife/search.rb
+++ lib/chef/knife/search.rb
@@ -99,7 +99,7 @@ class Chef
           q.search(@type, @query, search_args) do |item|
             formatted_item = Hash.new
             if config[:id_only]
-              formatted_item = format_for_display({ 'id' => item["__display_name"] })
+              formatted_item = format_for_display({ "id" => item["__display_name"] })
             elsif item.is_a?(Hash)
               # doing a little magic here to set the correct name
               formatted_item[item["__display_name"]] = item.reject { |k| k == "__display_name" }
