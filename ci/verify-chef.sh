ea5ca5745a6c3dfa597a2d7655b96ffdbdef35da
Scott Hain <shain@chef.io>
2018-02-15 11:55:21 -0800

    Kill any orphaned tests
    Signed-off-by: Scott Hain <shain@chef.io>

--- ci/verify-chef.sh
+++ ci/verify-chef.sh
@@ -8,6 +8,9 @@ export TMPDIR
 sudo rm -rf $TMPDIR
 mkdir -p $TMPDIR
 
+# Verify that we kill any orphaned test processes. Kill any orphaned rspec processes.
+ps ax | egrep 'rspec' | grep -v grep | awk '{ print $1 }' | xargs sudo kill -s KILL || true
+
 # $PROJECT_NAME is set by Jenkins, this allows us to use the same script to verify
 # Chef and Angry Chef
 PATH=/opt/$PROJECT_NAME/bin:$PATH
