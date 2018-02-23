155d173ec342e4a6f7bb36f2a2f7a0ce6e941313
Chef Expeditor <eng-services-ops+chef-ci@chef.io>
2018-02-23 15:27:40 +0000

    Bump version to 13.8.0 by Expeditor

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -21,7 +21,7 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "13.7.28"
+  VERSION = "13.8.0"
 end
 
 #
--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -23,7 +23,7 @@ require "chef/version_string"
 
 class Chef
   CHEF_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = Chef::VersionString.new("13.7.28")
+  VERSION = Chef::VersionString.new("13.8.0")
 end
 
 #
