a0c8791fe970954ba089194a7dc8e875bc9a66fa
Lamont Granquist <lamont@scriptkiddie.org>
2018-02-28 12:22:43 -0800

    also not a hash
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/knife/environment_compare.rb
+++ lib/chef/knife/environment_compare.rb
@@ -81,7 +81,7 @@ class Chef
 
       def constraint_list(environments)
         constraints = {}
-        environments.each_key do |env|
+        environments.each do |env, url| # rubocop:disable Performance/HashEachMethods
           # Because you cannot modify the default environment I filter it out here.
           unless env == "_default"
             envdata = Chef::Environment.load(env)
