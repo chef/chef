d5cc1c7a3fffea27abfbec3ad1faaa9daa1db584
Lamont Granquist <lamont@scriptkiddie.org>
2018-03-06 14:35:43 -0800

    chef_fs fix for ruby 2.5
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/chef_fs/path_utils.rb
+++ lib/chef/chef_fs/path_utils.rb
@@ -1,6 +1,6 @@
 #
 # Author:: John Keiser (<jkeiser@chef.io>)
-# Copyright:: Copyright 2012-2016, Chef Software Inc.
+# Copyright:: Copyright 2012-2018, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -90,7 +90,7 @@ class Chef
           begin
             path = File.realpath(path)
             break
-          rescue Errno::ENOENT
+          rescue Errno::ENOENT, Errno::EINVAL
             suffix << File.basename(path)
             path = parent_path
             parent_path = File.dirname(path)
