824a381b93fdd457ad3bde2abbe40d81de1954e9
Tim Smith <tsmith@chef.io>
2018-03-16 18:10:21 -0700

    Remove Chef 12-isms from the apt_repository resource
    We set a lot of nil defaults and did coersions here that are no longer necessary. Since we did accept nil in a lot of these properties at one point we need to keep it so I wrote a note to future me about that. I also added some specs for the remaining coersion and the defaults.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/resource/apt_repository_spec.rb
+++ spec/unit/resource/apt_repository_spec.rb
@@ -20,12 +20,43 @@ require "spec_helper"
 
 describe Chef::Resource::AptRepository do
   let(:node) { Chef::Node.new }
-  let(:events) { Chef::EventDispatch::Dispatcher.new }
-  let(:run_context) { Chef::RunContext.new(node, {}, events) }
+  let(:run_context) do
+    node.automatic[:lsb][:codename] = "superduper"
+    empty_events = Chef::EventDispatch::Dispatcher.new
+    Chef::RunContext.new(node, {}, empty_events)
+  end
   let(:resource) { Chef::Resource::AptRepository.new("multiverse", run_context) }
 
-  it "uses keyserver.ubuntu.com as the keyserver" do
+  it "keyserver defaults to keyserver.ubuntu.com" do
     expect(resource.keyserver).to eql("keyserver.ubuntu.com")
+  end
+
+  it "repo_name is the name property" do
+    expect(resource.repo_name).to eql("multiverse")
+  end
+
+  it "distribution defaults to the distro codename" do
+    expect(resource.distribution).to eql("superduper")
+  end
+
+  it "allows setting key to an Array of keys and does not coerce it" do
+    resource.key = %w{key1 key2}
+    expect(resource.key).to eql(%w{key1 key2})
+  end
+
+  it "allows setting key to nil and does not coerce it" do
+    resource.key = nil
+    expect(resource.key).to be_nil
+  end
+
+  it "allows setting key to false and does not coerce it" do
+    resource.key = false
+    expect(resource.key).to be false
+  end
+
+  it "allows setting key to a String and coerces it to an Array" do
+    resource.key = "key1"
+    expect(resource.key).to eql(["key1"])
   end
 
   it "fails if the user provides a repo_name with a forward slash" do
