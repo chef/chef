edeb5fe782cc75b8f89c1e436a655b310a295e77
Stuart Preston <stuart@chef.io>
2018-05-17 15:36:25 +0100

    Remote file content provider should not fetch the file in --why-run mode
    Signed-off-by: Stuart Preston <stuart@chef.io>

--- lib/chef/provider/remote_file/content.rb
+++ lib/chef/provider/remote_file/content.rb
@@ -68,7 +68,7 @@ class Chef
 
         # Given a source uri, return a Tempfile, or a File that acts like a Tempfile (close! method)
         def grab_file_from_uri(uri)
-          Chef::Provider::RemoteFile::Fetcher.for_resource(uri, @new_resource, @current_resource).fetch
+          Chef::Provider::RemoteFile::Fetcher.for_resource(uri, @new_resource, @current_resource).fetch unless Chef::Config[:why_run]
         end
 
         def current_resource_matches_target_checksum?
