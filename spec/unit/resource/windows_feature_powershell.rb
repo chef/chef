4e64fd215f637a0bd49c04cebb6f18446d57c3cf
Tim Smith <tsmith@chef.io>
2018-06-07 18:36:08 -0700

    Add windows_feature_powershell specs for running on 2k8r2
    Copy the specs from windows_feature_dism.  Also fix the platform_version value to match actual ohai data.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/resource/windows_feature_powershell.rb
+++ spec/unit/resource/windows_feature_powershell.rb
@@ -18,10 +18,22 @@
 require "spec_helper"
 
 describe Chef::Resource::WindowsFeaturePowershell do
-  let(:resource) { Chef::Resource::WindowsFeaturePowershell.new(%w{SNMP DHCP}) }
+  let(:node) { Chef::Node.new }
+  let(:events) { Chef::EventDispatch::Dispatcher.new }
+  let(:run_context) { Chef::RunContext.new(node, {}, events) }
+  let(:resource) { Chef::Resource::WindowsFeaturePowershell.new(%w{SNMP DHCP}, run_context) }
 
   it "sets resource name as :windows_feature_powershell" do
     expect(resource.resource_name).to eql(:windows_feature_powershell)
+  end
+
+  it "sets the default action as :install" do
+    expect(resource.action).to eql([:install])
+  end
+
+  it "the feature_name property is the name_property" do
+    node.automatic[:platform_version] = "6.2.9200"
+    expect(resource.feature_name).to eql(%w{snmp dhcp})
   end
 
   it "sets the default action as :install" do
@@ -34,18 +46,27 @@ describe Chef::Resource::WindowsFeaturePowershell do
     expect { resource.action :remove }.not_to raise_error
   end
 
-  it "sets the feature_name property as its name_property" do
-    expect(resource.feature_name).to eql(%w{SNMP DHCP})
+  it "coerces comma separated lists of features to a lowercase array on 2012+" do
+    node.automatic[:platform_version] = "6.2.9200"
+    resource.feature_name "SNMP, DHCP"
+    expect(resource.feature_name).to eql(%w{snmp dhcp})
   end
 
-  it "coerces comma separated lists of features to arrays" do
+  it "coerces a single feature as a String to a lowercase array on 2012+" do
+    node.automatic[:platform_version] = "6.2.9200"
+    resource.feature_name "SNMP"
+    expect(resource.feature_name).to eql(["snmp"])
+  end
+
+  it "coerces comma separated lists of features to an array, but preserves case on < 2012" do
+    node.automatic[:platform_version] = "6.1.7601"
     resource.feature_name "SNMP, DHCP"
     expect(resource.feature_name).to eql(%w{SNMP DHCP})
   end
 
-  it "coerces a single feature as a String into an array" do
+  it "coerces a single feature as a String to an array, but preserves case on < 2012" do
+    node.automatic[:platform_version] = "6.1.7601"
     resource.feature_name "SNMP"
     expect(resource.feature_name).to eql(["SNMP"])
   end
-
 end
