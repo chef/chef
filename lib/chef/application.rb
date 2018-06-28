42535b2b0327be3fb23ebf6cf581148e9591bef3
Nolan Davidson <ndavidson@chef.io>
2018-06-28 11:45:06 -0400

    Fix dupe stdout_logger
    I _think_ this resolves issue #7184
    
    Basically reverting back to the previous logic of
    `want_additional_logger?`. It was mentioned in the issue comments that
    the comparison against `STDOUT` wasn't working, but it seems to work for
    me currently on Centos7.
    
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- lib/chef/application.rb
+++ lib/chef/application.rb
@@ -177,7 +177,7 @@ class Chef
     # Based on config and whether or not STDOUT is a tty, should we setup a
     # secondary logger for stdout?
     def want_additional_logger?
-      !( Chef::Config[:log_location].is_a?(IO) && Chef::Config[:log_location].tty? ) && !Chef::Config[:daemonize]
+      ( Chef::Config[:log_location] != STDOUT ) && STDOUT.tty? && !Chef::Config[:daemonize]
     end
 
     def configure_stdout_logger
