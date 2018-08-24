ea8d850ca3a44305c143dbd70420f09895132ed7
Tim Smith <tsmith@chef.io>
2018-08-24 13:47:42 -0700

    Fix the openssl unit tests
    Rename the fixes
    Update the mode tests
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/resource/openssl_x509_crl_spec.rb
+++ spec/unit/resource/openssl_x509_crl_spec.rb
@@ -17,12 +17,12 @@
 
 require "spec_helper"
 
-describe Chef::Resource::OpensslX509Request do
+describe Chef::Resource::OpensslX509Crl do
 
-  let(:resource) { Chef::Resource::OpensslX509Request.new("fakey_fakerton") }
+  let(:resource) { Chef::Resource::OpensslX509Crl.new("fakey_fakerton") }
 
-  it "has a resource name of :openssl_x509_request" do
-    expect(resource.resource_name).to eql(:openssl_x509_request)
+  it "has a resource name of :openssl_x509_crl" do
+    expect(resource.resource_name).to eql(:openssl_x509_crl)
   end
 
   it "the path property is the name_property" do
@@ -37,31 +37,25 @@ describe Chef::Resource::OpensslX509Request do
     expect { resource.action :create }.not_to raise_error
   end
 
-  it "has a default mode of '0644'" do
-    expect(resource.mode).to eql("0644")
+  it "has a default revocation_reason of 0" do
+    expect(resource.revocation_reason).to eql(0)
   end
 
-  it "has a default key_type of 'ec'" do
-    expect(resource.key_type).to eql("ec")
+  it "has a default expiration of 8" do
+    expect(resource.expire).to eql(8)
   end
 
-  it "only accepts valid key_type values" do
-    expect { resource.key_type "fako" }.to raise_error(ArgumentError)
+  it "has a default renewal_threshold of 1" do
+    expect(resource.renewal_threshold).to eql(1)
   end
 
-  it "has a default key_length of '2048'" do
-    expect(resource.key_length).to eql(2048)
+  it "serial_to_revoke accepts both String and Integer values" do
+    expect { resource.serial_to_revoke "123" }.not_to raise_error
+    expect { resource.serial_to_revoke 123 }.not_to raise_error
   end
 
-  it "only accepts valid key_length values" do
-    expect { resource.key_length 1023 }.to raise_error(ArgumentError)
-  end
-
-  it "has a default key_curve of 'prime256v1'" do
-    expect(resource.key_curve).to eql("prime256v1")
-  end
-
-  it "only accepts valid key_curve values" do
-    expect { resource.key_curve "fako" }.to raise_error(ArgumentError)
+  it "mode accepts both String and Integer values" do
+    expect { resource.mode "644" }.not_to raise_error
+    expect { resource.mode 644 }.not_to raise_error
   end
 end
