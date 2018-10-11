1027712e4a17f4c376c31f0a2fd8ec5080bc248c
Lamont Granquist <lamont@scriptkiddie.org>
2018-10-10 22:27:41 -0700

    checkpoint
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/provider/file/file_editor.rb
+++ lib/chef/provider/file/file_editor.rb
@@ -62,62 +62,171 @@ class Chef
           @file_contents = []
         end
 
-        def append_if_no_such_line(line)
-          regex = /^#{Regexp.escape(line)}$/
-          append_if_no_line_matching(regex, line)
+        # set the eol string for the file
+        def eol(val)
         end
 
-        def append_if_no_line_matching(pattern, line)
+        # repetetive "append_if_no_such_line"
+        def append_lines(lines, replace: false, unique: false)
+          chkarg 1, lines, Array
+          chkarg :replace, replace, [ true, false ]
+          chkarg :unique, unique, [ true, false ]
+
+          unless lines.is_a?(Hash)
+            lines = lines.split("\n") unless lines.is_a?(Array)
+            lines.map(&:chomp!)
+            lines = lines.each_with_object({}) do |line, hash|
+              regex = /^#{Regexp.escape(line)}$/
+              hash[regex] = line
+            end
+          end
+          lines.each do |regex, line|
+            append_line_unless_match(regex, line, replace: replace, unique: unique) && modified = true
+          end
+          modified ? file_contents : nil
+        end
+
+        # requires first line to be delimiter match
+        # def append_block(lines)
+        #  lines = lines.split("\n") unless lines.is_a?(Array)
+        #  lines.map(&:chomp!)
+        #  regex = /#{Regexp.escape(lines[0])}/
+        #  i = file_contents.find_index { |l| l =~ regex }
+        #  return unless i
+        #  return if file_contents.size > i + 1 && file_contents[i + 1] == line
+        #  file_contents.insert(i + 1, line + "\n")
+        # end
+
+        # lower level one-line-at-a-time
+        def append_line_unless_match(pattern, line, replace: false, unique: false)
+          chkarg 1, pattern, [ String, Regexp ]
+          chkarg 2, line, String
+          chkarg :replace, replace, [ true, false ]
+          chkarg :unique, unique, [ true, false ]
+
+          modified = false
           regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
-          file_contents.push(line + "\n") if file_contents.grep(regex).empty?
+          if file_contents.grep(regex).empty?
+            unless file_contents.empty?
+              file_contents[-1].chomp!
+              file_contents[-1] << "\n"
+            end
+            file_contents.push(line + "\n")
+            modified = true
+          else
+            if replace
+              replace_lines(regex, line, unique: unique ? :last : false) && modified = true
+            end
+          end
+          modified ? file_contents : nil
         end
 
-        def append_after_line_matching(pattern, line)
-          regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
-          i = file_contents.find_index { |l| l =~ regex }
-          return unless i
-          return if file_contents.size > i + 1 && file_contents[i + 1] == line
-          file_contents.insert(i + 1, line + "\n")
+        # repetitive "prepend_if_no_such_line"
+        def prepend_lines(lines)
+          lines = lines.split("\n") unless lines.is_a?(Array)
+          lines.map(&:chomp!)
+          lines.reverse.each do |line|
+            regex = /^#{Regexp.escape(line)}$/
+            prepend_line_unless_match(regex, line)
+          end
         end
 
-        # FIXME: should this be replace lines?
-        def insert_between_lines_matching(line, start:, finish:)
-        end
+        # requires last line to be delimiter match
+        # def prepend_block(lines)
+        #  regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
+        #  i = file_contents.find_index { |l| l =~ regex }
+        #  return unless i
+        #  i = 1 if i == 0
+        #  return if file_contents[i - 1] == line
+        #  file_contents.insert(i - 1, line + "\n")
+        # end
 
-        def prepend_if_no_such_line(line)
-          regex = /^#{Regexp.escape(line)}$/
-          prepend_if_no_line_matching(regex, line)
-        end
-
-        def prepend_if_no_line_matching(pattern, line)
+        def prepend_line_unless_match(pattern, line)
           regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
           file_contents.unshift(line + "\n") if file_contents.grep(regex).empty?
         end
 
-        def prepend_before_line_matching(pattern, line)
-          regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
-          i = file_contents.find_index { |l| l =~ regex }
-          return unless i
-          i = 1 if i == 0
-          return if file_contents[i - 1] == line
-          file_contents.insert(i - 1, line + "\n")
-        end
-
+        # mass delete
         def delete_lines_matching(pattern)
           regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
           file_contents.reject! { |l| l =~ regex }
         end
 
-        def substitute_lines(pattern, replace, global: false)
-          regex = pattern.is_a?(String) ? /#{Regexp.escape(pattern)}/ : pattern
-          if global
-            file_contents.each { |l| l.gsub(regex, replace) }
-          else
-            file_contents.each { |l| l.sub(regex, replace) }
+        # delimited delete
+        def delete_between(start:, finish:, inclusive: false)
+          start = start.is_a?(String) ? /#{Regexp.escape(start)}/ : start
+          finish = finish.is_a?(String) ? /#{Regexp.escape(finish)}/ : finish
+          # find the start
+          i_start = file_contents.find_index { |l| l =~ start }
+          return unless i_start
+          # find the finish
+          i_finish = nil
+          i_start.upto(file_contents.size - 1) do |i|
+            if i >= i_start && file_contents[i] =~ finish
+              i_finish = i
+              break
+            end
           end
+          return unless i_finish
+          file_contents.slice!(i_start, i_finish)
         end
 
-        def substitution_block(start:, finish:, match:, replace:, global: false)
+        def replace_between(lines, start:, finish:)
+        end
+
+        # Search the entire file for matches on each line.  When the line matches, replace the line with the given string.  Can also be
+        # used to assert that only one occurance of the match is kept in the file.
+        #
+        # @param match [ String, Regexp ] The regular expression or substring to match
+        # @param line [ String ] The line to replace matching lines with
+        # @param unique [ false, :first, :last ] If unique is false, all lines are replaced.  If unique is set to :first or :last only the
+        #        first or last match is replaced and the other matches are deleted from the file.
+        # @return [ Array<String> ] the file_contets array, or nil if there was no modifications
+        #
+        def replace_lines(match, line, unique: false)
+          chkarg 1, match, [ String, Regexp ]
+          chkarg 2, line, String
+          chkarg :unique, unique, [ false, :first, :last ]
+
+          regex = match.is_a?(String) ? /#{Regexp.escape(match)}/ : match
+          modified = false
+          file_contents.map! do |l|
+            if l != line + "\n" && regex.match?(l)
+              modified = true
+              line + "\n"
+            else
+              l
+            end
+          end
+          modified ? file_contents : nil
+        end
+
+        # mass search-and-replace on substrings
+        def substitute_lines(match, replace, global: false)
+          chkarg 1, match, [ String, Regexp ]
+          chkarg 2, replace, String
+          chkarg :global, global, [ false, :first, :last ]
+
+          regex = match.is_a?(String) ? /#{Regexp.escape(match)}/ : match
+          modified = false
+          if global
+            file_contents.each do |l|
+              old = l
+              l.gsub!(regex, replace)
+              modified = true if l != old
+            end
+          else
+            file_contents.each do |l|
+              old = l
+              l.sub!(regex, replace)
+              modified = true if l != old
+            end
+          end
+          modified ? file_contents : nil
+        end
+
+        # delimited search-and-replace
+        def substitute_between(start:, finish:, match:, replace:, global: false, inclusive: false)
           start = start.is_a?(String) ? /#{Regexp.escape(start)}/ : start
           finish = finish.is_a?(String) ? /#{Regexp.escape(finish)}/ : finish
           match = match.is_a?(String) ? /#{Regexp.escape(match)}/ : match
@@ -141,24 +250,6 @@ class Chef
           end
         end
 
-        def delete_block(start:, finish:)
-          start = start.is_a?(String) ? /#{Regexp.escape(start)}/ : start
-          finish = finish.is_a?(String) ? /#{Regexp.escape(finish)}/ : finish
-          # find the start
-          i_start = file_contents.find_index { |l| l =~ start }
-          return unless i_start
-          # find the finish
-          i_finish = nil
-          i_start.upto(file_contents.size - 1) do |i|
-            if i >= i_start && file_contents[i] =~ finish
-              i_finish = i
-              break
-            end
-          end
-          return unless i_finish
-          file_contents.slice!(i_start, i_finish)
-        end
-
         # NOTE: This is intented to be used only on a tempfile so we open, truncate and append
         # because the file provider already has the machinery to atomically move a tempfile into place.
         # If we crash in the middle it doesn't matter if we leave a corrupted tempfile to be
@@ -172,6 +263,40 @@ class Chef
         def finish!
           ::File.open(path, "w") do |f|
             f.write file_contents.join
+          end
+        end
+
+        private
+
+        # FIXME: make this a mixin in chef-helper
+        # @api private
+        def chkarg(what, arg, matches)
+          matches = Array( matches )
+          matches.each do |match|
+            return true if match === arg
+          end
+          method = caller_locations(1, 1)[0].label
+          whatstr = if what.is_a?(Integer)
+                      "#{ordinalize(what)} argument"
+                    else
+                      "named '#{what}' argument"
+                    end
+          raise ArgumentError, "#{whatstr} to #{method} must be one of: #{matches.map { |v| v.inspect }.join(", ")}, you gave: #{arg.inspect}"
+        end
+
+        # FIXME: make this a mixin in chef-helper (and yeah we could humanize it and spell it out, but ain't got time for that)
+        # @api private
+        def ordinalize(int)
+          s = int.to_s
+          case
+          when s.end_with?("1")
+            "#{int}st"
+          when s.end_with?("2")
+            "#{int}nd"
+          when s.end_with?("3")
+            "#{int}rd"
+          else
+            "#{int}th"
           end
         end
       end
--- spec/functional/resource/file_edit_spec.rb
+++ spec/functional/resource/file_edit_spec.rb
@@ -1,5 +1,5 @@
 #
-# Copyright:: Copyright 2018, Chef Software, Inc.
+# Copyright:: Copyright 2018-2018, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,10 +33,31 @@ describe "edit property of the file resource" do
 
   let(:r) { Chef::Resource::File.new(path, run_context) }
 
-  context "#append_if_no_such_line" do
+  context "#empty!" do
+    it "empties a file" do
+      File.open(path, "a+") { |f| f.puts "stuff\nthings\notherstuff\n" }
+      r.edit do
+        empty!
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to be_empty
+    end
+
+    it "empties a file before an append" do
+      File.open(path, "a+") { |f| f.puts "stuff\nthings\notherstuff\n" }
+      r.edit do
+        empty!
+        append_lines "appended"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{appended\n})
+    end
+  end
+
+  context "#append_lines" do
     it "creates a file and appends to the empty file" do
       r.edit do
-        append_if_no_such_line "appended"
+        append_lines "appended"
       end
       r.run_action(:create)
       expect(IO.read(path).lines).to eql(["appended\n"])
@@ -45,7 +66,7 @@ describe "edit property of the file resource" do
     it "appends to a file that already exists" do
       FileUtils.touch(path)
       r.edit do
-        append_if_no_such_line "appended"
+        append_lines "appended"
       end
       r.run_action(:create)
       expect(IO.read(path).lines).to eql(["appended\n"])
@@ -54,7 +75,16 @@ describe "edit property of the file resource" do
     it "appends to a file that already exists with stuff in it" do
       File.open(path, "a+") { |f| f.puts "stuff" }
       r.edit do
-        append_if_no_such_line "appended"
+        append_lines "appended"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{stuff\n appended\n})
+    end
+
+    it "appends to a file that already exists with stuff in it, where stuff does not have a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        append_lines "appended"
       end
       r.run_action(:create)
       expect(IO.read(path).lines).to eql(%W{stuff\n appended\n})
@@ -62,7 +92,7 @@ describe "edit property of the file resource" do
 
     it "appends to content in the resource" do
       r.edit do
-        append_if_no_such_line "appended"
+        append_lines "appended"
       end
       r.content "stuff\n"
       r.run_action(:create)
@@ -71,11 +101,377 @@ describe "edit property of the file resource" do
 
     it "appends to content in the resource on a new line" do
       r.edit do
-        append_if_no_such_line "appended"
+        append_lines "appended"
       end
       r.content "stuff"
       r.run_action(:create)
       expect(IO.read(path).lines).to eql(%W{stuff\n appended\n})
     end
+
+    it "does not append multiple times" do
+      r.edit do
+        append_lines "appended"
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{appended\n})
+    end
+
+    it "when not appending, preserves files that do not end in a newline" do
+      File.open(path, "a+") { |f| f.write "matching\nstuff" }
+      r.edit do
+        append_lines "matching"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{matching\n stuff})
+    end
+
+    it "when not appending, preserves files that do not end in a newline, when the matching pattern is the line without the newline" do
+      File.open(path, "a+") { |f| f.write "matching\nstuff" }
+      r.edit do
+        append_lines "stuff"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{matching\n stuff})
+    end
+
+    it "does not append multiple times, where file does not already have a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        append_lines "appended"
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{stuff\n appended\n})
+    end
+
+    it "appends with a multi line string" do
+      r.edit do
+        append_lines <<~EOF
+          append1
+          append2
+        EOF
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{append1\n append2\n})
+    end
+
+    it "is idempotent with a multi line string" do
+      r.edit do
+        append_lines <<~EOF
+          append1
+          append2
+        EOF
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{append1\n append2\n})
+    end
+
+    it "is idempotent with a multi line string, on a file that contains a line without a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        append_lines <<~EOF
+          append1
+          append2
+        EOF
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{stuff\n append1\n append2\n})
+    end
+
+    it "appends with a array of strings" do
+      r.edit do
+        append_lines %W{append1\n append2\n}
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{append1\n append2\n})
+    end
+
+    it "is idempotent with an array of strings" do
+      r.edit do
+        append_lines %W{append1\n append2\n}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{append1\n append2\n})
+    end
+
+    it "is idempotent with an array of strings, on a file that contains a line without a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        append_lines %W{append1\n append2\n}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{stuff\n append1\n append2\n})
+    end
+
+    it "appends with a array of strings without newlines" do
+      r.edit do
+        append_lines %w{append1 append2}
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{append1\n append2\n})
+    end
+
+    it "is idempotent with an array of strings wihtout newlines" do
+      r.edit do
+        append_lines %w{append1 append2}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{append1\n append2\n})
+    end
+
+    it "is idempotent with an array of strings without newlines, on a file that contains a line without a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        append_lines %w{append1 append2}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{stuff\n append1\n append2\n})
+    end
+
+    it "hashes append lines that don't match, and doesn't update lines that do match" do
+      File.open(path, "a+") { |f| f.write "FOO=noreplace1\nBAR=noreplace2" }
+      r.edit do
+        append_lines({
+          "FOO=" => "FOO=shouldntreplace1",
+          "BAR=appended1" => "BAR=appended1",
+        })
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{FOO=noreplace1\n BAR=noreplace2\n BAR=appended1\n})
+    end
+
+    it "hashes append lines that don't match, and doesn't update lines that do match when using regexp keys" do
+      File.open(path, "a+") { |f| f.write "FOO=noreplace1\nBAR=noreplace2" }
+      r.edit do
+        append_lines({
+          /^.*FOO=/ => "FOO=shouldntreplace1",
+          /^.*BAR=appended1/ => "BAR=appended1",
+        })
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{FOO=noreplace1\n BAR=noreplace2\n BAR=appended1\n})
+    end
+
+    it "hashes append lines that don't match, and does update lines that do match, when replace is true" do
+      File.open(path, "a+") { |f| f.write "FOO=replace\nBAR=noreplace2" }
+      r.edit do
+        append_lines({
+          "FOO=" => "FOO=replaced",
+          "BAR=appended1" => "BAR=appended1",
+        }, replace: true)
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{FOO=replaced\n BAR=noreplace2\n BAR=appended1\n})
+    end
+
+    it "hashes append lines that don't match, and does update lines that do match when using regexp keys, when replace is true" do
+      File.open(path, "a+") { |f| f.write "FOO=replace\nBAR=noreplace2" }
+      r.edit do
+        append_lines({
+          /^.*FOO=/ => "FOO=replaced",
+          /^.*BAR=appended1/ => "BAR=appended1",
+        }, replace: true)
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{FOO=replaced\n BAR=noreplace2\n BAR=appended1\n})
+    end
+
+    it "hashes replace all the matching lines when replace is true" do
+      # note that substrings match, and a regexp has to be used to anchor
+      File.open(path, "a+") { |f| f.write "FOO=replace1\nBAR=noreplace2\nFIZZFOO=replace2" }
+      r.edit do
+        append_lines({
+          "FOO=" => "FOO=replaced",
+          "BAR=appended1" => "BAR=appended1",
+        }, replace: true)
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{FOO=replaced\n BAR=noreplace2\n FOO=replaced\n BAR=appended1\n})
+    end
+
+    it "hashes delete all matches except the last line which is replaced when replace is true and unique is true" do
+      # note that substrings match, and a regexp has to be used to anchor
+      File.open(path, "a+") { |f| f.write "FOO=replace1\nBAR=noreplace2\nFIZZFOO=replace2" }
+      r.edit do
+        append_lines({
+          "FOO=" => "FOO=replaced",
+          "BAR=appended1" => "BAR=appended1",
+        }, replace: true, unique: true)
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{BAR=noreplace2\n FOO=replaced\n BAR=appended1\n})
+    end
+  end
+
+  context "#prepend_lines" do
+    it "creates a file and prepends to the empty file" do
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(["prepended\n"])
+    end
+
+    it "prepends to a file that already exists" do
+      FileUtils.touch(path)
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(["prepended\n"])
+    end
+
+    it "prepends to a file that already exists with stuff in it" do
+      File.open(path, "a+") { |f| f.puts "stuff" }
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepended\n stuff\n})
+    end
+
+    it "prepends to a file that already exists with stuff in it, where stuff does not have a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepended\n stuff})
+    end
+
+    it "prepends to content in the resource" do
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.content "stuff\n"
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepended\n stuff\n})
+    end
+
+    it "prepends to content in the resource on a new line" do
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.content "stuff"
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepended\n stuff})
+    end
+
+    it "does not prepend multiple times" do
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepended\n})
+    end
+
+    it "does not prepend multiple times, where file does not already have a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        prepend_lines "prepended"
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepended\n stuff})
+    end
+
+    it "prepends with a multi line string" do
+      r.edit do
+        prepend_lines <<~EOF
+          prepend1
+          prepend2
+        EOF
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n})
+    end
+
+    it "is idempotent with a multi line string" do
+      r.edit do
+        prepend_lines <<~EOF
+          prepend1
+          prepend2
+        EOF
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n})
+    end
+
+    it "is idempotent with a multi line string, on a file that contains a line without a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        prepend_lines <<~EOF
+          prepend1
+          prepend2
+        EOF
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n stuff})
+    end
+
+    it "prepends with a array of strings" do
+      r.edit do
+        prepend_lines %W{prepend1\n prepend2\n}
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n})
+    end
+
+    it "is idempotent with an array of strings" do
+      r.edit do
+        prepend_lines %W{prepend1\n prepend2\n}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n})
+    end
+
+    it "is idempotent with an array of strings, on a file that contains a line without a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        prepend_lines %W{prepend1\n prepend2\n}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n stuff})
+    end
+
+    it "prepends with a array of strings without newlines" do
+      r.edit do
+        prepend_lines %w{prepend1 prepend2}
+      end
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n})
+    end
+
+    it "is idempotent with an array of strings wihtout newlines" do
+      r.edit do
+        prepend_lines %w{prepend1 prepend2}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n})
+    end
+
+    it "is idempotent with an array of strings without newlines, on a file that contains a line without a newline" do
+      File.open(path, "a+") { |f| f.write "stuff" }
+      r.edit do
+        prepend_lines %w{prepend1 prepend2}
+      end
+      r.run_action(:create)
+      r.run_action(:create)
+      expect(IO.read(path).lines).to eql(%W{prepend1\n prepend2\n stuff})
+    end
+
   end
 end
