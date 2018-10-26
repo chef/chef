a71493d575cb24fce33faf26e2530cdc45bcafe2
Tim Smith <tsmith@chef.io>
2018-10-26 14:07:01 -0700

    Remove unused route resource properties
    These were added, but never actually implemented. In the world of auto generated docs this really breaks things.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/resource/route.rb
+++ lib/chef/resource/route.rb
@@ -27,20 +27,30 @@ class Chef
 
       description "Use the route resource to manage the system routing table in a Linux environment."
 
-      property :target, String, identity: true, name_property: true
-      property :comment, [String, nil], introduced: "14.0"
-      property :metric, [Integer, nil]
-      property :netmask, [String, nil]
-      property :gateway, [String, nil]
-      property :device, [String, nil], desired_state: false # Has a partial default in the provider of eth0.
-      property :route_type, [:host, :net], default: :host, coerce: proc { |x| x.to_sym }, desired_state: false
+      property :target, String,
+               description: "The IP address of the target route.",
+               identity: true, name_property: true
 
-      # I can find no evidence of these properties actually being used by Chef. NK 2017-04-11
-      property :networking, [String, nil], desired_state: false
-      property :networking_ipv6, [String, nil], desired_state: false
-      property :hostname, [String, nil], desired_state: false
-      property :domainname, [String, nil], desired_state: false
-      property :domain, [String, nil], desired_state: false
+      property :comment, [String, nil],
+               description: "Add a comment for the route.",
+               introduced: "14.0"
+
+      property :metric, [Integer, nil],
+               description: "The route metric value."
+
+      property :netmask, [String, nil],
+               description: "The decimal representation of the network mask. For example: 255.255.255.0."
+
+      property :gateway, [String, nil],
+               description: "The gateway for the route."
+
+      property :device, [String, nil],
+               description: "The network interface to which the route applies.",
+               desired_state: false # Has a partial default in the provider of eth0.
+
+      property :route_type, Symbol,
+               description: "",
+               equal_to: [:host, :net], default: :host, desired_state: false
     end
   end
 end
