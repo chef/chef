ea124b4434be0ffe14eef981db0b1d75433b25bc
Jeroen Jacobs <git@jeroenj.be>
2019-01-21 16:32:49 +0100

    Fix cask resource running each chef-client run
    See https://github.com/chef-cookbooks/homebrew/commit/f22690186facfab5b7cae0553ab6592629f6ed28
    
    Signed-off-by: Jeroen Jacobs <chef@jeroenj.be>

--- lib/chef/resource/homebrew_cask.rb
+++ lib/chef/resource/homebrew_cask.rb
@@ -89,7 +89,7 @@ class Chef
 
         def casked?
           unscoped_name = new_resource.cask_name.split("/").last
-          shell_out!('#{new_resource.homebrew_path} cask list 2>/dev/null',
+          shell_out!("#{new_resource.homebrew_path} cask list 2>/dev/null",
             user: new_resource.owner,
             env:  { "HOME" => ::Dir.home(new_resource.owner), "USER" => new_resource.owner },
             cwd: ::Dir.home(new_resource.owner)).stdout.split.include?(unscoped_name)
