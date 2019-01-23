362f5cc8af50a378d844905dc71d6549c4e10502
Tim Smith <tsmith@chef.io>
2019-01-22 19:15:56 -0800

    Use the latest omnibus-software and nokogiri
    The new omnibus-software includes a patch I added to fix compilation
    failures on non C99 compatible compilers with 2.6.0.
    
    The omnibus release that gets pulled in is also necessary for Ruby 2.6 on macOS
    
    The new nokogiri supports windows on Ruby 2.6, which the previous
    version did not.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@
 # software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
 override :rubygems, version: "2.7.7"
 override :bundler, version: "1.17.3"
-override "nokogiri", version: "1.8.5"
+override "nokogiri", version: "1.10.1"
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
 override "liblzma", version: "5.2.4"
