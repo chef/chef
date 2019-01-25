638418e63fa13589c90a5a35ad16b0847cb908e9
Tim Smith <tsmith@chef.io>
2019-01-25 12:28:31 -0500

    Update copyright year
    Co-Authored-By: jjustice6 <jjustice6@bloomberg.net>
    Signed-off-by: Joshua Justice <jjustice6@bloomberg.net>

--- lib/chef/provider/group/solaris.rb
+++ lib/chef/provider/group/solaris.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Joshua Justice (<jjustice6@bloomberg.net>)
-# Copyright:: Copyright 2008-2016, Chef Software Inc.
+# Copyright:: Copyright 2008-2019, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
