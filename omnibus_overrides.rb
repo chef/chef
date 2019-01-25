18a301826c6562f7f2ea92ca7cbe48d36996ba30
Tim Smith <tsmith@chef.io>
2019-01-25 10:34:08 -0800

    Update Rubygems to 3.0.2
    Bump Chef 15 to the latest and greatest rubygems.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -4,7 +4,7 @@
 #
 # NOTE: You MUST update omnibus-software when adding new versions of
 # software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
-override :rubygems, version: "2.7.7"
+override :rubygems, version: "3.0.2"
 override :bundler, version: "1.17.3"
 override "nokogiri", version: "1.10.1"
 override "libffi", version: "3.2.1"
