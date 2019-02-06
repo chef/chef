959f34d5fe3d591a90b7bf9ee6d86c439afcbeb9
Tim Smith <tsmith@chef.io>
2019-02-06 14:23:23 -0800

    Update nokogiri to 1.10.1
    This is the latest and greatest release with a ton of bug fixes over the previous version.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,7 +3,7 @@
 # try to keep it machine-parsable.
 override :rubygems, version: "2.6.14"
 override :bundler, version: "1.15.4"
-override "nokogiri", version: "1.8.5"
+override "nokogiri", version: "1.10.1"
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
 override "liblzma", version: "5.2.4"
