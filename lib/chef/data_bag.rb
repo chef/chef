fb46ed8de333f035d8f915a8a318f660ac68087a
Lamont Granquist <lamont@scriptkiddie.org>
2019-03-15 15:55:45 -0700

    chefstyle fix
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/data_bag.rb
+++ lib/chef/data_bag.rb
@@ -2,7 +2,7 @@
 # Author:: Adam Jacob (<adam@chef.io>)
 # Author:: Nuo Yan (<nuo@chef.io>)
 # Author:: Christopher Brown (<cb@chef.io>)
-# Copyright:: Copyright 2009-2018, Chef Software Inc.
+# Copyright:: Copyright 2009-2019, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,8 +32,8 @@ class Chef
     include Chef::Mixin::FromFile
     include Chef::Mixin::ParamsValidate
 
-    VALID_NAME = /^[\.\-[:alnum:]_]+$/
-    RESERVED_NAMES = /^(node|role|environment|client)$/
+    VALID_NAME = /^[\.\-[:alnum:]_]+$/.freeze
+    RESERVED_NAMES = /^(node|role|environment|client)$/.freeze
 
     def self.validate_name!(name)
       unless name =~ VALID_NAME
