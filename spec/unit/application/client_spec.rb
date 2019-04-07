fd0508d32a3cd683b827c5d25f2bd07f0311243b
Marc Chamberland <mchamberland@pbsc.com>
2019-04-06 20:16:04 -0400

    fixing tests
    Signed-off-by: Marc Chamberland <mchamberland@pbsc.com>

--- spec/unit/application/client_spec.rb
+++ spec/unit/application/client_spec.rb
@@ -326,10 +326,10 @@ describe Chef::Application::Client, "reconfigure" do
       Chef::Config[:interval] = 600
       allow(ChefConfig).to receive(:windows?).and_return(false)
       expect(Chef::Application).to receive(:fatal!).with(
-        "Unforked chef-client interval runs are disabled in Chef 12.
+        "Unforked #{Chef::Dist::CLIENT} interval runs are disabled in #{Chef::Dist::PRODUCT} 12.
 Configuration settings:
   interval  = 600 seconds
-Enable chef-client interval runs by setting `:client_fork = true` in your config file or adding `--fork` to your command line options."
+Enable #{Chef::Dist::CLIENT} interval runs by setting `:client_fork = true` in your config file or adding `--fork` to your command line options."
       )
       app.reconfigure
     end
