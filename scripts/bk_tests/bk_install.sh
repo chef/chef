c084b78e8756f906ff38000249a71d7c2e5638f5
Jaymala Sinha <jsinha@chef.io>
2019-04-12 14:58:09 -0400

    Add more verification tests
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -4,7 +4,6 @@ gem uninstall bundler -a -x || true
 gem install bundler -v $(grep :bundler omnibus_overrides.rb | cut -d'"' -f2)
 bundle --version
 rm -f .bundle/config
-bundle install --without ci docgen guard integration maintenance omnibus_package --frozen
 # force all .rspec tests into progress display to reduce line count
 echo --color > .rspec
 echo -fp >> .rspec
