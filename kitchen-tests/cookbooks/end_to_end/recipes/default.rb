ae43342056c65422df4f350dc9c389c5bd5d166d
Tim Smith <tsmith@chef.io>
2019-04-15 10:37:37 -0700

    Add archive testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- kitchen-tests/cookbooks/end_to_end/recipes/default.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/default.rb
@@ -95,4 +95,16 @@ openssl_rsa_public_key "/etc/ssl/rsakey_aes128cbc.pub" do
   action :create
 end
 
+# test various archive formats in the archive_file resource
+%w(tourism.tar.gz tourism.tar.xz tourism.zip).each do |archive|
+   cookbook_file File.join(Chef::Config[:file_cache_path], archive) do
+     source archive
+   end
+
+   archive_file archive do
+     path File.join(Chef::Config[:file_cache_path], archive)
+     extract_to File.join(Chef::Config[:file_cache_path], archive.gsub('.', '_'))
+   end
+end
+
 include_recipe "::tests"
