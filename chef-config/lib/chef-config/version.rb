4ff9886fe012febe8432dccb485221dbed154ccf
Chef Expeditor <chef-ci@chef.io>
2019-04-19 19:00:20 +0000

    Bump version to 14.12.6 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -21,7 +21,7 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "14.12.5".freeze
+  VERSION = "14.12.6".freeze
 end
 
 #
--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -23,7 +23,7 @@ require "chef/version_string"
 
 class Chef
   CHEF_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = Chef::VersionString.new("14.12.5")
+  VERSION = Chef::VersionString.new("14.12.6")
 end
 
 #
