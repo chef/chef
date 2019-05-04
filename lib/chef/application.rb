jazaval/chef-1

Read license-acceptance from chef config (knife.rb, client.rb)
Signed-off-by: tyler-ball <tball@chef.io>

lib/chef/application.rb
@@ -250,7 +250,12 @@ class Chef
     end
 
     def check_license_acceptance
-      LicenseAcceptance::Acceptor.check_and_persist!("chef-client", Chef::VERSION.to_s, logger: logger)
+      LicenseAcceptance::Acceptor.check_and_persist!(
+        "chef-client",
+        Chef::VERSION.to_s,
+        logger: logger,
+        provided: Chef::Config[:chef_license]
+      )
     end
 
     # Actually run the application

chef/chef

lib/chef/application.rb
@@ -251,7 +251,12 @@ class Chef
     end
 
     def check_license_acceptance
-      LicenseAcceptance::Acceptor.check_and_persist!("chef-client", Chef::VERSION.to_s, logger: logger)
+      LicenseAcceptance::Acceptor.check_and_persist!(
+        "infra-client",
+        Chef::VERSION.to_s,
+        logger: logger,
+        provided: Chef::Config[:chef_license]
+      )
     end
 
     # Actually run the application