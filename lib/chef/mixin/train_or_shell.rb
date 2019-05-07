a8198cc042869b34937873db8f371cff77748f33
Lamont Granquist <lamont@scriptkiddie.org>
2019-05-07 11:46:24 -0700

    Target mode code tweaks
    The train_or_shell mixin should mixin its dependent mixins and
    require them, which removes the need to do that in the calling classes.
    
    Include TrainOrShell correctly in the Universal DSL.
    
    Few more code tweaks ("protected" is largely worthless in ruby and was
    always being misused here).
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/mixin/train_or_shell.rb
+++ lib/chef/mixin/train_or_shell.rb
@@ -17,20 +17,16 @@
 #
 
 require "ostruct"
+require "chef/mixin/shell_out"
+require "chef/mixin/powershell_out"
+require "chef/config"
 
 class Chef
   module Mixin
     module TrainOrShell
-      #
-      # Train #run_command returns a Train::Extras::CommandResult which
-      # includes `exit_status` but Mixlib::Shellout returns exitstatus
-      # This wrapper makes the result look like Mixlib::ShellOut to make it
-      # easier to swap providers from #shell_out to #train_or_shell
-      #
-      def train_to_shellout_result(stdout, stderr, exit_status)
-        status = OpenStruct.new(success?: ( exit_status == 0 ))
-        OpenStruct.new(stdout: stdout, stderr: stderr, exitstatus: exit_status, status: status)
-      end
+
+      include Chef::Mixin::ShellOut
+      include Chef::Mixin::PowershellOut
 
       def train_or_shell(*args, **opts)
         if Chef::Config.target_mode?
@@ -68,6 +64,19 @@ class Chef
         else
           powershell_out!(*args)
         end
+      end
+
+      private
+
+      #
+      # Train #run_command returns a Train::Extras::CommandResult which
+      # includes `exit_status` but Mixlib::Shellout returns exitstatus
+      # This wrapper makes the result look like Mixlib::ShellOut to make it
+      # easier to swap providers from #shell_out to #train_or_shell
+      #
+      def train_to_shellout_result(stdout, stderr, exit_status)
+        status = OpenStruct.new(success?: ( exit_status == 0 ))
+        OpenStruct.new(stdout: stdout, stderr: stderr, exitstatus: exit_status, status: status)
       end
     end
   end
