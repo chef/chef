7d992bc339acb716a27c631cd18bde79086b3556
dheerajd-msys <dheeraj.dubey@msystechnologies.com>
2019-05-24 05:29:43 +0530

    Fix service enable idempotency in sles11 (#8256)
    * fix service enable idempotency in sles11
    
    Signed-off-by: dheerajd-msys <dheeraj.dubey@msystechnologies.com>

--- lib/chef/provider/service/insserv.rb
+++ lib/chef/provider/service/insserv.rb
@@ -36,7 +36,9 @@ class Chef
           super
 
           # Look for a /etc/rc.*/SnnSERVICE link to signify that the service would be started in a runlevel
-          if Dir.glob("/etc/rc**/S*#{Chef::Util::PathHelper.escape_glob_dir(current_resource.service_name)}").empty?
+          service_name = Chef::Util::PathHelper.escape_glob_dir(current_resource.service_name)
+
+          if Dir.glob("/etc/rc*/**/S*#{service_name}").empty?
             current_resource.enabled false
           else
             current_resource.enabled true
