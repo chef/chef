16a638197c978721b1a98a51acdad06165ff3ffe
Lamont Granquist <lamont@scriptkiddie.org>
2019-07-15 22:21:38 -0700

    Tidy up awkward reading comment
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/application/base.rb
+++ lib/chef/application/base.rb
@@ -29,8 +29,7 @@ require "mixlib/archive" unless defined?(Mixlib::Archive)
 # This is a temporary class being used as a part of an effort to reduce duplication
 # between Chef::Application::Client and Chef::Application::Solo.
 #
-# If you are looking to make edits to the Client/Solo behavior please make changes here,
-# this Base class is the correct behavior.
+# If you are looking to make edits to the Client/Solo behavior please make changes here.
 #
 # If you are looking to reference or subclass this class, use Chef::Application::Client
 # instead.  This base class will be removed once the work is complete and external code
