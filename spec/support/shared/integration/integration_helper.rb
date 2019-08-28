c106a633ef28f5d53cac6bfa64adaddd3aa2f9ca
Lamont Granquist <lamont@scriptkiddie.org>
2019-08-28 16:40:52 -0700

    remove app_server_support spec file
    Convert to using TinyServer and remove the duplicated functionality
    with the "app_server_support" helper.
    
    The fact that this change hits the knife raw / knife redirection specs
    that have so many timeout issues is very likely not a coincidence.
    
    I would bet a small, but not entirely insignificant amount of money that
    this makes those timeout issues go away.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- spec/support/shared/integration/integration_helper.rb
+++ spec/support/shared/integration/integration_helper.rb
@@ -1,7 +1,7 @@
 #
 # Author:: John Keiser (<jkeiser@chef.io>)
 # Author:: Ho-Sheng Hsiao (<hosh@chef.io>)
-# Copyright:: Copyright 2012-2016, Chef Software Inc.
+# Copyright:: Copyright 2012-2019, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,7 +23,6 @@ require "chef/config"
 require "chef/json_compat"
 require "chef/server_api"
 require "support/shared/integration/knife_support"
-require "support/shared/integration/app_server_support"
 require "cheffish/rspec/chef_run_support"
 require "spec_helper"
 
