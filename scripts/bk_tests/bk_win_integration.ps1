591cdb62ab7e7a2088c8340132b9da8dd60b6664
Jaymala Sinha <jsinha@chef.io>
2019-09-03 14:06:49 -0400

    Run windows verification in powershell
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

---
+++ scripts/bk_tests/bk_win_integration.ps1
@@ -0,0 +1,13 @@
+echo "--- system details"
+$Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
+Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
+ruby -v
+bundle --version
+
+echo "--- bundle install"
+bundle install --jobs=3 --retry=3 --without omnibus_package docgen chefstyle
+
+echo "+++ bundle exec rake"
+bundle exec rake spec:integration
+
+exit $LASTEXITCODE
