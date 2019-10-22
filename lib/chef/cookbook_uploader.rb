9c245b8e9ee919fe1f996ca118816c27744788b7
Vasu1105 <vasundhara.jagdale@msystechnologies.com>
2019-10-22 14:40:47 +0530

    Fixed chef style error
    Signed-off-by: Vasu1105 <vasundhara.jagdale@msystechnologies.com>

--- lib/chef/cookbook_uploader.rb
+++ lib/chef/cookbook_uploader.rb
@@ -140,6 +140,7 @@ class Chef
     def validate_cookbooks
       cookbooks.each do |cb|
         next if cb.nil?
+
         syntax_checker = Chef::Cookbook::SyntaxCheck.for_cookbook(cb.root_dir)
         Chef::Log.info("Validating ruby files")
         exit(1) unless syntax_checker.validate_ruby_files
