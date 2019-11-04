22fe1098164e5e2aacc1477cf9cb4249e467700f
Kimber, Jeremy <Jeremy.Kimber@cerner.com>
2019-11-04 10:03:36 -0600

    readability improvements for empty hash return
    Signed-off-by: Kimber, Jeremy <Jeremy.Kimber@cerner.com>

--- lib/chef/resource_reporter.rb
+++ lib/chef/resource_reporter.rb
@@ -183,7 +183,11 @@ class Chef
 
     # get only the top level resources and strip out the subcollections
     def updated_resources
-      @updated_resources ||= action_collection&.filtered_collection(max_nesting: 0, up_to_date: false, skipped: false, unprocessed: false) || {}
+      @updated_resources ||= if action_collection.respond_to?(:filtered_collection)
+            action_collection.filtered_collection(max_nesting: 0, up_to_date: false, skipped: false, unprocessed: false)
+          else
+            {}
+          end
     end
 
     def total_res_count
