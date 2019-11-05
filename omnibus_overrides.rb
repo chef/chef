5a6ea98a211738f0f9b3da69bca411562f23f842
Tim Smith <tsmith84@gmail.com>
2019-11-05 12:12:30 -0800

    Update nokogiri, libxml2, and libxslt to latest
    Update our deps to the latest
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,23 +5,23 @@
 # software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
 override :rubygems, version: "2.7.9"
 override :bundler, version: "1.16.6"
-override "nokogiri", version: "1.10.4"
+override "nokogiri", version: "1.10.5"
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
 override "liblzma", version: "5.2.4"
 override "libtool", version: "2.4.2"
-override "libxml2", version: "2.9.9"
-override "libxslt", version: "1.1.30"
+override "libxml2", version: "2.9.10"
+override "libxslt", version: "1.1.34"
 override "libyaml", version: "0.1.7"
 override "makedepend", version: "1.0.5"
 override "ncurses", version: "5.9"
+override "openssl", version: "1.0.2t"
 override "pkg-config-lite", version: "0.28-1"
 override "ruby", version: "2.5.7"
 override "ruby-windows-devkit-bash", version: "3.1.23-4-msys-1.0.18"
 override "util-macros", version: "1.19.0"
 override "xproto", version: "7.0.28"
 override "zlib", version: "1.2.11"
-override "openssl", version: "1.0.2t"
 
 # we build both a chef and ohai omnibus-software defintion which create the
 # chef-client and ohai binstubs. Out of the box the ohai definition uses whatever
