a239557f72823e92d42d9e5fb54cafd2ae5617f5
Robb Kidd <robb@thekidds.org>
2019-11-05 14:28:10 -0500

    move minimum hab check/install to a reusable script
    And use it in both the verify/habitat and habitat/test pipelines.
    
    verify/habitat needs hab >= 0.85 to both build with environment
    variables that don't include the studio path[1] and to binlink the runtime
    with stubs that include the package and system environment.[2]
    
    habitat/test needs hab >= 0.85 to binlink the runtime with stubs that
    include the package and system environment.
    
    [1] https://github.com/habitat-sh/habitat/pull/6850
    [2] https://github.com/habitat-sh/habitat/pull/6826
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- scripts/ci/verify-plan.ps1
+++ scripts/ci/verify-plan.ps1
@@ -12,19 +12,11 @@ $Plan = 'chef-infra-client'
 
 Write-Host "--- :8ball: :windows: Verifying $Plan"
 
-$hab_version = (hab --version)
-$hab_minor_version = $hab_version.split('.')[1]
-if ( -not $? -Or $hab_minor_version -lt 85 ) {
-    Write-Host "--- :habicat: Installing the version of Habitat required"
-    install-habitat --version 0.85.0.20190916
-} else {
-    Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
-}
-
+powershell -File "./scripts/ci/ensure-minimum-viable-hab.ps1"
+if (-not $?) { throw "Could not ensure the minimum hab version required is installed." }
 
 Write-Host "--- :key: Generating fake origin key"
 hab origin key generate $env:HAB_ORIGIN
-
 
 $project_root = "$(git rev-parse --show-toplevel)"
 Set-Location $project_root
