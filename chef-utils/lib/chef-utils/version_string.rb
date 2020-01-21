3a7dc2359fd833e5f920818f67f6d0474bfb9042
Lamont Granquist <lamont@scriptkiddie.org>
2020-01-21 13:53:07 -0800

    fix namespacing bugs
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- chef-utils/lib/chef-utils/version_string.rb
+++ chef-utils/lib/chef-utils/version_string.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class ChefUtils
+module ChefUtils
   # String-like object for version strings.
   #
   # @since 13.2
