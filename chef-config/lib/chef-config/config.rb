2be4e0e3c52151c90767571adb1f834fb746f821
Marc Chamberland <chamberland.marc@gmail.com>
2020-01-29 21:25:48 -0500

    Pass distro and platform specific paths into bootstrap templates
    Signed-off-by: Marc Chamberland <chamberland.marc@gmail.com>

--- chef-config/lib/chef-config/config.rb
+++ chef-config/lib/chef-config/config.rb
@@ -75,20 +75,21 @@ module ChefConfig
     end
 
     # On *nix, /etc/chef
-    def self.etc_chef_dir
-      path = ChefUtils.windows? ? c_chef_dir : PathHelper.join("/etc", ChefConfig::Dist::DIR_SUFFIX)
+
+    def self.etc_chef_dir(is_windows = ChefUtils.windows?)
+      path = is_windows ? c_chef_dir : PathHelper.join("/etc", ChefConfig::Dist::DIR_SUFFIX)
       PathHelper.cleanpath(path)
     end
 
     # On *nix, /var/chef
-    def self.var_chef_dir
-      path = ChefUtils.windows? ? c_chef_dir : PathHelper.join("/var", ChefConfig::Dist::DIR_SUFFIX)
+    def self.var_chef_dir(is_windows = ChefUtils.windows?)
+      path = is_windows ? c_chef_dir : PathHelper.join("/var", ChefConfig::Dist::DIR_SUFFIX)
       PathHelper.cleanpath(path)
     end
 
     # On *nix, the root of /var/, used to test if we can create and write in /var/chef
-    def self.var_root_dir
-      path = ChefUtils.windows? ? c_chef_dir : "/var"
+    def self.var_root_dir(is_windows = ChefUtils.windows?)
+      path = is_windows ? c_chef_dir : "/var"
       PathHelper.cleanpath(path)
     end
 
