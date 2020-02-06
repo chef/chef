f8c467b2a5df25230f979848589401c5704e1a73
Tim Smith <tsmith84@gmail.com>
2020-02-05 17:18:36 -0800

    Properly name the modes in the help text
    It's Chef Infra Client mode vs. Chef Infra Solo vs. Standalone
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/shell.rb
+++ lib/chef/shell.rb
@@ -241,7 +241,7 @@ module Shell
     option :standalone,
       short: "-a",
       long: "--standalone",
-      description: "standalone session",
+      description: "Standalone session",
       default: true,
       boolean: true
 
@@ -255,7 +255,7 @@ module Shell
     option :client,
       short: "-z",
       long: "--client",
-      description: "#{Chef::Dist::CLIENT} session",
+      description: "#{Chef::Dist::PRODUCT} session",
       boolean: true
 
     option :solo_legacy_shell,
