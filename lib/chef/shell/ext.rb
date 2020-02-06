3cc840234b32ac5e4887179bbcfa8305877eebbf
Tim Smith <tsmith84@gmail.com>
2020-02-05 17:19:29 -0800

    Rework the welcome text to be more helpful
    Get rid of the old Ohai meme reference
    Link to the specific doc on how to use the shell
    Give the version of chef-shell and don't mention Chef Infra Client
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/shell/ext.rb
+++ lib/chef/shell/ext.rb
@@ -210,10 +210,8 @@ module Shell
 
       desc "prints information about #{Chef::Dist::PRODUCT}"
       def version
-        puts "This is the #{Chef::Dist::SHELL}.\n" +
-          " #{Chef::Dist::PRODUCT} Version: #{::Chef::VERSION}\n" +
-          " #{Chef::Dist::WEBSITE}\n" +
-          " https://docs.chef.io/"
+        puts "Welcome to the #{Chef::Dist::SHELL} #{::Chef::VERSION}\n" +
+          "For usage see https://docs.chef.io/chef_shell.html"
         :ucanhaz_automation
       end
       alias :shell :version
