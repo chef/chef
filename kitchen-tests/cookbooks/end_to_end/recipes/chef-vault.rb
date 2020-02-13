af2ad70594a3ca9898e3689f30111beefe638067
Tim Smith <tsmith84@gmail.com>
2020-02-12 20:15:28 -0800

    Chefstyle fix of the test cookbook
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- kitchen-tests/cookbooks/end_to_end/recipes/chef-vault.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/chef-vault.rb
@@ -5,28 +5,28 @@
 # Copyright:: 2020, Chef Software, Inc.
 #
 
-chef_data_bag 'creds'
+chef_data_bag "creds"
 
-openssl_rsa_private_key '/root/bob_bobberson.pem' do
+openssl_rsa_private_key "/root/bob_bobberson.pem" do
   key_length 2048
   action :create
 end
 
-chef_client 'bob_bobberson' do
-  source_key_path '/root/bob_bobberson.pem'
+chef_client "bob_bobberson" do
+  source_key_path "/root/bob_bobberson.pem"
 end
 
-chef_node 'bob_bobberson'
+chef_node "bob_bobberson"
 
-chef_vault_secret 'super_secret_1' do
-  data_bag 'creds'
-  raw_data('auth' => '1234')
-  admins 'bob_bobberson'
-  search '*:*'
+chef_vault_secret "super_secret_1" do
+  data_bag "creds"
+  raw_data("auth" => "1234")
+  admins "bob_bobberson"
+  search "*:*"
 end
 
-chef_vault_secret 'super_secret_2' do
-  data_bag 'creds'
-  raw_data('auth' => '4321')
-  admins 'bob_bobberson'
+chef_vault_secret "super_secret_2" do
+  data_bag "creds"
+  raw_data("auth" => "4321")
+  admins "bob_bobberson"
 end
