c7a824e43080813db8da0e63ca090f587c4ea7f0
sanga17 <sausekar@msystechnologies.com>
2020-03-16 11:47:24 +0530

    Powershell script replaced with the ohai data
    Signed-off-by: sanga17 <sausekar@msystechnologies.com>

--- lib/chef/provider/package/powershell.rb
+++ lib/chef/provider/package/powershell.rb
@@ -36,7 +36,7 @@ class Chef
 
         def define_resource_requirements
           super
-          if powershell_out("$PSVersionTable.PSVersion.Major").stdout.strip.to_i < 5
+          if ChefUtils.powershell_version(node).strip.to_i < 5
             raise "Minimum installed PowerShell Version required is 5"
           end
 
