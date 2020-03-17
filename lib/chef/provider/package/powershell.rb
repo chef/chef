28c0bd4bf191eb922afa9360f1336b1d210f85b6
Sangmesh Ausekar <58020082+sanga1794@users.noreply.github.com>
2020-03-17 11:05:15 +0530

    Update lib/chef/provider/package/powershell.rb
    Co-Authored-By: Tim Smith <tsmith@chef.io>

--- lib/chef/provider/package/powershell.rb
+++ lib/chef/provider/package/powershell.rb
@@ -36,7 +36,7 @@ class Chef
 
         def define_resource_requirements
           super
-          if node["languages"] && node["languages"]["powershell"] && node["languages"]["powershell"]["version"].strip.to_i < 5
+          if powershell_version < 5
             raise "Minimum installed PowerShell Version required is 5"
           end
 
