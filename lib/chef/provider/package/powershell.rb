57a367f5aef2ce2ce0627fe1a3f3eca5e1921145
sanga17 <sausekar@msystechnologies.com>
2020-03-17 12:37:41 +0530

    Unwanted code removed
    Signed-off-by: sanga17 <sausekar@msystechnologies.com>

--- lib/chef/provider/package/powershell.rb
+++ lib/chef/provider/package/powershell.rb
@@ -36,7 +36,7 @@ class Chef
 
         def define_resource_requirements
           super
-          if powershell_version.to_i < 5
+          if powershell_version < 5
             raise "Minimum installed PowerShell Version required is 5"
           end
 
