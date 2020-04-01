333d075a648493a32ee11e928a7abcea0520502f
Lamont Granquist <lamont@scriptkiddie.org>
2020-04-01 10:50:43 -0700

    testing DO NOT MERGE
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- kitchen-tests/cookbooks/end_to_end/recipes/chef-vault.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/chef-vault.rb
@@ -2,7 +2,7 @@
 # Cookbook:: end_to_end
 # Recipe:: chef-vault
 #
-# Copyright:: 2020, Chef Software, Inc.
+# Copyright:: 2020-2020, Chef Software Inc.
 #
 
 chef_data_bag "creds"
@@ -17,6 +17,19 @@ chef_client "bob_bobberson" do
 end
 
 chef_node "bob_bobberson"
+
+chef_vault_secret "super_secret_1" do
+  data_bag "creds"
+  raw_data("auth" => "1234")
+  admins "bob_bobberson"
+  search "*:*"
+end
+
+chef_vault_secret "super_secret_2" do
+  data_bag "creds"
+  raw_data("auth" => "4321")
+  admins "bob_bobberson"
+end
 
 chef_vault_secret "super_secret_1" do
   data_bag "creds"
