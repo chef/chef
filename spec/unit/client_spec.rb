f5625991fcf1c722d430455ce602f2a1640c8c0c
Tim Smith <tsmith84@gmail.com>
2020-04-15 19:47:22 -0700

    Simplify the logic since we know when 15 goes EOL
    There's no need to calculate it here since this code will only ever run
    on 15
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -308,15 +308,13 @@ describe Chef::Client do
   end
 
   describe "eol release warning" do
-    it "warns when running an EOL release" do
-      stub_const("Chef::VERSION", 15)
+    it "warns when the date is after April 30, 2021" do
       allow(Time).to receive(:now).and_return(Time.new(2021, 5, 1, 5))
       expect(logger).to receive(:warn).with(/This release of.*became end of life \(EOL\) on May 1st 2021/)
       client.warn_if_eol
     end
 
-    it "does not warn when running an non-EOL release" do
-      stub_const("Chef::VERSION", 15)
+    it "does not warn when date is before May 1st 2021" do
       allow(Time).to receive(:now).and_return(Time.new(2021, 4, 31))
       expect(logger).to_not receive(:warn).with(/became end of life/)
       client.warn_if_eol
