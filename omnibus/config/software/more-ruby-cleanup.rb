fdfc4c3968ec8ebe5b20e0afcb38af202ce6992f
Tim Smith <tsmith84@gmail.com>
2020-05-05 21:25:25 -0700

    Delete unnecessary gem bin/console and bin/setup files
    There's no need for these in gems we ship. There's a bunch of them.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/config/software/more-ruby-cleanup.rb
+++ omnibus/config/software/more-ruby-cleanup.rb
@@ -25,6 +25,15 @@ license :project_license
 source path: "#{project.files_path}/#{name}"
 
 build do
+  block "Removing console and setup binaries" do
+    Dir.glob("#{install_dir}/embedded/lib/ruby/gems/*/gems/*/bin/{console,setup}").each do |f|
+      puts "Deleting #{f}"
+      FileUtils.rm_rf(f)
+    end
+  end
+end
+
+build do
   block "Removing additional non-code files from installed gems" do
     # find the embedded ruby gems dir and clean it up for globbing
     target_dir = "#{install_dir}/embedded/lib/ruby/gems/*/gems".tr('\\', "/")
