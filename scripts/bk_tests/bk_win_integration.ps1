b4eeea2f42e13ddb582e91d84145b5d32259983a
Tim Smith <tsmith84@gmail.com>
2020-05-10 20:48:36 -0700

    Stop updating rubygems in our test scripts
    Rubygems is built into ruby and we're not updating it anymore in our
    builds. We get the version that ships in Ruby
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- scripts/bk_tests/bk_win_integration.ps1
+++ scripts/bk_tests/bk_win_integration.ps1
@@ -7,26 +7,18 @@ $Env:Path="C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin;C:\ruby
 
 winrm quickconfig -q
 
-echo "--- update bundler and rubygems"
+echo "--- update bundler"
 
 ruby -v
 if (-not $?) { throw "Can't run Ruby. Is it installed?" }
 
-$env:RUBYGEMS_VERSION=$(findstr rubygems omnibus_overrides.rb | %{ $_.split(" ")[3] })
 $env:BUNDLER_VERSION=$(findstr bundler omnibus_overrides.rb | %{ $_.split(" ")[3] })
-
-$env:RUBYGEMS_VERSION=($env:RUBYGEMS_VERSION -replace '"', "")
 $env:BUNDLER_VERSION=($env:BUNDLER_VERSION -replace '"', "")
-
-echo $env:RUBYGEMS_VERSION
 echo $env:BUNDLER_VERSION
 
-gem update --system $env:RUBYGEMS_VERSION
-if (-not $?) { throw "Unable to update system Rubygems" }
-gem --version
 gem install bundler -v $env:BUNDLER_VERSION --force --no-document --quiet
 if (-not $?) { throw "Unable to update Bundler" }
-bundle --versio
+bundle --version
 
 echo "--- bundle install"
 bundle install --jobs=3 --retry=3 --without omnibus_package docgen chefstyle
