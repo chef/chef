d073e776b741140642525c78f91a38b30c99ecee
snehaldwivedi <sdwivedi@msystechnologies.com>
2020-06-05 19:43:40 +0530

    Updated exception message
    Signed-off-by: snehaldwivedi <sdwivedi@msystechnologies.com>

--- spec/unit/data_bag_spec.rb
+++ spec/unit/data_bag_spec.rb
@@ -243,7 +243,7 @@ describe Chef::DataBag do
 
         expect do
           Chef::DataBag.load("foo")
-        end.to raise_error Chef::Exceptions::InvalidDataBagPath, "Please create databag folder at '/var/chef/data_bags'"
+        end.to raise_error Chef::Exceptions::InvalidDataBagPath, "Data bag path '/var/chef/data_bags' not found. Please create this directory."
       end
 
     end
