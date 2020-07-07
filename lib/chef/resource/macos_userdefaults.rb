30eaf758922c395082a4db64ac61bd5485e74c75
Tim Smith <tsmith84@gmail.com>
2020-07-06 20:31:29 -0700

    Simplify logic and fix a few failures
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/resource/macos_userdefaults.rb
+++ lib/chef/resource/macos_userdefaults.rb
@@ -139,11 +139,11 @@ class Chef
 
       action_class do
         def defaults_write_cmd
-          cmd = ["defaults", "write", new_resource.domain, new_resource.key]
+          cmd = ["defaults", "write", "'#{new_resource.domain}'", "'#{new_resource.key}'"]
           cmd.prepend("sudo") if new_resource.sudo
 
           value = new_resource.value
-          type = !new_resource.type.empty? ? new_resource.type : value_type(value)
+          type = new_resource.type || value_type(value)
           # creates a string of Key1 Value1 Key2 Value2...
           value = value.map { |k, v| "\"#{k}\" \"#{v}\"" }.join(" ") if type == "dict"
           if type == "array"
