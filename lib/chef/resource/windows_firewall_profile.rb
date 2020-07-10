b5264438c130eb03b2a9a50cf3adff2684c7ac0a
Davin Taddeo <davin@chef.io>
2020-07-10 15:49:24 -0400

    removing powershell_exec for 32 bit compatibility. Using ruby true/false for properties, cleaning up so straggling nil's. updating the examples
    Signed-off-by: Davin Taddeo <davin@chef.io>

--- lib/chef/resource/windows_firewall_profile.rb
+++ lib/chef/resource/windows_firewall_profile.rb
@@ -27,8 +27,7 @@ class Chef
       **Enable and Configure the Private Profile of the Windows Profile**:
 
       ```ruby
-      windows_firewall_profile 'Configure and Enable Windows Firewall Private Profile' do
-        profiles 'Private'
+      windows_firewall_profile 'Private' do
         default_inbound_action 'Block'
         default_outbound_action 'Allow'
         allow_inbound_rules 'True'
@@ -40,8 +39,7 @@ class Chef
       **Enable and Configure the Public Profile of the Windows Firewall**:
 
       ```ruby
-      windows_firewall_profile 'Configure and Enable Windows Firewall Public Profile' do
-        profile 'Public'
+      windows_firewall_profile 'Public' do
         default_inbound_action 'Block'
         default_outbound_action 'Allow'
         allow_inbound_rules 'False'
@@ -75,13 +73,13 @@ class Chef
         equal_to: %w{ Allow Block NotConfigured },
         description: "Set the default policy for outbound network traffic"
 
-      property :allow_inbound_rules, String, equal_to: %w{ True False NotConfigured }, description: "Allow users to set inbound firewall rules"
-      property :allow_local_firewall_rules, String, equal_to: %w{ True False NotConfigured }, description: "Merges inbound firewall rules into the policy"
-      property :allow_local_ipsec_rules, String, equal_to: %w{ True False NotConfigured }, description: "Allow users to manage local connection security rules"
-      property :allow_user_apps, String, equal_to: %w{ True False NotConfigured }, description: "Allow user applications to manage firewall"
-      property :allow_user_ports, String, equal_to: %w{ True False NotConfigured }, description: "Allow users to manage firewall port rules"
-      property :allow_unicast_response, String, equal_to: %w{ True False NotConfigured }, description: "Allow unicast responses to multicast and broadcast messages"
-      property :display_notification, String, equal_to: %w{ True False NotConfigured }, description: "Display a notification when firewall blocks certain activity"
+      property :allow_inbound_rules, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Allow users to set inbound firewall rules"
+      property :allow_local_firewall_rules, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Merges inbound firewall rules into the policy"
+      property :allow_local_ipsec_rules, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Allow users to manage local connection security rules"
+      property :allow_user_apps, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Allow user applications to manage firewall"
+      property :allow_user_ports, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Allow users to manage firewall port rules"
+      property :allow_unicast_response, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Allow unicast responses to multicast and broadcast messages"
+      property :display_notification, [true, false, String], equal_to: [true, false, "NotConfigured"], description: "Display a notification when firewall blocks certain activity"
 
       load_current_value do |desired|
         ps_get_net_fw_profile = load_firewall_state(desired.profile)
@@ -94,13 +92,33 @@ class Chef
 
         default_inbound_action state["default_inbound_action"]
         default_outbound_action state["default_outbound_action"]
-        allow_inbound_rules state["allow_inbound_rules"]
-        allow_local_firewall_rules state["allow_local_firewall_rules"]
-        allow_local_ipsec_rules state["allow_local_ipsec_rules"]
-        allow_user_apps state["allow_user_apps"]
-        allow_user_ports state["allow_user_ports"]
-        allow_unicast_response state["allow_unicast_response"]
-        display_notification state["display_notification"]
+        allow_inbound_rules convert_to_ruby(state["allow_inbound_rules"])
+        allow_local_firewall_rules convert_to_ruby(state["allow_local_firewall_rules"])
+        allow_local_ipsec_rules convert_to_ruby(state["allow_local_ipsec_rules"])
+        allow_user_apps convert_to_ruby(state["allow_user_apps"])
+        allow_user_ports convert_to_ruby(state["allow_user_ports"])
+        allow_unicast_response convert_to_ruby(state["allow_unicast_response"])
+        display_notification convert_to_ruby(state["display_notification"])
+      end
+
+      def convert_to_ruby(obj)
+        if obj.to_s.downcase == "true"
+          true
+        elsif obj.to_s.downcase == "false"
+          false
+        elsif obj.to_s.downcase == "notconfigured"
+          "NotConfigured"
+        end
+      end
+
+      def convert_to_powershell(obj)
+        if obj.to_s.downcase == "true"
+          "True"
+        elsif obj.to_s.downcase == "false"
+          "False"
+        elsif obj.to_s.downcase == "notconfigured"
+          "NotConfigured"
+        end
       end
 
       action :enable do
@@ -112,7 +130,7 @@ class Chef
         unless firewall_enabled?(new_resource.profile)
           converge_by "Enable the #{new_resource.profile} Firewall Profile" do
             cmd = "Set-NetFirewallProfile -Profile #{new_resource.profile} -Enabled \"True\""
-            powershell_exec!(cmd)
+            powershell_out!(cmd)
           end
         end
       end
@@ -121,7 +139,7 @@ class Chef
         if firewall_enabled?(new_resource.profile)
           converge_by "Disable the #{new_resource.profile} Firewall Profile" do
             cmd = "Set-NetFirewallProfile -Profile #{new_resource.profile} -Enabled \"False\""
-            powershell_exec(cmd)
+            powershell_out!(cmd)
           end
         end
       end
@@ -131,13 +149,13 @@ class Chef
           cmd = "Set-NetFirewallProfile -Profile \"#{fw_profile}\""
           cmd << " -DefaultInboundAction \"#{new_resource.default_inbound_action}\"" unless new_resource.default_inbound_action.nil?
           cmd << " -DefaultOutboundAction \"#{new_resource.default_outbound_action}\"" unless new_resource.default_outbound_action.nil?
-          cmd << " -AllowInboundRules \"#{new_resource.allow_inbound_rules}\"" unless new_resource.allow_inbound_rules.nil?
-          cmd << " -AllowLocalFirewallRules \"#{new_resource.allow_local_firewall_rules}\""
-          cmd << " -AllowLocalIPsecRules \"#{new_resource.allow_local_ipsec_rules}\"" unless new_resource.allow_local_ipsec_rules.nil?
-          cmd << " -AllowUserApps \"#{new_resource.allow_user_apps}\"" unless new_resource.allow_user_apps.nil?
-          cmd << " -AllowUserPorts \"#{new_resource.allow_user_ports}\"" unless new_resource.allow_user_ports.nil?
-          cmd << " -AllowUnicastResponseToMulticast \"#{new_resource.allow_unicast_response}\"" unless new_resource.allow_unicast_response.nil?
-          cmd << " -NotifyOnListen \"#{new_resource.display_notification}\"" unless new_resource.display_notification.nil?
+          cmd << " -AllowInboundRules \"#{convert_to_powershell(new_resource.allow_inbound_rules)}\"" unless new_resource.allow_inbound_rules.nil?
+          cmd << " -AllowLocalFirewallRules \"#{convert_to_powershell(new_resource.allow_local_firewall_rules)}\"" unless new_resource.allow_local_firewall_rules.nil?
+          cmd << " -AllowLocalIPsecRules \"#{convert_to_powershell(new_resource.allow_local_ipsec_rules)}\"" unless new_resource.allow_local_ipsec_rules.nil?
+          cmd << " -AllowUserApps \"#{convert_to_powershell(new_resource.allow_user_apps)}\"" unless new_resource.allow_user_apps.nil?
+          cmd << " -AllowUserPorts \"#{convert_to_powershell(new_resource.allow_user_ports)}\"" unless new_resource.allow_user_ports.nil?
+          cmd << " -AllowUnicastResponseToMulticast \"#{convert_to_powershell(new_resource.allow_unicast_response)}\"" unless new_resource.allow_unicast_response.nil?
+          cmd << " -NotifyOnListen \"#{convert_to_powershell(new_resource.display_notification)}\"" unless new_resource.display_notification.nil?
           cmd
         end
 
@@ -160,12 +178,18 @@ class Chef
         end
 
         def firewall_enabled?(profile_name)
-          powershell_exec(<<-CODE).result
+          cmd = <<~CODE
             $#{profile_name} = Get-NetFirewallProfile -Profile #{profile_name}
             if ($#{profile_name}.Enabled) {
                 return $true
             } else {return $false}
           CODE
+          firewall_status = powershell_out(cmd).stdout
+          if firewall_status =~ /True/
+            true
+          elsif firewall_status =~ /False/
+            false
+          end
         end
       end
     end
