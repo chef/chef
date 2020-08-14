a827f24cb7bb0944a30f7787e1784b4d7341ca12
Tim Smith <tsmith84@gmail.com>
2020-08-14 13:38:53 -0700

    Convert the functional tests over to macos? as well
    I missed these.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/functional/resource/link_spec.rb
+++ spec/functional/resource/link_spec.rb
@@ -653,7 +653,7 @@ describe Chef::Resource::Link do
           end
           context "and the link does not yet exist" do
             it "links to the target file" do
-              skip("macOS/FreeBSD/AIX/Solaris symlink? and readlink working on hard links to symlinks") if os_x? || freebsd? || aix? || solaris?
+              skip("macOS/FreeBSD/AIX/Solaris symlink? and readlink working on hard links to symlinks") if macos? || freebsd? || aix? || solaris?
               resource.run_action(:create)
               expect(File.exist?(target_file)).to be_truthy
               # macOS gets angry about this sort of link. Bug in macOS, IMO.
@@ -672,7 +672,7 @@ describe Chef::Resource::Link do
           end
           context "and the link does not yet exist" do
             it "links to the target file" do
-              skip("macOS/FreeBSD/AIX/Solaris fails to create hardlinks to broken symlinks") if os_x? || freebsd? || aix? || solaris?
+              skip("macOS/FreeBSD/AIX/Solaris fails to create hardlinks to broken symlinks") if macos? || freebsd? || aix? || solaris?
               resource.run_action(:create)
               expect(File.exist?(target_file) || File.symlink?(target_file)).to be_truthy
               expect(symlink?(target_file)).to be_truthy
