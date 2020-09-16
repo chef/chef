ba4f800319adefd04f8adcf268f62f20d6975a0f
Tim Smith <tsmith@chef.io>
2020-09-15 19:17:34 -0700

    Update lib/chef/resource/chef_handler.rb
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    Co-authored-by: pete higgins <pete@peterhiggins.org>

--- lib/chef/resource/chef_handler.rb
+++ lib/chef/resource/chef_handler.rb
@@ -110,7 +110,7 @@ class Chef
 
       **JsonFile Handler**
 
-      The json_file handler is available from the chef_handler cookbook and can be used with exceptions and reports. It serializes run status data to a JSON file. This handler may be enabled in one of the following ways.
+      The json_file handler is available from the `chef_handler` cookbook and can be used with exceptions and reports. It serializes run status data to a JSON file. This handler may be enabled in one of the following ways.
 
       By adding the following lines of Ruby code to either the client.rb file or the solo.rb file, depending on how Chef Infra Client is being run:
 
