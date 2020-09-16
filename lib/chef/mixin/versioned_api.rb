e3179ff2c94e3ed59d5d3b78243dd564d4b127f7
Tim Smith <tsmith@chef.io>
2020-09-16 16:00:32 -0700

    Update lib/chef/mixin/versioned_api.rb
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    Co-authored-by: pete higgins <pete@peterhiggins.org>

--- lib/chef/mixin/versioned_api.rb
+++ lib/chef/mixin/versioned_api.rb
@@ -50,7 +50,7 @@ class Chef
           # in which case we'll just start with the highest version and see what happens
           ServerAPIVersions.instance.min_server_version.nil? || (version >= ServerAPIVersions.instance.min_server_version && version <= ServerAPIVersions.instance.send(type))
         end
-          .max { |a, b| a.send(:minimum_api_version) <=> b.send(:minimum_api_version) }
+          .max_by { |a| a.send(:minimum_api_version) }
       end
 
       def def_versioned_delegator(method)
