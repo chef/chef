fbde8dc0c3da8ae96e1cda3d36338779dff65ff1
Tim Smith <tsmith84@gmail.com>
2020-10-12 20:16:24 -0700

    Add InSpec test
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ kitchen-tests/test/integration/end-to-end/default_spec.rb
@@ -0,0 +1,10 @@
+client_rb = if os.windows?
+              'C:\chef\client.rb'
+            else
+              "/etc/chef/client.rb"
+            end
+
+describe file(client_rb) do
+  its("content") { should match(%r{chef_server_url = "https://localhost"}) }
+  its("content") { should match(/chef_license = "accept"/) }
+end
