a10fb12a67d885f96d165f4853453e6d2914086b
Tim Smith <tsmith84@gmail.com>
2020-10-28 13:22:56 -0700

    Make sure we set both multipackage configs in the docs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- tasks/docs.rb
+++ tasks/docs.rb
@@ -172,7 +172,10 @@ namespace :docs_site do
       properties = {}
 
       # these package properties support passing arrays for the package name
-      properties["common_resource_functionality_multiple_packages"] = true if %w{snap_package dpkg_package yum_package apt_package zypper_package homebrew_package dnf_package pacman_package homebrew_package}.include?(name)
+      if %w{snap_package dpkg_package yum_package apt_package zypper_package homebrew_package dnf_package pacman_package homebrew_package}.include?(name)
+        properties["common_resource_functionality_multiple_packages"] = true
+        properties["properties_multiple_packages"] = true
+      end
 
       properties["common_resource_functionality_resources_common_windows_security"] = true if name == "remote_directory"
 
@@ -187,8 +190,6 @@ namespace :docs_site do
       properties["nameless_apt_update"] = true if name == "apt_update"
 
       properties["nameless_build_essential"] = true if name == "build_essential"
-
-      properties["properties_multiple_packages"] = true if %w{dnf_package package zypper_package}.include?(name)
 
       properties["properties_resources_common_windows_security"] = true if %w{cookbook_file file template remote_file directory}.include?(name)
 
