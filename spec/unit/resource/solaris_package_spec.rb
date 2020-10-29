30a5d1ca70425a7dd9e4b4e3a76c48348a5962b8
Tim Smith <tsmith84@gmail.com>
2020-10-28 19:28:59 -0700

    Fix spec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/resource/solaris_package_spec.rb
+++ spec/unit/resource/solaris_package_spec.rb
@@ -27,7 +27,7 @@ describe Chef::Resource::SolarisPackage, "initialize" do
     name: :solaris_package,
     action: :install,
     os: "solaris2",
-    platform_family: solaris2
+    platform_family: "solaris2"
   )
 
   let(:resource) { Chef::Resource::SolarisPackage.new("foo") }
