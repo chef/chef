34bc28587eb9b6c18f4734636ce28d282ed3f09d
Tim Smith <tsmith84@gmail.com>
2020-11-02 10:35:58 -0800

    Remove the freezes in code now
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-utils/lib/chef-utils/dist.rb
+++ chef-utils/lib/chef-utils/dist.rb
@@ -4,95 +4,95 @@ module ChefUtils
   module Dist
     class Apply
       # The chef-apply product name
-      PRODUCT = "Chef Infra Apply".freeze
+      PRODUCT = "Chef Infra Apply"
 
       # The chef-apply binary
-      EXEC = "chef-apply".freeze
+      EXEC = "chef-apply"
     end
 
     class Automate
       # name of the automate product
-      PRODUCT = "Chef Automate".freeze
+      PRODUCT = "Chef Automate"
     end
 
     class Infra
       # When referencing a product directly, like Chef (Now Chef Infra)
-      PRODUCT = "Chef Infra Client".freeze
+      PRODUCT = "Chef Infra Client"
 
       # A short designation for the product, used in Windows event logs
       # and some nomenclature.
-      SHORT = "chef".freeze
+      SHORT = "chef"
 
       # The client's alias (chef-client)
-      CLIENT = "chef-client".freeze
+      CLIENT = "chef-client"
 
       # The chef executable, as in `chef gem install` or `chef generate cookbook`
-      EXEC = "chef".freeze
+      EXEC = "chef"
 
       # The chef-shell executable
-      SHELL = "chef-shell".freeze
+      SHELL = "chef-shell"
 
       # Configuration related constants
       # The chef-shell configuration file
-      SHELL_CONF = "chef_shell.rb".freeze
+      SHELL_CONF = "chef_shell.rb"
 
       # The user's configuration directory
-      USER_CONF_DIR = ".chef".freeze
+      USER_CONF_DIR = ".chef"
 
       # The suffix for Chef's /etc/chef, /var/chef and C:\\Chef directories
       # "chef" => /etc/cinc, /var/cinc, C:\\cinc
-      DIR_SUFFIX = "chef".freeze
+      DIR_SUFFIX = "chef"
     end
 
     class Org
       # product Website address
-      WEBSITE = "https://chef.io".freeze
+      WEBSITE = "https://chef.io"
 
       # The downloads site
-      DOWNLOADS_URL = "downloads.chef.io".freeze
+      DOWNLOADS_URL = "downloads.chef.io"
 
       # The legacy conf folder: C:/opscode/chef. Specifically the "opscode" part
       # DIR_SUFFIX is appended to it in code where relevant
-      LEGACY_CONF_DIR = "opscode".freeze
+      LEGACY_CONF_DIR = "opscode"
 
       # Enable forcing Chef EULA
       ENFORCE_LICENSE = true
 
       # product patents page
-      PATENTS = "https://www.chef.io/patents".freeze
+      PATENTS = "https://www.chef.io/patents"
 
       # knife documentation page
-      KNIFE_DOCS = "https://docs.chef.io/workstation/knife/".freeze
+      KNIFE_DOCS = "https://docs.chef.io/workstation/knife/"
     end
 
     class Server
       # The name of the server product
-      PRODUCT = "Chef Infra Server".freeze
+      PRODUCT = "Chef Infra Server"
 
       # The server's configuration directory
-      CONF_DIR = "/etc/chef-server".freeze
+      CONF_DIR = "/etc/chef-server"
 
       # The servers's alias (chef-server)
-      SERVER = "chef-server".freeze
+      SERVER = "chef-server"
 
       # The server's configuration utility
-      SERVER_CTL = "chef-server-ctl".freeze
+      SERVER_CTL = "chef-server-ctl"
     end
 
     class Solo
       # Chef-Solo's product name
-      PRODUCT = "Chef Infra Solo".freeze
+      PRODUCT = "Chef Infra Solo"
 
       # The chef-solo executable (legacy local mode)
-      EXEC = "chef-solo".freeze
+      EXEC = "chef-solo"
     end
 
     class Zero
       # chef-zero executable
-      PRODUCT = "Chef Infra Zero".freeze
+      PRODUCT = "Chef Infra Zero"
 
       # The chef-zero executable (local mode)
-      EXEC = "chef-zero".freeze
+      EXEC = "chef-zero"
     end
   end
 end
