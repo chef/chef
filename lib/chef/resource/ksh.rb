2cf204d3e19b572d6b6f8f3492092e84f6c541d9
Tim Smith <tsmith@chef.io>
2020-11-03 14:30:30 -0800

    Update lib/chef/resource/ksh.rb
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    Co-authored-by: pete higgins <pete@peterhiggins.org>

--- lib/chef/resource/ksh.rb
+++ lib/chef/resource/ksh.rb
@@ -30,7 +30,7 @@ class Chef
                   " that are available to the **execute** resource. Commands that are executed"\
                   " with this resource are (by their nature) not idempotent, as they are"\
                   " typically unique to the environment in which they are run. Use not_if"\
-                  " and only_if to guard this resource for idempotence."
+                  " and `only_if` to guard this resource for idempotence."
       introduced "12.6"
 
       def initialize(name, run_context = nil)
