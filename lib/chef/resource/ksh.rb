407cebdcec68308434d184d809189c777c86e11e
Tim Smith <tsmith84@gmail.com>
2020-11-03 14:33:25 -0800

    A bit more formatting
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/resource/ksh.rb
+++ lib/chef/resource/ksh.rb
@@ -29,7 +29,7 @@ class Chef
                   " interpreter. This resource may also use any of the actions and properties"\
                   " that are available to the **execute** resource. Commands that are executed"\
                   " with this resource are (by their nature) not idempotent, as they are"\
-                  " typically unique to the environment in which they are run. Use not_if"\
+                  " typically unique to the environment in which they are run. Use `not_if`"\
                   " and `only_if` to guard this resource for idempotence."
       introduced "12.6"
 
