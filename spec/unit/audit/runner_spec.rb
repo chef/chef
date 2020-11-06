9ef4b3620de3f085aeb042ec2ec7582621c319a7
Pete Higgins <pete@peterhiggins.org>
2020-11-05 16:28:58 -0800

    Fill in pending specs for audit runner's #enabled? method.
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>

--- spec/unit/audit/runner_spec.rb
+++ spec/unit/audit/runner_spec.rb
@@ -1,14 +1,63 @@
 require "spec_helper"
 
 describe Chef::Audit::Runner do
+  let(:test_class) do
+    Class.new(Chef::Audit::Runner) do
+      def initialize(run_status = nil)
+        @run_status = run_status
+      end
+    end
+  end
+
   describe "#enabled?" do
-    before do
-      # stub logger
+    let(:cookbook_collection) { Chef::CookbookCollection.new }
+    let(:event_dispatcher) { Chef::EventDispatch::Dispatcher.new }
+    let(:node) { Chef::Node.new(logger: double(:logger).as_null_object) }
+    let(:run_context) { Chef::RunContext.new(node, cookbook_collection, event_dispatcher) }
+    let(:run_status) do
+      Chef::RunStatus.new(node, event_dispatcher).tap do |rs|
+        rs.run_context = run_context
+      end
     end
 
-    it "is true if the node attributes have audit profiles and the audit cookbook is not present"
-    it "is false if the node attributes have audit profiles and the audit cookbook is present"
-    it "is false if the node attributes do not have audit profiles and the audit cookbook is not present"
-    it "is false if the node attributes do not have audit profiles and the audit cookbook is present"
+    let(:runner) { test_class.new(run_status) }
+
+    it "is true if the node attributes have audit profiles and the audit cookbook is not present" do
+      node.default["audit"] = {}
+      node.default["audit"]["profiles"] = {}
+      node.default["audit"]["profiles"]["ssh"] = { 'compliance': "base/ssh" }
+
+      expect(runner).to be_enabled
+    end
+
+    it "is false if the node attributes have audit profiles and the audit cookbook is present" do
+      node.default["audit"] = {}
+      node.default["audit"]["profiles"] = {}
+      node.default["audit"]["profiles"]["ssh"] = { 'compliance': "base/ssh" }
+
+      cookbook_collection["audit"] = double(:audit_cookbook, version: "1.2.3")
+
+      expect(runner).not_to be_enabled
+    end
+
+    it "is false if the node attributes do not have audit profiles and the audit cookbook is not present" do
+      node.default["audit"] = {}
+      node.default["audit"]["profiles"] = {}
+
+      expect(runner).not_to be_enabled
+    end
+
+    it "is false if the node attributes do not have audit profiles and the audit cookbook is present" do
+      node.default["audit"] = {}
+      node.default["audit"]["profiles"] = {}
+
+      cookbook_collection["audit"] = double(:audit_cookbook, version: "1.2.3")
+
+      expect(runner).not_to be_enabled
+    end
+
+    it "is false if the node attributes do not have audit attributes and the audit cookbook is not present" do
+      expect(runner).not_to be_enabled
+    end
   end
 end
