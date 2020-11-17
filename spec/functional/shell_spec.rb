731eb2475b8f1bd43db2f2efc3940b5e25875f91
Christopher A. Snapp <csnapp@chef.io>
2020-11-17 14:28:17 -0700

    Add executables tag to shell_spec
    Signed-off-by: Christopher A. Snapp <csnapp@chef.io>

--- spec/functional/shell_spec.rb
+++ spec/functional/shell_spec.rb
@@ -98,13 +98,13 @@ describe Shell do
       [output, e.status]
     end
 
-    it "boots correctly with -lauto" do
+    it "boots correctly with -lauto", :executables do
       output, exitstatus = run_chef_shell_with("-lauto")
       expect(output).to include("done")
       expect(exitstatus).to eq(0)
     end
 
-    it "sets the log_level from the command line" do
+    it "sets the log_level from the command line", :executables do
       output, exitstatus = run_chef_shell_with("-lfatal") do |out, keyboard|
         show_log_level_code = %q[puts "===#{Chef::Log.level}==="]
         keyboard.puts(show_log_level_code)
@@ -115,13 +115,13 @@ describe Shell do
     end
 
     context "on solo mode" do
-      it "starts correctly" do
+      it "starts correctly", :executables do
         output, exitstatus = run_chef_shell_with("--solo")
         expect(output).to include("done")
         expect(exitstatus).to eq(0)
       end
 
-      it "should be able to use the API" do
+      it "should be able to use the API", :executables do
         output, exitstatus = run_chef_shell_with("-s") do |out, keyboard|
           simple_api_get = "api.get('data')"
           keyboard.puts(simple_api_get)
@@ -132,7 +132,7 @@ describe Shell do
       end
     end
 
-    it "sets the override_runlist from the command line" do
+    it "sets the override_runlist from the command line", :executables do
       output, exitstatus = run_chef_shell_with("-o 'override::foo,override::bar'") do |out, keyboard|
         show_recipes_code = %q[puts "#{node["recipes"].inspect}"]
         keyboard.puts(show_recipes_code)
