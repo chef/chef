e33babda9681f0276e9ef6661d5f5c86f644e0bb
mwrock <matt@mattwrock.com>
2020-12-04 09:47:37 -0800

    only run dsc_script functional tests on 64 bit ruby
    Signed-off-by: mwrock <matt@mattwrock.com>

--- spec/functional/resource/dsc_script_spec.rb
+++ spec/functional/resource/dsc_script_spec.rb
@@ -21,7 +21,7 @@ require "chef/mixin/powershell_exec"
 require "chef/mixin/windows_architecture_helper"
 require "support/shared/integration/integration_helper"
 
-describe Chef::Resource::DscScript, :windows_powershell_dsc_only, :windows64_only do
+describe Chef::Resource::DscScript, :windows_powershell_dsc_only, :ruby64_only do
   include Chef::Mixin::WindowsArchitectureHelper
   include Chef::Mixin::PowershellExec
   before(:all) do
