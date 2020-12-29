29eba0caea7503791d4145c25d837227f982809b
Tim Smith <tsmith84@gmail.com>
2020-12-28 17:41:58 -0800

    Improve debug on windows
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/scripts/bk_win_prep.ps1
+++ .expeditor/scripts/bk_win_prep.ps1
@@ -2,9 +2,11 @@ echo "--- system details"
 $Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
 Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
 
+echo "ruby version:"
 ruby -v
 if (-not $?) { throw "Can't run Ruby. Is it installed?" }
 
+echo "bundler version: "
 bundle --version
 if (-not $?) { throw "Can't run Bundler. Is it installed?" }
 
