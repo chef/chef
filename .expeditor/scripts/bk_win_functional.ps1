725ff1e3913f6673f4f7f8c1651150bd763f16e2
Tim Smith <tsmith84@gmail.com>
2020-12-28 17:49:33 -0800

    Skip bundler update in the windows functional tests
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/scripts/bk_win_functional.ps1
+++ .expeditor/scripts/bk_win_functional.ps1
@@ -8,6 +8,9 @@ Remove-Item -Path C:\ProgramData\chocolatey\bin\choco.exe -ErrorAction SilentlyC
 $ErrorActionPreference = 'Stop'
 
 Write-Output "--- Enable Ruby 2.7"
+ruby -v
+if (-not $?) { throw "Can't run Ruby. Is it installed?" }
+
 Write-Output "Add Uru to Environment PATH"
 $env:PATH = "C:\Program Files (x86)\Uru;" + $env:PATH
 [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine)
@@ -18,21 +21,7 @@ uru 271
 if (-not $?) { throw "Can't Activate Ruby. Did Uru Registration Succeed?" }
 
 Write-Output "--- configure winrm"
-
 winrm quickconfig -q
-
-Write-Output "--- update bundler"
-
-ruby -v
-if (-not $?) { throw "Can't run Ruby. Is it installed?" }
-
-$env:BUNDLER_VERSION=$(findstr bundler omnibus_overrides.rb | %{ $_.split(" ")[3] })
-$env:BUNDLER_VERSION=($env:BUNDLER_VERSION -replace '"', "")
-Write-Output $env:BUNDLER_VERSION
-
-gem install bundler -v $env:BUNDLER_VERSION --force --no-document --quiet
-if (-not $?) { throw "Unable to update Bundler" }
-bundle --version
 
 Write-Output "--- bundle install"
 bundle install --jobs=3 --retry=3 --without omnibus_package
