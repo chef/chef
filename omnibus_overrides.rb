20c62879d26a57eb10d6be5a4f2aab24ce0df9dc
Lamont Granquist <lamont@scriptkiddie.org>
2021-01-19 19:28:41 -0800

    adhoc test
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -13,7 +13,6 @@ override "libxslt", version: "1.1.34"
 override "libyaml", version: "0.1.7"
 override "makedepend", version: "1.0.5"
 override "ncurses", version: "5.9"
-override "nokogiri", version: "1.11.0"
 override "openssl", version: "1.0.2x"
 override "pkg-config-lite", version: "0.28-1"
 override "ruby", version: "2.7.2"
@@ -21,11 +20,3 @@ override "ruby-windows-devkit-bash", version: "3.1.23-4-msys-1.0.18"
 override "util-macros", version: "1.19.0"
 override "xproto", version: "7.0.28"
 override "zlib", version: "1.2.11"
-
-# We build both chef and ohai omnibus-software definitions which creates the
-# chef-client and ohai binstubs. Out of the box the ohai definition uses whatever
-# is in master, which won't match what's in the Gemfile.lock and used by the chef
-# definition. This pin will ensure that ohai and chef-client commands use the
-# same (released) version of ohai.
-gemfile_lock = File.join(File.expand_path(__dir__), "Gemfile.lock")
-override "ohai", version: "#{::File.readlines(gemfile_lock).find { |l| l =~ /^\s+ohai \((\d+\.\d+\.\d+)\)/ }; "v" + $1}" # rubocop: disable Layout/SpaceInsideStringInterpolation
