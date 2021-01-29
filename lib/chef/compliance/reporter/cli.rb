e4090938e8cea73e425352ae27c8d06b448b0ec4
Milan Stastny <milan@stastnej.ch>
2021-01-29 10:57:23 +0100

    Fix output to be more parametric
    Signed-off-by: Milan Stastny <mistastn@cisco.com>

--- lib/chef/compliance/reporter/cli.rb
+++ lib/chef/compliance/reporter/cli.rb
@@ -12,7 +12,7 @@ class Chef
             profile[:controls].each do |control|
               next if control[:results].nil?
 
-              output << "      #{control[:title]}"
+              output << "#{' ' * 6}#{control[:title]}"
               control[:results].each do |result|
                 output << format_result(result)
               end
@@ -38,31 +38,31 @@ class Chef
           if result[:status] == "failed"
             if result[:code_desc]
               found = true
-              output << pastel.red("         #{result[:code_desc]}")
+              output << pastel.red("#{' ' * 9}- #{result[:code_desc]}")
             end
             if result[:message]
               if found
                 result[:message].split(/\n/).reject(&:empty?).each do |m|
-                  output << pastel.red("            #{m}")
+                  output << pastel.red("#{' ' * 12}#{m}")
                 end
               else
                 result[:message].split(/\n/).reject(&:empty?).each do |m|
-                  output << pastel.red("         #{prefix}#{m}")
+                  output << pastel.red("#{' ' * 9}#{m}")
                 end
               end
               found = true
             end
             unless found
-              output << pastel.red("         #{result[:status]}")
+              output << pastel.red("#{' ' * 9}- #{result[:status]}")
             end
           else
             found = false
             if result[:code_desc]
               found = true
-              output << pastel.green("         #{result[:code_desc]}")
+              output << pastel.green("#{' ' * 9}+ #{result[:code_desc]}")
             end
             unless found
-              output << pastel.green("         #{result[:status]}")
+              output << pastel.green("#{' ' * 9}+ #{result[:status]}")
             end
           end
           output
