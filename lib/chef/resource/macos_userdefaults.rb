cc59e2a20eec52d5de2e8926db5204fd289dc523
Tim Smith <tsmith84@gmail.com>
2021-02-22 15:25:25 -0800

    Further improve docs generation
    Fix some markdown generration in the rake task and general cleanup of content in the resources.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/resource/macos_userdefaults.rb
+++ lib/chef/resource/macos_userdefaults.rb
@@ -97,7 +97,7 @@ class Chef
         desired_state: false
 
       property :sudo, [TrueClass, FalseClass],
-        description: "Set to true if the setting you wish to modify requires privileged access. This requires passwordless sudo for the '/usr/bin/defaults' command to be setup for the user running #{ChefUtils::Dist::Infra::PRODUCT}.",
+        description: "Set to true if the setting you wish to modify requires privileged access. This requires passwordless sudo for the `/usr/bin/defaults` command to be setup for the user running #{ChefUtils::Dist::Infra::PRODUCT}.",
         default: false,
         desired_state: false
 
--- lib/chef/resource/mdadm.rb
+++ lib/chef/resource/mdadm.rb
@@ -31,6 +31,52 @@ class Chef
                   " reboot. If the config file is required, it must be done by specifying a template with the correct array layout,"\
                   " and then by using the mount provider to create a file systems table (fstab) entry."
 
+      examples <<~DOC
+      **Create and assemble a RAID 0 array**
+
+      The mdadm command can be used to create RAID arrays. For example, a RAID 0 array named /dev/md0 with 10 devices would have a command similar to the following:
+
+      ```
+      mdadm --create /dev/md0 --level=0 --raid-devices=10 /dev/s01.../dev/s10
+      ```
+
+      where /dev/s01 .. /dev/s10 represents 10 devices (01, 02, 03, and so on). This same command, when expressed as a recipe using the mdadm resource, would be similar to:
+
+      ```ruby
+      mdadm '/dev/md0' do
+        devices [ '/dev/s01', ... '/dev/s10' ]
+        level 0
+        action :create
+      end
+      ```
+
+      (again, where /dev/s01 .. /dev/s10 represents devices /dev/s01, /dev/s02, /dev/s03, and so on).
+
+      **Create and assemble a RAID 1 array**
+
+      ```ruby
+      mdadm '/dev/md0' do
+        devices [ '/dev/sda', '/dev/sdb' ]
+        level 1
+        action [ :create, :assemble ]
+      end
+      ```
+
+      **Create and assemble a RAID 5 array**
+
+      The mdadm command can be used to create RAID arrays. For example, a RAID 5 array named /dev/sd0 with 4, and a superblock type of 0.90 would be similar to:
+
+      ```ruby
+      mdadm '/dev/sd0' do
+        devices [ '/dev/s1', '/dev/s2', '/dev/s3', '/dev/s4' ]
+        level 5
+        metadata '0.90'
+        chunk 32
+        action :create
+      end
+      ```
+      DOC
+
       default_action :create
       allowed_actions :create, :assemble, :stop
 
@@ -78,7 +124,7 @@ class Chef
         end
       end
 
-      action :create do
+      action :create, description: "Create an array with per-device superblocks. If an array already exists (but does not match), update that array to match." do
         unless current_resource.exists
           converge_by("create RAID device #{new_resource.raid_device}") do
             command = "yes | mdadm --create #{new_resource.raid_device} --level #{new_resource.level}"
@@ -96,7 +142,7 @@ class Chef
         end
       end
 
-      action :assemble do
+      action :assemble, description: "Assemble a previously created array into an active array." do
         unless current_resource.exists
           converge_by("assemble RAID device #{new_resource.raid_device}") do
             command = "yes | mdadm --assemble #{new_resource.raid_device} #{new_resource.devices.join(" ")}"
@@ -109,7 +155,7 @@ class Chef
         end
       end
 
-      action :stop do
+      action :stop, description: "Stop an active array." do
         if current_resource.exists
           converge_by("stop RAID device #{new_resource.raid_device}") do
             command = "yes | mdadm --stop #{new_resource.raid_device}"
