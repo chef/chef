175be74f8ecffd15c26eafa635de6563fa2a7675
Marc A. Paradise <marc.paradise@gmail.com>
2021-03-02 14:57:51 -0500

    Update version to match chef
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- knife/lib/chef/knife/version.rb
+++ knife/lib/chef/knife/version.rb
@@ -2,8 +2,10 @@
 class Chef
   class Knife
     KNIFE_ROOT = File.expand_path("../..", __dir__)
-    # MPTD - Chef::VersionString - move into ChefUtil?
-    VERSION = "17.0.92".freeze
+    # MPTD - under chef this a Chef::VersionString, but we can't use that here
+    # without making a circular dep. We should probalby move VersionString to into ChefUtil?
+    # MPTD - this should be getting auto-updated
+    VERSION = "17.0.132".freeze
   end
 end
 
