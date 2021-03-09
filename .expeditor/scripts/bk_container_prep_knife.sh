02c80c5bec6bda9cc57ff3179e8ba4d28268e080
Marc A. Paradise <marc.paradise@gmail.com>
2021-03-09 16:20:37 -0500

    expeditor fixes
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

---
+++ .expeditor/scripts/bk_container_prep_knife.sh
@@ -0,0 +1,28 @@
+# This script gets a container ready to run our various tests in BuildKite
+
+echo "--- Container Config..."
+
+source /etc/os-release
+echo $PRETTY_NAME
+
+echo "ruby version:"
+ruby -v
+echo "bundler version:"
+bundle -v
+
+echo "--- Preparing Container..."
+
+export FORCE_FFI_YAJL="ext"
+export CHEF_LICENSE="accept-no-persist"
+export BUNDLE_GEMFILE="/workdir/knife/Gemfile"
+
+# make sure we have the network tools in place for various network specs
+if [ -f /etc/debian_version ]; then
+  touch /etc/network/interfaces
+fi
+
+# remove default bundler config if there is one
+rm -f .bundle/config
+rm -rf .knife/bundle_config
+
+echo "+++ Run tests"
