3a8de60f7d90666577919eeb2d75f69268619b98
Tim Smith <tsmith84@gmail.com>
2021-03-15 12:26:55 -0700

    Fix interpolation and use the right variable
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/update_bundler_dep.sh
+++ .expeditor/update_bundler_dep.sh
@@ -11,7 +11,7 @@
 
 set -evx
 
-branch="expeditor/$EXPEDITOR_REPO_$EXPEDITOR_LATEST_COMMIT"
+branch="expeditor/${EXPEDITOR_REPO}_${EXPEDITOR_LATEST_COMMIT}"
 git checkout -b "$branch"
 
 bundle lock --update
@@ -20,7 +20,7 @@ git add .
 
 # give a friendly message for the commit and make sure it's noted for any future audit of our codebase that no
 # DCO sign-off is needed for this sort of PR since it contains no intellectual property
-git commit --message "Bump $DEPNAME to $EXPEDITOR_LATEST_COMMIT" --message "This pull request was triggered automatically via Expeditor when $DEPNAME $EXPEDITOR_LATEST_COMMIT was merged." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
+git commit --message "Bump $EXPEDITOR_REPO to $EXPEDITOR_LATEST_COMMIT" --message "This pull request was triggered automatically via Expeditor when $DEPNAME $EXPEDITOR_LATEST_COMMIT was merged." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
 
 open_pull_request "$EXPEDITOR_BRANCH"
 
