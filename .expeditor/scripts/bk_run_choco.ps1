07bbd831014b435ba37c4395b125bf4634f8b6f7
Tim Smith <tsmith84@gmail.com>
2021-04-13 19:46:38 -0700

    Run choco tests on Windows 2016/2019
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/scripts/bk_run_choco.ps1
+++
@@ -1,9 +0,0 @@
-$CurrentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$PrepScript = Join-Path $CurrentDirectory "bk_win_prep.ps1"
-Invoke-Expression $PrepScript
-
-choco --version
-
-echo "+++ bundle exec rspec chocolatey_package_spec"
-bundle exec rspec spec/functional/resource/chocolatey_package_spec.rb
-if (-not $?) { throw "Chef chocolatey functional tests failing." }
