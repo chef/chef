6cfc2d4f02c9d0341f2bfa3d6deddfc1ed9c8537
Marc A. Paradise <marc.paradise@gmail.com>
2021-04-29 23:50:26 -0400

    [drop] re-enable pipelines and include knife in component tests
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- tasks/rspec.rb
+++ tasks/rspec.rb
@@ -25,7 +25,7 @@ begin
 
   desc "Run specs for Chef's Gem Components"
   task :component_specs do
-    %w{chef-utils chef-config}.each do |gem|
+    %w{chef-utils chef-config knife}.each do |gem|
       Dir.chdir(gem) do
         puts "--- Running #{gem} specs"
         Bundler.with_unbundled_env do
@@ -44,9 +44,7 @@ begin
   RSpec::Core::RakeTask.new(:spec) do |t|
     t.verbose = false
     t.rspec_opts = %w{--profile}
-    t.pattern = FileList["spec/**/*_spec.rb"].reject do |path|
-      path =~ /knife.*/
-    end
+    t.pattern = FileList["spec/**/*_spec.rb"]
   end
 
   namespace :spec do
@@ -55,9 +53,6 @@ begin
       t.verbose = false
       t.rspec_opts = %w{--profile}
       t.pattern = FileList["spec/**/*_spec.rb"]
-      t.pattern = FileList["spec/**/*_spec.rb"].reject do |path|
-        path =~ /knife.*/
-      end
     end
 
     desc "Print Specdoc for all specs"
@@ -80,9 +75,7 @@ begin
       RSpec::Core::RakeTask.new(sub) do |t|
         t.verbose = false
         t.rspec_opts = %w{--profile}
-        t.pattern = FileList["spec/#{sub}/**/*_spec.rb"].reject do |path|
-          path =~ /knife.*/
-        end
+        t.pattern = FileList["spec/#{sub}/**/*_spec.rb"]
       end
     end
   end
