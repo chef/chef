d508cd0e1a7de5023324f250c9fd4b668089f6df
Jeff Brimager <jbrimager@chef.io>
2021-05-07 13:27:58 -0500

    connecting habitat hart provider to habitat_package resource
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/provider/package/habitat.rb
+++ lib/chef/provider/package/habitat.rb
@@ -18,8 +18,8 @@
 require_relative "../http/simple"
 require_relative "../json_compat"
 require_relative "../exceptions"
-require_relative "../resource"
-require_relative "../resource/package"
+require_relative "../package"
+require_relative "../resource/habitat/habitat_package"
 # Bring in needed shared methods
 
 class Chef
