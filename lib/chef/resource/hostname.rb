60489bed88a13485e7b5f7cfa656875a7c6aea7c
Tim Smith <tsmith@chef.io>
2021-05-06 23:07:43 -0700

    Add introduced to the new hostname properties
    Make sure the docs get generated correctly
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/resource/hostname.rb
+++ lib/chef/resource/hostname.rb
@@ -90,10 +90,12 @@ class Chef
         default: true
 
       property :domain_user, String,
-        description: "The domain username with permissions to change the local hostname. Specified in the form of 'Domain\User'"
+        description: "The domain username with permissions to change the local hostname. Specified in the form of 'Domain\User'",
+        introduced: "17.1"
 
       property :domain_password, String,
-        description: "The password associated with the domain user account"
+        description: "The password associated with the domain user account",
+        introduced: "17.1"
 
       action_class do
         def append_replacing_matching_lines(path, regex, string)
