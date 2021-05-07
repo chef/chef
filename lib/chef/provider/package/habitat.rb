08da40e95d0bb567dfd13455b6d9d6bfb160ca3d
Jeff Brimager <jbrimager@chef.io>
2021-05-07 16:33:20 -0500

    still cleaning up relational maps for habitat package provider
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/provider/package/habitat.rb
+++ lib/chef/provider/package/habitat.rb
@@ -19,7 +19,7 @@ require_relative "../../http/simple"
 require_relative "../../json_compat"
 require_relative "../../exceptions"
 require_relative "../package"
-require_relative "../resource/habitat/habitat_package"
+require_relative "../../resource/habitat/habitat_package"
 # Bring in needed shared methods
 
 class Chef
