28087eba151db3021112ad1216a80667d287340a
Jeff Brimager <jbrimager@chef.io>
2021-05-07 14:46:48 -0500

    refactoring habitat resource tests from chefspec to rspec
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- spec/functional/resource/habitat_install_spec.rb
+++ spec/functional/resource/habitat_install_spec.rb
@@ -1,20 +1,49 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Chef::Resource::HabitatInstall do
 
-  context 'when compiling the install recipe for chefspec' do
-    it 'install habitat' do
-      expect(chef_run).to install_habitat_install('install habitat')
+  let(:bldr_url) { "https://localhost" }
+  let(:tmp_dir) { "/foo/bar" }
+  let(:license) { "accept" }
+  let(:hab_version) { "1.5.50" }
+  let(:verify_hab) { proc { shell_out!("hab -v").stdout.chomp } }
+
+  let(:run_context) do
+    Chef::RunContext.new(Chef::Node.new, {}, Chef::EventDispatch::Dispatcher.new)
+  end
+
+  subject do
+    new_resource = Chef::Resource::HabitatInstall.new("test habitat install", run_context)
+    new_resource.tmp_dir tmp_dir
+    new_resource.hab_version hab_version
+    new_resource.license license
+    new_resource.bldr_url bldr_url
+    new_resource
+  end
+
+  let(:provider) do
+    provider = subject.provider_for_action(subject.action)
+    provider
+  end
+
+  before(:all) do
+    ENV["Path"] = ENV.delete("Path")
+  end
+
+  context "install habitat" do
+    it "installs habitat" do
+      subject.run_action(:install)
+      expect(verify_hab.call).to eq("1.5.50")
     end
 
-    it 'installs habitat with a depot url' do
-      expect(chef_run).to install_habitat_install('install habitat with depot url')
-        .with(bldr_url: 'https://localhost')
-    end
+    # it 'installs habitat with a depot url' do
+    #   expect(chef_run).to install_habitat_install('install habitat with depot url')
+    #     .with(bldr_url: 'https://localhost')
+    # end
 
-    it 'installs habitat with tmp_dir' do
-      expect(chef_run).to install_habitat_install('install habitat with tmp_dir')
-        .with(tmp_dir: '/foo/bar')
-    end
+    # it 'installs habitat with tmp_dir' do
+    #   expect(chef_run).to install_habitat_install('install habitat with tmp_dir')
+    #     .with(tmp_dir: '/foo/bar')
+    # end
   end
 end
