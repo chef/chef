2c56c5b61d1a11fb630e7c30fd32089d27b5bcb9
Jeff Brimager <jbrimager@chef.io>
2021-05-10 15:33:18 -0500

    connect shared resources
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/provider/package/habitat.rb
+++ lib/chef/provider/package/habitat.rb
@@ -18,7 +18,7 @@ require_relative "../../http/simple"
 require_relative "../../json_compat"
 require_relative "../../exceptions"
 require_relative "../package"
-require_relative "../../resource/habitat/habitat_shared"
+require_relative "../../resource/habitat/_habitat_shared"
 # Bring in needed shared methods
 
 class Chef
