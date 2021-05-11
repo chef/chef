409ebf32431b4490c840d5f77ca61009e9a2936d
Jeff Brimager <jbrimager@chef.io>
2021-05-11 11:57:50 -0500

    correcting template path in habitat_sup resource
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/resource/habitat/habitat_sup.rb
+++ lib/chef/resource/habitat/habitat_sup.rb
@@ -117,7 +117,7 @@ class Chef
           end
 
           template "/hab/sup/default/config/sup.toml" do
-            source ::File.expand_path("../support/sup.toml.erb")
+            source "../support/sup.toml.erb"
             sensitive true
             variables(
               bldr_url: new_resource.bldr_url,
