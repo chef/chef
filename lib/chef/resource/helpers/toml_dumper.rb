b492ae8ef22772376b0448b143462e916ae1d832
Jeff Brimager <jbrimager@chef.io>
2021-05-13 13:47:27 -0500

    reworked toml_dumper resource helper to make sure all end statements were correct and it was being made available to other resource
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/resource/helpers/toml_dumper.rb
+++ lib/chef/resource/helpers/toml_dumper.rb
@@ -1,3 +1,19 @@
+#
+# Copyright:: Copyright (c) Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations
+
 class Chef
   module ResourceHelpers
     module TomlDumper
@@ -104,11 +120,11 @@ class Chef
       def quote_key(key)
         '"' + key.gsub('"', '\\"') + '"'
       end
-    end
 
-    def toml_dump(hash)
-      Dumper.new(hash).toml_str
+      def toml_dump(hash)
+        Dumper.new(hash).toml_str
+      end
+      extend self
     end
-    extend self
   end
 end
