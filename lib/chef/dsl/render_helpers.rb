2175043780b829eab23bc787f35cac8df2c5e68a
Jeff Brimager <jbrimager@chef.io>
2021-05-14 23:04:26 -0500

    wiring chef-utls/internal to render_helpers
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/dsl/render_helpers.rb
+++ lib/chef/dsl/render_helpers.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require_relative "../internal"
+autoload :ChefUtils, "chef-utils/internal"
 require_relative "toml"
 
 require_relative "../json_compat"
