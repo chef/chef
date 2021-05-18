fbca74d54a412180e33a8ccd6e54892566e5ebfc
Jeff Brimager <jbrimager@chef.io>
2021-05-18 11:32:46 -0500

    connecting habitat_package with _provider
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/resource/habitat/habitat_package_provider.rb
+++ lib/chef/resource/habitat/habitat_package_provider.rb
@@ -23,172 +23,174 @@ require_relative "habitat_package"
 # Bring in needed shared methods
 
 class Chef
-  class Resource
-    class Habitat < Chef::Resource
-      use "habitat_shared"
-      provides :habitat_package
+  class Provider
+    class Package
+      class Habitat < Chef::Provider::Package
+        use "habitat_shared"
+        provides :habitat_package
 
-      #
-      # TODO list for `hab pkg`:
-      #
-      # kinda sorta analogous to:
-      #   apt-cache search
-      #   dpkg -l
-      #   dpkg -r / dpkg -P (without depsolving?)
-      #   apt-get remove/purge (with depsolving?)
-      #
-      # - hab pkg search ruby
-      # - hab pkg info lamont-granquist/ruby
-      # - hab pkg info lamont-granquist/ruby/2.3.1
-      # - hab pkg info lamont-granquist/ruby/2.3.1/20160101010101
-      #   ^^^^^ these will all need client-side caches for the "universe" of the depot
-      # - hab pkg uninstall lamont-granquist/ruby
-      # - hab pkg uninstall lamont-granquist/ruby/2.3.1
-      # - hab pkg uninstall lamont-granquist/ruby/2.3.1/20160101010101
-      # - hab pkg list (localinfo?) lamont-granquist/ruby
-      # - hab pkg list (localinfo?) lamont-granquist/ruby/2.3.1
-      # - hab pkg list (localinfo?) lamont-granquist/ruby/2.3.1/20160101010101
-      #   ^^^^^ need a better name
-      #
-      # Probably also want to support installation of local packages
-      # Service resource supports running services from locally installed packages
-      # But we provide no way to handle installation
+        #
+        # TODO list for `hab pkg`:
+        #
+        # kinda sorta analogous to:
+        #   apt-cache search
+        #   dpkg -l
+        #   dpkg -r / dpkg -P (without depsolving?)
+        #   apt-get remove/purge (with depsolving?)
+        #
+        # - hab pkg search ruby
+        # - hab pkg info lamont-granquist/ruby
+        # - hab pkg info lamont-granquist/ruby/2.3.1
+        # - hab pkg info lamont-granquist/ruby/2.3.1/20160101010101
+        #   ^^^^^ these will all need client-side caches for the "universe" of the depot
+        # - hab pkg uninstall lamont-granquist/ruby
+        # - hab pkg uninstall lamont-granquist/ruby/2.3.1
+        # - hab pkg uninstall lamont-granquist/ruby/2.3.1/20160101010101
+        # - hab pkg list (localinfo?) lamont-granquist/ruby
+        # - hab pkg list (localinfo?) lamont-granquist/ruby/2.3.1
+        # - hab pkg list (localinfo?) lamont-granquist/ruby/2.3.1/20160101010101
+        #   ^^^^^ need a better name
+        #
+        # Probably also want to support installation of local packages
+        # Service resource supports running services from locally installed packages
+        # But we provide no way to handle installation
 
-      def load_current_resource
-        @current_resource = Chef::Resource::HabitatPackage.new(new_resource.name)
-        current_resource.package_name(strip_version(new_resource.package_name))
+        def load_current_resource
+          @current_resource = Chef::Resource::HabitatPackage.new(new_resource.name)
+          current_resource.package_name(strip_version(new_resource.package_name))
 
-        @candidate_version = candidate_versions
-        current_resource.version(current_versions)
+          @candidate_version = candidate_versions
+          current_resource.version(current_versions)
 
-        current_resource
-      end
+          current_resource
+        end
 
-      def install_package(names, versions)
-        names.zip(versions).map do |n, v|
-          opts = ["pkg", "install", "--channel", new_resource.channel, "--url", new_resource.bldr_url]
-          opts += ["--auth", new_resource.auth_token] if new_resource.auth_token
-          opts += ["#{strip_version(n)}/#{v}", new_resource.options]
-          opts += ["--binlink"] if new_resource.binlink
-          opts += ["--force"] if new_resource.binlink.eql? :force
-          hab(opts)
+        def install_package(names, versions)
+          names.zip(versions).map do |n, v|
+            opts = ["pkg", "install", "--channel", new_resource.channel, "--url", new_resource.bldr_url]
+            opts += ["--auth", new_resource.auth_token] if new_resource.auth_token
+            opts += ["#{strip_version(n)}/#{v}", new_resource.options]
+            opts += ["--binlink"] if new_resource.binlink
+            opts += ["--force"] if new_resource.binlink.eql? :force
+            hab(opts)
+          end
+        end
+
+        alias_method :upgrade_package, :install_package
+
+        def remove_package(names, versions)
+          # raise 'It is too dangerous to :remove packages with the habitat_package resource right now. This functionality should be deferred to the hab cli.'
+          names.zip(versions).map do |n, v|
+            opts = %w{pkg uninstall}
+            opts += ["--keep-latest", new_resource.keep_latest ] if new_resource.keep_latest
+            opts += ["#{strip_version(n).chomp("/")}#{v}", new_resource.options]
+            opts += ["--exclude"] if new_resource.exclude
+            opts += ["--no-deps"] if new_resource.no_deps
+            hab(opts)
+            # action :remove
+          end
         end
       end
 
-      alias_method :upgrade_package, :install_package
+      alias_method :purge_package, :remove_package
 
-      def remove_package(names, versions)
-        # raise 'It is too dangerous to :remove packages with the habitat_package resource right now. This functionality should be deferred to the hab cli.'
-        names.zip(versions).map do |n, v|
-          opts = %w{pkg uninstall}
-          opts += ["--keep-latest", new_resource.keep_latest ] if new_resource.keep_latest
-          opts += ["#{strip_version(n).chomp("/")}#{v}", new_resource.options]
-          opts += ["--exclude"] if new_resource.exclude
-          opts += ["--no-deps"] if new_resource.no_deps
-          hab(opts)
-          # action :remove
+      private
+
+      def validate_name!(name)
+        raise ArgumentError, "package name must be specified as 'origin/name', use the 'version' property to specify a version" unless name.squeeze("/").count("/") < 2
+      end
+
+      def strip_version(name)
+        validate_name!(name)
+        n = name.squeeze("/").chomp("/").sub(%r{^\/}, "")
+        n = n[0..(n.rindex("/") - 1)] while n.count("/") >= 2
+        n
+      end
+
+      def platform_target
+        if windows?
+          "target=x86_64-windows"
+        elsif node["kernel"]["release"].to_i < 3
+          "target=x86_64-linux-kernel2"
+        else
+          ""
         end
       end
-    end
 
-    alias_method :purge_package, :remove_package
+      def depot_package(name, version = nil)
+        @depot_package ||= {}
+        @depot_package[name] ||=
+          begin
+            origin, pkg_name = name.split("/")
+            name_version = [pkg_name, version].compact.join("/").squeeze("/").chomp("/").sub(%r{^\/}, "")
+            url = if new_resource.bldr_url.include?("/v1/")
+                    "#{new_resource.bldr_url.chomp("/")}/depot/channels/#{origin}/#{new_resource.channel}/pkgs/#{name_version}"
+                  else
+                    "#{new_resource.bldr_url.chomp("/")}/v1/depot/channels/#{origin}/#{new_resource.channel}/pkgs/#{name_version}"
+                  end
+            url << "/latest" unless name_version.count("/") >= 2
+            url << "?#{platform_target}" unless platform_target.empty?
 
-    private
+            headers = {}
+            headers["Authorization"] = "Bearer #{new_resource.auth_token}" if new_resource.auth_token
 
-    def validate_name!(name)
-      raise ArgumentError, "package name must be specified as 'origin/name', use the 'version' property to specify a version" unless name.squeeze("/").count("/") < 2
-    end
-
-    def strip_version(name)
-      validate_name!(name)
-      n = name.squeeze("/").chomp("/").sub(%r{^\/}, "")
-      n = n[0..(n.rindex("/") - 1)] while n.count("/") >= 2
-      n
-    end
-
-    def platform_target
-      if windows?
-        "target=x86_64-windows"
-      elsif node["kernel"]["release"].to_i < 3
-        "target=x86_64-linux-kernel2"
-      else
-        ""
+            Chef::JSONCompat.parse(http.get(url, headers))
+          rescue Net::HTTPServerException
+            nil
+          end
       end
-    end
 
-    def depot_package(name, version = nil)
-      @depot_package ||= {}
-      @depot_package[name] ||=
-        begin
-          origin, pkg_name = name.split("/")
-          name_version = [pkg_name, version].compact.join("/").squeeze("/").chomp("/").sub(%r{^\/}, "")
-          url = if new_resource.bldr_url.include?("/v1/")
-                  "#{new_resource.bldr_url.chomp("/")}/depot/channels/#{origin}/#{new_resource.channel}/pkgs/#{name_version}"
-                else
-                  "#{new_resource.bldr_url.chomp("/")}/v1/depot/channels/#{origin}/#{new_resource.channel}/pkgs/#{name_version}"
-                end
-          url << "/latest" unless name_version.count("/") >= 2
-          url << "?#{platform_target}" unless platform_target.empty?
+      def package_version(name, version = nil)
+        p = depot_package(name, version)
+        "#{p["ident"]["version"]}/#{p["ident"]["release"]}" unless p.nil?
+      end
 
-          headers = {}
-          headers["Authorization"] = "Bearer #{new_resource.auth_token}" if new_resource.auth_token
+      def http
+        # FIXME: use SimpleJSON when the depot mime-type is fixed
+        @http ||= Chef::HTTP::Simple.new(new_resource.bldr_url.to_s)
+      end
 
-          Chef::JSONCompat.parse(http.get(url, headers))
-        rescue Net::HTTPServerException
-          nil
+      def candidate_versions
+        package_name_array.zip(new_version_array).map do |n, v|
+          package_version(n, v)
         end
-    end
-
-    def package_version(name, version = nil)
-      p = depot_package(name, version)
-      "#{p["ident"]["version"]}/#{p["ident"]["release"]}" unless p.nil?
-    end
-
-    def http
-      # FIXME: use SimpleJSON when the depot mime-type is fixed
-      @http ||= Chef::HTTP::Simple.new(new_resource.bldr_url.to_s)
-    end
-
-    def candidate_versions
-      package_name_array.zip(new_version_array).map do |n, v|
-        package_version(n, v)
       end
-    end
 
-    def current_versions
-      package_name_array.map do |n|
-        installed_version(n)
+      def current_versions
+        package_name_array.map do |n|
+          installed_version(n)
+        end
       end
-    end
 
-    def installed_version(ident)
-      hab("pkg", "path", ident).stdout.chomp.split(windows? ? "\\" : "/")[-2..-1].join("/")
-    rescue Mixlib::ShellOut::ShellCommandFailed
-      nil
-    end
-
-    # This is used by the superclass Chef::Provider::Package
-    def version_requirement_satisfied?(current_version, new_version)
-      return false if new_version.nil? || current_version.nil?
-
-      nv_parts = new_version.squeeze("/").split("/")
-
-      if nv_parts.count < 2
-        current_version.squeeze("/").split("/")[0] == new_version.squeeze("/")
-      else
-        current_version.squeeze("/") == new_resource.version.squeeze("/")
+      def installed_version(ident)
+        hab("pkg", "path", ident).stdout.chomp.split(windows? ? "\\" : "/")[-2..-1].join("/")
+      rescue Mixlib::ShellOut::ShellCommandFailed
+        nil
       end
-    end
 
-    # This is used by the superclass Chef::Provider::Package
-    def version_compare(v1, v2)
-      return unless Chef::Provider::Package.methods.include?(:version_compare)
+      # This is used by the superclass Chef::Provider::Package
+      def version_requirement_satisfied?(current_version, new_version)
+        return false if new_version.nil? || current_version.nil?
 
-      # Convert the package version (X.Y.Z/DATE) into a version that Mixlib::Versioning understands (X.Y.Z+DATE)
-      hab_v1 = Mixlib::Versioning.parse(v1.tr("/", "+"))
-      hab_v2 = Mixlib::Versioning.parse(v2.tr("/", "+"))
+        nv_parts = new_version.squeeze("/").split("/")
 
-      hab_v1 <=> hab_v2
+        if nv_parts.count < 2
+          current_version.squeeze("/").split("/")[0] == new_version.squeeze("/")
+        else
+          current_version.squeeze("/") == new_resource.version.squeeze("/")
+        end
+      end
+
+      # This is used by the superclass Chef::Provider::Package
+      def version_compare(v1, v2)
+        return unless Chef::Provider::Package.methods.include?(:version_compare)
+
+        # Convert the package version (X.Y.Z/DATE) into a version that Mixlib::Versioning understands (X.Y.Z+DATE)
+        hab_v1 = Mixlib::Versioning.parse(v1.tr("/", "+"))
+        hab_v2 = Mixlib::Versioning.parse(v2.tr("/", "+"))
+
+        hab_v1 <=> hab_v2
+      end
     end
   end
 end
