d5360e53a4a14faed4e802bcec905b6c9c012987
Jeff Brimager <jbrimager@chef.io>
2021-05-18 13:56:18 -0500

    reverting the habitat_package provider to previous state
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/resource/habitat/habitat_package.rb
+++ lib/chef/resource/habitat/habitat_package.rb
@@ -18,7 +18,6 @@
 #
 
 require_relative "../package"
-require_relative "habitat_package_provider"
 
 class Chef
   class Resource
@@ -27,6 +26,8 @@ class Chef
 
       provides :habitat_package
       use "habitat_shared"
+      description "Install the specified Habitat package from builder. Requires that Habitat is installed"
+      introduced "17.2"
 
       examples <<~DOC
       ```ruby
@@ -74,20 +75,28 @@ class Chef
       DOC
 
       property :bldr_url, String, default: "https://bldr.habitat.sh",
+      description: "The habitat builder url where packages will be downloaded from (defaults to public habitat builder)"
 
       property :channel, String, default: "stable",
+      description: "The release channel to install from (defaults to `stable`)"
 
       property :auth_token, String,
+      description: "Auth token for installing a package from a private organization on builder"
 
       property :binlink, [true, false, :force], default: false,
+      description: "If habitat should attempt to binlink the package. Acceptable values: `true`, `false`, `:force`. Will fail on binlinking if set to `true` and binary or binlink exists."
 
       property :options, String,
+      description: "Pass any additional parameters to the habitat package command"
 
       property :keep_latest, String,
+      description: "Ability to uninstall while retaining a specified version (Default is not set. `This feature only works in Habitat 1.5.86+`)"
 
       property :exclude, String,
+      description: "Identifier of one or more packages that should not be uninstalled. (ex: core/redis, core/busybox-static/1.42.2/21120102031201)"
 
       property :no_deps, [true, false], default: false,
+      description: "Remove package but retain dependencies (Default is `false`)"
     end
   end
 end
