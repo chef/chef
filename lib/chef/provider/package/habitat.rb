6f393528fbaf08eaf5548319379ddb7231048fec
Jeff Brimager <jbrimager@chef.io>
2021-05-18 14:05:13 -0500

    removing bad test
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- lib/chef/provider/package/habitat.rb
+++ lib/chef/provider/package/habitat.rb
@@ -182,15 +182,15 @@ class Chef
       end
 
       # This is used by the superclass Chef::Provider::Package
-      def version_compare(v1, v2)
-        return unless Chef::Provider::Package.methods.include?(:version_compare)
+      # def version_compare(v1, v2)
+      #   return unless Chef::Provider::Package.methods.include?(:version_compare)
 
-        # Convert the package version (X.Y.Z/DATE) into a version that Mixlib::Versioning understands (X.Y.Z+DATE)
-        hab_v1 = Mixlib::Versioning.parse(v1.tr("/", "+"))
-        hab_v2 = Mixlib::Versioning.parse(v2.tr("/", "+"))
+      #   # Convert the package version (X.Y.Z/DATE) into a version that Mixlib::Versioning understands (X.Y.Z+DATE)
+      #   hab_v1 = Mixlib::Versioning.parse(v1.tr("/", "+"))
+      #   hab_v2 = Mixlib::Versioning.parse(v2.tr("/", "+"))
 
-        hab_v1 <=> hab_v2
-      end
+      #   hab_v1 <=> hab_v2
+      # end
     end
   end
 end
