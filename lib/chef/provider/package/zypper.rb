c2d5ef282add16b716da0ca043101ced7c744571
Ashwin <ashvinvarma2@gmail.com>
2021-06-17 17:48:31 +0530

    zypper_package is not idempotent on SUSE - Fixed the issue by handling the exception which raises if SUSE system is not able to search a particular package.
    Signed-off-by: Ashwin <ashvinvarma2@gmail.com>

--- lib/chef/provider/package/zypper.rb
+++ lib/chef/provider/package/zypper.rb
@@ -97,7 +97,11 @@ class Chef
 
         def resolve_available_version(package_name, new_version)
           search_string = new_version.nil? ? package_name : "#{package_name}=#{new_version}"
-          so = shell_out!("zypper", "--non-interactive", "search", "-s", "--provides", "--match-exact", "--type=package", search_string)
+          so = begin
+                shell_out!("zypper", "--non-interactive", "search", "-s", "--provides", "--match-exact", "--type=package", search_string)
+              rescue Exception => e
+                Mixlib::ShellOut.new
+              end
           so.stdout.each_line do |line|
             if md = line.match(/^(\S*)\s+\|\s+(\S+)\s+\|\s+(\S+)\s+\|\s+(\S+)\s+\|\s+(\S+)\s+\|\s+(.*)$/)
               (status, name, type, version, arch, repo) = [ md[1], md[2], md[3], md[4], md[5], md[6] ]
