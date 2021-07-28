cfcf4a18c54697887c7fe2d1971a7e155b533915
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-07-28 12:34:27 -0400

    use expeditor version so we dont lose track of the correct version when promotions happen
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- .expeditor/promote-docker-images.sh
+++ .expeditor/promote-docker-images.sh
@@ -2,12 +2,10 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-VERSION=$(cat VERSION)
-
 echo "--- Creating manifest for ${EXPEDITOR_TARGET_CHANNEL}"
 docker manifest create "chef/chef:${EXPEDITOR_TARGET_CHANNEL}" \
-  --amend "chef/chef:${VERSION}-arm64" \
-  --amend "chef/chef:${VERSION}-amd64"
+  --amend "chef/chef:${EXPEDITOR_VERSION}-arm64" \
+  --amend "chef/chef:${EXPEDITOR_VERSION}-amd64"
 
 echo "--- Pushing manifest for ${EXPEDITOR_TARGET_CHANNEL}"
 docker manifest push "chef/chef:${EXPEDITOR_TARGET_CHANNEL}"
@@ -15,27 +13,27 @@ docker manifest push "chef/chef:${EXPEDITOR_TARGET_CHANNEL}"
 if [[ $EXPEDITOR_TARGET_CHANNEL == "stable" ]]; then
   echo "--- Creating manifest for latest"
   docker manifest create "chef/chef:latest" \
-    --amend "chef/chef:${VERSION}-arm64" \
-    --amend "chef/chef:${VERSION}-amd64"
+    --amend "chef/chef:${EXPEDITOR_VERSION}-arm64" \
+    --amend "chef/chef:${EXPEDITOR_VERSION}-amd64"
 
   echo "--- Pushing manifest for latest"
   docker manifest push "chef/chef:latest"
 
   # split the version and add the tags for major and major.minor
-  version=(${VERSION//./ })
+  version=(${EXPEDITOR_VERSION//./ })
 
   echo "--- Creating manifest for ${version[0]}"
   docker manifest create "chef/chef:${version[0]}" \
-    --amend "chef/chef:${VERSION}-arm64" \
-    --amend "chef/chef:${VERSION}-amd64"
+    --amend "chef/chef:${EXPEDITOR_VERSION}-arm64" \
+    --amend "chef/chef:${EXPEDITOR_VERSION}-amd64"
 
   echo "--- Pushing manifest for ${version[0]}"
   docker manifest push "chef/chef:${version[0]}"
 
   echo "--- Creating manifest for ${version[0]}.${version[1]}"
   docker manifest create "chef/chef:${version[0]}.${version[1]}" \
-    --amend "chef/chef:${VERSION}-arm64" \
-    --amend "chef/chef:${VERSION}-amd64"
+    --amend "chef/chef:${EXPEDITOR_VERSION}-arm64" \
+    --amend "chef/chef:${EXPEDITOR_VERSION}-amd64"
 
   echo "--- Pushing manifest for ${version[0]}.${version[1]}"
   docker manifest push "chef/chef:${version[0]}.${version[1]}"
