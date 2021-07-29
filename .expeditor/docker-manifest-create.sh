4522dc91c4ba8f793cffc81b65624a210587a061
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-07-29 12:06:41 -0400

    clean up scripts and docker login to auth to chef docker org
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- .expeditor/docker-manifest-create.sh
+++ .expeditor/docker-manifest-create.sh
@@ -3,18 +3,21 @@ set -eu -o pipefail
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
+channel="${EXPEDITOR_CHANNEL:-unstable}"
+version="${EXPEDITOR_VERSION:?You must manually set the EXPEDITOR_VERSION environment variable to an existing semantic version.}"
+
 function create_and_push_manifest() {
   manifest_tag="${1}"
 
   echo "--- Creating manifest for ${manifest_tag}"
   docker manifest create "chef/chef:${manifest_tag}" \
-    --amend "chef/chef:${EXPEDITOR_VERSION}-arm64" \
-    --amend "chef/chef:${EXPEDITOR_VERSION}-amd64"
+    --amend "chef/chef:${version}-arm64" \
+    --amend "chef/chef:${version}-amd64"
 
   echo "--- Pushing manifest for ${manifest_tag}"
   docker manifest push "chef/chef:${manifest_tag}"
 }
 
 # create the initial version and initial channel docker images
-create_and_push_manifest "${EXPEDITOR_VERSION}"
-create_and_push_manifest "${EXPEDITOR_CHANNEL}"
+create_and_push_manifest "${version}"
+create_and_push_manifest "${channel}"
