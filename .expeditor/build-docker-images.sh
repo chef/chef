1e43611779dbfa0b3d5666d34eafb3bd42a54307
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-07-29 15:21:19 -0400

    update dockerfile to use correct package for os/arch
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- .expeditor/build-docker-images.sh
+++ .expeditor/build-docker-images.sh
@@ -2,6 +2,15 @@
 set -eu -o pipefail
 
 arch=$1
+
+if [[ $arch == "arm64" ]]; then
+  dockerfile_pkg_version="7"
+  dockerfile_arch="aarch64"
+else
+  dockerfile_pkg_version="6"
+  dockerfile_arch="x86_64"
+fi
+
 channel="${EXPEDITOR_CHANNEL:-unstable}"
 version="${EXPEDITOR_VERSION:?You must manually set the EXPEDITOR_VERSION environment variable to an existing semantic version.}"
 
@@ -9,6 +18,8 @@ echo "--- Building chef/chef:${version} docker image for ${arch}"
 docker build \
   --build-arg "CHANNEL=${channel}" \
   --build-arg "VERSION=${version}" \
+  --build-arg "ARCH=${dockerfile_arch}" \
+  --build-arg "PKG_VERSION=${dockerfile_pkg_version}" \
   -t "chef/chef:${version}-${arch}" .
 
 echo "--- Pushing chef/chef:${version} docker image for ${arch} to dockerhub"
