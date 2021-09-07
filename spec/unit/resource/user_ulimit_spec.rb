1f082d4a01ca08758781c5e36aa1cf12fd0a104d
Ashwin <avarma@msystechnologies.com>
2021-09-07 17:07:46 +0530

    Fixing sensitive property. - Sensitive property was not working on user_ulimit. - Added required specs.
    Signed-off-by: Ashwin <avarma@msystechnologies.com>

--- spec/unit/resource/user_ulimit_spec.rb
+++ spec/unit/resource/user_ulimit_spec.rb
@@ -50,4 +50,18 @@ describe Chef::Resource::UserUlimit do
     expect { resource.action :create }.not_to raise_error
     expect { resource.action :delete }.not_to raise_error
   end
+
+  describe "sensitive attribute" do
+    context "should be insensitive by default" do
+      it { expect(resource.sensitive).to(be_falsey) }
+    end
+
+    context "when set" do
+      before { resource.sensitive(true) }
+
+      it "should be set on the resource" do
+        expect(resource.sensitive).to(be_truthy)
+      end 
+    end
+  end
 end
