d02a0bc3ac62c8a4caa6b1e203720db93f9c3be9
Jason Barnett <jason.w.barnett@gmail.com>
2021-09-20 15:57:25 -0600

    Set the RUBY_DLL_PATH
    Signed-off-by: Jason Barnett <jason.w.barnett@gmail.com>

--- .expeditor/scripts/bk_win_unit.ps1
+++ .expeditor/scripts/bk_win_unit.ps1
@@ -2,6 +2,11 @@ $CurrentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $PrepScript = Join-Path $CurrentDirectory "bk_win_prep.ps1"
 Invoke-Expression $PrepScript
 
+$gemdir = (bundle exec gem environment gemdir)
+$libarchive_dir = (Get-ChildItem -Recurse -Path $gemdir -Filter libarchive.dll)[0].Directory.FullName
+Write-Output "libarchive_dir: ${libarchive_dir}"
+$env:RUBY_DLL_PATH = $libarchive_dir
+
 echo "+++ bundle exec rake"
 bundle exec rake spec:unit
 if (-not $?) { throw "Chef unit tests failing." }
