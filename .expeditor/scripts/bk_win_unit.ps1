08ecf3fd1ceaa690d51daafd196eb6796023281e
Jason Barnett <jason.w.barnett@gmail.com>
2021-09-20 12:28:20 -0600

    Set RUBY_DLL_PATH for Windows tests
    The DLL loading wasn't working properly and I stumbled upon this article which
    layed out how DLL loading is impacted by the Ruby Installer.
    https://github.com/oneclick/rubyinstaller2/wiki/For-gem-developers#-dll-loading
    
    Signed-off-by: Jason Barnett <jason.w.barnett@gmail.com>

--- .expeditor/scripts/bk_win_unit.ps1
+++ .expeditor/scripts/bk_win_unit.ps1
@@ -2,6 +2,8 @@ $CurrentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $PrepScript = Join-Path $CurrentDirectory "bk_win_prep.ps1"
 Invoke-Expression $PrepScript
 
+$env:RUBY_DLL_PATH = "${CurrentDirectory}\..\..\vendor\bundle\ruby\3.0.0\gems\mixlib-archive-1.1.7-universal-mingw32\distro\ruby_bin_folder"
+
 echo "+++ bundle exec rake"
 bundle exec rake spec:unit
 if (-not $?) { throw "Chef unit tests failing." }
