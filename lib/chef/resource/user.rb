6f263e246114b1748d8cec60497c1dca98479b02
Corey Hemminger <hemminger@hotmail.com>
2021-10-04 13:48:15 -0500

    updated introduced lines to 17.7
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- lib/chef/resource/user.rb
+++ lib/chef/resource/user.rb
@@ -52,12 +52,12 @@ class Chef
 
       property :expire_date, [ String, NilClass ],
         description: "(Linux) The date on which the user account will be disabled. The date is specified in the format YYYY-MM-DD.",
-        introduced: "17.6",
+        introduced: "17.7",
         desired_state: false
 
       property :inactive, [ Integer, NilClass ],
         description: "(Linux) The number of days after a password expires until the account is permanently disabled. A value of 0 disables the account as soon as the password has expired, and a value of -1 disables the feature.",
-        introduced: "17.6",
+        introduced: "17.7",
         desired_state: false
 
       property :non_unique, [ TrueClass, FalseClass ],
