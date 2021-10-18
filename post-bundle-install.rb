8394290e857c0ecef3395fada091a449abdbd215
Tim Smith <tsmith84@gmail.com>
2021-10-18 16:19:34 -0700

    Updates to cspell and a typo
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -19,7 +19,7 @@ Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
 
   puts "re-installing #{gem_name}..."
 
-  # we can't use "commmand" or "bundle" or "gem" DSL methods here since those are lazy and we need to run commands immediately
+  # we can't use "command" or "bundle" or "gem" DSL methods here since those are lazy and we need to run commands immediately
   # (this is like a shell_out inside of a ruby_block in core chef, you don't use an execute resource inside of a ruby_block or
   # things get really weird and unexpected)
   Dir.chdir(gempath) do
