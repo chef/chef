2a84babf282ef59e1515efb66be2620aa2fb3f4a
Corey Hemminger <hemminger@hotmail.com>
2021-12-27 17:30:13 -0600

    update kitchen-test end_to_end recipes _chef_client_config.rb
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_chef_client_config.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_chef_client_config.rb
@@ -1,5 +1,6 @@
 chef_client_config "Create chef-client's client.rb" do
   chef_server_url "https://localhost"
+  log_location windows? ? 'C:\\chef\\log_test\client.log' : '/var/log/chef/log_test/client.log'
   chef_license "accept"
   ohai_optional_plugins %i{Passwd Lspci Sysctl}
   ohai_disabled_plugins %i{Sessions Interrupts}
