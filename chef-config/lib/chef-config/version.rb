c6fc0a9fb2fc23ec83efa2a898f4b02914f485b1
Chef Expeditor <chef-ci@chef.io>
2022-01-20 16:04:25 +0000

    Bump version to 17.9.34 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -15,5 +15,5 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("..", __dir__)
-  VERSION = "17.9.33".freeze
+  VERSION = "17.9.34".freeze
 end
--- knife/lib/chef/knife/version.rb
+++ knife/lib/chef/knife/version.rb
@@ -17,7 +17,7 @@
 class Chef
   class Knife
     KNIFE_ROOT = File.expand_path("../..", __dir__)
-    VERSION = "17.9.33".freeze
+    VERSION = "17.9.34".freeze
   end
 end
 
