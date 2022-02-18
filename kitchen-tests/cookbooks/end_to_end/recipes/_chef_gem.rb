676d352ab1ad46ef41052615d5931cf844af4bf0
Lamont Granquist <lamont@scriptkiddie.org>
2022-02-18 15:18:09 -0800

    add note on when the test can be re-enabled
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- kitchen-tests/cookbooks/end_to_end/recipes/_chef_gem.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_chef_gem.rb
@@ -7,6 +7,7 @@
 
 # make sure customers can install knife back into the client for now
 # and also make sure chef_gem works in general
+# ===== knife test has to be disabled until chef 17.10 is released ====
 # gem_name = rhel6? ? "community_cookbook_releaser" : "knife"
 gem_name = "community_cookbook_releaser"
 
