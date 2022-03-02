e9e0c909f2e7e0ad1968f0a3f1fa3ebd4d224414
Chef Expeditor <chef-ci@chef.io>
2022-03-02 16:21:10 +0000

    Bump version to 16.17.53 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-bin/lib/chef-bin/version.rb
+++ chef-bin/lib/chef-bin/version.rb
@@ -21,7 +21,7 @@
 
 module ChefBin
   CHEFBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "16.17.52".freeze
+  VERSION = "16.17.53".freeze
 end
 
 #
--- chef-utils/lib/chef-utils/version.rb
+++ chef-utils/lib/chef-utils/version.rb
@@ -16,5 +16,5 @@
 
 module ChefUtils
   CHEFUTILS_ROOT = File.expand_path("..", __dir__)
-  VERSION = "16.17.52"
+  VERSION = "16.17.53"
 end
