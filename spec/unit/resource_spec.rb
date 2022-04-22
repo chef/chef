d1064b9282987220be4ece16c244e32d0e2eb1f7
John McCrae <john.mccrae@progress.com>
2022-04-22 13:16:49 -0700

    Updated Testing Ruby 3.1
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- spec/unit/resource_spec.rb
+++ spec/unit/resource_spec.rb
@@ -939,14 +939,14 @@ describe Chef::Resource do
 
     it "adds mappings for all platforms", ruby: "< 2.7" do
       expect(Chef.resource_handler_map).to receive(:set).with(
-        :tape_deck, Chef::Resource::Klz
+        :tape_deck, Chef::Resource::Klz, {}
       )
       klz.provides :tape_deck
     end
 
     it "adds mappings for all platforms", ruby: ">= 2.7" do
       expect(Chef.resource_handler_map).to receive(:set).with(
-        :tape_deck, Chef::Resource::Klz, {}
+        :tape_deck, Chef::Resource::Klz
       )
       klz.provides :tape_deck
     end
