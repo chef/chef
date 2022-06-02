771928e0bea6024b2e5ffc7047169429f8cbb483
John McCrae <john.mccrae@progress.com>
2022-06-02 10:01:07 -0700

    Adjusting cert creation for older Windows platforms
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- spec/integration/client/client_spec.rb
+++ spec/integration/client/client_spec.rb
@@ -37,7 +37,14 @@ describe "chef-client" do
 
   def install_certificate_in_store(client_name)
     if ChefUtils.windows?
-      powershell_exec!("New-SelfSignedCertificate -certstorelocation cert:\\localmachine\\my -Subject #{client_name} -FriendlyName #{client_name} -KeyExportPolicy Exportable")
+      powershell_exec! <<~EOH
+        if ($PSVersionTable.PSVersion -match 3) {
+          New-SelfSignedCertificate -CertStoreLocation Cert:\\LocalMachine\\My -DnsName #{client_name}
+        }
+        else {
+          New-SelfSignedCertificate -certstorelocation cert:\\localmachine\\my -Subject #{client_name} -FriendlyName #{client_name} -KeyExportPolicy Exportable
+        }
+      EOH
     end
   end
 
