10bc628529c316bc0ab1b213373b538764259f20
John McCrae <john.mccrae@progress.com>
2022-06-13 14:33:44 -0700

    Backing out a couple of changes
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/scripts/bk_win_unit.ps1
+++ .expeditor/scripts/bk_win_unit.ps1
@@ -3,7 +3,7 @@ $PrepScript = Join-Path $CurrentDirectory "bk_win_prep.ps1"
 Invoke-Expression $PrepScript
 
 echo "+++ bundle exec rake"
-bundle exec rake spec:unit --verbose
+bundle exec rake spec:unit
 if (-not $?) { throw "Chef unit tests failing." }
 bundle exec rake component_specs
 if (-not $?) { throw "Chef component specs failing." }
