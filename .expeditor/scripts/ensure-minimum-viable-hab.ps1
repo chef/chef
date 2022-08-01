2184a29ead4420cd5a8d341984618c1afcb2f87a
Thomas Powell <powell@progress.com>
2022-08-01 15:49:04 -0400

    Install habitat properly
    Signed-off-by: Thomas Powell <powell@progress.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -3,7 +3,8 @@
 Write-Host "!!! $hab_version"
 if ($hab_version -lt [Version]"0.85.0" ) {
     Write-Host "--- :habicat: Installing the version of Habitat required"
-    install-habitat --version 0.85.0.20190916
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
     if (-not $?) { throw "Hab version is older than 0.85 and could not update it." }
 } else {
     Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
