d31564c91ce7bb2dca5551b4f1d15ec523ea485d
Thomas Powell <powell@progress.com>
2022-08-02 13:34:23 -0400

    Force a refresh of the path
    Signed-off-by: Thomas Powell <powell@progress.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -7,7 +7,7 @@ if ($hab_version -lt [Version]"0.85.0" ) {
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
     if (-not $?) { throw "Hab version is older than 0.85 and could not update it." }
     (Get-Command hab).Path
-    $Env.Path
+    Write-Host $Env.Path
 } else {
     Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
 }
