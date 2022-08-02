970ffdcff9df3ba2223d44134b2e2d8974453e1e
Thomas Powell <powell@progress.com>
2022-08-02 10:43:52 -0400

    Debugging paths for habitat
    Signed-off-by: Thomas Powell <powell@progress.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -6,6 +6,8 @@ if ($hab_version -lt [Version]"0.85.0" ) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
     if (-not $?) { throw "Hab version is older than 0.85 and could not update it." }
+    (Get-Command hab).Path
+    $Env.Path
 } else {
     Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
 }
