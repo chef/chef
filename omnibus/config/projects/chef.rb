45da2d21ac1b95ff1e695313f9078b42f0bc159d
John McCrae <john.mccrae@progress.com>
2022-08-03 18:20:17 -0700

    removed 32-bit builds and updated an ENV variable
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- omnibus/config/projects/chef.rb
+++ omnibus/config/projects/chef.rb
@@ -95,7 +95,7 @@ package :msi do
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
   signing_identity "AF21BA8C9E50AE20DA9907B6E2D4B0CC3306CA03", machine_store: true
-  parameters ChefLogDllPath: windows_safe_path(gem_path("chef-[0-9]*-mingw32/ext/win32-eventlog/chef-log.dll")),
+  parameters ChefLogDllPath: windows_safe_path(gem_path("chef-[0-9]*-x64-mingw-ucrt/ext/win32-eventlog/chef-log.dll")),
              ProjectLocationDir: project_location_dir
 end
 
