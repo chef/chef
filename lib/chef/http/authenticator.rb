2583af8cec1fa92391d81c27a08fde23b4ca7f29
Neha Pansare <neha.pansare@progress.com>
2022-08-04 12:32:39 +0530

    Use PtrToStringBSTR instead of PtrToStringAuto
    Signed-off-by: Neha Pansare <neha.pansare@progress.com>

--- lib/chef/http/authenticator.rb
+++ lib/chef/http/authenticator.rb
@@ -208,8 +208,8 @@ class Chef
 
       def self.decrypt_pfx_pass(password)
         powershell_code = <<~CODE
-          $secure_string = ConvertTo-SecureString "#{password}" -AsPlainText -Force 
-          $string = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($secure_string))))
+          $secure_string = "#{password}" | ConvertTo-SecureString
+          $string = [Runtime.InteropServices.Marshal]::PtrToStringBSTR([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($secure_string))))
           return $string
         CODE
         powershell_exec!(powershell_code).result
