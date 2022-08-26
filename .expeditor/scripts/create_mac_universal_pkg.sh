731f804d7c0452ae8bb0a005b2179f9c0d9647a4
Vikram Karve <vikram.karve@progress.com>
2022-08-26 14:11:09 +0530

    Add pipeline for macos universal pkg. Initial commit, version hardcoded.
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

---
+++ .expeditor/scripts/create_mac_universal_pkg.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+mkdir -p ./stage/pkg/tmp/chef-pkgs
+mkdir -p ./stage/script
+
+pushd ./stage/pkg/tmp/chef-pkgs
+
+curl -O https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.x86_64.dmg
+hdiutil attach chef-18.0.92-1.x86_64.dmg
+cp /Volumes/Chef\ Infra\ Client/chef-18.0.92-1.x86_64.pkg ./
+hdiutil detach /Volumes/Chef\ Infra\ Client 
+rm chef-18.0.92-1.x86_64.dmg
+
+curl -O https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.arm64.dmg
+hdiutil attach chef-18.0.92-1.arm64.dmg
+cp /Volumes/Chef\ Infra\ Client/chef-18.0.92-1.arm64.pkg ./
+hdiutil detach /Volumes/Chef\ Infra\ Client 
+rm chef-18.0.92-1.arm64.dmg
+
+popd
+
+cat << EOF > ./stage/script/postinstall 
+#!/bin/sh
+
+if [ `uname -m` == 'x86_64' ]
+then
+  installer -pkg /tmp/chef-pkgs/chef-18.0.92-1.x86_64.pkg -target LocalSystem -verbose
+elif [ `uname -m` == 'arm64' ]
+then
+  installer -pkg /tmp/chef-pkgs/chef-18.0.92-1.arm64.pkg -target LocalSystem -verbose
+fi
+EOF
+
+chmod +x ./stage/script/postinstall
+
+pkgbuild --root stage/pkg --scripts stage/script --identifier "io.chef.infra-client.wrapper" \
+ --version "18.0.92" --install-location / --min-os-version 10.15 stage/pkg/chef-18.0.92-1.universal.pkg
