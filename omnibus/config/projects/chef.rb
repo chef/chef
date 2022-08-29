ac6a23d9ff5818d0eb113b876efd777d4df8e01a
Thomas Powell <powell@progress.com>
2022-08-29 13:40:41 -0400

    Update signing identity for msi
    Signed-off-by: Thomas Powell <powell@progress.com>

--- omnibus/config/projects/chef.rb
+++ omnibus/config/projects/chef.rb
@@ -94,7 +94,7 @@ package :msi do
   upgrade_code msi_upgrade_code
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
-  signing_identity "AF21BA8C9E50AE20DA9907B6E2D4B0CC3306CA03", machine_store: true
+  signing_identity "13B510D1CF1B3467856A064F1BEA12D0884D2528", machine_store: true
   parameters ChefLogDllPath: windows_safe_path(gem_path("chef-[0-9]*-x64-mingw-ucrt/ext/win32-eventlog/chef-log.dll")),
              ProjectLocationDir: project_location_dir
 end
