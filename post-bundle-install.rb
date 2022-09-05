2a4f4cfc525d3c7f901b4fb6590943629701d9c6
Neha Pansare <neha.pansare@progress.com>
2022-09-05 15:01:49 +0530

    Changes to install chef-powershell gem from git repo instead of ruby gems
    Signed-off-by: Neha Pansare <neha.pansare@progress.com>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 gem_home = Gem.paths.home
 
 puts "fixing bundle installed gems in #{gem_home}"
@@ -9,10 +11,28 @@ puts "fixing bundle installed gems in #{gem_home}"
 # rake install since we need --conservative --minimal-deps in order to not install duplicate gems.
 #
 Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
+  puts "===Gempath: #{gempath.inspect}"
   matches = File.basename(gempath).match(/.*-[A-Fa-f0-9]{12}/)
   next unless matches
 
-  gem_name = File.basename(Dir["#{gempath}/*.gemspec"].first, ".gemspec")
+  if gempath.match("chef-powershell")
+    path = "#{gempath}/chef-powershell"
+    
+    #For now copy the windowspowershell dlls at chef/chef to gem here
+    dll_files = Dir.glob(File.expand_path("distro/ruby_bin_folder/#{ENV["PROCESSOR_ARCHITECTURE"]}", __dir__) + "**/*")
+    puts "=== dll files #{dll_files.inspect}"
+    bin_path = "#{path}/bin/ruby_bin_folder/#{ENV["PROCESSOR_ARCHITECTURE"]}"
+    puts "==== bin_path:#{bin_path}"
+
+    FileUtils.mkdir_p "#{bin_path}"
+    FileUtils.cp_r dll_files, bin_path
+
+    puts "=== bin_path contents after copying: #{Dir[bin_path + '/*']}}"
+  else  
+    path = "#{gempath}" 
+  end
+
+  gem_name = File.basename(Dir["#{path}/*.gemspec"].first, ".gemspec")
   # FIXME: should strip any valid ruby platform off of the gem_name if it matches
 
   next unless gem_name
@@ -22,7 +42,11 @@ Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
 
   puts "re-installing #{gem_name}..."
 
-  Dir.chdir(gempath) do
+  Dir.chdir(path) do
+    if gem_name == "chef-powershell"
+      File.delete("#{gem_name}.gemspec") if File.exist?("#{gem_name}.gemspec")
+      File.rename("template.txt", "chef-powershell.gemspec")
+    end
     system("gem build #{gem_name}.gemspec") or raise "gem build failed"
     system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
   end
