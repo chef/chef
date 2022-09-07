419ae2b2547f965eb3a2189165f3cb0453c9f461
Vikram Karve <vikram.karve@progress.com>
2022-09-07 21:59:07 +0530

    remove unsupported --no-progress-meter option
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- .expeditor/scripts/create_mac_universal_pkg.sh
+++ .expeditor/scripts/create_mac_universal_pkg.sh
@@ -5,13 +5,13 @@ mkdir -p ./stage/script
 
 pushd ./stage/pkg/tmp/chef-pkgs
 
-curl -O --no-progress-meter https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.x86_64.dmg
+curl -O https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.x86_64.dmg
 hdiutil attach -quiet chef-18.0.92-1.x86_64.dmg
 cp /Volumes/Chef\ Infra\ Client/chef-18.0.92-1.x86_64.pkg ./
 hdiutil detach -quiet /Volumes/Chef\ Infra\ Client
 rm chef-18.0.92-1.x86_64.dmg
 
-curl -O --no-progress-meter https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.arm64.dmg
+curl -O https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.arm64.dmg
 hdiutil attach -quiet chef-18.0.92-1.arm64.dmg
 cp /Volumes/Chef\ Infra\ Client/chef-18.0.92-1.arm64.pkg ./
 hdiutil detach -quiet /Volumes/Chef\ Infra\ Client
