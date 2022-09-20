7bed381c341d6c9807a9c64deb4ac44e4f661a9e
Marc A. Paradise <marc.paradise@gmail.com>
2022-09-20 11:35:45 -0400

    remove tests to focus windows on only knife tests
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- .expeditor/scripts/bk_win_unit.ps1
+++ .expeditor/scripts/bk_win_unit.ps1
@@ -2,9 +2,9 @@ $CurrentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $PrepScript = Join-Path $CurrentDirectory "bk_win_prep.ps1"
 Invoke-Expression $PrepScript
 
-echo "+++ bundle exec rake spec:unit"
-bundle exec rake spec:unit
-if (-not $?) { throw "Chef unit tests failing." }
+#echo "+++ bundle exec rake spec:unit"
+#bundle exec rake spec:unit
+#if (-not $?) { throw "Chef unit tests failing." }
 echo "+++ bundle exec rake component_specs"
 bundle exec rake component_specs
 if (-not $?) { throw "Chef component specs failing." }
