b9cee483106a57bde15b5e581dfc44e3808f9233
Joseph Larionov <jlarionov@webmd.net>
2022-09-26 11:48:52 -0700

    docs: windows_package Mercurial example used an old url
    Signed-off-by: Joseph Larionov <jlarionov@webmd.net>

--- lib/chef/resource/windows_package.rb
+++ lib/chef/resource/windows_package.rb
@@ -100,7 +100,7 @@ class Chef
 
       ```ruby
       windows_package 'Mercurial 3.6.1 (64-bit)' do
-        source 'http://mercurial.selenic.com/release/windows/Mercurial-3.6.1-x64.exe'
+        source 'https://www.mercurial-scm.org/release/windows/Mercurial-3.6.1-x64.exe'
         checksum 'febd29578cb6736163d232708b834a2ddd119aa40abc536b2c313fc5e1b5831d'
       end
       ```
