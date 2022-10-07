chef/chef

commit d4c3b8ad111ec7ff6b32ebccf9edd1b6a2f9e8e2

add expire and inactive options to linux user resource (#11659)
* add expire and inactive options to linux user resource
* fix spell check
* add tests

Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

lib/chef/provider/user/linux.rb
@@ -23,6 +23,22 @@ class Chef
         provides :linux_user
         provides :user, os: "linux"
 
+        def load_current_resource
+          super
+          load_shadow_options
+        end
+
+        def compare_user
+          super
+          %i{expire_date inactive}.each do |user_attrib|
+            new_val = new_resource.send(user_attrib)
+            cur_val = current_resource.send(user_attrib)
+            if !new_val.nil? && new_val.to_s != cur_val.to_s
+              @change_desc << "change #{user_attrib} from #{cur_val} to #{new_val}"
+            end
+          end
+        end
+
         def create_user
           shell_out!("useradd", universal_options, useradd_options, new_resource.username)
         end
@@ -52,7 +68,9 @@ class Chef
         def universal_options
           opts = []
           opts << "-c" << new_resource.comment if should_set?(:comment)
+          opts << "-e" << new_resource.expire_date if prop_is_set?(:expire_date)
           opts << "-g" << new_resource.gid if should_set?(:gid)
+          opts << "-f" << new_resource.inactive if prop_is_set?(:inactive)
           opts << "-p" << new_resource.password if should_set?(:password)
           opts << "-s" << new_resource.shell if should_set?(:shell)
           opts << "-u" << new_resource.uid if should_set?(:uid)
@@ -115,6 +133,12 @@ class Chef
 
           # FIXME: should probably go on the current_resource
           @locked
+        end
+
+        def prop_is_set?(prop)
+          v = new_resource.send(prop.to_sym)
+
+          !v.nil? && v != ""
         end
       end
     end

spec/unit/provider/user/linux_spec.rb
@@ -70,4 +70,54 @@ describe Chef::Provider::User::Linux do
       expect( provider.useradd_options ).to eql(["-m"])
     end
   end
+
+  describe "expire_date behavior" do
+    before(:each) do
+      @new_resource = Chef::Resource::User::LinuxUser.new("adam", @run_context)
+      @current_resource = Chef::Resource::User::LinuxUser.new("adam", @run_context)
+    end
+
+    it "defaults expire_date to nil" do
+      expect( @new_resource.expire_date ).to be nil
+    end
+
+    it "by default expire_date is nil and we use ''" do
+      expect( provider.universal_options ).to eql([])
+    end
+
+    it "setting expire_date to nil includes ''" do
+      @new_resource.expire_date nil
+      expect( provider.universal_options ).to eql([])
+    end
+
+    it "setting expire_date to 1982-04-16 includes -e" do
+      @new_resource.expire_date "1982-04-16"
+      expect( provider.universal_options ).to eql(["-e", "1982-04-16"])
+    end
+  end
+
+  describe "inactive behavior" do
+    before(:each) do
+      @new_resource = Chef::Resource::User::LinuxUser.new("adam", @run_context)
+      @current_resource = Chef::Resource::User::LinuxUser.new("adam", @run_context)
+    end
+
+    it "defaults inactive to nil" do
+      expect( @new_resource.inactive ).to be nil
+    end
+
+    it "by default inactive is nil and we use ''" do
+      expect( provider.universal_options ).to eql([])
+    end
+
+    it "setting inactive to nil includes ''" do
+      @new_resource.inactive nil
+      expect( provider.universal_options ).to eql([])
+    end
+
+    it "setting inactive to 90 includes -f" do
+      @new_resource.inactive 90
+      expect( provider.universal_options ).to eql(["-f", 90])
+    end
+  end
 end

spec/unit/resource/user/linux_user_spec.rb
@@ -1,0 +1,42 @@
+require "spec_helper"
+
+describe Chef::Resource::User, "initialize" do
+  let(:resource) { Chef::Resource::User::LinuxUser.new("notarealuser") }
+
+  describe "inactive attribute" do
+    it "allows a string" do
+      resource.inactive "100"
+      expect(resource.inactive).to eql("100")
+    end
+
+    it "allows an integer" do
+      resource.inactive 100
+      expect(resource.inactive).to eql(100)
+    end
+
+    it "does not allow a hash" do
+      expect { resource.inactive({ woot: "i found it" }) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "expire_date attribute" do
+    it "allows a string" do
+      resource.expire_date "100"
+      expect(resource.expire_date).to eql("100")
+    end
+
+    it "does not allow an integer" do
+      expect { resource.expire_date(90) }.to raise_error(ArgumentError)
+    end
+
+    it "does not allow a hash" do
+      expect { resource.expire_date({ woot: "i found it" }) }.to raise_error(ArgumentError)
+    end
+  end
+
+  %w{inactive expire_date}.each do |prop|
+    it "sets #{prop} to nil" do
+      expect(resource.send(prop)).to eql(nil)
+    end
+  end
+end

lib/chef/provider/user.rb
@@ -66,21 +66,33 @@ class Chef
           end
           current_resource.comment(user_info.gecos)
 
-          if new_resource.password && current_resource.password == "x"
-            begin
-              require "shadow"
-            rescue LoadError
-              @shadow_lib_ok = false
-            else
-              shadow_info = Shadow::Passwd.getspnam(new_resource.username)
-              current_resource.password(shadow_info.sp_pwdp)
-            end
+          begin
+            require "shadow"
+          rescue LoadError
+            @shadow_lib_ok = false
+          else
+            @shadow_info = Shadow::Passwd.getspnam(new_resource.username)
+            current_resource.password(@shadow_info.sp_pwdp) if new_resource.password && current_resource.password == "x"
           end
 
           convert_group_name if new_resource.gid
         end
 
         current_resource
+      end
+
+      def load_shadow_options
+        unless @shadow_info.nil?
+          current_resource.inactive(@shadow_info.sp_inact&.to_i)
+          # sp_expire gives time since epoch in days till expiration. Need to convert that
+          # to time in seconds since epoch and output date format for comparison
+          expire_date = if @shadow_info.sp_expire.nil?
+                          @shadow_info.sp_expire
+                        else
+                          Time.at(@shadow_info.sp_expire * 60 * 60 * 24).strftime("%Y-%m-%d")
+                        end
+          current_resource.expire_date(expire_date)
+        end
       end
 
       def define_resource_requirements
@@ -94,6 +106,12 @@ class Chef
           a.failure_message Chef::Exceptions::MissingLibrary, "You must have ruby-shadow installed for password support!"
           a.whyrun "ruby-shadow is not installed. Attempts to set user password will cause failure.  Assuming that this gem will have been previously installed." \
             "Note that user update converge may report false-positive on the basis of mismatched password. "
+        end
+        requirements.assert(:all_actions) do |a|
+          # either neither linux-only value is set, or we need to be on Linux.
+          a.assertion { (!new_resource.expire_date && !new_resource.inactive) || linux? }
+          a.failure_message Chef::Exceptions::User, "Properties expire_date and inactive are not supported by this OS or have not been implemented for this OS yet."
+          a.whyrun "Properties expire_date and inactive are ignored as they are not supported by this OS or have not been implemented yet for this OS"
         end
         requirements.assert(:modify, :lock, :unlock) do |a|
           a.assertion { @user_exists }

lib/chef/resource/user.rb
@@ -72,6 +72,16 @@ class Chef
         description: "The numeric group identifier."
 
       alias_method :group, :gid
+
+      property :expire_date, [ String, NilClass ],
+               description: "(Linux) The date on which the user account will be disabled. The date is specified in the format YYYY-MM-DD.",
+               introduced: "18.0",
+               desired_state: false
+
+      property :inactive, [ String, Integer, NilClass ],
+               description: "(Linux) The number of days after a password expires until the account is permanently disabled. A value of 0 disables the account as soon as the password has expired, and a value of -1 disables the feature.",
+               introduced: "18.0",
+               desired_state: false
     end
   end
 end

commit f4fab1f6ccef9acc59ada9b12adad156ab727fe6

Fixes bug in compare_user on Linux systems
d4c3b8ad111ec7ff6b32ebccf9edd1b6a2f9e8e2 introduced a bug making
`compare_user` on Linux systems return `%i{expire_date inactive}`
instead of wether `@change_desc` is empty.

Signed-off-by: Frederik Thuysbaert <frederik.thuysbaert@combell.group>

lib/chef/provider/user/linux.rb
@@ -29,7 +29,10 @@ class Chef
         end
 
         def compare_user
-          super
+          user_changed = super
+
+          @change_desc ||= []
+
           %i{expire_date inactive}.each do |user_attrib|
             new_val = new_resource.send(user_attrib)
             cur_val = current_resource.send(user_attrib)
@@ -37,6 +40,8 @@ class Chef
               @change_desc << "change #{user_attrib} from #{cur_val} to #{new_val}"
             end
           end
+
+          user_changed || !@change_desc.empty?
         end
 
         def create_user

spec/functional/resource/user/linux_user_spec.rb
@@ -1,0 +1,120 @@
+#
+# Copyright:: Copyright (c) Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "spec_helper"
+require "chef/mixin/shell_out"
+
+metadata = {
+  requires_root: true,
+}
+
+describe "Chef::Resource::User with Chef::Provider::User::LinuxUser provider", metadata do
+  include Chef::Mixin::ShellOut
+
+  def clean_user
+    shell_out!("/usr/sbin/userdel #{username}")
+  rescue Mixlib::ShellOut::ShellCommandFailed
+    # Raised when the user is already cleaned
+  end
+
+  def ensure_file_cache_path_exists
+    path = Chef::Config["file_cache_path"]
+    FileUtils.mkdir_p(path) unless File.directory?(path)
+  end
+
+  def user_should_exist
+    expect(shell_out("grep -q #{username} /etc/passwd").error?).to be(false)
+  end
+
+  def check_password(pass, user)
+    expect(shell_out("grep ^#{user}: /etc/shadow | cut -d: -f2 | grep ^#{pass}$").exitstatus).to eq(0)
+  end
+
+  let(:node) do
+    n = Chef::Node.new
+    n.consume_external_attrs(OHAI_SYSTEM.data.dup, {})
+    n
+  end
+
+  let(:events) do
+    Chef::EventDispatch::Dispatcher.new
+  end
+
+  let(:run_context) do
+    Chef::RunContext.new(node, {}, events)
+  end
+
+  let(:username) do
+    "greatchef"
+  end
+
+  let(:uid) { nil }
+  let(:gid) { 20 }
+  let(:home) { nil }
+  let(:manage_home) { false }
+  let(:password) { "XXXYYYZZZ" }
+  let(:comment) { "Great Chef" }
+  let(:shell) { "/bin/bash" }
+  let(:salt) { nil }
+
+  let(:user_resource) do
+    r = Chef::Resource::User::LinuxUser.new("TEST USER RESOURCE", run_context)
+    r.username(username)
+    r.uid(uid)
+    r.gid(gid)
+    r.home(home)
+    r.shell(shell)
+    r.comment(comment)
+    r.manage_home(manage_home)
+    r.password(password)
+    r.salt(salt)
+    r
+  end
+
+  before do
+    clean_user
+    ensure_file_cache_path_exists
+  end
+
+  after(:each) do
+    clean_user
+  end
+
+  describe "action :create" do
+    it "should create the user" do
+      user_resource.run_action(:create)
+      user_should_exist
+      check_password(password, username)
+    end
+  end
+
+  describe "when user exists" do
+    before do
+      existing_resource = user_resource.dup
+      existing_resource.run_action(:create)
+      user_should_exist
+    end
+
+    describe "when password is updated" do
+      it "should update the password of the user" do
+        user_resource.password("mykitchen")
+        user_resource.run_action(:create)
+        check_password("mykitchen", username)
+      end
+    end
+  end
+end

spec/unit/provider/user/linux_spec.rb
@@ -120,4 +120,49 @@ describe Chef::Provider::User::Linux do
       expect( provider.universal_options ).to eql(["-f", 90])
     end
   end
+
+  describe "compare_user_linux" do
+    before(:each) do
+      @new_resource = Chef::Resource::User::LinuxUser.new("notarealuser")
+      @current_resource = Chef::Resource::User::LinuxUser.new("notarealuser")
+    end
+
+    let(:mapping) do
+      {
+        "username" => %w{notarealuser notarealuser},
+        "comment" => ["Nota Realuser", "Not a Realuser"],
+        "uid" => [1000, 1001],
+        "gid" => [1000, 1001],
+        "home" => ["/home/notarealuser", "/Users/notarealuser"],
+        "shell" => ["/usr/bin/zsh", "/bin/bash"],
+        "password" => %w{abcd 12345},
+      }
+    end
+
+    %w{uid gid comment home shell password}.each do |property|
+      it "should return true if #{property} doesn't match" do
+        @new_resource.send(property, mapping[property][0])
+        @current_resource.send(property, mapping[property][1])
+        expect(provider.compare_user).to eql(true)
+      end
+    end
+
+    %w{uid gid}.each do |property|
+      it "should return false if string #{property} matches fixnum" do
+        @new_resource.send(property, "100")
+        @current_resource.send(property, 100)
+        expect(provider.compare_user).to eql(false)
+      end
+    end
+
+    it "should return false if the objects are identical" do
+      expect(provider.compare_user).to eql(false)
+    end
+
+    it "should ignore differences in trailing slash in home paths" do
+      @new_resource.home "/home/notarealuser"
+      @current_resource.home "/home/notarealuser/"
+      expect(provider.compare_user).to eql(false)
+    end
+  end
 end