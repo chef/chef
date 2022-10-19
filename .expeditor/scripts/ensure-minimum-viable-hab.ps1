acc264465db71ee9741395c97b27db38464cc9ad
John McCrae <john.mccrae@progress.com>
2022-10-19 18:54:15 +0600

    Correcting for Hab not being installed
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -12,7 +12,7 @@ try {
 }
 catch {
     # This install fails if Hab isn't on the path when we check for the version. This ensures it is installed
-    Write-Host "--- :habicat: Installing the version of Habitat required"
+    Write-Host "--- :habicat: Forcing an install of habitat"
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
