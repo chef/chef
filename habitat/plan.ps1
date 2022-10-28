482f5e7ca9d98a3e6a111e86cfab369022eb14f4
John McCrae <john.mccrae@progress.com>
2022-10-28 22:35:31 +0600

    Why is git archive failing? Replacing it with Compress-Archive
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -45,7 +45,8 @@ function Invoke-Download() {
     $git_path += "c:\\Program Files\\Git\\bin"
     try {
         Push-Location (Resolve-Path "$PLAN_CONTEXT/../").Path
-        [System.Diagnostics.Process]::Start("$git_path\\git", "archive --format=zip --output=${HAB_CACHE_SRC_PATH}\\${pkg_filename} HEAD")
+        Compress-Archive -Path ${HAB_CACHE_SRC_PATH}\\${pkg_filename}
+        # [System.Diagnostics.Process]::Start("$git_path\\git", "archive --format=zip --output=${HAB_CACHE_SRC_PATH}\\${pkg_filename} HEAD")
         Start-Sleep -Seconds 30
         # getting an error about the archive being in use, adding the sleep to let other handles on the file finish.
         if (-not $?) { throw "unable to create archive of source" }
