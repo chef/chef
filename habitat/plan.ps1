b6013070a2833e76eca0237b12b043764c6c526f
John McCrae <john.mccrae@progress.com>
2022-10-30 07:57:11 +0600

    Is git even loaded?
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -47,6 +47,8 @@ function Invoke-Download() {
     $env:Path = $git_path + ";" + [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     try {
         Push-Location (Resolve-Path "$PLAN_CONTEXT/../").Path
+        Write-Output "Installing Choco"
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
         choco install gh -y
         choco install git -y 
         refrehsenv
