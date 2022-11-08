c7007dd2b37e8226dc424f0300d3bc0811aa03ff
Thomas Powell <powell@progress.com>
2022-11-08 10:18:11 -0500

    Updated omnibus order

--- omnibus/local_build.ps1
+++ omnibus/local_build.ps1
@@ -1,11 +1,3 @@
-bundle config set --local without development
-bundle install
-pushd ..
-bundle install
-popd
-$env:MSYSTEM = "UCRT64"
-$env:MSYS2_INSTALL_DIR = "C:\Ruby31-x64\msys64"
-
 # omnibus/omnibus.rb looking for x64 or x86 or defaults to x86
 $env:OMNIBUS_WINDOWS_ARCH = "x64"
 $env:OMNIBUS_FIPS_MODE="true"
@@ -14,5 +6,13 @@ $env:PATH="$env:MSYS2_INSTALL_DIR\usr\bin;C:\Program Files\git\bin;$mePath"
 
 $env:OMNIBUS_GITHUB_BRANCH="tp/debug-fips-locally"
 $env:OMNIBUS_SOFTARE_GITHUB_BRANCH="tp/debug-fips-locally"
+
+bundle config set --local without development
+bundle install
+pushd ..
+bundle install
+popd
+$env:MSYSTEM = "UCRT64"
+$env:MSYS2_INSTALL_DIR = "C:\Ruby31-x64\msys64"
 
 bundle exec omnibus build chef
