ba83be2d0878119134ecd7572dc99ee7f80fc9bf
Thomas Powell <powell@progress.com>
2022-11-09 11:46:43 -0500

    Broaden scope of openssl error expectation
    Signed-off-by: Thomas Powell <powell@progress.com>

--- spec/integration/client/fips_spec.rb
+++ spec/integration/client/fips_spec.rb
@@ -1,5 +1,5 @@
 describe "chef-client fips", :windows_only do
    it "Should not error on enabling fips_mode" do
-     expect { OpenSSL.fips_mode = true }.not_to raise_error(OpenSSL::OpenSSLError, /fingerprint/)
+     expect { OpenSSL.fips_mode = true }.not_to raise_error(OpenSSL::OpenSSLError) #, /fingerprint/)
    end
  end
