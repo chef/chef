1d263564dd91534683ef5b5a58b1a0cbb4401c68
Thomas Powell <thomas.powell@progress.com>
2022-11-09 12:36:51 -0800

    Fix the environment variable name for omnibus-software

--- omnibus/local_build.ps1
+++ omnibus/local_build.ps1
@@ -7,7 +7,7 @@ $mePath=$env:PATH
 $env:PATH="C:\Program Files\7-Zip;C:\Ruby31-x64\msys64\usr\bin;C:\Ruby31-x64\msys64\ucrt64\bin;$env:MSYS2_INSTALL_DIR\usr\bin;C:\Program Files\git\bin;$mePath"
 
 $env:OMNIBUS_GITHUB_BRANCH="tp/debug-fips-locally"
-$env:OMNIBUS_SOFTARE_GITHUB_BRANCH="tp/debug-fips-locally"
+$env:OMNIBUS_SOFTWARE_GITHUB_BRANCH="tp/debug-fips-locally"
 
 bundle config set --local without development
 bundle update --conservative omnibus
