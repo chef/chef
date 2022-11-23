4faf2ff3d7bc9dda3c8a76d4f0f86956d4e28781
Thomas Powell <powell@progress.com>
2022-11-23 08:34:06 -0500

    Fix Style/StringLiterals
    Signed-off-by: Thomas Powell <powell@progress.com>

--- spec/integration/client/fips_spec.rb
+++ spec/integration/client/fips_spec.rb
@@ -1,6 +1,6 @@
 describe "chef-client fips" do
   def enable_fips_if_supported
-    OpenSSL.fips_mode = ENV["OMNIBUS_FIPS_MODE"].to_s.downcase == 'true'
+    OpenSSL.fips_mode = ENV["OMNIBUS_FIPS_MODE"].to_s.downcase == "true"
   end
   it "Should not error on enabling fips_mode" do
     expect { enable_fips_if_supported }.not_to raise_error
