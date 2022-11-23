41bf4aa2513a6927605876eb6b02860831a7d109
Thomas Powell <powell@progress.com>
2022-11-23 14:04:16 -0500

    Turn fips on and off

--- spec/integration/client/fips_spec.rb
+++ spec/integration/client/fips_spec.rb
@@ -1,6 +1,14 @@
 describe "chef-client fips" do
   def enable_fips_if_supported
-    OpenSSL.fips_mode = ENV["OMNIBUS_FIPS_MODE"].to_s.downcase == "true"
+    if ENV["OMNIBUS_FIPS_MODE"].to_s.downcase == "true"
+      OpenSSL.fips_mode = true
+      # This is a really bizarre thing, yes, but in my testing
+      # I found that I was getting false negatives on whether
+      # fips_mode was broken due to exiting the process before
+      # whatever cascade of things behind the scenes needed to happen
+      # happened.
+      OpenSSL.fips_mode = false
+    end
   end
   it "Should not error on enabling fips_mode" do
     expect { enable_fips_if_supported }.not_to raise_error
