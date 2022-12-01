bfbb428adea974493ab188668e7ad6aeeec1ce35
Thomas Powell <powell@progress.com>
2022-12-01 12:06:19 -0500

    add more pathing

--- .expeditor/scripts/test_powershells.ps1
+++ .expeditor/scripts/test_powershells.ps1
@@ -1,1 +1,18 @@
+[Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine)
+
+Write-Output "Register Installed Ruby Version 2.7 With Uru"
+Start-Process "C:\Program Files (x86)\Uru\uru_rt.exe" -ArgumentList 'admin add C:\ruby27\bin' -Wait
+# We removed patch version otherwise uru fails to activate ruby as with the latest ruby releases patch version keeps getting updated.
+uru 27
+if (-not $?) { throw "Can't Activate Ruby. Did Uru Registration Succeed?" }
+ruby -v
+if (-not $?) { throw "Can't run Ruby. Is it installed?" }
+
+Write-Output "--- configure winrm"
+winrm quickconfig -q
+
+Write-Output "--- bundle install"
+bundle config set --local without 'omnibus_package'
+bundle install --jobs=3 --retry=3
+$env:PATH = "C:\Program Files (x86)\Uru;" + $env:PATH
 bundle exec ruby /workdir/.expeditor/scripts/more_basic_powershell.rb
