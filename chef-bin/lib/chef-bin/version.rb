7fb1155a6f15be250c7a515b57b55a3c3c306593
Chef Expeditor <chef-ci@chef.io>
2022-12-09 06:25:06 +0000

    Bump version to 18.0.195 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-bin/lib/chef-bin/version.rb
+++ chef-bin/lib/chef-bin/version.rb
@@ -21,7 +21,7 @@
 
 module ChefBin
   CHEFBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "18.0.194".freeze
+  VERSION = "18.0.195".freeze
 end
 
 #
--- knife/lib/chef/knife/version.rb
+++ knife/lib/chef/knife/version.rb
@@ -17,7 +17,7 @@
 class Chef
   class Knife
     KNIFE_ROOT = File.expand_path("../..", __dir__)
-    VERSION = "18.0.194".freeze
+    VERSION = "18.0.195".freeze
   end
 end
 
