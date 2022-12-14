5bfb553a9d23d1e2a3c800d1df93920387f52bcc
John <john.mccrae@progress.com>
2022-12-14 15:24:32 -0800

    Correcting the habitat pipeline issue for Windows. Calling PowerShell from yml files is problematic.
    Signed-off-by: John <john.mccrae@progress.com>

---
+++ .expeditor/scripts/habitat-test.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+
+$EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS = 'chef/chef-infra-client/18.0.179/20221109104144'
+$ScriptRoute = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "ensure-minimum-viable-hab.ps1"))
+& "$ScriptRoute"
+# . ./scripts/ensure-minimum-viable-hab.ps1
+Write-Host "--- Installing $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+hab pkg install $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS
+. ./habitat/tests/test.ps1 -PackageIdentifier $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS
