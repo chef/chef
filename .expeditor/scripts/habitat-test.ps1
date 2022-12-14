96288eb9c132943f927392ce7816edb532874803
Sean Simmons <ssimmons@progress.com>
2022-12-14 16:50:48 -0500

    fixing line
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .expeditor/scripts/habitat-test.ps1
+++ .expeditor/scripts/habitat-test.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 function test-script {
 
     $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS = 'chef/chef-infra-client/18.0.179/20221109104144'
-    Invoke-Command ./ensure-minimum-viable-hab.ps1
+    & ((Split-Path $MyInvocation.InvocationName) + "\ensure-ensure-minimum-viable-hab.ps1")
     Write-Output "################ Testing EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS ##############"
     Write-Host "--- Installing $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS"
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
