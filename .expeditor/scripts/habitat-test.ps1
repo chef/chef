15af79b32ba7537f18c8537e6b594ac633c16ad9
Sean Simmons <ssimmons@progress.com>
2022-12-14 16:40:50 -0500

    adding $
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .expeditor/scripts/habitat-test.ps1
+++ .expeditor/scripts/habitat-test.ps1
@@ -7,7 +7,7 @@ function test-script {
     Write-Output "################ Testing EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS ##############"
     . ./.expeditor/scripts/ensure-minimum-viable-hab.ps1
     Write-Host "--- Installing $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS"
-    env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     hab pkg install $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS
     . ./habitat/tests/test.ps1 -PackageIdentifier $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS
 }
