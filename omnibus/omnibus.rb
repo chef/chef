781a33608108389cf1f79af431d080fef3b5606c
Kanad Kumar <kankumar@progress.com>
2022-12-15 13:58:15 +0530

    AWS_git caching true
    Signed-off-by: Kanad Kumar <kankumar@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -36,7 +36,7 @@ use_git_caching true
 
 # Enable S3 asset caching
 # ------------------------------
-use_s3_caching false
+use_s3_caching true
 s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
 s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
 s3_bucket      "opscode-omnibus-cache"
