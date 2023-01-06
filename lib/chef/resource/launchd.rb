e0dfca865eb5b6f0fb557181cd0d34a5e6d41585
alex.alequin <alex.alequin@uber.com>
2023-01-05 22:48:16 -0500

    launchd - array instead of a hash for Apple's associated_bundle_identifiers key
    Signed-off-by: alex.alequin <alex.alequin@uber.com>

--- lib/chef/resource/launchd.rb
+++ lib/chef/resource/launchd.rb
@@ -129,8 +129,8 @@ class Chef
       property :abandon_process_group, [ TrueClass, FalseClass ],
         description: "If a job dies, all remaining processes with the same process ID may be kept running. Set to true to kill all remaining processes."
 
-      property :associated_bundle_identifiers, Hash,
-        description: "This optional key indicates which bundles the **Login Items Added by Apps** panel associates with the helper executable."
+      property :associated_bundle_identifiers, Array,
+        description: "This optional key indicates which bundles the Login Items Added by Apps panel associates with the helper executable."
 
       property :debug, [ TrueClass, FalseClass ],
         description: "Sets the log mask to `LOG_DEBUG` for this job."
