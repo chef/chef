01d3e4d8fa875cb51c65c2e700082aa71c9aebdd
John <john.mccrae@progress.com>
2023-01-09 11:53:43 -0800

    Rewriting the zypper code to run only on openSUSE 15
    Signed-off-by: John <john.mccrae@progress.com>

---
+++ .expeditor/scripts/zypper_prep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION_STRING=$(cat /etc/os-release | grep '^VERSION=' | sed 's/VERSION=\"\([0-9]\+\).*/\1/')
+if [[ $VERSION_STRING == "15" ]]
+then
+  echo "--- :hammer_and_wrench: Updating Zypper Repos on openSUSE 15"
+  zypper addrepo --check --priority 50 --refresh --name "Chefzypper-repo" "https://mirror.fcix.net/opensuse/distribution/leap/15.3/repo/oss/" "chefzypper"
+  zypper --gpg-auto-import-keys ref
+else
+  echo "--- :hammer_and_wrench: Not Running on openSUSE 15, nothing to do"
+fi
