db362b682924e189cd913421ed1ea4a96093aec5
Jesse Prieur <jesse.prieur@gmail.com>
2023-01-12 11:05:31 -0800

    Removing more-ruby-cleanup; Removing debugging steps

--- .buildkite/verify.pipeline.sh
+++ .buildkite/verify.pipeline.sh
@@ -7,147 +7,147 @@ echo "---"
 echo "steps:"
 echo ""
 
-# test_platforms=("centos-6" "centos-7" "centos-8" "rhel-9" "debian-9" "ubuntu-1604" "sles-15")
+test_platforms=("centos-6" "centos-7" "centos-8" "rhel-9" "debian-9" "ubuntu-1604" "sles-15")
 
-# for platform in ${test_platforms[@]}; do
-#   echo "- label: \"{{matrix}} $platform :ruby:\""
-#   echo "  agents:"
-#   echo "    queue: default-privileged"
-#   echo "  matrix:"
-#   echo "    - \"Unit\""
-#   echo "    - \"Integration\""
-#   echo "    - \"Functional\""
-#   echo "  plugins:"
-#   echo "  - docker#v3.5.0:"
-#   echo "      image: chefes/omnibus-toolchain-${platform#*:}:$OMNIBUS_TOOLCHAIN_VERSION"
-#   echo "      privileged: true"
-#   echo "      environment:"
-#   echo "        - CHEF_FOUNDATION_VERSION"
-#   echo "      propagate-environment: true"
-#   echo "  commands:"
-#   echo "    - .expeditor/scripts/prep_and_run_tests.sh {{matrix}}"
-# done
+for platform in ${test_platforms[@]}; do
+  echo "- label: \"{{matrix}} $platform :ruby:\""
+  echo "  agents:"
+  echo "    queue: default-privileged"
+  echo "  matrix:"
+  echo "    - \"Unit\""
+  echo "    - \"Integration\""
+  echo "    - \"Functional\""
+  echo "  plugins:"
+  echo "  - docker#v3.5.0:"
+  echo "      image: chefes/omnibus-toolchain-${platform#*:}:$OMNIBUS_TOOLCHAIN_VERSION"
+  echo "      privileged: true"
+  echo "      environment:"
+  echo "        - CHEF_FOUNDATION_VERSION"
+  echo "      propagate-environment: true"
+  echo "  commands:"
+  echo "    - .expeditor/scripts/prep_and_run_tests.sh {{matrix}}"
+done
 
-# win_test_platforms=("windows-2019:windows-2019")
+win_test_platforms=("windows-2019:windows-2019")
 
-# for platform in ${win_test_platforms[@]}; do
-#   echo "- label: \"{{matrix}} ${platform#*:} :windows:\""
-#   echo "  agents:"
-#   echo "    queue: default-${platform%:*}-privileged"
-#   echo "  matrix:"
-#   echo "    - \"Unit\""
-#   echo "    - \"Integration\""
-#   echo "  plugins:"
-#   echo "  - docker#v3.5.0:"
-#   echo "      image: chefes/omnibus-toolchain-${platform#*:}:$OMNIBUS_TOOLCHAIN_VERSION"
-#   echo "      shell:"
-#   echo "      - powershell"
-#   echo "      - \"-Command\""
-#   echo "      environment:"
-#   echo "        - CHEF_FOUNDATION_VERSION"
-#   echo "      propagate-environment: true"
-#   echo "  commands:"
-#   echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 {{matrix}}"
+for platform in ${win_test_platforms[@]}; do
+  echo "- label: \"{{matrix}} ${platform#*:} :windows:\""
+  echo "  agents:"
+  echo "    queue: default-${platform%:*}-privileged"
+  echo "  matrix:"
+  echo "    - \"Unit\""
+  echo "    - \"Integration\""
+  echo "  plugins:"
+  echo "  - docker#v3.5.0:"
+  echo "      image: chefes/omnibus-toolchain-${platform#*:}:$OMNIBUS_TOOLCHAIN_VERSION"
+  echo "      shell:"
+  echo "      - powershell"
+  echo "      - \"-Command\""
+  echo "      environment:"
+  echo "        - CHEF_FOUNDATION_VERSION"
+  echo "      propagate-environment: true"
+  echo "  commands:"
+  echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 {{matrix}}"
 
-# done
+done
 
-# for platform in ${win_test_platforms[@]}; do
-#   echo "- label: \"Functional ${platform#*:} :windows:\""
-#   echo "  commands:"
-#   echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 Functional"
-#   echo "  agents:"
-#   echo "    queue: single-use-windows-2019-privileged"
-#   echo "  env:"
-#   echo "  - CHEF_FOUNDATION_VERSION"
-#   echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 {{matrix}}"
-# done
+for platform in ${win_test_platforms[@]}; do
+  echo "- label: \"Functional ${platform#*:} :windows:\""
+  echo "  commands:"
+  echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 Functional"
+  echo "  agents:"
+  echo "    queue: single-use-windows-2019-privileged"
+  echo "  env:"
+  echo "  - CHEF_FOUNDATION_VERSION"
+  echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 {{matrix}}"
+done
 
-# external_gems=("chef-zero" "cheffish" "chefspec" "knife-windows" "berkshelf")
+external_gems=("chef-zero" "cheffish" "chefspec" "knife-windows" "berkshelf")
 
-# for gem in ${external_gems[@]}; do
-#   echo "- label: \"$gem gem :ruby:\""
-#   echo "  agents:"
-#   echo "    queue: default"
-#   echo "  plugins:"
-#   echo "  - docker#v3.5.0:"
-#   echo "      image: chefes/omnibus-toolchain-ubuntu-1804:$OMNIBUS_TOOLCHAIN_VERSION"
-#   echo "      environment:"
-#   echo "        - CHEF_FOUNDATION_VERSION"
-#   if [ $gem == "chef-zero" ] 
-#   then
-#     echo "        - PEDANT_OPTS=--skip-oc_id"
-#     echo "        - CHEF_FS=true"
-#   fi
-#   echo "      propagate-environment: true"
-#   echo "  - chef/cache#v1.5.0:"
-#   echo "      s3_bucket: core-buildkite-cache-chef-oss-prod"
-#   echo "      cached_folders:"
-#   echo "      - vendor"
-#   echo "  commands:"
-#   echo "    - .expeditor/scripts/bk_container_prep.sh"
-#   if [ $gem == "berkshelf" ]
-#   then
-#     echo "    - export PATH=\"/opt/chef/bin:/usr/local/sbin:/usr/sbin:/sbin:${PATH}\""
-#     echo "    - apt-get update -y"
-#     echo "    - apt-get install -y graphviz"
-#     echo "    - bundle config set --local without omnibus_package"
-#   else
-#     echo "    - export PATH=\"/opt/chef/bin:${PATH}\""
-#     echo "    - bundle config set --local without omnibus_package"
-#     echo "    - bundle config set --local path 'vendor/bundle'"
-#   fi
-#   echo "    - bundle install --jobs=3 --retry=3"
-#   case $gem in 
-#     "chef-zero")
-#       echo "    - bundle exec tasks/bin/run_external_test chef/chef-zero main rake pedant"
-#       ;;
-#     "cheffish")
-#       echo "    - bundle exec tasks/bin/run_external_test chef/cheffish main rake spec"
-#       ;;
-#     "chefspec")
-#       echo "    - bundle exec tasks/bin/run_external_test chefspec/chefspec main rake"
-#       ;;
-#     "knife-windows")
-#       echo "    - bundle exec tasks/bin/run_external_test chef/knife-windows main rake spec"
-#       ;;
-#     "berkshelf")
-#       echo "    - bundle exec tasks/bin/run_external_test chef/berkshelf main rake"
-#       ;;
-#     *)
-#       echo -e "\n Gem $gem is not valid\n" >&2
-#       exit 1
-#       ;;
-#   esac
-# done
+for gem in ${external_gems[@]}; do
+  echo "- label: \"$gem gem :ruby:\""
+  echo "  agents:"
+  echo "    queue: default"
+  echo "  plugins:"
+  echo "  - docker#v3.5.0:"
+  echo "      image: chefes/omnibus-toolchain-ubuntu-1804:$OMNIBUS_TOOLCHAIN_VERSION"
+  echo "      environment:"
+  echo "        - CHEF_FOUNDATION_VERSION"
+  if [ $gem == "chef-zero" ] 
+  then
+    echo "        - PEDANT_OPTS=--skip-oc_id"
+    echo "        - CHEF_FS=true"
+  fi
+  echo "      propagate-environment: true"
+  echo "  - chef/cache#v1.5.0:"
+  echo "      s3_bucket: core-buildkite-cache-chef-oss-prod"
+  echo "      cached_folders:"
+  echo "      - vendor"
+  echo "  commands:"
+  echo "    - .expeditor/scripts/bk_container_prep.sh"
+  if [ $gem == "berkshelf" ]
+  then
+    echo "    - export PATH=\"/opt/chef/bin:/usr/local/sbin:/usr/sbin:/sbin:${PATH}\""
+    echo "    - apt-get update -y"
+    echo "    - apt-get install -y graphviz"
+    echo "    - bundle config set --local without omnibus_package"
+  else
+    echo "    - export PATH=\"/opt/chef/bin:${PATH}\""
+    echo "    - bundle config set --local without omnibus_package"
+    echo "    - bundle config set --local path 'vendor/bundle'"
+  fi
+  echo "    - bundle install --jobs=3 --retry=3"
+  case $gem in 
+    "chef-zero")
+      echo "    - bundle exec tasks/bin/run_external_test chef/chef-zero main rake pedant"
+      ;;
+    "cheffish")
+      echo "    - bundle exec tasks/bin/run_external_test chef/cheffish main rake spec"
+      ;;
+    "chefspec")
+      echo "    - bundle exec tasks/bin/run_external_test chefspec/chefspec main rake"
+      ;;
+    "knife-windows")
+      echo "    - bundle exec tasks/bin/run_external_test chef/knife-windows main rake spec"
+      ;;
+    "berkshelf")
+      echo "    - bundle exec tasks/bin/run_external_test chef/berkshelf main rake"
+      ;;
+    *)
+      echo -e "\n Gem $gem is not valid\n" >&2
+      exit 1
+      ;;
+  esac
+done
 
-# habitat_plans=("linux" "linux-kernel2" "windows")
+habitat_plans=("linux" "linux-kernel2" "windows")
 
-# for plan in ${habitat_plans[@]}; do
-#   echo "- label: \":habicat: $plan plan\""
-#   echo "  agents:"
-#   if [ $plan == "windows" ]
-#   then
-#     echo "    queue: single-use-windows-2019-privileged"
-#   else
-#     echo "    queue: single-use-privileged"
-#   fi
-#   echo "  plugins:"
-#   echo "  - chef/cache#v1.5.0:"
-#   echo "      s3_bucket: core-buildkite-cache-chef-oss-prod"
-#   echo "      cached_folders:"
-#   echo "      - vendor"
-#   echo "  commands:"
-#   if [ $plan == "windows" ]
-#   then
-#     echo "    - ./.expeditor/scripts/verify-plan.ps1"
-#   else
-#     echo "    - sudo ./.expeditor/scripts/install-hab.sh 'x86_64-$plan'"
-#     echo "    - sudo ./.expeditor/scripts/verify-plan.sh"
-#   fi
-# done
+for plan in ${habitat_plans[@]}; do
+  echo "- label: \":habicat: $plan plan\""
+  echo "  agents:"
+  if [ $plan == "windows" ]
+  then
+    echo "    queue: single-use-windows-2019-privileged"
+  else
+    echo "    queue: single-use-privileged"
+  fi
+  echo "  plugins:"
+  echo "  - chef/cache#v1.5.0:"
+  echo "      s3_bucket: core-buildkite-cache-chef-oss-prod"
+  echo "      cached_folders:"
+  echo "      - vendor"
+  echo "  commands:"
+  if [ $plan == "windows" ]
+  then
+    echo "    - ./.expeditor/scripts/verify-plan.ps1"
+  else
+    echo "    - sudo ./.expeditor/scripts/install-hab.sh 'x86_64-$plan'"
+    echo "    - sudo ./.expeditor/scripts/verify-plan.sh"
+  fi
+done
 
-# echo "- block: Build & Test Omnibus Packages"
-# echo "  prompt: Continue to run omnibus package build and tests for applicable platforms?"
+echo "- block: Build & Test Omnibus Packages"
+echo "  prompt: Continue to run omnibus package build and tests for applicable platforms?"
 
 omnibus_build_platforms=("centos-6" "centos-7" "centos-8" "rhel-9" "debian-9" "ubuntu-1604" "sles-15")
 
