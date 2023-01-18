2aca64cbda81315cf4648455102c82b25358c8cf
Thomas Powell <powell@progress.com>
2023-01-18 09:13:29 -0500

    Handle error return values
    Signed-off-by: Thomas Powell <powell@progress.com>

--- .expeditor/scripts/habitat-test.ps1
+++ .expeditor/scripts/habitat-test.ps1
@@ -1,11 +1,15 @@
 $ErrorActionPreference = 'Stop'
 
-$EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS=$args[0]
-
 $ScriptRoute = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "ensure-minimum-viable-hab.ps1"))
 & "$ScriptRoute"
 # . ./scripts/ensure-minimum-viable-hab.ps1
 Write-Host "--- Installing $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 hab pkg install $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS
+
+if (-not $?) { throw "Unable to install $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS" }
+
 . ./habitat/tests/test.ps1 -PackageIdentifier $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64WINDOWS
+
+if (-not $?) { throw "Habitat tests failed" }
+
