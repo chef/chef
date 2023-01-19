7a807aa02da45fd74967817010a6a974af1b3307
tpowell-progress <104777878+tpowell-progress@users.noreply.github.com>
2023-01-19 07:14:33 -0800

    Fix Chef 16 Habitat Test pipeline (#13514)
    * refactoring the hab powershell script to ensure habitat gets installed. Backport from chef-18
    * test script for habitat
    * Packaged actions for habitat test into habitat-test.ps1
    * pass package value from yml
    * Handle error return values in habitat-test.ps1
    
    Signed-off-by: Thomas Powell <powell@progress.com>
    Co-authored-by: John <john.mccrae@progress.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -1,9 +1,21 @@
-[Version]$hab_version = (hab --version).split(" ")[1].split("/")[0]
-if ($hab_version -lt [Version]"0.85.0" ) {
+Write-Host "--- :habicat: Verifying that Hab is installed and updating as necessary"
+try {
+    [Version]$hab_version = (hab --version).split(" ")[1].split("/")[0]
+    if ($hab_version -lt [Version]"0.85.0" ) {
+        Write-Host "--- :habicat: Installing the version of Habitat required"
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
+        if (-not $?) { throw "Hab version is older than 0.85 and could not update it." }
+    }
+    else {
+        Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
+    }
+}
+catch {
+    # This install fails if Hab isn't on the path when we check for the version. This ensures it is installed
     Write-Host "--- :habicat: Installing the version of Habitat required"
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
-    if (-not $?) { throw "Hab version is older than 0.85 and could not update it." }
-} else {
-    Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
 }
+Write-Host "--- :habicat: Refreshing the Path"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") + ";C:\ProgramData\Habitat;"
