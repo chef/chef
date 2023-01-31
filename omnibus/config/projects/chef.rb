26d7edaf480a9fbc0438e11c831a6ce6569cd35c
Evan Ahlberg <evanahlberg@gmail.com>
2023-01-31 13:14:12 -0500

    add back in shebang-cleanup
    Signed-off-by: Evan Ahlberg <evanahlberg@gmail.com>

--- omnibus/config/projects/chef.rb
+++ omnibus/config/projects/chef.rb
@@ -58,7 +58,7 @@ dependency "chef-local-source"
 # # FIXME?: might make sense to move dependencies below into the omnibus-software chef
 # #  definition or into a chef-complete definition added to omnibus-software.
 # dependency "gem-permissions"
-dependency "shebang-shecleanup"
+dependency "shebang-cleanup"
 # dependency "version-manifest"
 # dependency "openssl-customization"
 
