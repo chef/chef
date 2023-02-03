98a3e58da9370a178aff8db1ac38d79c2765ce3d
Evan Ahlberg <evanahlberg@gmail.com>
2023-02-03 13:40:21 -0500

    move ruby code back to chef-local-source and skip transitive licensing on chef-foundation
    Signed-off-by: Evan Ahlberg <evanahlberg@gmail.com>

--- omnibus/config/software/chef-foundation.rb
+++ omnibus/config/software/chef-foundation.rb
@@ -6,25 +6,15 @@ license_file "LICENSE"
 # So that Open4/deep_merge/diff-lcs disclaimers are present in Omnibus LICENSES tree.
 license_file "NOTICE"
 
+skip_transitive_dependency_licensing true
+
 source_path = if windows?
                 "c:/opscode/chef"
               else
                  "/opt/chef"
               end
 
-source path: source_path
-
 relative_path "chef-foundation"
-
-# In order to pass notarization we need to sign any binaries and libraries included in the package.
-# This makes sure we include and bins and libs that are brought in by gems.
-ruby_version = `#{source_path}/embedded/bin/ruby -v`
-ruby_version = ruby_version.split(" ")[1][0..2]
-ruby_mmv = "#{ruby_version}.0"
-ruby_dir = "#{install_dir}/embedded/lib/ruby/#{ruby_mmv}"
-gem_dir = "#{install_dir}/embedded/lib/ruby/gems/#{ruby_mmv}"
-bin_dirs bin_dirs.concat ["#{gem_dir}/gems/*/bin/**"]
-lib_dirs ["#{ruby_dir}/**", "#{gem_dir}/extensions/**", "#{gem_dir}/bundler/gems/extensions/**", "#{gem_dir}/bundler/gems/*", "#{gem_dir}/bundler/gems/*/lib/**", "#{gem_dir}/gems/*", "#{gem_dir}/gems/*/lib/**", "#{gem_dir}/gems/*/ext/**"]
 
 build do
   sync "#{project_dir}", "#{install_dir}"
