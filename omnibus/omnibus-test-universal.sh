8365becb028a005c97dc4129a7d44590a9b2c107
Poornima <94897054+poorndm@users.noreply.github.com>
2023-02-14 17:18:14 +0530

    Create omnibus-test-universal.sh

---
+++ omnibus/omnibus-test-universal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -d "/opt/chef" ]; then
+  echo "Chef client installation directory exists."
+else
+  echo "Chef client installation directory does not exist."
+  exit 1
+fi
+
+echo "--- Ensure 'chef-client' command works"
+/opt/chef/bin/chef-client --version
+
+FORK_OWNER="chef"
+REPO_NAME="chef.git"
+TAG_NAME="v18.0.185"
+echo "--- Getting $FORK_OWNER/$REPO_NAME repository cloning ---"
+# git clone -b [tag_name] [repository_url]
+git clone -b  $TAG_NAME https://github.com/$FORK_OWNER/$REPO_NAME
+
+cd chef
+#source ../chef/omnibus/omnibus-test.sh
+#exec "$@"
