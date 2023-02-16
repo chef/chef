dc7cb0a03ad984ea9eceb22345ceca142722b07b
poorndm <poorndm@progress.com>
2023-02-16 16:19:28 +0530

    Add adhoc to test universal-package
    Signed-off-by: poorndm <poorndm@progress.com>

---
+++ omnibus/files/universal-package/creator/create_universal_pkg.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -v
+
+# this is temporary
+echo "${EXPEDITOR_CHANNEL}"
+echo "${EXPEDITOR_VERSION}"
+echo "${EXPEDITOR_PRODUCT_NAME}"
+echo "${EXPEDITOR_PRODUCT_KEY}"
+# end temporary
+
+chef_client_version="18.0.185"
+
+mkdir stage
+
+pushd stage
+
+curl -O https://packages.chef.io/files/current/chef/${chef_client_version}/mac_os_x/11/chef-${chef_client_version}-1.x86_64.dmg
+hdiutil attach -quiet chef-${chef_client_version}-1.x86_64.dmg
+cp /Volumes/Chef\ Infra\ Client/chef-${chef_client_version}-1.x86_64.pkg ./
+hdiutil detach -quiet /Volumes/Chef\ Infra\ Client
+rm chef-${chef_client_version}-1.x86_64.dmg
+
+curl -O https://packages.chef.io/files/current/chef/${chef_client_version}/mac_os_x/11/chef-${chef_client_version}-1.arm64.dmg
+hdiutil attach -quiet chef-${chef_client_version}-1.arm64.dmg
+cp /Volumes/Chef\ Infra\ Client/chef-${chef_client_version}-1.arm64.pkg ./
+hdiutil detach -quiet /Volumes/Chef\ Infra\ Client
+rm chef-${chef_client_version}-1.arm64.dmg
+
+popd
