82de16d6104e50ba5c90ef412f01ec949c84d307
Thomas Powell <powell@progress.com>
2023-02-22 15:03:04 -0500

    Zones file removal
    Signed-off-by: Thomas Powell <powell@progress.com>

--- .expeditor/zones.rb
+++
@@ -1,9 +0,0 @@
-lines=<<-LINES
-  Time.now.localtime.zone
-  "EST"
-LINES
-
-lines.each_line.each_with_index do |line, index|
-  puts "#{line.chomp} #=> \"#{eval line}\"" if index % 2 == 0
-  puts "#{line.chomp}.encoding #=> #{(eval line).encoding}"
-end
