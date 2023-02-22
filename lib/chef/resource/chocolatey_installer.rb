dd40d54b810e5c95399254e652ced0caafd28760
John <john.mccrae@progress.com>
2023-02-22 09:17:13 -0800

    Adding a chocolatey installer
    Signed-off-by: John <john.mccrae@progress.com>

---
+++ lib/chef/resource/chocolatey_installer.rb
@@ -0,0 +1,129 @@
+class Chef
+  class Resource
+    class ChocolateyInstaller < Chef::Resource
+      provides :chocolatey_installer
+
+      description "Use the Chocolatey Installer resource to ensure that Choco is installed to your specification. Use the Chocolatey Feature resource to customize your install"
+      introduced "18.1"
+      examples <<~DOC
+        **Install Chocolatey**
+
+        ```ruby
+        chocolatey_installer 'latest' do
+          action :install
+        end
+        ```
+
+        **Uninstall Chocolatey**
+
+        ```ruby
+        chocolatey_installer 'Some random verbiage' do
+          action :uninstall
+        end
+        ```
+        **Install Chocolatey with Parameters**
+
+        ```ruby
+        chocolatey_installer 'latest' do
+          action :install
+          download_url "https://www.contoso.com/foo"
+          chocolatey_version 2.12.24
+        end
+        ```
+      DOC
+
+      allowed_actions :install, :uninstall
+
+      property :download_url, String,
+        description: "The URL to download Chocolatey from. This defaults to the value of $env:chocolateyDownloadUrl, if it is set, and otherwise falls back to the official Chocolatey community repository to download the Chocolatey package. It can be used for offline installation by providing a path to a Chocolatey.nupkg."
+
+      property :chocolatey_version, String,
+        description: "Specifies a target version of Chocolatey to install. By default, the latest stable version is installed. This will use the value in $env:chocolateyVersion by default, if that environment variable is present. This parameter is ignored if download_url is set."
+
+      property :use_native_unzip, [TrueClass, FalseClass], default: false,
+        description: "If set, uses built-in Windows decompression tools instead of 7zip when unpacking the downloaded nupkg. This will be set by default if use_native_unzip is set to a value other than 'false' or '0'. This parameter will be ignored in PS 5+ in favour of using the Expand-Archive built in PowerShell cmdlet directly."
+
+      property :ignore_proxy, [TrueClass, FalseClass], default: false,
+        description: "If set, ignores any configured proxy. This will override any proxy environment variables or parameters. This will be set by default if ignore_proxy is set to a value other than 'false' or '0'."
+
+      property :proxy_url, String,
+        description: "Specifies the proxy URL to use during the download."
+
+      property :proxy_user, String,
+        description: "The username to use to build a proxy credential with. Will be consumed by the proxy_credential property if both this property and proxy_password are set"
+
+      property :proxy_password, String,
+        description: "The password to use to build a proxy credential with. Will be consumed by the proxy_credential property if both this property and proxy_user are set"
+
+      load_current_value do
+        current_state = fetch_choco_installer
+        current_value_does_not_exist! if current_state == false
+        current_state
+      end
+
+      def fetch_choco_installer
+        ::File.exist?("#{ENV["ALLUSERSPROFILE"]}\\chocolatey\\bin\\choco.exe")
+      end
+
+      action :install, description: "Installs Chocolatey package manager" do
+        unless new_resource.download_url.nil?
+          "Set-Item -path env:chocolateyDownloadUrl -Value #{new_resource.download_url}"
+        end
+
+        unless new_resource.chocolatey_version.nil?
+          "Set-Item -path env:chocolateyVersion -Value #{new_resource.chocolatey_version}"
+        end
+
+        if new_resource.use_native_unzip == true
+          "Set-Item -path env:chocolateyUseWindowsCompression -Value true"
+        end
+
+        if new_resource.ignore_proxy == true
+          "Set-Item -path env:chocolateyIgnoreProxy -Value true"
+        end
+
+        unless new_resource.proxy_url.nil?
+          "Set-Item -path env:chocolateyProxyLocation -Value #{new_resource.proxy_url}"
+        end
+
+        if !new_resource.proxy_user.nil? && !new_resource.proxy_password.nil?
+          "Set-Item -path env:chocolateyProxyUser -Value #{new_resource.proxy_user}; Set-Item -path env:chocolateyProxyPassword -Value #{new_resource.proxy_password}"
+        elsif (!new_resource.proxy_user.nil? && new_resource.proxy_password.nil?) || (new_resource.proxy_user.nil? && !new_resource.proxy_password.nil?)
+          Chef::Log.error("Both a Proxy User and a Proxy Password must be set or neither can be set")
+        end
+
+        converge_if_changed do
+          powershell_code = <<-CODE
+            Set-ExecutionPolicy Bypass -Scope Process -Force;
+            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+            iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+          CODE
+          powershell_exec!(powershell_code).result
+        end
+      end
+
+      action :uninstall, description: "Uninstall Chocolatey package manager" do
+        converge_by("Uinstall Choco") do
+          path = 'c:\programdata\chocolatey\bin' # rubocop:disable Style/StringLiterals
+          powershell_code = <<~CODE
+            Remove-Item $env:ALLUSERSPROFILE\\chocolatey -Recurse -Force
+            [Environment]::SetEnvironmentVariable("ChocolateyLastPathUpdate", $null ,"User")
+            [Environment]::SetEnvironmentVariable("ChocolateyToolsLocation", $null ,"User")
+            [Environment]::SetEnvironmentVariable("ChocolateyInstall", $null ,"Machine")
+            $path = [System.Environment]::GetEnvironmentVariable(
+                'PATH',
+                'Machine'
+            )
+            $path = ($path.Split(';') | Where-Object { $_ -ne #{path} }) -join ";"
+            [System.Environment]::SetEnvironmentVariable(
+                'PATH',
+                $path,
+                'Machine'
+            )
+          CODE
+          powershell_exec!(powershell_code).result
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/resource/chocolatey_installer_spec.rb
@@ -0,0 +1,135 @@
+#
+# Copyright:: Copyright (c) Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "spec_helper"
+
+describe Chef::Resource::ChocolateyInstaller do
+  include RecipeDSLHelper
+
+  let(:resource) { Chef::Resource::ChocolateyInstaller.new("fakey_fakerton") }
+  let(:config) do
+    <<-CONFIG
+  <?xml version="1.0" encoding="utf-8"?>
+  <chocolatey xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <config>
+      <add key="containsLegacyPackageInstalls" value="true" description="Install has packages installed prior to 0.9.9 series." />
+    </config>
+    <sources>
+      <source id="chocolatey" value="https://chocolatey.org/api/v2/" disabled="false" bypassProxy="false" selfService="false" adminOnly="false" priority="0" />
+    </sources>
+    <features>
+      <feature name="checksumFiles" enabled="true" setExplicitly="false" description="Checksum files when pulled in from internet (based on package)." />
+    </features>
+    <apiKeys />
+  </chocolatey>
+    CONFIG
+  end
+
+  # we save off the ENV and set ALLUSERSPROFILE so these specs will work on *nix and non-C drive Windows installs
+  before(:each) do
+    @original_env = ENV.to_hash
+    ENV["ALLUSERSPROFILE"] = "C:\\ProgramData"
+  end
+
+  after(:each) do
+    ENV.clear
+    ENV.update(@original_env)
+  end
+
+  describe "basic chocolatey settings" do
+    it "has a resource name of :chocolatey_installer" do
+      expect(resource.resource_name).to eql(:chocolatey_installer)
+    end
+
+    it "sets the default action as :install" do
+      expect(resource.action).to eql([:install])
+    end
+
+    it "supports :install and :uninstall actions" do
+      expect { resource.action :install }.not_to raise_error
+      expect { resource.action :uninstall }.not_to raise_error
+    end
+  end
+
+  describe "installing chocolatey" do
+    it "can install Chocolatey with parameters" do
+      expect { resource.action :install }.not_to raise_error
+    end
+
+    it "returns false if a chocolatey install cannot be found" do
+      allow(::File).to receive(:exist?).with('C:\ProgramData\chocolatey\bin\choco.exe').and_return(false)
+      expect(resource.fetch_choco_installer).to eql(false)
+    end
+  end
+
+  describe "chocolatey is idempotent because it" do
+    it "does not install choco again if it is already installed" do
+      install_choco
+      chocolatey_installer "install" do
+        action :install
+      end.should_not_be_updated
+    end
+  end
+
+  describe "uinstalling chocolatey" do
+    it "doesn't error out uninstalling chocolatey if chocolatey is not installed" do
+      allow(::File).to receive(:exist?).with('C:\ProgramData\chocolatey\bin\choco.exe').and_return(false)
+      expect { resource.action :uninstall }.not_to raise_error
+    end
+  end
+
+  def install_choco
+    powershell_code = <<-CODE
+      Set-ExecutionPolicy Bypass -Scope Process -Force;
+      [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+      iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    CODE
+    powershell_exec!(powershell_code)
+  end
+
+
+
+    # it "install a single feature" do
+    #   resource.feature_name "snmp"
+    #   expect { resource.action :install }.not_to raise_error
+    # end
+
+  #   it "bypass_proxy property defaults to false" do
+  #     expect { resource.bypass_proxy.to be_false }
+  #   end
+
+  #   describe "#fetch_config_element" do
+  #     it "raises and error if the config file cannot be found" do
+  #       allow(::File).to receive(:exist?).with('C:\ProgramData\chocolatey\config\chocolatey.config').and_return(false)
+  #       expect { resource.fetch_config_element("foo") }.to raise_error(RuntimeError)
+  #     end
+
+  #     it "returns the value if present in the config file" do
+  #       allow(::File).to receive(:exist?).with('C:\ProgramData\chocolatey\config\chocolatey.config').and_return(true)
+  #       allow(::File).to receive(:read).with('C:\ProgramData\chocolatey\config\chocolatey.config').and_return(config)
+  #       expect(resource.fetch_config_element("containsLegacyPackageInstalls")).to eq("true")
+  #       expect { resource.fetch_config_element("foo") }.not_to raise_error
+  #     end
+
+  #     it "returns nil if the element is not present in the config file" do
+  #       allow(::File).to receive(:exist?).with('C:\ProgramData\chocolatey\config\chocolatey.config').and_return(true)
+  #       allow(::File).to receive(:read).with('C:\ProgramData\chocolatey\config\chocolatey.config').and_return(config)
+  #       expect(resource.fetch_config_element("foo")).to be_nil
+  #       expect { resource.fetch_config_element("foo") }.not_to raise_error
+  #     end
+  #   end
+end
