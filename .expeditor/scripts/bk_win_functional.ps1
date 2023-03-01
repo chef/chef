57c75ed3b5788e575a66845f791e6efcb23344c0
John <john.mccrae@progress.com>
2023-02-28 18:14:47 -0800

    Verifying Ruby 3.0 is correctly installed
    Signed-off-by: John <john.mccrae@progress.com>

--- .expeditor/scripts/bk_win_functional.ps1
+++ .expeditor/scripts/bk_win_functional.ps1
@@ -7,14 +7,25 @@ Remove-Item -Path C:\ProgramData\chocolatey\bin\choco.exe -ErrorAction SilentlyC
 
 $ErrorActionPreference = 'Stop'
 
-Write-Output "--- Enable Ruby 3.0"
+Write-Output "--- Enable Ruby 3.0`r"
 
-Write-Output "Register Installed Ruby Version 3.0 With Uru"
-Start-Process "uru_rt.exe" -ArgumentList 'admin add C:\ruby27\bin' -Wait
-uru 30
-if (-not $?) { throw "Can't Activate Ruby. Did Uru Registration Succeed?" }
+Write-Output  "Installing Ruby 3.0 and refreshing the path"
+
+if (-not(Test-Path -Path "C:\ProgramData\chocolatey\bin\choco.exe")){
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('http://internal/odata/repo/ChocolateyInstall.ps1'))
+}
+
+choco install ruby --version=3.0.5.1 --package-parameters="'/InstallDir:C:\ruby30'" -y
+refreshenv
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") +  ";C:\Ruby30\bin"
 ruby -v
-if (-not $?) { throw "Can't run Ruby. Is it installed?" }
+
+# Write-Output "Register Installed Ruby Version 3.0 With Uru"
+# Start-Process "uru_rt.exe" -ArgumentList 'admin add C:\ruby27\bin' -Wait
+# uru 30
+# if (-not $?) { throw "Can't Activate Ruby. Did Uru Registration Succeed?" }
+# ruby -v
+# if (-not $?) { throw "Can't run Ruby. Is it installed?" }
 
 Write-Output "--- configure winrm"
 winrm quickconfig -q
