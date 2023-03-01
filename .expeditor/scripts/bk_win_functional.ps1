de4abd58f1888e1c3018240d2a132f420445593c
John <john.mccrae@progress.com>
2023-03-01 08:11:34 -0800

    Updating Ruby 3.0 support in gemspec et al
    Signed-off-by: John <john.mccrae@progress.com>

--- .expeditor/scripts/bk_win_functional.ps1
+++ .expeditor/scripts/bk_win_functional.ps1
@@ -11,6 +11,8 @@ Write-Output "--- Enable Ruby 3.0`r"
 
 Write-Output  "Installing Ruby 3.0 and refreshing the path"
 
+Remove-Item -path "C:\ProgramData\chocolatey" -Recurse -Force -ErrorAction SilentlyContinue
+
 if (-not(Test-Path -Path "C:\ProgramData\chocolatey\bin\choco.exe")){
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
