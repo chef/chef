789a2fe5df42053faffa452da0ac698d9b3310c2
John <john.mccrae@progress.com>
2023-03-24 13:06:56 -0700

    Moved bash code to file as it wasn't executing properly in yml
    Signed-off-by: John <john.mccrae@progress.com>

--- .expeditor/scripts/habitat-preplinux2.sh
+++ .expeditor/scripts/habitat-preplinux2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64LINUX="chef/chef-infra-client/18.2.0/20230323102435"
+export EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64LINUX2="chef/chef-infra-client/18.2.0/20230323102435"
 sudo ./.expeditor/scripts/install-hab.sh x86_64-linux-kernel2
 echo "--- Installing $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64LINUXKERNEL2"
 sudo hab pkg install $EXPEDITOR_PKG_IDENTS_CHEFINFRACLIENTX86_64LINUXKERNEL2
