206af32ae34d5fb161357df40530a7376b62cf17
Sean Simmons <ssimmons@progress.com>
2023-03-28 12:45:54 -0400

    testing windows builds
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -16,10 +16,10 @@ container_platforms=("amazon-2:centos-7" "centos-6:centos-6" "centos-7:centos-7"
 container_platforms_arm64=("ubuntu-1804-arm:ubuntu-1804" "ubuntu-2004-arm:ubuntu-2004" "ubuntu-2204-arm:ubuntu-2204" "sles-15-arm:sles-12" "centos-7-arm:centos-7" "centos-8-arm:centos-8" "rhel-9-arm:rhel-9" "amazon-2-arm:amazon-2")
 
 # add rest of windows platforms to tests, if not on chef-oss org
-if [ $BUILDKITE_ORGANIZATION_SLUG != "chef-oss" ]
-then
-  container_platforms=( "${container_platforms[@]}" "windows-2012:windows-2019" "windows-2012r2:windows-2019" "windows-2016:windows-2019" "windows-2022:windows-2019" "windows-10:windows-2019" "windows-11:windows-2019" )
-fi
+# if [ $BUILDKITE_ORGANIZATION_SLUG != "chef-oss" ]
+# then
+#   container_platforms=( "${container_platforms[@]}" "windows-2012:windows-2019" "windows-2012r2:windows-2019" "windows-2016:windows-2019" "windows-2022:windows-2019" "windows-10:windows-2019" "windows-11:windows-2019" )
+# fi
 
 # array of all esoteric platforms in the format test-platform:build-platform
 esoteric_platforms=("el-7-ppc64:el-7-ppc64" "el-7-ppc64le:el-7-ppc64le" "el-7-s390x:el-7-s390x" "el-8-s390x:el-7-s390x" "freebsd-12-amd64:freebsd-12-amd64" "freebsd-13-amd64:freebsd-12-amd64" "mac_os_x-10.15-x86_64:mac_os_x-10.15-x86_64" "mac_os_x-11-x86_64:mac_os_x-10.15-x86_64" "mac_os_x-12-x86_64:mac_os_x-10.15-x86_64" "mac_os_x-11-arm64:mac_os_x-11-arm64" "mac_os_x-12-arm64:mac_os_x-11-arm64" "solaris2-5.11-i386:solaris2-5.11-i386" "solaris2-5.11-sparc:solaris2-5.11-sparc" "sles-12-s390x:sles-12-s390x" "sles-15-s390x:sles-12-s390x")
