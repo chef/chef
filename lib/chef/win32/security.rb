16119312661695e60a2ca0e23092947e38b6f61c
Thomas Powell <powell@progress.com>
2023-04-05 12:29:04 -0400

    Guard case in which FFI::Pointer gets to finalizer
    Signed-off-by: Thomas Powell <powell@progress.com>

--- lib/chef/win32/security.rb
+++ lib/chef/win32/security.rb
@@ -721,6 +721,12 @@ class Chef
         unless LogonUserW(username, domain, password, logon_type, logon_provider, token)
           Chef::ReservedNames::Win32::Error.raise!
         end
+
+        # Handle.new(_non handle value_) is not ideal because the finalizer for Handle
+        # was silently failing when comparing the parameter with the return from
+        # `GetCurrentProcess()`. In this case, the value is an FFI::Pointer, but the logic
+        # for Token.new works the same otherwise, so I'm leaving this as is and guarding against
+        # trying to treat an FFI::Pointer as a Win32 handle in the finalizer.
         Token.new(Handle.new(token.read_pointer))
       end
 
