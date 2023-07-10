dfb1cc7fbdf7ce49b8f6c5dc83609de11884b2ca
John <john.mccrae@progress.com>
2023-07-10 19:23:00 +0000

    Updating choco version for kitchen testing
    Signed-off-by: John <john.mccrae@progress.com>

--- .expeditor/scripts/bk_win_prep.ps1
+++ .expeditor/scripts/bk_win_prep.ps1
@@ -15,21 +15,3 @@ bundle config set --local without omnibus_package
 bundle config set --local path 'vendor/bundle'
 bundle install --jobs=3 --retry=3
 if (-not $?) { throw "Unable to install gem dependencies" }
-
-# This is temporary until we get Choco 2.x working great
-function install_choco{
-  Set-ExecutionPolicy Bypass -Scope Process -Force;
-  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-  iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-}
-$result = Invoke-Expression -Command "choco --version"
-if(($null -ne $result ) -and ($result -lt "2.0.0"))
-  {
-    Remove-Item -path $env:ChocolateyInstall -Recurse -Force
-    Remove-Item env:ChocolateyInstall
-    if(Test-Path env:ChocolateyVersion){
-      Remove-Item env:ChocolateyVersion
-    }
-    $env:ChocolateyVersion = "2.1.0"
-    install_choco
-}
--- .expeditor/scripts/prep_and_run_tests.ps1
+++ .expeditor/scripts/prep_and_run_tests.ps1
@@ -3,25 +3,6 @@ param(
     [Parameter(Position=0)][String]$TestType
 )
 
-# This is temporary until we get Choco 2.x working great
-function install_choco{
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-  }
-  $result = Invoke-Expression -Command "choco --version"
-  if(($null -ne $result ) -and ($result -lt "2.0.0"))
-    {
-      Remove-Item -path $env:ChocolateyInstall -Recurse -Force
-      Remove-Item env:ChocolateyInstall
-      if(Test-Path env:ChocolateyVersion){
-        Remove-Item env:ChocolateyVersion
-      }
-      $env:ChocolateyVersion = "2.1.0"
-      install_choco
-  }
-# End Choco 2 changes
-
 . { Invoke-WebRequest -useb https://omnitruck.chef.io/chef/install.ps1 } | Invoke-Expression; install -channel "current" -project "chef-foundation" -v $CHEF_FOUNDATION_VERSION
 $env:Path = 'C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\ProgramData\chocolatey\bin;C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit\;C:\Program Files\Git\cmd;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\WindowsApps;C:\opscode\chef\bin;C:\opscode\chef\embedded\bin'
 
