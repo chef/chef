aa4c0a51fd2298a9a9cfb0d14003705a005c928c
poorndm <poorndm@progress.com>
2023-07-16 11:00:52 +0530

    Test chef-foundation version picked on windows
    Signed-off-by: poorndm <poorndm@progress.com>

--- .expeditor/scripts/omnibus_chef_build.ps1
+++ .expeditor/scripts/omnibus_chef_build.ps1
@@ -28,7 +28,7 @@ $env:ARTIFACTORY_BASE_PATH="com/getchef"
 $env:ARTIFACTORY_ENDPOINT="https://artifactory-internal.ps.chef.co/artifactory"
 $env:ARTIFACTORY_USERNAME="buildkite"
 
-Write-Output "--- Install Chef Foundation"
+Write-Output "--- Install Chef Foundation : ${CHEF_FOUNDATION_VERSION}"
 . { Invoke-WebRequest -useb https://omnitruck.chef.io/chef/install.ps1 } | Invoke-Expression; install -channel "current" -project "chef-foundation" -v $CHEF_FOUNDATION_VERSION
 
 $env:PROJECT_NAME="chef"
