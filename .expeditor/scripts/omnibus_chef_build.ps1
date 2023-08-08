6e6650aff28ec7b546548c106f0305504d0031bb
Poornima <94897054+poorndm@users.noreply.github.com>
2023-08-09 04:23:50 +0530

    Fix  to pick pinned  version of  chef-foundation  on windows (#13842)
    Fix to pick  chef-foundation pinned version on windows
    
    Signed-off-by: poorndm <poorndm@progress.com>
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    Co-authored-by: Sean Simmons <ssimmons@progress.com>

--- .expeditor/scripts/omnibus_chef_build.ps1
+++ .expeditor/scripts/omnibus_chef_build.ps1
@@ -28,8 +28,9 @@ $env:ARTIFACTORY_BASE_PATH="com/getchef"
 $env:ARTIFACTORY_ENDPOINT="https://artifactory-internal.ps.chef.co/artifactory"
 $env:ARTIFACTORY_USERNAME="buildkite"
 
-Write-Output "--- Install Chef Foundation"
-. { Invoke-WebRequest -useb https://omnitruck.chef.io/chef/install.ps1 } | Invoke-Expression; install -channel "current" -project "chef-foundation" -v $CHEF_FOUNDATION_VERSION
+
+Write-Output "--- Installing Chef Foundation ${env:CHEF_FOUNDATION_VERSION}"
+. { Invoke-WebRequest -useb https://omnitruck.chef.io/chef/install.ps1 } | Invoke-Expression; install -channel "current" -project "chef-foundation" -v ${env:CHEF_FOUNDATION_VERSION}
 
 $env:PROJECT_NAME="chef"
 $env:OMNIBUS_PIPELINE_DEFINITION_PATH="${ScriptDir}/../release.omnibus.yml"
