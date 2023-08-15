1a02d18a807b0ce7a0fe0aaf69c3d50b06ca192b
Thomas Powell <thomas.powell@progress.com>
2023-08-15 10:57:22 -0400

    reactivate libarchive tests
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/functional/resource/archive_file_spec.rb
+++ spec/functional/resource/archive_file_spec.rb
@@ -20,7 +20,7 @@ require "tmpdir"
 
 # AIX is broken, see https://github.com/chef/omnibus-software/issues/1566
 # Windows tests are disbled since we'd need libarchive on windows testers in buildkite for PRs
-describe Chef::Resource::ArchiveFile, :not_supported_on_aix, :not_supported_on_windows do
+describe Chef::Resource::ArchiveFile, :libarchive, :not_supported_on_windows do
   include RecipeDSLHelper
 
   let(:tmp_path) { Dir.mktmpdir }
