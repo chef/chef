74c67a31a4069831bf78d41ab79471f27405935b
Sean Simmons <ssimmons@progress.com>
2023-09-08 19:42:38 -0400

    first test with the new cert and on windows
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- omnibus/config/projects/chef.rb
+++ omnibus/config/projects/chef.rb
@@ -73,7 +73,7 @@ package :msi do
   upgrade_code msi_upgrade_code
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
-  signing_identity ENV.fetch("OMNIBUS_SIGNING_IDENTITY", "13B510D1CF1B3467856A064F1BEA12D0884D2528"), machine_store: true
+  signing_identity ENV.fetch("OMNIBUS_SIGNING_IDENTITY", "769E6AF679126F184850AAC7C5C823A80DB3ADAA"), machine_store: true
   parameters ChefLogDllPath: windows_safe_path(gem_path("chef-[0-9]*-x64-mingw-ucrt/ext/win32-eventlog/chef-log.dll")),
              ProjectLocationDir: project_location_dir
 end
