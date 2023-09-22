1b3ff950219238ea0a5aa0f7dac7810fba4e74c1
Thomas Powell <thomas.powell@progress.com>
2023-09-22 16:00:18 -0400

    Initial changes for chef_client_config
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_chef_client_config.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_chef_client_config.rb
@@ -2,6 +2,11 @@ chef_client_config "Create chef-client's client.rb" do
   chef_server_url "https://localhost"
   log_location windows? ? "C:\\chef\\log_test\\client.log" : "/var/log/chef/log_test/client.log"
   chef_license "accept"
+
+  report_handlers  << Chef::Handler::SlowReport.new
+  exception_handlers << Chef::Handler::ErrorReport.new
+  start_handlers     << Chef::Handler::JsonFile.new
+
   ohai_optional_plugins %i{Passwd Lspci Sysctl}
   ohai_disabled_plugins %i{Sessions Interrupts}
   rubygems_url "https://rubygems.org/"
