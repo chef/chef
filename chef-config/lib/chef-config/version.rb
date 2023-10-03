b1057a9365b8a79eb6d6ec80a7e9e48a1d0f0df2
Chef Expeditor <chef-ci@chef.io>
2023-10-03 13:12:40 +0000

    Bump version to 17.10.85 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -15,5 +15,5 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("..", __dir__)
-  VERSION = "17.10.84".freeze
+  VERSION = "17.10.85".freeze
 end
--- chef-utils/lib/chef-utils/version.rb
+++ chef-utils/lib/chef-utils/version.rb
@@ -16,5 +16,5 @@
 
 module ChefUtils
   CHEFUTILS_ROOT = File.expand_path("..", __dir__)
-  VERSION = "17.10.84"
+  VERSION = "17.10.85"
 end
