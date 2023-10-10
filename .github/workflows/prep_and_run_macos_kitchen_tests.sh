0fc4ff15459dd64d4d18d6da70c2d4939fbb8698
Thomas Powell <thomas.powell@progress.com>
2023-10-10 13:04:12 -0400

    Quote gem build
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- .github/workflows/prep_and_run_macos_kitchen_tests.sh
+++ .github/workflows/prep_and_run_macos_kitchen_tests.sh
@@ -6,7 +6,7 @@ sudo rm -f /opt/chef/embedded/bin/{htmldiff,ldiff}
 export BERKSHELF_GEMSPEC=$(find . -name berkshelf.gemspec)
 sudo /opt/chef/embedded/bin/gem build $BERKSHELF_GEMSPEC
 export BERKSHELF_GEM=$(find . -name 'berkshelf*.gem')
-sudo /opt/chef/embedded/bin/gem install $BERKSHELF_GEM
+sudo /opt/chef/embedded/bin/gem install "$BERKSHELF_GEM"
 # sudo /opt/chef/embedded/bin/gem install berkshelf --no-doc
 sudo /opt/chef/embedded/bin/berks vendor cookbooks
 sudo /opt/chef/bin/chef-client -z -o end_to_end --chef-license accept-no-persist
