52b35612d865aa8ec93883265f7103607128b14b
Thomas Powell <thomas.powell@progress.com>
2023-10-10 13:18:15 -0400

    Try to break out array
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- .github/workflows/prep_and_run_macos_kitchen_tests.sh
+++ .github/workflows/prep_and_run_macos_kitchen_tests.sh
@@ -4,7 +4,7 @@ sudo /opt/chef/embedded/bin/bundle config set --local path 'vendor/bundle'
 sudo /opt/chef/embedded/bin/bundle install --jobs=3 --retry=3
 sudo rm -f /opt/chef/embedded/bin/{htmldiff,ldiff}
 export BERKSHELF_GEMSPEC=$(find . -name berkshelf.gemspec)
-cat $BERKSHELF_GEMSPEC
+cat $BERKSHELF_GEMSPEC[1]
 sudo /opt/chef/embedded/bin/gem build "$BERKSHELF_GEMSPEC"
 export BERKSHELF_GEM=$(find . -name 'berkshelf*.gem')
 
