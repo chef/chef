6b4d5eee207e3b1e67a0c705362c5d8af4a9c887
Thomas Powell <thomas.powell@progress.com>
2023-10-10 12:38:43 -0400

    Break up the find and variable name
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- .github/workflows/prep_and_run_macos_kitchen_tests.sh
+++ .github/workflows/prep_and_run_macos_kitchen_tests.sh
@@ -3,8 +3,10 @@ sudo /opt/chef/embedded/bin/bundle config set --local without 'omnibus_package'
 sudo /opt/chef/embedded/bin/bundle config set --local path 'vendor/bundle'
 sudo /opt/chef/embedded/bin/bundle install --jobs=3 --retry=3
 sudo rm -f /opt/chef/embedded/bin/{htmldiff,ldiff}
-sudo /opt/chef/embedded/bin/gem build $(find . -name berkshelf.gemspec)
-sudo /opt/chef/embedded/bin/gem install $(find . -name 'berkshelf*.gem')
+export BERKSHELF_GEMSPEC=$(find . -name berkshelf.gemspec)
+sudo /opt/chef/embedded/bin/gem build $BERKSHELF_GEMSPEC
+export BERKSHELF_GEM=$(find . -name 'berkshelf*.gem')
+sudo /opt/chef/embedded/bin/gem install $BERKSHELF_GEM
 # sudo /opt/chef/embedded/bin/gem install berkshelf --no-doc
 sudo /opt/chef/embedded/bin/berks vendor cookbooks
 sudo /opt/chef/bin/chef-client -z -o end_to_end --chef-license accept-no-persist
