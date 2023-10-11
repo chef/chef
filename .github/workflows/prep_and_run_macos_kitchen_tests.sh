0eee65d07f739ae99c9998314a20ce34bda680e1
Thomas Powell <thomas.powell@progress.com>
2023-10-11 13:24:01 -0400

    Describe the hack
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- .github/workflows/prep_and_run_macos_kitchen_tests.sh
+++ .github/workflows/prep_and_run_macos_kitchen_tests.sh
@@ -1,4 +1,6 @@
 ## this somehow prevents ffi signature issues with macOS on kitchen tests
+# related to https://github.com/ffi/ffi/issues/836 issue with code signature
+
 cd kitchen-tests
 
 sudo /opt/chef/embedded/bin/bundle config set --local without 'omnibus_package'
@@ -6,6 +8,11 @@ sudo /opt/chef/embedded/bin/bundle config set --local path 'vendor/bundle'
 sudo /opt/chef/embedded/bin/bundle install --jobs=3 --retry=3
 sudo rm -f /opt/chef/embedded/bin/{htmldiff,ldiff}
 
+# gem installing berkshelf from rubygems picks up the newest ffi ~> 1.9
+# match which conflicts with what's installed chef current. gem build and
+# gem install from the cached gemspec appears to behave equivalent to bundler
+# in using the ffi already installed (1.15.5 at the time of this writing) instead
+# of installng the latest (1.16.3 at this moment)
 export BERKSHELF_GEMSPEC=$(find . -name 'berkshelf.gemspec')
 pushd $(dirname $BERKSHELF_GEMSPEC)
 sudo /opt/chef/embedded/bin/gem build berkshelf.gemspec
