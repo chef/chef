c40d7cce2eac99670bc04fb6ecbb449dfb36f1f0
William Theaker <williamtheaker@users.noreply.github.com>
2023-11-21 16:16:56 -0500

    Add resource for installing .pkg files on macOS (#13685)
    * Add resource for installing .pkg files on macOS
    
    ---------
    
    Signed-off-by: William Theaker <william.theaker@gusto.com>

---
+++ kitchen-tests/cookbooks/end_to_end/recipes/_macos_pkg.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: end_to_end
+# Recipe:: _macos_pkg
+#
+# Copyright:: Copyright (c) Chef Software Inc.
+#
+
+macos_pkg "osquery" do
+  checksum   "1fea8ac9b603851d2e76c5fc73138a468a3075a3002c8cb1fd7fff53b889c4dd"
+  package_id "io.osquery.agent"
+  source     "https://pkg.osquery.io/darwin/osquery-5.8.2.pkg"
+  action     :install
+end
