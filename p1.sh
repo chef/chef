7023fbe222a7642ee67ee9d0d13f9f045fb8ef3f
poorndm <poorndm@progress.com>
2023-12-04 18:55:01 +0530

    update for config-file
    Signed-off-by: poorndm <poorndm@progress.com>

--- p1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CHEF_FOUNDATION_VERSION=$(cat .buildkite-platform.json | jq -r '.chef_foundation')
-export CHEF_FOUNDATION_VERSION
-echo "Chef Foundation Version: $CHEF_FOUNDATION_VERSION"
