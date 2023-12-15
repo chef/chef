c2e9395a192f8af4b103d9b02b712ac938a0851f
Thomas Powell <Powell@progress.com>
2023-12-15 09:47:01 -0500

    Fix tests and code for OpenSSL 3.x... may need switching on OpenSSL version temporarily
    Signed-off-by: Thomas Powell <Powell@progress.com>

--- lib/chef/mixin/openssl_helper.rb
+++ lib/chef/mixin/openssl_helper.rb
@@ -157,7 +157,9 @@ class Chef
         raise TypeError, "curve must be a string" unless curve.is_a?(String)
         raise ArgumentError, "Specified curve is not available on this system" unless %w{prime256v1 secp384r1 secp521r1}.include?(curve)
 
-        ::OpenSSL::PKey::EC.new(curve).generate_key
+        # https://stackoverflow.com/questions/76252414/pkeys-are-immutable-on-openssl-3-0-e0m-opensslpkeypkeyerror
+        # ::OpenSSL::PKey::EC.new(curve).generate_key
+        key = OpenSSL::PKey::EC.generate(curve)
       end
 
       # generate pem format of the public key given a private key
