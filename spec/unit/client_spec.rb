d0612403268cc8a0a55a78f5bbdd3e6da326e857
John McCrae <john.mccrae@progress.com>
2024-01-09 05:45:27 -0800

    Updating EOL Date
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -313,9 +313,9 @@ describe Chef::Client do
       stub_const("Chef::VERSION", 15)
       # added a call to client because Time.now gets invoked multiple times during instantiation. Don't mock Time until after client initialized
       client
-      expect(Time).to receive(:now).and_return(Time.new(2024, 11, 3, 5))
+      expect(Time).to receive(:now).and_return(Time.new(2024, 12, 1, 5))
       allow(client).to receive(:eol_override).and_return(false)
-      expect(logger).to receive(:warn).with("This release of Chef Infra Client became end of life (EOL) on Nov 01, 2024. Please update to a supported release to receive new features, bug fixes, and security updates.")
+      expect(logger).to receive(:warn).with("This release of Chef Infra Client became end of life (EOL) on Nov 30, 2024. Please update to a supported release to receive new features, bug fixes, and security updates.")
       client.warn_if_eol
     end
 
