7e664bbb06b53f5cadbac32242bb452100723fb7
poorndm <poorndm@progress.com>
2024-01-10 16:30:28 +0530

    test ruby 3.1
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,20 +3,5 @@
 #
 # NOTE: You MUST update omnibus-software when adding new versions of
 # software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
-override "libffi", version: "3.4.2"
-override "libiconv", version: "1.16"
-override "liblzma", version: "5.2.5"
-override "libtool", version: "2.4.2"
-override "libxml2", version: "2.9.10" if windows?
-override "libxslt", version: "1.1.34" if windows?
-override "libyaml", version: "0.1.7"
-override "makedepend", version: "1.0.5"
-override "ncurses", version: "6.3"
-override "nokogiri", version: "1.13.1"
-override "openssl", version: mac_os_x? ? "1.1.1m" : "1.0.2zi"
-override "pkg-config-lite", version: "0.28-1"
-override "ruby", version: "3.0.3"
-override "ruby-windows-devkit-bash", version: "3.1.23-4-msys-1.0.18"
-override "util-macros", version: "1.19.0"
-override "xproto", version: "7.0.28"
-override "zlib", version: "1.2.11"
+#override "ruby", version: "3.0.3"
+override :ruby, version: aix? ? "3.0.3" : "3.1.2
