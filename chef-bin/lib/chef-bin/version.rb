501f8339a0305ccee086aff37f2cb7d381170cfb
Chef Expeditor <chef-ci@chef.io>
2024-01-29 15:44:13 +0000

    Bump version to 17.10.116 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-bin/lib/chef-bin/version.rb
+++ chef-bin/lib/chef-bin/version.rb
@@ -21,7 +21,7 @@
 
 module ChefBin
   CHEFBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "17.10.115".freeze
+  VERSION = "17.10.116".freeze
 end
 
 #
--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -15,5 +15,5 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("..", __dir__)
-  VERSION = "17.10.115".freeze
+  VERSION = "17.10.116".freeze
 end
