854478b16e1cfaa121cc43ff30798055d035cf9b
muthuja <muthujabavaji.vempalli@progress.com>
2024-01-31 14:32:34 +0530

    updated script
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -92,6 +92,7 @@ then
       echo "  plugins:"
       echo "  - docker#v3.5.0:"
       if [[ $platform == *"arm"* ]]; then
+        echo " ************** DEBUGGINIG ********************** Current Platform: $platform"
         echo "      image: chefes/omnibus-toolchain-${platform%????}:$OMNIBUS_TOOLCHAIN_VERSION"
       else
         echo "      image: chefes/omnibus-toolchain-$platform:$OMNIBUS_TOOLCHAIN_VERSION"
