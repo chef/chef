e310a82f07a4762b08723596419213cd2790a46f
Thomas Powell <thomas.powell@progress.com>
2024-02-14 15:37:49 -0500

    test propagation of envs
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -220,6 +220,8 @@ then
   for platform in ${omnibus_test_platforms[@]}; do
     if [[ $platform != *"windows"* ]]; then
       echo "- env:"
+      echo "    IGNORE_ARTIFACTORY_RUBY_PROXY: $IGNORE_ARTIFACTORY_RUBY_PROXY"
+      echo "    IGNORE_CACHE: $IGNORE_CACHE"
       echo "    OMNIBUS_BUILDER_KEY: build-${platform#*:}"
       if [[ $platform == *"arm"* ]]; then
         echo "  label: \":mag::docker::muscle: ${platform%:*}\""
@@ -235,7 +237,7 @@ then
         echo "    queue: docker-linux-arm64"
       else
         echo "    queue: default-privileged"
-      fi      
+      fi
       echo "  plugins:"
       echo "  - docker#v3.5.0:"
       echo "      image: chefes/omnibus-toolchain-${platform%:*}:$OMNIBUS_TOOLCHAIN_VERSION" | sed 's/-arm//'
