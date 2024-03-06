a16f81306e3117c6ac179d7d2c4887158b5ed10a
Thomas Powell <thomas.powell@progress.com>
2024-03-06 07:58:41 -0500

    Misspelling of chocolatey
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_chocolatey.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_chocolatey.rb
@@ -7,14 +7,14 @@ chocolatey_installer 'latest' do
   action :install
 end
 
-chocolate_package 'install non-existent package' do
+chocolatey_package 'install non-existent package' do
   package_name 'gvim'
 end
 
-chocolate_package 'install vim' do
+chocolatey_package 'install vim' do
   package_name 'vim'
 end
 
-chocolate_package 'install the_silver_searcher' do
+chocolatey_package 'install the_silver_searcher' do
   package_name 'ag'
 end
