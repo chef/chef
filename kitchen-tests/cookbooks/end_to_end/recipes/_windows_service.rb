5e84907b4b993b1c1a312a9dda07f6615bc7f9e0
Thomas Powell <thomas.powell@progress.com>
2024-04-10 17:11:06 -0400

    Windows Service description validation
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

---
+++ kitchen-tests/cookbooks/end_to_end/recipes/_windows_service.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: windows_service
+# Recipe:: _windows_service.rb
+#
+# Copyright:: Copyright (c) Chef Software Inc.
+#
+
+windows_service 'chef-client' do
+  action :create
+  binary_path_name "c:/opscode/chef/bin"
+  description "Test description 2"
+  startup_type :manual
+end
