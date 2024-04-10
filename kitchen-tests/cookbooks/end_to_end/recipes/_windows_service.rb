d837b0ed540f212c73fce124eff28dde71cdc972
Thomas Powell <thomas.powell@progress.com>
2024-04-10 18:28:35 -0400

    lint
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_windows_service.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_windows_service.rb
@@ -5,7 +5,7 @@
 # Copyright:: Copyright (c) Chef Software Inc.
 #
 
-windows_service 'chef-client' do
+windows_service "chef-client" do
   action :create
   binary_path_name "c:/opscode/chef/bin"
   description "Test description 2"
