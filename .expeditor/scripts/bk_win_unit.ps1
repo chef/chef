89de8cf899282379f201d75517a19c9b66378d1c
John McCrae <john.mccrae@progress.com>
2024-06-05 11:41:10 -0700

    Updating vault
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/scripts/bk_win_unit.ps1
+++ .expeditor/scripts/bk_win_unit.ps1
@@ -2,6 +2,12 @@ $CurrentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $PrepScript = Join-Path $CurrentDirectory "bk_win_prep.ps1"
 Invoke-Expression $PrepScript
 
+echo "+++ Ensuring Chef-PowerShell is installed"
+$is_chef_powershell_installed = gem list chef-powershell
+if (-not($is_chef_powershell_installed.Contains("18"))){
+    gem install chef-powershell:18.1.0
+}
+
 echo "+++ bundle exec rake"
 bundle exec rake spec:unit
 if (-not $?) { throw "Chef unit tests failing." }
