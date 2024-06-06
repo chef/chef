83150cda41134731f62d0e4dd52abe124f0bb6e7
Sonu Saha <sonu.saha@progress.com>
2024-06-06 17:06:03 +0530

    config: add chef licensing configuration
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ lib/chef/licensing_config.rb
@@ -0,0 +1,10 @@
+require "chef-licensing"
+require_relative "log"
+
+ChefLicensing.configure do |config|
+  config.chef_product_name = "Infra"
+  config.chef_entitlement_id = "a5213d76-181f-4924-adba-4b7ed2b098b5"
+  config.chef_executable_name = "chef-client"
+  config.license_server_url = "https://services.chef.io/licensing"
+  config.logger = Chef::Log
+end
