8b6641240e25b9243be05d3888ffe8a08850ab2b
muthuja <muthujabavaji.vempalli@progress.com>
2024-06-11 20:28:23 +0530

    pin the chef-foundation version to 3.2.2 on aix
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -164,7 +164,12 @@ then
     echo "  plugins:"
     echo "  - chef/omnibus#v0.2.88:"
     echo "      build: chef"
-    echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
+    if [[ $platform == *"aix"* ]]
+    then
+      echo "      chef-foundation-version: 3.2.2"
+    else
+      echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
+    fi
     echo "      config: omnibus/omnibus.rb"
     echo "      install-dir: \"/opt/chef\""
     if [ $build_key == "mac_os_x-11-x86_64" ]
