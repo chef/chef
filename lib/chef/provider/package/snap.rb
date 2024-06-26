0f7d558ca8c8dc4c697ba8769f2e2094a540c2b1
Thomas Heinen <thomasheinen@gmail.com>
2024-06-26 22:37:44 +0200

    Add debugging to snap packages
    Signed-off-by: Thomas Heinen <thomasheinen@gmail.com>

--- lib/chef/provider/package/snap.rb
+++ lib/chef/provider/package/snap.rb
@@ -346,6 +346,7 @@ class Chef
             request["ignore_validation"] = true if options.include?("ignore-validation")
           end
           request["revision"] = revision unless revision.nil?
+          Chef::Log.warn(response.to_json.to_s) # debugging
           request
         end
 
