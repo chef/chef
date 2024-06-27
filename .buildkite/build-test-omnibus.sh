2a7acb618a1018fe513bcd913f45d7bd79c94081
poorndm <poorndm@progress.com>
2024-06-27 16:31:29 +0530

    update omnibus-plugin-version
    Signed-off-by: poorndm <poorndm@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -162,7 +162,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#e25c0ab5a8fa25e9e4e4f8c538945f55c0647ac2:"
+    echo "  - chef/omnibus#ecf3a030b4dcd2ab2e637d1330943d748f350c04:"
     echo "      build: chef"
     echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
     echo "      config: omnibus/omnibus.rb"
@@ -207,7 +207,7 @@ then
   echo "- key: create-build-record"
   echo "  label: \":artifactory: Create Build Record\""
   echo "  plugins:"
-  echo "  - chef/omnibus#e25c0ab5a8fa25e9e4e4f8c538945f55c0647ac2:"
+  echo "  - chef/omnibus#ecf3a030b4dcd2ab2e637d1330943d748f350c04:"
   echo "      create-build-record: chef"
 fi
 
@@ -302,7 +302,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#e25c0ab5a8fa25e9e4e4f8c538945f55c0647ac2:"
+    echo "  - chef/omnibus#ecf3a030b4dcd2ab2e637d1330943d748f350c04:"
     echo "      test: chef"
     echo "      test-path: omnibus/omnibus-test.sh"
     echo "      install-dir: \"/opt/chef\""
@@ -328,6 +328,6 @@ then
   echo "- key: promote"
   echo "  label: \":artifactory: Promote to Current\""
   echo "  plugins:"
-  echo "  - chef/omnibus#e25c0ab5a8fa25e9e4e4f8c538945f55c0647ac2:"
+  echo "  - chef/omnibus#ecf3a030b4dcd2ab2e637d1330943d748f350c04:"
   echo "      promote: chef"
 fi
