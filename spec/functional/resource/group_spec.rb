3c195681899bf8dbad55917a2186cc237b6e4542
John McCrae <john.mccrae@progress.com>
2024-08-08 22:22:48 +0000

    Updating group_spec to filter out AIX on some tests
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- spec/functional/resource/group_spec.rb
+++ spec/functional/resource/group_spec.rb
@@ -441,7 +441,7 @@ describe Chef::Resource::Group, :requires_root_or_running_windows do
       end
     end
 
-    describe "when there is a group", :not_supported_on_macos do
+    describe "when there is a group", :not_supported_on_macos, :not_supported_on_aix do
       it_behaves_like "correct group management"
     end
 
@@ -483,7 +483,7 @@ describe Chef::Resource::Group, :requires_root_or_running_windows do
       end
     end
 
-    describe "when there is a group", :not_supported_on_macos do
+    describe "when there is a group", :not_supported_on_macos, :not_supported_on_aix do
       it_behaves_like "correct group management"
     end
 
