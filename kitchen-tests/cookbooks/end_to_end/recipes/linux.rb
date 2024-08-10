a465eebb58d04a4312383b882645b5b57e2c75f8
John McCrae <john.mccrae@progress.com>
2024-08-10 21:53:44 +0000

    Removing the ntp test for Ubuntu 22.04
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/linux.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/linux.rb
@@ -38,7 +38,7 @@ end
 include_recipe "::_packages"
 include_recipe "::_chef_gem"
 
-include_recipe "ntp" unless fedora? # fedora 34+ doesn't have NTP
+include_recipe "ntp" unless fedora? || (ubuntu_platform? && node[:platform_version] == "22.04") # fedora 34+ doesn't have NTP
 
 resolver_config "/etc/resolv.conf" do
   nameservers [ "8.8.8.8", "8.8.4.4" ]
