0de8e1660c79b7f10b7cc7c971739e970aa32c5c
Thomas Powell <thomas.powell@progress.com>
2024-08-27 20:00:08 -0400

    Clear user privileges before deleting user in Windows
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

---
+++ kitchen-tests/cookbooks/end_to_end/recipes/_windows_user_privilege.rb
@@ -0,0 +1,21 @@
+ # Add a user to the system
+ user "testuser"
+
+ # Update a privilege to include the new user account
+ windows_user_privilege "SeNetworkLogonRight" do
+   privilege "SeNetworkLogonRight"
+   users ["BUILTIN\\Administrators", "NT AUTHORITY\\Authenticated Users", "testuser"]
+   action :set
+ end
+
+ # Remove the added test user
+ user "testuser" do
+   action :remove
+ end
+
+ # Attempt to manage the same privilege and an exception will be raised when the resource attempts to remove `testuser` from the listing
+ windows_user_privilege "SeNetworkLogonRight" do
+   privilege "SeNetworkLogonRight"
+   users ["BUILTIN\\Administrators", "NT AUTHORITY\\Authenticated Users"]
+   action :set
+ end
