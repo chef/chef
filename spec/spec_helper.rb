350c719c6109527634af3bdf30fa7d2321483270
Thomas Powell <thomas.powell@progress.com>
2024-09-10 06:48:05 -0400

    Add HAB_TEST env and non-! shell_out method
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -348,10 +348,7 @@ end
 # Enough stuff needs json serialization that I'm just adding it here for equality asserts
 require "chef/json_compat"
 
-STDERR.puts "<<<<<<<<<<<<<<<<<<<<==hab_test debug-->>>>>>>>>>>>>>>>>>>>"
-STDERR.puts ENV.inspect
-
-if hab_test?
+if ENV["HAB_TEST"] == "true"
   # the chef-bin executables not in the bundle if run through hab
   class Chef
     module Mixin
