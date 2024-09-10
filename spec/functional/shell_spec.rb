df2626273a0a240f500d3d3e10b1695a666b3b7d
Thomas Powell <thomas.powell@progress.com>
2024-09-10 09:08:38 -0400

    Fix ternary syntax
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/functional/shell_spec.rb
+++ spec/functional/shell_spec.rb
@@ -86,7 +86,7 @@ describe Shell do
       ENV["TERM"] = "vt100" if ["", "unknown"].include?(ENV["TERM"].to_s)
 
       config = File.expand_path("shef-config.rb", CHEF_SPEC_DATA)
-      bundler_prefix = hab_test? "" : "bundle exec "
+      bundler_prefix = hab_test? ? "" : "bundle exec "
       reader, writer, pid = PTY.spawn("#{bundler_prefix}#{ChefUtils::Dist::Infra::SHELL} --no-multiline --no-singleline --no-colorize -c #{config} #{options}")
       read_until(reader, "chef (#{Chef::VERSION})>")
       yield reader, writer if block_given?
