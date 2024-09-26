6cd59ec1bf4f77246917525d6b463e05cd7b736c
Sonu Saha <sonu.saha@progress.com>
2024-09-26 13:36:52 +0530

    chore: print some values to debug issue
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -13,18 +13,44 @@ Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
   matches = File.basename(gempath).match(/.*-[A-Fa-f0-9]{12}/)
   next unless matches
 
-  gem_name = File.basename(Dir["#{gempath}/*.gemspec"].first, ".gemspec")
-  # FIXME: should strip any valid ruby platform off of the gem_name if it matches
+  puts "files in gempath: #{Dir["#{gempath}/*"]}"
+  puts "gemspec files in gempath: #{Dir["#{gempath}/*.gemspec"]}"
+
+  # Output the gempath
+  puts "gempath is: #{gempath}"
+
+  # Find the gemspec file recursively in the gempath
+  gemspec_files = Dir["#{gempath}/**/*.gemspec"]
+
+  # Output the files found in gempath
+  puts "Found the following files in gempath:"
+  gemspec_files.each { |file| puts file }
+
+  gem_name = nil
+  # Ensure there is at least one gemspec file found
+  if gemspec_files.any?
+    gemspec_path = gemspec_files.first
+    gem_name = File.basename(gemspec_path, ".gemspec")
+    puts "First gemspec file found: #{gemspec_path}"
+    puts "Gem name is: #{gem_name}"
+  else
+    puts "No gemspec file found in #{gempath}"
+  end
 
   next unless gem_name
 
-  # FIXME: should omit the gem which is in the current directory and not hard code chef
+  # Check for excluded gem names
   next if %w{chef chef-universal-mingw-ucrt proxifier}.include?(gem_name)
 
-  puts "re-installing #{gem_name}..."
+  puts "Re-installing #{gem_name}..."
 
-  Dir.chdir(gempath) do
-    system("gem build #{gem_name}.gemspec") or raise "gem build failed"
+  # Change to the directory where the gemspec is located
+  gemspec_directory = File.dirname(gemspec_files.first)
+
+  Dir.chdir(gemspec_directory) do
+    # Build and install the gem from the correct directory
+    system("gem build #{File.basename(gemspec_path)}") or raise "gem build failed"
     system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
   end
+
 end
