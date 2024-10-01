ee5f6553203f70ed1915a069a9f59e6a727f7c1f
Thomas Powell <thomas.powell@progress.com>
2024-10-01 08:54:34 -0400

    Turn off fips tests in verify
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -348,9 +348,6 @@ end
 # Enough stuff needs json serialization that I'm just adding it here for equality asserts
 require "chef/json_compat"
 
-STDERR.puts "<<<<<<<<<<<<<<<<<<<<==hab_test debug-->>>>>>>>>>>>>>>>>>>>"
-STDERR.puts ENV.inspect
-
 if hab_test?
   # the chef-bin executables not in the bundle if run through hab
   class Chef
