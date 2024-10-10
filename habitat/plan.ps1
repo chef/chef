77c7e3b61c723851aed64a46d07f03e564f24323
John McCrae <john.mccrae@progress.com>
2024-10-10 10:16:42 -0700

    updating to match Neha'
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -35,7 +35,7 @@ function Invoke-SetupEnvironment {
     Push-RuntimeEnv -IsPath GEM_PATH "$pkg_prefix/vendor"
 
     Set-RuntimeEnv APPBUNDLER_ALLOW_RVM "true" # prevent appbundler from clearing out the carefully constructed runtime GEM_PATH
-    Set-RuntimeEnv FORCE_FFI_YAJL "ffi" # default: ext - Always use the C-extensions because we use MRI on all the things and C is fast.
+    Set-RuntimeEnv FORCE_FFI_YAJL "ext" # default: ext - Always use the C-extensions because we use MRI on all the things and C is fast.
     Set-RuntimeEnv -f -IsPath SSL_CERT_FILE "$(Get-HabPackagePath cacerts)/ssl/cert.pem"
     Set-RuntimeEnv LANG "en_US.UTF-8"
     Set-RuntimeEnv LC_CTYPE "en_US.UTF-8"
