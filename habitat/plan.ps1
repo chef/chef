8dbb332a5b777c0d18871a84c2e71ad58607af62
Phani Sajja <sajja.phani@gmail.com>
2024-10-16 18:43:19 +0530

    Fix windows plan (#14655)
    * fix openssl error
    
    Signed-off-by: Phani Sajja <psajja@progress.com>
    
    * remove switch --prefer-local
    
    Signed-off-by: Phani Sajja <psajja@progress.com>
    
    * address review comments
    
    Signed-off-by: Phani Sajja <psajja@progress.com>
    
    * update RUBY_DLL_PATH with openssl
    
    Signed-off-by: Phani Sajja <psajja@progress.com>
    
    * pin openssl to :3.2.0
    
    Signed-off-by: Phani Sajja <psajja@progress.com>
    
    ---------
    
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -35,10 +35,12 @@ function Invoke-SetupEnvironment {
     Push-RuntimeEnv -IsPath GEM_PATH "$pkg_prefix/vendor"
 
     Set-RuntimeEnv APPBUNDLER_ALLOW_RVM "true" # prevent appbundler from clearing out the carefully constructed runtime GEM_PATH
-    Set-RuntimeEnv FORCE_FFI_YAJL "ext" # default: ext - Always use the C-extensions because we use MRI on all the things and C is fast.
+    # Set-RuntimeEnv FORCE_FFI_YAJL "ffi" # default: ext - Always use the C-extensions because we use MRI on all the things and C is fast.
     Set-RuntimeEnv -f -IsPath SSL_CERT_FILE "$(Get-HabPackagePath cacerts)/ssl/cert.pem"
     Set-RuntimeEnv LANG "en_US.UTF-8"
     Set-RuntimeEnv LC_CTYPE "en_US.UTF-8"
+
+    Set-RuntimeEnv -f -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath openssl)/bin;$env:RUBY_DLL_PATH"
 }
 
 function Invoke-Download() {
@@ -78,6 +80,9 @@ function Invoke-Prepare {
         if (-not $?) { throw "unable to configure bundler to restrict gems to be installed" }
         bundle config --local retry 5
         bundle config --local silence_root_warning 1
+        $openssl_dir = "$(Get-HabPackagePath core/openssl)"
+        Write-BuildLine "OpenSSL Dir $openssl_dir"
+        bundle config build.openssl --with-openssl-dir=$openssl_dir
     } finally {
         Pop-Location
     }
@@ -89,6 +94,8 @@ function Invoke-Build {
 
         $env:_BUNDLER_WINDOWS_DLLS_COPIED = "1"
 
+        $openssl_dir = "$(Get-HabPackagePath core/openssl)"
+        gem install openssl:3.2.0 -- --with-openssl-dir=$openssl_dir --with-openssl-include="$openssl_dir/include" --with-openssl-lib="$openssl_dir/lib"
         Write-BuildLine " ** Using bundler to retrieve the Ruby dependencies"
         bundle install --jobs=3 --retry=3
         if (-not $?) { throw "unable to install gem dependencies" }
