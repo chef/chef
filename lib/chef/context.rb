25d8c9b259dc5a3439c865081a0b98ae39d3bc9b
Ashique Saidalavi <Ashique.saidalavi@progress.com>
2024-11-05 20:14:40 +0530

    Hanled the secrets with env variable
    Signed-off-by: Ashique Saidalavi <Ashique.saidalavi@progress.com>

--- lib/chef/context.rb
+++ lib/chef/context.rb
@@ -14,40 +14,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+require_relative "licensing_config"
 
 class Chef
   class Context
     class << self
-      COMMON_KEY = "2f3b66cbbafa2d326b2856bccc4c8ebe".freeze
-      FILE_NAME = "c769508738d671db424b7442".freeze
-
       def test_kitchen_context?
         return @context if defined?(@context)
 
         @context = false
-        file_path = (ChefUtils.windows? ? Dir.tmpdir : "/tmp") + "/kitchen/#{FILE_NAME}"
-        if File.exist?(file_path)
-          file_content = {}
-          File.open(file_path, "r:bom|utf-16le:utf-8") do |file|
-            file.each_line do |line|
-              key, value = line.strip.split(":")
-              file_content[key] = value
-            end
-          end
+        return @context if context_secret.nil? || context_secret.empty?
 
-          received_nonce = file_content["nonce"]
-          received_timestamp = file_content["timestamp"]
-          received_signature = file_content["signature"]
-
+        if File.exist?(signed_file_path)
+          received_nonce, received_timestamp, received_signature = read_file_content
           current_time = Time.now.utc.to_i
           if (current_time - received_timestamp.to_i).abs < 30
-            message = "#{received_nonce}:#{received_timestamp}"
-            expected_signature = OpenSSL::HMAC.hexdigest("SHA256", COMMON_KEY, message)
-            if expected_signature == received_signature
+            if expected_signature(received_nonce, received_timestamp) == received_signature
               @context = true
             end
+
           end
-          File.delete(TMP_FILE_PATH)
+          File.delete(signed_file_path)
         end
 
         @context
@@ -57,8 +44,35 @@ class Chef
         puts "Running under Test-Kitchen: Switching License to Chef Workstation entitlement"
         ChefLicensing.configure do |config|
           # Reset entitlement ID to the ID of Chef Workstation
-          config.chef_entitlement_id = "x6f3bc76-a94f-4b6c-bc97-4b7ed2b045c0"
+          config.chef_entitlement_id = Chef::LicensingConfig::WORKSTATION_ENTITLEMENT_ID
         end
+      end
+
+      private
+
+      def context_secret
+        ENV.fetch("TEST_KITCHEN_CONTEXT", "")
+      end
+
+      def signed_file_path
+        "#{Dir.tmpdir}/kitchen/#{context_secret}"
+      end
+
+      def read_file_content
+        file_content = {}
+        File.open(signed_file_path, "r:bom|utf-16le:utf-8") do |file|
+          file.each_line do |line|
+            key, value = line.strip.split(":")
+            file_content[key] = value
+          end
+        end
+
+        [file_content["nonce"], file_content["timestamp"], file_content["signature"]]
+      end
+
+      def expected_signature(nonce, timestamp)
+        message = "#{nonce}:#{timestamp}"
+        OpenSSL::HMAC.hexdigest("SHA256", context_secret, message)
       end
     end
   end
---
+++ spec/unit/context_spec.rb
@@ -0,0 +1,48 @@
+#
+# Copyright:: Copyright (c) Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "spec_helper"
+require "chef/context"
+require "openssl"
+
+describe Chef::Context do
+  context "when executed from test kitchen" do
+    let (:context_key) { "key-123" }
+
+    before(:each) do
+      allow(ENV).to receive(:fetch).with("TEST_KITCHEN_CONTEXT", "").and_return(context_key)
+
+      # Mock the signed file content
+      nonce = Base64.encode64(SecureRandom.random_bytes(16)).strip
+      timestamp = Time.now.utc.to_i
+      signature = OpenSSL::HMAC.hexdigest('SHA256', context_key, "#{nonce}:#{timestamp}")
+
+      file_data = "nonce:#{nonce}\ntimestamp:#{timestamp}\nsignature:#{signature}"
+      allow(File).to receive(:exist?).with(described_class.send(:signed_file_path)).and_return(true)
+      allow(File).to receive(:open).with(described_class.send(:signed_file_path), "r:bom|utf-16le:utf-8").and_yield(StringIO.new(file_data))
+      allow(File).to receive(:delete).with(described_class.send(:signed_file_path)).and_return(true)
+    end
+
+    it "#context_secret should return the context key" do
+      expect(described_class.send(:context_secret)).to eq(context_key)
+    end
+
+    it "#test_kitchen_context? should return true" do
+      expect(described_class.test_kitchen_context?).to eq(true)
+    end
+  end
+end
