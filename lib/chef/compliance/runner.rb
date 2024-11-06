efd733e36e9f918b903a1b2678b78dd05c08d3b0
Neha Pansare <neha.pansare@progress.com>
2024-11-06 20:16:33 +0530

    Require inspec instead lazy load to get past license validation failures
    Signed-off-by: Neha Pansare <neha.pansare@progress.com>

--- lib/chef/compliance/runner.rb
+++ lib/chef/compliance/runner.rb
@@ -1,4 +1,9 @@
-autoload :Inspec, "inspec"
+# Requiring inspec instead of lazy loading temporarily to get past License validation failures in Inspec 6.
+# When compliance phase of Infra Client is run, the lazy loading ends up calling production environment values for few variables(such as Licensing server URL)
+# which are set as default in Inspec, instead of the test values passed in pipelines
+# 
+#autoload :Inspec, "inspec"
+require "inspec"
 
 require_relative "default_attributes"
 
