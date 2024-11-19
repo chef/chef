d02d7e162c0c68d964692c746dcf8a8346df3edd
Thomas Powell <thomas.powell@progress.com>
2024-11-19 11:54:53 -0500

    skip openssl version check on verify pipeline
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/integration/client/open_ssl_spec.rb
+++ spec/integration/client/open_ssl_spec.rb
@@ -11,7 +11,7 @@ describe "openssl checks" do
 
   %w{version library_version}.each do |method|
     # macOS just picks up its own for some reason, maybe it circumvents a build step
-    example "check #{method}", not_supported_on_macos: true do
+    example "check #{method}", not_supported_on_macos: true, openssl_version_check: true do
       expect(OpenSSL.const_get("OPENSSL_#{method.upcase}")).to match(openssl_version_default), "OpenSSL doesn't match omnibus_overrides.rb"
     end
   end
