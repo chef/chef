4f4ca8cf4c2281cb5d8e33eee050234cc6006221
John <john.mccrae@progress.com>
2024-11-20 00:48:18 +0000

    What is going on with Windows bit masks and ACE's
    Signed-off-by: John <john.mccrae@progress.com>

--- spec/support/shared/functional/securable_resource.rb
+++ spec/support/shared/functional/securable_resource.rb
@@ -642,7 +642,23 @@ shared_examples_for "a securable resource without existing target" do
       # non-inherited ACLs. Filter them out here.
       parent_inherited_acls = parent_acls.dacl.collect(&:inherited?)
 
+      # trace = TracePoint.new(:line) do |tp|
+      #   case tp.path
+      #   when /^</
+      #     puts tp.path
+      #   when /^\(eval\)$/
+      #     puts tp.path
+      #   else
+      #     puts "+ #{File.open(tp.path) { |f| f.each_line.to_a[tp.lineno-1] }}"
+      #   end
+      # end
+      
+      # trace.enable
+      binding.pry
       resource.run_action(:create)
+      # trace.disable
+      # line_handler.disable
+      # Similarly filter out the non-inherited ACLs
 
       # Similarly filter out the non-inherited ACLs
       resource_inherited_acls = descriptor.dacl.collect(&:inherited?)
