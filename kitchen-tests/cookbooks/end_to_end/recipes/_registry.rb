bf1babe127fd124b90a0d4376d60dd5b968deef8
Thomas Powell <thomas.powell@progress.com>
2024-12-09 12:03:31 -0500

    64k string value
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_registry.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_registry.rb
@@ -2,7 +2,7 @@ registry_key 'HKEY_LOCAL_MACHINE\System\RegTest' do
   values [{
     name: 'test',
     type: :string,
-    data: SecureRandom.base64(1024)
+    data: SecureRandom.base64(2**16)
     }]
   action :create
   recursive true
