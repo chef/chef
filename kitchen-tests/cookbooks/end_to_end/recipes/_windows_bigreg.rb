9f38ec136a5f03fd3439fc95bedd99791dcd6cb4
Thomas Powell <thomas.powell@progress.com>
2024-12-19 15:29:15 -0500

    See if I can break the reg issue open
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

---
+++ kitchen-tests/cookbooks/end_to_end/recipes/_windows_bigreg.rb
@@ -0,0 +1,19 @@
+1000.times do |i|
+  registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Notepad\\Pad{i}" do
+    values [{
+      name: "AThing",
+      type: :string,
+      data: "NaN"*1400
+    }]
+  end
+end
+
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Notepad\Pad' do
+  values [{
+    name: 'ProtectionMode',
+    type: :dword,
+    data: '1'
+  }]
+  action :create
+  recursive true
+end
