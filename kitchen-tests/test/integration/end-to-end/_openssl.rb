edd074c49bc49761346c84b67461890af45c79c7
Neha Pansare <neha.pansare@progress.com>
2025-01-13 14:45:38 +0530

    * Enable kitchen tests under GHA. Windows, Mac and linux on vagrant will be tested using legacy test-kitchen(license validation will be skipped) whereas linux docker containers will be tested using test-kitchen-enterprise (license will be validated) 1. Rename kitchen-fips workflow as wndows-fips as it is not a test-kitchen in reality 2. Install chef/chef-cli from unstable channel as there's none in stable 3. Install specific version of gcc and use that to fix bundle install issues at run time 4. Modify OpenSSL integration test being run using test-kitchen so that it picks up habitat openSSL binary on docker images and falls back to omnibus binary path of openssl to support community test-kitchen runs in pipelines on mac, windows and linux VMs (until test-kitchen-enterprise starts supporting those plaforms) 5. Fix unit tests to correctly use habitat binary path of Infra client instead of omnibus path, update specs to stub the method calls 6. Temporarily update linux recipes being tested in test-kitchen pipeline which test resources that have `chef_binary_path` property. Since the default value is now habitat path for infra client binary, vagrant boxes on which these recipes are run will fail since they use omnibus package with community test-kitchen
    * Fetching binary path to Infra client's habitat installation:
    1.Add a new constant in chef-utils to signify hab package name
    2.Create a new path helper which will traverse through the directories to fetch the executable path
    3. Use the path in resources to set the `chef_binary_path` property's default value
    
    Signed-off-by: Neha Pansare <neha.pansare@progress.com>

--- kitchen-tests/test/integration/end-to-end/_openssl.rb
+++ kitchen-tests/test/integration/end-to-end/_openssl.rb
@@ -1,10 +1,29 @@
 # Reference recipes/_openssl.rb test to 'generate and sign a certificate with the CA'
 # This ensures that the generated certificate is valid.
-cmd = if os.windows?
-        "C:\\opscode\\chef\\embedded\\bin\\openssl.exe verify -CAfile C:\\ssl_test\\my_ca.crt C:\\ssl_test\\my_signed_cert.crt"
-      else
-        "/opt/chef/embedded/bin/openssl verify -CAfile /etc/ssl_test/my_ca.crt /etc/ssl_test/my_signed_cert.crt"
-      end
+
+if os.windows?
+  openssl_bin = Dir.glob("C:/hab/pkgs/core/openssl/*/*/bin/openssl.exe").max
+  ca_file = "C:\\ssl_test\\my_ca.crt"
+  cert_file = "C:\\ssl_test\\my_signed_cert.crt"
+
+  # Currently community test kitchen is still used to run kitchen tests on windows, so we need to use omnibus path there
+  # We will to do this until test-kitchen-enterprise supports other platforms and drivers.
+  if !openssl_bin || !File.exist?(openssl_bin)
+    openssl_bin = "C:\\opscode\\chef\\embedded\\bin\\openssl.exe"
+  end
+else
+  openssl_bin = Dir.glob("/hab/pkgs/core/openssl/*/*/bin/openssl").max
+  ca_file = "/etc/ssl_test/my_ca.crt"
+  cert_file = "/etc/ssl_test/my_signed_cert.crt"
+
+  # Currently community test kitchen is still used to run kitchen tests on windows, so we need to use omnibus path there
+  # We will to do this until test-kitchen-enterprise supports other platforms and drivers.
+  if !openssl_bin || !File.exist?(openssl_bin)
+    openssl_bin = "/opt/chef/embedded/bin/openssl"
+  end
+end
+
+cmd = "#{openssl_bin} verify -CAfile #{ca_file} #{cert_file}"
 describe command(cmd) do
   its("stdout") { should match /my_signed_cert.*OK/ }
   its("stderr") { should be_empty }
