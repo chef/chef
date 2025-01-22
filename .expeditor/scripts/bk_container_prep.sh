1ea326faabf6f868c2d4df34ca934d5b3cf9ee99
John McCrae <mccrae@progress.com>
2025-01-22 14:03:53 -0800

    Updating chef to remove foundation during builds
    Signed-off-by: John McCrae <mccrae@progress.com>

--- .expeditor/scripts/bk_container_prep.sh
+++ .expeditor/scripts/bk_container_prep.sh
@@ -10,7 +10,7 @@
 
 # Install Ruby to get the bundler gem.
 echo "--- Ruby Config..."
-sudo apt-get install jq
+sudo apt-get install jq -y
 RUBY_VERSION=$(cat .buildkite-platform.json | jq -r '.ruby_version')
 export RUBY_VERSION
 echo "Proposed Ruby Version is: $RUBY_VERSION"
