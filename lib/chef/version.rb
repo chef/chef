ada1d5a953a4e4cca5c0bee66aa373fcc1ab87f9
Chef Expeditor <chef-ci@chef.io>
2025-01-28 21:42:27 +0000

    Bump version to 19.0.82 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- lib/chef/version.rb
+++ lib/chef/version.rb
@@ -23,7 +23,7 @@ require_relative "version_string"
 
 class Chef
   CHEF_ROOT = File.expand_path("..", __dir__)
-  VERSION = Chef::VersionString.new("19.0.81")
+  VERSION = Chef::VersionString.new("19.0.82")
 end
 
 #
