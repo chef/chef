5c2613c6bd3dde0b9f58a9284b13457d1c380e32
John McCrae <mccrae@progress.com>
2025-02-04 06:24:37 -0800

    where is Inspec now and why isn't it in /embedded/bin?
    Signed-off-by: John McCrae <mccrae@progress.com>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -92,6 +92,11 @@ echo " Is hab actually installed?"
 export MYHAB=$(which hab)
 which hab
 "$MYHAB" pkg list --origin chef
+
+echo " Is Inspec even installed correctly?"
+export MYINSPEC=$(which inspec)
+which inspec
+"$MYINSPEC" --version
 cp "$(pkg_path_for inspec)/bin/inspec" $EMBEDDED_BIN_DIR/inspec
 
 echo " --- Where in the world is Inspec? ---"
