a21545cf665f96c4129c94fb2467e59054ba08d1
poorndm <poorndm@progress.com>
2025-03-03 17:39:33 +0530

    test gem publish modified script to install
    Signed-off-by: poorndm <poorndm@progress.com>

--- .expeditor/scripts/release-gem.sh
+++ .expeditor/scripts/release-gem.sh
@@ -3,7 +3,12 @@ set -x
 set -eou pipefail
 
  sudo yum update -y
- sudo yum install -y ruby-full build-essential 
+sudo yum install gcc
+ gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 3.2
+ruby --version
  gem install bundler --no-document
 echo "Installing dependencies.."
 bundle config set --local without docgen chefstyle development test
