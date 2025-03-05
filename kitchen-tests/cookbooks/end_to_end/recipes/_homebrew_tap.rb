e35c2cb06d1539d481087b7b2af95b0e0cff280b
Thomas Powell <thomas.powell@progress.com>
2025-03-05 13:03:16 -0500

    Pare down the test
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_homebrew_tap.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_homebrew_tap.rb
@@ -5,11 +5,7 @@ homebrew_tap "chef/chef" do
 end
 
 homebrew_tap("microsoft/git") do
-  action [:tap]
-  default_guard_interpreter :default
-  declared_type :homebrew_tap
-  cookbook_name "aces"
-  recipe_name "git_prep"
+  action :tap
   homebrew_path "/usr/local/bin/brew"
   tap_name "microsoft/git"
 end
