091fd46285decb9f18a3e8baf841be54f1df9cb5
Thomas Powell <thomas.powell@progress.com>
2025-03-13 12:28:04 -0400

    merge in empty hash as a default to force a new header hash
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/tiny_server.rb
+++ spec/tiny_server.rb
@@ -173,7 +173,9 @@ module TinyServer
 
     def initialize(response_code = 200, data = nil, headers = nil, &block)
       @response_code, @data = response_code, data
-      @response_headers = headers ? HEADERS.merge(headers) : HEADERS
+      # .merge creates a new hash, headers is sometimes passed as nil, @response_headers gets mutated somewhere
+      # in processing
+      @response_headers = HEADERS.merge(headers || {})
       @block = block_given? ? block : nil
     end
 
