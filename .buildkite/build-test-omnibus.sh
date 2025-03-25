a13b42101cf246d5e2c03af88ff9c44a68a642e9
poorndm <poorndm@progress.com>
2025-03-25 20:34:29 +0530

    modfied the os list
    Signed-off-by: poorndm <poorndm@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -20,7 +20,7 @@ then
 fi
 
 # array of all esoteric platforms in the format test-platform:build-platform
-esoteric_platforms=("aix-7.1-powerpc:aix-7.1-powerpc" "aix-7.2-powerpc:aix-7.1-powerpc" "aix-7.3-powerpc:aix-7.1-powerpc" "el-7-ppc64:el-7-ppc64" "el-7-ppc64le:el-7-ppc64le" "el-7-s390x:el-7-s390x" "el-8-s390x:el-7-s390x" "freebsd-13-amd64:freebsd-13-amd64"  "mac_os_x-14-x86_64:mac_os_x-14-x86_64" "mac_os_x-14-arm64:mac_os_x-14-arm64" "mac_os_x-13-x86_64:mac_os_x-13-x86_64" "mac_os_x-13-arm64:mac_os_x-13-arm64" "mac_os_x-12-x86_64:mac_os_x-12-x86_64" "mac_os_x-12-arm64:mac_os_x-12-arm64" "solaris2-5.11-i386:solaris2-5.11-i386" "solaris2-5.11-sparc:solaris2-5.11-sparc" "sles-12-x86_64:sles-12-x86_64" "sles-12-s390x:sles-12-s390x" "sles-15-s390x:sles-12-s390x")
+esoteric_platforms=("aix-7.1-powerpc:aix-7.1-powerpc" "aix-7.2-powerpc:aix-7.1-powerpc" "aix-7.3-powerpc:aix-7.1-powerpc" "el-7-ppc64:el-7-ppc64" "el-7-ppc64le:el-7-ppc64le" "el-7-s390x:el-7-s390x" "el-8-s390x:el-7-s390x" "freebsd-13-amd64:freebsd-13-amd64" "mac_os_x-14-arm64:mac_os_x-14-arm64" "mac_os_x-13-arm64:mac_os_x-13-arm64" "mac_os_x-12-x86_64:mac_os_x-12-x86_64" "mac_os_x-12-arm64:mac_os_x-12-arm64" "solaris2-5.11-i386:solaris2-5.11-i386" "solaris2-5.11-sparc:solaris2-5.11-sparc" "sles-12-x86_64:sles-12-x86_64" "sles-12-s390x:sles-12-s390x" "sles-15-s390x:sles-12-s390x")
 
 omnibus_build_platforms=()
 omnibus_test_platforms=()
@@ -162,12 +162,12 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#d54ce49f51690b67dcf6e949a1a754e621bd5f2a:"
+    echo "  - chef/omnibus#376104a89d32fe7071e30fd404152946dcaea5fe:"
     echo "      build: chef"
     echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
     echo "      config: omnibus/omnibus.rb"
     echo "      install-dir: \"/opt/chef\""
-    if [ "$build_key" == "mac_os_x-12-x86_64" ] || [ "$build_key" == "mac_os_x-12-arm64" ] || [ "$build_key" == "mac_os_x-13-x86_64" ] || [ "$build_key" == "mac_os_x-13-arm64" ] || [ "$build_key" == "mac_os_x-14-x86_64" ] || [ "$build_key" == "mac_os_x-14-arm64" ]; then
+    if [ "$build_key" == "mac_os_x-12-x86_64" ] || [ "$build_key" == "mac_os_x-12-arm64" ] || [ "$build_key" == "mac_os_x-13-arm64" ] || [ "$build_key" == "mac_os_x-14-arm64" ]; then
         echo "      remote-host: buildkite-omnibus-$platform"
     fi        
     echo "      omnibus-pipeline-definition-path: \".expeditor/release.omnibus.yml\""
@@ -183,11 +183,11 @@ then
     echo "- key: notarize-macos"
     echo "  label: \":lock_with_ink_pen: Notarize macOS Packages\""
     echo "  agents:"
-    [[ $platform == *"arm"* ]] && echo "    queue: omnibus-mac_os_x-12-arm64" || echo "    queue: omnibus-mac_os_x-12-x86_64" || echo "    queue: omnibus-mac_os_x-13-x86_64" || echo "    queue: omnibus-mac_os_x-13-arm64" || echo "    queue: omnibus-mac_os_x-14-x86_64" || echo "    queue: omnibus-mac_os_x-14-arm64"
+    [[ $platform == *"arm"* ]] && echo "    queue: omnibus-mac_os_x-12-arm64" || echo "    queue: omnibus-mac_os_x-12-x86_64" || echo "    queue: omnibus-mac_os_x-13-arm64" || echo "    queue: omnibus-mac_os_x-14-arm64"
     echo "  plugins:"
-    echo "  - chef/omnibus#d54ce49f51690b67dcf6e949a1a754e621bd5f2a:"
+    echo "  - chef/omnibus#376104a89d32fe7071e30fd404152946dcaea5fe:"
     echo "      config: omnibus/omnibus.rb"
-    [[ $platform == *"arm"* ]] && echo "      remote-host: buildkite-omnibus-mac_os_x-12-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-12-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-13-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-13-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-14-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-14-arm64"
+    [[ $platform == *"arm"* ]] && echo "      remote-host: buildkite-omnibus-mac_os_x-12-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-12-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-13-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-14-arm64"
     echo "      notarize-macos-package: chef"
     echo "      omnibus-pipeline-definition-path: \".expeditor/release.omnibus.yml\""
     echo "  depends_on:"
@@ -206,7 +206,7 @@ then
   echo "- key: create-build-record"
   echo "  label: \":artifactory: Create Build Record\""
   echo "  plugins:"
-  echo "  - chef/omnibus#d54ce49f51690b67dcf6e949a1a754e621bd5f2a:"
+  echo "  - chef/omnibus#376104a89d32fe7071e30fd404152946dcaea5fe:"
   echo "      create-build-record: chef"
 fi
 
@@ -295,13 +295,13 @@ then
     fi
     echo "  agents:"
     echo "    queue: omnibus-${platform%:*}"
-    if [ $build_key == "mac_os_x-12-x86_64" ] || [ $build_key == "mac_os_x-12-arm64" ] || [ $build_key == "mac_os_x-13-x86_64" ] || [ $build_key == "mac_os_x-13-arm64" ] || [ $build_key == "mac_os_x-14-x86_64" ] || [ $build_key == "mac_os_x-14-arm64" ]
+    if [ $build_key == "mac_os_x-12-x86_64" ] || [ $build_key == "mac_os_x-12-arm64" ] || [ $build_key == "mac_os_x-13-arm64" ] || [ $build_key == "mac_os_x-14-arm64" ]
     then
       echo "    omnibus: tester"
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#d54ce49f51690b67dcf6e949a1a754e621bd5f2a:"
+    echo "  - chef/omnibus#376104a89d32fe7071e30fd404152946dcaea5fe:"
     echo "      test: chef"
     echo "      test-path: omnibus/omnibus-test.sh"
     echo "      install-dir: \"/opt/chef\""
@@ -327,6 +327,6 @@ then
   echo "- key: promote"
   echo "  label: \":artifactory: Promote to Current\""
   echo "  plugins:"
-  echo "  - chef/omnibus#d54ce49f51690b67dcf6e949a1a754e621bd5f2a:"
+  echo "  - chef/omnibus#376104a89d32fe7071e30fd404152946dcaea5fe:"
   echo "      promote: chef"
 fi
