6dd5b4966a996a1717a1d1f059816479152cc8d6
rishichawda <rishichawda@users.noreply.github.com>
2025-03-28 13:13:50 +0530

    fetch key directly inside script
    this should ideally be in buildkite pre-command hook but since that doesn't seem to working at the moment, we are temporarily going to do this here and then move this once it is resolved. ref: https://github.com/chef/chef/pull/14918
    
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/release-gem.sh
+++ .expeditor/scripts/release-gem.sh
@@ -10,7 +10,10 @@ export HAB_BLDR_CHANNEL="LTS-2024"
 export PROJECT_NAME="chef"
 export ARTIFACTORY_ENDPOINT="https://artifactory-internal.ps.chef.co/artifactory"
 export ARTIFACTORY_USERNAME="buildkite"
-export GEM_HOST_API_KEY="Basic ${ARTIFACTORY_API_KEY}"
+
+lita_password=$(aws ssm get-parameter --name "artifactory-lita-password" --with-decryption --query Parameter.Value --output text --region us-west-2)
+artifactory_api_key=$(echo -n "lita:${lita_password}" | base64)
+export GEM_HOST_API_KEY="Basic ${artifactory_api_key}"
 
 gem install artifactory -v 3.0.17 --no-document
 
