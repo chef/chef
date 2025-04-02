207dc2890a5d6f3b3b72664851e5329a0fa828eb
Thomas Powell <thomas.powell@progress.com>
2025-04-02 16:32:40 -0400

    More explicit path for gem
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- omnibus/config/software/remove-old-gems.rb
+++ omnibus/config/software/remove-old-gems.rb
@@ -5,10 +5,14 @@ skip_transitive_dependency_licensing true
 
 build do
   block "Removing old versions of rexml < 3.3.6" do
+    gemfile = "#{install_dir}/embedded/bin/gem"
+
+    next unless File.exist?(gemfile)
+
     puts "Removing old versions of rexml < 3.3.6"
     env = with_standard_compiler_flags(with_embedded_path)
 
     # remove [-a]ll rexml < 3.3.6 including e[-x]ecutables and [-I]gnore dependencies
-    command "gem uninstall rexml -v '<3.3.6' -a -x -I", env: env
+    command "#{gemfile} uninstall rexml -v '<3.3.6' -a -x -I", env: env
   end
 end
