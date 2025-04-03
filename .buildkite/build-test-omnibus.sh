845709bf652095da43f4cf5c4db113a376a9de96
muthuja <muthujabavaji.vempalli@progress.com>
2025-04-03 15:17:44 +0530

    pick plugin sha
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -162,7 +162,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.89:"
+    echo "  - chef/omnibus#8b40258a1ac1cf51b25dbc06ade3beff2f9c406c:"
     echo "      build: chef"
     echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
     echo "      config: omnibus/omnibus.rb"
@@ -186,7 +186,7 @@ then
     echo "  agents:"
     echo "    queue: omnibus-mac_os_x-12-x86_64"
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.86:"
+    echo "  - chef/omnibus#8b40258a1ac1cf51b25dbc06ade3beff2f9c406c:"
     echo "      config: omnibus/omnibus.rb"
     echo "      remote-host: buildkite-omnibus-mac_os_x-12-x86_64"
     echo "      notarize-macos-package: chef"
@@ -207,7 +207,7 @@ then
   echo "- key: create-build-record"
   echo "  label: \":artifactory: Create Build Record\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.89:"
+  echo "  - chef/omnibus#8b40258a1ac1cf51b25dbc06ade3beff2f9c406c:"
   echo "      create-build-record: chef"
 fi
 
@@ -302,7 +302,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.89:"
+    echo "  - chef/omnibus#8b40258a1ac1cf51b25dbc06ade3beff2f9c406c:"
     echo "      test: chef"
     echo "      test-path: omnibus/omnibus-test.sh"
     echo "      install-dir: \"/opt/chef\""
@@ -328,6 +328,6 @@ then
   echo "- key: promote"
   echo "  label: \":artifactory: Promote to Current\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.89:"
+  echo "  - chef/omnibus#8b40258a1ac1cf51b25dbc06ade3beff2f9c406c:"
   echo "      promote: chef"
 fi
