88c7ad9a8baface5c6cd6625c9d6ec91f5f7256a
Thomas Powell <thomas.powell@progress.com>
2025-06-05 14:05:50 -0400

    lib/openssl.rb as well
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -30,10 +30,8 @@ end
 
 if RUBY_PLATFORM =~ /mswin|mingw|windows/
   puts "Found openssl.rb files in the following gem paths:"
-  Dir["#{gem_home}/**/openssl.rb"].each do |gempath|
+  Dir["#{gem_home}/**/openssl-*/lib/openssl.rb", "#{gem_home}/../../../openssl.rb"].each do |openssl|
     puts gempath
-  end
-  Dir["#{gem_home}/**/openssl-*/lib/openssl.rb"].each do |openssl|
     File.open(openssl, "r+") do |f|
       unpatched_openssl_rb = f.read
       if unpatched_openssl_rb =~ /require\s+['"]ssl_env_hack['"]/
