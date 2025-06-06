41493130d951e691f9ac7306b4be75e5102da945
Thomas Powell <thomas.powell@progress.com>
2025-06-06 08:02:59 -0400

    Observe double call of apt_repository
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_apt.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_apt.rb
@@ -30,3 +30,13 @@ apt_repository "test" do
   signed_by true
   key "https://ftp-master.debian.org/keys/archive-key-12.asc"
 end
+
+apt_repository "test" do
+  uri "http://ftp.be.debian.org/debian/"
+  distribution "bookworm"
+  components %w{main contrib non-free}
+  signed_by true
+  key "https://ftp-master.debian.org/keys/archive-key-12.asc"
+end
+
+
