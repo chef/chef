386cc9983b8c74f836ff832945fca0d9f169dc1f
Thomas Powell <thomas.powell@progress.com>
2025-06-13 09:22:25 -0400

    More debug
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- spec/integration/client/open_ssl_spec.rb
+++ spec/integration/client/open_ssl_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 puts "<<< before require openssl >>>"
 require "openssl"
+puts "openssl defined from"
+p $LOADED_FEATURES.grep(/openssl\.rb/)
 puts "<<< after require openssl >>>"
 
 describe "openssl checks" do
@@ -22,6 +24,7 @@ describe "openssl checks" do
   end
 
   example "check SSL_ENV_HACK", windows_only: true do
+    puts "openssl defined from"
     p $LOADED_FEATURES.grep(/openssl\.rb/)
     expect(::SSL_ENV_HACK).to be_defined, "SSL_ENV_HACK is not defined, did you forget to include the openssl-customization.rb file in your project?"
   end
