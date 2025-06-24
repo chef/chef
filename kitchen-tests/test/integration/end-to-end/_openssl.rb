cf6e57a24f592dd177c73261056eb5a6817879f6
Neha Pansare <neha.pansare@progress.com>
2025-06-24 17:50:14 +0530

    Openssl test: try absolute path inside container
    Signed-off-by: Neha Pansare <neha.pansare@progress.com>

--- kitchen-tests/test/integration/end-to-end/_openssl.rb
+++ kitchen-tests/test/integration/end-to-end/_openssl.rb
@@ -13,30 +13,18 @@ if os.windows?
     openssl_bin = "C:\\opscode\\chef\\embedded\\bin\\openssl.exe"
   end
 else
+  openssl_paths = Dir.glob("/hab/hab/pkgs/core/openssl/*/*/bin/openssl").sort
+  puts "**** openssl paths: #{openssl_paths.inspect} ****"
+  openssl_bin = openssl_paths.last
+
   ca_file = "/etc/ssl_test/my_ca.crt"
   cert_file = "/etc/ssl_test/my_signed_cert.crt"
-  begin
-    # This will work only on dokker containers which use Habitat packages
-    openssl_bin = `hab pkg exec chef/chef-infra-client which openssl`.strip
-    puts "**** Using dynamically resolved OpenSSL: #{openssl_bin} ****"
-  rescue
-    # This is for VMs which still use omnibus packages
-    if !openssl_bin || !File.exist?(openssl_bin)
-      openssl_bin = "/opt/chef/embedded/bin/openssl"
-    end
+
+  # Currently community test kitchen is still used to run kitchen tests on linux VMs, so we need to use omnibus path there
+  # We will to do this until test-kitchen-enterprise supports other platforms and drivers.
+  if !openssl_bin || !File.exist?(openssl_bin)
+    openssl_bin = "/opt/chef/embedded/bin/openssl"
   end
-  # openssl_paths = Dir.glob("/hab/pkgs/core/openssl/*/*/bin/openssl").sort
-  # puts "**** openssl paths: #{openssl_paths.inspect} ****"
-  # openssl_bin = openssl_paths.last
-
-  # ca_file = "/etc/ssl_test/my_ca.crt"
-  # cert_file = "/etc/ssl_test/my_signed_cert.crt"
-
-  # # Currently community test kitchen is still used to run kitchen tests on linux VMs, so we need to use omnibus path there
-  # # We will to do this until test-kitchen-enterprise supports other platforms and drivers.
-  # if !openssl_bin || !File.exist?(openssl_bin)
-  #   openssl_bin = "/opt/chef/embedded/bin/openssl"
-  # end
 end
 
 cmd = "#{openssl_bin} verify -CAfile #{ca_file} #{cert_file}"
