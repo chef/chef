4517f3e521460a7031a02f92b174716926861110
rishichawda <rishichawda@users.noreply.github.com>
2025-07-07 16:40:40 +0530

    set up hab based adhoc pipeline
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

---
+++ .expeditor/scripts/buildkite_adhoc_metadata.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+./.expeditor/scripts/install-hab.sh x86_64-linux
+
+echo "--- Installing Chef Infra Client from unstable channel via Habitat"
+hab pkg install chef/chef-infra-client --channel unstable
+
+echo "--- Setting INFRA_HAB_PKG_IDENT metadata for buildkite agent"
+export PKG_IDENT=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]')
+echo "setting INFRA_HAB_PKG_IDENT to $PKG_IDENT"
+buildkite-agent meta-data set "INFRA_HAB_PKG_IDENT" "$PKG_IDENT"
