93b328a81cfa9f4571b20e0a9e8b7805f213fd91
poorndm <poorndm@progress.com>
2025-07-08 16:35:50 +0530

    modified the script
    Signed-off-by: poorndm <poorndm@progress.com>

--- .buildkite/validate-adhoc-habitat.sh
+++ .buildkite/validate-adhoc-habitat.sh
@@ -1,14 +1,13 @@
-set -e pipefail
+#!/bin/bash
+set -euo pipefail
 
 targets=("amazon-2:centos-7" "centos-7:centos-7" "rhel-9:rhel-9" "debian-9:debian-9" "debian-10:debian-9" "debian-11:debian-9" "ubuntu-2004:ubuntu-2004" "ubuntu-2204:ubuntu-2204" "rocky-8:rocky-8" "rocky-9:rocky-9" "amazon-2023:amazon-2023" "windows-2016:windows-2019" "windows-2022:windows-2019" "windows-10:windows-2019" "windows-11:windows-2019")
-
 arm_targets=("centos-7-arm:centos-7-arm" "amazon-2-arm:amazon-2-arm" "rhel-9-arm:rhel-9-arm" "ubuntu-1804-arm:ubuntu-1804-arm" "ubuntu-2004-arm:ubuntu-2004-arm" "ubuntu-2204-arm:ubuntu-2204-arm" "amazon-2023-arm:amazon-2023-arm")
 
 if [ -n "${ARM_ENABLED:-}" ] && [ "${ARM_ENABLED:-}" = "1" ]; then
   targets+=("${arm_targets[@]}")
 fi
 
-# Start generating YAML
 echo "---"
 echo "env:"
 echo "  BUILD_TIMESTAMP: $(date +'%Y-%m-%d_%H-%M-%S')"
@@ -19,7 +18,7 @@ echo "- label: \":habicat: Fetching latest package identifier.\""
 echo "  commands:"
 echo "    - sudo ./.expeditor/scripts/install-hab.sh x86_64-linux"
 echo "    - sudo hab pkg install chef/chef-infra-client --channel unstable"
-echo "    - export PKG_IDENT=\$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]')"
+echo "    - export PKG_IDENT=\$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+')"
 echo "    - buildkite-agent meta-data set \"INFRA_HAB_PKG_IDENT\" \$PKG_IDENT"
 echo "  expeditor:"
 echo "    executor:"
@@ -31,17 +30,18 @@ echo "        single-use: true"
 echo "- wait: ~"
 
 # Generate steps for each target
-for target in ${targets[@]}; do
-  if [[ "${target%:*}" == *"windows"* ]]; then
-    echo "- label: :hammer_and_wrench::windows:${target%:*}"
+for target in "${targets[@]}"; do
+  build_platform="${target%%:*}"
+  if [[ "${build_platform}" == *"windows"* ]]; then
+    echo "- label: :hammer_and_wrench::windows:${build_platform}"
     echo "  retry:"
     echo "    automatic:"
     echo "      limit: 1"
     echo "  agents:"
-    echo "    queue: default-${target%:*}-privileged"
+    echo "    queue: default-${build_platform}-privileged"
     echo "  plugins:"
     echo "    - docker#v3.5.0:"
-    echo "        image: chefes/omnibus-toolchain-${target%:*}:$OMNIBUS_TOOLCHAIN_VERSION"
+    echo "        image: chefes/omnibus-toolchain-${build_platform}:\$OMNIBUS_TOOLCHAIN_VERSION"
     echo "        shell:"
     echo "          - powershell"
     echo "          - \"-Command\""
@@ -51,29 +51,5 @@ for target in ${targets[@]}; do
     echo "    - ./.expeditor/scripts/validate_adhoc_build.ps1 \$env:PKG_IDENT"
     echo "  timeout_in_minutes: 120"
   else
-    echo "- label: :hammer_and_wrench::docker:${target%:*}"
-    echo "  retry:"
-    echo "    automatic:"
-    echo "      limit: 1"
-    echo "  agents:"
-    if [[ "${target%:*}" == *"arm"* ]]; then
-      echo "    queue: docker-linux-arm64"
-    else
-      echo "    queue: default-privileged"
-    fi
-    echo "  plugins:"
-    echo "    - docker#v3.5.0:"
-    echo "        image: chefes/omnibus-toolchain-${target%:*}:$OMNIBUS_TOOLCHAIN_VERSION"
-    echo "        privileged: true"
-    echo "        propagate-environment: true"
-    echo "  commands:"
-    if [[ "${target%:*}" == *"arm"* ]]; then
-      echo "    - sudo ./.expeditor/scripts/install-hab.sh <arm>"
-    else
-      echo "    - sudo ./.expeditor/scripts/install-hab.sh x86_64-linux"
-    fi
-    echo "    - export PKG_IDENT=\$(buildkite-agent meta-data get \"INFRA_HAB_PKG_IDENT\")"
-    echo "    - sudo ./.expeditor/scripts/validate_adhoc_build.sh \$PKG_IDENT"
-    echo "  timeout_in_minutes: 120"
-  fi
-done
+    echo "- label: :hammer_and_wrench::docker
+
