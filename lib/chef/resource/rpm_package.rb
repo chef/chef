a54dabf95f55262780ab8505840250c33f35e686
Frederik Thuysbaert <frederik.thuysbaert@combell.group>
2025-07-09 09:40:13 +0200

    Fix backport #14868 property introduced version
    Signed-off-by: Frederik Thuysbaert <frederik.thuysbaert@combell.group>

--- lib/chef/resource/rpm_package.rb
+++ lib/chef/resource/rpm_package.rb
@@ -41,7 +41,7 @@ class Chef
         description: "The version of a package to be installed or upgraded."
 
       property :environment, Hash,
-        introduced: "19.0",
+        introduced: "18.8",
         description: "A Hash of environment variables in the form of {'ENV_VARIABLE' => 'VALUE'} to be set before running the command.",
         default: {}, desired_state: false
     end
