680223433ecd6d546972d70540d812bca12287ad
rishichawda <rishichawda@users.noreply.github.com>
2025-07-11 16:16:53 +0530

    fix regex for full package identifier
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/buildkite_adhoc_metadata.sh
+++ .expeditor/scripts/buildkite_adhoc_metadata.sh
@@ -8,6 +8,6 @@ echo "--- Installing Chef Infra Client from unstable channel via Habitat"
 hab pkg install chef/chef-infra-client --channel unstable
 
 echo "--- Setting INFRA_HAB_PKG_IDENT metadata for buildkite agent"
-export PKG_IDENT=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]')
+export PKG_IDENT=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
 echo "setting INFRA_HAB_PKG_IDENT to $PKG_IDENT"
 buildkite-agent meta-data set "INFRA_HAB_PKG_IDENT" "$PKG_IDENT"
