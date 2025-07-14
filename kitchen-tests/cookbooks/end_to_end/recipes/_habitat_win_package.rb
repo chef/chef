b1088c1033929f9a5abe13fb11a383887795ae67
Thomas Powell <104777878+tpowell-progress@users.noreply.github.com>
2025-07-14 08:41:35 -0400

    Use chef/ vs. removed skylerto/ package in hab for tests (#15098)
    * Use chef/ vs. removed skylerto/ package in hab for tests
    * Update sensu-agent-win to chef as well
    
    ---------
    
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_habitat_win_package.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_habitat_win_package.rb
@@ -2,6 +2,6 @@ habitat_install "default" do
   license "accept"
 end
 
-habitat_package "skylerto/splunkforwarder" do
-  version "7.0.3/20180418161444"
+habitat_package "chef/splunkforwarder" do
+  version "7.0.3/20250711082337"
 end
