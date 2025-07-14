1de6b237507703294242e20a5f1ca6c8f75b21a2
rishichawda <rishichawda@users.noreply.github.com>
2025-07-14 11:33:32 +0530

    update openssl version in tests
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/validate_adhoc_build.sh
+++ .expeditor/scripts/validate_adhoc_build.sh
@@ -5,7 +5,7 @@ git config --global --add safe.directory /workdir
 export PKG_IDENT=$(buildkite-agent meta-data get "INFRA_HAB_PKG_IDENT")
 
 echo "--- Installing $PKG_IDENT from unstable channel"
-hab pkg install $PKG_IDENT --channel unstable --binlink
+sudo hab pkg install $PKG_IDENT --channel unstable --binlink
 
 pkg_ident=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
 
