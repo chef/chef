9189baaeee4300c208bad8ec014aedbd2efa87a8
rishichawda <rishichawda@users.noreply.github.com>
2025-07-15 12:49:09 +0530

    disable windows targets
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .buildkite/validate-adhoc-habitat.sh
+++ .buildkite/validate-adhoc-habitat.sh
@@ -1,8 +1,11 @@
 set -e pipefail
 
-targets=("amazon-2:centos-7" "centos-7:centos-7" "rhel-9:rhel-9" "debian-9:debian-9" "debian-10:debian-9" "debian-11:debian-9" "ubuntu-2004:ubuntu-2004" "ubuntu-2204:ubuntu-2204" "rocky-8:rocky-8" "rocky-9:rocky-9" "amazon-2023:amazon-2023" "windows-2016:windows-2019" "windows-2022:windows-2019" "windows-10:windows-2019" "windows-11:windows-2019")
+targets=("amazon-2:centos-7" "centos-7:centos-7" "rhel-9:rhel-9" "debian-9:debian-9" "debian-10:debian-9" "debian-11:debian-9" "ubuntu-2004:ubuntu-2004" "ubuntu-2204:ubuntu-2204" "rocky-8:rocky-8" "rocky-9:rocky-9" "amazon-2023:amazon-2023")
 
 arm_targets=("centos-7-arm:centos-7-arm" "amazon-2-arm:amazon-2-arm" "rhel-9-arm:rhel-9-arm" "ubuntu-1804-arm:ubuntu-1804-arm" "ubuntu-2004-arm:ubuntu-2004-arm" "ubuntu-2204-arm:ubuntu-2204-arm" "amazon-2023-arm:amazon-2023-arm")
+
+# append windows target later when images are available.
+# win_targets=("windows-2016:windows-2019" "windows-2022:windows-2019" "windows-10:windows-2019" "windows-11:windows-2019")
 
 if [ -n "${ARM_ENABLED:-}" ] && [ "${ARM_ENABLED:-}" = "1" ]; then
   targets+=("${arm_targets[@]}")
@@ -26,7 +29,7 @@ for target in ${targets[@]}; do
     echo "    automatic:"
     echo "      limit: 1"
     echo "  agents:"
-    echo "    queue: default-${target%:*}-privileged"
+    echo "    queue: omnibus-${target%:*}-x86_64"
     echo "  plugins:"
     echo "    - docker#v3.5.0:"
     echo "        image: chefes/omnibus-toolchain-${target%:*}:$OMNIBUS_TOOLCHAIN_VERSION"
@@ -35,7 +38,7 @@ for target in ${targets[@]}; do
     echo "          - \"-Command\""
     echo "        propagate-environment: true"
     echo "  commands:"
-    echo "    - sudo -E ./.expeditor/scripts/validate_adhoc_build.ps1"
+    echo "    - ./.expeditor/scripts/validate_adhoc_build.ps1"
     echo "  timeout_in_minutes: 120"
   else
     echo "- label: :hammer_and_wrench::docker:${target%:*}"
