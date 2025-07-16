ee0fa3b8230b740e68b78f15502ec902327c42a6
John McCrae <john.mccrae@progress.com>
2025-07-16 11:32:24 -0700

    Ohai custom plug-ins Chef-11588 (#15090)
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- lib/chef/resource/ohai.rb
+++ lib/chef/resource/ohai.rb
@@ -85,6 +85,16 @@ class Chef
         converge_by("re-run ohai and merge results into node attributes") do
           ohai = ::Ohai::System.new
 
+          # Load any custom plugins from cookbooks if they exist
+          # This ensures that cookbook-provided Ohai plugins are available
+          # when the resource reloads Ohai data
+          ohai_plugin_path = Chef::Config[:ohai_segment_plugin_path]
+          if ohai_plugin_path && Dir.exist?(ohai_plugin_path) && !Dir.empty?(ohai_plugin_path)
+            # Configure Ohai to load plugins from the cookbook segment path
+            ohai.config[:plugin_path] << ohai_plugin_path
+            logger.trace("Added cookbook plugin path to ohai: #{ohai_plugin_path}")
+          end
+
           # If new_resource.plugin is nil, ohai will reload all the plugins
           # Otherwise it will only reload the specified plugin
           # Note that any changes to plugins, or new plugins placed on
