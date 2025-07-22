46051df12289473dc052d0d81342e35e9cc3a346
rishichawda <rishichawda@users.noreply.github.com>
2025-07-22 10:18:36 +0530

    build and upload artifact
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/validate_adhoc_build.sh
+++ .expeditor/scripts/validate_adhoc_build.sh
@@ -1,20 +1,22 @@
 set -e pipefail
 
-echo "--- Checking HAB_AUTH_TOKEN"
-if [ -z "${HAB_AUTH_TOKEN:-}" ]; then
-  echo "HAB_AUTH_TOKEN is not set"
-else
-  echo "HAB_AUTH_TOKEN is set"
-fi
-
 git config --global --add safe.directory /workdir
 
-export PKG_IDENT=$(buildkite-agent meta-data get "INFRA_HAB_PKG_IDENT")
+echo "--- Downloading package artifact"
+export PKG_ARTIFACT=$(buildkite-agent meta-data get "INFRA_HAB_ARTIFACT")
+buildkite-agent artifact download "$PKG_ARTIFACT" .
 
-echo "--- Installing $PKG_IDENT from unstable channel"
-sudo hab pkg install $PKG_IDENT --channel unstable --auth $HAB_AUTH_TOKEN --binlink
+echo ":key: Setting up origin key"
+buildkite-agent artifact download "ci-key.pub" .
+cat ci-key.pub | hab origin key import
+if [ $? -ne 0 ]; then
+  echo "Failed to import origin key"
+  exit 1
+fi
 
-pkg_ident=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
+echo "--- Installing $PKG_ARTIFACT"
+sudo hab pkg install $PKG_ARTIFACT
 
+pkg_ident=$(hab pkg path ci/chef-infra-client | grep -oP 'ci/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
 echo "--- Resolved package identifier: $pkg_ident, attempting to run tests"
 ./habitat/tests/test.sh "$pkg_ident"
