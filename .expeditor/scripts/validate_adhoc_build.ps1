43f88b36d76ec443d57409892b3b718d76dbfe35
rishichawda <rishichawda@users.noreply.github.com>
2025-07-22 10:18:36 +0530

    build and upload artifact
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/validate_adhoc_build.ps1
+++ .expeditor/scripts/validate_adhoc_build.ps1
@@ -4,13 +4,15 @@ git config --global --add safe.directory /workdir
 
 $ScriptRoute = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "ensure-minimum-viable-hab.ps1"))
 & "$ScriptRoute"
-
-$env:PKG_IDENT = $(buildkite-agent meta-data get "INFRA_HAB_PKG_IDENT")
-
-Write-Host "--- Installing latest version of $env:PKG_IDENT from unstable channel"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-hab pkg install $env:PKG_IDENT --channel unstable --auth $HAB_AUTH_TOKEN
-if (-not $?) { throw "Unable to install latest package of $env:PKG_IDENT from unstable channel" }
+
+Write-Host "--- Downloading package artifact"
+$env:PKG_ARTIFACT = $(buildkite-agent meta-data get "INFRA_HAB_ARTIFACT")
+buildkite-agent artifact download "$env:PKG_ARTIFACT" .
+
+Write-Host "--- Installing $env:PKG_ARTIFACT"
+hab pkg install $env:PKG_ARTIFACT
+if (-not $?) { throw "Unable to install $env:PKG_ARTIFACT" }
 
 $pkgPath = hab pkg path chef/chef-infra-client
 $pkgIdent = $pkgPath -match 'chef/chef-infra-client/\d+\.\d+\.\d+/\d+' | Out-Null; $Matches[0]
