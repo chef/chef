316b9d7fc75039e2aff3d2f21eb5f72de6bfd715
rishichawda <rishichawda@users.noreply.github.com>
2025-07-22 10:18:36 +0530

    build and upload artifact
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/buildkite_adhoc_metadata.sh
+++ .expeditor/scripts/buildkite_adhoc_metadata.sh
@@ -2,19 +2,30 @@
 
 set -euo pipefail
 
-echo "--- Checking HAB_AUTH_TOKEN"
-if [ -z "${HAB_AUTH_TOKEN:-}" ]; then
-  echo "HAB_AUTH_TOKEN is not set"
-else
-  echo "HAB_AUTH_TOKEN is set"
-fi
-
 ./.expeditor/scripts/install-hab.sh x86_64-linux
 
-echo "--- Installing Chef Infra Client from unstable channel via Habitat"
-hab pkg install chef/chef-infra-client --channel unstable
+export HAB_ORIGIN='ci'
+export PLAN='chef-infra-client'
+export CHEF_LICENSE="accept-no-persist"
+export HAB_LICENSE="accept-no-persist"
+export HAB_NONINTERACTIVE="true"
+export HAB_BLDR_CHANNEL="LTS-2024"
 
-echo "--- Setting INFRA_HAB_PKG_IDENT metadata for buildkite agent"
-export PKG_IDENT=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
-echo "setting INFRA_HAB_PKG_IDENT to $PKG_IDENT"
-buildkite-agent meta-data set "INFRA_HAB_PKG_IDENT" "$PKG_IDENT"
+echo "--- :key: Generating fake origin key"
+hab origin key generate "$HAB_ORIGIN"
+
+echo "--- Building Chef Infra Client package"
+hab pkg build . --refresh-channel LTS-2024 || error 'unable to build'
+
+project_root="$(git rev-parse --show-toplevel)"
+source "${project_root}/results/last_build.env" || error 'unable to determine details about this build'
+
+echo "--- :package: Uploading package"
+cd "${project_root}/results"
+buildkite-agent artifact upload "$pkg_artifact" || error 'unable to upload package'
+
+echo "--- Setting INFRA_HAB_ARTIFACT metadata for buildkite agent"
+echo "setting INFRA_HAB_ARTIFACT to $pkg_artifact"
+buildkite-agent meta-data set "INFRA_HAB_ARTIFACT" "$pkg_artifact"
+hab origin key export "$HAB_ORIGIN" > "${project_root}/results/${HAB_ORIGIN}-key.pub"
+buildkite-agent artifact upload "${HAB_ORIGIN}-key.pub"
