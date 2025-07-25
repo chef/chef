128b2e30bd0a711240ccf4796f744b658907e841
Tensibai <tensibai@iabis.net>
2025-07-25 12:05:14 +0000

    fix: lazy eval on base too

--- lib/chef/resource/habitat/habitat_base.rb
+++ lib/chef/resource/habitat/habitat_base.rb
@@ -5,13 +5,13 @@ require "chef-utils/dist" unless defined?(ChefUtils::Dist)
 class Chef
   class Resource
     class HabitatBase < Chef::Resource
-      property :bldr_url, String, default: node["habitat"]["bldr_url"] || "https://bldr.habitat.sh",
+      property :bldr_url, String, default: lazy { node["habitat"]["bldr_url"] || "https://bldr.habitat.sh" },
       description: "The habitat builder url where packages will be downloaded from. **Defaults to public Habitat Builder**"
 
-      property :channel, String, default: node["habitat"]["channel"] || "stable",
+      property :channel, String, default: lazy { node["habitat"]["channel"] || "stable" },
       description: "The release channel to install your package from."
 
-      property :auth_token, String, default: node["habitat"]["auth_token"] || Nil,
+      property :auth_token, String, default: lazy { node["habitat"]["auth_token"] || Nil },
       description: "Auth token for installing a package."
 
       action_class do
