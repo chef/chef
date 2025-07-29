f002fa2a424008b961d21e46f45e59f6cabd07f4
John McCrae <john.mccrae@progress.com>
2025-07-29 15:41:27 +0000

    UNDO THIS COMMIT
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/x86_64-windows/plan.ps1
+++ habitat/x86_64-windows/plan.ps1
@@ -155,7 +155,7 @@ function Invoke-Build {
         bundle install --jobs=3 --retry=3
         if (-not $?) { throw "unable to install gem dependencies" }
         Write-BuildLine " ** 'rake install' any gem sourced as a git reference so they'll look like regular gems."
-        foreach($git_gem in (Get-ChildItem "$env:GEM_HOME/bundler/gems")) {
+        foreach($git_gem in (Get-ChildItem "$env:GEM_HOME/gems")) {
             try {
                 Push-Location $git_gem
                 Write-BuildLine " -- installing $git_gem"
