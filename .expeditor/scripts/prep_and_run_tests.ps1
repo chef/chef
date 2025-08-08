c92db04f81609f81943bd1816977e0f781b36540
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2025-08-08 18:13:53 +0530

    set RUBY_DLL_PATH
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/prep_and_run_tests.ps1
+++ .expeditor/scripts/prep_and_run_tests.ps1
@@ -77,6 +77,11 @@ ruby -e "require 'openssl'; puts 'OpenSSL loaded successfully: ' + OpenSSL::OPEN
 Write-Output "--- Running Chef bundle install"
 bundle install --jobs=3 --retry=3
 
+# making sure we find the dlls from chef powershell
+$powershell_gem_lib = gem which chef-powershell | Select-Object -First 1
+$powershell_gem_path = Split-Path $powershell_gem_lib | Split-Path
+$env:RUBY_DLL_PATH = "$powershell_gem_path/bin/ruby_bin_folder/$env:PROCESSOR_ARCHITECTURE"
+
 switch ($TestType) {
     "Unit"          {[string[]]$RakeTest = 'spec:unit','component_specs'; break}
     "Integration"   {[string[]]$RakeTest = "spec:integration"; break}
