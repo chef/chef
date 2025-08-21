26f559c8196e377201189124db67d0b0579e580a
muthuja <muthujabavaji.vempalli@progress.com>
2025-08-21 16:58:23 +0530

    install ruby and fix ruby script and pick correct docker image
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .buildkite/validate-adhoc.rb
+++ .buildkite/validate-adhoc.rb
@@ -82,7 +82,7 @@ targets.each do |target|
       },
       "plugins" => {
         "docker#v3.5.0" => {
-          "image" => "chef/omnibus-toolchain-#{platform}:#{ENV['OMNIBUS_TOOLCHAIN_VERSION']}",
+          "image" => "chefes/omnibus-toolchain-#{platform}:#{ENV['OMNIBUS_TOOLCHAIN_VERSION']}",
           "shell" => [
             "powershell",
             "-Command"
@@ -118,7 +118,7 @@ targets.each do |target|
       "agents" => agents,
       "plugins" => {
         "docker#v3.5.0" => {
-          "image" => "chef/omnibus-toolchain-#{platform}:#{ENV['OMNIBUS_TOOLCHAIN_VERSION']}",
+          "image" => "chefes/omnibus-toolchain-#{platform}:#{ENV['OMNIBUS_TOOLCHAIN_VERSION']}",
           "privileged" => true,
           "propagate-environment" => true,
           "environment" => [
