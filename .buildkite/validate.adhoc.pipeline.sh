23e6c723b666df0b0e44531ef8b9d2d8a46c44a0
muthuja <muthujabavaji.vempalli@progress.com>
2025-08-21 14:14:45 +0530

    export path and see
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

---
+++ .buildkite/validate.adhoc.pipeline.sh
@@ -0,0 +1,23 @@
+workdir = $(pwd)
+
+echo "--- Installing Ruby.."
+curl -sSL https://cache.ruby-lang.org/pub/ruby/3.4/ruby-3.4.2.tar.xz | tar -xJ -C /tmp
+
+cd /tmp/ruby-3.4.2
+./configure --prefix=/tmp/ruby-3.4.2-install
+make -j"$(nproc)"
+make install
+
+echo "Installed Ruby version"
+/tmp/ruby-3.4.2-install/bin/ruby -v
+
+cd $workdir
+echo "running where am"
+pwd
+ls -la
+echo "going back to workdir"
+cd $workdir
+pwd
+
+echo "--- Generating pipeline configuration.."
+/tmp/ruby-3.4.2-install/bin/ruby .buildkite/validate-adhoc.rb | buildkite-agent pipeline upload
