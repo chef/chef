ab84080f2da0bab1a10d4070daf365eb68ed120f
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2025-08-28 20:39:31 +0530

    cleaning up
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- .buildkite/validate.adhoc.pipeline.sh
+++ .buildkite/validate.adhoc.pipeline.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Install Ruby to get the bundler gem.
-# echo "--- Installing dev tools.."
-# yum groupinstall "Development Tools" -y
-# yum install -y git curl gcc openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
-
-# echo "--- Installing Ruby..."
-# RUBY_VERSION=$(cat .buildkite-platform.json | awk -F'"' '/"ruby_version"/ {print $4}')
-# export RUBY_VERSION
-
-# curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# eval "$(rbenv init -)"
-
-# rbenv install ${RUBY_VERSION}
-# rbenv global ${RUBY_VERSION}
-
-# echo "Successfully installed ruby"
-# ruby -v
-
 workdir=$(pwd)
 
 echo "--- Installing Ruby.."
@@ -33,8 +14,6 @@ echo "Installed Ruby version"
 /tmp/ruby-3.4.2-install/bin/ruby -v
 
 cd $workdir
-echo "where am i?"
-ls -la
 
 echo "--- Generating pipeline configuration.."
 /tmp/ruby-3.4.2-install/bin/ruby .buildkite/validate-adhoc.rb > pipeline-config.yaml
