1ed17965624159309132dfeda84e0a13b3475a40
John McCrae <john.mccrae@progress.com>
2025-09-05 13:52:48 -0700

    Updating Hab channels (#15242)
    * Updating Hab channels
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/scripts/verify-plan.sh
+++ .expeditor/scripts/verify-plan.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export HAB_ORIGIN='ci'
+export HAB_ORIGIN='chef'
 export PLAN='chef-infra-client'
 export CHEF_LICENSE="accept-no-persist"
 # Read the CHEF_LICENSE_SERVER value from chef_license_server_url.txt
@@ -13,7 +13,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export CHEF_LICENSE_SERVER=$(cat "$SCRIPT_DIR/chef_license_server_url.txt")
 export HAB_LICENSE="accept-no-persist"
 export HAB_NONINTERACTIVE="true"
-export HAB_BLDR_CHANNEL="LTS-2024"
+export HAB_BLDR_CHANNEL="base-2025"
 
 # print error message followed by usage and exit
 error () {
@@ -28,6 +28,9 @@ echo "--- :habicat: Patching /etc/nsswitch.conf to ensure 'files' is listed firs
 sed -i 's/hosts:      files dns myhostname/hosts:      files dns/g' /etc/nsswitch.conf
 sed -i 's/networks:   files dns/networks:   files/g' /etc/nsswitch.conf
 
+echo "--- :git: Configuring git safe directory"
+git config --global --add safe.directory /workdir
+
 echo "--- :8ball: :linux: Verifying $PLAN"
 project_root="$(git rev-parse --show-toplevel)"
 
@@ -37,7 +40,7 @@ hab origin key generate "$HAB_ORIGIN"
 echo "--- :construction: Building $PLAN (solely for verification testing)"
 (
   cd "$project_root" || error 'cannot change directory to project root'
-  DO_CHECK=true hab pkg build . --refresh-channel LTS-2024 || error 'unable to build'
+  DO_CHECK=true hab pkg build . --refresh-channel base-2025 || error 'unable to build'
 )
 
 source "${project_root}/results/last_build.env" || error 'unable to determine details about this build'
