7ed60b7c08975d6ac88b7c0bfc01fec3f765f956
John McCrae <john.mccrae@progress.com>
2025-09-18 21:25:37 +0000

    correcting adhoc builders
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/scripts/validate_adhoc_build.sh
+++ .expeditor/scripts/validate_adhoc_build.sh
@@ -15,8 +15,8 @@ export PKG_ARTIFACT=$(buildkite-agent meta-data get "INFRA_HAB_ARTIFACT")
 buildkite-agent artifact download "$PKG_ARTIFACT" .
 
 echo ":key: Setting up origin key"
-buildkite-agent artifact download "ci-key.pub" .
-cat ci-key.pub | hab origin key import
+buildkite-agent artifact download "chef-key.pub" .
+cat chef-key.pub | hab origin key import
 if [ $? -ne 0 ]; then
   echo "Failed to import origin key"
   exit 1
@@ -25,6 +25,6 @@ fi
 echo "--- Installing $PKG_ARTIFACT"
 sudo hab pkg install $PKG_ARTIFACT --auth $HAB_AUTH_TOKEN --binlink
 
-pkg_ident=$(hab pkg path ci/chef-infra-client | grep -oP 'ci/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
+pkg_ident=$(hab pkg path chef/chef-infra-client | grep -oP 'chef/chef-infra-client/[0-9]+\.[0-9]+\.[0-9]+/[0-9]+')
 echo "--- Resolved package identifier: $pkg_ident, attempting to run tests"
 ./habitat/tests/test.sh "$pkg_ident"
