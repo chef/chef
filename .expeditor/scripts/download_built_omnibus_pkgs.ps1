c95802503d19e35c5b60ed31594ef36d7e27c905
Sean Simmons <ssimmons@progress.com>
2025-09-29 13:48:56 -0400

    downloading error
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .expeditor/scripts/download_built_omnibus_pkgs.ps1
+++ .expeditor/scripts/download_built_omnibus_pkgs.ps1
@@ -2,7 +2,13 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "--- Installing package from BuildKite"
 buildkite-agent artifact download "omnibus-ruby\chef\pkg\*.msi" . --step "${Env:OMNIBUS_BUILDER_KEY}"
-$package_file = (Get-ChildItem pkg -Filter "*.msi").FullName 
+
+if (Test-Path "omnibus-ruby\chef\pkg") {
+    $package_file = (Get-ChildItem omnibus-ruby\chef\pkg\ -Filter "*.msi" | Select-Object -First 1).FullName
+} else {
+    Write-Error "Downloaded artifact directory not found"
+    exit 1
+}
 
 Write-Output "--- Installing $package_file"
 Start-Process "$package_file" /quiet -Wait
