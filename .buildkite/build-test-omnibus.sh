6f52278284c48efa6a9fcab5c8589d5c097ab448
Sean <111799713+sean-sype-simmons@users.noreply.github.com>
2025-10-01 10:03:26 -0400

    Sean sype simmons/chef 24775 omnibus configuration pipeline configurations3 cache cconfiguration (#15310)
    * updating files for a test build
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating version
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating version
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating sha
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * force cache true
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating version for addtional error handling
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * reverting omnibus
    
    extending powershell output
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * testing omnibus profile setting
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * testing s3 aws keys
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating build scripts with new omnibus configuration overrdies, as the build does NOT use the omnibus-buildkite-plugin we have for the other esortic machines
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * attempting to set the role arn instead of using aws ssm params
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * attempting to set before script runs that doesnt break pre-command
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * s3 assume
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * testing with inline
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * region set
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updated build script
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * using correct var names now...
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * moving to pre command
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating script to match corrected path
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating new value for buildkite plugin that fixed the esoteric error
    
    also fixing to ensure mac doesnt grab the values they dont need
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * intial update to test security changes to omnibus
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating path download
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * fixing upload
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * windows pathings...
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * downloading error
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * downloading error
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * adding extra spec directories to chek where we are at
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * getting things ready for the merge
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * getting things ready for the merge
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * updating chef_foundation
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * testing one last build with latest omnibus changes
    
    had to run through robo cop and sonarqube changes on omnibus project
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * testing one last build with latest omnibus changes
    
    had to run through robo cop and sonarqube changes on omnibus project
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    * bumpbing to latest omnibus buildkite plugin as well as the omnibus version that brings in the s3 auth changes for caching
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>
    
    ---------
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -119,6 +119,9 @@ then
       echo "        - ARTIFACTORY_API_KEY"
       echo "        - RPM_SIGNING_KEY"
       echo "        - CHEF_FOUNDATION_VERSION"
+      echo "        - AWS_S3_ACCESS_KEY"
+      echo "        - AWS_S3_SECRET_KEY"
+      echo "        - AWS_REGION"
       echo "  commands:"
       echo "    - ./.expeditor/scripts/omnibus_chef_build.sh"
       echo "  timeout_in_minutes: 60"
@@ -145,6 +148,9 @@ then
       echo "        - AWS_ACCESS_KEY_ID"
       echo "        - AWS_SECRET_ACCESS_KEY"
       echo "        - AWS_SESSION_TOKEN"
+      echo "        - AWS_S3_ACCESS_KEY"
+      echo "        - AWS_S3_SECRET_KEY"
+      echo "        - AWS_REGION"
       echo "      volumes:"
       echo '        - "c:\\buildkite-agent:c:\\buildkite-agent"'
       echo "  commands:"
@@ -182,7 +188,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.99:"
+    echo "  - chef/omnibus#v0.2.101:"
     echo "      build: chef"
     echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
     echo "      config: omnibus/omnibus.rb"
@@ -205,7 +211,7 @@ then
     echo "  agents:"
     [[ $platform == *"arm"* ]] && echo "    queue: omnibus-mac_os_x-12-arm64" || echo "    queue: omnibus-mac_os_x-12-x86_64" || echo "    queue: omnibus-mac_os_x-13-arm64" || echo "    queue: omnibus-mac_os_x-14-arm64"
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.99:"
+    echo "  - chef/omnibus#v0.2.101:"
     echo "      config: omnibus/omnibus.rb"
     [[ $platform == *"arm"* ]] && echo "      remote-host: buildkite-omnibus-mac_os_x-12-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-12-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-13-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-14-arm64"
     echo "      notarize-macos-package: chef"
@@ -226,7 +232,7 @@ then
   echo "- key: create-build-record"
   echo "  label: \":artifactory: Create Build Record\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.99:"
+  echo "  - chef/omnibus#v0.2.101:"
   echo "      create-build-record: chef"
 fi
 
@@ -321,7 +327,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.99:"
+    echo "  - chef/omnibus#v0.2.101:"
     echo "      test: chef"
     echo "      test-path: omnibus/omnibus-test.sh"
     echo "      install-dir: \"/opt/chef\""
@@ -347,6 +353,6 @@ then
   echo "- key: promote"
   echo "  label: \":artifactory: Promote to Current\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.99:"
+  echo "  - chef/omnibus#v0.2.101:"
   echo "      promote: chef"
 fi
--- .expeditor/scripts/download_built_omnibus_pkgs.ps1
+++ .expeditor/scripts/download_built_omnibus_pkgs.ps1
@@ -1,8 +1,14 @@
 $ErrorActionPreference = "Stop"
 
 Write-Host "--- Installing package from BuildKite"
-buildkite-agent artifact download "pkg\*.msi" . --step "${Env:OMNIBUS_BUILDER_KEY}"
-$package_file = (Get-ChildItem pkg -Filter "*.msi").FullName 
+buildkite-agent artifact download "omnibus-ruby\chef\pkg\*.msi" . --step "${Env:OMNIBUS_BUILDER_KEY}"
+
+if (Test-Path "omnibus-ruby\chef\pkg") {
+    $package_file = (Get-ChildItem omnibus-ruby\chef\pkg\ -Filter "*.msi" | Select-Object -First 1).FullName
+} else {
+    Write-Error "Downloaded artifact directory not found"
+    exit 1
+}
 
 Write-Output "--- Installing $package_file"
 Start-Process "$package_file" /quiet -Wait
