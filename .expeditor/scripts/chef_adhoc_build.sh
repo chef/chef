183bfa8340cdb2c32be21b4751c6dddf568c62ef
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2025-10-10 20:44:39 +0530

    fixing adhoc builds (#15300)
    * update windows queue
    * attach volume to get access to buildkite-agent
    * use the single use queue
    * update path to include buildkite agent installation
    * pass environment variables
    * accept license
    * add buildkite-agent to path
    * use windows 2022 for build
    * check the key contents
    * use chef origin
    * add secret key
    * add error details when hab install fails
    * fix package ident discovery
    * update linux builds to use the same flow
    ---------
    
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
    Signed-off-by: Matt Wrock <matt@mattwrock.com>
    Co-authored-by: Matt Wrock <matt@mattwrock.com>

--- .expeditor/scripts/chef_adhoc_build.sh
+++ .expeditor/scripts/chef_adhoc_build.sh
@@ -4,15 +4,16 @@ set -euo pipefail
 
 ./.expeditor/scripts/install-hab.sh x86_64-linux
 
-export HAB_ORIGIN='ci'
+export HAB_ORIGIN='chef'
 export PLAN='chef-infra-client'
 export CHEF_LICENSE="accept-no-persist"
 export HAB_LICENSE="accept-no-persist"
 export HAB_NONINTERACTIVE="true"
 export HAB_BLDR_CHANNEL="base-2025"
 
-echo "--- :key: Generating fake origin key"
-hab origin key generate "$HAB_ORIGIN"
+echo "--- :key: Downloading origin keys"
+hab origin key download "$HAB_ORIGIN"
+hab origin key download "$HAB_ORIGIN" --secret
 
 echo "--- Building Chef Infra Client package"
 hab pkg build . --refresh-channel base-2025 || error 'unable to build'
@@ -24,8 +25,6 @@ echo "--- :package: Uploading package"
 cd "${project_root}/results"
 buildkite-agent artifact upload "$pkg_artifact" || error 'unable to upload package'
 
-echo "--- Setting INFRA_HAB_ARTIFACT metadata for buildkite agent"
-echo "setting INFRA_HAB_ARTIFACT to $pkg_artifact"
-buildkite-agent meta-data set "INFRA_HAB_ARTIFACT" "$pkg_artifact"
-hab origin key export "$HAB_ORIGIN" > "${project_root}/results/${HAB_ORIGIN}-key.pub"
-buildkite-agent artifact upload "${HAB_ORIGIN}-key.pub"
+echo "--- Setting INFRA_HAB_ARTIFACT_LINUX metadata for buildkite agent"
+echo "setting INFRA_HAB_ARTIFACT_LINUX to $pkg_artifact"
+buildkite-agent meta-data set "INFRA_HAB_ARTIFACT_LINUX" "$pkg_artifact"
