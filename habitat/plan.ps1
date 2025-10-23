94e9cbf1dc9279be135576789cf03f10a5702aea
poorndm <poorndm@progress.com>
2025-10-23 18:10:54 +0530

    Add new pipeline to handle bundle_chef-official gem
    Signed-off-by: poorndm <poorndm@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -195,6 +195,18 @@ function Invoke-Install {
         Push-Location $pkg_prefix
         $env:BUNDLE_GEMFILE="${HAB_CACHE_SRC_PATH}/${pkg_dirname}/Gemfile"
 
+        # Install chef-official-distribution gem from artifactory
+        Write-BuildLine "** Installing chef-official-distribution gem from artifactory"
+        $ArtifactoryUrl = "https://artifactory-internal.ps.chef.co/artifactory/omnibus-gems-local/"
+        gem sources --add $ArtifactoryUrl
+        gem install chef-official-distribution
+        gem sources --remove $ArtifactoryUrl
+
+        # Verify chef-official-distribution installation
+        Write-BuildLine "** Verifying chef-official-distribution installation"
+        gem list chef-official-distribution
+        If ($lastexitcode -ne 0) { Exit $lastexitcode }
+
         foreach($gem in ("chef-bin", "chef", "inspec-core-bin", "ohai")) {
             Write-BuildLine "** generating binstubs for $gem with precise version pins"
             appbundler.bat "${HAB_CACHE_SRC_PATH}/${pkg_dirname}" $pkg_prefix/bin $gem
