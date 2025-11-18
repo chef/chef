3405dfe06fa64bc81d5d773c2f2006dc06e5a214
Chef Expeditor <chef-ci@chef.io>
2025-11-18 06:36:22 +0000

    Bump version to 19.1.112 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-bin/lib/chef-bin/version.rb
+++ chef-bin/lib/chef-bin/version.rb
@@ -21,7 +21,7 @@
 
 module ChefBin
   CHEFBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "19.1.111".freeze
+  VERSION = "19.1.112".freeze
 end
 
 #
--- knife/lib/chef/knife/version.rb
+++ knife/lib/chef/knife/version.rb
@@ -17,6 +17,6 @@
 class Chef
   class Knife
     KNIFE_ROOT = File.expand_path("../..", __dir__)
-    VERSION = "19.1.111".freeze
+    VERSION = "19.1.112".freeze
   end
 end
