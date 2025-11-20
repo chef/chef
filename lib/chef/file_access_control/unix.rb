2c83ecef806d4ef9f2962bb3461165d3e163b794
dcrosby <dcrosby@meta.com>
2025-11-20 13:59:22 -0800

    [FileAccessControl] Save return values in should_update methods
    Signed-off-by: David Crosby <dcrosby@fb.com>

--- lib/chef/file_access_control/unix.rb
+++ lib/chef/file_access_control/unix.rb
@@ -77,15 +77,15 @@ class Chef
       end
 
       def should_update_owner?
-        if target_uid.nil?
+        if (target = target_uid).nil?
           # the user has not specified a permission on the new resource, so we never manage it with FAC
           Chef::Log.trace("Found target_uid == nil, so no owner was specified on resource, not managing owner")
           false
-        elsif current_uid.nil?
+        elsif (current = current_uid).nil?
           # the user has specified a permission, and we are creating a file, so always enforce permissions
           Chef::Log.trace("Found current_uid == nil, so we are creating a new file, updating owner")
           true
-        elsif target_uid != current_uid
+        elsif target != current
           # the user has specified a permission, and it does not match the file, so fix the permission
           Chef::Log.trace("Found target_uid != current_uid, updating owner")
           true
@@ -138,15 +138,15 @@ class Chef
       end
 
       def should_update_group?
-        if target_gid.nil?
+        if (target = target_gid).nil?
           # the user has not specified a permission on the new resource, so we never manage it with FAC
           Chef::Log.trace("Found target_gid == nil, so no group was specified on resource, not managing group")
           false
-        elsif current_gid.nil?
+        elsif (current = current_gid).nil?
           # the user has specified a permission, and we are creating a file, so always enforce permissions
           Chef::Log.trace("Found current_gid == nil, so we are creating a new file, updating group")
           true
-        elsif target_gid != current_gid
+        elsif target != current
           # the user has specified a permission, and it does not match the file, so fix the permission
           Chef::Log.trace("Found target_gid != current_gid, updating group")
           true
@@ -189,15 +189,15 @@ class Chef
       end
 
       def should_update_mode?
-        if target_mode.nil?
+        if (target = target_mode).nil?
           # the user has not specified a permission on the new resource, so we never manage it with FAC
           Chef::Log.trace("Found target_mode == nil, so no mode was specified on resource, not managing mode")
           false
-        elsif current_mode.nil?
+        elsif (current = current_mode).nil?
           # the user has specified a permission, and we are creating a file, so always enforce permissions
           Chef::Log.trace("Found current_mode == nil, so we are creating a new file, updating mode")
           true
-        elsif target_mode != current_mode
+        elsif target != current
           # the user has specified a permission, and it does not match the file, so fix the permission
           Chef::Log.trace("Found target_mode != current_mode, updating mode")
           true
