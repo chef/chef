f916a43a8167c13524094bea98d405fbf43bfab4
Thomas Powell <thomas.powell@progress.com>
2025-11-20 17:09:45 -0500

    Chefstyle hates the redundant runtime error
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -122,6 +122,6 @@ if resolv_info.include?("Installed at (default):") && resolv_info.include?("reso
   end
 
   puts "Installing resolv gem..."
-  system("gem install resolv") || raise(RuntimeError, "gem install resolv failed")
+  system("gem install resolv") or raise "gem install resolv failed"
   puts "resolv gem installed successfully"
 end
