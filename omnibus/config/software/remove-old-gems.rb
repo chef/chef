ce469f46dd40a753c86f6e4a735f359bb19d9291
Thomas Powell <104777878+tpowell-progress@users.noreply.github.com>
2025-11-21 13:53:43 -0500

    NIST CVE-2025-58767: Remove vulnerable rexml bundled gems (#15462)
    Remove bundled versions of rexml that get flagged in CVE scans using the omnibus remove-old-gems
    
    Ruby <= 3.1.6: 3.2.5
    Ruby 3.1.7: 3.3.9
    
    ---------
    
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- omnibus/config/software/remove-old-gems.rb
+++ omnibus/config/software/remove-old-gems.rb
@@ -4,15 +4,15 @@ license :project_license
 skip_transitive_dependency_licensing true
 
 build do
-  block "Removing old versions of rexml < 3.3.6" do
+  block "Removing old versions of rexml < 3.4.2" do
     gemfile = "#{install_dir}/embedded/bin/gem"
 
     next unless File.exist?(gemfile)
 
-    puts "Removing old versions of rexml < 3.3.6"
+    puts "Removing old versions of rexml < 3.4.2"
     env = with_standard_compiler_flags(with_embedded_path)
 
-    # remove [-a] all rexml < 3.3.6 including [-x] executables and [-I] ignore dependencies
-    command "#{gemfile} uninstall rexml -v \"<3.3.6\" -a -x -I", env: env
+    # remove [-a] all rexml < 3.4.2 including [-x] executables and [-I] ignore dependencies
+    command "#{gemfile} uninstall rexml -v \"<3.4.2\" -a -x -I", env: env
   end
 end
