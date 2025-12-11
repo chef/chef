68ecad06c14e24b408b1658463c66e5380ee07cb
Matt Wrock <matt@mattwrock.com>
2025-12-11 14:42:02 -0800

    output path before testing archive
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/windows.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/windows.rb
@@ -191,6 +191,8 @@ include_recipe "::_chef_client_trusted_certificate"
 
 include_recipe "git"
 
+puts "Hello. Here is my path."
+puts ENV["PATH"]
 # test various archive formats in the archive_file resource
 %w{tourism.tar.gz tourism.tar.xz tourism.zip}.each do |archive|
   cookbook_file File.join(Chef::Config[:file_cache_path], archive) do
