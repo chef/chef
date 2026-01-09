6efebb2be76d9a747075c21d8066fd30b326ff00
John McCrae <john.mccrae@progress.com>
2026-01-09 19:08:45 +0000

    Debugging the _openssl.rb issues
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- kitchen-tests/test/integration/end-to-end/_openssl.rb
+++ kitchen-tests/test/integration/end-to-end/_openssl.rb
@@ -1,6 +1,7 @@
 # Reference recipes/_openssl.rb test to 'generate and sign a certificate with the CA'
 # This ensures that the generated certificate is valid.
 cmd = if os.windows?
+puts "Running OpenSSL verify command on Windows"
         openssl_path = if File.exist?("C:\\ssl_test\\openssl_path.txt")
                          File.read("C:\\ssl_test\\openssl_path.txt").strip
                        else
@@ -10,6 +11,7 @@ cmd = if os.windows?
       else
         "/opt/chef/embedded/bin/openssl verify -CAfile /etc/ssl_test/my_ca.crt /etc/ssl_test/my_signed_cert.crt"
       end
+puts "Executing command: #{cmd}"
 describe command(cmd) do
   its("stdout") { should match /my_signed_cert.*OK/ }
   its("stderr") { should be_empty }
