8a73c1d02513b8c2cb35b383585e54416180859a
John McCrae <john.mccrae@progress.com>
2026-01-12 16:55:23 +0000

    Now debugging Macos
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/macos.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/macos.rb
@@ -91,7 +91,8 @@ homebrew_update "update" do
   action :update
 end
 
-homebrew_package "dos2unix"
+# homebrew_package "dos2unix"
+Mixlib::ShellOut.new("brew install dos2unix --verbose --debug").run_command
 
 homebrew_package "dos2unix" do
   action :purge
