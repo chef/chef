c143f49049c47c71a8e2908c5e97c8da33edfa97
John McCrae <john.mccrae@progress.com>
2026-01-13 23:18:05 +0000

    Updating openssl and getting Test Kitchen working
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- kitchen-tests/test/integration/end-to-end/_openssl.rb
+++ kitchen-tests/test/integration/end-to-end/_openssl.rb
@@ -1,7 +1,12 @@
 # Reference recipes/_openssl.rb test to 'generate and sign a certificate with the CA'
 # This ensures that the generated certificate is valid.
 cmd = if os.windows?
-        "C:\\opscode\\chef\\embedded\\bin\\openssl.exe verify -CAfile C:\\ssl_test\\my_ca.crt C:\\ssl_test\\my_signed_cert.crt"
+        openssl_path = if File.exist?("C:\\ssl_test\\openssl_path.txt")
+                         File.read("C:\\ssl_test\\openssl_path.txt", encoding: "UTF-8").strip.delete("\uFEFF")
+                       else
+                         "C:\\opscode\\chef\\embedded\\bin\\openssl.exe"
+                       end
+        "#{openssl_path} verify -CAfile C:\\ssl_test\\my_ca.crt C:\\ssl_test\\my_signed_cert.crt"
       else
         "/opt/chef/embedded/bin/openssl verify -CAfile /etc/ssl_test/my_ca.crt /etc/ssl_test/my_signed_cert.crt"
       end
