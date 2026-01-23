0f99998476e17d9a2c1b2fb5baad78967c8c7457
Thomas Powell <thomas.powell@progress.com>
2026-01-23 11:22:11 -0500

    Go up one level
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- habitat/binstub_patch.rb
+++ habitat/binstub_patch.rb
@@ -1,5 +1,5 @@
 # Load RUNTIME_ENVIRONMENT from .env file
-runtime_env_file = File.join(__dir__, "RUNTIME_ENVIRONMENT")
+runtime_env_file = File.join(__dir__, "..", "RUNTIME_ENVIRONMENT")
 if File.exist?(runtime_env_file)
   File.readlines(runtime_env_file).each do |line|
     line = line.strip
