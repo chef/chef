00545ef4f0d0ca93c3b387a123af463a02cea435
Thomas Powell <thomas.powell@progress.com>
2026-01-23 14:10:50 -0500

    Skip the patching if coming from hab pkg exec
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- habitat/binstub_patch.bat
+++ habitat/binstub_patch.bat
@@ -1,10 +1,13 @@
-IF EXIST "%~dp0..\RUNTIME_ENVIRONMENT" (
-  FOR /F "usebackq tokens=* delims=" %%A IN ("%~dp0..\RUNTIME_ENVIRONMENT") DO (
-    SET "line=%%A"
-    REM Skip empty lines and comments
-    IF NOT "!line!"=="" (
-      IF NOT "!line:~0,1!"=="#" (
-        SET "%%A"
+REM skip this if hab pkg exec has already done it
+IF NOT DEFINED APPBUNDLER_ALLOW_RVM (
+  IF EXIST "%~dp0..\RUNTIME_ENVIRONMENT" (
+    FOR /F "usebackq tokens=* delims=" %%A IN ("%~dp0..\RUNTIME_ENVIRONMENT") DO (
+      SET "line=%%A"
+      REM Skip empty lines and comments
+      IF NOT "!line!"=="" (
+        IF NOT "!line:~0,1!"=="#" (
+          SET "%%A"
+        )
       )
     )
   )
