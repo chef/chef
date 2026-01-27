aef2182931650b7ec857dbc3ffaea033ccc0750c
Sean Simmons <ssimmons@progress.com>
2026-01-27 13:16:58 -0500

    adding docker fixes
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .expeditor/build-docker-images.sh
+++ .expeditor/build-docker-images.sh
@@ -15,11 +15,18 @@ fi
 channel="${EXPEDITOR_CHANNEL:-unstable}"
 version="${EXPEDITOR_VERSION:?You must manually set the EXPEDITOR_VERSION environment variable to an existing semantic version.}"
 
+# Verify HAB_AUTH_TOKEN is set for non-stable channels
+if [[ "${channel}" != "stable" ]] && [[ -z "${HAB_AUTH_TOKEN:-}" ]]; then
+  echo "ERROR: HAB_AUTH_TOKEN environment variable must be set for channel: ${channel}"
+  exit 1
+fi
+
 echo "--- Building chef/chef-hab:${version} docker image for ${arch}"
 docker build \
   --build-arg "CHANNEL=${channel}" \
   --build-arg "VERSION=${version}" \
   --build-arg "ARCH=${dockerfile_arch}" \
+  --build-arg "HAB_AUTH_TOKEN=${HAB_AUTH_TOKEN}" \
   -t "chef/chef-hab:${version}-${arch}" .
 
 echo "--- Pushing chef/chef-hab:${version} docker image for ${arch} to dockerhub"
