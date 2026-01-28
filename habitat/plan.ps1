c9cb541cd4303bd05122588ae886a83c2b69b7ed
John McCrae <john.mccrae@progress.com>
2026-01-28 03:03:06 +0000

    Updating the spelling
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -19,7 +19,7 @@ $pkg_deps=@(
   "core/libarchive"
   "core/ruby3_4-plus-devkit/3.4.8"
   # "chef/chef-powershell-shim" - Rmoved for the first Chef-19 release
-  # "core/visual-cpp-redist-2022" - Removed for the first Chef-19 release because of licneinse issues
+  # "core/visual-cpp-redist-2022" - Removed for the first Chef-19 release because of license issues
 )
 
 function Invoke-Begin {
