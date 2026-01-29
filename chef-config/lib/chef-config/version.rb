09fc61fef35913946766ceabde50e3d517d9d28d
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2026-01-29 18:26:39 +0530

    manually bumping version to 19.1.158
    expeditor is failing to bump version to 19.1.158 so trying to see if it can get past that
    
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -15,5 +15,5 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("..", __dir__)
-  VERSION = "19.1.157".freeze
+  VERSION = "19.1.158".freeze
 end
--- chef-utils/lib/chef-utils/version.rb
+++ chef-utils/lib/chef-utils/version.rb
@@ -16,5 +16,5 @@
 
 module ChefUtils
   CHEFUTILS_ROOT = File.expand_path("..", __dir__)
-  VERSION = "19.1.157"
+  VERSION = "19.1.158"
 end
