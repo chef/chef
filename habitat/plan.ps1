c8caaa81773fe2a74fecb1daed1fda6abf4aa8ef
John McCrae <john.mccrae@progress.com>
2026-01-28 22:52:15 -0800

    Add back chef-powershell-shim and visual-cpp-redist-2022 as pkg_deps to infra client windows habitat plan (#15596)
    * Turns out we REALLY need vcpp in the builds
    
    Signed-off-by: John McCrae <john.mccrae@progress.com>
    
    * adding powershell back
    
    just bringing the PR to the point where it can be called a revert of https://github.com/chef/chef/pull/15594
    
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: John McCrae <john.mccrae@progress.com>
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
    Co-authored-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -18,8 +18,8 @@ $pkg_deps=@(
   "core/xz"
   "core/libarchive"
   "core/ruby3_4-plus-devkit/3.4.8"
-  # "chef/chef-powershell-shim" - Removed for the first Chef-19 release
-  # "core/visual-cpp-redist-2022" - Removed for the first Chef-19 release because of license issues
+  "chef/chef-powershell-shim"
+  "core/visual-cpp-redist-2022"
 )
 
 function Invoke-Begin {
@@ -46,7 +46,7 @@ function Invoke-SetupEnvironment {
 
     Push-RuntimeEnv -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath openssl)/bin"
     Push-RuntimeEnv -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath zlib)/bin"
-    # Push-RuntimeEnv -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath visual-cpp-redist-2022)/bin"
+    Push-RuntimeEnv -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath visual-cpp-redist-2022)/bin"
     Push-RuntimeEnv -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath libarchive)/bin"
     Push-RuntimeEnv -IsPath RUBY_DLL_PATH "$(Get-HabPackagePath xz)/bin"
 
