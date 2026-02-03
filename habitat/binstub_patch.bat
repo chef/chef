671a9026daa673247c6d2ff0d5dca34c7d37117b
Thomas Powell <thomas.powell@progress.com>
2026-02-03 12:04:22 -0500

    More complex binstub patching
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- habitat/binstub_patch.bat
+++ habitat/binstub_patch.bat
@@ -1,31 +1,71 @@
+@ECHO OFF
+
 REM skip this if hab pkg exec has already done it, APPBUNDLER_ALLOW_RVM will be set if hab pkg exec
 IF NOT DEFINED APPBUNDLER_ALLOW_RVM (
   REM Get the drive letter of the current batch file
   SET "BINSTUB_DRIVE=%~d0"
 
-  REM Prepend vendor path to PATH if it exists
+  REM Prepend vendor path to PATH if it exists (using full path with drive)
   IF EXIST "%~dp0..\vendor" (
-    SET "PATH=%BINSTUB_DRIVE%%~p0..\vendor;%PATH%"
+    SET "PATH=%~dp0..\vendor;%PATH%"
   )
 
   IF EXIST "%~dp0..\RUNTIME_ENVIRONMENT" (
-    FOR /F "usebackq tokens=* delims=" %%A IN ("%~dp0..\RUNTIME_ENVIRONMENT") DO (
-      SET "line=%%A"
-      REM Skip empty lines and comments
-      IF NOT "!line!"=="" (
-        IF NOT "!line:~0,1!"=="#" (
-          REM Check if this is a PATH variable assignment
-          SET "var=%%A"
-          IF "!var:~0,5!"=="PATH=" (
-            REM Extract the PATH value and prepend drive letter
-            SET "pathval=!var:~5!"
-            SET "PATH=%BINSTUB_DRIVE%!pathval!;%PATH%"
+    REM Process each line from RUNTIME_ENVIRONMENT
+    FOR /F "usebackq tokens=1* delims==" %%A IN ("%~dp0..\RUNTIME_ENVIRONMENT") DO (
+      REM Skip comments (lines starting with #)
+      SET "varname=%%A"
+      SET "firstchar=%%A"
+      SET "firstchar=!firstchar:~0,1!"
+      IF NOT "!firstchar!"=="#" (
+        SET "varvalue=%%B"
+        IF NOT "!varvalue!"=="" (
+          REM Check if this is PATH
+          IF /I "%%A"=="PATH" (
+            REM Process PATH value - add drive letter to paths starting with backslash
+            CALL :ProcessPath "%%B"
           ) ELSE (
-            REM For other variables, prepend drive letter if it's a path-like value
-            SET "%%A"
+            REM For other variables, add drive letter if starts with backslash
+            SET "testchar=%%B"
+            SET "testchar=!testchar:~0,1!"
+            IF "!testchar!"=="\" (
+              SET "%%A=%BINSTUB_DRIVE%%%B"
+            ) ELSE (
+              SET "%%A=%%B"
+            )
           )
         )
       )
     )
   )
+
+  SET "APPBUNDLER_ALLOW_RVM=true"
 )
+
+REM Continue to the rest of the script (this file is patched into other batch files)
+GOTO :SkipBinstubFunctions
+
+:ProcessPath
+SET "origpath=%~1"
+SET "newpath="
+:pathloop
+FOR /F "tokens=1* delims=;" %%X IN ("%origpath%") DO (
+  SET "segment=%%X"
+  SET "testchar=%%X"
+  SET "testchar=!testchar:~0,1!"
+  IF "!testchar!"=="\" (
+    SET "segment=%BINSTUB_DRIVE%%%X"
+  )
+  IF "%newpath%"=="" (
+    SET "newpath=!segment!"
+  ) ELSE (
+    SET "newpath=!newpath!;!segment!"
+  )
+  SET "origpath=%%Y"
+  IF NOT "%%Y"=="" GOTO pathloop
+)
+SET "PATH=%newpath%;%PATH%"
+GOTO :EOF
+
+:SkipBinstubFunctions
+REM Binstub functions defined above, continue with the rest of the script
