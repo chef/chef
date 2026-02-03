c8c36100fe606fd8643630c528d7b326aa202867
Thomas Powell <thomas.powell@progress.com>
2026-02-03 13:56:39 -0500

    Redundant checks
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- lib/chef/resource/helpers/path_helpers.rb
+++ lib/chef/resource/helpers/path_helpers.rb
@@ -24,12 +24,11 @@ class Chef
 
         # On Windows, prefer the PowerShell wrapper if it exists and we're running chef-client
         # because hab pkg .bat binstubs getting the pathing correct is a challenge.
-        if ChefUtils.windows? && bin == "#{ChefUtils::Dist::Infra::CLIENT}"
+        if bin == "#{ChefUtils::Dist::Infra::CLIENT}"
           ps1_path = 'C:\hab\chef\bin\chef-client.ps1'
-          return ps1_path if File.exist?(ps1_path)
+          return ps1_path if File.exist?(ps1_path) && ChefUtils.windows?
+          return path
         end
-
-        return path if bin == "#{ChefUtils::Dist::Infra::CLIENT}"
 
         # Return empty string if no valid path is found
         ""
