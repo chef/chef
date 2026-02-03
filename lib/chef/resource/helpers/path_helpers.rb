e9f32ac4e05de98b817228be7dbaae64ef00da87
Thomas Powell <thomas.powell@progress.com>
2026-02-03 13:50:11 -0500

    Prefer powershell wrapper if it exists
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- lib/chef/resource/helpers/path_helpers.rb
+++ lib/chef/resource/helpers/path_helpers.rb
@@ -21,6 +21,13 @@ class Chef
       def chef_client_hab_binary_path
         path = File.realpath($PROGRAM_NAME)
         bin = File.basename(path)
+        
+        # On Windows, prefer the PowerShell wrapper if it exists and we're running chef-client
+        if ChefUtils.windows? && bin == "#{ChefUtils::Dist::Infra::CLIENT}"
+          ps1_path = 'C:\hab\chef\bin\chef-client.ps1'
+          return ps1_path if File.exist?(ps1_path)
+        end
+        
         return path if bin == "#{ChefUtils::Dist::Infra::CLIENT}"
 
         # Return empty string if no valid path is found
