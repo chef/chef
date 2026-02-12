88ed319c191784d98c6431bcd312d756991bbcce
Thomas Powell <thomas.powell@progress.com>
2026-02-12 14:59:55 -0500

    remove old net-imap: Add code to remove vulnerable versions of net-imap
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- omnibus/config/software/remove-old-gems.rb
+++ omnibus/config/software/remove-old-gems.rb
@@ -4,15 +4,24 @@ license :project_license
 skip_transitive_dependency_licensing true
 
 build do
-  block "Removing old versions of rexml < 3.4.2" do
+  block "Removing old gem versions" do
+    # Hash of gems to remove with version constraints
+    # Key: gem name, Value: version constraint (e.g., "<3.4.2")
+    gems_to_remove = {
+      "rexml" => "<3.4.2",
+      "net-imap" => "<0.2.5",
+    }
+
     gemfile = "#{install_dir}/embedded/bin/gem"
 
     next unless File.exist?(gemfile)
 
-    puts "Removing old versions of rexml < 3.4.2"
     env = with_standard_compiler_flags(with_embedded_path)
 
-    # remove [-a] all rexml < 3.4.2 including [-x] executables and [-I] ignore dependencies
-    command "#{gemfile} uninstall rexml -v \"<3.4.2\" -a -x -I", env: env
+    gems_to_remove.each do |gem_name, version_constraint|
+      puts "Removing old versions of #{gem_name} #{version_constraint}"
+      # remove [-a] all versions matching constraint including [-x] executables and [-I] ignore dependencies
+      command "#{gemfile} uninstall #{gem_name} -v \"#{version_constraint}\" -a -x -I", env: env
+    end
   end
 end
