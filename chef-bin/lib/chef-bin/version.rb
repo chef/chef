f470feac0fdc04124d3064761fe1f85b98bcdd13
Chef Expeditor <chef-ci@chef.io>
2026-03-04 18:00:05 +0000

    Bump version to 19.2.7 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-bin/lib/chef-bin/version.rb
+++ chef-bin/lib/chef-bin/version.rb
@@ -21,7 +21,7 @@
 
 module ChefBin
   CHEFBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "19.2.6".freeze
+  VERSION = "19.2.7".freeze
 end
 
 #
--- chef-config/lib/chef-config/version.rb
+++ chef-config/lib/chef-config/version.rb
@@ -15,5 +15,5 @@
 
 module ChefConfig
   CHEFCONFIG_ROOT = File.expand_path("..", __dir__)
-  VERSION = "19.2.6".freeze
+  VERSION = "19.2.7".freeze
 end
