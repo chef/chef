4203ca936ee9da770a5395e9428a41ecceb75e04
poorndm <poorndm@progress.com>
2026-03-20 11:23:40 +0530

    Test with latest plugin version
    Signed-off-by: poorndm <poorndm@progress.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -190,7 +190,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.102:"
+    echo "  - chef/omnibus#66b8821b81d1b226dac67f177cc945c51e730c39:"
     echo "      build: chef"
     echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
     echo "      config: omnibus/omnibus.rb"
@@ -213,7 +213,7 @@ then
     echo "  agents:"
     [[ $platform == *"arm"* ]] && echo "    queue: omnibus-mac_os_x-12-arm64" || echo "    queue: omnibus-mac_os_x-12-x86_64" || echo "    queue: omnibus-mac_os_x-13-arm64" || echo "    queue: omnibus-mac_os_x-14-arm64"
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.102:"
+    echo "  - chef/omnibus#66b8821b81d1b226dac67f177cc945c51e730c39:"
     echo "      config: omnibus/omnibus.rb"
     [[ $platform == *"arm"* ]] && echo "      remote-host: buildkite-omnibus-mac_os_x-12-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-12-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-13-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-14-arm64"
     echo "      notarize-macos-package: chef"
@@ -234,7 +234,7 @@ then
   echo "- key: create-build-record"
   echo "  label: \":artifactory: Create Build Record\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.102:"
+  echo "  - chef/omnibus#66b8821b81d1b226dac67f177cc945c51e730c39:"
   echo "      create-build-record: chef"
 fi
 
@@ -329,7 +329,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.102:"
+    echo "  - chef/omnibus#66b8821b81d1b226dac67f177cc945c51e730c39:"
     echo "      test: chef"
     echo "      test-path: omnibus/omnibus-test.sh"
     echo "      install-dir: \"/opt/chef\""
@@ -355,6 +355,6 @@ then
   echo "- key: promote"
   echo "  label: \":artifactory: Promote to Current\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.102:"
+  echo "  - chef/omnibus#66b8821b81d1b226dac67f177cc945c51e730c39:"
   echo "      promote: chef"
 fi
