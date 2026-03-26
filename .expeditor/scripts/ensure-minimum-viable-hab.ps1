5e97323b89415de6b2fc3c28bb7ef4b4b98f8eff
John McCrae <john.mccrae@progress.com>
2026-03-26 13:58:01 +0000

    habitat version pin (#15736)
    * Updating the hab installers to ensure the older version
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -1,17 +1,27 @@
+$required_hab_version = [Version]"1.6.1245"
+$hab_install_script_url = 'https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'
+$hab_install_script_path = Join-Path $env:TEMP 'hab-install.ps1'
+
+function Install-RequiredHabitat {
+    $web_client = New-Object System.Net.WebClient
+    $web_client.DownloadFile($hab_install_script_url, $hab_install_script_path)
+    & $hab_install_script_path -Version $required_hab_version
+}
+
 try {
     [Version]$hab_version = (hab --version).split(" ")[1].split("/")[0]
-    if ($hab_version -lt [Version]"0.85.0" ) {
-        Write-Host "--- :habicat: Installing the version of Habitat required"
+    if ($hab_version -ne $required_hab_version) {
+        Write-Host "--- :habicat: Installing Habitat $required_hab_version"
         Set-ExecutionPolicy Bypass -Scope Process -Force
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
-        if (-not $?) { throw "Hab version is older than 0.85 and could not update it." }
+        Install-RequiredHabitat
+        if (-not $?) { throw "Hab version is $hab_version and could not be updated to $required_hab_version." }
     } else {
-        Write-Host "--- :habicat: :thumbsup: Minimum required version of Habitat already installed"
+        Write-Host "--- :habicat: :thumbsup: Required Habitat version $required_hab_version already installed"
     }
 }
 catch {
-    # This install fails if Hab isn't on the path when we check for the version. This ensures it is installed
-    Write-Host "--- :habicat: Installing the version of Habitat required"
+    # This install fails if Hab isn't on the path when we check for the version. This ensures the required version is installed.
+    Write-Host "--- :habicat: Installing Habitat $required_hab_version"
     Set-ExecutionPolicy Bypass -Scope Process -Force
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
+    Install-RequiredHabitat
 }
