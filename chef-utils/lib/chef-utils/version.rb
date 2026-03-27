9dc212d351efe1943758312f392da5baf386d3ba
Chef Expeditor <chef-ci@chef.io>
2026-03-27 22:04:08 +0000

    Bump version to 19.2.26 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- chef-utils/lib/chef-utils/version.rb
+++ chef-utils/lib/chef-utils/version.rb
@@ -16,5 +16,5 @@
 
 module ChefUtils
   CHEFUTILS_ROOT = File.expand_path("..", __dir__)
-  VERSION = "19.2.25"
+  VERSION = "19.2.26"
 end
--- knife/lib/chef/knife/version.rb
+++ knife/lib/chef/knife/version.rb
@@ -17,6 +17,6 @@
 class Chef
   class Knife
     KNIFE_ROOT = File.expand_path("../..", __dir__)
-    VERSION = "19.2.25".freeze
+    VERSION = "19.2.26".freeze
   end
 end
