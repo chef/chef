f695b6cf7293896405fa8aed522dfb87374ee845
Phil Dibowitz <phil@ipom.com>
2026-04-22 12:17:39 -0700

    Revert "Omnibus privatization submodule (#15659)"
    You cannot make the build infrastructure for an OPEN SOURCE project be
    private. That is unacceptable.
    
    This reverts commit 623d32af0a024a4fdd144159d6efd4496b841460.
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- .buildkite/build-test-omnibus.sh
+++ .buildkite/build-test-omnibus.sh
@@ -116,7 +116,6 @@ then
       echo "      propagate-environment: true"
       echo "      environment:"
       echo "        - ARTIFACTORY_PASSWORD"
-      echo "        - GITHUB_TOKEN"
       echo "        - ARTIFACTORY_API_KEY"
       echo "        - RPM_SIGNING_KEY"
       echo "        - CHEF_FOUNDATION_VERSION"
@@ -144,7 +143,6 @@ then
       echo "      environment:"
       echo "        - CHEF_FOUNDATION_VERSION"
       echo "        - BUILDKITE_AGENT_ACCESS_TOKEN"
-      echo "        - GITHUB_TOKEN"
       echo "        - ARTIFACTORY_PASSWORD"
       echo "        - ARTIFACTORY_API_KEY"
       echo "        - AWS_ACCESS_KEY_ID"
@@ -190,7 +188,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.103:"
+    echo "  - chef/omnibus#v0.2.102:"
     echo "      build: chef"
     echo "      chef-foundation-version: $CHEF_FOUNDATION_VERSION"
     echo "      config: omnibus/omnibus.rb"
@@ -213,7 +211,7 @@ then
     echo "  agents:"
     [[ $platform == *"arm"* ]] && echo "    queue: omnibus-mac_os_x-12-arm64" || echo "    queue: omnibus-mac_os_x-12-x86_64" || echo "    queue: omnibus-mac_os_x-13-arm64" || echo "    queue: omnibus-mac_os_x-14-arm64"
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.103:"
+    echo "  - chef/omnibus#v0.2.102:"
     echo "      config: omnibus/omnibus.rb"
     [[ $platform == *"arm"* ]] && echo "      remote-host: buildkite-omnibus-mac_os_x-12-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-12-x86_64" || echo "      remote-host: buildkite-omnibus-mac_os_x-13-arm64" || echo "      remote-host: buildkite-omnibus-mac_os_x-14-arm64"
     echo "      notarize-macos-package: chef"
@@ -234,7 +232,7 @@ then
   echo "- key: create-build-record"
   echo "  label: \":artifactory: Create Build Record\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.103:"
+  echo "  - chef/omnibus#v0.2.102:"
   echo "      create-build-record: chef"
 fi
 
@@ -329,7 +327,7 @@ then
       echo "    omnibus-toolchain: \"*\""
     fi
     echo "  plugins:"
-    echo "  - chef/omnibus#v0.2.103:"
+    echo "  - chef/omnibus#v0.2.102:"
     echo "      test: chef"
     echo "      test-path: omnibus/omnibus-test.sh"
     echo "      install-dir: \"/opt/chef\""
@@ -355,6 +353,6 @@ then
   echo "- key: promote"
   echo "  label: \":artifactory: Promote to Current\""
   echo "  plugins:"
-  echo "  - chef/omnibus#v0.2.103:"
+  echo "  - chef/omnibus#v0.2.102:"
   echo "      promote: chef"
 fi
