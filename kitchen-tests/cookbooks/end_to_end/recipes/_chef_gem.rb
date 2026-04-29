d648cf4bdcaebbe99d1875aa0f6837d66213820c
Thomas Powell <thomas.powell@progress.com>
2026-04-29 13:47:52 -0400

    Test mysql2 gem install
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_chef_gem.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_chef_gem.rb
@@ -16,3 +16,18 @@ chef_gem "aws-sdk-ec2" do
   action :install
   compile_time false
 end
+
+# Install prerequisites for building the mysql2 gem native extensions
+mysql2_dev_pkg = value_for_platform_family(
+  "debian" => "default-libmysqlclient-dev",
+  "rhel" => "mariadb-devel",
+  "fedora" => "mariadb-devel",
+  "suse" => "libmariadb-devel",
+  "amazon" => node["platform_version"].to_i >= 2023 ? "mariadb105-devel" : "mariadb-devel"
+)
+
+package mysql2_dev_pkg if mysql2_dev_pkg
+
+chef_gem "mysql2" do
+  compile_time false
+end
