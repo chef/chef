47299d1c288d82593f46478c94a1f3be1e532b89
John Haggerty <john.haggerty@optum.com>
2026-05-22 16:45:38 -0500

    Added permission overrides for client dirs
    Signed-off-by: John Haggerty <john.haggerty@optum.com>

--- lib/chef/resource/chef_client_config.rb
+++ lib/chef/resource/chef_client_config.rb
@@ -31,9 +31,6 @@ class Chef
         backups: { mode: "0750", inherits: :auto },
       }.freeze
 
-      provides :chef_client_config, target_mode: true
-      target_mode support: :full
-
       description "Use the **chef_client_config** resource to create a client.rb file in the #{ChefUtils::Dist::Infra::PRODUCT} configuration directory. See the [client.rb docs](https://docs.chef.io/config_rb_client/) for more details on options available in the client.rb configuration file."
       introduced "16.6"
       examples <<~DOC
