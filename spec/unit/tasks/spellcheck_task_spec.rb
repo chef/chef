81ffb571d794e498547675a9623a572d43d4c91a
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2026-05-28 21:46:37 +0530

    ci(lint): add strict cookstyle gate for spec/unit/tasks
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- spec/unit/tasks/spellcheck_task_spec.rb
+++ spec/unit/tasks/spellcheck_task_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # Copyright:: Copyright (c) 2009-2026 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
 # License:: Apache License, Version 2.0
