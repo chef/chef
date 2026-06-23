211add0d1264408b6e1e3369cef9f89a80251604
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2026-06-23 17:35:12 +0530

    bump required hab to 2.1.23 in ci (#16133)
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/ensure-minimum-viable-hab.ps1
+++ .expeditor/scripts/ensure-minimum-viable-hab.ps1
@@ -1,4 +1,4 @@
-$HabitatVersion = if ($env:HAB_VERSION) { $env:HAB_VERSION } else { '2.0.504' }
+$HabitatVersion = if ($env:HAB_VERSION) { $env:HAB_VERSION } else { '2.1.23' }
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
