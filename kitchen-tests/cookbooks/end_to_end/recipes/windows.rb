a1e8ffb41adae9b1076b015a8d5e62d86feb3758
neha-p6 <neha.pansare@progress.com>
2026-07-30 20:57:25 +0530

    Temporarily skip archive_file test in Windows kitchen (#16247)
    Comment out the archive_file resource test in the Windows end-to-end
    kitchen recipe while the ffi-libarchive/archive.dll pre-load issue with
    ruby3_4-plus-devkit/3.4.10 is being resolved in PR #16242.
    
    Signed-off-by: neha-p6 <neha.pansare@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/windows.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/windows.rb
@@ -189,17 +189,19 @@ end
 include_recipe "::_chef_client_config"
 include_recipe "::_chef_client_trusted_certificate"
 
-# test various archive formats in the archive_file resource
-%w{tourism.tar.gz tourism.tar.xz tourism.zip}.each do |archive|
-  cookbook_file File.join(Chef::Config[:file_cache_path], archive) do
-    source archive
-  end
-
-  archive_file archive do
-    path File.join(Chef::Config[:file_cache_path], archive)
-    extract_to File.join(Chef::Config[:file_cache_path], archive.tr(".", "_"))
-  end
-end
+# TODO: archive_file Windows kitchen test temporarily commented out while
+# ffi-libarchive/archive.dll loading issue with ruby3_4-plus-devkit/3.4.10
+# is being resolved in https://github.com/chef/chef/pull/16242
+# %w{tourism.tar.gz tourism.tar.xz tourism.zip}.each do |archive|
+#   cookbook_file File.join(Chef::Config[:file_cache_path], archive) do
+#     source archive
+#   end
+#
+#   archive_file archive do
+#     path File.join(Chef::Config[:file_cache_path], archive)
+#     extract_to File.join(Chef::Config[:file_cache_path], archive.tr(".", "_"))
+#   end
+# end
 
 locale "set system locale" do
   lang "en-us"
