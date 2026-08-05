22d24ea092801d5d7e2690287ca560f9f7a7dd8f
neha-p6 <neha.pansare@progress.com>
2026-08-05 16:33:01 +0530

    Add back Windows kitchen recipes after libarchive fix
    Reverts temporary workarounds from #16247 and #16249 that commented
    out archive_file and habitat_install tests in Windows kitchen recipes.
    
    The root cause was a missing z.dll dependency in core/libarchive 3.8.1
    (introduced in base-2025 channel). This has been fixed upstream in the
    core/libarchive and core/zlib packages. Windows kitchen tests are now
    restored.
    
    Also removes the two kitchen recipe files from chronoguard.yml watched
    file list that were added as part of the workaround.
    
    Signed-off-by: neha-p6 <neha.pansare@progress.com>

--- kitchen-tests/cookbooks/end_to_end/recipes/_habitat_win_package.rb
+++ kitchen-tests/cookbooks/end_to_end/recipes/_habitat_win_package.rb
@@ -1,9 +1,6 @@
-# TODO: habitat_install Windows kitchen test temporarily commented out while
-# ffi-libarchive/archive.dll loading issue with ruby3_4-plus-devkit/3.4.10
-# is being resolved in https://github.com/chef/chef/pull/16242 - fail-after:2026-08-13
-# habitat_install "default" do
-#   license "accept"
-# end
+habitat_install "default" do
+  license "accept"
+end
 
 habitat_package "chef/splunkforwarder" do
   version "7.0.3/20250714155325"
