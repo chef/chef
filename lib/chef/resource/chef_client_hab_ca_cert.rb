4b433816dbf25ca47dfb95854f377e7e48bbedc5
Corey Hemminger <hemminger@hotmail.com>
2026-08-24 11:32:28 -0500

    add hab license acceptance to hab command
    chef_client_hab_ca_cert resource shells out to hab cli to get the pkg path for finding the ca cert that ruby is using for ssl verification. If a user hasn't accepted hab license this fails. This would be especially prevalent on newly provisioned system running chef-client for the first time.
    
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- lib/chef/resource/chef_client_hab_ca_cert.rb
+++ lib/chef/resource/chef_client_hab_ca_cert.rb
@@ -107,7 +107,7 @@ class Chef
           # Result should be: chef/chef-infra-client/VERSION/RELEASE
           package_ident = ::File.join(Pathname.new(current_chef_path).each_filename.to_a[2..5])
 
-          ca_pkg = shell_out("#{current_hab_path} pkg dependencies #{package_ident}")
+          ca_pkg = shell_out("HAB_LICENSE=accept-no-persist #{current_hab_path} pkg dependencies #{package_ident}")
           if ca_pkg.error?
             raise "Failed to determine CA Certs for the #{ChefUtils::Dist::Infra::PRODUCT}'s habitat package"
           end
@@ -118,7 +118,7 @@ class Chef
             raise "Unable to find 'core/cacerts' package in dependencies. Failed to determine CA Certs."
           end
 
-          ca_path = shell_out("#{current_hab_path} pkg path #{hab_cacerts_pkg}")
+          ca_path = shell_out("HAB_LICENSE=accept-no-persist #{current_hab_path} pkg path #{hab_cacerts_pkg}")
           if ca_path.error?
             raise "Unable to find path for the 'core/cacerts' habitat package."
           end
