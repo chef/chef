51e5ef2840b486601e609c09520406b27f9a3dd6
John McCrae <mccrae@progress.com>
2026-09-14 19:24:06 +0000

    correcting the missing hab token
    Signed-off-by: John McCrae <mccrae@progress.com>

--- .buildkite/verify.pipeline.sh
+++ .buildkite/verify.pipeline.sh
@@ -64,6 +64,7 @@ for platform in ${win_test_platforms[@]}; do
   echo "      - \"-Command\""
   echo "      environment:"
   echo "        - CHEF_FOUNDATION_VERSION"
+  echo "        - HAB_AUTH_TOKEN"
   echo "      propagate-environment: true"
   echo "  commands:"
   echo "    - .\.expeditor\scripts\prep_and_run_tests.ps1 {{matrix}}"
@@ -82,7 +83,6 @@ for platform in ${win_test_platforms[@]}; do
   echo "    queue: single-use-windows-2019-privileged"
   echo "  env:"
   echo "    CHEF_FOUNDATION_VERSION:"
-  echo "    HAB_AUTH_TOKEN:"
   echo "  timeout_in_minutes: 120"
 done
 
