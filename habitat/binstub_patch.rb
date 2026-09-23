55475659124302f686bfcd819d3ae1c899ac5014
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2026-09-23 12:20:52 +0530

    isolate GEM_HOME and GEM_PATH in binstubs
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>

--- habitat/binstub_patch.rb
+++ habitat/binstub_patch.rb
@@ -1,4 +1,6 @@
 unless ENV["APPBUNDLER_ALLOW_RVM"]
+  vendor_dir = File.expand_path(File.join(__dir__, "..", "vendor"))
   ENV["APPBUNDLER_ALLOW_RVM"] = "true"
-  ENV["GEM_PATH"] = [File.expand_path(File.join(__dir__, "..", "vendor")), ENV["GEM_PATH"]].compact.join(File::PATH_SEPARATOR)
+  ENV["GEM_PATH"] = vendor_dir
+  ENV["GEM_HOME"] = vendor_dir
 end
